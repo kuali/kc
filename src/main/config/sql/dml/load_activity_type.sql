2fb4c0823a66b4545b105b5e8b372b21bb060f11
Geoff McGregor <gmcgrego@iu.edu>
2007-07-12 14:02:34 +0000

    ref. tables - ddl/dml

---
+++ src/main/config/sql/dml/load_activity_type.sql
@@ -0,0 +1,9 @@
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('1','Organized Research',sysdate,user);
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('2','Instruction and Departmental Research',sysdate,user);
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('3','Fellowship - Pre-Doctoral',sysdate,user);
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('4','Public Service',sysdate,user);
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('5','Student Services',sysdate,user);
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('6','Other',sysdate,user);
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('7','Fellowship - Post-Doctoral',sysdate,user);
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('8','Clinical Trial',sysdate,user);
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('9','Construction',sysdate,user);
---
+++ src/main/config/sql/tables/proposal_response.ddl
@@ -0,0 +1,11 @@
+create table proposal_response 
+(
+  proposal_response_code  number (3) constraint proposal_response_n1 not null,
+  description         varchar2 (200) constraint proposal_response_n2 not null,
+  update_timestamp    date constraint proposal_response_n3 not null,
+  update_user         varchar2 (8)  constraint proposal_response_n4 not null,
+  VER_NBR NUMBER(8,0) DEFAULT 1 constraint proposal_response_n5 NOT NULL,
+  OBJ_ID VARCHAR2(36) DEFAULT SYS_GUID() constraint proposal_response_n6 NOT NULL,
+  constraint proposal_response_p1 primary key (proposal_response_code),
+  constraint activity_type_c0 unique (obj_id)
+);
