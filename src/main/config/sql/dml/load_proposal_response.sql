47268599a9188a688b04fa498a2ce682468fd61b
Geoff McGregor <gmcgrego@iu.edu>
2007-07-12 14:02:34 +0000

    ref. tables - ddl/dml

---
+++ src/main/config/sql/dml/load_proposal_response.sql
@@ -0,0 +1,7 @@
+insert into proposal_response(proposal_response_code,description,update_timestamp,update_user) values('1','Federal Solicitation',sysdate,user);
+insert into proposal_response(proposal_response_code,description,update_timestamp,update_user) values('2','Unsolicited',sysdate,user);
+insert into proposal_response(proposal_response_code,description,update_timestamp,update_user) values('3','Verbal Request for Proposal',sysdate,user);
+insert into proposal_response(proposal_response_code,description,update_timestamp,update_user) values('4','SBIR Solicitation',sysdate,user);
+insert into proposal_response(proposal_response_code,description,update_timestamp,update_user) values('5','STTR Solicitation',sysdate,user);
+insert into proposal_response(proposal_response_code,description,update_timestamp,update_user) values('6','Non-Federal Solicitation',sysdate,user);
+insert into proposal_response(proposal_response_code,description,update_timestamp,update_user) values('7','MIT Alliance/Internal',sysdate,user);
