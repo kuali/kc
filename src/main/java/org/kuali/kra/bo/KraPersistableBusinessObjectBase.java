4e3762f3a83ecff09b7385beddbd022620551391
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2007-07-13 20:29:11 +0000

    KRACOEUS-95 - Updated KraPersistableBusinessObjectBase to set updateUser and updateTimestamp in the beforeSave() and beforeInsert() methods

--- src/main/java/org/kuali/kra/bo/KraPersistableBusinessObjectBase.java
+++ src/main/java/org/kuali/kra/bo/KraPersistableBusinessObjectBase.java
@@ -2,13 +2,47 @@ package org.kuali.kra.bo;
 
 import java.sql.Timestamp;
 
+import org.apache.ojb.broker.PersistenceBroker;
+import org.apache.ojb.broker.PersistenceBrokerException;
 import org.kuali.core.bo.PersistableBusinessObjectBase;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.core.service.DateTimeService;
 
 public abstract class KraPersistableBusinessObjectBase extends PersistableBusinessObjectBase {
-	
+
 	private String updateUser;
 	private Timestamp updateTimestamp;
-	
+
+    /**
+     * @see org.kuali.core.bo.PersistableBusinessObjectBase#beforeInsert()
+     */
+    @Override
+    public void beforeInsert(PersistenceBroker persistenceBroker) throws PersistenceBrokerException {
+        super.beforeInsert(persistenceBroker);
+        setUpdateFields();
+    }
+
+    /**
+     * @see org.kuali.core.bo.PersistableBusinessObjectBase#beforeInsert()
+     */
+    @Override
+    public void beforeUpdate(PersistenceBroker persistenceBroker) throws PersistenceBrokerException {
+        super.beforeUpdate(persistenceBroker);
+        setUpdateFields();
+    }
+
+    /**
+     * Set updateTimestamp and updateUser prior to persistence
+     */
+    private void setUpdateFields() {
+        Timestamp updateTimestamp = ((DateTimeService)KraServiceLocator.getService("dateTimeService")).getCurrentTimestamp();
+
+        setUpdateTimestamp(updateTimestamp);
+        // Since the UPDATE_USER column is only VACHAR(8), we need to truncate this string
+        setUpdateUser(GlobalVariables.getUserSession().getLoggedInUserNetworkId().substring(0, 8));
+    }
+
 	public Timestamp getUpdateTimestamp() {
 		return updateTimestamp;
 	}
