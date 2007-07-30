7ccbc60842794b51cd3e49c00e4065cdce678270
Lin-Long Shyu <lshyukuali@gmail.com>
2007-07-30 12:42:13 +0000

    KRACOEUS60 : Proposal Type - Create table, OJB mapping, BO, Data Dictionary, and Maintenance Doc files - initial commit

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/bo/ProposalType.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.bo;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+public class ProposalType  extends KraPersistableBusinessObjectBase {
+
+    private Integer proposalTypeCode;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getProposalTypeCode() {
+        return proposalTypeCode;
+    }
+
+    public void setProposalTypeCode(Integer proposalTypeCode) {
+        this.proposalTypeCode = proposalTypeCode;
+    }
+
+    @Override
+    protected LinkedHashMap toStringMapper() {
+        LinkedHashMap propMap = new LinkedHashMap();
+        propMap.put("proposalTypeCode", this.getProposalTypeCode());
+        propMap.put("description", this.getDescription());
+        propMap.put("updateTimestamp", this.getUpdateTimestamp());
+        propMap.put("updateUser", this.getUpdateUser());
+        return propMap;
+    }
+
+}
+
+
