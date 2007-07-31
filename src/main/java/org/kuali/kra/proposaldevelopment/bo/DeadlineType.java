aff28343c007e8f50def41bc1d0613aba6c2ddb9
Geo Thomas <geot@mit.edu>
2007-07-31 23:02:32 +0000

    JIRA # KRACOEUS-62
    Proposal Deadline Type - Create table, OJB mapping, BO, Data Dictionary, and Maintenance Doc files

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/bo/DeadlineType.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2006-2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.bo;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+/**
+ * Class representation of the Deadline Type Business Object
+ *
+ */
+public class DeadlineType extends KraPersistableBusinessObjectBase {
+	
+	private String deadlineTypeCode;
+	private String description;
+	
+    /**
+     * Gets the deadlineTypeCode attribute. 
+     * @return Returns the deadlineTypeCode.
+     */
+    public String getDeadlineTypeCode() {
+        return deadlineTypeCode;
+    }
+
+    /**
+     * Sets the deadlineTypeCode attribute value.
+     * @param deadlineTypeCode The deadlineTypeCode to set.
+     */
+    public void setDeadlineTypeCode(String deadlineTypeCode) {
+        this.deadlineTypeCode = deadlineTypeCode;
+    }
+
+    /**
+     * Retrieves the description attribute
+     * 
+     * @return String
+     */
+	public String getDescription() {
+		return description;
+	}
+    
+    /**
+     * Assigns the description attribute
+     *
+     * @param description
+     */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+    @Override
+    protected LinkedHashMap toStringMapper() {
+        LinkedHashMap propMap = new LinkedHashMap();
+        propMap.put("deadlineTypeCode", this.getDeadlineTypeCode());
+        propMap.put("description", this.getDescription());
+        propMap.put("updateTimestamp", this.getUpdateTimestamp());
+        propMap.put("updateUser", this.getUpdateUser());
+        return propMap;
+    }
+
+}
