b2ee9a2114eff990d193008e160cae62d2c36722
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2007-08-03 13:57:51 +0000

    KRACOEUS-101: Sponsor and Program Information Panel - mostly unit tests for ValuesFinders - changed NoticeOfOpportunity key from Integer to String

---
+++ src/test/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/DeadlineTypeValuesFinderTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ *
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.opensource.org/licenses/ecl1.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.lookup.keyvalue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.core.web.ui.KeyLabelPair;
+import org.kuali.kra.keyvalue.ValuesFinderTestBase;
+
+/**
+ * This class tests DeadlineTypeValuesFinder.
+ */
+public class DeadlineTypeValuesFinderTest extends ValuesFinderTestBase {
+
+    public DeadlineTypeValuesFinderTest() {
+        setTestClass(DeadlineTypeValuesFinder.class);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test public void testGetKeyValues() throws Exception {
+        super.testGetKeyValues();
+    }
+
+    @Override
+    protected void addKeyValues() {
+        testKeyValues.add(new KeyLabelPair("", "select:"));
+        testKeyValues.add(new KeyLabelPair("1", "postmark"));
+        testKeyValues.add(new KeyLabelPair("2", "target"));
+        testKeyValues.add(new KeyLabelPair("3", "receipt"));
+    }
+
+}
