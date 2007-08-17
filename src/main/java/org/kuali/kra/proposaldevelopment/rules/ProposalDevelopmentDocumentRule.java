5ecda2bef99fbc67b34665fefcea32c0cbbeda2a
Lin-Long Shyu <lshyukuali@gmail.com>
2007-08-17 21:12:03 +0000

    KRACOEUS-148 : Implement Special Review page in Proposal Development

--- src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentDocumentRule.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentDocumentRule.java
@@ -19,6 +19,8 @@ import org.kuali.core.document.Document;
 import org.kuali.core.rules.DocumentRuleBase;
 import org.kuali.core.util.ErrorMap;
 import org.kuali.core.util.GlobalVariables;
+import org.kuali.kra.bo.SpecialReview;
+import org.kuali.kra.bo.ValidSpRevApproval;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.kra.proposaldevelopment.bo.PropSpecialReview;
@@ -27,10 +29,11 @@ import org.kuali.rice.KNSServiceLocator;
 
 /**
  * This class...
+ * 
  * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
  */
 public class ProposalDevelopmentDocumentRule extends DocumentRuleBase {
-    
+
     @Override
     protected boolean processCustomSaveDocumentBusinessRules(Document document) {
         if (!(document instanceof ProposalDevelopmentDocument)) {
@@ -42,49 +45,79 @@ public class ProposalDevelopmentDocumentRule extends DocumentRuleBase {
         ProposalDevelopmentDocument proposalDevelopmentDocument = (ProposalDevelopmentDocument) document;
 
         GlobalVariables.getErrorMap().addToErrorPath("document");
-        
-        //changing this to '0' so it doesn't validate reference objects within a list
-        KNSServiceLocator.getDictionaryValidationService().validateDocumentRecursively(proposalDevelopmentDocument, 0);
-        valid &= processSpecialReviewApprovalStatus(proposalDevelopmentDocument);
-        
-        GlobalVariables.getErrorMap().removeFromErrorPath("document");
-        
 
-        
+        // changing this to '0' so it doesn't validate reference objects within a list
+        KNSServiceLocator.getDictionaryValidationService().validateDocumentRecursively(proposalDevelopmentDocument, 0);
+        valid &= processSpecialReviewBusinessRule(proposalDevelopmentDocument);
+
+        GlobalVariables.getErrorMap().removeFromErrorPath("document");
+
+
         return valid;
     }
-    
-    private boolean processSpecialReviewApprovalStatus(ProposalDevelopmentDocument proposalDevelopmentDocument) {
+
+    private boolean processSpecialReviewBusinessRule(ProposalDevelopmentDocument proposalDevelopmentDocument) {
         boolean valid = true;
-        
+
         ErrorMap errorMap = GlobalVariables.getErrorMap();
-        
+
         int i = 0;
-        
+
         for (PropSpecialReview propSpecialReview : proposalDevelopmentDocument.getPropSpecialReviews()) {
             errorMap.addToErrorPath("propSpecialReviews[" + i + "]");
             propSpecialReview.refresh();
-            
-            if (propSpecialReview.getApprovalTypeCode().intValue() == Constants.APPROVAL_STATUS.intValue()) {
-               if (propSpecialReview.getApplicationDate()==null) {
-                   valid = false;
-                   errorMap.putError("applicationDate", KeyConstants.ERROR_REQUIRED_FOR_APPROVED_SPECIALREVIEW,"Application Date");
-               }
-               if (propSpecialReview.getApprovalDate()==null){
-                   valid = false;
-                   errorMap.putError("approvalDate", KeyConstants.ERROR_REQUIRED_FOR_APPROVED_SPECIALREVIEW,"Approval Date");
-               } 
-               if (propSpecialReview.getProtocolNumber()==null  ) {
-                   valid = false;
-                   errorMap.putError("protocolNumber", KeyConstants.ERROR_REQUIRED_FOR_APPROVED_SPECIALREVIEW,"Protocol Number");
-               }
-            } else {
-                if (propSpecialReview.getApprovalDate()!=null){
-                    valid = false;
-                    errorMap.putError("approvalDate", KeyConstants.ERROR_NOT_APPROVED_SPECIALREVIEW,"Approval Date");
-                }                 
+            propSpecialReview.refreshReferenceObject("validSpRevApproval");
+            if (propSpecialReview.getApprovalTypeCode() != null && propSpecialReview.getSpecialReviewCode() != null) {
+                ValidSpRevApproval validSpRevApproval = propSpecialReview.getValidSpRevApproval();
+                if (validSpRevApproval != null) {
+                    if (validSpRevApproval.isProtocolNumberFlag() && propSpecialReview.getProtocolNumber() == null) {
+                        valid = false;
+                        errorMap.putError("protocolNumber", KeyConstants.ERROR_REQUIRED_FOR_VALID_SPECIALREVIEW, "Protocol Number",
+                                validSpRevApproval.getSpecialReview().getDescription() + "/"
+                                        + validSpRevApproval.getSpecialReviewApprovalType().getDescription());
+                    }
+                    if (validSpRevApproval.isApplicationDateFlag() && propSpecialReview.getApplicationDate() == null) {
+                        valid = false;
+                        errorMap.putError("applicationDate", KeyConstants.ERROR_REQUIRED_FOR_VALID_SPECIALREVIEW,
+                                "Protocol Number", validSpRevApproval.getSpecialReview().getDescription() + "/"
+                                        + validSpRevApproval.getSpecialReviewApprovalType().getDescription());
+                    }
+                    if (validSpRevApproval.isApprovalDateFlag() && propSpecialReview.getApprovalDate() == null) {
+                        valid = false;
+                        errorMap.putError("approvalDate", KeyConstants.ERROR_REQUIRED_FOR_VALID_SPECIALREVIEW, "Protocol Number",
+                                validSpRevApproval.getSpecialReview().getDescription() + "/"
+                                        + validSpRevApproval.getSpecialReviewApprovalType().getDescription());
+                    }
+
+                }
+
             }
-            
+            // TODO : are all the sp rev business rules defined in valid_sp_rev_approval table too?
+            // If they are, then the following are not needed
+            if (propSpecialReview.getApprovalTypeCode() != null) {
+                if (propSpecialReview.getApprovalTypeCode().intValue() == Constants.APPROVAL_STATUS.intValue()) {
+                    if (propSpecialReview.getApplicationDate() == null) {
+                        valid = false;
+                        errorMap.putError("applicationDate", KeyConstants.ERROR_REQUIRED_FOR_APPROVED_SPECIALREVIEW,
+                                "Application Date");
+                    }
+                    if (propSpecialReview.getApprovalDate() == null) {
+                        valid = false;
+                        errorMap.putError("approvalDate", KeyConstants.ERROR_REQUIRED_FOR_APPROVED_SPECIALREVIEW, "Approval Date");
+                    }
+                    if (propSpecialReview.getProtocolNumber() == null) {
+                        valid = false;
+                        errorMap.putError("protocolNumber", KeyConstants.ERROR_REQUIRED_FOR_APPROVED_SPECIALREVIEW,
+                                "Protocol Number");
+                    }
+                }
+                else {
+                    if (propSpecialReview.getApprovalDate() != null) {
+                        valid = false;
+                        errorMap.putError("approvalDate", KeyConstants.ERROR_NOT_APPROVED_SPECIALREVIEW, "Approval Date");
+                    }
+                }
+            }
             errorMap.removeFromErrorPath("propSpecialReviews[" + i++ + "]");
         }
         return valid;
