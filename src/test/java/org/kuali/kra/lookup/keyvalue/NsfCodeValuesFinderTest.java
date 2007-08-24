5a63e9681260dbe6dab68713e926a1ad401e156a
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2007-08-24 14:03:45 +0000

    made values finder tests consistent with load* table scripts (temporary fix for Bamboo test failures)

--- src/test/java/org/kuali/kra/lookup/keyvalue/NsfCodeValuesFinderTest.java
+++ src/test/java/org/kuali/kra/lookup/keyvalue/NsfCodeValuesFinderTest.java
@@ -47,13 +47,44 @@ public class NsfCodeValuesFinderTest extends ValuesFinderTestBase {
     @Override
     protected void addKeyValues() {
         testKeyValues.add(new KeyLabelPair("", "select:"));
-        testKeyValues.add(new KeyLabelPair("J.02", "Law - Non-Science and Engineering Fields: J.02"));
-        testKeyValues.add(new KeyLabelPair("A.01", "Aeronautical and Astronautical - Engineering: A.01"));
-        testKeyValues.add(new KeyLabelPair("F.01", "Agricultural - Life Sciences: F.01"));
-        testKeyValues.add(new KeyLabelPair("B.01", "Astronomy - Physical Sciences: B.01"));
-        testKeyValues.add(new KeyLabelPair("C.01", "Atmospheric - Environmental Sciences: C.01"));
-        testKeyValues.add(new KeyLabelPair("A.02", "Bioengineering/Biomedical - Engineering: A.02"));
-        testKeyValues.add(new KeyLabelPair("F.02", "Biological - Life Sciences: F.02"));
+        testKeyValues.add(new KeyLabelPair("J.02","Law - Non-Science and Engineering Fields: J.02"));
+        testKeyValues.add(new KeyLabelPair("A.01","Aeronautical and Astronautical - Engineering: A.01"));
+        testKeyValues.add(new KeyLabelPair("F.01","Agricultural - Life Sciences: F.01"));
+        testKeyValues.add(new KeyLabelPair("B.01","Astronomy - Physical Sciences: B.01"));
+        testKeyValues.add(new KeyLabelPair("C.01","Atmospheric - Environmental Sciences: C.01"));
+        testKeyValues.add(new KeyLabelPair("A.02","Bioengineering/Biomedical - Engineering: A.02"));
+        testKeyValues.add(new KeyLabelPair("F.02","Biological - Life Sciences: F.02"));
+        testKeyValues.add(new KeyLabelPair("J.05","Business and Management - Non-Science and Engineering Fields: J.05"));
+        testKeyValues.add(new KeyLabelPair("A.03","Chemical - Engineering: A.03"));
+        testKeyValues.add(new KeyLabelPair("B.02","Chemistry - Physical Sciences: B.02"));
+        testKeyValues.add(new KeyLabelPair("A.04","Civil - Engineering: A.04"));
+        testKeyValues.add(new KeyLabelPair("J.06","Communications, Journalism and Library Sciences - Non-Science and Engineering Fields: J.06"));
+        testKeyValues.add(new KeyLabelPair("E.01","Computer Sciences: E.01"));
+        testKeyValues.add(new KeyLabelPair("Z.99","Costs not included in NSF report - used for reconciliation purposes: Z.99"));
+        testKeyValues.add(new KeyLabelPair("J.98","Costs to be allocated: J.98"));
+        testKeyValues.add(new KeyLabelPair("C.02","Earth Sciences - Environmental Sciences: C.02"));
+        testKeyValues.add(new KeyLabelPair("H.01","Economics - Social Sciences: H.01"));
+        testKeyValues.add(new KeyLabelPair("J.01","Education - Non-Science and Engineering Fields: J.01"));
+        testKeyValues.add(new KeyLabelPair("A.05","Electrical - Engineering: A.05"));
+        testKeyValues.add(new KeyLabelPair("J.03","Humanities - Non-Science and Engineering Fields: J.03"));
+        testKeyValues.add(new KeyLabelPair("D.01","Mathematical Science: D.01"));
+        testKeyValues.add(new KeyLabelPair("A.06","Mechanical  - Engineering: A.06"));
+        testKeyValues.add(new KeyLabelPair("F.03","Medical - Life Sciences: F.03"));
+        testKeyValues.add(new KeyLabelPair("A.07","Metallurgical and Materials  - Engineering: A.07"));
+        testKeyValues.add(new KeyLabelPair("C.03","Oceanography - Environmental Sciences: C.03"));
+        testKeyValues.add(new KeyLabelPair("A.99","Other - Engineering: A.99"));
+        testKeyValues.add(new KeyLabelPair("C.99","Other - Environmental Sciences: C.99"));
+        testKeyValues.add(new KeyLabelPair("F.99","Other - Life Sciences: F.99"));
+        testKeyValues.add(new KeyLabelPair("J.99","Other - Non-Science and Engineering Fields: J.99"));
+        testKeyValues.add(new KeyLabelPair("B.99","Other - Physical Sciences: B.99"));
+        testKeyValues.add(new KeyLabelPair("H.99","Other - Social Sciences: H.99"));
+        testKeyValues.add(new KeyLabelPair("I.01","Other Sciences, n.e.c.: I.01"));
+        testKeyValues.add(new KeyLabelPair("B.03","Physics - Physical Sciences: B.03"));
+        testKeyValues.add(new KeyLabelPair("H.02","Political Science - Social Sciences: H.02"));
+        testKeyValues.add(new KeyLabelPair("G.01","Psychology: G.01"));
+        testKeyValues.add(new KeyLabelPair("J.07","Social Work - Non-Science and Engineering Fields: J.07"));
+        testKeyValues.add(new KeyLabelPair("H.03","Sociology - Social Sciences: H.03"));
+        testKeyValues.add(new KeyLabelPair("J.04","Visual and Performing Arts - Non-Science and Engineering Fields: J.04"));
     }
 
 }
