95c4d8adb035569732dbdeb174853350c780e690
Lin-Long Shyu <lshyukuali@gmail.com>
2007-08-30 14:20:26 +0000

    KRACOEUS-151 : htmlunit test

--- src/test/java/org/kuali/kra/KraTestBase.java
+++ src/test/java/org/kuali/kra/KraTestBase.java
@@ -16,41 +16,75 @@
 
 package org.kuali.kra;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.kuali.core.util.ErrorMap;
 import org.kuali.core.util.GlobalVariables;
+import org.kuali.core.document.Document;
+import org.kuali.core.service.DocumentService;
+import org.kuali.rice.KNSServiceLocator;
+import org.kuali.rice.lifecycle.Lifecycle;
 import org.kuali.rice.testharness.KNSTestCase;
 import org.kuali.rice.testharness.TransactionalLifecycle;
 
 public class KraTestBase extends KNSTestCase {
 
-	private TransactionalLifecycle transactionalLifecycle;
+    private TransactionalLifecycle transactionalLifecycle;
+    private DocumentService documentService = null;
 
-	@Before
-	public void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         setContextName("/kra-dev");
-		setRelativeWebappRoot("/src/main/webapp");
-		setSqlFilename("classpath:DefaultTestData.sql");
-		setSqlDelimiter(";");
-		setXmlFilename("classpath:DefaultTestData.xml");
-		setTestConfigFilename("classpath:META-INF/kra-test-config.xml");
-		super.setUp();
-                GlobalVariables.setErrorMap(new ErrorMap());
-		transactionalLifecycle = new TransactionalLifecycle();
-		transactionalLifecycle.start();
-	}
+        setRelativeWebappRoot("/src/main/webapp");
+        setSqlFilename("classpath:DefaultTestData.sql");
+        setSqlDelimiter(";");
+        setXmlFilename("classpath:DefaultTestData.xml");
+        setTestConfigFilename("classpath:META-INF/kra-test-config.xml");
+        super.setUp();
+        documentService = KNSServiceLocator.getDocumentService();
+        GlobalVariables.setErrorMap(new ErrorMap());
+        transactionalLifecycle = new TransactionalLifecycle();
+        transactionalLifecycle.start();
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		transactionalLifecycle.stop();
-                GlobalVariables.setErrorMap(new ErrorMap());
-		super.tearDown();
-	}
+    @After
+    public void tearDown() throws Exception {
+        transactionalLifecycle.stop();
+        GlobalVariables.setErrorMap(new ErrorMap());
+        super.tearDown();
+        documentService = null;
+    }
 
-	@Override
-	protected String getModuleName() {
-		return "";
-	}
+    @Override
+    protected String getModuleName() {
+        return "";
+    }
 
+
+    @Override
+    public List<Lifecycle> getSuiteLifecycles() {
+        // TODO Auto-generated method stub
+        List<Lifecycle> lifeCycles= super.getSuiteLifecycles();
+        lifeCycles.add(new KraSQLDataLoaderLifecycle());
+        lifeCycles.add(new KraKEWXmlDataLoaderLifecycle());        
+        return lifeCycles;
+    }
+
+    public DocumentService getDocumentService() throws Exception {
+        return documentService;
+    }
+
+    public void setDocumentService(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    protected Document getDocument(String documentNumber) throws Exception {
+        transactionalLifecycle.stop();
+        Document doc=getDocumentService().getByDocumentHeaderId(documentNumber);
+        transactionalLifecycle.start();
+        return doc;
+
+    }
 }
