e28c1286915e6e630a08ae4130e0f32403c07029
Lin-Long Shyu <lshyukuali@gmail.com>
2007-08-31 13:59:25 +0000

    KRACOEUS-151: htmlunit test add load data scripts and xml files to ingest for testing.  This is to temporarily resolve the classpath issue.

---
+++ src/test/resources/sql/dml/load_activity_type.sql
@@ -0,0 +1,9 @@
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('1','Organized Research',sysdate,'kradev');
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('2','Instruction and Departmental Research',sysdate,'kradev');
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('3','Fellowship - Pre-Doctoral',sysdate,'kradev');
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('4','Public Service',sysdate,'kradev');
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('5','Student Services',sysdate,'kradev');
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('6','Other',sysdate,'kradev');
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('7','Fellowship - Post-Doctoral',sysdate,'kradev');
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('8','Clinical Trial',sysdate,'kradev');
+insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('9','Construction',sysdate,'kradev');
