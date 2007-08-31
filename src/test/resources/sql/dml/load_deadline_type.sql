7c0a7068ec1c37d3737d03a8ed5defa71c36f474
Lin-Long Shyu <lshyukuali@gmail.com>
2007-08-31 13:59:25 +0000

    KRACOEUS-151: htmlunit test add load data scripts and xml files to ingest for testing.  This is to temporarily resolve the classpath issue.

---
+++ src/test/resources/sql/dml/load_deadline_type.sql
@@ -0,0 +1,2 @@
+insert into deadline_type (deadline_type_code,description,update_timestamp,update_user) values('P','Postmark',sysdate,'kradev');
+insert into deadline_type (deadline_type_code,description,update_timestamp,update_user) values('R','Receipt',sysdate,'kradev');
