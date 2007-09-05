9e1774c201dafb751643a76d6144ce064dbd59bc
Leo Przybylski <leo@rsmart.com>
2007-09-05 17:21:47 +0000

    KRACOEUS-180 Adding business rules

--- src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentKeyPersonsRule.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentKeyPersonsRule.java
@@ -15,13 +15,20 @@
  */
 package org.kuali.kra.proposaldevelopment.rules;
 
+import java.util.Iterator;
+
 import org.kuali.core.document.Document;
 import org.kuali.core.rules.DocumentRuleBase;
 import org.kuali.core.util.ErrorMap;
 import org.kuali.core.util.GlobalVariables;
 import org.kuali.kra.proposaldevelopment.bo.ProposalPerson;
+import org.kuali.kra.proposaldevelopment.bo.ProposalPersonUnit;
 import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
 import org.kuali.kra.proposaldevelopment.rule.AddKeyPersonRule;
+
+import static org.apache.commons.lang.StringUtils.isBlank;
+import static org.kuali.kra.infrastructure.Constants.PRINCIPAL_INVESTIGATOR_ROLE;
+import static org.kuali.kra.infrastructure.Constants.CO_INVESTIGATOR_ROLE;
 
 /**
  * Implementation of business rules required for the Key Persons Page of the 
@@ -29,7 +36,7 @@ import org.kuali.kra.proposaldevelopment.rule.AddKeyPersonRule;
  *
  * @see org.kuali.core.rules.BusinessRule
  * @author $Author: lprzybyl $
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class ProposalDevelopmentKeyPersonsRule extends DocumentRuleBase implements AddKeyPersonRule { 
     @Override
@@ -37,10 +44,91 @@ public class ProposalDevelopmentKeyPersonsRule extends DocumentRuleBase implemen
         return true;
     }
 
+    @Override
+    protected boolean processCustomRouteDocumentBusinessRules(Document document) {
+        ProposalDevelopmentDocument pd = (ProposalDevelopmentDocument) document;
+        boolean retval = true;
+        
+        retval &= hasPrincipalInvestigator(pd);
+                
+        return retval;
+    }
+
     /**
      * @see org.kuali.kra.proposaldevelopment.rule.AddKeyPersonRule#processAddKeyPersonBusinessRules(ProposalDevelopmentDocument,ProposalPerson)
      */
     public boolean processAddKeyPersonBusinessRules(ProposalDevelopmentDocument document, ProposalPerson person) {
-        return true;
+        boolean retval = true;
+
+        if (!(isPrincipalInvestigator(person) && hasPrincipalInvestigator(document))) {
+            reportError("newProposalPerson", "proposalPerson", "");
+            retval = false;
+        }
+
+        retval &= validateInvestigator(person);
+        
+        if (!isBlank(person.getProposalPersonRoleId())) {
+            reportError("newProposalPerson", "proposalPerson", "");
+        }
+        
+        return retval;
+    }
+        
+    protected boolean validateInvestigator(ProposalPerson person) {
+        boolean retval = true;
+        
+        if (!isInvestigator(person)) {
+            return retval;
+        }
+
+        retval &= validateInvestigatorUnits(person);
+        
+        return retval;
+    }
+    
+    protected boolean validateInvestigatorUnits(ProposalPerson person) {
+        boolean retval = true;
+        
+        if (person.getUnits().size() > 0) {
+            reportError("newProposalPerson", "proposalPerson", "");
+        }
+        
+        for (ProposalPersonUnit unit : person.getUnits()) {
+            if (!isBlank(unit.getUnitNumber())) {
+                reportError("newProposalPerson", "proposalPerson", "");
+            }
+        }
+        
+        return retval;
+    }
+
+    protected boolean isPrincipalInvestigator(ProposalPerson person) {
+        return PRINCIPAL_INVESTIGATOR_ROLE.equals(person.getProposalPersonRoleId());
+    }
+
+    protected boolean isCoInvestigator(ProposalPerson person) {
+        return CO_INVESTIGATOR_ROLE.equals(person.getProposalPersonRoleId());
+    }
+    
+    protected boolean isInvestigator(ProposalPerson person) {
+        return isPrincipalInvestigator(person) || isCoInvestigator(person);
+    }
+        
+    protected boolean hasPrincipalInvestigator(ProposalDevelopmentDocument document) {
+        boolean retval = false;
+
+        for (Iterator<ProposalPerson> person_it = document.getProposalPersons().iterator();
+             person_it.hasNext() && !retval;) {
+            ProposalPerson person = person_it.next();
+            retval |= isPrincipalInvestigator(person);
+        }
+        
+        return retval;
+    }
+    
+    private void reportError(String errorPathPrefix, String property, String key) {
+        GlobalVariables.getErrorMap().addToErrorPath(errorPathPrefix);
+        GlobalVariables.getErrorMap().putError(property, key);
+        GlobalVariables.getErrorMap().removeFromErrorPath(errorPathPrefix);
     }
 }
