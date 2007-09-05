ba90910c5bad31022948b537c41ae4b648037472
Leo Przybylski <leo@rsmart.com>
2007-09-05 15:29:16 +0000

    KRACOEUS-178 Adding event system to proposal development. Added new KeyPerson events and KraDocumentEventBase.

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/rule/event/KeyPersonEvent.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.rule.event;
+
+import org.kuali.core.rule.event.KualiDocumentEvent;
+import org.kuali.kra.proposaldevelopment.bo.ProposalPerson;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
+
+
+/**
+ * Event triggered when a Key Person state is modified on a 
+ * <code>{@link ProposalDevelopmentDocument}</code>
+ *
+ * @author $Author: lprzybyl $
+ * @version $Revision: 1.1 $
+ */
+public interface KeyPersonEvent extends KualiDocumentEvent {
+    
+    /**
+     * @return <code>{@link ProposalPerson}</code> that triggered this event.
+     */
+    public ProposalPerson getProposalPerson();
+}
