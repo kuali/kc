89560ab452cd071ae9585782f704bc498777ba71
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2007-09-06 12:24:29 +0000

    KRACOEUS-101 (Sponsor / Program Info panel) - another test for fixed NPE that was thrown when saving without a Proposal Type defined
    Also:
    - refactored htmlunit tests
    - a couple sql fixes and commented out org.kuali.bo.Role in repository.xml to make unit tests run

--- src/main/config/sql/dml/load_deadline_type.sql
+++ src/main/config/sql/dml/load_deadline_type.sql
@@ -1,2 +1,3 @@
 insert into deadline_type (deadline_type_code,description,update_timestamp,update_user) values('P','Postmark',sysdate,'kradev');
+insert into deadline_type (deadline_type_code,description,update_timestamp,update_user) values('T','Target',sysdate,'kradev');
 insert into deadline_type (deadline_type_code,description,update_timestamp,update_user) values('R','Receipt',sysdate,'kradev');
