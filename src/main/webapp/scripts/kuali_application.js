2726e142c755619d492028a754fed763ec1c4a6b
Rajeev Mancheril <rajeev@genesistec.com>
2007-09-14 20:37:14 +0000

    Task KRACOEUS-182

--- src/main/webapp/scripts/kuali_application.js
+++ src/main/webapp/scripts/kuali_application.js
@@ -1,5 +1,18 @@
 
 
+function selectAllKeywords(document) {
+    var j = 0;
+	for (var i = 0; i < document.KualiForm.elements.length; i++) {
+	  var e = document.KualiForm.elements[i];
+	  if(e.type == 'checkbox') {
+	  	var name = 'document.propScienceKeywords[' + j + '].selectKeyword';
+	  	if (e.name == name) {
+ 		    e.checked = true;
+	  		j++;
+	  	}
+	  }
+	}
+}
 
 function textAreaPop(text,textAreaName,htmlFormAction,textAreaLabel){
   url=window.location.href
