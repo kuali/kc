be05f24fbff2602349b324c30d351d7d726e71dc
Don Barre <dbarre@arizona.edu>
2007-10-12 18:21:57 +0000

    KRACOEUS-191 Added Proposal Abstracts

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/bo/ProposalAbstract.java
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kuali.kra.proposaldevelopment.bo;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+/**
+ * Every Proposal can have zero or more Abstracts attached to it.
+ * An Abstract is composed of a unique type code and a textual string
+ * known as the Abstract Details.
+ * 
+ * FUDGE FACTOR: The astute reader will notice the addition of the
+ * bigAbstractDetails property via the get/set methods.  This is a hack
+ * to support a second data dictionary attribute entry.  The functional
+ * requirements indicate that the GUI text area control for Details 
+ * must support two different sizes depending upon the context. The 
+ * problem is that the data dictionary attributes have a one-to-one 
+ * correspondence with BO properties.  By introducing a "big" Details
+ * property, a second data dictionary attribute with a bigger text area 
+ * control can be defined.  This "bigger" attribute entry can then
+ * be referenced by a JSP. One last note: while this hack was needed
+ * it is unfortunate; a BO should not require a change of this sort in
+ * order to support the GUI.
+ * 
+ * @author Donald A. Barre
+ */
+public class ProposalAbstract extends KraPersistableBusinessObjectBase {
+    
+    /**
+     * Each Abstract is associated with one and only one proposal
+     * which is identified by its unique <code>proposalNumber</code>.
+     */
+    private Integer proposalNumber;
+    
+    /**
+     * Identifies what type of abstract this is.
+     */
+    private String abstractTypeCode;
+    
+	/**
+	 * The user-defined textual string for the abstract.
+	 */
+	private String abstractDetails;
+	
+	/**
+	 * The AbstractType instance that the above <code>abstractTypeCode</code>  
+	 * refers to.  It is stored here to make it easy for a JSP page to
+	 * access the abstract type's description.
+	 */
+	private AbstractType abstractType;
+	
+	/**
+	 * Constructs a ProposalAbstract.
+	 */
+	public ProposalAbstract(){
+		super();
+		this.proposalNumber = null;
+		this.setAbstractTypeCode("");
+		this.setAbstractDetails("");
+	}
+
+	/**
+	 * Gets the Proposal Number.
+	 * @return the proposal number this abstract is associated with; 
+	 *         null if not associated with any proposal.
+	 */
+	public Integer getProposalNumber() {
+		return proposalNumber;
+	}
+
+	/**
+	 * Sets the proposal number that this abstract is associated with.
+	 * @param proposalNumber a Proposal's unique number; may be null.
+	 */
+	public void setProposalNumber(Integer proposalNumber) {
+		this.proposalNumber = proposalNumber;
+	}
+
+    /**
+     * Gets the Abstract Type Code for this abstract.
+     * @return the Abstract Type Code.
+     */
+    public String getAbstractTypeCode() {
+        return abstractTypeCode;
+    }
+
+    /**
+     * Sets the Abstract Type Code for this abstract.
+     * @param abstractTypeCode one of the valid abstract type codes.  The database
+     *        can be consulted to find the valid type codes.
+     */
+    public void setAbstractTypeCode(String abstractTypeCode) {
+        this.abstractTypeCode = abstractTypeCode;
+        
+        // When the abstract type code changes, the corresponding
+        // abstractType field must also be updated.  A refresh will
+        // cause the abstract type to be read from the database. By
+        // the magic of OJB, the data member is automatically updated.
+        
+        if (this.abstractTypeCode == null || this.abstractTypeCode.equals("")) {
+            abstractType = null;
+        } else {
+            this.refreshReferenceObject("abstractType");
+        }
+    }
+    
+	/**
+	 * Gets the Details for this abstract.  
+	 * @return the abstract's details.
+	 */
+	public String getAbstractDetails() {
+		return abstractDetails;
+	}
+
+	/**
+	 * Sets the abstract's details.
+	 * @param abstractDetails a user-defined textual string.
+	 */
+	public void setAbstractDetails(String abstractDetails) {
+		this.abstractDetails = abstractDetails;
+	}
+
+    /**
+     * Gets the AbstractType for this abstract.
+     * @return the abstract's AbstractType.
+     */
+    public AbstractType getAbstractType() {
+        return abstractType;
+    }
+
+    /**
+     * Sets the abstract's AbstractType.
+     * @param abstractType the AbstractType.
+     */
+    public void setAbstractType(AbstractType abstractType) {
+        this.abstractType = abstractType;
+        this.abstractTypeCode = abstractType.getAbstractTypeCode();
+    }
+
+    /**
+     * Bogus method to support another Data Dictionary attribute.
+     * @return always returns null.
+     */
+    public String getBigAbstractDetails() {
+        return null;
+    }
+
+    /**
+     * Bogus method to support another Data Dictionary attribute.
+     * @param bigAbstractDetails this parameter is ignored.
+     */
+    public void setBigAbstractDetails(String bigAbstractDetails) {
+        // do nothing
+    }
+    
+    @Override
+	protected LinkedHashMap toStringMapper() {
+		LinkedHashMap hashMap = new LinkedHashMap();
+		hashMap.put("proposalNumber", getProposalNumber());
+		hashMap.put("abstractTypeCode", getAbstractTypeCode());
+		hashMap.put("abstractDetails", getAbstractDetails());
+		return hashMap;
+	}
+}
