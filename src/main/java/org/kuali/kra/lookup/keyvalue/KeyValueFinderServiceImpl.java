75af0d1efd30aee446af18216d10c7381980523f
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2007-10-16 17:44:21 +0000

    - modified KeyValueFinderServiceImpl and SponsorServiceImpl to use BusinessObjectService to find objects - test data and unit test cleanup for updated unit data (string (IN-PERS) rather than numeric (000002) key)

--- src/main/java/org/kuali/kra/lookup/keyvalue/KeyValueFinderServiceImpl.java
+++ src/main/java/org/kuali/kra/lookup/keyvalue/KeyValueFinderServiceImpl.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,19 +26,20 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.kuali.core.dao.BusinessObjectDao;
+import org.kuali.core.service.BusinessObjectService;
 import org.kuali.core.web.ui.KeyLabelPair;
 
 /**
  * This class...
  */
 public class KeyValueFinderServiceImpl implements KeyValueFinderService {
-    private BusinessObjectDao businessObjectDao;
+    private BusinessObjectService businessObjectService;
     private static final Log LOG = LogFactory.getLog(KeyValueFinderServiceImpl.class);
     /**
      * @see org.kuali.kra.lookup.keyvalue.KeyValueFinderService#getKeyValuesFor(java.lang.Class)
      */
     public List<KeyLabelPair> getKeyValues(Class keyValClass,String codePropName,String valPropName) {
-        Collection keyVals = businessObjectDao.findAll(keyValClass);
+        Collection keyVals = businessObjectService.findAll(keyValClass);
         List<KeyLabelPair> keyValueList = new ArrayList<KeyLabelPair>(keyVals.size());
         keyValueList.add(new KeyLabelPair("", "select:"));
         for (Iterator iterator = keyVals.iterator(); iterator.hasNext();) {
@@ -50,7 +51,7 @@ public class KeyValueFinderServiceImpl implements KeyValueFinderService {
                 String code = (String)getCodeMeth.invoke(keyValObj, null);
                 String value = (String)getValMeth.invoke(keyValObj, null);
                 keyValueList.add(new KeyLabelPair(code, value));
-                
+
             }
             catch (SecurityException e) {
                 LOG.debug(e.getMessage(), e);
@@ -76,17 +77,9 @@ public class KeyValueFinderServiceImpl implements KeyValueFinderService {
         return keyValueList;
     }
 
-    public BusinessObjectDao getBusinessObjectDao() {
-        return businessObjectDao;
-    }
-
-    public void setBusinessObjectDao(BusinessObjectDao businessObjectDao) {
-        this.businessObjectDao = businessObjectDao;
-    }
-
     public List<KeyLabelPair> getKeyValues(Class keyValClass, String codePropName, String valPropName, String groupPropName,
             String groupValue) {
-        Collection keyVals = businessObjectDao.findAll(keyValClass);
+        Collection keyVals = businessObjectService.findAll(keyValClass);
         List<KeyLabelPair> keyValueList = new ArrayList<KeyLabelPair>(keyVals.size());
         keyValueList.add(new KeyLabelPair("", "select:"));
         for (Iterator iterator = keyVals.iterator(); iterator.hasNext();) {
@@ -102,7 +95,7 @@ public class KeyValueFinderServiceImpl implements KeyValueFinderService {
                 if (StringUtils.isNotBlank(group) && group.equals(groupValue)) {
                     keyValueList.add(new KeyLabelPair(code, value));
                 }
-                
+
             }
             catch (SecurityException e) {
                 LOG.debug(e.getMessage(), e);
@@ -126,6 +119,22 @@ public class KeyValueFinderServiceImpl implements KeyValueFinderService {
             }
         }
         return keyValueList;
+    }
+
+    /**
+     * Gets the businessObjectService attribute.
+     * @return Returns the businessObjectService.
+     */
+    public BusinessObjectService getBusinessObjectService() {
+        return businessObjectService;
+    }
+
+    /**
+     * Sets the businessObjectService attribute value.
+     * @param businessObjectService The businessObjectService to set.
+     */
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
     }
 
 }
