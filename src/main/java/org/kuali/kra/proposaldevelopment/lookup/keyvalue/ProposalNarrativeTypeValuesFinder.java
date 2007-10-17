340bacdf5f592f832806ec0bddce442d14ff198a
Geo Thomas <geot@mit.edu>
2007-10-17 14:17:36 +0000

    JIRA # KRACOEUS-183 filtering out all non proposal attachments from proposal attachment section

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/ProposalNarrativeTypeValuesFinder.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.lookup.keyvalue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.kuali.core.lookup.keyvalues.KeyValuesBase;
+import org.kuali.core.service.KeyValuesService;
+import org.kuali.core.web.ui.KeyLabelPair;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.lookup.keyvalue.KeyValueFinderService;
+import org.kuali.kra.proposaldevelopment.bo.NarrativeType;
+
+public class ProposalNarrativeTypeValuesFinder extends KeyValuesBase {
+    KeyValueFinderService keyValueFinderService= (KeyValueFinderService)KraServiceLocator.getService("keyValueFinderService");
+    public List<KeyLabelPair> getKeyValues() {
+        return keyValueFinderService.getKeyValues(NarrativeType.class, "narrativeTypeCode", "description","narrativeTypeGroup","P");
+    }
+}
