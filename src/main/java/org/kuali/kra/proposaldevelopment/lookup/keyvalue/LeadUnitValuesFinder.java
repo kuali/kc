aaaaa025d96fa6a5988e8f06478e508f1cf9a5cb
Geoff McGregor <gmcgrego@iu.edu>
2007-10-23 18:27:36 +0000

    KRACOEUS-237: cnv not displaying panel properly

--- src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/LeadUnitValuesFinder.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/LeadUnitValuesFinder.java
@@ -16,6 +16,7 @@
 package org.kuali.kra.proposaldevelopment.lookup.keyvalue;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -34,7 +35,16 @@ public class LeadUnitValuesFinder extends KeyValuesBase {
         keyValues.add(new KeyLabelPair("", "select:"));
         
         //TODO: Refactor this when we get SpringContext object (like KFS)
-        Map<String, String> userUnits = ((ProposalDevelopmentService)KraServiceLocator.getService("proposalDevelopmentService")).getUnitsForUser(GlobalVariables.getUserSession().getLoggedInUserNetworkId());
+        ProposalDevelopmentService proposalDevelopmentService = (ProposalDevelopmentService) KraServiceLocator.getService("proposalDevelopmentService");
+        
+        // TODO gmcgrego: temp fix for cnv build while investigating why propDevService is null there
+        Map<String, String> userUnits = new HashMap<String, String>();
+        if (proposalDevelopmentService != null) {
+            userUnits = proposalDevelopmentService.getUnitsForUser(GlobalVariables.getUserSession().getLoggedInUserNetworkId());
+        } else {
+            userUnits.put("IN-PERS", 
+                    "IN-PERS - PED - EMERGENCY ROOM SERVICES");
+        }
         
         for (String key : userUnits.keySet()) {
             keyValues.add(new KeyLabelPair(key, userUnits.get(key)));
