129bf0f92bbc9270972537054a444f8b0e8e8b4d
Don Barre <dbarre@arizona.edu>
2007-10-24 00:14:55 +0000

    KRACOEUS-191 Added business rule and minor changes for mock

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/rule/event/AddAbstractEvent.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.rule.event;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.core.rule.BusinessRule;
+import org.kuali.core.util.ObjectUtils;
+import org.kuali.kra.proposaldevelopment.bo.ProposalAbstract;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
+import org.kuali.kra.proposaldevelopment.rule.AbstractsRule;
+import org.kuali.kra.rule.event.KraDocumentEventBase;
+
+/**
+ * The AddAbstractEvent is generated when a Proposal Abstract is to be added to
+ * a Proposal Development Document.
+ * 
+ * @author Donald A. Barre
+ */
+public class AddAbstractEvent extends KraDocumentEventBase {
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(NarrativeEventBase.class);
+    
+    private ProposalAbstract proposalAbstract;
+    
+    /**
+     * Constructs an AddAbstractEvent.
+     * 
+     * @param document proposal development document
+     * @param proposalAbstract the proposal abstract that is being added
+     */
+    public AddAbstractEvent(ProposalDevelopmentDocument document, ProposalAbstract proposalAbstract) {
+        super("adding abstract to document " + getDocumentId(document), "", document);
+    
+        // by doing a deep copy, we are ensuring that the business rule class can't update
+        // the original object by reference
+        this.proposalAbstract = (ProposalAbstract) ObjectUtils.deepCopy(proposalAbstract);
+    
+        logEvent();
+    }
+    
+    /**
+     * @see org.kuali.core.rule.event.KualiDocumentEventBase#validate()
+     */
+    public void validate() {
+        super.validate();
+        if (this.proposalAbstract == null) {
+            throw new IllegalArgumentException("invalid (null) proposal abstract");
+        }
+    }
+    
+    /**
+     * @see org.kuali.kra.rule.event.KraDocumentEventBase#logEvent()
+     */
+    @Override
+    protected void logEvent() {
+        StringBuffer logMessage = new StringBuffer(StringUtils.substringAfterLast(this.getClass().getName(), "."));
+        logMessage.append(" with ");
+
+        // vary logging detail as needed
+        if (this.proposalAbstract == null) {
+            logMessage.append("null proposalAbstract");
+        }
+        else {
+            logMessage.append(this.proposalAbstract.toString());
+        }
+
+        LOG.debug(logMessage);
+    }
+
+    /**
+     * @see org.kuali.core.rule.event.KualiDocumentEvent#getRuleInterfaceClass()
+     */
+    public Class getRuleInterfaceClass() {
+        return AbstractsRule.class;
+    }
+
+    /**
+     * @see org.kuali.core.rule.event.KualiDocumentEvent#invokeRuleMethod(org.kuali.core.rule.BusinessRule)
+     */
+    public boolean invokeRuleMethod(BusinessRule rule) {
+        return ((AbstractsRule) rule).processAddAbstractBusinessRules((ProposalDevelopmentDocument) this.getDocument(), 
+                                                                        this.proposalAbstract);
+    }
+}
