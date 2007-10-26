34214c069a7278b96e4893c0be48d0b63459d136
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2007-10-26 19:35:43 +0000

    KRACOEUS-101 (Sponsor / Program Info panel): made some strings in Audit check into Constants per 10/25/07 code review

--- src/main/java/org/kuali/kra/infrastructure/Constants.java
+++ src/main/java/org/kuali/kra/infrastructure/Constants.java
@@ -27,7 +27,7 @@ public interface Constants {
     public static final String TEXT_AREA_FIELD_LABEL = "textAreaFieldLabel";
     public static final String TEXT_AREA_FIELD_ANCHOR = "textAreaFieldAnchor";
     public static final Integer APPROVAL_STATUS = 2;
-    
+
     public static final String PRINCIPAL_INVESTIGATOR_ROLE = "PI";
     public static final String CO_INVESTIGATOR_ROLE = "COI";
     public static final String MULTIPLE_VALUE = "multipleValues";
@@ -38,17 +38,17 @@ public interface Constants {
     public static final String NEW_PERSON_LOOKUP_FLAG = "newPersonLookupFlag";
     public static final String MAPPING_CLOSE_PAGE = "closePage";
     public static final String MAPPING_NARRATIVE_ATTACHMENT_RIGHTS_PAGE = "attachmentRights";
-    
+
     public static final String CREDIT_SPLIT_ENABLED_RULE_NAME = "proposaldevelopment.creditsplit.enabled";
     public static final String CREDIT_SPLIT_ENABLED_FLAG = "creditSplitEnabledFlag";
-    
+
     public static final String NARRATIVE_MODULE_NUMBER = "proposalDevelopment.narrative.moduleNumber";
     public static final String NARRATIVE_MODULE_SEQUENCE_NUMBER = "proposalDevelopment.narrative.moduleSequenceNumber";
     public static final String PROP_PERSON_BIO_NUMBER = "proposalDevelopment.proposalPersonBiography.biographyNumber";
     public static final String PROPOSAL_LOCATION_SEQUENCE_NUMBER = "proposalDevelopment.proposalLocation.locationSequenceNumber";
     public static final String PROPOSAL_SPECIALREVIEW_NUMBER = "proposalDevelopment.proposalSpecialReview.specialReviewNumber";
     public static final String PROPOSAL_NARRATIVE_TYPE_GROUP = "proposalNarrativeTypeGroup";
-    
+
     public static final String PROPOSAL_PERSON_ROLE_PARAMETER_PREFIX = "proposaldevelopment.personrole.";
 
     public static final String PARAMETER_MODULE_PROPOSAL_DEVELOPMENT = "KRA-PD";
@@ -57,5 +57,11 @@ public interface Constants {
     public static final String NARRATIVE_MODULE_STATUS_COMPLETE = "C";
 
     public static final String ABSTRACTS_PROPERTY_KEY = "document.proposalAbstracts";
+
+    public static final String AUDIT_WARNINGS = "Warnings";
+
+    public static final String PROPOSAL_PAGE = "proposal";
+    public static final String SPONSOR_PROGRAM_INFORMATION_PANEL_ANCHOR = "SponsorProgramInformation";
+    public static final String SPONSOR_PROGRAM_INFORMATION_PANEL_NAME = "Sponsor & Program Information";
 
 }
