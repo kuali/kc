4e7c862c37645c9ecfac6aaa5040f151939385df
Rajeev Mancheril <rajeev@genesistec.com>
2007-11-01 17:03:50 +0000

    YNQ Maintenance Document KRACOEUS-195

---
+++ src/main/java/org/kuali/kra/lookup/keyvalue/YnqRequiredForValuesFinder.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.lookup.keyvalue;
+
+import org.kuali.core.lookup.keyvalues.KeyValuesBase;
+import org.kuali.core.web.ui.KeyLabelPair;
+import java.util.ArrayList;
+import java.util.List;
+
+public class YnqRequiredForValuesFinder extends KeyValuesBase {
+
+    public List<KeyLabelPair> getKeyValues() {
+        List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
+        keyValues.add(new KeyLabelPair("Y", "Yes"));
+        keyValues.add(new KeyLabelPair("N", "No"));
+        keyValues.add(new KeyLabelPair("YN", "Both"));
+        keyValues.add(new KeyLabelPair(null, "None"));
+        return keyValues;
+    }
+}
