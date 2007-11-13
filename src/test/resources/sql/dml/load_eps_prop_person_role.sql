93b5985907fd5713a5d929cdcbc8dd69ca4fc3fc
Lin-Long Shyu <lshyukuali@gmail.com>
2007-11-13 19:45:55 +0000

    KRACOEUS-151 : add new load files and remove load_eps_prop_location  . load_user_kra is loading kra_user.  It needs to be loaded after unit is loaded and before user_role is loading.  SO, the name is not standard.  Rice will look into data loading issues, so we don't have to worry about the order  of loading

---
+++ src/test/resources/sql/dml/load_eps_prop_person_role.sql
@@ -0,0 +1,3 @@
+insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER) values ('PI', 'Principal Investigator', sysdate, 'kradev');
+insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER) values ('COI', 'Co-Investigator', sysdate, 'kradev');
+insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER) values ('KP', 'Key Person', sysdate, 'kradev');
