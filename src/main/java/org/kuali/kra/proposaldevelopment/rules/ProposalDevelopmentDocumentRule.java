8a8a6ab8743616de1f2c2f61426f479eb4436164
Lin-Long Shyu <lshyukuali@gmail.com>
2007-11-27 21:00:57 +0000

    KRACOEUS-276 : refactoring by moving validatedocumentrecirsively to documentvalidationservice

--- src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentDocumentRule.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentDocumentRule.java
@@ -17,29 +17,16 @@ package org.kuali.kra.proposaldevelopment.rules;
 
 import static org.kuali.kra.infrastructure.KraServiceLocator.getService;
 
-import java.beans.PropertyDescriptor;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Vector;
-
-import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang.StringUtils;
-import org.apache.ojb.broker.metadata.ClassDescriptor;
-import org.apache.ojb.broker.metadata.ObjectReferenceDescriptor;
-import org.apache.ojb.broker.metadata.fieldaccess.PersistentField;
-import org.kuali.core.bo.PersistableBusinessObject;
 import org.kuali.core.document.Document;
 import org.kuali.core.rule.DocumentAuditRule;
 import org.kuali.core.service.DataDictionaryService;
 import org.kuali.core.util.ErrorMap;
 import org.kuali.core.util.GlobalVariables;
-import org.kuali.core.util.ObjectUtils;
 import org.kuali.kra.bo.ValidSpecialReviewApproval;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.kra.infrastructure.KraServiceLocator;
-import org.kuali.kra.proposaldevelopment.bo.Narrative;
 import org.kuali.kra.proposaldevelopment.bo.ProposalAbstract;
 import org.kuali.kra.proposaldevelopment.bo.ProposalCopyCriteria;
 import org.kuali.kra.proposaldevelopment.bo.ProposalLocation;
@@ -48,22 +35,25 @@ import org.kuali.kra.proposaldevelopment.bo.ProposalPersonBiography;
 import org.kuali.kra.proposaldevelopment.bo.ProposalSpecialReview;
 import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
 import org.kuali.kra.proposaldevelopment.rule.AbstractsRule;
+import org.kuali.kra.proposaldevelopment.rule.AddInstituteAttachmentRule;
 import org.kuali.kra.proposaldevelopment.rule.AddKeyPersonRule;
 import org.kuali.kra.proposaldevelopment.rule.AddNarrativeRule;
 import org.kuali.kra.proposaldevelopment.rule.CopyProposalRule;
+import org.kuali.kra.proposaldevelopment.rule.SaveInstituteAttachmentsRule;
 import org.kuali.kra.proposaldevelopment.rule.SaveNarrativesRule;
+import org.kuali.kra.proposaldevelopment.rule.event.AddInstituteAttachmentEvent;
 import org.kuali.kra.proposaldevelopment.rule.event.AddNarrativeEvent;
+import org.kuali.kra.proposaldevelopment.rule.event.SaveInstituteAttachmentsEvent;
 import org.kuali.kra.proposaldevelopment.rule.event.SaveNarrativesEvent;
-import org.kuali.kra.proposaldevelopment.service.ProposalDevelopmentService;
 import org.kuali.kra.rules.ResearchDocumentRuleBase;
-import org.kuali.rice.KNSServiceLocator;
+import org.kuali.kra.service.DocumentValidationService;
 
 /**
  * This class...
  *
  * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
  */
-public class ProposalDevelopmentDocumentRule extends ResearchDocumentRuleBase implements AddKeyPersonRule, AddNarrativeRule,SaveNarrativesRule, DocumentAuditRule, AbstractsRule, CopyProposalRule {
+public class ProposalDevelopmentDocumentRule extends ResearchDocumentRuleBase implements AddKeyPersonRule, AddNarrativeRule,SaveNarrativesRule, AddInstituteAttachmentRule,SaveInstituteAttachmentsRule, DocumentAuditRule, AbstractsRule, CopyProposalRule {
 
     @Override
     protected boolean processCustomRouteDocumentBusinessRules(Document document) {
@@ -96,7 +86,7 @@ public class ProposalDevelopmentDocumentRule extends ResearchDocumentRuleBase im
         // hack to get rid of message that contains '.documentBusinessObject.' in path
         // rice should look into this problem ?
         // The '.documentBusinessObject.' is filtered out in validateDocumentRecursively for now.
-        getService(ProposalDevelopmentService.class).validateDocumentRecursively(proposalDevelopmentDocument,10);
+        getService(DocumentValidationService.class).validateDocumentRecursively(proposalDevelopmentDocument,10);
 //        List errorList = new ArrayList();
 //        for (Iterator iter = GlobalVariables.getErrorMap().keySet().iterator(); iter.hasNext();) {
 //            String property = (String) iter.next();
@@ -113,7 +103,6 @@ public class ProposalDevelopmentDocumentRule extends ResearchDocumentRuleBase im
         valid &= processOrganizationLocationBusinessRule(proposalDevelopmentDocument);
         valid &= processSpecialReviewBusinessRule(proposalDevelopmentDocument);
         valid &= processPersonnelAttachmentBusinessRule(proposalDevelopmentDocument);
-        valid &= processInstitutionalAttachmentBusinessRule(proposalDevelopmentDocument);
 
         GlobalVariables.getErrorMap().removeFromErrorPath("document");
 
@@ -267,41 +256,6 @@ public class ProposalDevelopmentDocumentRule extends ResearchDocumentRuleBase im
         return valid;
     }
 
-    /**
-     * This method validates 'Institute Attachment'. It checks the following :
-     * If attachment type and description are not empty, then filename is a required field.
-     *
-     * @param proposalDevelopmentDocument : The proposalDevelopmentDocument that is being validated
-     * @return valid Does the validation pass
-     */
-    private boolean processInstitutionalAttachmentBusinessRule(ProposalDevelopmentDocument proposalDevelopmentDocument) {
-        boolean valid = true;
-
-        ErrorMap errorMap = GlobalVariables.getErrorMap();
-
-        int i = 0;
-
-        // TODO : this will combine errors with proposal attachments panel
-        for (Narrative institute : proposalDevelopmentDocument.getInstitutes()) {
-            errorMap.addToErrorPath("institutes[" + i + "]");
-            institute.refresh();
-            if (StringUtils.isNotBlank(institute.getNarrativeTypeCode()) && StringUtils.isNotBlank(institute.getModuleTitle())) {
-                    if (StringUtils.isBlank(institute.getFileName())) {
-                        valid = false;
-                        errorMap.putError("fileName", KeyConstants.ERROR_REQUIRED_FOR_FILE_NAME, "File Name");
-                    }
-
-
-            }
-            errorMap.removeFromErrorPath("institutes[" + i++ + "]");
-        }
-        return valid;
-
-    }
-
-
-
-
 
     public boolean processAddKeyPersonBusinessRules(ProposalDevelopmentDocument document, ProposalPerson person) {
         return new ProposalDevelopmentKeyPersonsRule().processAddKeyPersonBusinessRules(document, person);
@@ -339,6 +293,22 @@ public class ProposalDevelopmentDocumentRule extends ResearchDocumentRuleBase im
      */
     public boolean processCopyProposalBusinessRules(ProposalDevelopmentDocument document, ProposalCopyCriteria criteria) {
         return new ProposalDevelopmentCopyRule().processCopyProposalBusinessRules(document, criteria);
+    }
+
+    /**
+     * 
+     * @see org.kuali.kra.proposaldevelopment.rule.AddInstituteAttachmentRule#processAddInstituteAttachmentBusinessRules(org.kuali.kra.proposaldevelopment.rule.event.AddInstituteAttachmentEvent)
+     */
+    public boolean processAddInstituteAttachmentBusinessRules(AddInstituteAttachmentEvent addInstituteAttachmentEvent) {
+        return new ProposalDevelopmentNarrativeRule().processAddInstituteAttachmentBusinessRules(addInstituteAttachmentEvent);    
+    }
+
+    /**
+     * 
+     * @see org.kuali.kra.proposaldevelopment.rule.SaveInstituteAttachmentsRule#processSaveInstituteAttachmentsBusinessRules(org.kuali.kra.proposaldevelopment.rule.event.SaveInstituteAttachmentsEvent)
+     */
+    public boolean processSaveInstituteAttachmentsBusinessRules(SaveInstituteAttachmentsEvent saveInstituteAttachmentsEvent) {
+        return new ProposalDevelopmentNarrativeRule().processSaveInstituteAttachmentsBusinessRules(saveInstituteAttachmentsEvent);
     }
 
 }
