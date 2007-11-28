70ec60fc481e92900f65f9e036aaa0872b0d393e
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2007-11-28 16:51:27 +0000

    KRACOEUS-227: removed SystemParametersPropertyHolder and related exception post 0.9.2 and refactored the way system parameters are referenced in JSPs
    Also:
    - refactored OrganizationLocation web test into it's own class (attempting to ease web test performance issues)
    - commented out 3 help link tests as these are failing on the Bamboo server

--- src/main/java/org/kuali/kra/infrastructure/KraServiceLocatorListener.java
+++ src/main/java/org/kuali/kra/infrastructure/KraServiceLocatorListener.java
@@ -1,13 +1,7 @@
 package org.kuali.kra.infrastructure;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-
-import org.kuali.kra.infrastructure.SystemParametersPropertyHolder;
-
-import static org.kuali.kra.infrastructure.Constants.PARAMETER_MODULE_PROPOSAL_DEVELOPMENT;
-import static org.kuali.kra.infrastructure.Constants.PARAMETER_COMPONENT_DOCUMENT;
 
 public class KraServiceLocatorListener implements ServletContextListener {
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(KraServiceLocatorListener.class);
@@ -20,11 +14,6 @@ public class KraServiceLocatorListener implements ServletContextListener {
         LOG.error("Starting KraServiceLocatorListener");
 
 		KraServiceLocator.getAppContext();
-        ServletContext context = sce.getServletContext();
-
-        // publish application constants into JSP app context with name "Constants"
-        context.setAttribute("ProposalDevelopmentParameters", new SystemParametersPropertyHolder(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, PARAMETER_COMPONENT_DOCUMENT));
-
 	}
 
 }
