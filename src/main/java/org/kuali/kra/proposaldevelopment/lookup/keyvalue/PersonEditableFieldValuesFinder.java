5dab867090071b83f9dc95163e9dcc6802187ea0
Leo Przybylski <leo@rsmart.com>
2007-11-28 01:47:36 +0000

    KRACOEUS-318 Adding default value.

--- src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/PersonEditableFieldValuesFinder.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/PersonEditableFieldValuesFinder.java
@@ -37,7 +37,7 @@ import org.kuali.kra.proposaldevelopment.bo.ProposalPerson;
  * Lookup acceptable values for <code>{@link PersonEditableField}</code> business object
  *  
  * @author $Author: lprzybyl $
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class PersonEditableFieldValuesFinder extends KeyValuesBase {
     Map<String, AttributeDefinition> attributes; 
@@ -56,7 +56,8 @@ public class PersonEditableFieldValuesFinder extends KeyValuesBase {
      */
     public List<KeyLabelPair> getKeyValues() {
         List<KeyLabelPair> retval = new ArrayList<KeyLabelPair>();
-        
+        retval.add(new KeyLabelPair("", "select:"));
+
         for (AttributeDefinition attribute : getAttributeMap().values()) {
             if (!getExcludedAttributes().contains(attribute.getName())) {
                 retval.add(new KeyLabelPair(attribute.getName(), attribute.getLabel()));
