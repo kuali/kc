eb9f34aea7ff854b40aea720671d68042bcb0cea
Rajeev Mancheril <rajeev@genesistec.com>
2007-12-03 18:04:29 +0000

    ynq data

---
+++ src/main/config/sql/dml/load_ynq.sql
@@ -0,0 +1,32 @@
+Insert into ynq
+   (QUESTION_ID, DESCRIPTION, QUESTION_TYPE, NO_OF_ANSWERS, EXPLANATION_REQUIRED_FOR, DATE_REQUIRED_FOR, STATUS, EFFECTIVE_DATE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+ Values
+   ('P001', 'Test Q', 'C', 2, 'N', 'YN', 'A', TO_DATE('10/28/2007 00:00:00', 'MM/DD/YYYY HH24:MI:SS'), 'Group Conflicts', TO_DATE('10/28/2007 23:38:26', 'MM/DD/YYYY HH24:MI:SS'), 'quicksta', 1, '04C945CD-0776-CB01-8C52-A4F57B93CC4C');
+Insert into ynq
+   (QUESTION_ID, DESCRIPTION, QUESTION_TYPE, NO_OF_ANSWERS, EXPLANATION_REQUIRED_FOR, DATE_REQUIRED_FOR, STATUS, EFFECTIVE_DATE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+ Values
+   ('P003', 'Military Services Barred from Recruitment Activities at the Proposing Organization''s Site(s)', 'P', 3, 'N', 'N', 'A', TO_DATE('11/01/2007 00:00:00', 'MM/DD/YYYY HH24:MI:SS'), 'Organization', TO_DATE('11/01/2007 12:39:23', 'MM/DD/YYYY HH24:MI:SS'), 'quicksta', 1, '5B6BCEE9-E807-EC54-F854-97719FA16836');
+Insert into ynq
+   (QUESTION_ID, DESCRIPTION, QUESTION_TYPE, NO_OF_ANSWERS, EXPLANATION_REQUIRED_FOR, DATE_REQUIRED_FOR, STATUS, EFFECTIVE_DATE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+ Values
+   ('P002', 'Is there a space change on this proposal?', 'P', 3, 'Y', 'Y', 'A', TO_DATE('11/01/2007 00:00:00', 'MM/DD/YYYY HH24:MI:SS'), 'General Questions asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf as', TO_DATE('11/01/2007 12:16:18', 'MM/DD/YYYY HH24:MI:SS'), 'quicksta', 1, '392C8C65-583C-2EC1-0828-0A0F83297C23');
+Insert into ynq
+   (QUESTION_ID, DESCRIPTION, QUESTION_TYPE, NO_OF_ANSWERS, STATUS, EFFECTIVE_DATE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+ Values
+   ('P004', 'Lobbying activities have been conducted regarding the proposal ', 'I', 2, 'A', TO_DATE('11/01/2007 00:00:00', 'MM/DD/YYYY HH24:MI:SS'), 'Group Investigator', TO_DATE('11/01/2007 12:16:18', 'MM/DD/YYYY HH24:MI:SS'), 'quicksta', 1, '3E2DFDCBE92D00CCE043814FD88100CC');
+Insert into ynq
+   (QUESTION_ID, DESCRIPTION, QUESTION_TYPE, NO_OF_ANSWERS, EXPLANATION_REQUIRED_FOR, DATE_REQUIRED_FOR, STATUS, EFFECTIVE_DATE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+ Values
+   ('P005', 'Are you familiar with the requirements of the Procurement Integrity Act [(OFPP, Section 27 (1-3)] (http://web.mit.edu/osp/www/Procuint.htm) and will you report any violations to the Office of Sponsored Programs?', 'I', 3, 'N', 'N', 'A', TO_DATE('11/06/2007 00:00:00', 'MM/DD/YYYY HH24:MI:SS'), 'Group Investigator', TO_DATE('11/06/2007 12:37:32', 'MM/DD/YYYY HH24:MI:SS'), 'quicksta', 1, '685631AE-FB11-BFF3-0E9E-CE955EC1253D');
+Insert into ynq
+   (QUESTION_ID, DESCRIPTION, QUESTION_TYPE, NO_OF_ANSWERS, EXPLANATION_REQUIRED_FOR, DATE_REQUIRED_FOR, STATUS, EFFECTIVE_DATE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+ Values
+   ('P006', 'Can you certify that the information submitted within this application is true, complete and accurate to the best of your knowledge? That any false, fictitious, or fraudulent statements or claims may subject you, as the PI/Co-PI/Co-I to criminal, civil or administrative penalties? That you agree to accept responsibility for the scientific conduct of the project and to provide the required ', 'I', 2, 'N', 'N', 'A', TO_DATE('11/06/2007 00:00:00', 'MM/DD/YYYY HH24:MI:SS'), 'Group Investigator', TO_DATE('11/06/2007 12:37:32', 'MM/DD/YYYY HH24:MI:SS'), 'quicksta', 1, '3E48C77C54A6F178E043814FD881F178');
+Insert into ynq
+   (QUESTION_ID, DESCRIPTION, QUESTION_TYPE, NO_OF_ANSWERS, STATUS, EFFECTIVE_DATE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+ Values
+   ('P007', 'NSF Beginning Investigator?', 'P', 3, 'A', TO_DATE('11/06/2007 00:00:00', 'MM/DD/YYYY HH24:MI:SS'), 'General Questions asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf as', TO_DATE('11/06/2007 12:37:32', 'MM/DD/YYYY HH24:MI:SS'), 'quicksta', 1, '3EBEEDE5260B3150E043814FD8813150');
+Insert into ynq
+   (QUESTION_ID, DESCRIPTION, QUESTION_TYPE, NO_OF_ANSWERS, STATUS, EFFECTIVE_DATE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+ Values
+   ('P008', 'NIH Beginning Investigator?', 'P', 2, 'A', TO_DATE('11/06/2007 00:00:00', 'MM/DD/YYYY HH24:MI:SS'), 'General Questions asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf as', TO_DATE('11/06/2007 12:37:32', 'MM/DD/YYYY HH24:MI:SS'), 'quicksta', 1, '3EBEEDE5260D3150E043814FD8813150');
