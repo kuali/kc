ecef2338499d6db0a9cb22b53ccc80f87b4d8a49
Bryan Hutchinson <bryan.hutchinson@cornell.edu>
2007-12-03 15:44:45 +0000

    KRACOEUS-370: added testGetKeyValues() method to AbstractTypeValuesFinderTest so that it would run locally as a JUnit test

--- src/test/java/org/kuali/kra/lookup/keyvalue/AbstractTypeValuesFinderTest.java
+++ src/test/java/org/kuali/kra/lookup/keyvalue/AbstractTypeValuesFinderTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2007 The Kuali Foundation.
- * 
+ *
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.opensource.org/licenses/ecl1.php
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,21 +15,26 @@
  */
 package org.kuali.kra.lookup.keyvalue;
 
+import org.junit.Test;
 import org.kuali.kra.keyvalue.ValuesFinderTestBase;
 import org.kuali.kra.proposaldevelopment.lookup.keyvalue.AbstractTypeValuesFinder;
 
 /**
  * Test the Abstract Type Values Finder.
- * 
+ *
  * @author Kuali Research Administration Team (kualidev@oncourse.iu.edu)
  */
 public class AbstractTypeValuesFinderTest extends ValuesFinderTestBase {
-    
+
     /**
      * Constructs an AbstractTypeValuesFinderTest.
      */
     public AbstractTypeValuesFinderTest() {
         setTestClass(AbstractTypeValuesFinder.class);
+    }
+
+    @Test public void testGetKeyValues() throws Exception {
+        super.testGetKeyValues();
     }
 
     /**
