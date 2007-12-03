10d2050bdb0ea12ea0b93fc9e7bc62e7d04e5bdb
Rajeev Mancheril <rajeev@genesistec.com>
2007-12-03 21:03:08 +0000

    refactor ynq service

--- src/main/java/org/kuali/kra/service/impl/YnqServiceImpl.java
+++ src/main/java/org/kuali/kra/service/impl/YnqServiceImpl.java
@@ -25,11 +25,17 @@ import java.util.Map;
 
 import org.kuali.core.service.BusinessObjectService;
 import org.kuali.core.service.DateTimeService;
+import org.kuali.kra.bo.Person;
 import org.kuali.kra.bo.Ynq;
 import org.kuali.kra.bo.YnqExplanationType;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.infrastructure.YnqConstants;
 import org.kuali.kra.proposaldevelopment.bo.ProposalPerson;
+import org.kuali.kra.proposaldevelopment.bo.ProposalPersonYnq;
+import org.kuali.kra.proposaldevelopment.bo.ProposalYnq;
+import org.kuali.kra.proposaldevelopment.bo.YnqGroupName;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
 import org.kuali.kra.service.YnqService;
 
 public class YnqServiceImpl implements YnqService {
@@ -77,6 +83,84 @@ public class YnqServiceImpl implements YnqService {
             }
         } 
         return ynqs;
+    }
+
+    /**
+     *
+     * @param proposalPerson
+     * @return ProposalPerson
+     */
+    public ProposalPerson getPersonYNQ(ProposalPerson proposalPerson) {
+        /* get YNQ for person */
+        if(proposalPerson.getProposalPersonYnqs().isEmpty()) {
+            String questionType = Constants.QUESTION_TYPE_INDIVIDUAL;
+            List<Ynq> ynqs = (KraServiceLocator.getService(YnqService.class).getYnq(questionType));
+            for (Ynq type : ynqs) {
+                ProposalPersonYnq proposalPersonYnq = new ProposalPersonYnq();
+                proposalPersonYnq.setQuestionId(type.getQuestionId());
+                proposalPersonYnq.setYnq(type); 
+                proposalPerson.getProposalPersonYnqs().add(proposalPersonYnq);
+            }
+        }
+        return proposalPerson;
+    }
+    
+    /**
+     * @see org.kuali.kra.proposaldevelopment.service.YnqService#populateQuestions()
+     */
+    /* get YNQ for proposal */
+    public void populateProposalQuestions(List<ProposalYnq> proposalYnqs, List<YnqGroupName> ynqGroupNames) {
+        //List<ProposalYnq> proposalYnqs = proposalDevelopmentDocument.getProposalYnqs();
+        //List<YnqGroupName> ynqGroupNames = proposalDevelopmentDocument.getYnqGroupNames();
+        String questionType = Constants.QUESTION_TYPE_PROPOSAL;
+        List<Ynq> ynqs = getYnq(questionType);
+        for (Ynq type : ynqs) {
+            ProposalYnq proposalYnq = new ProposalYnq();
+            proposalYnq.setQuestionId(type.getQuestionId());
+            proposalYnq.setYnq(type); 
+            /* check Date required for column in required */
+            if(type.getDateRequiredFor() == null) {
+                proposalYnq.setReviewDateRequired(false);
+            }else {
+                proposalYnq.setReviewDateRequiredDescription(Constants.YNQ_REVIEW_DATE_REQUIRED.concat(getYnqRequiredLabel(type.getDateRequiredFor())));
+            }
+            /* check Explanation required for column is mandatory */
+            if(type.getExplanationRequiredFor() == null) {
+                proposalYnq.setExplanationRequried(false);
+            }else {
+                proposalYnq.setExplanationRequiredDescription(Constants.YNQ_EXPLANATION_REQUIRED.concat(getYnqRequiredLabel(type.getExplanationRequiredFor())));
+            }
+            proposalYnqs.add(proposalYnq);
+            /* add distinct group names */
+            if(!isDuplicateGroupName(type.getGroupName(), ynqGroupNames)) {
+                YnqGroupName ynqGroupName = new YnqGroupName();
+                ynqGroupName.setGroupName(type.getGroupName());
+                ynqGroupNames.add(ynqGroupName);
+            }
+        }
+    }
+    
+    private boolean isDuplicateGroupName(String groupName, List<YnqGroupName> ynqGroupNames) {
+        boolean duplicateGroupName = false;
+        for (YnqGroupName type : ynqGroupNames) {
+            if(type.getGroupName().equalsIgnoreCase(groupName)) {
+                duplicateGroupName = true;
+                break;
+            }
+        }
+        return duplicateGroupName;
+    }
+
+    /* get ynq explanation required / review date required column label */
+    private String getYnqRequiredLabel(String ynqCode) {
+        String retValue = null;
+        for (YnqConstants ynqConstants : YnqConstants.values()) {
+            if(ynqConstants.code().equalsIgnoreCase(ynqCode)) {
+                retValue =  ynqConstants.description();
+                break;
+            }
+        }
+        return retValue;
     }
 
     /**
