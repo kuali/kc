d898bfe030994a3ab49a4dccf47de532bb9593b7
Lin-Long Shyu <lshyukuali@gmail.com>
2007-12-04 19:17:32 +0000

    KRACOEUS-193: institutional attachment . click 'add' will save to DB . click 'delete' will remove it from DB

--- src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentInstituteAttachmentRule.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentInstituteAttachmentRule.java
@@ -43,7 +43,7 @@ import org.kuali.kra.proposaldevelopment.rule.event.AddInstituteAttachmentEvent;
 import org.kuali.kra.proposaldevelopment.rule.event.SaveInstituteAttachmentsEvent;
 import org.kuali.kra.rules.ResearchDocumentRuleBase;
 
-public class ProposalDevelopmentInstituteAttachmentRule extends ResearchDocumentRuleBase implements AddInstituteAttachmentRule,SaveInstituteAttachmentsRule { 
+public class ProposalDevelopmentInstituteAttachmentRule extends ResearchDocumentRuleBase implements AddInstituteAttachmentRule { 
     private static final String NARRATIVE_TYPE_ALLOWMULTIPLE_NO = "N";
     private static final String INSTITUTE = "Institute";
     private static final String NEW_INSTITUTE = "newInstitute";
@@ -61,11 +61,11 @@ public class ProposalDevelopmentInstituteAttachmentRule extends ResearchDocument
 
         if(StringUtils.isBlank(institute.getNarrativeTypeCode())){
             rulePassed = false;
-            reportError(errorPath, ERROR_ATTACHMENT_TYPE_NOT_SELECTED);
+            reportError(errorPath+".institutionalAttachmentTypeCode", ERROR_ATTACHMENT_TYPE_NOT_SELECTED);
         }
         if(StringUtils.isBlank(institute.getModuleStatusCode())){
             rulePassed = false;
-            reportError(errorPath, ERROR_ATTACHMENT_STATUS_NOT_SELECTED);
+            reportError(errorPath+".moduleStatusCode", ERROR_ATTACHMENT_STATUS_NOT_SELECTED);
         }
         if (rulePassed) {
             populateNarrativeType(institute);
@@ -75,49 +75,22 @@ public class ProposalDevelopmentInstituteAttachmentRule extends ResearchDocument
                 for (Narrative narr : document.getInstitutes()) {
                     if (narr!=null && StringUtils.equals(narr.getNarrativeTypeCode(),institute.getNarrativeTypeCode())) {
                         LOG.debug(ERROR_NARRATIVE_TYPE_DUPLICATE);
-                        reportError(errorPath, ERROR_NARRATIVE_TYPE_DUPLICATE, param);
+                        reportError(errorPath+".institutionalAttachmentTypeCode", ERROR_NARRATIVE_TYPE_DUPLICATE, param);
                         rulePassed = false;
                     }
                 }
             }
         }
-        
+        if (rulePassed && StringUtils.isNotBlank(institute.getNarrativeTypeCode()) && StringUtils.isNotBlank(institute.getModuleTitle())) {
+            if (StringUtils.isBlank(institute.getFileName())) {
+                rulePassed = false;
+                reportError(errorPath+".narrativeFile", KeyConstants.ERROR_REQUIRED_FOR_FILE_NAME, "File Name");
+            }
+    }
+
         return rulePassed;
     }
     
-    /**
-     * This method validates 'Institute Attachment'. It checks the following :
-     * If attachment type and description are not empty, then filename is a required field.
-     *
-     * @param saveInstituteAttachmentsEvent 
-     * @return valid Does the validation pass
-     */
-    public boolean processSaveInstituteAttachmentsBusinessRules(SaveInstituteAttachmentsEvent saveInstituteAttachmentsEvent) {
-        boolean valid = true;
-
-        ErrorMap errorMap = GlobalVariables.getErrorMap();
-        if (errorMap.isEmpty()) {
-            errorMap.addToErrorPath("document");
-        }
-
-        int i = 0;
-        ProposalDevelopmentDocument document = (ProposalDevelopmentDocument)saveInstituteAttachmentsEvent.getDocument();
-
-        // TODO : this will combine errors with proposal attachments panel
-        for (Narrative institute : document.getInstitutes()) {
-            errorMap.addToErrorPath("institute[" + i + "]");
-            institute.refresh();
-            if (StringUtils.isNotBlank(institute.getNarrativeTypeCode()) && StringUtils.isNotBlank(institute.getModuleTitle())) {
-                    if (StringUtils.isBlank(institute.getFileName())) {
-                        valid = false;
-                        errorMap.putError("fileName", KeyConstants.ERROR_REQUIRED_FOR_FILE_NAME, "File Name");
-                    }
-            }
-            errorMap.removeFromErrorPath("institute[" + i++ + "]");
-        }
-        errorMap.removeFromErrorPath("document");
-        return valid;
-    }
     private void populateNarrativeType(Narrative narrative) {
         Map<String,String> narrativeTypeMap = new HashMap<String,String>();
         narrativeTypeMap.put(NARRATIVE_TYPE_CODE, narrative.getNarrativeTypeCode());
