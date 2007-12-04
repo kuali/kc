2cd791b598305fcd364cef95e6deb43a7c600420
Lin-Long Shyu <lshyukuali@gmail.com>
2007-12-04 19:16:35 +0000

    KRACOEUS-192: personnel attachment . click 'add' button will save personnel attachment to DB. . click 'delete' will remove it from DB . rules applied when 'add' is clicked

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/rule/event/AddPersonnelAttachmentEvent.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.rule.event;
+
+import org.kuali.core.document.Document;
+import org.kuali.core.rule.BusinessRule;
+import org.kuali.kra.proposaldevelopment.bo.ProposalPersonBiography;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
+import org.kuali.kra.proposaldevelopment.rule.AddPersonnelAttachmentRule;
+
+public class AddPersonnelAttachmentEvent extends PersonnelAttachmentEventBase{
+    /**
+     * Constructs an AddPersonnelAttachmentEvent with the given errorPathPrefix, document, and proposalPersonBiography.
+     * 
+     * @param errorPathPrefix
+     * @param proposalDevelopmentDocument
+     * @param proposalPersonBiography
+     */
+    public AddPersonnelAttachmentEvent(String errorPathPrefix, ProposalDevelopmentDocument document, ProposalPersonBiography proposalPersonBiography) {
+        super("adding narrative to document " + getDocumentId(document), errorPathPrefix, document, proposalPersonBiography);
+    }
+
+    /**
+     * Constructs an AddPersonnelAttachmentEvent with the given errorPathPrefix, document, and proposalPersonBiography.
+     * 
+     * @param errorPathPrefix
+     * @param document
+     * @param narrative
+     */
+    public AddPersonnelAttachmentEvent(String errorPathPrefix, Document document, ProposalPersonBiography proposalPersonBiography) {
+        this(errorPathPrefix, (ProposalDevelopmentDocument) document, proposalPersonBiography);
+    }
+
+    /**
+     * @see org.kuali.core.rule.event.KualiDocumentEvent#getRuleInterfaceClass()
+     */
+    public Class getRuleInterfaceClass() {
+        return AddPersonnelAttachmentRule.class;
+    }
+
+    /**
+     * @see org.kuali.core.rule.event.KualiDocumentEvent#invokeRuleMethod(org.kuali.core.rule.BusinessRule)
+     */
+    public boolean invokeRuleMethod(BusinessRule rule) {
+        return ((AddPersonnelAttachmentRule) rule).processAddPersonnelAttachmentBusinessRules(this);
+    }
+
+}
