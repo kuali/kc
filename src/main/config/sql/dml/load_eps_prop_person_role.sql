5b3980dc5a030093741cde9a7ddfb41fce57009e
Rajeev Mancheril <rajeev@genesistec.com>
2007-12-17 18:42:08 +0000

    KRACOEUS-388 field indicator for person certification

--- src/main/config/sql/dml/load_eps_prop_person_role.sql
+++ src/main/config/sql/dml/load_eps_prop_person_role.sql
@@ -1,6 +1,6 @@
 #
-# $Id: load_eps_prop_person_role.sql,v 1.2 2007-10-18 15:49:28 lprzybyl Exp $
+# $Id: load_eps_prop_person_role.sql,v 1.3 2007-12-17 18:39:18 rmancher Exp $
 #
-insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER) values ('PI', 'Principal Investigator', sysdate, 'kradev');
-insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER) values ('COI', 'Co-Investigator', sysdate, 'kradev');
-insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER) values ('KP', 'Key Person', sysdate, 'kradev');
+insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, CERTIFICATION_REQUIRED, CERTIFICATION_REQUIREDUPDATE_TIMESTAMP, UPDATE_USER) values ('PI', 'Principal Investigator', 'Y', 'sysdate, 'kradev');
+insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, CERTIFICATION_REQUIRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('COI', 'Co-Investigator', 'Y', sysdate, 'kradev');
+insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, CERTIFICATION_REQUIRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('KP', 'Key Person', 'N', sysdate, 'kradev');
