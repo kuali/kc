b6ac9db74375e36b163ff8ec9da0188329677dcb
Rajeev Mancheril <rajeev@genesistec.com>
2007-12-17 20:09:36 +0000

    removed extra quotes

--- src/main/config/sql/dml/load_eps_prop_person_role.sql
+++ src/main/config/sql/dml/load_eps_prop_person_role.sql
@@ -1,6 +1,6 @@
 #
-# $Id: load_eps_prop_person_role.sql,v 1.3 2007-12-17 18:39:18 rmancher Exp $
+# $Id: load_eps_prop_person_role.sql,v 1.4 2007-12-17 20:09:36 rmancher Exp $
 #
-insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, CERTIFICATION_REQUIRED, CERTIFICATION_REQUIREDUPDATE_TIMESTAMP, UPDATE_USER) values ('PI', 'Principal Investigator', 'Y', 'sysdate, 'kradev');
+insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, CERTIFICATION_REQUIRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('PI', 'Principal Investigator', 'Y', sysdate, 'kradev');
 insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, CERTIFICATION_REQUIRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('COI', 'Co-Investigator', 'Y', sysdate, 'kradev');
 insert into EPS_PROP_PERSON_ROLE (PROP_PERSON_ROLE_ID, DESCRIPTION, CERTIFICATION_REQUIRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('KP', 'Key Person', 'N', sysdate, 'kradev');
