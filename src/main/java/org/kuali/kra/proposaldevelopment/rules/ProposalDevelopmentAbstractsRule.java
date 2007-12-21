e14c24b3516efbe1d949b894ff02db816dd911d4
Don Barre <dbarre@arizona.edu>
2007-12-21 17:50:09 +0000

    KRACOEUS-431: Bug fixes from code review (log classname, new for loop, asterick in tabErrorKey, etc.)

--- src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentAbstractsRule.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/rules/ProposalDevelopmentAbstractsRule.java
@@ -37,7 +37,7 @@ import org.kuali.kra.rules.ResearchDocumentRuleBase;
  * @author Kuali Research Administration Team (kualidev@oncourse.iu.edu)
  */
 public class ProposalDevelopmentAbstractsRule extends ResearchDocumentRuleBase implements AbstractsRule {
-    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(ProposalDevelopmentKeyPersonsRule.class);
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(ProposalDevelopmentAbstractsRule.class);
 
     /**
      * Don't allow abstracts with an invalid abstract type code or duplicate abstracts, i.e.
