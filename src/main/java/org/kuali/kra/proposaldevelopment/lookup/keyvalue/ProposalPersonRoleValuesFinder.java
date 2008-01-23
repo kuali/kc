0bb2350221d89973851b3cb9f39ef15ba36fa51c
Leo Przybylski <leo@rsmart.com>
2008-01-23 01:02:30 +0000

    KRACOEUS-552	- Adding check to ProposalPersonRoleValuesFinder for PI

--- src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/ProposalPersonRoleValuesFinder.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/ProposalPersonRoleValuesFinder.java
@@ -27,7 +27,12 @@ import org.kuali.core.web.ui.KeyLabelPair;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.proposaldevelopment.bo.ProposalPersonRole;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
+import org.kuali.kra.proposaldevelopment.service.KeyPersonnelService;
+import org.kuali.kra.proposaldevelopment.web.struts.form.ProposalDevelopmentForm;
 
+import static org.kuali.core.util.GlobalVariables.getKualiForm;
+import static org.kuali.kra.infrastructure.Constants.PRINCIPAL_INVESTIGATOR_ROLE;
 import static org.kuali.kra.infrastructure.Constants.PROPOSAL_PERSON_ROLE_PARAMETER_PREFIX;
 import static org.kuali.kra.infrastructure.Constants.PARAMETER_COMPONENT_DOCUMENT;
 import static org.kuali.kra.infrastructure.Constants.PARAMETER_MODULE_PROPOSAL_DEVELOPMENT;
@@ -37,7 +42,7 @@ import static org.kuali.kra.infrastructure.KraServiceLocator.getService;
  * Temporary class until this can be gotten working via table.
  *
  * @author $Author: lprzybyl $
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class ProposalPersonRoleValuesFinder extends KeyValuesBase {
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(ProposalPersonRoleValuesFinder.class);
@@ -46,15 +51,18 @@ public class ProposalPersonRoleValuesFinder extends KeyValuesBase {
      * @see org.kuali.core.lookup.keyvalues.KeyValuesBase#getKeyValues()
      */
     public List getKeyValues() {
-        KeyValuesService keyValuesService = (KeyValuesService) KraServiceLocator.getService("keyValuesService");
-        Collection<ProposalPersonRole> roles = keyValuesService.findAll(ProposalPersonRole.class);
+        final Collection<ProposalPersonRole> roles = getKeyValuesService().findAll(ProposalPersonRole.class);
+        final ProposalDevelopmentDocument document = ((ProposalDevelopmentForm) getKualiForm()).getProposalDevelopmentDocument();
+        final boolean hasPrincipalInvestigator = getKeyPersonnelService().hasPrincipalInvestigator(document);
         List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
         keyValues.add(new KeyLabelPair("", "select:"));
 
         for (ProposalPersonRole role : roles) {
             LOG.debug("Adding role " + role.getProposalPersonRoleId());
             LOG.debug("With description " + findRoleDescription(role));
-            keyValues.add(new KeyLabelPair(role.getProposalPersonRoleId(), findRoleDescription(role)));
+            if (!hasPrincipalInvestigator || !PRINCIPAL_INVESTIGATOR_ROLE.equals(role.getProposalPersonRoleId())) {
+                keyValues.add(new KeyLabelPair(role.getProposalPersonRoleId(), findRoleDescription(role)));
+            }
         }
 
         return keyValues;
@@ -72,7 +80,30 @@ public class ProposalPersonRoleValuesFinder extends KeyValuesBase {
                                                            + role.getProposalPersonRoleId().toLowerCase());    
     }
 
+    /**
+     * Locate from Spring a singleton instance of the <code>{@link KualiConfigurationService}</code>.
+     * 
+     * @return KualiConfigurationService
+     */
     protected KualiConfigurationService getConfigurationService() {
         return getService(KualiConfigurationService.class);
+    }
+    
+    /**
+     * Locate from Spring a singleton instance of the <code>{@link KeyPersonnelService}</code>.
+     * 
+     * @return KeyPersonnelService
+     */
+    private KeyPersonnelService getKeyPersonnelService() {
+        return getService(KeyPersonnelService.class);
+    }
+    
+    /**
+     * Locate from Spring a singleton instance of the <code>{@link KeyValuesService}</code>.
+     * 
+     * @return KeyValuesService
+     */
+    private KeyValuesService getKeyValuesService() {
+        return getService(KeyValuesService.class);
     }
 }
