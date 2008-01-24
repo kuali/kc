d74ca6db0d943b5676563eacd8b4e3b345b72620
Geo Thomas <geot@mit.edu>
2008-01-24 20:38:01 +0000

    Fixing data type conversion issues while converting from coeus s2s object to kra-s2s bos

--- src/main/java/org/kuali/kra/s2s/bo/S2sAppSubmission.java
+++ src/main/java/org/kuali/kra/s2s/bo/S2sAppSubmission.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2007 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.kuali.kra.s2s.bo;
 
 import java.util.LinkedHashMap;
-import java.sql.Date;
+import java.sql.Timestamp;
 
 import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
 
@@ -11,9 +26,9 @@ public class S2sAppSubmission extends KraPersistableBusinessObjectBase {
 	private String agencyTrackingId;
 	private String comments;
 	private String ggTrackingId;
-	private Date lastModifiedDate;
-	private Date lastNotifiedDate;
-	private Date receivedDate;
+	private Timestamp lastModifiedDate;
+	private Timestamp lastNotifiedDate;
+	private Timestamp receivedDate;
 	private String status;
 
 	public String getProposalNumber() {
@@ -56,27 +71,27 @@ public class S2sAppSubmission extends KraPersistableBusinessObjectBase {
 		this.ggTrackingId = ggTrackingId;
 	}
 
-	public Date getLastModifiedDate() {
+	public Timestamp getLastModifiedDate() {
 		return lastModifiedDate;
 	}
 
-	public void setLastModifiedDate(Date lastModifiedDate) {
+	public void setLastModifiedDate(Timestamp lastModifiedDate) {
 		this.lastModifiedDate = lastModifiedDate;
 	}
 
-	public Date getLastNotifiedDate() {
+	public Timestamp getLastNotifiedDate() {
 		return lastNotifiedDate;
 	}
 
-	public void setLastNotifiedDate(Date lastNotifiedDate) {
+	public void setLastNotifiedDate(Timestamp lastNotifiedDate) {
 		this.lastNotifiedDate = lastNotifiedDate;
 	}
 
-	public Date getReceivedDate() {
+	public Timestamp getReceivedDate() {
 		return receivedDate;
 	}
 
-	public void setReceivedDate(Date receivedDate) {
+	public void setReceivedDate(Timestamp receivedDate) {
 		this.receivedDate = receivedDate;
 	}
 
