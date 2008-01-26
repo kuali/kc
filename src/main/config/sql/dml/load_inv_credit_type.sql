aba30cdcc74172c6af69c640955a14d4b54b243f
Leo Przybylski <leo@rsmart.com>
2008-01-26 19:18:52 +0000

    KRACOEUS-559  Reusing PersistableBusinessObjectValuesFinder more

--- src/main/config/sql/dml/load_inv_credit_type.sql
+++ src/main/config/sql/dml/load_inv_credit_type.sql
@@ -1,7 +1,7 @@
 #
-# $Id: load_inv_credit_type.sql,v 1.2 2007-10-18 15:49:28 lprzybyl Exp $
+# $Id: load_inv_credit_type.sql,v 1.3 2008-01-26 19:18:52 lprzybyl Exp $
 #
-insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('0', 'Recognition', 'N', sysdate, 'kradev');
-insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('1', 'Responsibility', 'N', sysdate, 'kradev');
-insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('2', 'Space', 'N', sysdate, 'kradev');
-insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('3', 'Financial', 'N', sysdate, 'kradev');
+insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('0', 'Recognition', 'Y', sysdate, 'kradev');
+insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('1', 'Responsibility', 'Y', sysdate, 'kradev');
+insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('2', 'Space', 'Y', sysdate, 'kradev');
+insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('3', 'Financial', 'Y', sysdate, 'kradev');
