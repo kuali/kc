3acbb3419a92bdcb108523bee8ff257a2b759bbb
Leo Przybylski <leo@rsmart.com>
2008-01-28 20:00:33 +0000

    KRACOEUS-333 - updating dml to make entries active

--- src/main/config/sql/dml/load_inv_credit_type.sql
+++ src/main/config/sql/dml/load_inv_credit_type.sql
@@ -1,7 +1,7 @@
 #
-# $Id: load_inv_credit_type.sql,v 1.3 2008-01-26 19:18:52 lprzybyl Exp $
+# $Id: load_inv_credit_type.sql,v 1.4 2008-01-28 20:00:33 lprzybyl Exp $
 #
-insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('0', 'Recognition', 'Y', sysdate, 'kradev');
-insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('1', 'Responsibility', 'Y', sysdate, 'kradev');
-insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('2', 'Space', 'Y', sysdate, 'kradev');
-insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER) values ('3', 'Financial', 'Y', sysdate, 'kradev');
+insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER, ACTIVE_FLAG) values ('0', 'Recognition', 'Y', sysdate, 'kradev', 'Y');
+insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER, ACTIVE_FLAG) values ('1', 'Responsibility', 'Y', sysdate, 'kradev', 'Y');
+insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER, ACTIVE_FLAG) values ('2', 'Space', 'Y', sysdate, 'kradev', 'Y');
+insert into INV_CREDIT_TYPE (INV_CREDIT_TYPE_CODE, DESCRIPTION, ADDS_TO_HUNDRED, UPDATE_TIMESTAMP, UPDATE_USER, ACTIVE_FLAG) values ('3', 'Financial', 'Y', sysdate, 'kradev', 'Y');
