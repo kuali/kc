67af8cd5f6187fe1245055b6e285189eb68c06b6
Geoff McGregor <gmcgrego@iu.edu>
2008-02-16 19:21:54 +0000

    remove unneeded trvl  data

--- src/main/resources/DefaultTestData.sql
+++ src/main/resources/DefaultTestData.sql
@@ -1,34 +1,6 @@
 
-insert into trv_acct_fo (acct_fo_id, acct_fo_user_name) values (1, 'fred')
-;
-insert into trv_acct_fo (acct_fo_id, acct_fo_user_name) values (2, 'fran')
-;
-insert into trv_acct_fo (acct_fo_id, acct_fo_user_name) values (3, 'frank')
-;
-insert into TRV_ACCT_TYPE values ('CAT', 'Clearing Account Type')
-;
-insert into TRV_ACCT_TYPE values ('EAT', 'Expense Account Type')
-;
-insert into TRV_ACCT_TYPE values ('IAT', 'Income Account Type')
-;
-insert into TRV_ACCT values ('a1', 'a1', 'CAT', 1)
-;
-insert into TRV_ACCT values ('a2', 'a2', 'EAT', 2)
-;
-insert into TRV_ACCT values ('a3', 'a3', 'IAT', 3)
-;
-insert into TRV_ACCT_EXT values ('a1', 'CAT')
-;
-insert into TRV_ACCT_EXT values ('a2', 'EAT')
-;
-insert into TRV_ACCT_EXT values ('a3', 'IAT')
-;
-insert into TRV_DOC_ACCT (DOC_HDR_ID, ACCT_NUM) values (1, 'a1')
-;
-insert into TRV_DOC_ACCT (DOC_HDR_ID, ACCT_NUM) values (1, 'a2')
-;
-insert into TRV_DOC_ACCT (DOC_HDR_ID, ACCT_NUM) values (1, 'a3')
-;
+
+
 insert into en_wrkgrp_t values (1,1,'WorkflowAdmin',1,'W','Workflow Administrator Workgroup',1,-1,0)
 ;
 insert into EN_WRKGRP_MBR_T (WRKGRP_MBR_PRSN_EN_ID, WRKGRP_ID, WRKGRP_MBR_TYP, WRKGRP_VER_NBR, DB_LOCK_VER_NBR)
