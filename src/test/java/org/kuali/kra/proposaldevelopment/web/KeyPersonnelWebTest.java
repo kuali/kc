0f264588bfc2858d909845f453e3b24200015cb5
Leo Przybylski <leo@rsmart.com>
2008-02-26 19:43:20 +0000

    KRACOEUS-698 - Adding Unit Test to check Rolodex

--- src/test/java/org/kuali/kra/proposaldevelopment/web/KeyPersonnelWebTest.java
+++ src/test/java/org/kuali/kra/proposaldevelopment/web/KeyPersonnelWebTest.java
@@ -24,7 +24,7 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 /**
  *  Web Test class for testing the Key Personnel Tab of the <code>{@link ProposalDevelopmentDocument}</code>
  *  @author $Author: lprzybyl $
- *  @version $Revision: 1.6 $
+ *  @version $Revision: 1.7 $
  */
 public class KeyPersonnelWebTest extends ProposalDevelopmentWebTestBase {
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(KeyPersonnelWebTest.class);
@@ -81,6 +81,21 @@ public class KeyPersonnelWebTest extends ProposalDevelopmentWebTestBase {
     public void addPrincipalInvestigator() throws Exception {
         HtmlPage keyPersonnelPage = lookup(getKeyPersonnelPage(), "org.kuali.kra.bo.Person");
         assertEquals("Terry Durkin", getFieldValue(keyPersonnelPage, "newProposalPerson.fullName"));
+        setFieldValue(keyPersonnelPage,"newProposalPerson.proposalPersonRoleId", "PI");
+
+        keyPersonnelPage = clickOn(getElementByName(keyPersonnelPage, "methodToCall.insertProposalPerson", true));
+
+        assertFalse(keyPersonnelPage.asText().contains(ERRORS_FOUND_ON_PAGE));
+        saveAndSearchDoc(keyPersonnelPage);
+    }
+
+    /**
+     * Test adding a principal investigator
+     */
+    @Test
+    public void addPrincipalInvestigator_Rolodex() throws Exception {
+        HtmlPage keyPersonnelPage = lookup(getKeyPersonnelPage(), "org.kuali.kra.bo.NonOrganizationalRolodex");
+        assertEquals("First Name Middle Name Last Name", getFieldValue(keyPersonnelPage, "newProposalPerson.fullName"));
         setFieldValue(keyPersonnelPage,"newProposalPerson.proposalPersonRoleId", "PI");
 
         keyPersonnelPage = clickOn(getElementByName(keyPersonnelPage, "methodToCall.insertProposalPerson", true));
