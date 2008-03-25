77dee9d41b1b0b0f905ee9d4444015c5f930fbeb
Geo Thomas <geot@mit.edu>
2008-03-25 04:25:06 +0000

    Adding more budget calculation tests depending on the individual scripts

---
+++ src/test/java/org/kuali/kra/budget/calculator/BudgetCalculatorServiceTest.java
@@ -0,0 +1,275 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.budget.calculator;
+
+import java.sql.Timestamp;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.core.UserSession;
+import org.kuali.core.service.BusinessObjectService;
+import org.kuali.core.service.DocumentService;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.kra.KraTestBase;
+import org.kuali.kra.bo.InstituteLaRate;
+import org.kuali.kra.bo.InstituteRate;
+import org.kuali.kra.budget.BudgetDecimal;
+import org.kuali.kra.budget.bo.BudgetLineItem;
+import org.kuali.kra.budget.bo.BudgetLineItemCalculatedAmount;
+import org.kuali.kra.budget.bo.BudgetPeriod;
+import org.kuali.kra.budget.bo.BudgetPersonnelDetails;
+import org.kuali.kra.budget.bo.BudgetProposalLaRate;
+import org.kuali.kra.budget.bo.BudgetProposalRate;
+import org.kuali.kra.budget.document.BudgetDocument;
+import org.kuali.kra.budget.service.BudgetCalculationService;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
+import org.kuali.rice.KNSServiceLocator;
+import org.kuali.rice.test.data.PerTestUnitTestData;
+import org.kuali.rice.test.data.UnitTestData;
+import org.kuali.rice.test.data.UnitTestFile;
+import org.kuali.rice.test.data.UnitTestSql;
+
+/**
+ * This class is for testing Line item calculations
+ */
+@PerTestUnitTestData(
+        @UnitTestData(order = { 
+                UnitTestData.Type.SQL_STATEMENTS, UnitTestData.Type.SQL_FILES }, 
+        sqlStatements = {
+                      @UnitTestSql("delete from VALID_CE_RATE_TYPES where cost_element like '900%'"),
+                      @UnitTestSql("delete from cost_element where cost_element like '900%'"),
+                      }, 
+        sqlFiles = {
+                @UnitTestFile(filename = "classpath:sql/dml/load_calc_service_test_cost_element.sql", delimiter = ";")
+                ,@UnitTestFile(filename = "classpath:sql/dml/load_calc_service_test_valid_ce_rate_types.sql", delimiter = ";")
+                })
+        )
+
+public class BudgetCalculatorServiceTest extends KraTestBase {
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(BudgetCalculatorServiceTest.class);
+    private DocumentService documentService = null;
+    private BusinessObjectService bos;
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        GlobalVariables.setUserSession(new UserSession("quickstart"));
+        documentService = KNSServiceLocator.getDocumentService();
+        bos = KraServiceLocator.getService(BusinessObjectService.class);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        GlobalVariables.setUserSession(null);
+        documentService = null;
+        super.tearDown();
+    }
+    private BudgetDocument createBudgetDocument() throws Exception{
+        ProposalDevelopmentDocument document = (ProposalDevelopmentDocument) documentService.getNewDocument("ProposalDevelopmentDocument");
+        Date requestedStartDateInitial = new Date(System.currentTimeMillis());
+        Date requestedEndDateInitial = new Date(System.currentTimeMillis());
+        setBaseDocumentFields(document, "ProposalDevelopmentDocumentTest test doc", "005770", "project title", requestedStartDateInitial, requestedEndDateInitial, "1", "1", "000001");
+        documentService.saveDocument(document);
+        BudgetDocument bd = (BudgetDocument)documentService.getNewDocument("BudgetDocument");
+        setBaseDocumentFields(bd,document.getProposalNumber());
+        documentService.saveDocument(bd);
+        BudgetDocument savedBudgetDocument = (BudgetDocument)documentService.getByDocumentHeaderId(bd.getDocumentNumber());
+        populateDummyRates(savedBudgetDocument);
+        assertNotNull("Budget document not saved",savedBudgetDocument);
+        return savedBudgetDocument;
+    }
+    @Test
+    public void calculateLineItemWithoutRatesTest() throws Exception{
+        List<String> errors = new ArrayList<String>();
+        BudgetDocument bd = createBudgetDocument();
+        assertNotNull("Budget document not saved",bd);
+        
+        BudgetPeriod bp = getBudgetPeriod(bd,1,"2004-01-01","2005-12-31");
+        bd.getBudgetPeriods().add(bp);
+        BudgetLineItem bli = getLineItem(bp, 1, "900000",java.sql.Date.valueOf("2005-01-01"),
+                java.sql.Date.valueOf("2005-12-31"),10000.00d,100.00d);
+        bp.getBudgetLineItems().add(bli);
+        BudgetCalculationService bcs = getService(BudgetCalculationService.class);
+        bcs.calculateBudgetLineItem(bd,bli);
+        List<BudgetLineItemCalculatedAmount> calcAmounts = bli.getBudgetLineItemCalculatedAmounts();
+        assertTrue(calcAmounts.isEmpty());
+        for (BudgetLineItemCalculatedAmount budgetLineItemCalculatedAmount : calcAmounts) {
+            LOG.info(budgetLineItemCalculatedAmount);
+        }
+        BudgetDecimal directCost = bli.getDirectCost();
+        assertEquals(new BudgetDecimal(10000.00),directCost );
+    }
+    @Test
+    public void calculateLineItemWithMTDCTest() throws Exception{
+        List<String> errors = new ArrayList<String>();
+        BudgetDocument bd = createBudgetDocument();
+        assertNotNull("Budget document not saved",bd);
+        
+        BudgetPeriod bp = getBudgetPeriod(bd,1,"2004-01-01","2005-12-31");
+        bd.getBudgetPeriods().add(bp);
+        BudgetLineItem bli = getLineItem(bp, 1, "900001",java.sql.Date.valueOf("2000-01-01"),
+                java.sql.Date.valueOf("2000-06-30"),10000.00d,100.00d);
+        bp.getBudgetLineItems().add(bli);
+        BudgetCalculationService bcs = getService(BudgetCalculationService.class);
+        bcs.calculateBudgetLineItem(bd,bli);
+        List<BudgetLineItemCalculatedAmount> calcAmounts = bli.getBudgetLineItemCalculatedAmounts();
+        assertTrue(calcAmounts.size()==1);
+        for (BudgetLineItemCalculatedAmount budgetLineItemCalculatedAmount : calcAmounts) {
+            LOG.info(budgetLineItemCalculatedAmount);
+        }
+        BudgetDecimal directCost = bli.getDirectCost();
+        assertEquals(new BudgetDecimal(10000.00),directCost );
+        BudgetDecimal indirectCost = bli.getIndirectCost();
+        assertEquals(new BudgetDecimal(6350.00),indirectCost );
+        
+    }
+
+    private BudgetPersonnelDetails getPersonnelLineItem(BudgetPeriod bp, int lineItemNumber, 
+            String costElement,Date startDate,Date endDate,
+            double lineItemCost,double costSharingAmount, int personNumber,int personSequenceNumber,String personId,String jobCode,double effort,double charged) {
+        BudgetPersonnelDetails bli = new BudgetPersonnelDetails();
+        bli.setProposalNumber(bp.getProposalNumber());
+        bli.setBudgetVersionNumber(bp.getBudgetVersionNumber());
+        bli.setBudgetPeriod(bp.getBudgetPeriod());
+        bli.setStartDate(startDate);
+        bli.setEndDate(endDate);
+        bli.setApplyInRateFlag(true);
+        bli.setOnOffCampusFlag(true);
+        bli.setLineItemNumber(lineItemNumber);
+        bli.setBudgetCategoryCode("1");
+        bli.setCostElement(costElement);
+        bli.setLineItemCost(new BudgetDecimal(lineItemCost));
+        bli.setCostSharingAmount(new BudgetDecimal(costSharingAmount));
+        bli.setUpdateTimestamp(bp.getUpdateTimestamp());
+        bli.setUpdateUser(bp.getUpdateUser());
+        bli.setPersonNumber(personNumber);
+        bli.setPersonSequenceNumber(personSequenceNumber);
+        bli.setPersonId(personId);
+        bli.setJobCode(jobCode);
+        bli.setPercentEffort(new BudgetDecimal(effort));
+        bli.setPercentCharged(new BudgetDecimal(charged));
+        return bli;
+    }
+
+    private void populateDummyRates(BudgetDocument bd) {
+        List<BudgetProposalRate> budgetProposalRates = bd.getBudgetProposalRates();
+        List<InstituteRate> instRates = (List)bos.findAll(InstituteRate.class);
+        for (InstituteRate instituteRate : instRates) {
+            BudgetProposalRate bpr = new BudgetProposalRate();
+            bpr.setProposalNumber(bd.getProposalNumber().toString());
+            bpr.setProposalNumber(bd.getProposalNumber().toString());
+            bpr.setBudgetVersionNumber(bd.getBudgetVersionNumber());
+            bpr.setActivityTypeCode(instituteRate.getActivityTypeCode());
+            bpr.setFiscalYear(instituteRate.getFiscalYear());
+            bpr.setOnOffCampusFlag(instituteRate.getOnOffCampusFlag());
+            bpr.setRateClassCode(instituteRate.getRateClassCode());
+            bpr.setRateTypeCode(instituteRate.getRateTypeCode());
+            bpr.setStartDate(instituteRate.getStartDate());
+            bpr.setUnitNumber(instituteRate.getUnitNumber());
+            bpr.setInstituteRate(instituteRate.getInstituteRate());
+            bpr.setApplicableRate(bpr.getInstituteRate());
+            budgetProposalRates.add(bpr);
+        }
+        List<BudgetProposalLaRate> budgetProposalLaRates = bd.getBudgetProposalLaRates();
+        List<InstituteLaRate> instLaRates = (List)bos.findAll(InstituteLaRate.class);
+        
+        for (InstituteLaRate instituteLaRate : instLaRates) {
+            BudgetProposalLaRate bpr = new BudgetProposalLaRate();
+            bpr.setProposalNumber(bd.getProposalNumber().toString());
+            bpr.setProposalNumber(bd.getProposalNumber().toString());
+            bpr.setBudgetVersionNumber(bd.getBudgetVersionNumber());
+            bpr.setFiscalYear(instituteLaRate.getFiscalYear());
+            bpr.setOnOffCampusFlag(instituteLaRate.getOnOffCampusFlag());
+            bpr.setRateClassCode(instituteLaRate.getRateClassCode());
+            bpr.setRateTypeCode(instituteLaRate.getRateTypeCode());
+            bpr.setStartDate(instituteLaRate.getStartDate());
+            bpr.setUnitNumber(instituteLaRate.getUnitNumber());
+            bpr.setInstituteRate(instituteLaRate.getInstituteRate());
+            bpr.setApplicableRate(bpr.getInstituteRate());
+            budgetProposalLaRates.add(bpr);
+        }
+        
+    }
+
+    private BudgetPeriod getBudgetPeriod(BudgetDocument bd, int period, String startDate, String endDate) {
+        BudgetPeriod bp = new BudgetPeriod();
+        bp.setProposalNumber(bd.getProposalNumber().toString());
+        bp.setBudgetVersionNumber(bd.getBudgetVersionNumber());
+        bp.setBudgetPeriod(period);
+        bp.setStartDate(java.sql.Date.valueOf(startDate));
+        bp.setEndDate(java.sql.Date.valueOf(endDate));
+        bp.setUpdateUser(bd.getUpdateUser());
+        bp.setUpdateTimestamp(bd.getUpdateTimestamp());
+        return bp;
+//        addLineItem(bd,1,1,1,"400250");
+        
+//        bd.getBudgetPeriods().add(bp);
+    }
+
+    private BudgetLineItem getLineItem(BudgetPeriod bp, int lineItemNumber, 
+                String costElement,Date startDate,Date endDate,
+                double lineItemCost,double costSharingAmount) {
+        BudgetLineItem bli = new BudgetLineItem();
+        bli.setProposalNumber(bp.getProposalNumber());
+        bli.setBudgetVersionNumber(bp.getBudgetVersionNumber());
+        bli.setBudgetPeriod(bp.getBudgetPeriod());
+        bli.setStartDate(startDate);
+        bli.setEndDate(endDate);
+        bli.setApplyInRateFlag(true);
+        bli.setOnOffCampusFlag(true);
+        bli.setLineItemNumber(lineItemNumber);
+        bli.setBudgetCategoryCode("1");
+        bli.setCostElement(costElement);
+        bli.setLineItemCost(new BudgetDecimal(lineItemCost));
+        bli.setCostSharingAmount(new BudgetDecimal(costSharingAmount));
+        bli.setUpdateTimestamp(bp.getUpdateTimestamp());
+        bli.setUpdateUser(bp.getUpdateUser());
+        return bli;
+        
+    }
+
+    private void setBaseDocumentFields(BudgetDocument bd,String proposalNumber) {
+        bd.getDocumentHeader().setFinancialDocumentDescription("Test budget calculation");
+//        bd.setDocumentNumber(bd.getDocumentNumber());
+        bd.setProposalNumber(proposalNumber);
+        bd.setBudgetVersionNumber(1);
+        bd.setStartDate(java.sql.Date.valueOf("2002-01-01"));
+        bd.setEndDate(java.sql.Date.valueOf("2008-12-31"));
+        bd.setUpdateTimestamp(new Timestamp(System.currentTimeMillis()));
+        bd.setUpdateUser("KRADEV");
+        bd.setOhRateClassCode("1");
+        bd.setUrRateClassCode("1");
+        bd.setModularBudgetFlag("N");
+        bd.setActivityTypeCode("1");
+    }
+    
+    
+    
+    private void setBaseDocumentFields(ProposalDevelopmentDocument document, String description, String sponsorCode, String title, Date requestedStartDateInitial, Date requestedEndDateInitial, String activityTypeCode, String proposalTypeCode, String ownedByUnit) {
+        document.getDocumentHeader().setFinancialDocumentDescription(description);
+        document.setSponsorCode(sponsorCode);
+        document.setTitle(title);
+        document.setRequestedStartDateInitial(requestedStartDateInitial);
+        document.setRequestedEndDateInitial(requestedEndDateInitial);
+        document.setActivityTypeCode(activityTypeCode);
+        document.setProposalTypeCode(proposalTypeCode);
+        document.setOwnedByUnitNumber(ownedByUnit);
+    }
+}
