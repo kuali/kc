2b8776b483c60b279baf916e4c8e303bed290527
Lin-Long Shyu <lshyukuali@gmail.com>
2008-03-28 12:29:56 +0000

    KRACOEUS-883: organization maintainance doc - add several collections

---
+++ src/main/java/org/kuali/kra/bo/OrganizationType.java
@@ -0,0 +1,56 @@
+package org.kuali.kra.bo;
+
+import java.util.LinkedHashMap;
+
+public class OrganizationType extends KraPersistableBusinessObjectBase {
+
+	private String organizationId;
+	private Integer organizationTypeCode;
+	private OrganizationTypeList organizationTypeList;
+	private Organization organization;
+
+	public OrganizationType(){
+		super();
+	}
+
+	public String getOrganizationId() {
+		return organizationId;
+	}
+
+	public void setOrganizationId(String organizationId) {
+		this.organizationId = organizationId;
+	}
+
+	public Integer getOrganizationTypeCode() {
+		return organizationTypeCode;
+	}
+
+	public void setOrganizationTypeCode(Integer organizationTypeCode) {
+		this.organizationTypeCode = organizationTypeCode;
+	}
+
+
+	@Override 
+	protected LinkedHashMap toStringMapper() {
+		LinkedHashMap hashMap = new LinkedHashMap();
+		hashMap.put("organizationId", getOrganizationId());
+		hashMap.put("organizationTypeCode", getOrganizationTypeCode());
+		return hashMap;
+	}
+
+    public OrganizationTypeList getOrganizationTypeList() {
+        return organizationTypeList;
+    }
+
+    public void setOrganizationTypeList(OrganizationTypeList organizationTypeList) {
+        this.organizationTypeList = organizationTypeList;
+    }
+
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
+}
