d9c35c90af50eb6ba7898dcd346c7aaa93f2a983
Jaffrin Salam <abdulj@ais.msu.edu>
2008-03-28 22:58:09 +0000

    KRACOEUS-630

--- src/test/resources/sql/dml/Load_users.sql
+++ src/test/resources/sql/dml/Load_users.sql
@@ -1,4 +1,1 @@
-delete from en_rule_rsp_t t where t.rule_base_val_id in (select t1.rule_base_val_id from en_rule_base_val_t t1 where t1.doc_typ_nm = 'ProposalDevelopmentDocument');
-delete from en_rule_base_val_t t where t.doc_typ_nm = 'ProposalDevelopmentDocument';
-delete from en_rule_tmpl_t t where t.rule_tmpl_nm in ('FirstApprovalRouting', 'SecondApprovalRouting', 'FinalApprovalRouting');
-delete from en_doc_typ_t t where t.doc_typ_nm = 'ProposalDevelopmentDocument';  
+Insert into EN_USR_T (PRSN_EN_ID,PRSN_UNIV_ID,PRSN_NTWRK_ID,PRSN_UNVL_USR_ID,PRSN_EMAIL_ADDR,PRSN_NM,PRSN_GVN_NM,PRSN_LST_NM,USR_CRTE_DT,USR_LST_UPDT_DT,PRSN_ID_MSNG_IND,DB_LOCK_VER_NBR) values ('tdurkin','tdurkin','tdurkin','000000001',null,'Terry Durkin','Terry Durkin','Durkin',null,null,0,0);
