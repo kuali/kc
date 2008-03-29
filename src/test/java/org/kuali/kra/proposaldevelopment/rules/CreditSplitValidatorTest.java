6627d33b5c0c818ef635774da17e35b73451cc4f
Leo Przybylski <leo@rsmart.com>
2008-03-29 04:01:11 +0000

    Moving credit split related tests where they belong

--- src/test/java/org/kuali/kra/proposaldevelopment/rules/CreditSplitValidatorTest.java
+++ src/test/java/org/kuali/kra/proposaldevelopment/rules/CreditSplitValidatorTest.java
@@ -15,9 +15,23 @@
  */
 package org.kuali.kra.proposaldevelopment.rules;
 
+import static org.kuali.core.util.GlobalVariables.getAuditErrorMap;
+import static org.kuali.core.util.GlobalVariables.setAuditErrorMap;
+import static org.kuali.core.util.GlobalVariables.setUserSession;
+import static org.kuali.kra.test.fixtures.ProposalDevelopmentDocumentFixture.NORMAL_DOCUMENT;
+import static org.kuali.kra.test.fixtures.ProposalPersonFixture.INVESTIGATOR_OVER_ONE_HUNDRED;
+import static org.kuali.kra.test.fixtures.ProposalPersonFixture.INVESTIGATOR_SPLIT_ADDS_TO_ONE_HUNDRED;
+import static org.kuali.kra.test.fixtures.ProposalPersonFixture.INVESTIGATOR_UNDER_ZERO;
+import static org.kuali.kra.test.fixtures.ProposalPersonFixture.INVESTIGATOR_UNIT_NOT_TO_ONE_HUNDRED;
+import static org.kuali.kra.test.fixtures.ProposalPersonFixture.INVESTIGATOR_UNIT_OVER_ONE_HUNDRED;
+import static org.kuali.kra.test.fixtures.ProposalPersonFixture.INVESTIGATOR_UNIT_UNDER_ZERO;
+
+import java.util.HashMap;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.kuali.core.UserSession;
 import org.kuali.kra.KraTestBase;
 
 import org.kuali.kra.proposaldevelopment.bo.ProposalPerson;
@@ -29,16 +43,20 @@ import org.kuali.kra.proposaldevelopment.rules.CreditSplitValidator;
  * Class to test use cases of <code>{@link CreditSplitValidator}</code>
  *
  * @author $Author: lprzybyl $
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class CreditSplitValidatorTest extends KraTestBase {
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(CreditSplitValidatorTest.class);
     private CreditSplitValidator validator;
+    private ProposalDevelopmentDocument document;
     
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        validator = new CreditSplitValidator();
+        setUserSession(new UserSession("quickstart"));
+        setAuditErrorMap(new HashMap());
+        validator = CreditSplitValidator.getInstance();
+        document = NORMAL_DOCUMENT.getDocument();
     }
     
     @After
@@ -46,40 +64,73 @@ public class CreditSplitValidatorTest extends KraTestBase {
         super.tearDown();
     }
 
-       
+
     /**
-     *
-     * @return ProposalDevelopmentDocument
+     * A <code>{@link ProposalDevelopmentDocument}</code> instance can only have one Proposal Investigator. Test that the rule applies.<br/> 
+     * <br/>
+     * This tests whether exactly one Principle Investigator will work. This test should yield a passed rule.
+     * 
+     * @see org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument
+     * @see org.kuali.kra.proposaldevelopment.rules.KeyPersonnelAuditRule
      */
-    private ProposalDevelopmentDocument createInvalidDocument() {
-        ProposalDevelopmentDocument retval = new ProposalDevelopmentDocument();
-        retval.getProposalPersons().add(createInvalidPersonWithCreditSplit());
-        return retval;
-    }
-    
-    private ProposalPerson createInvalidPersonWithCreditSplit() {
-        ProposalPerson retval = new ProposalPerson();
-
-        return retval;
-    }
-    
-    private ProposalPersonUnit createInvalidUnitWithCreditSplit() {
-        return null;
-    }
-    
-    private ProposalDevelopmentDocument createValidDocument() {
-        ProposalDevelopmentDocument retval = new ProposalDevelopmentDocument();
-        retval.getProposalPersons().add(createValidPersonWithCreditSplit());
-        return retval;
+    @Test
+    public void validProposalInvestigatorNormal() throws Exception {
+        LOG.info("Starting validProposalInvetigatorNormal");
+        ProposalPerson person1 = INVESTIGATOR_SPLIT_ADDS_TO_ONE_HUNDRED.getPerson();
+        INVESTIGATOR_SPLIT_ADDS_TO_ONE_HUNDRED.populatePerson(document, person1);
+        document.addProposalPerson(person1);
+        assertTrue("Audit Rule shouldn't produce audit errors", validator.validate(document));
+        assertEquals(0, getAuditErrorMap().size());
+        LOG.info("Ending validProposalInvetigatorNormal");
     }
 
-    private ProposalPerson createValidPersonWithCreditSplit() {
-        ProposalPerson retval = new ProposalPerson();
-        
-        return retval;
+    @Test
+    public void invalidProposalInvestigatorFail() throws Exception {
+        ProposalDevelopmentDocument document = NORMAL_DOCUMENT.getDocument();
+        ProposalPerson person = INVESTIGATOR_UNIT_NOT_TO_ONE_HUNDRED.getPerson();
+        document.addProposalPerson(person);
+        INVESTIGATOR_UNIT_NOT_TO_ONE_HUNDRED.populatePerson(document, person);
+        assertFalse("Audit Rule should produce audit errors", validator.validate(document));
+        assertFalse(getAuditErrorMap().size() < 1);
     }
     
-    private ProposalPersonUnit createValidUnitWithCreditSplit() {
-        return null;
+    @Test
+    public void invalidProposalInvestigatorOverHundred() throws Exception {
+        ProposalDevelopmentDocument document = NORMAL_DOCUMENT.getDocument();
+        ProposalPerson person = INVESTIGATOR_OVER_ONE_HUNDRED.getPerson();
+        document.addProposalPerson(person);
+        INVESTIGATOR_OVER_ONE_HUNDRED.populatePerson(document, person);
+        assertFalse("Audit Rule should produce audit errors", validator.validate(document));
+        assertFalse(getAuditErrorMap().size() < 1);
+    }
+
+    @Test
+    public void invalidProposalInvestigatorUnderZero() throws Exception {
+        ProposalDevelopmentDocument document = NORMAL_DOCUMENT.getDocument();
+        ProposalPerson person = INVESTIGATOR_UNDER_ZERO.getPerson();
+        document.addProposalPerson(person);
+        INVESTIGATOR_UNDER_ZERO.populatePerson(document, person);
+        assertFalse("Audit Rule should produce audit errors", validator.validate(document));
+        assertFalse(getAuditErrorMap().size() < 1);
+    }
+
+    @Test
+    public void invalidProposalInvestigatorUnitsUnderZero() throws Exception {
+        ProposalDevelopmentDocument document = NORMAL_DOCUMENT.getDocument();
+        ProposalPerson person = INVESTIGATOR_UNIT_UNDER_ZERO.getPerson();
+        document.addProposalPerson(person);
+        INVESTIGATOR_UNIT_UNDER_ZERO.populatePerson(document, person);
+        assertFalse("Audit Rule should produce audit errors", validator.validate(document));
+        assertFalse(getAuditErrorMap().size() < 1);
+    }
+
+    @Test
+    public void invalidProposalInvestigatorUnitsOverHundred() throws Exception {
+        ProposalDevelopmentDocument document = NORMAL_DOCUMENT.getDocument();
+        ProposalPerson person = INVESTIGATOR_UNIT_OVER_ONE_HUNDRED.getPerson();
+        document.addProposalPerson(person);
+        INVESTIGATOR_UNIT_OVER_ONE_HUNDRED.populatePerson(document, person);
+        assertFalse("Audit Rule should produce audit errors", validator.validate(document));
+        assertFalse(getAuditErrorMap().size() < 1);
     }
 }
