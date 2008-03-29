a857ab8075ee6c650a71c1798b72ab5ef4a4e885
Jaffrin Salam <abdulj@ais.msu.edu>
2008-03-29 16:34:40 +0000

    Changed the file name KRACOEUS-630

---
+++ src/test/resources/sql/dml/load_users.sql
@@ -0,0 +1,1 @@
+Insert into EN_USR_T (PRSN_EN_ID,PRSN_UNIV_ID,PRSN_NTWRK_ID,PRSN_UNVL_USR_ID,PRSN_EMAIL_ADDR,PRSN_NM,PRSN_GVN_NM,PRSN_LST_NM,USR_CRTE_DT,USR_LST_UPDT_DT,PRSN_ID_MSNG_IND,DB_LOCK_VER_NBR) values ('tdurkin','tdurkin','tdurkin','000000001',null,'Terry Durkin','Terry Durkin','Durkin',null,null,0,0);
