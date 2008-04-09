0333fe8afb409fc726bcd1bf77f59f6b982c0ffc
Lin-Long Shyu <lshyukuali@gmail.com>
2008-04-09 18:48:00 +0000

    KRACOEUS-736: most of these changes are to add ';' or "/" at the end of the statement.

--- src/main/config/sql/s2s/views/OSP$EPS_PROP_PERSON_DEGREE.sql
+++ src/main/config/sql/s2s/views/OSP$EPS_PROP_PERSON_DEGREE.sql
@@ -3,4 +3,4 @@ CREATE OR REPLACE VIEW osp$eps_prop_person_degree ( PROPOSAL_NUMBER, PERSON_ID, 
 	   AS select a.PROPOSAL_NUMBER, decode(b.PERSON_ID,null,b.rolodex_id,b.person_id), DEGREE_CODE, to_date(GRADUATION_YEAR,'YYYY'), a.DEGREE, FIELD_OF_STUDY, SPECIALIZATION, a.SCHOOL, SCHOOL_ID_CODE, SCHOOL_ID, a.UPDATE_TIMESTAMP, a.UPDATE_USER
 	from eps_prop_person_degree a ,eps_prop_person b
 	where a.proposal_number = b.proposal_number
-		  and a.prop_person_number = b.prop_person_number
+		  and a.prop_person_number = b.prop_person_number;
