31b8b6550b501c5182ff3da8f5bc5e6110009c8d
Lin-Long Shyu <lshyukuali@gmail.com>
2008-04-10 12:59:46 +0000

    KRACOEUS-879 :

--- src/main/java/org/kuali/kra/infrastructure/BudgetSummaryErrorConstants.java
+++ src/main/java/org/kuali/kra/infrastructure/BudgetSummaryErrorConstants.java
@@ -19,13 +19,13 @@ import java.util.Map;
 
 public enum BudgetSummaryErrorConstants {
         ERROR_PERIOD_START_BEFORE_PROJECT_START("error.periodStartDate.before.projectStartDate", "startDate"),
-        ERROR_PERIOD_START_BEFORE_PREVIOUS_START("error.periodStartDate.before.previousEndDate", "startDate"),
+        ERROR_PERIOD_START_BEFORE_PREVIOUS_END("error.periodStartDate.before.previousEndDate", "startDate"),
         ERROR_NEW_PERIOD_VALID("error.newPeriod.validDate", "startDate"),
         ERROR_NEW_PERIOD_START_AFTER_PROJECT_END("error.new.PeriodStartDate.after.projectEndDate", "startDate"),
         ERROR_PERIOD_START_REQUIRED("error.periodStartDate.required", "startDate"),
         ERROR_NEW_PERIOD_INVALID("error.new.PeriodStartDate.invalid", "startDate"),
         ERROR_PERIOD_START_AFTER_PERIOD_END("error.periodStart.after.periodEnd", "endDate"),
-        ERROR_PERIOD_START_AFTER_PROJECT_END("error.periodStartDate.after.projectEndDate", "endDate"),
+        ERROR_PERIOD_START_AFTER_PROJECT_END("error.periodStartDate.after.projectEndDate", "startDate"),
         ERROR_PERIOD_END_BEFORE_PROJECT_START("error.periodEnd.before.projectStart", "endDate"),
         ERROR_PERIOD_END_AFTER_PROJECT_END("error.periodEndDate.after.projectEndDate", "endDate"),
         ERROR_PERIOD_END_BEFORE_PREVIOUS_END("error.periodEnd.before.previousEnd", "endDate"),
