3a97513ae63de1c5e3cff1ada0f168767963ea67
Lin-Long Shyu <lshyukuali@gmail.com>
2008-04-28 19:39:41 +0000

    KRACOEUS-1119: budget audit rule to check period date if project start/end date has been changed

--- src/main/java/org/kuali/kra/proposaldevelopment/web/struts/action/ProposalDevelopmentAction.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/web/struts/action/ProposalDevelopmentAction.java
@@ -444,40 +444,4 @@ public class ProposalDevelopmentAction extends ProposalActionBase {
     }
 
 
-    /**
-     * 
-     * This method is for audit rule to forward to the page that the audit error fix is clicked.
-     * @param mapping
-     * @param form
-     * @param request
-     * @param response
-     * @return
-     */
-    public ActionForward budgetPersonnel(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        // TODO : should be able to refactor as more budget audit rules are implemented for more budget pages
-        // This is just an example set up to test jira 780
-        ProposalDevelopmentForm pdForm = (ProposalDevelopmentForm) form;
-        ProposalDevelopmentDocument pdDoc = pdForm.getProposalDevelopmentDocument();
-        BudgetDocument budgetDocument = null;
-        String forward = null;
-        try {
-            for (BudgetVersionOverview budgetVersion: pdDoc.getBudgetVersionOverviews()) {
-                if (budgetVersion.isFinalVersionFlag()) {
-                    DocumentService documentService = KraServiceLocator.getService(DocumentService.class);
-                    budgetDocument = (BudgetDocument) documentService.getByDocumentHeaderId(budgetVersion.getDocumentNumber());
-                }
-            }
-            Long routeHeaderId = budgetDocument.getDocumentHeader().getWorkflowDocument().getRouteHeaderId();
-            forward = buildForwardUrl(routeHeaderId);
-        } catch (Exception e) {
-            LOG.info("forward to budgetpersonnel "+e.getStackTrace());
-            //TODO what is the forward here
-        }
-        // TODO : what if forward is null
-        forward = StringUtils.replace(forward, "budgetVersions.do?", "budgetPersonnel.do?audit=true&");
-        
-        return new ActionForward(forward, true);
-    }
-
-    
 }
