9b3fb0cd25373be7efb8fdc63541786a6d7dcecc
Geoff McGregor <gmcgrego@iu.edu>
2008-04-30 21:23:36 +0000

    KRACOEUS-1189

--- src/main/config/sql/tables/custom_attribute_data_type.sql
+++ src/main/config/sql/tables/custom_attribute_data_type.sql
@@ -3,7 +3,7 @@ create table custom_attribute_data_type
   data_type_code  varchar2(3) constraint custom_attribute_data_type_n1 not null,
   description varchar2 (200) constraint custom_attribute_data_type_n2 not null,
   update_timestamp date constraint custom_attribute_data_type_n3 not null,
-  update_user varchar2 (8) constraint custom_attribute_data_type_n4 not null,
+  update_user varchar2 (60) constraint custom_attribute_data_type_n4 not null,
   ver_nbr number(8,0) DEFAULT 1 constraint custom_attribute_data_type_n5 NOT NULL,
   obj_id varchar2(36) DEFAULT SYS_GUID() constraint custom_attribute_data_type_n6 NOT NULL,
   constraint custom_attribute_data_type_p1 primary key (data_type_code),
