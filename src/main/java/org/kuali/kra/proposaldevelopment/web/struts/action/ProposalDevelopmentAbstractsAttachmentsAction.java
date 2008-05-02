3b63fdfbc33b13b03e1a652c836146cf38bd174a
Don Barre <dbarre@arizona.edu>
2008-05-02 16:39:09 +0000

    KRACOEUS-656: Another file to extend username field to 60 characters

--- src/main/java/org/kuali/kra/proposaldevelopment/web/struts/action/ProposalDevelopmentAbstractsAttachmentsAction.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/web/struts/action/ProposalDevelopmentAbstractsAttachmentsAction.java
@@ -490,9 +490,9 @@ public class ProposalDevelopmentAbstractsAttachmentsAction extends ProposalDevel
     private void updateUserTimestamp(KraPersistableBusinessObjectBase bo) {
         String updateUser = GlobalVariables.getUserSession().getLoggedInUserNetworkId();
     
-        // Since the UPDATE_USER column is only VACHAR(8), we need to truncate this string if it's longer than 8 characters
-        if (updateUser.length() > 8) {
-            updateUser = updateUser.substring(0, 8);
+        // Since the UPDATE_USER column is only VACHAR(60), we need to truncate this string if it's longer than 60 characters
+        if (updateUser.length() > 60) {
+            updateUser = updateUser.substring(0, 60);
         }
         bo.setUpdateTimestamp((getService(DateTimeService.class)).getCurrentTimestamp());
         bo.setUpdateUser(updateUser);
