e30e8e6349dfe978d6630e38ab37c0923e3726f0
Leo Przybylski <leo@rsmart.com>
2008-05-23 00:47:08 +0000

    KRACOEUS-1311  Adding DML to the relase 1.0 branch.

---
+++ src/main/config/sql/dml/load_system_params.sql
@@ -0,0 +1,108 @@
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetVersionsHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetSummaryHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetPersonnelHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetRatesHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetExpensesHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetTotalsHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetDistributionAndIncomeHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetModularBudgetHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetActionsHelp','CONFG','blah://blah','Budget Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetStatusHelpUrl','CONFG','blah://blah','Budget Status Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetPeriodHelpUrl','CONFG','blah://blah','Budget Period Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetRateClassHelpUrl','CONFG','blah://blah','Rate Class Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetPersonHelpUrl','CONFG','blah://blah','Budget Person Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetUnrecoveredFandAHelpUrl','CONFG','blah://blah','Budget Unrecovered F and A Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetProjectIncomeHelpUrl','CONFG','blah://blah','Budget Project Income Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-B','D','budgetCategoryHelpUrl','CONFG','blah://blah','Budget Category Help','A','Y');
+
+
+
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentKeyPersonnelHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentSpecialReviewHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentCustomDataHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentQuestionsHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentPermissionsHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentAbstractsAttachmentsHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentProposalHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentGrantsGovHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentActionsHelp','CONFG','blah://blah','Proposal Development Page Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentSponsorHelpUrl','CONFG','blah://blah','Sponsor Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentOrganizationHelpUrl','CONFG','blah://blah','Organization Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentLocationHelpUrl','CONFG','blah://blah','Location Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentKeywordsHelpUrl','CONFG','blah://blah','Keywords Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentCreditSplitHelpUrl','CONFG','blah://blah','Credit Split Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentYnqHelpUrl','CONFG','blah://blah','Yes/No Questions Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentPersonHelpUrl','CONFG','blah://blah','Person Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentOpportunityHelpUrl','CONFG','blah://blah','Grants.gov Opportunity Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind)
+values ('KRA-PD','D','proposalDevelopmentNarrativeHelpUrl','CONFG','blah://blah','Narrative Help','A','Y');
+
+insert into sh_parm_t (sh_parm_nmspc_cd,sh_parm_dtl_typ_cd,sh_parm_nm,sh_parm_typ_cd,sh_parm_txt,sh_parm_desc,sh_parm_cons_cd,active_ind) 
+values ('KRA-PD','D','proposalDevelopmentAbstractHelpUrl','CONFG','blah://blah','Abstract Help','A','Y');
+
