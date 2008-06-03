f9b970d3444d30240f9c7a28df5d06a325a1bf1b
Jaffrin Salam <abdulj@ais.msu.edu>
2008-06-03 13:55:31 +0000

    Multi PI KRACOEUS-1353

--- src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/ProposalPersonRoleValuesFinder.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/lookup/keyvalue/ProposalPersonRoleValuesFinder.java
@@ -21,33 +21,45 @@ import static org.kuali.kra.infrastructure.Constants.PARAMETER_COMPONENT_DOCUMEN
 import static org.kuali.kra.infrastructure.Constants.PARAMETER_MODULE_PROPOSAL_DEVELOPMENT;
 import static org.kuali.kra.infrastructure.Constants.PRINCIPAL_INVESTIGATOR_ROLE;
 import static org.kuali.kra.infrastructure.Constants.PROPOSAL_PERSON_ROLE_PARAMETER_PREFIX;
+import static org.kuali.kra.infrastructure.Constants.SPONSOR_LEVEL_HIERARCHY;
+import static org.kuali.kra.infrastructure.Constants.NIH_SPONSOR_ACRONYM;
+import static org.kuali.kra.infrastructure.Constants.SPONSOR_HIERARCHY_NAME;
 import static org.kuali.kra.infrastructure.KraServiceLocator.getService;
 import static org.kuali.kra.logging.FormattedLogger.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.kuali.core.lookup.keyvalues.KeyValuesBase;
+import org.kuali.core.service.BusinessObjectService;
 import org.kuali.core.service.KeyValuesService;
 import org.kuali.core.service.KualiConfigurationService;
 import org.kuali.core.web.ui.KeyLabelPair;
+import org.kuali.kra.bo.Person;
+import org.kuali.kra.bo.SponsorHierarchy;
+import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.proposaldevelopment.bo.ProposalPersonRole;
 import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
 import org.kuali.kra.proposaldevelopment.service.KeyPersonnelService;
 import org.kuali.kra.proposaldevelopment.web.struts.form.ProposalDevelopmentForm;
 
+import edu.iu.uis.eden.engine.node.KeyValuePair;
+
 /**
  * Temporary class until this can be gotten working via table.
  *
- * @author $Author: lprzybyl $
- * @version $Revision: 1.12 $
+ * @author $Author: jsalam $
+ * @version $Revision: 1.13 $
  */
 public class ProposalPersonRoleValuesFinder extends KeyValuesBase {
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(ProposalPersonRoleValuesFinder.class);
-    
+
     private String forAddedPerson;
-       
+    private boolean nih=false;
     /**
      * @see org.kuali.core.lookup.keyvalues.KeyValuesBase#getKeyValues()
      */
@@ -57,42 +69,116 @@ public class ProposalPersonRoleValuesFinder extends KeyValuesBase {
         final boolean hasPrincipalInvestigator = getKeyPersonnelService().hasPrincipalInvestigator(document);
         List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
         keyValues.add(new KeyLabelPair("", "select"));
+        Map valueMap = new HashMap();
+        valueMap.put("sponsorCode", document.getSponsorCode());
+        valueMap.put("hierarchyName",getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                PARAMETER_COMPONENT_DOCUMENT, 
+                SPONSOR_HIERARCHY_NAME ));
+        BusinessObjectService bos = KraServiceLocator.getService(BusinessObjectService.class);
+        Collection<Person> sponsor_hierarchy=  bos.findMatching(SponsorHierarchy.class, valueMap);
+        if (CollectionUtils.isNotEmpty(sponsor_hierarchy)) {
+            for (Object variable : sponsor_hierarchy) {
+                SponsorHierarchy sponhierarchy=(SponsorHierarchy) variable;
+                if(sponhierarchy.getLevel1().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    nih=true;
+                }else if(sponhierarchy.getLevel2().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    
+                    nih=true;
+                }else if(sponhierarchy.getLevel3().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    
+                    nih=true;
+                }else if(sponhierarchy.getLevel4().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    nih=true;
+                }else if(sponhierarchy.getLevel5().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    nih=true;
+                }else if(sponhierarchy.getLevel6().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    nih=true;
+                }else if(sponhierarchy.getLevel7().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    nih=true;
+                }else if(sponhierarchy.getLevel8().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    nih=true;
+                }else if(sponhierarchy.getLevel9().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    nih=true;
+                }else if (sponhierarchy.getLevel10().equals(getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                        PARAMETER_COMPONENT_DOCUMENT, 
+                        SPONSOR_LEVEL_HIERARCHY ))){
+                    nih=true;
+                }
+
+            }
+
+        }
 
         for (ProposalPersonRole role : roles) {
             info("Adding role %s", role.getProposalPersonRoleId());
             info("With description %s", findRoleDescription(role));
-            
+
             boolean showRole = true;
-            
+
             // If the person has already been added, then exclude Key Person
             if (isPersonAdded()) {
                 showRole = !KEY_PERSON_ROLE.equals(role.getProposalPersonRoleId());
             }
 
             info("showRole = %s", showRole);
-            
-            if (showRole) {
-                keyValues.add(new KeyLabelPair(role.getProposalPersonRoleId(), findRoleDescription(role)));
-            }
-        }
 
-        info("Returning %s", keyValues);
-        
+            if (showRole) {
+                if(nih){
+                    keyValues.add(new KeyLabelPair(role.getProposalPersonRoleId(), findNIHRoleDescription(role)));
+                }
+                else
+                {
+                    keyValues.add(new KeyLabelPair(role.getProposalPersonRoleId(), findRoleDescription(role)));
+                }
+            }
+
+            info("Returning %s", keyValues);
+        }
         return keyValues;
     }
-
     protected String getRoleIdPrefix() {
-        return new String();
+        if(nih){
+            return "nonnih.";
+        }
+        else
+        {
+            return new String();
+        }
+    }
+    protected String findRoleDescription(ProposalPersonRole role) {
+          return getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                PARAMETER_COMPONENT_DOCUMENT, 
+                PROPOSAL_PERSON_ROLE_PARAMETER_PREFIX 
+                + getRoleIdPrefix()
+                + role.getProposalPersonRoleId().toLowerCase());    
     }
 
-    protected String findRoleDescription(ProposalPersonRole role) {
-        return getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
-                                                           PARAMETER_COMPONENT_DOCUMENT, 
-                                                           PROPOSAL_PERSON_ROLE_PARAMETER_PREFIX 
-                                                           + getRoleIdPrefix()
-                                                           + role.getProposalPersonRoleId().toLowerCase());    
+
+    protected String findNIHRoleDescription(ProposalPersonRole role) {
+            return getConfigurationService().getParameterValue(PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                PARAMETER_COMPONENT_DOCUMENT, 
+                PROPOSAL_PERSON_ROLE_PARAMETER_PREFIX 
+                + getRoleIdPrefix()
+                + role.getProposalPersonRoleId().toLowerCase());    
     }
-        
     /**
      * Used to indicate to the values finder whether the role has already been rendered
      * 
@@ -110,7 +196,7 @@ public class ProposalPersonRoleValuesFinder extends KeyValuesBase {
     protected KualiConfigurationService getConfigurationService() {
         return getService(KualiConfigurationService.class);
     }
-    
+
     /**
      * Locate from Spring a singleton instance of the <code>{@link KeyPersonnelService}</code>.
      * 
@@ -119,7 +205,7 @@ public class ProposalPersonRoleValuesFinder extends KeyValuesBase {
     private KeyPersonnelService getKeyPersonnelService() {
         return getService(KeyPersonnelService.class);
     }
-    
+
     /**
      * Locate from Spring a singleton instance of the <code>{@link KeyValuesService}</code>.
      * 
@@ -128,7 +214,7 @@ public class ProposalPersonRoleValuesFinder extends KeyValuesBase {
     private KeyValuesService getKeyValuesService() {
         return getService(KeyValuesService.class);
     }
-    
+
     private Boolean isPersonAdded() {
         return new Boolean(getForAddedPerson());
     }
