9628d17ffffde32530271af7529fee66c8325466
Lin-Long Shyu <lshyukuali@gmail.com>
2008-06-04 16:07:04 +0000

    KRACOEUS-1430

---
+++ src/main/java/org/kuali/kra/rules/OrganizationMaintenanceDocumentRule.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.rules;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.RiceKeyConstants;
+import org.kuali.core.document.MaintenanceDocument;
+import org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.kra.bo.Organization;
+import org.kuali.kra.bo.OrganizationYnq;
+
+public class OrganizationMaintenanceDocumentRule  extends MaintenanceDocumentRuleBase {
+
+    /**
+     * Constructs a OrganizationMaintenanceDocumentRule.java.
+     */
+    public OrganizationMaintenanceDocumentRule() {
+        super();
+    }
+    
+    /**
+     * 
+     * @see org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase#processCustomRouteDocumentBusinessRules(org.kuali.core.document.MaintenanceDocument)
+     */ 
+    public boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+        return checkYNQ(document);
+    }
+    
+    /**
+     * 
+     * @see org.kuali.core.maintenance.rules.MaintenanceDocumentRuleBase#processCustomApproveDocumentBusinessRules(org.kuali.core.document.MaintenanceDocument)
+     */
+    @Override
+    public boolean processCustomApproveDocumentBusinessRules(MaintenanceDocument document) {
+        return checkYNQ(document);
+    }
+
+    /**
+     * 
+     * This method to check Ynq's explanation and review date is required field based on answer.
+     * @param maintenanceDocument
+     * @return
+     */
+    private boolean checkYNQ(MaintenanceDocument maintenanceDocument) {
+
+        boolean valid = true;
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("new maintainable is: " + maintenanceDocument.getNewMaintainableObject().getClass());
+        }
+        Organization newOrganization = (Organization) maintenanceDocument.getNewMaintainableObject().getBusinessObject();
+
+        int i = 0;
+        for (OrganizationYnq organizationYnq : newOrganization.getOrganizationYnqs()) {
+            organizationYnq.refreshReferenceObject("ynq");
+            if (StringUtils.isNotBlank(organizationYnq.getAnswer()) && 
+                    organizationYnq.getAnswer().equalsIgnoreCase(organizationYnq.getYnq().getExplanationRequiredFor()) && StringUtils.isBlank(organizationYnq.getExplanation())) {
+                GlobalVariables.getErrorMap().putError("document.newMaintainableObject.organizationYnqs["+i+"].explanation", RiceKeyConstants.ERROR_REQUIRED,
+                        new String[] { "Explanation" });
+                valid = false;
+            }
+            if (StringUtils.isNotBlank(organizationYnq.getAnswer()) && 
+                    organizationYnq.getAnswer().equalsIgnoreCase(organizationYnq.getYnq().getDateRequiredFor()) && 
+                    organizationYnq.getReviewDate() == null
+                   ) {
+                    valid = false;
+                    GlobalVariables.getErrorMap().putError("document.newMaintainableObject.organizationYnqs["+i+"].reviewDate", RiceKeyConstants.ERROR_REQUIRED,
+                            new String[] { "Review Date" });
+            }
+            i++;
+        }
+
+
+        return valid;
+
+    }
+
+}
