12415481a951e3d0e8009d2b81bdefc612221d8e
Leo Przybylski <leo@rsmart.com>
2008-06-04 09:20:56 +0000

    KRACOEUS-1421 - Using SponsorName instead of SponsorCode

--- src/test/java/org/kuali/kra/proposaldevelopment/web/ProposalDevelopmentDocumentWebTest.java
+++ src/test/java/org/kuali/kra/proposaldevelopment/web/ProposalDevelopmentDocumentWebTest.java
@@ -205,7 +205,7 @@ public class ProposalDevelopmentDocumentWebTest extends ProposalDevelopmentWebTe
         final URL url = new URL("http://localhost:" + getPort() + "/kra-dev/");
         final HtmlPage page3 = login(webClient, url, "proposalDevelopmentProposal.do?methodToCall=docHandler&command=initiate&docTypeName=ProposalDevelopmentDocument");
         
-        assertContains(page3,"Sponsor Code:");
+        assertContains(page3,"Sponsor Name:");
         assertContains(page3,"PI:");
         
         final HtmlForm kualiForm = (HtmlForm) page3.getForms().get(0);
@@ -220,7 +220,7 @@ public class ProposalDevelopmentDocumentWebTest extends ProposalDevelopmentWebTe
 
         assertNotNull(doc);
 
-        assertContains(page3,"Sponsor Code:");
+        assertContains(page3,"Sponsor Name:");
         assertContains(page3,"PI:");        
     }
 
