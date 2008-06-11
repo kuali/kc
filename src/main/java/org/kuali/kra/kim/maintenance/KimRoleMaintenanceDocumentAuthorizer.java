8af1667ac38df4f82dba60cade8d92e2fad8b675
Don Barre <dbarre@arizona.edu>
2008-06-11 17:01:45 +0000

    KRACOEUS-1503 : Set role properties to read-only on modification using authorizer.

---
+++ src/main/java/org/kuali/kra/kim/maintenance/KimRoleMaintenanceDocumentAuthorizer.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.kim.maintenance;
+
+import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.document.MaintenanceDocument;
+import org.kuali.core.document.authorization.MaintenanceDocumentAuthorizations;
+import org.kuali.core.document.authorization.MaintenanceDocumentAuthorizerBase;
+import org.kuali.kra.kim.bo.KimRole;
+
+/**
+ * When a role is initially created, its name, role type, and descend flag can be
+ * set.  Once created, those values cannot be modified.
+ */
+public class KimRoleMaintenanceDocumentAuthorizer extends MaintenanceDocumentAuthorizerBase {
+
+    /**
+     * @see org.kuali.core.document.authorization.MaintenanceDocumentAuthorizerBase#getFieldAuthorizations(org.kuali.core.document.MaintenanceDocument, org.kuali.core.bo.user.UniversalUser)
+     */
+    @Override
+    public MaintenanceDocumentAuthorizations getFieldAuthorizations(MaintenanceDocument document, UniversalUser user) {
+
+        MaintenanceDocumentAuthorizations auths = new MaintenanceDocumentAuthorizations();
+        KimRole role = (KimRole) document.getNewMaintainableObject().getBusinessObject();
+
+        // The ID of a role is null only for creation.  Therefore, if it isn't null,
+        // then it is a modification and we must set some properties to be read-only.
+        
+        if (role.getId() != null) {
+            auths.addReadonlyAuthField("name");
+            auths.addReadonlyAuthField("roleTypeCode");
+            auths.addReadonlyAuthField("descend");
+        }
+
+        return auths;
+    }
+}
