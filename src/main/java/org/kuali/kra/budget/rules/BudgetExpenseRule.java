5f5c45250405d3324355fffbfe9662f07ac8c904
Lin-Long Shyu <lshyukuali@gmail.com>
2008-06-13 20:15:28 +0000

    KRACOEUS-1528 KRACOEUS-1529

---
+++ src/main/java/org/kuali/kra/budget/rules/BudgetExpenseRule.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.budget.rules;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.kuali.core.service.BusinessObjectService;
+import org.kuali.core.util.ErrorMap;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.kra.budget.bo.BudgetLineItem;
+import org.kuali.kra.budget.bo.BudgetPersonnelDetails;
+import org.kuali.kra.budget.document.BudgetDocument;
+import org.kuali.kra.infrastructure.KeyConstants;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+
+public class BudgetExpenseRule {
+
+    public BudgetExpenseRule() {
+    }
+
+    public boolean processCheckExistBudgetPersonnelDetailsBusinessRules(BudgetDocument budgetDocument, BudgetLineItem budgetLineItem, int lineItemToDelete) {
+        boolean valid = true;
+        
+        ErrorMap errorMap = GlobalVariables.getErrorMap();
+        if (CollectionUtils.isNotEmpty(budgetLineItem.getBudgetPersonnelDetailsList())) {
+                // just try to make sure key is on budget personnel tab
+                errorMap.putError("document.budgetPeriods["+(budgetLineItem.getBudgetPeriod()-1)+"].budgetLineItems["+lineItemToDelete+"].costElement", KeyConstants.ERROR_DELETE_LINE_ITEM);
+                    valid = false;
+        }
+                    
+        return valid;
+    }
+
+}
