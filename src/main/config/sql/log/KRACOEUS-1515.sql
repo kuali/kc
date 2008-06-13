35d5fb29dc43ba7cda40a09126dcdf582516f395
Don Barre <dbarre@arizona.edu>
2008-06-13 17:35:38 +0000

    KRACOEUS-1458 : Final changes for new roles and permissions

---
+++ src/main/config/sql/log/KRACOEUS-1515.sql
@@ -0,0 +1,5 @@
+/* Add the PRINT_PROPOSAL permission to the proposal roles. */
+insert into KIM_ROLES_PERMISSIONS_T (ROLE_ID,PERMISSION_ID) values(2,10);
+insert into KIM_ROLES_PERMISSIONS_T (ROLE_ID,PERMISSION_ID) values(3,10);
+insert into KIM_ROLES_PERMISSIONS_T (ROLE_ID,PERMISSION_ID) values(4,10);
+insert into KIM_ROLES_PERMISSIONS_T (ROLE_ID,PERMISSION_ID) values(5,10);
