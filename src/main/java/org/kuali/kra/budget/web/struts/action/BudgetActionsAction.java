b9fd37d18ca454e67d31bd6a4cb4faccaf41dd1c
Jaffrin Salam <abdulj@ais.msu.edu>
2008-06-16 21:32:04 +0000

    Overrode the close method [KRACOEUS-1505]

--- src/main/java/org/kuali/kra/budget/web/struts/action/BudgetActionsAction.java
+++ src/main/java/org/kuali/kra/budget/web/struts/action/BudgetActionsAction.java
@@ -28,8 +28,13 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.kuali.RiceConstants;
+import org.kuali.RiceKeyConstants;
+import org.kuali.core.question.ConfirmationQuestion;
+import org.kuali.core.service.KualiConfigurationService;
 import org.kuali.core.util.GlobalVariables;
 import org.kuali.core.util.WebUtils;
+import org.kuali.core.web.struts.form.KualiDocumentFormBase;
 import org.kuali.kra.budget.BudgetException;
 import org.kuali.kra.budget.document.BudgetDocument;
 import org.kuali.kra.budget.service.BudgetJustificationService;
@@ -40,6 +45,7 @@ import org.kuali.kra.budget.web.struts.form.BudgetJustificationWrapper;
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.proposaldevelopment.bo.AttachmentDataSource;
 import org.kuali.kra.proposaldevelopment.web.struts.action.ProposalDevelopmentAbstractsAttachmentsAction;
+import org.kuali.rice.KNSServiceLocator;
 
 public class BudgetActionsAction extends BudgetAction {
     private BudgetJustificationService budgetJustificationService;
@@ -81,6 +87,42 @@ public class BudgetActionsAction extends BudgetAction {
         budgetJustificationService.preSave(getBudgetDocument(form), getBudgetJusticationWrapper(form));
         
         return super.save(mapping, form, request, response);
+    }
+    /**
+     * Close the document and take the user back to the index; only after asking the user if they want to save the document first.
+     * Only users who have the "canSave()" permission are given this option.
+     *
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return ActionForward
+     * @throws Exception
+     */
+    @Override
+    public ActionForward close(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        KualiDocumentFormBase docForm = (KualiDocumentFormBase) form;
+        // only want to prompt them to save if they already can save
+        if (docForm.getDocumentActionFlags().getCanSave()) {
+            Object question = request.getParameter(RiceConstants.QUESTION_INST_ATTRIBUTE_NAME);
+            KualiConfigurationService kualiConfiguration = KNSServiceLocator.getKualiConfigurationService();
+
+            // logic for close question
+            if (question == null) {
+                // ask question if not already asked
+                return this.performQuestionWithoutInput(mapping, form, request, response, RiceConstants.DOCUMENT_SAVE_BEFORE_CLOSE_QUESTION, kualiConfiguration.getPropertyString(RiceKeyConstants.QUESTION_SAVE_BEFORE_CLOSE), RiceConstants.CONFIRMATION_QUESTION, RiceConstants.MAPPING_CLOSE, "");
+            }
+            else {
+                Object buttonClicked = request.getParameter(RiceConstants.QUESTION_CLICKED_BUTTON);
+                if ((RiceConstants.DOCUMENT_SAVE_BEFORE_CLOSE_QUESTION.equals(question)) && ConfirmationQuestion.YES.equals(buttonClicked)) {
+                    // if yes button clicked - save the doc
+                    budgetJustificationService.preSave(getBudgetDocument(form), getBudgetJusticationWrapper(form));
+                }
+                // else go to close logic below
+            }
+        }
+
+        return super.close(mapping, form, request, response);
     }
     
     @Override
