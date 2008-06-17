11384fe08fa83acf1f1f6813701b595bc9e99f3a
Rajeev Mancheril <rajeev@genesistec.com>
2008-06-17 02:27:45 +0000

    KRACOEUS-1544 KRACOEUS-1510

---
+++ src/main/config/sql/log/KRACOEUS-1544.sql
@@ -0,0 +1,11 @@
+update Narrative_type
+set description = '1st Institutional Attachment'
+where narrative_type_code = 59;
+
+
+update Narrative_type
+set description = '2nd Institutional Attachment'
+where narrative_type_code = 60;
+
+commit;
+
