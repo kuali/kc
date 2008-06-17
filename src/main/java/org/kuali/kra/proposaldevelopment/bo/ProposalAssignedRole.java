81d0c6caf0322c270af5b8a5fbc1f267c4a95b57
Don Barre <dbarre@arizona.edu>
2008-06-17 22:44:32 +0000

    KRACOEUS-1524 : In Proposal, make all Proposal roles available to the end user

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/bo/ProposalAssignedRole.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.bo;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.kuali.core.bo.BusinessObjectBase;
+
+public class ProposalAssignedRole extends BusinessObjectBase {
+
+    private String roleName;
+    private List<String> userNames;
+    
+    public ProposalAssignedRole(String roleName, List<String> userNames) {
+        this.roleName = roleName;
+        this.userNames = userNames;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public List<String> getUserNames() {
+        return userNames;
+    }
+
+    @Override
+    protected LinkedHashMap toStringMapper() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void refresh() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    
+}
---
+++ src/main/java/org/kuali/kra/proposaldevelopment/bo/ProposalRoleState.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.bo;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.core.bo.BusinessObjectBase;
+
+public class ProposalRoleState extends BusinessObjectBase {
+
+    private String name;
+    private Boolean state;
+    
+    public ProposalRoleState() {
+        name = null;
+        state = Boolean.FALSE;
+    }
+    
+    public ProposalRoleState(String name) {
+        this.name = name;
+        state = Boolean.FALSE;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getState() {
+        return state;
+    }
+
+    public void setState(Boolean state) {
+        this.state = state;
+    }
+
+    @Override
+    protected LinkedHashMap toStringMapper() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void refresh() {
+        // do nothing
+    }
+}
