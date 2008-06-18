ea84c40b7838a1ab08848423968767495ffcffd2
Jaffrin Salam <abdulj@ais.msu.edu>
2008-06-18 13:24:16 +0000

    Reconcile head and branch.

--- src/test/resources/sql/dml/load_activity_type.sql
+++ src/test/resources/sql/dml/load_activity_type.sql
@@ -1,9 +1,5 @@
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('1','Organized Research',sysdate,'kradev');
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('2','Instruction and Departmental Research',sysdate,'kradev');
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('3','Fellowship - Pre-Doctoral',sysdate,'kradev');
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('4','Public Service',sysdate,'kradev');
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('5','Student Services',sysdate,'kradev');
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('6','Other',sysdate,'kradev');
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('7','Fellowship - Post-Doctoral',sysdate,'kradev');
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('8','Clinical Trial',sysdate,'kradev');
-insert into activity_type (activity_type_code,description,update_timestamp,update_user) values('9','Construction',sysdate,'kradev');
+Insert into activity_type (ACTIVITY_TYPE_CODE,DESCRIPTION,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR,OBJ_ID) values ('1','Research',to_date('15-APR-08','DD-MON-RR'),'kradev',1,'7AA6C5B5C3824B34866D6B130CC2E6D6');
+Insert into activity_type (ACTIVITY_TYPE_CODE,DESCRIPTION,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR,OBJ_ID) values ('2','Instruction',to_date('15-APR-08','DD-MON-RR'),'kradev',1,'936D9E62489F435E84B9539A81738994');
+Insert into activity_type (ACTIVITY_TYPE_CODE,DESCRIPTION,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR,OBJ_ID) values ('3','Public Service',to_date('15-APR-08','DD-MON-RR'),'kradev',1,'FC8498B022A64A18897A146C5062ECDB');
+Insert into activity_type (ACTIVITY_TYPE_CODE,DESCRIPTION,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR,OBJ_ID) values ('4','Clinical Trial',to_date('15-APR-08','DD-MON-RR'),'kradev',1,'B38880C8F78F4C46A4F291785534E8BD');
+Insert into activity_type (ACTIVITY_TYPE_CODE,DESCRIPTION,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR,OBJ_ID) values ('5','other',to_date('15-APR-08','DD-MON-RR'),'kradev',1,'D5D69B90DC684013B2EE91FA3D9E8D26');
