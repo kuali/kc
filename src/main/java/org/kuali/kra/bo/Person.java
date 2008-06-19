b25d3f13703f39c3635aa4504250fcc322313d00
Leo Przybylski <leo@rsmart.com>
2008-06-19 03:28:14 +0000

    KRACOEUS-1549 - Modifying DD files so that names and descriptions are used instead of cryptic codes and ids

--- src/main/java/org/kuali/kra/bo/Person.java
+++ src/main/java/org/kuali/kra/bo/Person.java
@@ -28,7 +28,7 @@ import org.kuali.kra.kim.bo.KimPerson;
  *
  * @see org.kuali.core.bo.BusinessObject
  * @see org.kuali.core.bo.PersistableBusinessObject
- * $Id: Person.java,v 1.10 2008-06-05 06:05:37 dbarre Exp $
+ * $Id: Person.java,v 1.11 2008-06-19 03:28:14 lprzybyl Exp $
  */
 public class Person extends KraPersistableBusinessObjectBase {
     private String personId;
@@ -94,9 +94,9 @@ public class Person extends KraPersistableBusinessObjectBase {
     private Long kimPersonId = null;
     private KimPerson kimPerson = null;
     private Boolean active = true;
+    private Unit unit;
     
     public Person() {
-        super();
     }
 
     /**
@@ -1201,6 +1201,24 @@ public class Person extends KraPersistableBusinessObjectBase {
     
     public Boolean getActive() {
         return active;
+    }
+
+    /**
+     * Unit reference referred by {@link #getOwnedByUnit()}
+     *
+     * @param unit 
+     */
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    /**
+     * Unit reference referred by {@link #getOwnedByUnit()}
+     *
+     * @return unit 
+     */
+    public Unit getUnit() {
+        return unit;
     }
 
 	@Override 
