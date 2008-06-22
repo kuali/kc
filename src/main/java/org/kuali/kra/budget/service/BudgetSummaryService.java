fa1babf58a74512f8b39ee11f42089a785a87ed3
Lin-Long Shyu <lshyukuali@gmail.com>
2008-06-22 00:16:51 +0000

    kracoeus-1483

--- src/main/java/org/kuali/kra/budget/service/BudgetSummaryService.java
+++ src/main/java/org/kuali/kra/budget/service/BudgetSummaryService.java
@@ -73,5 +73,5 @@ public interface BudgetSummaryService {
      */
     public void setupOldStartEndDate (List <BudgetLineItem > budgetLineItems);
 
-
+    public List<Date> getNewStartEndDates(List<Date> startEndDates, int gap, int duration, Date prevDate);
 }
