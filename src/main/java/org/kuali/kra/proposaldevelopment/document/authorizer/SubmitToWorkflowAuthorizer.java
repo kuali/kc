41b7d1c704ad46ebb7773cbdd82321f71500458d
Don Barre <dbarre@arizona.edu>
2008-07-03 18:37:10 +0000

    KRACOEUS-1582: Added SUBMIT_PROPOSAL permission and SubmitToWorkflowAuthorizer.

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/document/authorizer/SubmitToWorkflowAuthorizer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2008 The Kuali Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.document.authorizer;
+
+import org.kuali.kra.infrastructure.PermissionConstants;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
+import org.kuali.kra.proposaldevelopment.document.authorization.ProposalTask;
+
+/**
+ * The Submit to Workflow Authorizer determines if the user can
+ * submit a proposal to workflow.  This is only allowed if the
+ * proposal is not already in workflow and the person has the
+ * necessary permission.
+ */
+public class SubmitToWorkflowAuthorizer extends ProposalAuthorizer {
+
+    /**
+     * @see org.kuali.kra.proposaldevelopment.document.authorizer.ProposalAuthorizer#isAuthorized(org.kuali.core.bo.user.UniversalUser, org.kuali.kra.proposaldevelopment.web.struts.form.ProposalDevelopmentForm)
+     */
+    public boolean isAuthorized(String username, ProposalTask task) {
+        ProposalDevelopmentDocument doc = task.getProposalDevelopmentDocument();
+        return !kraWorkflowService.isInWorkflow(doc) &&
+               hasProposalPermission(username, doc, PermissionConstants.SUBMIT_PROPOSAL);
+    }
+}
