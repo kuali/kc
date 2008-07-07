4b4cfaa236c26b59b4772b9c5791f28507e9bcd8
Lin-Long Shyu <lshyukuali@gmail.com>
2008-07-07 14:29:42 +0000

    KRACOEUS-1558

--- src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
+++ src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
@@ -216,7 +216,7 @@ public class KeyConstants {
     public static final String WARNING_EFFDT_AFTER_PERIOD_START_DATE = "warning.effdt.after.periodStartDate";
 
     public static final String ERROR_COST_SHARE_PERCENTAGE = "error.costSharing.percentage";
-    public static final String ERROR_PERSONNEL_ETAIL_DATES = "error.personnel.detail.dates";
+    public static final String ERROR_PERSONNEL_DETAIL_DATES = "error.personnel.detail.dates";
     public static final String ERROR_NO_FIELD_TO_EDIT = "error.alterproposaldata.nofieldtoedit";
 
 }
