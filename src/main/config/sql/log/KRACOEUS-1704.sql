61d71f12212865aebcd33c4136174469ffdf67f2
Lin-Long Shyu <lshyukuali@gmail.com>
2008-07-15 14:07:09 +0000

    KRACOEUS-1704

---
+++ src/main/config/sql/log/KRACOEUS-1704.sql
@@ -0,0 +1,3 @@
+alter table BUDGET_PERSONS modify person_id varchar2(10); 
+alter table BUDGET_PERSONNEL_DETAILS modify person_id varchar2(10);  
+alter table BUDGET_PER_DET_RATE_AND_BASE modify person_id varchar2(10);     
