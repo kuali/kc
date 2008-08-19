d7157050ab121c74425e689feafd4907941164ee
Tyler Wilson <tyler.wilson@colostate.edu>
2008-08-19 16:51:59 +0000

    adding script for JIRA KRACOEUS-1813

---
+++ src/main/config/sql/log/KRACOEUS-1813.sql
@@ -0,0 +1,3 @@
+INSERT
+INTO sh_parm_t(sh_parm_nmspc_cd,    sh_parm_dtl_typ_cd,    sh_parm_nm,    sh_parm_typ_cd,    sh_parm_txt,    sh_parm_desc,    sh_parm_cons_cd,    active_ind)
+VALUES('KRA-B',    'D',    'budgetPersonDefaultPeriodType',    'CONFG',    '3',    'Default Period Type',    'A',    'Y');
