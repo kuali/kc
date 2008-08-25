0de7ce6933260b4083b8847b99b9349e5ad4a49d
Tyler Wilson <tyler.wilson@colostate.edu>
2008-08-25 15:42:31 +0000

    Applying KRACOEUS-1817 sql script to 1.0.1 Patch Script

---
+++ src/main/config/sql/log/KRACOEUS-1817.sql
@@ -0,0 +1,3 @@
+INSERT
+INTO fp_doc_type_t(fdoc_typ_cd,    ver_nbr,    fdoc_grp_cd,    fdoc_nm,    fin_elim_elgbl_cd,    fdoc_typ_active_cd,    fdoc_rtng_rule_cd,    fdoc_autoaprv_days,    fdoc_balanced_cd,    trn_scrbbr_ofst_gen_ind)
+VALUES('POSS',    1,    'KR',    'POST SUBMISSION STATUS CODE',    'N',    'Y',    'N',    0,    'N',    'N');
