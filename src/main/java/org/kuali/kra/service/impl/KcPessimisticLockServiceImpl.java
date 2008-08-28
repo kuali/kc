a5dda2a84285e2528858f7de1ad5a1b8dc42bddf
Don Barre <dbarre@arizona.edu>
2008-08-28 15:33:18 +0000

    KRACOEUS-1530 : Add Quartz timer to delete old pessimistic locks from database

---
+++ src/main/java/org/kuali/kra/service/impl/KcPessimisticLockServiceImpl.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.service.impl;
+
+import java.util.Collection;
+
+import org.kuali.core.document.authorization.PessimisticLock;
+import org.kuali.core.service.BusinessObjectService;
+import org.kuali.core.service.DateTimeService;
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.KeyConstants;
+import org.kuali.kra.service.KcPessimisticLockService;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * KC Pessimistic Lock Service Implementation.
+ */
+public class KcPessimisticLockServiceImpl implements KcPessimisticLockService {
+    
+    /**
+     * Convert minutes to milliseconds.
+     */
+    private static final long MINUTES_TO_MILLISECONDS = 60L * 1000L;
+    
+    /**
+     * Default timeout period in minutes.
+     */
+    private static final int DEFAULT_TIMEOUT = 24 * 60; // one day in minutes
+    
+    private KualiConfigurationService configurationService;
+    private BusinessObjectService businessObjectService;
+    private DateTimeService dateTimeService;
+    
+    /**
+     * Set the Configuration Service.  Injected by Spring.
+     * @param configurationService the configuration service
+     */
+    public void setConfigurationService(KualiConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+    
+    /**
+     * Set the Date Time Service.  Injected by Spring.
+     * @param dateTimeService the date time service
+     */
+    public void setDateTimeService(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
+    
+    /**
+     * Set the Business Object Service.  Injected by Spring.
+     * @param businessObjectService the business object service
+     */
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+    
+    /**
+     * Retrieve all of the locks from the database.  Delete those that
+     * have been around for longer than the timeout period.
+     * @see org.kuali.kra.service.KcPessimisticLockService#clearExpiredLocks()
+     */
+    @Transactional
+    public void clearExpiredLocks() {
+        long now = getCurrentTime();
+        long timeoutMillis = getTimeoutMillis();
+        Collection<PessimisticLock> locks = getAllLocks();
+        for (PessimisticLock lock : locks) {
+            long lockTime = lock.getGeneratedTimestamp().getTime();
+            if (now - lockTime >= timeoutMillis) {
+                businessObjectService.delete(lock);
+            }
+        }
+    }
+    
+    /**
+     * Get all of the Pessimistic Locks from the database.
+     * @return all of the pessimistic locks
+     */
+    private Collection<PessimisticLock> getAllLocks() {
+        return businessObjectService.findAll(PessimisticLock.class);
+    }
+    
+    /**
+     * Get the current time in milliseconds.
+     * @return the current time in milliseconds
+     */
+    private long getCurrentTime() {
+        return dateTimeService.getCurrentTimestamp().getTime();
+    }
+    
+    /**
+     * Get the timeout period in milliseconds.
+     * @return the timeout period in milliseconds
+     */
+    private long getTimeoutMillis() {
+        return getLockTimeout() * MINUTES_TO_MILLISECONDS;
+    }
+    
+    /**
+     * Get the Lock Timeout parameter value from the system parameters.
+     * @return the Lock Tiemout value in minutes
+     */
+    private int getLockTimeout() {
+        try {
+            String timeoutStr = getParameterValue(KeyConstants.PESSIMISTIC_LOCKING_TIMEOUT);
+            return Integer.parseInt(timeoutStr);
+        } catch (Exception ex) {
+            return DEFAULT_TIMEOUT;
+        }
+    }
+
+    /**
+     * Get a proposal development system parameter value.
+     * @param key the key (name) of the parameter
+     * @return the parameter's value
+     */
+    private String getParameterValue(String key) {
+        return configurationService.getParameterValue(Constants.PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                                                      Constants.PARAMETER_COMPONENT_DOCUMENT, key);
+    }
+}
---
+++ src/test/java/org/kuali/kra/service/impl/KcPessimisticLockServiceImplTest.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.service.impl;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.kuali.core.document.authorization.PessimisticLock;
+import org.kuali.core.service.BusinessObjectService;
+import org.kuali.core.service.DateTimeService;
+import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.KeyConstants;
+
+@RunWith(JMock.class)
+public class KcPessimisticLockServiceImplTest {
+
+    private static final long ONE_DAY = 24L * 60L * 60L * 1000L;
+    
+    private Mockery context = new JUnit4Mockery();
+    
+    private List<PessimisticLock> locks;
+    private Timestamp cronJobTime;
+    
+    /**
+     * First, a cron job time must be established as the baseline.
+     * The cron job time is the time that the service is invoked
+     * to delete the expired locks.
+     * 
+     * We will test four pessimistic locks.  One is only a half
+     * day old, one is two days old, one millisecond less than a
+     * day old, and one millisecond greater than a day old.
+     */
+    @Before
+    public void setUp() {
+        long now = System.currentTimeMillis();
+        cronJobTime = new Timestamp(now);
+        locks = new ArrayList<PessimisticLock>();
+        locks.add(createPessimisticLock(now - (ONE_DAY / 2)));
+        locks.add(createPessimisticLock(now - (ONE_DAY * 2)));
+        locks.add(createPessimisticLock(now - (ONE_DAY - 1)));
+        locks.add(createPessimisticLock(now - (ONE_DAY + 1)));
+    }
+    
+    /**
+     * Create a Pessimistic Lock with the given timestamp.
+     * @param timestamp the creation timestamp of the lock
+     * @return the new Pessimistic Lock
+     */
+    private PessimisticLock createPessimisticLock(long timestamp) {
+        PessimisticLock lock = new PessimisticLock();
+        lock.setGeneratedTimestamp(new Timestamp(timestamp));
+        return lock;
+    }
+
+    /**
+     * From the Pessimistic Locks created in setUp(), only the second and fourth
+     * locks are supposed to be considered expired.  
+     */
+    @Test
+    public void testExpiredLocks() {
+        KcPessimisticLockServiceImpl pessimisticLockService = new KcPessimisticLockServiceImpl();
+        
+        /*
+         * The configuration service will be invoked once to get the Lock Timeout Period
+         * which for this test will be one day (1440 minutes).
+         */
+        final KualiConfigurationService configurationService = context.mock(KualiConfigurationService.class);
+        context.checking(new Expectations() {{
+            one(configurationService).getParameterValue(Constants.PARAMETER_MODULE_PROPOSAL_DEVELOPMENT, 
+                                                        Constants.PARAMETER_COMPONENT_DOCUMENT, 
+                                                        KeyConstants.PESSIMISTIC_LOCKING_TIMEOUT); 
+            will(returnValue("1440"));
+        }});
+        pessimisticLockService.setConfigurationService(configurationService);
+        
+        /*
+         * The date time service is invoked once to get the current time of day.
+         * We will use our cron job time.
+         */
+        final DateTimeService dateTimeService = context.mock(DateTimeService.class);
+        context.checking(new Expectations() {{
+            one(dateTimeService).getCurrentTimestamp(); will(returnValue(cronJobTime));
+        }});
+        pessimisticLockService.setDateTimeService(dateTimeService);
+        
+        /*
+         * The business object service is used to retrieve all of the locks and
+         * will then be used to delete the second and fourth only.
+         */
+        final BusinessObjectService businessObjectService = context.mock(BusinessObjectService.class);
+        context.checking(new Expectations() {{
+            one(businessObjectService).findAll(PessimisticLock.class); will(returnValue(locks));
+            one(businessObjectService).delete(locks.get(1));
+            one(businessObjectService).delete(locks.get(3));
+        }});
+        pessimisticLockService.setBusinessObjectService(businessObjectService);
+        
+        /*
+         * Let's clear the expired locks and see if things
+         * happen as we expect.
+         */
+        pessimisticLockService.clearExpiredLocks();
+    }
+}
