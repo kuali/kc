9acdcd99028bc96cf9476327a45f68bd5a240f13
Jeff Covey <jcovey@rsmart.com>
2008-09-24 19:01:23 +0000

    added logic to BudgetRatesAction.java that renders question page when clicking buttons sync rates, sync all rates, reset rates, reset all rates Budget Rates Tab.

--- src/main/java/org/kuali/kra/budget/web/struts/action/BudgetRatesAction.java
+++ src/main/java/org/kuali/kra/budget/web/struts/action/BudgetRatesAction.java
@@ -15,6 +15,8 @@
  */
 package org.kuali.kra.budget.web.struts.action;
 
+import static org.kuali.RiceConstants.QUESTION_INST_ATTRIBUTE_NAME;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -30,10 +32,16 @@ import org.kuali.kra.budget.bo.RateClassType;
 import org.kuali.kra.budget.document.BudgetDocument;
 import org.kuali.kra.budget.web.struts.form.BudgetForm;
 import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.KeyConstants;
+import org.kuali.kra.web.struts.action.StrutsConfirmation;
 
 public class BudgetRatesAction extends BudgetAction {
     private static final Log LOG = LogFactory.getLog(BudgetRatesAction.class);
 
+    private static final String CONFIRM_SYNC_RATES = "confirmSyncRates";
+    private static final String CONFIRM_SYNC_ALL_RATES = "confirmSyncAllRates";
+    private static final String CONFIRM_RESET_RATES = "confirmResetRates";
+    private static final String CONFIRM_RESET_ALL_RATES = "confirmResetAllRates";
 
     @Override
     public ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -56,6 +64,23 @@ public class BudgetRatesAction extends BudgetAction {
         return mapping.findForward("rates_save");
     }
     
+    /**
+     * 
+     */
+    public ActionForward resetRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        return confirm(buildResetRatesConfirmationQuestion(mapping, form, request, response), CONFIRM_RESET_RATES, "");
+    }
+    
+    /**
+     * 
+     */
+    
+    private StrutsConfirmation buildResetRatesConfirmationQuestion(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        return buildParameterizedConfirmationQuestion(mapping, form, request, response, CONFIRM_RESET_RATES, 
+                KeyConstants.QUESTION_RESET_RATES, "");
+    }
     
     /**
      * Action called to reset budget rates for each panel.
@@ -66,13 +91,35 @@ public class BudgetRatesAction extends BudgetAction {
      * @param response
      * @return ActionForward instance for forwarding to the tab.
      */
-    public ActionForward resetRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        BudgetForm budgetForm = (BudgetForm) form;
-        BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
-        RateClassType rateClassT = budgetDocument.getRateClassTypes().get(getSelectedLine(request));
-        String rateClassType = rateClassT.getRateClassType();
-        budgetDocument.getBudgetRatesService().resetBudgetRatesForRateClassType(rateClassType, budgetDocument);
+    public ActionForward confirmResetRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        
+        Object question = request.getParameter(QUESTION_INST_ATTRIBUTE_NAME);
+        if (CONFIRM_RESET_RATES.equals(question)) {
+            BudgetForm budgetForm = (BudgetForm) form;
+            BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
+            RateClassType rateClassT = budgetDocument.getRateClassTypes().get(getSelectedLine(request));
+            String rateClassType = rateClassT.getRateClassType();
+            budgetDocument.getBudgetRatesService().resetBudgetRatesForRateClassType(rateClassType, budgetDocument);
+        }
         return mapping.findForward(Constants.MAPPING_BASIC);
+    }
+    
+    /**
+     * 
+     */
+    public ActionForward syncRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        return confirm(buildSyncRatesConfirmationQuestion(mapping, form, request, response), CONFIRM_SYNC_RATES, "");
+    }
+    
+    /**
+     * 
+     */
+    private StrutsConfirmation buildSyncRatesConfirmationQuestion(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        return buildParameterizedConfirmationQuestion(mapping, form, request, response, CONFIRM_SYNC_RATES, 
+                KeyConstants.QUESTION_SYNC_RATES, "");
     }
 
     /**
@@ -84,17 +131,39 @@ public class BudgetRatesAction extends BudgetAction {
      * @param response
      * @return ActionForward instance for forwarding to the tab.
      */
-    public ActionForward syncRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        BudgetForm budgetForm = (BudgetForm) form;
-        BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
-        RateClassType rateClassT = budgetDocument.getRateClassTypes().get(getSelectedLine(request));
-        String rateClassType = rateClassT.getRateClassType();
-        budgetDocument.getBudgetRatesService().syncBudgetRatesForRateClassType(rateClassType, budgetDocument);
-        budgetDocument.setRateClassTypesReloaded(false);
-        if (rateClassType.equals("O")) {
-            budgetDocument.setRateSynced(true);
+    public ActionForward confirmSyncRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        
+        Object question = request.getParameter(QUESTION_INST_ATTRIBUTE_NAME);
+        if (CONFIRM_SYNC_RATES.equals(question)) {
+            BudgetForm budgetForm = (BudgetForm) form;
+            BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
+            RateClassType rateClassT = budgetDocument.getRateClassTypes().get(getSelectedLine(request));
+            String rateClassType = rateClassT.getRateClassType();
+            budgetDocument.getBudgetRatesService().syncBudgetRatesForRateClassType(rateClassType, budgetDocument);
+            budgetDocument.setRateClassTypesReloaded(false);
+            if (rateClassType.equals("O")) {
+                budgetDocument.setRateSynced(true);
+            }
         }
         return mapping.findForward(Constants.MAPPING_BASIC);
+    }
+    
+    /**
+     * 
+     */
+    public ActionForward syncAllRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        return confirm(buildSyncAllRatesConfirmationQuestion(mapping, form, request, response), CONFIRM_SYNC_ALL_RATES, "");
+    }
+    
+    /**
+     * 
+     */
+     private StrutsConfirmation buildSyncAllRatesConfirmationQuestion(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+             HttpServletResponse response) throws Exception {
+        return buildParameterizedConfirmationQuestion(mapping, form, request, response, CONFIRM_SYNC_ALL_RATES, 
+                KeyConstants.QUESTION_SYNC_ALL_RATES, "");
     }
 
     /**
@@ -106,18 +175,40 @@ public class BudgetRatesAction extends BudgetAction {
      * @param response
      * @return ActionForward instance for forwarding to the tab.
      */
-    public ActionForward syncAllRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        BudgetForm budgetForm = (BudgetForm) form;
-        BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
-        budgetDocument.getBudgetRatesService().syncAllBudgetRates(budgetDocument);
-        budgetDocument.setRateClassTypesReloaded(false);
-        budgetDocument.setRateSynced(true);
-        if (!budgetDocument.getActivityTypeCode().equals(budgetDocument.getProposal().getActivityTypeCode())) {
-            budgetDocument.setActivityTypeCode(budgetDocument.getProposal().getActivityTypeCode());
+    public ActionForward confirmSyncAllRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        
+        Object question = request.getParameter(QUESTION_INST_ATTRIBUTE_NAME);
+        if (CONFIRM_SYNC_ALL_RATES.equals(question)) {
+            BudgetForm budgetForm = (BudgetForm) form;
+            BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
+            budgetDocument.getBudgetRatesService().syncAllBudgetRates(budgetDocument);
+            budgetDocument.setRateClassTypesReloaded(false);
+            budgetDocument.setRateSynced(true);
+            if (!budgetDocument.getActivityTypeCode().equals(budgetDocument.getProposal().getActivityTypeCode())) {
+                budgetDocument.setActivityTypeCode(budgetDocument.getProposal().getActivityTypeCode());
+            }
         }
         return mapping.findForward(Constants.MAPPING_BASIC);
     }
-
+    
+    /**
+     * 
+     */
+    public ActionForward resetAllRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        return confirm(buildResetAllRatesConfirmationQuestion(mapping, form, request, response), CONFIRM_RESET_ALL_RATES, "");
+    }
+    
+    /**
+     * 
+     */
+    private StrutsConfirmation buildResetAllRatesConfirmationQuestion(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        return buildParameterizedConfirmationQuestion(mapping, form, request, response, 
+                CONFIRM_RESET_ALL_RATES, KeyConstants.QUESTION_RESET_ALL_RATES, "");
+    }
+    
     /**
      * Action called to reset all budget rates.
      * 
@@ -127,10 +218,15 @@ public class BudgetRatesAction extends BudgetAction {
      * @param response
      * @return ActionForward instance for forwarding to the tab.
      */
-    public ActionForward resetAllRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        BudgetForm budgetForm = (BudgetForm) form;
-        BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
-        budgetDocument.getBudgetRatesService().resetAllBudgetRates(budgetDocument);
+    public ActionForward confirmResetAllRates(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
+        
+        Object question = request.getParameter(QUESTION_INST_ATTRIBUTE_NAME);
+        if (CONFIRM_RESET_ALL_RATES.equals(question)) {
+            BudgetForm budgetForm = (BudgetForm) form;
+            BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
+            budgetDocument.getBudgetRatesService().resetAllBudgetRates(budgetDocument);
+        }
         return mapping.findForward(Constants.MAPPING_BASIC);
     }
 
@@ -143,7 +239,8 @@ public class BudgetRatesAction extends BudgetAction {
      * @param response
      * @return ActionForward instance for forwarding to the tab.
      */
-    public ActionForward updateRatesView(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+    public ActionForward updateRatesView(ActionMapping mapping, ActionForm form, HttpServletRequest request, 
+            HttpServletResponse response) throws Exception {
         BudgetForm budgetForm = (BudgetForm) form;
         BudgetDocument budgetDocument = budgetForm.getBudgetDocument();
         budgetDocument.getBudgetRatesService().viewLocation(budgetForm.getViewLocation(),budgetForm.getViewBudgetPeriod(), budgetDocument);
