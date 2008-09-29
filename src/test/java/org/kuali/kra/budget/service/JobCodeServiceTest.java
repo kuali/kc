d9b4fd40e810e45bf40d893bded88505d4184880
Chris Denne <chris.denne@colostate.edu>
2008-09-29 22:37:42 +0000

    KRACOEUS-1750: pt. 2

---
+++ src/test/java/org/kuali/kra/budget/service/JobCodeServiceTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.budget.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.core.UserSession;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.kra.KraTestBase;
+
+public class JobCodeServiceTest extends KraTestBase {
+    
+    JobCodeService jobCodeService;
+    
+    private static final String CEO_CODE = "AA001";
+    
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        GlobalVariables.setUserSession(new UserSession("quickstart"));
+        jobCodeService = getService(JobCodeService.class);
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        GlobalVariables.setUserSession(null);
+        jobCodeService = null;
+        super.tearDown();
+    }
+    
+    /**
+     * Test testGetJobTitle() method
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testGetJobTitle() throws Exception {
+        
+        String  title = jobCodeService.findJobCodeTitle(CEO_CODE);
+        
+        log.debug("JobTitle for jobCode("+CEO_CODE+" is ["+title+"]");
+        // Verify that status is final
+        //assertEquals(RiceConstants.DocumentStatusCodes.APPROVED, budgetDocument.getDocumentHeader().getFinancialDocumentStatusCode());
+    }
+
+}
