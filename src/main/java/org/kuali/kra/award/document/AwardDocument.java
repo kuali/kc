de4402d787e926f5c36eb698e6d9bebebbc9025a
Varun Soni <vls45@cornell.edu>
2008-10-22 18:46:16 +0000

    KCAWD-11 - Award Skeleton - Award BO

--- src/main/java/org/kuali/kra/award/document/AwardDocument.java
+++ src/main/java/org/kuali/kra/award/document/AwardDocument.java
@@ -15,35 +15,23 @@
  */
 package org.kuali.kra.award.document;
 
-import java.util.ArrayList;
-
-import org.kuali.core.web.format.Formatter;
-import org.kuali.core.web.ui.KeyLabelPair;
-import org.kuali.kra.bo.InstituteLaRate;
-import org.kuali.kra.bo.InstituteRate;
-import org.kuali.kra.budget.BudgetDecimal;
-import org.kuali.kra.budget.RateDecimal;
-import org.kuali.kra.budget.bo.BudgetCostShare;
-import org.kuali.kra.budget.bo.BudgetPeriod;
-import org.kuali.kra.budget.bo.BudgetPerson;
-import org.kuali.kra.budget.bo.BudgetPersonnelDetails;
-import org.kuali.kra.budget.bo.BudgetPrintForm;
-import org.kuali.kra.budget.bo.BudgetProjectIncome;
-import org.kuali.kra.budget.bo.BudgetProposalLaRate;
-import org.kuali.kra.budget.bo.BudgetProposalRate;
-import org.kuali.kra.budget.bo.BudgetSubAwards;
-import org.kuali.kra.budget.bo.BudgetUnrecoveredFandA;
-import org.kuali.kra.budget.bo.RateClass;
-import org.kuali.kra.budget.bo.RateClassType;
+import org.kuali.kra.award.bo.Award;
 import org.kuali.kra.document.ResearchDocumentBase;
-import org.kuali.kra.infrastructure.BudgetDecimalFormatter;
-import org.kuali.kra.infrastructure.RateDecimalFormatter;
 
 public class AwardDocument extends ResearchDocumentBase {
     private static final String DETAIL_TYPE_CODE = "D";
     private static final String AWARD_NAMESPACE_CODE = "KRA-A";
+    private Award award;
     
-    public AwardDocument(){
-        super();        
+    public AwardDocument(){        
+        super();
+    }
+
+    public Award getAward() {
+        return award;
+    }
+
+    public void setAward(Award award) {
+        this.award = award;
     }
 }
