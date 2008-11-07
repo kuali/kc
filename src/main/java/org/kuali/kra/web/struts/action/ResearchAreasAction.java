91835173133968f38953e46cc35b5f389ecaf162
Lin-Long Shyu <lshyukuali@gmail.com>
2008-11-07 21:35:29 +0000

    KCIRB-17 : research areas hierarchy maintenance - initial commit

---
+++ src/main/java/org/kuali/kra/web/struts/action/ResearchAreasAction.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.web.struts.action;
+
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.core.bo.PersistableBusinessObject;
+import org.kuali.core.lookup.LookupResultsService;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.core.web.struts.action.KualiAction;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.web.struts.form.ResearchAreasForm;
+
+public class ResearchAreasAction extends KualiAction {
+    
+    @Override
+    public ActionForward refresh(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        super.refresh(mapping, form, request, response);
+        ResearchAreasForm researchAreasForm = (ResearchAreasForm) form;
+        
+            return mapping.findForward("lookup");            
+    }
+
+
+}
