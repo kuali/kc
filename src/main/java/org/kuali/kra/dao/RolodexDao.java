5565217434cb972d2729adcedf3400069fb49fe0
Don Barre <dbarre@arizona.edu>
2008-11-13 16:28:40 +0000

    KRACOEUS-2085 : Fixed Rolodex lookup cast exception

--- src/main/java/org/kuali/kra/dao/RolodexDao.java
+++ src/main/java/org/kuali/kra/dao/RolodexDao.java
@@ -52,17 +52,4 @@ public interface RolodexDao {
      * @return Criteria
      */
     public Criteria getNonOrganizationalRolodexCriteria(Class businessObjectClass, Map fieldValues, boolean usePrimaryKeys);
-
-
-    public LookupDao getLookupDao();
-    
-    /**
-     * Convenience method for casting <code>{@link LookupDao}</code> to <code>{@link LookupDaoOjb}</code> because some methods in 
-     * <code>{@link LookupDaoOjb}</code> are public but not in <code>{@link LookupDao}</code>. This is a hack until the two are sync'd up.
-     * 
-     * @return LookupDaoOjb instance
-     */
-    public LookupDaoOjb getLookupDaoOjb();
-
-    public void setLookupDao(LookupDao lookupDao);
 }
