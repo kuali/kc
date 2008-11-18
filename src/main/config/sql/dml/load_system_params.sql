fed0e772942ce6888c24a6db83a15ac8aea50ffc
Kiltesh Patel <kjpatel@ucdavis.edu>
2008-11-18 18:46:58 +0000

    KCIRB-16 Reference ID1 & Reference ID2: the labels are configurable at implementation time.

--- src/main/config/sql/dml/load_system_params.sql
+++ src/main/config/sql/dml/load_system_params.sql
@@ -692,3 +692,23 @@ values ('KRA-PD','D','pessimisticLocking.expirationAge','CONFG','1440','The expi
 INSERT
 INTO sh_parm_t(sh_parm_nmspc_cd,    sh_parm_dtl_typ_cd,    sh_parm_nm,    sh_parm_typ_cd,    sh_parm_txt,    sh_parm_desc,    sh_parm_cons_cd,    wrkgrp_nm,    active_ind)
 VALUES('KRA-PD',    'A',    'numberPerSponsorHierarchyGroup',   'CONFG',    '300',    'Number of nodes per sponsor group',    'A',    'WorkflowAdmin',    'Y');
+
+Insert into SH_PARM_NMSPC_T
+( SH_PARM_NMSPC_CD, OBJ_ID, VER_NBR, SH_PARM_NMSPC_NM, ACTIVE_IND)
+Values 
+('KC-PROTOCOL', '40B51A08D06DA044E043814FD881A041', 1, 'KC IRB Protocol', 'Y');
+
+Insert into SH_PARM_DTL_TYP_T 
+( SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, OBJ_ID, VER_NBR, SH_PARM_DTL_TYP_NM, ACTIVE_IND)
+Values 
+('KC-PROTOCOL','D','1EA4E50A05844D36964A0FCBB2992881',1,'Document','Y');
+
+Insert into SH_PARM_T 
+( SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM, OBJ_ID, VER_NBR, SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND)
+Values 
+('KC-PROTOCOL','D','irb.protocol.referenceID1','F1C228F9D4D8408A8E0BBC801C9525ab',1,'CONFG','Reference ID1','Referece id is configurable at impl time','A','WorkflowAdmin','Y');
+
+Insert into SH_PARM_T 
+( SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM, OBJ_ID, VER_NBR, SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND)
+Values
+('KC-PROTOCOL','D','irb.protocol.referenceID2','F1C228F9D4D8408A8E0BBC801C9525az',1,'CONFG','Reference ID2','Referece id is configurable at impl time','A','WorkflowAdmin','Y');
