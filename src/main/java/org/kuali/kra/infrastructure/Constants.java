ea383afc0900bbfdcdbe493148c79af3cf31e151
Kiltesh Patel <kjpatel@ucdavis.edu>
2008-11-18 18:46:58 +0000

    KCIRB-16 Reference ID1 & Reference ID2: the labels are configurable at implementation time.

--- src/main/java/org/kuali/kra/infrastructure/Constants.java
+++ src/main/java/org/kuali/kra/infrastructure/Constants.java
@@ -69,6 +69,9 @@ public interface Constants {
     public static final String SPONSOR_LEVEL_HIERARCHY = "sponsorLevelHierarchy";
     public static final String SPONSOR_HIERARCHY_NAME = "sponsorGroupHierarchyName";
     
+    public static final String PARAMETER_MODULE_PROTOCOL = "KC-PROTOCOL";
+    public static final String PARAMETER_MODULE_PROTOCOL_REFERENCEID1 = "irb.protocol.referenceID1";
+    public static final String PARAMETER_MODULE_PROTOCOL_REFERENCEID2 = "irb.protocol.referenceID2";
     public static final String PARAMETER_MODULE_PROPOSAL_DEVELOPMENT = "KRA-PD";
     public static final String PARAMETER_COMPONENT_DOCUMENT = "D";
     public static final String INSTITUTE_NARRATIVE_TYPE_GROUP_CODE = "O";
