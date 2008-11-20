ea478975dab568c717e8a042e5e521890f452e42
Varun Soni <vls45@cornell.edu>
2008-11-20 19:28:20 +0000

    KCAWD-20 - DB Script for system parameter update

--- src/main/config/sql/dml/load_system_params.sql
+++ src/main/config/sql/dml/load_system_params.sql
@@ -712,3 +712,12 @@ Insert into SH_PARM_T
 ( SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM, OBJ_ID, VER_NBR, SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND)
 Values
 ('KC-PROTOCOL','D','irb.protocol.referenceID2','F1C228F9D4D8408A8E0BBC801C9525az',1,'CONFG','Reference ID2','Referece id is configurable at impl time','A','WorkflowAdmin','Y');
+
+INSERT INTO sh_parm_nmspc_t
+(SH_PARM_NMSPC_CD,SH_PARM_NMSPC_NM,ACTIVE_IND, OBJ_ID, VER_NBR)
+values('KC-AWARD', 'Award','Y',sys_guid(),'1');
+
+INSERT into SH_PARM_T 
+( SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM,  SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND, OBJ_ID, VER_NBR)
+Values 
+('KC-AWARD','D','mit.idc.validation.enabled','CONFG','1','MitIdcValidationEnabled is configurable at impl time','A','WorkflowAdmin','Y',sys_guid(),'1');
