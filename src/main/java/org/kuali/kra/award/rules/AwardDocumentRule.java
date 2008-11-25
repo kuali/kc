008fffe367443a864a1bd03ccca127411f327475
Varun Soni <vls45@cornell.edu>
2008-11-25 15:18:38 +0000

    KCAWD-20 - Added new JUnit Tests; re-factored the code to accommodate unit tests and some checkstyle errors

--- src/main/java/org/kuali/kra/award/rules/AwardDocumentRule.java
+++ src/main/java/org/kuali/kra/award/rules/AwardDocumentRule.java
@@ -16,6 +16,7 @@
 package org.kuali.kra.award.rules;
 
 import java.util.HashMap;
+import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.core.document.Document;
@@ -32,20 +33,30 @@ import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.rules.ResearchDocumentRuleBase;
 
 /**
- * Main Business Rule class for <code>{@link AwardDocument}</code>. Responsible for delegating rules to independent rule classes.
+ * Main Business Rule class for <code>{@link AwardDocument}</code>. 
+ * Responsible for delegating rules to independent rule classes.
  *
- * @author Kuali Nervous System Team (kualidev@oncourse.iu.edu)
  */
 public class AwardDocumentRule extends ResearchDocumentRuleBase implements AddIndirectCostRateRule {
     
     public static final String DOCUMENT_ERROR_PATH = "document";
     public static final String AWARD_ERROR_PATH = "award";
     
+    /**
+     * 
+     * @see org.kuali.core.rules.DocumentRuleBase#processCustomRouteDocumentBusinessRules(
+     * org.kuali.core.document.Document)
+     */
     @Override
     protected boolean processCustomRouteDocumentBusinessRules(Document document) {
         return super.processCustomRouteDocumentBusinessRules(document);
     }
 
+    /**
+     * 
+     * @see org.kuali.core.rules.DocumentRuleBase#processCustomSaveDocumentBusinessRules(
+     * org.kuali.core.document.Document)
+     */
     @Override
     protected boolean processCustomSaveDocumentBusinessRules(Document document) {
         boolean retval = true;
@@ -60,7 +71,8 @@ public class AwardDocumentRule extends ResearchDocumentRuleBase implements AddIn
 
 
     /**
-     * @see org.kuali.core.rule.DocumentAuditRule#processRunAuditBusinessRules(org.kuali.core.document.Document)
+     * @see org.kuali.core.rule.DocumentAuditRule#processRunAuditBusinessRules(
+     * org.kuali.core.document.Document)
      */
     public boolean processRunAuditBusinessRules(Document document){
         return super.processRunAuditBusinessRules(document);
@@ -68,7 +80,9 @@ public class AwardDocumentRule extends ResearchDocumentRuleBase implements AddIn
     
     /**
      * 
-     * @see org.kuali.kra.award.rule.AddIndirectCostRateRule#processAddIndirectCostRatesBusinessRules(org.kuali.kra.award.rule.event.AddAwardIndirectCostRateEvent)
+     * @see org.kuali.kra.award.rule.AddIndirectCostRateRule#
+     * processAddIndirectCostRatesBusinessRules(
+     * org.kuali.kra.award.rule.event.AddAwardIndirectCostRateEvent)
      */
     public boolean processAddIndirectCostRatesBusinessRules(AddAwardIndirectCostRateEvent 
             addAwardIndirectCostRateEvent) {        
@@ -76,23 +90,44 @@ public class AwardDocumentRule extends ResearchDocumentRuleBase implements AddIn
                 addAwardIndirectCostRateEvent);            
     }    
     
+    /**
+     * 
+     * This method evaluates the business rules for <code>AwardIndirectCostRate</code>
+     * business object.
+     * @param document
+     * @return
+     */
     protected boolean processAwardIndirectCostRateBusinessRules(Document document) {
         boolean retval = true;
-        AwardDocument awardDocument = (AwardDocument)document;
+        AwardDocument awardDocument = (AwardDocument) document;
         if(StringUtils.equalsIgnoreCase(
                 getKualiConfigurationService().getParameter(Constants.PARAMETER_MODULE_AWARD, 
                         Constants.PARAMETER_COMPONENT_DOCUMENT,
-                        KeyConstants.MIT_IDC_VALIDATION_ENABLED).getParameterValue(),KeyConstants.MIT_IDC_VALIDATION_ENABLED_VALUE_FOR_COMPARISON)){
-            retval = isIndirectCostRateInputInPairs(awardDocument);
+                        KeyConstants.MIT_IDC_VALIDATION_ENABLED).getParameterValue(),
+                        KeyConstants.MIT_IDC_VALIDATION_ENABLED_VALUE_FOR_COMPARISON)){
+            retval = isIndirectCostRateInputInPairs(awardDocument.getAward().getAwardIndirectCostRate());
         }        
         return retval;
     }
     
+    /**
+     * 
+     * @see org.kuali.core.rules.DocumentRuleBase#getKualiConfigurationService()
+     */
     protected KualiConfigurationService getKualiConfigurationService(){
         return KraServiceLocator.getService(KualiConfigurationService.class);
     }
     
-    protected boolean isIndirectCostRateInputInPairs(AwardDocument awardDocument){
+    /**
+     * 
+     * This method takes as the input a list of <code>AwardIndirectCostRate</code>,
+     * iterates through it twice to find out whether both on campus and off campus entries
+     * are present for every indirectRateTypeCode. 
+     * Returns true if they both are present.
+     * @param awardIndirectCostRateList
+     * @return
+     */
+    protected boolean isIndirectCostRateInputInPairs(List<AwardIndirectCostRate> awardIndirectCostRateList){
         HashMap<Integer,String> a1 = new HashMap<Integer,String>();
         HashMap<Integer,String> b1 = new HashMap<Integer,String>();        
         ErrorMap errorMap = GlobalVariables.getErrorMap();
@@ -101,18 +136,20 @@ public class AwardDocumentRule extends ResearchDocumentRuleBase implements AddIn
         errorMap.addToErrorPath(DOCUMENT_ERROR_PATH);
         errorMap.addToErrorPath(AWARD_ERROR_PATH);
         
-        for(AwardIndirectCostRate awardIndirectCostRate:awardDocument.getAward().getAwardIndirectCostRate()){
+        for(AwardIndirectCostRate awardIndirectCostRate : awardIndirectCostRateList){
             if(StringUtils.equalsIgnoreCase(awardIndirectCostRate.getOnCampusFlag(),"N")){
                 a1.put(awardIndirectCostRate.getIdcRateTypeCode(), awardIndirectCostRate.getOnCampusFlag());
             }else if(StringUtils.equalsIgnoreCase(awardIndirectCostRate.getOnCampusFlag(),"F")){
                 b1.put(awardIndirectCostRate.getIdcRateTypeCode(), awardIndirectCostRate.getOnCampusFlag());
             }
-        }       
-        
-        for(AwardIndirectCostRate awardIndirectCostRate:awardDocument.getAward().getAwardIndirectCostRate()){            
-            if((a1.containsKey(awardIndirectCostRate.getIdcRateTypeCode()) && !b1.containsKey(awardIndirectCostRate.getIdcRateTypeCode()))
-                    ||(b1.containsKey(awardIndirectCostRate.getIdcRateTypeCode()) && !a1.containsKey(awardIndirectCostRate.getIdcRateTypeCode()))){                
-                errorMap.putError("awardIndirectCostRate[" + i + "].idcRateTypeCode", KeyConstants.INDIRECT_COST_RATE_NOT_IN_PAIR);
+        }
+        for(AwardIndirectCostRate awardIndirectCostRate : awardIndirectCostRateList){            
+            if((a1.containsKey(awardIndirectCostRate.getIdcRateTypeCode()) 
+                    && !b1.containsKey(awardIndirectCostRate.getIdcRateTypeCode()))
+                    ||(b1.containsKey(awardIndirectCostRate.getIdcRateTypeCode()) 
+                            && !a1.containsKey(awardIndirectCostRate.getIdcRateTypeCode()))){                
+                errorMap.putError("awardIndirectCostRate[" + i + "].idcRateTypeCode"
+                        , KeyConstants.INDIRECT_COST_RATE_NOT_IN_PAIR);
                 return false;
             }
             i++;
