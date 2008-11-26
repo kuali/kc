b5dc94f58ab4d45d2dd9c456d19668e43c1972ef
Claus Niesen <cniesen@iastate.edu>
2008-11-26 19:26:40 +0000

    KCIRB-27:  Created Participant Type maintenance document.

---
+++ src/main/java/org/kuali/kra/irb/bo/ParticipantType.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kuali.kra.irb.bo;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+import java.util.LinkedHashMap;
+import java.sql.Date;
+
+public class ParticipantType extends KraPersistableBusinessObjectBase { 
+	
+	private Integer participantTypeCode; 
+	private String description; 
+	
+	public ParticipantType() { 
+
+	} 
+	
+	public Integer getParticipantTypeCode() {
+		return participantTypeCode;
+	}
+
+	public void setParticipantTypeCode(Integer participantTypeCode) {
+		this.participantTypeCode = participantTypeCode;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override 
+	protected LinkedHashMap toStringMapper() {
+		LinkedHashMap hashMap = new LinkedHashMap();
+		hashMap.put("participantTypeCode", getParticipantTypeCode());
+		hashMap.put("description", getDescription());
+		return hashMap;
+	}
+	
+}
