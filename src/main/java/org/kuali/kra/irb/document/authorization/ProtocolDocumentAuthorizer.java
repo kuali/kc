b930a8fd0b3914c7d42d894d3154f22296b586af
Kiltesh Patel <kjpatel@ucdavis.edu>
2008-12-03 22:02:26 +0000

    ResearchAreas Look up in Additional Information Sub panel of Protocol KCIRB-33

--- src/main/java/org/kuali/kra/irb/document/authorization/ProtocolDocumentAuthorizer.java
+++ src/main/java/org/kuali/kra/irb/document/authorization/ProtocolDocumentAuthorizer.java
@@ -15,12 +15,30 @@
  */
 package org.kuali.kra.irb.document.authorization;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kuali.core.authorization.AuthorizationConstants;
+import org.kuali.core.bo.user.UniversalUser;
+import org.kuali.core.document.Document;
 import org.kuali.core.document.authorization.TransactionalDocumentAuthorizerBase;
 
-/**
- * 
- * This class represents the Protocol Document Authorizer.
- */
+
 public class ProtocolDocumentAuthorizer extends TransactionalDocumentAuthorizerBase {
+    private static final String TRUE = "TRUE";
+    private static final String FALSE = "FALSE";
     
+    /**
+     * Used for permissions
+     * @see org.kuali.core.document.authorization.DocumentAuthorizerBase#getEditMode(org.kuali.core.document.Document, org.kuali.core.bo.user.UniversalUser)
+     */
+    @SuppressWarnings("unchecked")
+    public Map getEditMode(Document doc, UniversalUser user) {
+                 
+        Map editModeMap = new HashMap();
+        editModeMap.put(AuthorizationConstants.EditMode.FULL_ENTRY, TRUE);
+        editModeMap.put("modifyProtocol", editModeMap.containsKey(AuthorizationConstants.EditMode.FULL_ENTRY) ? TRUE : FALSE);
+    
+        return editModeMap;
+    }
 }
