f62eb72ad7aa8f030059635954246261d787bef5
Jeff Covey <jcovey@rsmart.com>
2008-12-12 20:23:58 +0000

    KCAWD-18

---
+++ src/main/java/org/kuali/kra/bo/CostShareType.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.bo;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+public class CostShareType extends KraPersistableBusinessObjectBase {
+    
+    private Integer costShareTypeCode;
+    private String description;
+
+    public Integer getCostShareTypeCode() {
+        return costShareTypeCode;
+    }
+
+    public void setCostShareTypeCode(Integer costShareTypeCode) {
+        this.costShareTypeCode = costShareTypeCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @Override
+    protected LinkedHashMap<String,Object> toStringMapper() {        
+        LinkedHashMap<String,Object> hashMap = new LinkedHashMap<String,Object>();
+        hashMap.put("costShareTypeCode", getCostShareTypeCode());
+        hashMap.put("description", getDescription());
+        return hashMap;
+    }
+
+
+}
+
