895aca99bc4d57ae3b3829e7d1ce1354b6fec89e
Kiltesh Patel <kjpatel@ucdavis.edu>
2008-12-18 18:10:08 +0000

    Jira-16
    Billable feature /w authorizer

--- src/main/java/org/kuali/kra/irb/web/struts/form/ProtocolHelper.java
+++ src/main/java/org/kuali/kra/irb/web/struts/form/ProtocolHelper.java
@@ -17,19 +17,35 @@ package org.kuali.kra.irb.web.struts.form;
 
 import static org.kuali.kra.infrastructure.KraServiceLocator.getService;
 
+import org.kuali.core.bo.user.UniversalUser;
 import org.kuali.core.service.KualiConfigurationService;
+import org.kuali.core.util.GlobalVariables;
 import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.infrastructure.TaskName;
+import org.kuali.kra.irb.document.authorization.ProtocolTask;
+import org.kuali.kra.service.TaskAuthorizationService;
 
 public class ProtocolHelper {
     
     private String referenceId1Label;
     private String referenceId2Label;
+
+    private ProtocolForm form;
+    private boolean billableReadOnly = false;
+
+    public ProtocolHelper(ProtocolForm form) {
+        this.form = form;
+    }    
     
     public void prepareView() {
         // setup any Protocol System Parameters that will be needed
         KualiConfigurationService configService = getService(KualiConfigurationService.class);
         setReferenceId1Label((configService.getParameter(Constants.PARAMETER_MODULE_PROTOCOL, Constants.PARAMETER_COMPONENT_DOCUMENT, Constants.PARAMETER_MODULE_PROTOCOL_REFERENCEID1)).getParameterValue());
         setReferenceId2Label((configService.getParameter(Constants.PARAMETER_MODULE_PROTOCOL, Constants.PARAMETER_COMPONENT_DOCUMENT, Constants.PARAMETER_MODULE_PROTOCOL_REFERENCEID2)).getParameterValue());
+        
+        ProtocolTask task = new ProtocolTask(TaskName.MODIFY_PROTOCOL_BILLABLE, form.getProtocolDocument().getProtocol());
+        billableReadOnly = !getTaskAuthorizationService().isAuthorized(getUsername(), task);
     }
 
     public void setReferenceId1Label(String referenceId1Label) {
@@ -48,4 +64,17 @@ public class ProtocolHelper {
         return referenceId2Label;
     }
 
+    public boolean getBillableReadOnly() {
+        return billableReadOnly;
+    }
+
+    private TaskAuthorizationService getTaskAuthorizationService() {
+        return KraServiceLocator.getService(TaskAuthorizationService.class);
+    }
+
+    private String getUsername() {
+         UniversalUser user = GlobalVariables.getUserSession().getUniversalUser();
+         return user.getPersonUserIdentifier();
+    }
+    
 }
