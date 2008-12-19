e49a4ecdf27f9d7d4b69c990e81192c40ecf2689
Rajeev Mancheril <rajeev@genesistec.com>
2008-12-19 22:00:24 +0000

    KCIRB-37 protocol location tab

---
+++ src/main/java/org/kuali/kra/irb/service/impl/ProtocolLocationServiceImpl.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.service.impl;
+
+import org.kuali.kra.bo.Rolodex;
+import org.kuali.kra.irb.bo.Protocol;
+import org.kuali.kra.irb.bo.ProtocolLocation;
+import org.kuali.kra.irb.service.ProtocolLocationService;
+
+
+public class ProtocolLocationServiceImpl implements ProtocolLocationService {
+    
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ProtocolLocationServiceImpl.class);
+    
+    /**
+     * @see org.kuali.kra.irb.service.ProtocolReferenceService#addProtocolReference(org.kuali.kra.irb.document.ProtocolDocument, org.kuali.kra.irb.bo.ProtocolReference)
+     */
+    public void addProtocolLocation(Protocol protocol, ProtocolLocation protocolLocation) {
+        
+        //TODO Framework problem of 2 saves
+        protocolLocation.setProtocolNumber("0");
+        protocolLocation.setSequenceNumber(0);
+        protocolLocation.refreshReferenceObject("protocolOrganizationType");
+        protocolLocation.refreshReferenceObject("rolodex");
+        protocol.getProtocolLocations().add(protocolLocation);
+    }
+
+    /**
+     * @see org.kuali.kra.irb.service.ProtocolReferenceService#deleteProtocolReference(org.kuali.kra.irb.document.ProtocolDocument, java.lang.Integer)
+     */
+    public void deleteProtocolLocation(Protocol protocol, int lineNumber) {
+
+        protocol.getProtocolLocations().remove(lineNumber);  
+
+    }
+   
+    /**
+     * @see org.kuali.kra.irb.service.ProtocolLocationService#clearProtocolLocation(org.kuali.kra.irb.bo.Protocol, int)
+     */
+    public void clearProtocolLocationAddress(Protocol protocol, int lineNumber) {
+
+        protocol.getProtocolLocations().get(lineNumber).setRolodexId(new Integer(0));  
+        protocol.getProtocolLocations().get(lineNumber).setRolodex(new Rolodex());  
+
+    }
+
+}
