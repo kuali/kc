01fae0325d8340687e43ed439efee8ce4086e148
Kiltesh Patel <kjpatel@ucdavis.edu>
2008-12-20 00:06:59 +0000

    Jira-16
    Billable test cases added for authz

--- src/test/java/org/kuali/kra/irb/web/ProtocolAdditionalFieldsWebTest.java
+++ src/test/java/org/kuali/kra/irb/web/ProtocolAdditionalFieldsWebTest.java
@@ -18,10 +18,13 @@ package org.kuali.kra.irb.web;
 
 
 import org.junit.Test;
+import org.kuali.core.UserSession;
+import org.kuali.core.util.GlobalVariables;
 import org.kuali.rice.test.data.PerSuiteUnitTestData;
 import org.kuali.rice.test.data.UnitTestData;
 import org.kuali.rice.test.data.UnitTestFile;
 
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
 
@@ -34,6 +37,10 @@ import com.gargoylesoftware.htmlunit.html.HtmlTable;
 public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
     
     private static org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ProtocolAdditionalFieldsWebTest.class);
+    
+    protected static final String PROTOCOL_DOCUMENT_NAME = "Kuali :: Protocol Document";
+    protected static final String NEW_PROTOCOL = "Create Protocol";
+    protected static final String KUALI_PORTAL_INDEX = "Kuali Portal Index";
     
     //Test field for Additional Sub Panel
     protected static final String PROTOCOL_ISBILLABLE_ID =  "document.protocol.billable";
@@ -52,7 +59,11 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
     protected static final String PROTOCOL_DESCRIPTION =  "keyword_to_test1";
     protected static final String PROTOCOL_DESCRIPTION2 = "test should be done based on feature";
     
+    protected static final String PROTOCOL_DESCRIPTION_TEXTAREA = "methodToCall.kraUpdateTextArea.((#document.protocol.description:protocolProtocol:Summary/Keywords#))";
+    
     private static final String FIRST_ROW_DATA = "01.0508:TaxidermyTaxidermist ";
+    
+    private static final String  PROTOCOL_RESEARCHAREA_DELETEMETHOD = "methodToCall.deleteProtocolResearchArea.line0.anchor5";
     
     private static final String PROTOCOL_REFERENCE_PROTOCOLREFERENCETYPECODE_ID = "newProtocolReference.protocolReferenceTypeCode";
     private static final String PROTOCOL_REFERENCE_PROTOCOLREFERENCETYPECODE = "4";
@@ -61,6 +72,8 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
     private static final String PROTOCOL_REFERENCE_REFERENCEKEY_ID = "newProtocolReference.referenceKey";
     private static final String PROTOCOL_REFERENCE_REFERENCEKEY = "My Test";
     
+    private static final String PROTOCOL_REFERENCE_DELETEMETHOD = "methodToCall.deleteProtocolReference.line0.anchor5";
+    private static final String PROTOCOL_REFERENCE_ADDMETHOD ="methodToCall.addProtocolReference.anchor";
     /**
      * This method asserts the form's additional field value persistence. 
      * @throws Exception
@@ -69,16 +82,17 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
     public  void testAdditionalFields() throws Exception {
         
         //Click to create new protocol link
-        HtmlPage page = clickOn(getPortalPage(), "Create Protocol", "Kuali Portal Index");
+        HtmlPage page = clickOn(getPortalPage(), NEW_PROTOCOL,KUALI_PORTAL_INDEX);
         page = getInnerPages(page).get(0);
         
-        assertTrue("Kuali :: Protocol Document".equalsIgnoreCase(page.getTitleText()));
+        assertTrue(PROTOCOL_DOCUMENT_NAME.equalsIgnoreCase(page.getTitleText()));
+        
         //Required Fields to begin with for saving protocol document
         setRequiredFields(page);
         
         //Additional Fields to populate for this test case
         
-        super.setFieldValue(page, PROTOCOL_ISBILLABLE_ID, PROTOCOL_ISBILLABLE);
+        //super.setFieldValue(page, PROTOCOL_ISBILLABLE_ID, PROTOCOL_ISBILLABLE);
         super.setFieldValue(page, PROTOCOL_FDAAPPLICATIONNUMBER_ID, PROTOCOL_FDAAPPLICATIONNUMBER);
         super.setFieldValue(page, PROTOCOL_REFERENCENUMBER1_ID, PROTOCOL_REFERENCENUMBER1);
         super.setFieldValue(page, PROTOCOL_REFERENCENUMBER2_ID, PROTOCOL_REFERENCENUMBER2);
@@ -88,7 +102,7 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
         HtmlPage resultPage = super.saveDoc(page);
         
         assertNotNull(resultPage);
-        assertEquals("Kuali :: Protocol Document", resultPage.getTitleText());
+        assertEquals(PROTOCOL_DOCUMENT_NAME, resultPage.getTitleText());
         
         super.hasError(resultPage);
         
@@ -98,7 +112,7 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
         verifySavedRequiredFields();        
     
         //Assert Additional Fields
-        assertTrue(getProtocolDocument().getProtocol().isBillable());
+        //assertTrue(getProtocolDocument().getProtocol().isBillable());
         assertEquals(PROTOCOL_FDAAPPLICATIONNUMBER, getProtocolDocument().getProtocol().getFdaApplicationNumber());
         assertEquals(PROTOCOL_REFERENCENUMBER1, getProtocolDocument().getProtocol().getReferenceNumber1());
         assertEquals(PROTOCOL_REFERENCENUMBER2, getProtocolDocument().getProtocol().getReferenceNumber2());
@@ -106,24 +120,89 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
         
     }
     
+    /**
+     * This method test billable authz, to make sure only IRB Admin is allowed to edit billable checkbox
+     * @throws Exception
+     */
+    @Test
+    public void testBillable() throws Exception {
+      //Click to create new protocol link
+        HtmlPage page = clickOn(getPortalPage(), NEW_PROTOCOL,KUALI_PORTAL_INDEX);
+        page = getInnerPages(page).get(0);
+        
+        assertTrue(PROTOCOL_DOCUMENT_NAME.equalsIgnoreCase(page.getTitleText()));
+        
+        HtmlElement expandedField = getElement(page, PROTOCOL_ISBILLABLE_ID);
+        
+        //Assert that quickstart user doesn't have permission to edit billable checkbox
+        assertEquals("disabled",expandedField.getAttributeValue("disabled"));
+        
+        //Required Fields to begin with for saving protocol document
+        setRequiredFields(page);
+       
+        //Invoke save method by clicking save button on form
+        HtmlPage resultPage = super.saveDoc(page);
+        
+        assertNotNull(resultPage);
+        assertEquals(PROTOCOL_DOCUMENT_NAME, resultPage.getTitleText());
+        
+        super.hasError(resultPage);
+        
+        //Set user with KRA Admin permission
+        GlobalVariables.setUserSession(new UserSession("aslusar"));
+        setPortalPage(buildPageFromUrl("http://localhost:" + getPort() + "/kra-dev/",KUALI_PORTAL_INDEX));
+        
+        //Search doc saved earlier to edit check box field
+        HtmlPage editablePage = super.saveAndSearchDoc(resultPage);
+        
+        //Check the check box
+        super.setFieldValue(editablePage, PROTOCOL_ISBILLABLE_ID, PROTOCOL_ISBILLABLE);
+        
+        //Save document 
+        HtmlPage resultPageWithBillable = super.saveDoc(editablePage);
+        
+        assertNotNull(resultPageWithBillable);
+        assertEquals(PROTOCOL_DOCUMENT_NAME, resultPageWithBillable.getTitleText());
+        
+        super.hasError(resultPageWithBillable);
+
+        HtmlElement expandedFieldAfterSave = getElement(page, PROTOCOL_ISBILLABLE_ID);
+        
+        //Check after save checkbox value persistance
+        assertEquals("on",expandedFieldAfterSave.getAttributeValue("value"));
+    }
+   
+    /**
+     * This method is used to test Expanded Text Area of summary/keywords field
+     * @throws Exception
+     */
     @Test
     public void testExpandedTextArea() throws Exception {
          
         //Click to create new protocol link
-        HtmlPage page = clickOn(getPortalPage(), "Create Protocol", "Kuali Portal Index");
+        HtmlPage page = clickOn(getPortalPage(), NEW_PROTOCOL,KUALI_PORTAL_INDEX);
         page = getInnerPages(page).get(0);
-        super.checkExpandedTextArea(page, PROTOCOL_DESCRIPTION_ID, "methodToCall.kraUpdateTextArea.((#document.protocol.description:protocolProtocol:Summary/Keywords#))", 
-                PROTOCOL_DESCRIPTION, PROTOCOL_DESCRIPTION2);
+        super.checkExpandedTextArea(page, PROTOCOL_DESCRIPTION_ID, PROTOCOL_DESCRIPTION_TEXTAREA, PROTOCOL_DESCRIPTION, PROTOCOL_DESCRIPTION2);
     }
     
+    /**
+     * This method tests the Research Area sub panel 
+     * a. Adds new record
+     * b. saves it
+     * c. checks to see if record was added
+     * d. deletes it
+     * f. saves it
+     * g. checks to see if it is deleted
+     * @throws Exception
+     */
     @Test
     public void testProtoclResearchAreaPanel() throws Exception {
         
         //Click to create new protocol link
-        HtmlPage page = clickOn(getPortalPage(), "Create Protocol", "Kuali Portal Index");
+        HtmlPage page = clickOn(getPortalPage(), NEW_PROTOCOL,KUALI_PORTAL_INDEX);
         page = getInnerPages(page).get(0);
         
-        assertTrue("Kuali :: Protocol Document".equalsIgnoreCase(page.getTitleText()));
+        assertTrue(PROTOCOL_DOCUMENT_NAME.equalsIgnoreCase(page.getTitleText()));
         //Required Fields to begin with for saving protocol document
         setRequiredFields(page);
         
@@ -131,7 +210,7 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
         HtmlPage resultPage = super.saveDoc(page);
         
         assertNotNull(resultPage);
-        assertEquals("Kuali :: Protocol Document", resultPage.getTitleText());
+        assertEquals(PROTOCOL_DOCUMENT_NAME, resultPage.getTitleText());
         
         //performing science Research Area lookup 
         HtmlPage pageWithResearchAreaLookup = multiLookup(resultPage, "ResearchAreas", "description", "T*");
@@ -145,9 +224,9 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
         HtmlPage afterSavePage = super.saveDoc(pageWithResearchAreaLookup);
         
         assertNotNull(afterSavePage);
-        assertEquals("Kuali :: Protocol Document", afterSavePage.getTitleText());
+        assertEquals(PROTOCOL_DOCUMENT_NAME, afterSavePage.getTitleText());
         
-        HtmlPage pageAfterDelete = clickOn(afterSavePage, "methodToCall.deleteProtocolResearchArea.line0.anchor5");
+        HtmlPage pageAfterDelete = clickOn(afterSavePage, PROTOCOL_RESEARCHAREA_DELETEMETHOD);
         
         assertDoesNotContain(pageAfterDelete, FIRST_ROW_DATA);
         
@@ -158,14 +237,24 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
         assertDoesNotContain(saveAfterDeletePage, FIRST_ROW_DATA);
     }
 
+    /**
+     * This method tests the Other Identifier sub panel 
+     * a. Adds new record
+     * b. saves it
+     * c. checks to see if record was added
+     * d. deletes it
+     * f. saves it
+     * g. checks to see if it is deleted
+     * @throws Exception
+     */
     @Test
     public void testProtoclOtherIdentifierPanel() throws Exception {
         
         //Click to create new protocol link
-        HtmlPage page = clickOn(getPortalPage(), "Create Protocol", "Kuali Portal Index");
+        HtmlPage page = clickOn(getPortalPage(), NEW_PROTOCOL,KUALI_PORTAL_INDEX);
         page = getInnerPages(page).get(0);
         
-        assertTrue("Kuali :: Protocol Document".equalsIgnoreCase(page.getTitleText()));
+        assertTrue(PROTOCOL_DOCUMENT_NAME.equalsIgnoreCase(page.getTitleText()));
         //Required Fields to begin with for saving protocol document
         setRequiredFields(page);
         
@@ -180,7 +269,7 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
         super.setFieldValue(resultPage,PROTOCOL_REFERENCE_PROTOCOLREFERENCETYPECODE_ID, PROTOCOL_REFERENCE_PROTOCOLREFERENCETYPECODE);
         super.setFieldValue(resultPage,PROTOCOL_REFERENCE_REFERENCEKEY_ID, PROTOCOL_REFERENCE_REFERENCEKEY);
         
-        HtmlPage pageAfterAdd= clickOn(resultPage,"methodToCall.addProtocolReference.anchor");
+        HtmlPage pageAfterAdd= clickOn(resultPage, PROTOCOL_REFERENCE_ADDMETHOD);
         
         setProtocolDocument(null, pageAfterAdd); //Can also be set by child if required
                 
@@ -191,7 +280,7 @@ public class ProtocolAdditionalFieldsWebTest extends ProtocolWebTestBase {
         assertContains(resultPageWithOtherIdentifiers, PROTOCOL_REFERENCE_REFERENCEKEY);
         
         
-        HtmlPage pageAfterDelete = clickOn(resultPageWithOtherIdentifiers, "methodToCall.deleteProtocolReference.line0.anchor5");
+        HtmlPage pageAfterDelete = clickOn(resultPageWithOtherIdentifiers, PROTOCOL_REFERENCE_DELETEMETHOD);
         
         assertDoesNotContain(pageAfterDelete, PROTOCOL_REFERENCE_PROTOCOLREFERENCETYPECODE_VALUE);
         assertDoesNotContain(pageAfterDelete, PROTOCOL_REFERENCE_REFERENCEKEY);
