871a1b8e24a41c2fd49fb9058f37acd18a9470f1
Tyler Wilson <tyler.wilson@colostate.edu>
2009-01-13 06:36:21 +0000

    Database Script changes for Rel 1 Patch 1

--- src/main/config/sql/KC-RELEASE-1_0_1-PATCH-SCRIPT/DML/KCRA_DML-1_0_1-Patch.sql
+++ src/main/config/sql/KC-RELEASE-1_0_1-PATCH-SCRIPT/DML/KCRA_DML-1_0_1-Patch.sql
@@ -89,3 +89,10 @@ UPDATE YNQ
 SET GROUP_NAME = 'General Y/N Questions'
 WHERE GROUP_NAME IN ('COEUS', 'Grants.Gov');
 
+update sh_parm_t t
+   set t.sh_parm_txt = 'N'
+ where t.sh_parm_nmspc_cd = 'KR-NS'
+   AND T.SH_PARM_DTL_TYP_CD = 'Document'
+   and t.sh_parm_nm = 'DEFAULT_CAN_PERFORM_ROUTE_REPORT_IND';
+   
+COMMIT;
