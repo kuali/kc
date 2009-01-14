642377be9e56cd98d2a1324cf96dc3831cc3bfe4
Varun Soni <vls45@cornell.edu>
2009-01-14 16:38:37 +0000

    KCAWD-45 - Business Rules, Related Unit tests

---
+++ src/main/java/org/kuali/kra/award/bo/ValidClassReportFrequency.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.award.bo;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+/**
+ * 
+ * This class represents a ValidClassReportFrequency business objects and 
+ * maps to VALID_CLASS_REPORT_FREQ table in the database.
+ */
+public class ValidClassReportFrequency extends KraPersistableBusinessObjectBase { 
+    
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 369663255546045771L;
+    private Integer validClassReportFreqId; 
+    private Integer reportClassCode; 
+    private Integer reportCode; 
+    private Integer frequencyCode; 
+    
+    private Frequency frequency; 
+    private Report report; 
+    private ReportClass reportClass; 
+    
+    /**
+     * 
+     * Constructs a ValidClassReportFrequency.java.
+     */
+    public ValidClassReportFrequency() { 
+
+    } 
+    
+    /**
+     *
+     * @return
+     */
+    public Integer getValidClassReportFreqId() {
+        return validClassReportFreqId;
+    }
+
+    /**
+     *
+     * @param validClassReportFreqId
+     */
+    public void setValidClassReportFreqId(Integer validClassReportFreqId) {
+        this.validClassReportFreqId = validClassReportFreqId;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Integer getReportClassCode() {
+        return reportClassCode;
+    }
+
+    /**
+     *
+     * @param reportClassCode
+     */
+    public void setReportClassCode(Integer reportClassCode) {
+        this.reportClassCode = reportClassCode;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Integer getReportCode() {
+        return reportCode;
+    }
+
+    /**
+     *
+     * @param reportCode
+     */
+    public void setReportCode(Integer reportCode) {
+        this.reportCode = reportCode;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Integer getFrequencyCode() {
+        return frequencyCode;
+    }
+
+    /**
+     *
+     * @param frequencyCode
+     */
+    public void setFrequencyCode(Integer frequencyCode) {
+        this.frequencyCode = frequencyCode;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Frequency getFrequency() {
+        return frequency;
+    }
+
+    /**
+     *
+     * @param frequency
+     */
+    public void setFrequency(Frequency frequency) {
+        this.frequency = frequency;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Report getReport() {
+        return report;
+    }
+
+    /**
+     *
+     * @param report
+     */
+    public void setReport(Report report) {
+        this.report = report;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ReportClass getReportClass() {
+        return reportClass;
+    }
+
+    /**
+     *
+     * @param reportClass
+     */
+    public void setReportClass(ReportClass reportClass) {
+        this.reportClass = reportClass;
+    }
+
+    /**
+     * 
+     * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
+     */
+    @Override 
+    protected LinkedHashMap<String,Object> toStringMapper() {
+        LinkedHashMap<String,Object> hashMap = new LinkedHashMap<String,Object>();
+        hashMap.put("validClassReportFreqId", getValidClassReportFreqId());
+        hashMap.put("reportClassCode", getReportClassCode());
+        hashMap.put("reportCode", getReportCode());
+        hashMap.put("frequencyCode", getFrequencyCode());
+        return hashMap;
+    }
+    
+}
