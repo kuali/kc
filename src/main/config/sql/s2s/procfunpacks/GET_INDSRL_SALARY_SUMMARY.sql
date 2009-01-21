07c64d358d489de7543d642c6e2b2f7645d4107e
Chitra Chandran <chitra07@msu.edu>
2009-01-21 21:52:12 +0000

    KRACOEUS-2104: Fixing latest issues on Budget Print Functionality

--- src/main/config/sql/s2s/procfunpacks/GET_INDSRL_SALARY_SUMMARY.sql
+++ src/main/config/sql/s2s/procfunpacks/GET_INDSRL_SALARY_SUMMARY.sql
@@ -1,121 +1,153 @@
-CREATE OR REPLACE procedure GET_INDSRL_SALARY_SUMMARY
-   ( as_proposal_number IN osp$budget_per_details_for_edi.PROPOSAL_NUMBER%TYPE,
-	  ai_version			IN osp$budget_details.version_number%TYPE,
-	  ai_period  			IN osp$budget_per_details_for_edi.budget_period%TYPE,
-     cur_summary 	IN OUT result_sets.cur_generic) is
+create or replace procedure GET_INDSRL_SALARY_SUMMARY(as_proposal_number IN osp$budget_per_details_for_edi.PROPOSAL_NUMBER%TYPE,
+                                                      ai_version         IN osp$budget_details.version_number%TYPE,
+                                                      ai_period          IN osp$budget_per_details_for_edi.budget_period%TYPE,
+                                                      cur_summary        IN OUT result_sets.cur_generic) is
 
 begin
-open cur_summary for
-select start_date, end_date, category, person_name, PERCENT_EFFORT, percent_charged,
-		category_code,
-		salary,
-		PRINCIPAL_INVESTIGATOR, cost_element_desc
+  open cur_summary for
 
-		from (
-			 select min(pd.start_date) start_date, max(pd.end_date) end_date, DECODE(inv.PRINCIPAL_INVESTIGATOR_FLAG,
-				'Y', '1', 'N', '2', NULL, '3', '4') PRINCIPAL_INVESTIGATOR,
-	 			decode(decode(pd.person_id || pd.job_code, '999999999LA', 99, bd.budget_category_code) , 1, bc.description, 99, 'Lab Allocation', bc.description) category,
-				decode(pd.person_id || pd.job_code, '999999999LA', 'Allocated Admin Support',
-						'999999999000000', p.full_name || ' (' || to_char(bd.quantity) || ')',
-						p.full_name ) person_name,
-				decode(pd.person_id || pd.job_code, '999999999000000',  NULL, PERCENT_EFFORT) PERCENT_EFFORT,
-				decode(pd.person_id || pd.job_code, '999999999000000',  NULL, percent_charged) percent_charged,
-				sum(pd.salary_requested)+
-					decode(sum(eb.amt), NULL, 0, sum(eb.amt)) +
-					decode(sum(vac.amt), NULL, 0, sum(vac.amt)) +
-					decode(sum(oh.amt), NULL, 0, sum(oh.amt)) Salary,
-				to_char(decode(max(eb.rate), NULL, 0, max(eb.rate)), '990.00') || '%' eb_rate,
-				to_char(decode(max(vac.rate), NULL, 0, max(vac.rate)), '990.00') || '%' vac_rate,
-				decode(pd.person_id || pd.job_code, '999999999LA', 99, bd.budget_category_code) category_code,
-				ce.description cost_element_desc
-			from osp$budget_per_details_for_edi pd,
-					 osp$person p, osp$budget_details bd,
-					 osp$budget_category bc,
-					 osp$eps_prop_investigators inv,
-					 osp$cost_element ce,
-					 (select pcal.person_number person_num,
-						 pcal.rate_number rate_num,
-						 pcal.line_item_number line_num,
-						 pcal.rate_class_code rate_class,
-						 pcal.rate_type_code  rate_type,
-						 pcal.calculated_cost amt ,
-						 pcal.applied_rate rate
-							from osp$budget_per_cal_amt_for_edi pcal,
-							osp$rate_class rc
-							where pcal.rate_class_code = rc.rate_class_code
-							and rc.rate_class_type = 'E'
-							and  pcal.proposal_number = as_proposal_number
-							and pcal.budget_period = ai_period
-					 ) eb,
-					(select pcal.person_number person_num,
-						pcal.rate_number rate_num,
-						pcal.line_item_number line_num,
-						pcal.rate_class_code rate_class,
-						pcal.rate_type_code  rate_type,
-						pcal.calculated_cost amt ,
-						pcal.applied_rate rate
-						from osp$budget_per_cal_amt_for_edi pcal,
-						osp$rate_class rc
-						where pcal.rate_class_code = rc.rate_class_code
-						and rc.rate_class_type = 'V'
-						and  pcal.proposal_number = as_proposal_number
-						and pcal.budget_period = ai_period
-					) vac,
-					 (select pcal.person_number person_num,
-						 pcal.rate_number rate_num,
-						 pcal.line_item_number line_num,
-						 pcal.rate_class_code rate_class,
-						 pcal.rate_type_code  rate_type,
-						 pcal.calculated_cost amt ,
-						 pcal.applied_rate rate
-							from osp$budget_per_cal_amt_for_edi pcal,
-							osp$rate_class rc
-							where pcal.rate_class_code = rc.rate_class_code
-							and rc.rate_class_type = 'O'
-							and  pcal.proposal_number = as_proposal_number
-							and pcal.budget_period = ai_period
-					 ) oh
-			where pd.proposal_number = as_proposal_number
-				and pd.budget_period = ai_period
-				and bd.version_number = ai_version
-				and pd.proposal_number = bd.proposal_number
-				and pd.budget_period = bd.budget_period
-				and pd.line_item_number = bd.line_item_number
-				and bd.budget_category_code = bc.budget_category_code
-				and bd.cost_element = ce.cost_element
-				and pd.line_item_number = eb.line_num (+)
-				and pd.person_number = eb.person_num (+)
-				and pd.rate_number = eb.rate_num (+)
-				and pd.rate_number = vac.rate_num (+)
-				and pd.line_item_number = vac.line_num (+)
-				and pd.person_number = vac.person_num (+)
-				and pd.rate_number = oh.rate_num (+)
-				and pd.line_item_number = oh.line_num (+)
-				and pd.person_number = oh.person_num (+)
-				and pd.proposal_number = inv.proposal_number (+)
-				and pd.person_id = inv.person_id (+)
-				and pd.person_id = p.person_id
-			group by
-			decode(pd.person_id || pd.job_code, '999999999LA', 999, pd.person_number),
-			DECODE(inv.PRINCIPAL_INVESTIGATOR_FLAG, 'Y', '1', 'N', '2', NULL, '3', '4'),
-				decode(decode(pd.person_id || pd.job_code, '999999999LA', 99, bd.budget_category_code) , 1, bc.description,  99, 'Lab Allocation', bc.description),
-				decode(pd.person_id || pd.job_code, '999999999LA', 'Allocated Admin Support',
-				 	'999999999000000', p.full_name || ' (' || to_char(bd.quantity) || ')',
-				p.full_name ), --changed group by also
-				decode(pd.person_id || pd.job_code, '999999999000000', NULL, PERCENT_EFFORT) ,
-				decode(pd.person_id || pd.job_code, '999999999000000', NULL, percent_charged) ,
-				eb.rate, vac.rate , oh.rate, ce.DESCRIPTION,
-				decode(pd.person_id || pd.job_code, '999999999LA', 99, bd.budget_category_code)
-			order by decode(pd.person_id || pd.job_code, '999999999LA', 99, bd.budget_category_code),
-				DECODE(inv.PRINCIPAL_INVESTIGATOR_FLAG, 'Y', '1', 'N', '2', NULL, '3', '4'),
-				decode(pd.person_id || pd.job_code, '999999999LA', 'Allocated Admin Support',
-				 	'999999999000000', p.full_name || ' (' || to_char(bd.quantity) || ')',
-				p.full_name),ce.DESCRIPTION,
-				min(PD.START_DATE)
-		)
+    SELECT START_DATE,
+           END_DATE,
+           FDESC AS CATEGORY,
+           FULL_NAME AS PERSON_NAME,
+           PERCENT_EFFORT,
+           PERCENT_CHARGED,
+           BUDGET_CATEGORY_CODE AS CATEGORY_CODE,
+           MAX(SALARY_REQUESTED) + SUM(calculated_cost) AS SALARY,
+           PRINCIPAL_INVESTIGATOR,
+           GDESC AS COST_ELEMENT_DESC
 
-order by category_code, PRINCIPAL_INVESTIGATOR, person_name, start_date;
+      FROM (
+
+            select DISTINCT proposal_number,
+                             version_number,
+                             budget_period,
+                             line_item_number,
+                             budget_category_code,
+                             cost_element,
+                             fdesc,
+                             gdesc,
+                             person_number,
+                             person_id,
+                             full_name,
+                             percent_charged,
+                             percent_effort,
+                             job_code,
+                             start_date,
+                             end_date,
+                             SALARY_REQUESTED,
+                             decode(rate_class_code,
+                                    null,
+                                    sRATE_CLASS_CODE,
+                                    rate_class_code) rate_class_code,
+                             decode(rate_type_code,
+                                    null,
+                                    srate_type_code,
+                                    rate_type_code) rate_type_code,
+                             decode(rate_class_type,
+                                    null,
+                                    srate_class_type,
+                                    rate_class_type) rate_class_type,
+                             decode(calculated_cost,
+                                    null,
+                                    scalculated_cost,
+                                    calculated_cost) calculated_cost,
+                             PRINCIPAL_INVESTIGATOR
+              from (
+
+                     select A1.proposal_number        proposal_number,
+                             A1.version_number         version_number,
+                             A1.budget_period          budget_period,
+                             A1.line_item_number       line_item_number,
+                             A1.budget_category_code   budget_category_code,
+                             A1.cost_element           cost_element,
+                             A1.fdesc                  fdesc,
+                             A1.gdesc                  gdesc,
+                             A1.person_number          person_number,
+                             A1.person_id              person_id,
+                             A1.full_name              full_name,
+                             A1.percent_charged        percent_charged,
+                             A1.percent_effort         percent_effort,
+                             A1.job_code               job_code,
+                             A1.start_date             start_date,
+                             A1.end_date               end_date,
+                             A1.SALARY_REQUESTED       SALARY_REQUESTED,
+                             A1.rate_class_code        rate_class_code,
+                             A1.rate_type_code         rate_type_code,
+                             a1.rate_class_type        rate_class_type,
+                             a1.calculated_cost        calculated_cost,
+                             a2.RATE_CLASS_CODE        sRATE_CLASS_CODE,
+                             a2.RATE_TYPE_CODE         sRATE_TYPE_CODE,
+                             d.RATE_CLASS_TYPE         sRATE_CLASS_TYPE,
+                             rb.APPLIED_RATE           sAPPLIED_RATE,
+                             a2.CALCULATED_COST        sCALCULATED_COST,
+                             a1.PRINCIPAL_INVESTIGATOR PRINCIPAL_INVESTIGATOR
+
+                       from OSP$BUDGET_PER_COST_SUMMARY A1,
+                             osp$budget_details_cal_amts a2,
+                             osp$budget_rate_and_base    rb,
+                             osp$rate_class              d,
+                             osp$rate_type               e
+                      where a1.proposal_number = a2.PROPOSAL_NUMBER(+)
+                        AND A2.PROPOSAL_NUMBER = RB.PROPOSAL_NUMBER
+                        and a1.version_number = a2.VERSION_NUMBER(+)
+                        AND A2.VERSION_NUMBER = RB.VERSION_NUMBER
+                        and a1.budget_period = a2.BUDGET_PERIOD(+)
+                        AND A2.BUDGET_PERIOD = RB.BUDGET_PERIOD
+                        and a1.line_item_number = a2.LINE_ITEM_NUMBER(+)
+                        AND A2.LINE_ITEM_NUMBER = RB.LINE_ITEM_NUMBER
+                        AND A2.RATE_CLASS_CODE = RB.RATE_CLASS_CODE
+                        AND A2.RATE_TYPE_CODE = RB.RATE_TYPE_CODE
+                        AND RB.RATE_CLASS_CODE = D.RATE_CLASS_CODE
+                        AND D.RATE_CLASS_CODE = E.RATE_CLASS_CODE
+                        AND E.RATE_CLASS_CODE = RB.RATE_CLASS_CODE
+                        AND E.RATE_TYPE_CODE = RB.RATE_TYPE_CODE
+                        AND a1.proposal_number = as_proposal_number
+                        And a1.version_number = ai_version
+                        and a1.budget_period = ai_period)
+
+             ORDER by proposal_number,
+                       version_number,
+                       budget_period,
+                       line_item_number,
+                       budget_category_code,
+                       cost_element,
+                       fdesc,
+                       gdesc,
+                       PRINCIPAL_INVESTIGATOR,
+                       PERSON_NUMBER,
+                       person_id,
+                       full_name,
+                       percent_charged,
+                       percent_effort,
+                       job_code,
+                       START_DATE,
+                       END_DATE,
+                       SALARY_REQUESTED,
+                       rate_class_code,
+                       rate_type_code,
+                       rate_class_type,
+                       calculated_cost
+
+            )
+     GROUP by budget_category_code,
+              cost_element,
+              fdesc,
+              gdesc,
+              PRINCIPAL_INVESTIGATOR,
+              PERSON_NUMBER,
+              person_id,
+              full_name,
+              percent_charged,
+              percent_effort,
+              job_code,
+              START_DATE,
+              END_DATE
+
+     order by budget_category_code,
+              PRINCIPAL_INVESTIGATOR,
+              full_name,
+              START_DATE;
 
 end;
 /
-
