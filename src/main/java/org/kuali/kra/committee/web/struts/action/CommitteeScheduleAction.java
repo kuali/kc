0d6071cbb0169b1c808c9f861186ee73cb7f7e88
Kiltesh Patel <kjpatel@ucdavis.edu>
2009-01-22 00:29:30 +0000

    Jira -54

---
+++ src/main/java/org/kuali/kra/committee/web/struts/action/CommitteeScheduleAction.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.committee.web.struts.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kra.committee.web.struts.form.CommitteeForm;
+import org.kuali.kra.infrastructure.Constants;
+
+public class CommitteeScheduleAction extends CommitteeAction {
+    
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(CommitteeScheduleAction.class);
+    
+    public ActionForward addEvent(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+                
+        return mapping.findForward(Constants.MAPPING_BASIC );
+    }
+    
+    public ActionForward reload(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+        CommitteeForm committeeForm = (CommitteeForm) form;
+        committeeForm.getScheduleData().populateStyleClass();
+        //LOG.info("From action form ScheduleDate : ===================== :" + committeeForm.getScheduleEndDate());
+        
+        return mapping.findForward(Constants.MAPPING_BASIC );
+    }    
+}
