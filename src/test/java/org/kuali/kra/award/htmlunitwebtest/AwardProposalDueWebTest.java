c9cd469d849ffdc7d75df4b17923be3ab96b0a6b
Jack Frosch <jfrosch@objistics.com>
2009-01-23 20:45:15 +0000

    Refactored the base AwardWebTestBase class to extract an AwardPaymentsAndTerms page web test. Individual tabs (or even panels) on the AwardPaymentsAndTerms page panels should have their own web test class extend from the AwardPaymentsAndTermsWebTest; i.e.
    YourTabWebTest -- extends --> AwardPaymentsAndTermsWebTest -- extends --> AwardWebTestBase
    
    Likewise, other pages in Awards should have their own base web test so we don't pile everything into AwardWebTestBase

---
+++ src/test/java/org/kuali/kra/award/htmlunitwebtest/AwardProposalDueWebTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.award.htmlunitwebtest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+/**
+ * 
+ * This is the integration test for Award Payment Reports And Terms Page. 
+ */
+
+public class AwardProposalDueWebTest extends AwardPaymentsAndTermsWebTest {
+    
+    /**
+     * The set up method calls the parent super method and gets the 
+     * award Payment Reports And Terms page after that.
+     * @see org.kuali.kra.award.htmlunitwebtest.AwardWebTestBase#setUp()
+     */
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    /**
+     * This method calls parent tear down method and than sets paymentReportsAndTermsPage to null
+     * @see org.kuali.kra.award.htmlunitwebtest.AwardWebTestBase#tearDown()
+     */
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    /**
+     * 
+     * This method tests Preaward Authorizations Panel.  Adding amounts and effective dates. 
+     * and saving them.  
+     * @throws Exception
+     */
+    @Test
+    public void testProposalDuePanel() throws Exception{
+        System.out.println(paymentReportsAndTermsPage.asXml());
+        setFieldValue(paymentReportsAndTermsPage, "document.awardList[0].competingRenewalProposalDue", "8");
+        setFieldValue(paymentReportsAndTermsPage, "document.awardList[0].nonCompetingContProposalDue", "9");
+        
+        HtmlPage awardPaymentReportsAndTermsAfterSave = clickOn(paymentReportsAndTermsPage, "methodToCall.save");
+        assertDoesNotContain(awardPaymentReportsAndTermsAfterSave, ERROR_TABLE_OR_VIEW_DOES_NOT_EXIST);
+        assertDoesNotContain(awardPaymentReportsAndTermsAfterSave, ERRORS_FOUND_ON_PAGE);
+        assertContains(awardPaymentReportsAndTermsAfterSave,SAVE_SUCCESS_MESSAGE);
+        
+    }
+}
