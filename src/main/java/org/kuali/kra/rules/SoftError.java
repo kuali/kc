b9f13c4de1591be9668ffbb8881e3b067bd2f017
Jack Frosch <jfrosch@objistics.com>
2009-01-26 22:18:47 +0000

    KCAWD-16: Initial commit

---
+++ src/main/java/org/kuali/kra/rules/SoftError.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.rules;
+
+/**
+ * This class is used to report a soft error (i.e. a warning)
+ */
+public class SoftError {
+    private String errorKey;
+    private String[] errorParms;
+    
+    /**
+     * Constructs a SoftError
+     * @param errorKey
+     * @param errorParms
+     */
+    public SoftError(String errorkey, String[] errorParms) {
+        this.errorKey = errorkey;
+        this.errorParms = errorParms;
+    }
+
+    /**
+     * Gets the errorKey attribute. 
+     * @return Returns the errorKey.
+     */
+    public String getErrorKey() {
+        return errorKey;
+    }
+
+    /**
+     * Gets the errorParms attribute. 
+     * @return Returns the errorParms.
+     */
+    public String[] getErrorParms() {
+        return errorParms;
+    }
+    
+    
+}
