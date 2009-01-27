4a885f3b913666e186c082c2c6ef00a5b8ef9e2d
Jack Frosch <jfrosch@objistics.com>
2009-01-27 07:49:13 +0000

    KCAWD-16: Added system parms for DEV

--- src/main/config/sql/dml/load_system_params.sql
+++ src/main/config/sql/dml/load_system_params.sql
@@ -721,3 +721,18 @@ INSERT into SH_PARM_T
 ( SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM,  SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND, OBJ_ID, VER_NBR)
 Values 
 ('KC-AWARD','D','mit.idc.validation.enabled','CONFG','1','MitIdcValidationEnabled is configurable at impl time','A','WorkflowAdmin','Y',sys_guid(),'1');
+
+INSERT INTO SH_PARM_T 
+	(SH_PARM_NMSPC_CD,SH_PARM_DTL_TYP_CD,SH_PARM_NM,OBJ_ID,VER_NBR,SH_PARM_TYP_CD,SH_PARM_TXT,SH_PARM_DESC,SH_PARM_CONS_CD,WRKGRP_NM,ACTIVE_IND) 
+	VALUES 
+	('KC-AWARD','D','federalCapitalizationMinimum',sys_guid(),1,'CONFG','0.00','Federal Capitalization Minimum','A','WorkflowAdmin','Y')
+/
+  
+INSERT INTO SH_PARM_T 
+	(SH_PARM_NMSPC_CD,SH_PARM_DTL_TYP_CD,SH_PARM_NM,OBJ_ID,VER_NBR,SH_PARM_TYP_CD,SH_PARM_TXT,SH_PARM_DESC,SH_PARM_CONS_CD,WRKGRP_NM,ACTIVE_IND) 
+	VALUES 
+	('KC-AWARD','D','institutionCapitalizationMinimum',sys_guid(),1,'CONFG','0.00','Institution Capitalization Minimum','A','WorkflowAdmin','Y')
+/
+
+commit
+/
