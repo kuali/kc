571d1ccedc5057b9f7ab63fac2718aa04fff8f53
Rajeev Mancheril <rajeev@genesistec.com>
2009-01-28 23:15:03 +0000

    Protocol personnel database update personnel training parameter KCIRB-70

--- src/main/config/sql/dml/load_system_params.sql
+++ src/main/config/sql/dml/load_system_params.sql
@@ -734,5 +734,12 @@ INSERT INTO SH_PARM_T
 	('KC-AWARD','D','institutionCapitalizationMinimum',sys_guid(),1,'CONFG','0.00','Institution Capitalization Minimum','A','WorkflowAdmin','Y')
 /
 
+Insert into SH_PARM_T
+   (SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM, OBJ_ID, VER_NBR, SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND)
+ Values
+   ('KC-PROTOCOL', 'D', 'protocolPersonTrainingSectionRequired', '2347F6B21CAB41DAB20A395611C6ED23', 1, 'CONFG', 'True', 'Implementing institution can decide on whether to display training section', 'A', 'WorkflowAdmin', 'Y')
+/
+
+
 commit
 /
