02afaa2081b1486aad085ebc5fbd60cc15b6a6ed
Rajeev Mancheril <rajeev@genesistec.com>
2009-01-28 23:57:06 +0000

    Protocol personnel database update personnel training parameter - test environment KCIRB-70

--- src/test/resources/sql/dml/load_system_params.sql
+++ src/test/resources/sql/dml/load_system_params.sql
@@ -274,3 +274,8 @@ Insert into SH_PARM_T
 ( SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM, OBJ_ID, VER_NBR, SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND)
 Values
 ('KC-PROTOCOL','D','irb.protocol.referenceID2','F1C228F9D4D8408A8E0BBC801C9525az',1,'CONFG','Reference ID2','Referece id is configurable at impl time','A','WorkflowAdmin','Y');
+
+Insert into SH_PARM_T
+   (SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM, OBJ_ID, VER_NBR, SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND)
+ Values
+   ('KC-PROTOCOL', 'D', 'protocolPersonTrainingSectionRequired', '2347F6B21CAB41DAB20A395611C6ED23', 1, 'CONFG', 'True', 'Implementing institution can decide on whether to display training section', 'A', 'WorkflowAdmin', 'Y');
