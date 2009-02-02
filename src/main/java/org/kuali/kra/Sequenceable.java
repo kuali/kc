442037d6cdf153ffc358f6732d2f77598f45d433
Jack Frosch <jfrosch@objistics.com>
2009-02-02 22:27:00 +0000

    KRACOEUS-2121: Still a work in progress. Needed to commit to get files sync'ed to workstation from notebook

---
+++ src/main/java/org/kuali/kra/Sequenceable.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra;
+
+import java.io.Serializable;
+
+
+/**
+ * This interface defines versionable behavior
+ */
+public interface Sequenceable extends Serializable {
+    /**
+     * This gets the current version number
+     * @return
+     */
+    Integer getSequenceNumber();
+    
+    /**
+     * This sequences the object
+     * @param owner
+     */
+    void setSequenceOwner(Sequenceable owner);
+}
