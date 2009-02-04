499f53fbb1aa34f8cb1a03750d56af5a889e7a7b
Jack Frosch <jfrosch@objistics.com>
2009-02-04 22:05:04 +0000

    KRACOEUS-2121: Initial commit. Work still in progress.

---
+++ src/main/java/org/kuali/kra/SequenceAssociate.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra;
+
+/**
+ * This interface applies to classes that are associated with a sequence owner; i.e. AwardApprovedEquipment
+ */
+public interface SequenceAssociate extends Sequenceable {
+    /**
+     * This sets the sequence owner on the associate; should no-op this associate is the sequence owner
+     * @param owner
+     */
+    void setSequenceOwner(SequenceOwner newOwner);
+    
+    /**
+     * @return the SequenceOwner is returned; "this" should be returned if this associate is the sequence owner
+     */
+    SequenceOwner getSequenceOwner();
+}
