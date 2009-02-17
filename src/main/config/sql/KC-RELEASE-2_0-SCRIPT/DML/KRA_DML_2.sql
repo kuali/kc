8fb1e337dad9fe66a27098f617182d5b2bdc8c6b
Tyler Wilson <tyler.wilson@colostate.edu>
2009-02-17 22:28:02 +0000

    Database Script changes for Rel 2

--- src/main/config/sql/KC-RELEASE-2_0-SCRIPT/DML/KRA_DML_2.sql
+++ src/main/config/sql/KC-RELEASE-2_0-SCRIPT/DML/KRA_DML_2.sql
@@ -731,3 +731,8 @@ INSERT INTO RISK_LEVEL ( RISK_LEVEL_CODE, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_
 VALUES ( 5, 'High Risk', sysdate, user ); 
 
 commit;
+
+update SH_PARM_T
+set SH_PARM_TXT='6' where SH_PARM_NMSPC_CD = 'KC-AWARD' AND SH_PARM_DTL_TYP_CD = 'D' AND SH_PARM_NM='reportClassForPaymentsAndInvoices';
+
+COMMIT;
