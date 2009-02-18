8b057ac6d5d5209fe7089d6232da63e688c7e1b3
Varun Soni <vls45@cornell.edu>
2009-02-18 17:02:09 +0000

    KCAWD-41 - refactored values finders as per code review comments

--- src/main/java/org/kuali/kra/award/lookup/keyvalue/ReportCodeValuesFinder.java
+++ src/main/java/org/kuali/kra/award/lookup/keyvalue/ReportCodeValuesFinder.java
@@ -17,9 +17,9 @@ package org.kuali.kra.award.lookup.keyvalue;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.core.lookup.keyvalues.KeyValuesBase;
@@ -34,54 +34,44 @@ import org.kuali.kra.infrastructure.KraServiceLocator;
  */
 public class ReportCodeValuesFinder extends KeyValuesBase {
     
+    KeyValuesService keyValuesService;
+    
     private String reportClassCode;
     
-    public ReportCodeValuesFinder() {
-        super();
+    /**
+     * 
+     * Constructs a ReportCodeValuesFinder.java.
+     */
+    public ReportCodeValuesFinder(){
+        
     }
     
-    public ReportCodeValuesFinder(String reportClassCode) {
-        super();
-        this.reportClassCode = reportClassCode;
+    /**
+     * 
+     * Constructs a ReportCodeValuesFinder.java.
+     * @param reportClassCode
+     */
+    public ReportCodeValuesFinder(String reportClassCode){
+        this.reportClassCode=reportClassCode;
     }
     
     /**
      * Constructs the list of Reports.  Each entry
      * in the list is a &lt;key, value&gt; pair, where the "key" is the unique
      * report code and the "value" is the textual description that is viewed
-     * by a user.  The list is obtained from a lookup of the REPORT database table
+     * by a user. 
      * 
      * @return the list of &lt;key, value&gt; pairs of abstract types.  The first entry
      * is always &lt;"", "select:"&gt;.
      * @see org.kuali.core.lookup.keyvalues.KeyValuesFinder#getKeyValues()
      */
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings("all")
     public List<KeyLabelPair> getKeyValues() {
-        KeyValuesService keyValuesService = getKeyValuesService();
-            
-        List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
-                
-        Collection validClassReportFrequencies = 
-            keyValuesService.findAll(ValidClassReportFrequency.class);        
+        Collection<ValidClassReportFrequency> validClassReportFrequencies =  
+            (Collection<ValidClassReportFrequency>) getKeyValuesService()
+                .findAll(ValidClassReportFrequency.class);        
         
-        HashMap uniqueReportCodes = new HashMap();
-        ValidClassReportFrequency validClassReportFrequency = new ValidClassReportFrequency();
-        int key=0;
-        keyValues.add(new KeyLabelPair("","select"));
-        for (Iterator iter1 = validClassReportFrequencies.iterator(); iter1.hasNext();) {
-            validClassReportFrequency = (ValidClassReportFrequency) iter1.next();
-            validClassReportFrequency.refreshReferenceObject("report");            
-            if((!uniqueReportCodes.containsValue(validClassReportFrequency.getReportCode()))
-                    && StringUtils.equalsIgnoreCase(
-                            validClassReportFrequency.getReportClassCode().toString(),getReportClassCode())){
-                
-                keyValues.add(new KeyLabelPair(validClassReportFrequency.getReportCode().toString()
-                        , validClassReportFrequency.getReport().getDescription()));
-                uniqueReportCodes.put(key, validClassReportFrequency.getReportCode());                
-            }
-            key++;     
-        }
-        return keyValues;
+        return getKeyValues(getUniqueRelevantReportClassCodes(validClassReportFrequencies));
     }
     
     public String getReportClassCode() {
@@ -91,9 +81,62 @@ public class ReportCodeValuesFinder extends KeyValuesBase {
     public void setReportClassCode(String reportClassCode) {
         this.reportClassCode = reportClassCode;
     }
-    
+
     protected KeyValuesService getKeyValuesService(){
-        return (KeyValuesService) KraServiceLocator.getService("keyValuesService");        
+        if(keyValuesService == null){
+            keyValuesService = 
+                (KeyValuesService) KraServiceLocator.getService("keyValuesService");
+        }
+        return keyValuesService;
+    }
+    
+    /**
+     * 
+     * This method iterates through the validClassReportFrequency and puts the valid ones
+     * in a set for another method to process. This method sets the frequency code to null to make all the 
+     * objects having same report class code and report code but different frequency code to resemble same;
+     * as it is not relevant here. 
+     * @param validClassReportFrequencies
+     * @return
+     */
+    protected Set<String> getUniqueRelevantReportClassCodes(
+            Collection<ValidClassReportFrequency> validClassReportFrequencies){
+        
+        Set<String> uniqueRelevantReportClassCodes
+            = new HashSet<String>();
+        
+        for(ValidClassReportFrequency validClassReportFrequency: validClassReportFrequencies){
+            if(StringUtils.equalsIgnoreCase(
+                    validClassReportFrequency.getReportClassCode(),getReportClassCode())){
+                uniqueRelevantReportClassCodes.add(validClassReportFrequency.getReportCode());    
+            }
+        }
+        
+        return uniqueRelevantReportClassCodes;
+    }
+    
+    /**
+     * 
+     * This method browses through set and creates the keylabelpair list from it.
+     * 
+     * @param uniqueValidClassReportFrequencies
+     * @return
+     */
+    protected List<KeyLabelPair> getKeyValues(
+            Set<String> uniqueValidClassReportFrequencies){
+        
+        List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
+        
+        keyValues.add(new KeyLabelPair("","select"));
+        ValidClassReportFrequency validClassReportFrequency = new ValidClassReportFrequency();
+        for(String reportCode: uniqueValidClassReportFrequencies){        
+            validClassReportFrequency.setReportCode(reportCode);
+            validClassReportFrequency.refreshReferenceObject("report");
+            keyValues.add(new KeyLabelPair(validClassReportFrequency.getReportCode().toString()
+                    , validClassReportFrequency.getReport().getDescription()));            
+        }
+        
+        return keyValues;
     }
    
 }
