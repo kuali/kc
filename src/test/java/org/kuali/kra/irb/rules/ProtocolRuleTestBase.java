65f49b4b97ce6457b23cc53e76647cf9b89eba27
Rajeev Mancheril <rajeev@genesistec.com>
2009-02-19 03:45:42 +0000

    Protocol personnel update KCIRB-74 KCIRB-75

--- src/test/java/org/kuali/kra/irb/rules/ProtocolRuleTestBase.java
+++ src/test/java/org/kuali/kra/irb/rules/ProtocolRuleTestBase.java
@@ -24,6 +24,9 @@ import org.kuali.core.service.DocumentService;
 import org.kuali.core.util.ErrorMap;
 import org.kuali.core.util.GlobalVariables;
 import org.kuali.kra.KraTestBase;
+import org.kuali.kra.irb.bo.Protocol;
+import org.kuali.kra.irb.bo.ProtocolPerson;
+import org.kuali.kra.irb.bo.ProtocolUnit;
 import org.kuali.kra.irb.document.ProtocolDocument;
 import org.kuali.rice.KNSServiceLocator;
 
@@ -36,6 +39,17 @@ import edu.iu.uis.eden.exception.WorkflowException;
 public abstract class ProtocolRuleTestBase extends KraTestBase {
 
     protected DocumentService documentService = null;
+    protected static final String DEFAULT_DOCUMENT_DESCRIPTION = "Protocol Document";
+    protected static final String PROTOCOL_STATUS_STR = "100"; //test of option "Pending/In Progress";
+    protected static final String PROTOCOL_TYPE_CODE_STR = "1";//test of option "Standard";
+    protected static final String PROTOCOL_TITLE_STR = "New protocol test";
+    protected static final String PRINCIPAL_INVESTIGATOR_ID = "000000001";
+    protected static final String PRINCIPAL_INVESTIGATOR_NAME = "Terry Durkin";
+    protected static final String PRINCIPAL_INVESTIGATOR_UNIT = "BL-BL";
+    protected static final String PRINCIPAL_INVESTIGATOR_ROLE = "PI";
+    protected static final String REFERENCE_PERSON_ROLE = "protocolPersonRole";
+    protected static final String REFERENCE_UNIT = "unit";
+    
 
     @Before
     public void setUp() throws Exception {
@@ -64,4 +78,56 @@ public abstract class ProtocolRuleTestBase extends KraTestBase {
     protected ProtocolDocument getNewProtocolDocument() throws WorkflowException {
         return (ProtocolDocument) documentService.getNewDocument("ProtocolDocument");
     }
+
+    /**
+     * This method is to set required fields for Protocol document
+     * @param document
+     */
+    protected void setProtocolRequiredFields(ProtocolDocument document) {
+        Protocol protocol = document.getProtocol();
+        document.getDocumentHeader().setDocumentDescription(DEFAULT_DOCUMENT_DESCRIPTION);
+        protocol.setProtocolTypeCode(PROTOCOL_TYPE_CODE_STR);
+        protocol.setTitle(PROTOCOL_TITLE_STR);
+        
+        ProtocolPerson protocolPerson = getProtocolPerson(PRINCIPAL_INVESTIGATOR_ID, PRINCIPAL_INVESTIGATOR_NAME, PRINCIPAL_INVESTIGATOR_ROLE);
+        
+        ProtocolUnit protocolUnit = new ProtocolUnit();
+        protocolUnit.setUnitNumber(PRINCIPAL_INVESTIGATOR_UNIT);
+        protocolUnit.setLeadUnitFlag(true);
+        protocolUnit.setProtocolNumber("0");
+        protocolUnit.setSequenceNumber(0);
+        protocolUnit.refreshReferenceObject(REFERENCE_UNIT);
+
+        protocolPerson.getProtocolUnits().add(protocolUnit);
+        
+        protocol.getProtocolPersons().add(protocolPerson);
+    }
+    
+    /**
+     * This method is to get protocol person details
+     * @param personId
+     * @param personName
+     * @param personRole
+     * @return
+     */
+    protected ProtocolPerson getProtocolPerson(String personId, String personName, String personRole) {
+        ProtocolPerson protocolPerson = new ProtocolPerson();
+        protocolPerson.setPersonId(personId);
+        protocolPerson.setPersonName(personName);
+        protocolPerson.setProtocolPersonRoleId(personRole);
+        protocolPerson.setPreviousPersonRoleId(personRole);
+        protocolPerson.setProtocolNumber("0");
+        protocolPerson.setSequenceNumber(0);
+        protocolPerson.refreshReferenceObject(REFERENCE_PERSON_ROLE);
+        return protocolPerson;
+    }
+    
+    /**
+     * This method is to get protocol person with role PI
+     * @return
+     */
+    protected ProtocolPerson getPrincipalInvestigator() {
+        return getProtocolPerson(PRINCIPAL_INVESTIGATOR_ID, PRINCIPAL_INVESTIGATOR_NAME, PRINCIPAL_INVESTIGATOR_ROLE);
+    }
+
 }
