add6863372ecc5bca4b2f328c5d0cf4f928bb5c8
Don Barre <dbarre@arizona.edu>
2009-02-23 20:33:09 +0000

    KCIRB-133 : Initial Struts/JSP work for Copy Protocol

---
+++ src/main/java/org/kuali/kra/irb/web/struts/action/ProtocolProtocolActionsAction.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.web.struts.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.irb.web.struts.form.ProtocolForm;
+
+/**
+ * The set of actions for the Protocol Actions tab.
+ */
+public class ProtocolProtocolActionsAction extends ProtocolAction {
+
+    /**
+     * @see org.kuali.kra.web.struts.action.KraTransactionalDocumentActionBase#execute(org.apache.struts.action.ActionMapping, org.apache.struts.action.ActionForm, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+     */
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        ActionForward actionForward = super.execute(mapping, form, request, response);
+        
+        ((ProtocolForm)form).getProtocolHelper().prepareView();
+        ((ProtocolForm)form).getPersonnelHelper().prepareView();
+        ((ProtocolForm)form).getPermissionsHelper().prepareView();
+        
+        return actionForward;
+    }
+    
+    /**
+     * Invoked when the "copy proposal" button is clicked.
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    public ActionForward copyProtocol(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        return mapping.findForward(Constants.MAPPING_BASIC);
+    }
+    
+}
