c3e011dc6949b4f62535a6155b2aa7f4c3abdf2c
Geo Thomas <geot@mit.edu>
2009-03-02 20:38:25 +0000

    KCAWD-95 added missing insert statements for comment_type

--- src/main/config/sql/log/KCAWD-95.sql
+++ src/main/config/sql/log/KCAWD-95.sql
@@ -88,46 +88,55 @@ CREATE TABLE COMMENT_TYPE (
 alter table COMMENT_TYPE ADD CONSTRAINT PK_COMMENT_TYPE PRIMARY KEY(COMMENT_TYPE_CODE) ENABLE;
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(2, 'General Comments', 'Y', 'N', 'Y', sysdate, 'quickstart');
+            values(2, 'General Comments', 'Y', 'N', 'Y', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(4, 'Intellectual Property Comments', 'Y', 'N', 'Y', sysdate, 'quickstart');
+            values(4, 'Intellectual Property Comments', 'Y', 'N', 'Y', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(9, 'Cost Sharing Comments', 'Y', 'N', 'N', sysdate, 'quickstart');
+            values(9, 'Cost Sharing Comments', 'Y', 'N', 'N', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(3, 'Fiscal Report Comments', 'Y', 'N', 'Y', sysdate, 'quickstart');
+            values(3, 'Fiscal Report Comments', 'Y', 'N', 'Y', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(1, 'Invoice Instructions', 'N', 'N', 'N', sysdate, 'quickstart');
+            values(1, 'Invoice Instructions', 'N', 'N', 'N', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(10, 'Special Review Comments', 'N', 'N', 'N', sysdate, 'quickstart');
+            values(10, 'Special Review Comments', 'N', 'N', 'N', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(7, 'Special Rate', 'N', 'N', 'N', sysdate, 'quickstart');
+            values(7, 'Special Rate', 'N', 'N', 'N', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(8, 'Indirect Cost Comments', 'N', 'N', 'N', sysdate, 'quickstart');
+            values(8, 'Indirect Cost Comments', 'N', 'N', 'N', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(5, 'Procurement Comments', 'Y', 'N', 'Y', sysdate, 'quickstart');
+            values(5, 'Procurement Comments', 'Y', 'N', 'Y', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(12, 'Proposal Summary', 'N', 'N', 'N', sysdate, 'quickstart');
+            values(12, 'Proposal Summary', 'N', 'N', 'N', sysdate, user);
             
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(6, 'Property Comments', 'Y', 'N', 'Y', sysdate, 'quickstart');
+            values(6, 'Property Comments', 'Y', 'N', 'Y', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(13, 'Proposal Comments', 'N', 'N', 'N', sysdate, 'quickstart');
+            values(13, 'Proposal Comments', 'N', 'N', 'N', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(16, 'Proposal IP Review Comment', 'N', 'N', 'N', sysdate, 'quickstart');
+            values(16, 'Proposal IP Review Comment', 'N', 'N', 'N', sysdate, user);
 
 insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
-            values(17, 'Proposal IP Review Comment', 'N', 'N', 'N', sysdate, 'quickstart');
+            values(17, 'Proposal IP Review Comment', 'N', 'N', 'N', sysdate, user);
+            
+insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
+            values(18, 'Pre-Award Sponsor Auth Comments ', 'N', 'N', 'N', sysdate, user);
+insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
+            values(19, 'Pre-Award Institutional Auth Comments', 'N', 'N', 'N', sysdate, user);
+
+insert into COMMENT_TYPE (COMMENT_TYPE_CODE,DESCRIPTION,TEMPLATE_FLAG,CHECKLIST_FLAG,AWARD_COMMENT_SCREEN_FLAG,UPDATE_TIMESTAMP,UPDATE_USER)
+            values(20, 'Benefits Rates Comments', 'N', 'N', 'N', sysdate, user);
+            
 commit;
 
 alter table AWARD_COMMENT modify COMMENT_TYPE_CODE VARCHAR2(3);
