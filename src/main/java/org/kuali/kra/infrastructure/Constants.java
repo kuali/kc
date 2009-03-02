3b69fd4ffeb86923aa3ef0937650970c606087cf
Damien Eversmann <deversmann@vivantech.com>
2009-03-02 23:57:55 +0000

    KRACOEUS-2264 : Removed one unused constant and removed an asterisk from the string of another.

--- src/main/java/org/kuali/kra/infrastructure/Constants.java
+++ src/main/java/org/kuali/kra/infrastructure/Constants.java
@@ -116,8 +116,7 @@ public interface Constants {
     public static final String PERSON_EDITABLE_FIELD_NAME_PROPERTY_KEY = "fieldName";
     public static final String INVESTIGATOR_CREDIT_TYPE_CODE_PROPERTY_KEY = "invCreditTypeCode";
     public static final String EMPTY_STRING = "";
-    public static final String PROPOSAL_PERSON_KEY = "document.proposalPerson*";
-    public static final String PRINCIPAL_INVESTIGATOR_KEY = "newProposalPerson*";
+    public static final String PRINCIPAL_INVESTIGATOR_KEY = "newProposalPerson";
     public static final String CREDIT_SPLIT_KEY = "document.creditSplit";
 
    
