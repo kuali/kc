916be4e985119643cec6e5712acdb42e40868ce2
Tyler Wilson <tyler.wilson@colostate.edu>
2009-03-05 22:54:23 +0000

    Bringing S2S base scripts up to date with release 1.1.1

--- src/main/config/sql/s2s/procfunpacks/GetNSFPrevAward.sql
+++ src/main/config/sql/s2s/procfunpacks/GetNSFPrevAward.sql
@@ -1,19 +1,23 @@
-create or replace procedure GetNSFPrevAward (AS_PROPOSAL_NUMBER IN OSP$EPS_PROPOSAL.PROPOSAL_NUMBER%TYPE,
-     cur_generic IN OUT result_sets.cur_generic) IS
+CREATE OR REPLACE PROCEDURE Getnsfprevaward (AS_PROPOSAL_NUMBER IN OSP$EPS_PROPOSAL.PROPOSAL_NUMBER%TYPE,
+     cur_generic IN OUT Result_Sets.cur_generic) IS
 
-BEGIN																
-  OPEN cur_generic for
-	SELECT	DECODE(A.SPONSOR_AWARD_NUMBER,NULL,' ',A.SPONSOR_AWARD_NUMBER) AWARD
-	FROM		OSP$AWARD A, OSP$EPS_PROPOSAL P
-	WHERE		P.PROPOSAL_NUMBER = as_proposal_number
-	AND      P.CURRENT_AWARD_NUMBER = A.MIT_AWARD_NUMBER 
+BEGIN
+  OPEN cur_generic FOR
+ SELECT ' ' AWARD
+ FROM dual;
+  
+ /*
+ ************* There is no AWARD table - Code modified to return ' ' *************
+  
+ SELECT DECODE(A.SPONSOR_AWARD_NUMBER,NULL,' ',A.SPONSOR_AWARD_NUMBER) AWARD
+ FROM  OSP$AWARD A, OSP$EPS_PROPOSAL P
+ WHERE  P.PROPOSAL_NUMBER = as_proposal_number
+ AND      P.CURRENT_AWARD_NUMBER = A.MIT_AWARD_NUMBER
    AND      A.SEQUENCE_NUMBER = (SELECT MAX(SEQUENCE_NUMBER) FROM OSP$AWARD WHERE
-													MIT_AWARD_NUMBER = A.MIT_AWARD_NUMBER);
+             MIT_AWARD_NUMBER = A.MIT_AWARD_NUMBER);
+*/
 
 
-end;
-
+END;
 /
-
-
 
