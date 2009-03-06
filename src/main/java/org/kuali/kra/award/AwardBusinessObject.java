85799684f6ff8a9c14e39006c39a013defc11553
Jack Frosch <jfrosch@objistics.com>
2009-03-06 01:35:44 +0000

    Refactored AwardApprovedEquipment and AwardApprovedForeignTravel to inherit from new class AwardBusinessObject. AwardBusinessObject holds the Award, awardNumber, and sequenceNumber data.

---
+++ src/main/java/org/kuali/kra/award/AwardBusinessObject.java
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.award;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.kra.award.bo.Award;
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+/**
+ * This class handles the legacy sequenceNumber/awardNumber data from Coeus
+ */
+public class AwardBusinessObject extends KraPersistableBusinessObjectBase {
+    private static final long serialVersionUID = -1966175324490120727L;
+    
+    private String awardNumber;
+    private Integer sequenceNumber;
+    private Award award;
+    
+    /**
+     * @return
+     */
+    public String getAwardNumber() {
+        return awardNumber;
+    }
+
+    /**
+     * @return
+     */
+    public Integer getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    /**
+     * @return
+     */
+    public Award getAward() {
+        return award;
+    }
+
+    /**
+     * @param awardNumber
+     */
+    public void setAwardNumber(String awardNumber) {
+        this.awardNumber = awardNumber;
+    }
+
+    /**
+     * @param sequenceNumber
+     */
+    public void setSequenceNumber(Integer sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    /**
+     * @param award
+     */
+    public void setAward(Award award) {
+        this.award = award;
+        if(award != null) {
+            setSequenceNumber(award.getSequenceNumber());
+            setAwardNumber(award.getAwardNumber());
+        } else {
+            setSequenceNumber(0);
+            setAwardNumber("");
+        }
+    }
+
+    /**
+     * @see org.kuali.core.bo.BusinessObjectBase#toStringMapper()
+     */
+    @Override
+    protected LinkedHashMap<String, Object> toStringMapper() {
+        LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("awardNumber", awardNumber);
+        map.put("sequenceNumber", sequenceNumber);
+        map.put("award", award);
+        return map;
+    }
+
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = PRIME * result + ((awardNumber == null) ? 0 : awardNumber.hashCode());
+        result = PRIME * result + ((sequenceNumber == null) ? 0 : sequenceNumber.hashCode());
+        return result;
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof AwardBusinessObject)) {
+            return false;
+        }
+        AwardBusinessObject other = (AwardBusinessObject) obj;
+        if (awardNumber == null) {
+            if (other.awardNumber != null) {
+                return false;
+            }
+        } else if (!awardNumber.equals(other.awardNumber)) {
+            return false;
+        }
+        if (sequenceNumber == null) {
+            if (other.sequenceNumber != null) {
+                return false;
+            }
+        } else if (!sequenceNumber.equals(other.sequenceNumber)) {
+            return false;
+        }
+        return true;
+    }    
+}
