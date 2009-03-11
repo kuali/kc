8da9b560751c82ff25ec8fb4b2e8903d1315842e
Kiltesh Patel <kjpatel@ucdavis.edu>
2009-03-11 23:25:24 +0000

    KCIRB-54
    Web test cases added

---
+++ src/test/resources/sql/dml/load_schedule_status.sql
@@ -0,0 +1,4 @@
+INSERT INTO SCHEDULE_STATUS (schedule_status_code,description,UPDATE_TIMESTAMP,UPDATE_USER) VALUES (1,'Scheduled',sysdate,user);
+INSERT INTO SCHEDULE_STATUS (schedule_status_code,description,UPDATE_TIMESTAMP,UPDATE_USER) VALUES (2,'Cancelled',sysdate,user);
+INSERT INTO SCHEDULE_STATUS (schedule_status_code,description,UPDATE_TIMESTAMP,UPDATE_USER) VALUES (3,'Agenda Closed',sysdate,user);
+INSERT INTO SCHEDULE_STATUS (schedule_status_code,description,UPDATE_TIMESTAMP,UPDATE_USER) VALUES (4,'Committee has met',sysdate,user);
