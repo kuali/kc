a6640d3dc755adbffbfb9c44d0fa086a975999fd
Kiltesh Patel <kjpatel@ucdavis.edu>
2009-03-13 00:43:34 +0000

    KCIRB-54
    Added generic code to test bo's string mapper as well as fields and values passed along.

---
+++ src/test/java/org/kuali/kra/committee/bo/BoAttributeTestBase.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.committee.bo;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+/**
+ * This class is abstract template BO test class could use to test toStringMapper. 
+ * a. It checks for number of attributes used in toStringMapper.
+ * b. It also test for fields and values passed in toStringMapper.
+ */
+public abstract class BoAttributeTestBase<T extends KraPersistableBusinessObjectBase> extends TestCase {
+
+    private int attributeCount;
+
+    private T bo;
+
+    /**
+     * Constructs a BoAttributeTestBase.java.
+     * @param attributeCount is total count of persistent fields in BO used by toStringMapper().
+     * @param bo is instance of BO.
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    public BoAttributeTestBase(int attributeCount, T bo) throws IllegalArgumentException, IllegalAccessException,
+                                                        InvocationTargetException {
+        super();
+        this.attributeCount = attributeCount;
+        this.bo = bo;
+        boPrerequisite();
+        setUpBo();
+        boPostrequisite();
+    }
+
+    /**
+     * Concrete implementer should make a call to getToStringMapper() of BO class returning Map.
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    protected abstract Map getToStringMapper();
+
+    /**
+     * Concrete implementer should return Map<String, Object>, using bo's field and value. Map should include all the fields used in
+     * definition of toStringMapper().
+     * 
+     * @return Map<String, Object> of bo's key & value.
+     */
+    protected abstract Map<String, Object> getFieldMap();
+
+    /**
+     * This method can be used to set BO's post-requisites.
+     */
+    protected void boPrerequisite() {
+    }
+
+    /**
+     * This method can be used to set BO's prerequisites.
+     */
+    protected void boPostrequisite() {
+    }
+
+    /**
+     * This method tests attributes count in BO of toStringMapper.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testBoAttributesCount() throws Exception {
+        Assert.assertEquals(attributeCount, getToStringMapper().size());
+        ;
+    }
+
+    /**
+     * This method tests fields added to Map of toStringMapper in BO
+     * 
+     * @throws Exception
+     */
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testBoFields() throws Exception {
+        Map map = getToStringMapper();
+        Map fieldMap = getFieldMap();
+        Set<String> set = fieldMap.keySet();
+        for (String field : set) {
+            assertEquals(fieldMap.get(field), map.get(field));
+        }
+    }
+
+    /**
+     * This method is accessory of generic T type object.
+     * 
+     * @return
+     */
+    public T getBo() {
+        return bo;
+    }
+
+    /**
+     * This method uses reflection to set values of BO.
+     * 
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    @SuppressWarnings("unchecked")
+    private void setUpBo() throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+        Method[] methods = bo.getClass().getDeclaredMethods();
+        Map map = getFieldMap();
+        for (Method method : methods) {
+            String methodName = method.getName();
+            if (methodName.contains("set")) {
+                methodName = methodName.substring(3);
+                methodName = methodName.substring(0, 1).toLowerCase() + methodName.substring(1);
+                Object value = map.get(methodName);
+                if (null != value) {
+                    Object[] values = new Object[1];
+                    values[0] = value;
+                    method.invoke(bo, values);
+                }
+            }
+        }
+    }
+}
---
+++ src/test/java/org/kuali/kra/committee/bo/CommitteeScheduleTest.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.committee.bo;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.time.DateUtils;
+import org.kuali.kra.committee.web.struts.form.schedule.Time12HrFmt;
+import org.kuali.kra.committee.web.struts.form.schedule.Time12HrFmt.MERIDIEM;
+
+/**
+ * This class is Template implementation of BoAttributeTestBase<T> class, to test CommitteeSchedule BO for toStringMapper.
+ */
+public class CommitteeScheduleTest extends BoAttributeTestBase<CommitteeSchedule> {
+    
+    private static final int ATTRIBUTE_COUNT = 14;
+    
+    private static final Date date = new Date(new java.util.Date().getTime()); 
+    
+    private static final String FIELD_ID = "id";
+    private static final Long FIELD_ID_VALUE = 1l; 
+
+    private static final String FIELD_SCHEDULEID = "scheduleId"; 
+    private static final String FIELD_SCHEDULEID_VALUE = "1"; 
+
+    private static final String FIELD_COMMITTEEID = "committeeId";
+    private static final Long FIELD_COMMITTEEID_VALUE = 1l;
+
+    private static final String FIELD_SCHEDULEDDATE = "scheduledDate";
+    private static final Date FIELD_SCHEDULEDDATE_VALUE = date; 
+
+    private static final String FIELD_PLACE = "place";
+    private static final String FIELD_PLACE_VALUE = "Davis 103";
+
+    private static final String FIELD_TIME = "time";
+    private static Timestamp FIELD_TIME_VALUE;
+
+    private static final String FIELD_PROTOCOLSUBDEADLINE = "protocolSubDeadline";
+    private static final Date FIELD_PROTOCOLSUBDEADLINE_VALUE = date;
+
+    private static final String FIELD_SCHEDULESTATUSCODE = "scheduleStatusCode";
+    private static final Integer FIELD_SCHEDULESTATUSCODE_VALUE = 1;
+
+    private static final String  FIELD_MEETINGDATE = "meetingDate";
+    private static final Date FIELD_MEETINGDATE_VALUE = date;  
+
+    private static final String FIELD_STARTTIME = "startTime";
+    private static final Date FIELD_STARTTIME_VALUE = date; 
+    
+    private static final String FIELD_ENDTIME = "endTime";
+    private static final Date FIELD_ENDTIME_VALUE = date; 
+
+    private static final String FIELD_AGENDAPRODREVDATE = "agendaProdRevDate";
+    private static final Date FIELD_AGENDAPRODREVDATE_VALUE = date;
+
+    private static final String FIELD_MAXPROTOCOLS = "maxProtocols";
+    private static final Integer FIELD_MAXPROTOCOLS_VALUE = 1;
+
+    private static final String FIELD_COMMONTS = "comments";
+    private static final String FIELD_COMMONTS_VALUE = "Some comment to test should go here";
+    
+    private static CommitteeSchedule cm = new CommitteeSchedule();
+
+    /**
+     * Constructs a CommitteeScheduleTest.java.
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    public CommitteeScheduleTest() throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {        
+        super(ATTRIBUTE_COUNT, cm);
+    }
+
+    /**
+     * @see org.kuali.kra.committee.bo.BoAttributeTestBase#getFieldMap()
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    protected Map getFieldMap() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(FIELD_ID, FIELD_ID_VALUE);
+        map.put(FIELD_SCHEDULEID, FIELD_SCHEDULEID_VALUE);
+        map.put(FIELD_COMMITTEEID, FIELD_COMMITTEEID_VALUE);
+        map.put(FIELD_SCHEDULEDDATE, FIELD_SCHEDULEDDATE_VALUE);
+        map.put(FIELD_PLACE, FIELD_PLACE_VALUE);
+        map.put(FIELD_TIME, FIELD_TIME_VALUE);
+        map.put(FIELD_PROTOCOLSUBDEADLINE, FIELD_PROTOCOLSUBDEADLINE_VALUE);
+        map.put(FIELD_SCHEDULESTATUSCODE, FIELD_SCHEDULESTATUSCODE_VALUE);
+        map.put(FIELD_MEETINGDATE, FIELD_MEETINGDATE_VALUE);
+        map.put(FIELD_STARTTIME, FIELD_STARTTIME_VALUE);
+        map.put(FIELD_ENDTIME, FIELD_ENDTIME_VALUE);
+        map.put(FIELD_AGENDAPRODREVDATE, FIELD_AGENDAPRODREVDATE_VALUE);
+        map.put(FIELD_MAXPROTOCOLS, FIELD_MAXPROTOCOLS_VALUE);
+        map.put(FIELD_COMMONTS, FIELD_COMMONTS_VALUE);
+        return map;
+    }
+
+    /**
+     * @see org.kuali.kra.committee.bo.BoAttributeTestBase#getToStringMapper()
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    protected Map<String, Object> getToStringMapper() {
+        return getBo().toStringMapper();
+    }
+    
+    /**
+     * @see org.kuali.kra.committee.bo.BoAttributeTestBase#boPrerequisite()
+     */
+    @Override
+    protected void boPrerequisite(){
+        super.boPrerequisite();
+        Time12HrFmt time12HrFmt = new Time12HrFmt("10:30",MERIDIEM.AM);
+        java.util.Date dt = DateUtils.round(date, Calendar.DAY_OF_MONTH);
+        dt = DateUtils.addMinutes(dt, time12HrFmt.findMinutes());
+        FIELD_TIME_VALUE = new java.sql.Timestamp(dt.getTime());
+    }
+    
+    /**
+     * @see org.kuali.kra.committee.bo.BoAttributeTestBase#boPostrequisite()
+     */
+    @Override
+    protected void boPostrequisite() {
+        super.boPostrequisite();
+        cm.setViewTime(new Time12HrFmt("10:30",MERIDIEM.AM));
+    }
+}
