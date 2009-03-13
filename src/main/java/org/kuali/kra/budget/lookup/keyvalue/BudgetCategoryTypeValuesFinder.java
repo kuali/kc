2dc0628c5272655bf700dae01dc8967489cb7a14
Chitra Chandran <chitra07@msu.edu>
2009-03-13 21:30:50 +0000

    KRACOEUS-2332

--- src/main/java/org/kuali/kra/budget/lookup/keyvalue/BudgetCategoryTypeValuesFinder.java
+++ src/main/java/org/kuali/kra/budget/lookup/keyvalue/BudgetCategoryTypeValuesFinder.java
@@ -22,15 +22,10 @@ import java.util.List;
 
 import org.kuali.core.lookup.keyvalues.KeyValuesBase;
 import org.kuali.core.service.KeyValuesService;
-import org.kuali.core.util.GlobalVariables;
-import org.kuali.core.web.struts.form.KualiForm;
 import org.kuali.core.web.ui.KeyLabelPair;
 import org.kuali.kra.budget.bo.BudgetCategoryType;
-import org.kuali.kra.budget.bo.BudgetLineItem;
-import org.kuali.kra.budget.web.struts.form.BudgetForm;
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.lookup.keyvalue.KeyValueFinderService;
-import org.kuali.kra.proposaldevelopment.bo.ProposalType;
 
 public class BudgetCategoryTypeValuesFinder extends KeyValuesBase {
     KeyValueFinderService keyValueFinderService= (KeyValueFinderService)KraServiceLocator.getService("keyValueFinderService");
