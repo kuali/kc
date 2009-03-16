79d8a674ddd87bc87b9b7c442a4da02170530701
Kiltesh Patel <kjpatel@ucdavis.edu>
2009-03-16 23:10:03 +0000

    KCIRB-54
    Rule: Deadline must be before Schedule date.

---
+++ src/main/java/org/kuali/kra/committee/rules/CommitteeScheduleDeadlineDateRule.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.committee.rules;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.kuali.kra.committee.bo.CommitteeSchedule;
+import org.kuali.kra.committee.rule.DeadlineCommitteeScheduleRule;
+import org.kuali.kra.committee.rule.event.DeadlineCommitteeScheduleEvent;
+import org.kuali.kra.infrastructure.KeyConstants;
+import org.kuali.kra.rules.ResearchDocumentRuleBase;
+
+public class CommitteeScheduleDeadlineDateRule extends ResearchDocumentRuleBase implements  DeadlineCommitteeScheduleRule{
+            
+    public static final String ID = "document.committee.committeeSchedules[%1$s].protocolSubDeadline";
+    
+    /**
+     * @see org.kuali.kra.committee.rule.DeadlineCommitteeScheduleRule#processCommitteeScheduleDeadlineBusinessRules(org.kuali.kra.committee.rule.event.DeadlineCommitteeScheduleEvent)
+     */
+    public boolean processCommitteeScheduleDeadlineBusinessRules(DeadlineCommitteeScheduleEvent deadlineCommitteeScheduleEvent) {
+        
+        boolean rulePassed = true;
+        
+        List<CommitteeSchedule> committeeSchedules = deadlineCommitteeScheduleEvent.getCommitteeSchedules();
+        int count = 0;
+        for(CommitteeSchedule committeeSchedule : committeeSchedules) {
+            
+            Date deadline = committeeSchedule.getProtocolSubDeadline();
+            Date schedule = committeeSchedule.getScheduledDate();
+            
+            if(schedule.before(deadline)) {
+                reportError(String.format(ID, count), KeyConstants.ERROR_COMMITTEESCHEDULE_DEADLINE, deadline.toString(), schedule.toString());
+                rulePassed = false;
+            }
+            count++;
+        }
+        
+        return rulePassed;
+    }
+
+}
