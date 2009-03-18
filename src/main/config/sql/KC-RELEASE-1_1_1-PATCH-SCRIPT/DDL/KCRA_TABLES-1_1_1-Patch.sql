7b06e01272ef05e31eb73b74a732ee23f73304b4
Tyler Wilson <tyler.wilson@colostate.edu>
2009-03-18 06:33:56 +0000

    Database Script changes for Rel 1 Patch 1

--- src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/DDL/KCRA_TABLES-1_1_1-Patch.sql
+++ src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/DDL/KCRA_TABLES-1_1_1-Patch.sql
@@ -105,3 +105,11 @@ MODIFY
    
 -- Add Active Flag to Valid Rate Types.
 alter table VALID_CE_RATE_TYPES add ACTIVE_FLAG varchar2(1) DEFAULT 'Y' NOT NULL;
+
+create table chlength as select deadline_type_code, description, update_timestamp, update_user, ver_nbr, obj_id from deadline_type;
+delete from deadline_type;
+alter table deadline_type modify deadline_type_code char(1);
+insert into deadline_type (deadline_type_code, description, update_timestamp, update_user, ver_nbr, obj_id)
+    select deadline_type_code, description, update_timestamp, update_user, ver_nbr, obj_id from chlength;
+drop table chlength;
+commit;
