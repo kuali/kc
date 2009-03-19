fffe2379e2daadca28a79f8de1ef1e0fea63efb3
Tyler Wilson <tyler.wilson@colostate.edu>
2009-03-19 03:55:33 +0000

    Database Script changes for Rel 2

---
+++ src/main/config/sql/log/KCIRB-195.sql
@@ -0,0 +1,5 @@
+Insert into SH_PARM_T 
+( SH_PARM_NMSPC_CD, SH_PARM_DTL_TYP_CD, SH_PARM_NM, OBJ_ID, VER_NBR, SH_PARM_TYP_CD, SH_PARM_TXT, SH_PARM_DESC, SH_PARM_CONS_CD, WRKGRP_NM, ACTIVE_IND)
+Values
+('KC-PROTOCOL','D','irb.protocol.billable', 'F1C228F9D4D8408A8E0BBC801C9525ak', 1,'CONFG','Y','Billable is configurable at impl time','A','WorkflowAdmin','Y');
+commit;
