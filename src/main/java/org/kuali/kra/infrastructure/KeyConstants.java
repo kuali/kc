e765579cb26ec730275867c5977ab4e56c019eb1
Claus Niesen <cniesen@iastate.edu>
2009-03-27 22:36:36 +0000

    KCIRB-64 Expertise panel of Committee Membership
    Switched add to a multi-select lookup

--- src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
+++ src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
@@ -348,9 +348,7 @@ public final class KeyConstants {
     public static final String ERROR_COMMITTEE_MEMBERSHIP_ROLE_END_DATE_OUTSIDE_TERM = "error.committee.membership.roleEndDate.outside.term";
     public static final String ERROR_COMMITTEE_MEMBERSHIP_ROLE_MISSING = "error.committee.membership.role.missing";
     public static final String ERROR_COMMITTEE_MEMBERSHIP_ROLE_DUPLICATE = "error.committee.membership.role.duplicate";
-    public static final String ERROR_COMMITTEE_MEMBERSHIP_EXPERTISE_NOT_SPECIFIED = "error.committee.membership.expertise.not.specified";
     public static final String ERROR_COMMITTEE_MEMBERSHIP_EXPERTISE_MISSING = "error.committee.membership.expertise.missing";
-    public static final String ERROR_COMMITTEE_MEMBERSHIP_EXPERTISE_DUPLICATE = "error.committee.membership.expertise.duplicate";
 
     
     public static final String SOFT_ERRORS_KEY = "SOFT_ERRORS_KEY";
