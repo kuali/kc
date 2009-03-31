d8f3ae7253278c42ac559dece3311e6eb3459c29
Tyler Wilson <tyler.wilson@colostate.edu>
2009-03-31 04:07:20 +0000

    Database Script changes for Rel 1 Patch 1

--- src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/DML/KCRA_DML-1_1_1-Patch.sql
+++ src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/DML/KCRA_DML-1_1_1-Patch.sql
@@ -120,3 +120,12 @@ insert into osp$parameter(parameter,effective_date,value,update_timestamp,update
 	values('KRA_SCHEDULER_SERVICE_ENABLED',sysdate,'1',sysdate,substr(user,1,8));
 	
 commit;	
+
+insert into school_code(school_code,description,update_timestamp,update_user) values ('1','DUNS',sysdate,'kradev');
+insert into school_code(school_code,description,update_timestamp,update_user) values ('9','DUNS+4',sysdate,'kradev');
+insert into school_code(school_code,description,update_timestamp,update_user) values ('10','DODACC',sysdate,'kradev');
+insert into school_code(school_code,description,update_timestamp,update_user) values ('33','CAGE',sysdate,'kradev');
+
+INSERT INTO FP_DOC_TYPE_T (FDOC_TYP_CD,FDOC_GRP_CD,FDOC_NM,FIN_ELIM_ELGBL_CD,FDOC_TYP_ACTIVE_CD,FDOC_RTNG_RULE_CD,FDOC_AUTOAPRV_DAYS,FDOC_BALANCED_CD,TRN_SCRBBR_OFST_GEN_IND) VALUES ('SCHC','KR','SCHOOL CODE','N','Y','N',0,'N','N');
+
+commit;
