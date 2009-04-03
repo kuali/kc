ded1dc6a2983d90efd0f63da4963d2a7fde12d60
Claus Niesen <cniesen@iastate.edu>
2009-04-03 13:43:40 +0000

    KCIRB-65 Committee Membership Rule Tests for Add function.  Refactored CommitteeRuleTest.java and created the CommitteeRuleTestBase.java to be shared with the various committee tests.

---
+++ src/test/java/org/kuali/kra/committee/rules/CommitteeRuleTestBase.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.committee.rules;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.kuali.core.UserSession;
+import org.kuali.core.service.DocumentService;
+import org.kuali.core.util.ErrorMap;
+import org.kuali.core.util.ErrorMessage;
+import org.kuali.core.util.GlobalVariables;
+import org.kuali.core.util.TypedArrayList;
+import org.kuali.kra.KraTestBase;
+import org.kuali.kra.committee.bo.Committee;
+import org.kuali.kra.committee.document.CommitteeDocument;
+import org.kuali.rice.KNSServiceLocator;
+import org.kuali.rice.test.data.PerSuiteUnitTestData;
+import org.kuali.rice.test.data.UnitTestData;
+import org.kuali.rice.test.data.UnitTestFile;
+
+import edu.iu.uis.eden.exception.WorkflowException;
+
+/**
+ * Base class for Committee business rule tests.
+ */
+@PerSuiteUnitTestData(
+    @UnitTestData(
+        sqlFiles = {
+            @UnitTestFile(filename = "classpath:sql/dml/load_committee_type.sql", delimiter = ";")
+           ,@UnitTestFile(filename = "classpath:sql/dml/load_protocol_review_type.sql", delimiter = ";")
+        }
+    )
+)
+public class CommitteeRuleTestBase extends KraTestBase {
+
+    protected DocumentService documentService = null;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        GlobalVariables.setUserSession(new UserSession("aslusar"));
+        GlobalVariables.setErrorMap(new ErrorMap());
+        GlobalVariables.setAuditErrorMap(new HashMap());
+        documentService = KNSServiceLocator.getDocumentService();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        GlobalVariables.setUserSession(null);
+        GlobalVariables.setErrorMap(null);
+        GlobalVariables.setAuditErrorMap(null);
+        documentService = null;
+        super.tearDown();
+    }
+    
+    /**
+     * Set the required fields for a committee.
+     * @param document
+     */
+    protected void setCommitteeProperties(CommitteeDocument document) {
+        Committee committee = document.getCommittee();
+        document.getDocumentHeader().setDocumentDescription("test");
+        committee.setCommitteeId("888");
+        committee.setCommitteeName("test");
+        committee.setCommitteeTypeCode("1");
+        committee.setHomeUnitNumber("000001");
+        committee.setCommitteeDescription("description");
+        committee.setMaxProtocols(5);
+        committee.setMinimumMembersRequired(4);
+        committee.setAdvancedSubmissionDaysRequired(3);
+        committee.setReviewTypeCode("1");
+        committee.setScheduleDescription("schedule description");
+    }
+    
+    /**
+     * Get a new Committee Document.
+     * 
+     * @return a new Committee Document.
+     * @throws WorkflowException
+     */
+    protected CommitteeDocument getNewCommitteeDocument() throws WorkflowException {
+        return (CommitteeDocument) documentService.getNewDocument("CommitteeDocument");
+    }
+    
+    /**
+     * Assert an error.  The Error Map should have one error with the given
+     * property key and error key.
+     * @param propertyKey
+     * @param errorKey
+     */
+    protected void assertError(String propertyKey, String errorKey) {
+        TypedArrayList errors = GlobalVariables.getErrorMap().getMessages(propertyKey);
+        assertNotNull(errors);
+        assertTrue(errors.size() == 1);
+        
+        ErrorMessage message = (ErrorMessage) errors.get(0);
+        assertNotNull(message);
+        assertEquals(message.getErrorKey(), errorKey);
+    }
+}
