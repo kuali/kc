2fc0291dbc5e3ca6c278d5ea865171975e213ebb
Tyler Wilson <tyler.wilson@colostate.edu>
2009-04-07 16:51:07 +0000

    Database Script changes for Rel 1 Patch 1

--- src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/S2S/PROCFUNPACKS/All_ProcFunPacks.sql
+++ src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/S2S/PROCFUNPACKS/All_ProcFunPacks.sql
@@ -2,3 +2,4 @@
 @get_indsrl_salary_summary.sql
 @get_salary_summary.sql
 @s2sED524V11Pkg.sql
+@get_s2s_sub_attachment_data.sql
---
+++ src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/S2S/TYPES/All_Types.sql
@@ -0,0 +1,2 @@
+@type_pers_sal_summary.sql
+@type_table_pers_sal_summary.sql
