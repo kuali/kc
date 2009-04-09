e527e9d338296a67798b328ded3967bf247a3d02
Kiltesh Patel <kjpatel@ucdavis.edu>
2009-04-09 22:11:27 +0000

    KCIRB-197
    KCIRB-166
    
    Don has 47 tickets tied to this.

---
+++ src/main/java/org/kuali/kra/irb/bo/ProtocolAction.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.bo;
+
+import java.sql.Date;
+import java.util.LinkedHashMap;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+@Entity 
+@Table(name="PROTOCOL_ACTIONS")
+public class ProtocolAction extends KraPersistableBusinessObjectBase { 
+
+    private static final long serialVersionUID = -2148599171919464303L;
+
+    @Id 
+    @Column(name="ACTION_ID")
+    private Integer actionId; 
+
+    @Column(name="PROTOCOL_NUMBER")
+    private String protocolNumber; 
+
+    @Column(name="SEQUENCE_NUMBER")
+    private Integer sequenceNumber; 
+
+    @Column(name="COMMENTS")
+    private String comments; 
+
+    @Column(name="ACTION_DATE")
+    private Date actionDate; 
+
+    
+    @ManyToOne(fetch=FetchType.EAGER, cascade={CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name="PROTOCOL_ID", insertable=false, updatable=false)
+    private Protocol protocol;
+    
+    @ManyToOne(fetch=FetchType.EAGER, cascade={CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name="SUBMISSION_NUMBER", insertable=false, updatable=false)
+    private ProtocolSubmission protocolSubmission;    
+    
+    public ProtocolAction() { 
+
+    } 
+    
+    public Integer getActionId() {
+        return actionId;
+    }
+
+    public void setActionId(Integer actionId) {
+        this.actionId = actionId;
+    }
+
+    public String getProtocolNumber() {
+        return protocolNumber;
+    }
+
+    public void setProtocolNumber(String protocolNumber) {
+        this.protocolNumber = protocolNumber;
+    }
+
+    public Integer getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public void setSequenceNumber(Integer sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public Date getActionDate() {
+        return actionDate;
+    }
+
+    public void setActionDate(Date actionDate) {
+        this.actionDate = actionDate;
+    }
+
+    public Protocol getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(Protocol protocol) {
+        this.protocol = protocol;
+    }
+
+
+    public void setProtocolSubmission(ProtocolSubmission protocolSubmission) {
+        this.protocolSubmission = protocolSubmission;
+    }
+
+    public ProtocolSubmission getProtocolSubmission() {
+        return protocolSubmission;
+    }
+
+    /** {@inheritDoc} */
+    @Override 
+    protected LinkedHashMap<String, Object> toStringMapper() {
+        LinkedHashMap<String, Object> hashMap = new LinkedHashMap<String, Object>();
+        hashMap.put("actionId", this.getActionId());
+        hashMap.put("protocolNumber", this.getProtocolNumber());
+        hashMap.put("sequenceNumber", this.getSequenceNumber());
+        hashMap.put("comments", this.getComments());
+        hashMap.put("actionDate", this.getActionDate());
+        return hashMap;
+    }
+    
+}
---
+++ src/main/java/org/kuali/kra/irb/bo/ProtocolCorrespondence.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.bo;
+
+import java.util.LinkedHashMap;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+@Entity 
+@Table(name="PROTOCOL_CORRESPONDENCE")
+public class ProtocolCorrespondence extends KraPersistableBusinessObjectBase { 
+
+    private static final long serialVersionUID = 8032222937155468412L;
+
+    @Id 
+    @Column(name="ID")
+    private Integer id; 
+
+    @Column(name="PROTOCOL_NUMBER")
+    private String protocolNumber; 
+
+    @Column(name="SEQUENCE_NUMBER")
+    private Integer sequenceNumber; 
+
+    @Column(name="CORRESPONDENCE")
+    private byte[] correspondence; 
+    
+    @ManyToOne(fetch=FetchType.EAGER, cascade={CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name="PROTOCOL_ID", insertable=false, updatable=false)
+    private Protocol protocol;
+    
+    @ManyToOne(fetch=FetchType.EAGER, cascade={CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name="PROTO_CORRESP_TYPE_CODE", insertable=false, updatable=false)
+    private ProtocolCorrespondenceType protocolCorrespondenceType;
+    
+    @ManyToOne(fetch=FetchType.EAGER, cascade={CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinColumn(name="ACTION_ID", insertable=false, updatable=false)
+    private ProtocolAction protocolAction;
+    
+    public ProtocolCorrespondence() { 
+
+    } 
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProtocolNumber() {
+        return protocolNumber;
+    }
+
+    public void setProtocolNumber(String protocolNumber) {
+        this.protocolNumber = protocolNumber;
+    }
+
+    public Integer getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public void setSequenceNumber(Integer sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    public byte[] getCorrespondence() {
+        return correspondence;
+    }
+
+    public void setCorrespondence(byte[] correspondence) {
+        this.correspondence = correspondence;
+    }
+
+    public Protocol getProtocol() {
+        return protocol;
+    }
+
+    public void setProtocol(Protocol protocol) {
+        this.protocol = protocol;
+    }
+
+    public ProtocolAction getProtocolAction() {
+        return protocolAction;
+    }
+
+    public void setProtocolAction(ProtocolAction protocolAction) {
+        this.protocolAction = protocolAction;
+    }
+
+    public void setProtocolCorrespondenceType(ProtocolCorrespondenceType protocolCorrespondenceType) {
+        this.protocolCorrespondenceType = protocolCorrespondenceType;
+    }
+
+    public ProtocolCorrespondenceType getProtocolCorrespondenceType() {
+        return protocolCorrespondenceType;
+    }
+
+    /** {@inheritDoc} */
+    @Override 
+    protected LinkedHashMap<String, Object> toStringMapper() {
+        LinkedHashMap<String, Object> hashMap = new LinkedHashMap<String, Object>();
+        hashMap.put("id", this.getId());
+        hashMap.put("protocolNumber", this.getProtocolNumber());
+        hashMap.put("sequenceNumber", this.getSequenceNumber());
+        hashMap.put("correspondence", this.getCorrespondence());
+        return hashMap;
+    }
+    
+}
