4a2ad1ccc26218bf1fb45db4c093e4d40641f76d
Tyler Wilson <tyler.wilson@colostate.edu>
2009-04-15 18:38:52 +0000

    Prepped SQL scripts for 1.1.1 Release.

--- src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/KCRA-Release-1_1-1_1_1-Patch.sql
+++ src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/KCRA-Release-1_1-1_1_1-Patch.sql
@@ -7,7 +7,7 @@ set feedback off;
 set term off;
 set heading off;
 
-spool install_kcra_release-1_0_1-Patch.log;
+spool install_kcra_release-1_1_1-Patch.log;
 
 set term on;
 select '*************************** START INSTALLING KRA DATABASE OBJECTS *************************************************' from dual;
@@ -17,22 +17,22 @@ set term on;
 select '*************************** START CREATING KRA TABLES *************************************************' from dual;
 set term off;
 
-@DDL/KCRA_TABLES-1_0_1-Patch
+@DDL/KCRA_TABLES-1_1_1-Patch
 
-@TYPES/KCRA_TYPES-1_0_1-Patch
+@TYPES/KCRA_TYPES-1_1_1-Patch
 
 set term on;
 select '*************************** START CREATING SEQUENCES *************************************************' from dual;
 set term off;
 
-@SEQUENCES/KCRA_SEQUENCES-1_0_1-Patch
+@SEQUENCES/KCRA_SEQUENCES-1_1_1-Patch
 
 set term on;
 select '*************************** START LOADING BOOTSTRAP DATA *************************************************' from dual;
 set term off;
 set define ~;
 
-@DML/KCRA_DML-1_0_1-Patch
+@DML/KCRA_DML-1_1_1-Patch
 
 COMMIT;
 
@@ -40,23 +40,21 @@ set term on;
 select '*************************** START CREATING CONSTRAINTS *************************************************' from dual;
 set term off;
 
-@CONSTRAINTS/KCRA_PRIMARY_KEYS-1_0_1-Patch
-@CONSTRAINTS/KCRA_FOREIGN_KEYS-1_0_1-Patch
+@CONSTRAINTS/KCRA_PRIMARY_KEYS-1_1_1-Patch
+@CONSTRAINTS/KCRA_FOREIGN_KEYS-1_1_1-Patch
 
 set term on;
 select '*************************** START CREATING INDEXES *************************************************' from dual;
 set term off;
 
-REM @INDEXES/KCRA_INDEXES-1_0_1-Patch
+REM @INDEXES/KCRA_INDEXES-1_1_1-Patch
 
 set term on;
 select '*************************** START LOADING S2S OBJECTS *************************************************' from dual;
 set term off;
 @S2S\PROCFUNPACKS\All_ProcFunPacks.sql
 @S2S\VIEWS\All_Views.sql
-exec recompile;
-exec recompile;
-exec recompile;
+exec DBMS_UTILITY.COMPILE_SCHEMA(USER);
 
 set term on;
 select '*************************** END KRA DATABASE UPDATE *************************************************' from dual;
--- src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/S2S/TYPES/All_Types.sql
+++ src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/S2S/TYPES/All_Types.sql
@@ -1,2 +1,2 @@
-@type_pers_sal_summary.sql
-@type_table_pers_sal_summary.sql
+@S2S/TYPES/type_pers_sal_summary.sql
+@S2S/TYPES/type_table_pers_sal_summary.sql
