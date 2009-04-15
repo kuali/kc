82b52006300cdc337449ca8498fcf0cc3c4303c0
Tyler Wilson <tyler.wilson@colostate.edu>
2009-04-15 18:38:52 +0000

    Prepped SQL scripts for 1.1.1 Release.

--- src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/S2S/PROCFUNPACKS/All_ProcFunPacks.sql
+++ src/main/config/sql/KC-RELEASE-1_1_1-PATCH-SCRIPT/S2S/PROCFUNPACKS/All_ProcFunPacks.sql
@@ -1,5 +1,5 @@
-@get_cum_salary_summary.sql
-@get_indsrl_salary_summary.sql
-@get_salary_summary.sql
-@s2sED524V11Pkg.sql
-@get_s2s_sub_attachment_data.sql
+@S2S/PROCFUNPACKS/get_cum_salary_summary.sql
+@S2S/PROCFUNPACKS/get_indsrl_salary_summary.sql
+@S2S/PROCFUNPACKS/get_salary_summary.sql
+@S2S/PROCFUNPACKS/s2sED524V11Pkg.sql
+@S2S/PROCFUNPACKS/get_s2s_sub_attachment_data.sql
