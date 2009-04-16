3f6f58e3d43a242fb6868c4c1ce5fa15b9500679
Kiltesh Patel <kjpatel@ucdavis.edu>
2009-04-16 22:35:07 +0000

    KCIRB-306
    KCIRB-54

--- src/main/java/org/kuali/kra/scheduling/sequence/ScheduleSequenceDecorator.java
+++ src/main/java/org/kuali/kra/scheduling/sequence/ScheduleSequenceDecorator.java
@@ -17,20 +17,7 @@ package org.kuali.kra.scheduling.sequence;
 
 
 /**
- * This class is variation of DefaultSchduleSequence, it adds logic wrapper to dates returned by Quartz util.
- * <p>
- * Logic wrapper filters dates, to generate schedule for every week, every other week, etc ...
- * <p>
- * This implementation is very specifically addressed to deal with week, biweekly, etc date filtering requirements. Implementation
- * uses org.quartz.TriggerUtils class to generate List of dates from Cron expression.
- * <p>
- * It uses current time zone, where application is hosted.
- * <p>
- * Note: Dates used in generating schedule must be wrapped with required time accuracy.
- * e.g.  Start Date: 02/01/09 10:10 End Date: 02/05/09 10:10 
- * Generated Dates will be in between 02/01/09 10:10 and 02/05/09 10:10.
- * Any date expected before 02/01/09 10:09 will be ignored. Date 02/01/09 10:00 will be ignored.
- * Any date expected after  02/05/09 10:11 will be ignored. Date 02/05/09 12:00 will be ignored.
+ * This class implements decorator pattern for SheduleSequence.
  */
 public abstract class ScheduleSequenceDecorator  implements ScheduleSequence {
     
