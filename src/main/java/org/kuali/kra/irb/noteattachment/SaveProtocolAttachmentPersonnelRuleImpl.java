3abd32f2ca6b18acf91d9b93c7dfdb542a0d6ec1
Travis Schneeberger <leo.herbie@gmail.com>
2009-04-20 19:54:45 +0000

    KCIRB-204 & KCIRB-205
    getting audit error to display

--- src/main/java/org/kuali/kra/irb/noteattachment/SaveProtocolAttachmentPersonnelRuleImpl.java
+++ src/main/java/org/kuali/kra/irb/noteattachment/SaveProtocolAttachmentPersonnelRuleImpl.java
@@ -46,11 +46,7 @@ public class SaveProtocolAttachmentPersonnelRuleImpl {
         for (int i = 0; i < attachments.size(); i++) {
             final ProtocolAttachmentPersonnel attachment = attachments.get(i);
             this.setPropertyPrefixes(NoteAndAttachmentPrefix.ATTACHMENT_PERSONNEL.getIndexedPrefix(i));
-            
-            /*
-             * may want to consider moving this series of validations to a single method since the same is done on add
-             * in AddProtocolAttachmentPersonnelRuleImpl
-             */
+
             valid &= this.baseHelper.validDescription(attachment);
             valid &= this.personnelHelper.duplicateTypePerson(attachment, document);
         }
