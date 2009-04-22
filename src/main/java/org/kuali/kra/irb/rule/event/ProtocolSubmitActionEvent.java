47615d84acbea5aa102f40635462bc37aff97b46
Don Barre <dbarre@arizona.edu>
2009-04-22 17:28:23 +0000

    KCIRB-246 : Submit for Review Business Rules

---
+++ src/main/java/org/kuali/kra/irb/rule/event/ProtocolSubmitActionEvent.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.rule.event;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.core.rule.BusinessRule;
+import org.kuali.kra.irb.document.ProtocolDocument;
+import org.kuali.kra.irb.rule.ExecuteProtocolActionRule;
+import org.kuali.kra.irb.web.struts.bean.ProtocolSubmitAction;
+import org.kuali.kra.rule.event.KraDocumentEventBase;
+
+public class ProtocolSubmitActionEvent  extends KraDocumentEventBase {
+    
+    private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(ProtocolSubmitActionEvent.class);
+    
+    private ProtocolSubmitAction submitAction;
+    
+    public ProtocolSubmitActionEvent(ProtocolDocument document, ProtocolSubmitAction submitAction) {
+        super("Submitting for review for document " + getDocumentId(document), "", document);
+        this.submitAction = submitAction;
+        logEvent();
+    }
+
+    @Override
+    protected void logEvent() {
+        StringBuffer logMessage = new StringBuffer(StringUtils.substringAfterLast(this.getClass().getName(), "."));
+        logMessage.append(" with ");
+
+        // vary logging detail as needed
+        if (this.submitAction == null) {
+            logMessage.append("null submitAction");
+        }
+        else {
+            logMessage.append(this.submitAction.toString());
+        }
+
+        LOG.debug(logMessage);
+    }
+
+    public Class getRuleInterfaceClass() {
+        return ExecuteProtocolActionRule.class;
+    }
+
+    public boolean invokeRuleMethod(BusinessRule rule) {
+        return ((ExecuteProtocolActionRule) rule).processSubmitAction((ProtocolDocument) getDocument(), submitAction);
+    }
+}
