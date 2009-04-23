598b1e5356398ef95d8c226f7224958332a65944
Don Barre <dbarre@arizona.edu>
2009-04-23 23:41:04 +0000

    KCIRB-246 : Submit for Review Business Rules

--- src/main/java/org/kuali/kra/irb/rules/ProtocolActionRule.java
+++ src/main/java/org/kuali/kra/irb/rules/ProtocolActionRule.java
@@ -16,6 +16,7 @@
 package org.kuali.kra.irb.rules;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
@@ -26,9 +27,11 @@ import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.irb.bo.ProtocolReviewType;
+import org.kuali.kra.irb.bo.ProtocolReviewerType;
 import org.kuali.kra.irb.bo.ProtocolSubmissionType;
 import org.kuali.kra.irb.document.ProtocolDocument;
 import org.kuali.kra.irb.rule.ExecuteProtocolActionRule;
+import org.kuali.kra.irb.web.struts.bean.ProtocolReviewerBean;
 import org.kuali.kra.irb.web.struts.bean.ProtocolSubmitAction;
 import org.kuali.kra.rules.ResearchDocumentRuleBase;
 
@@ -65,15 +68,58 @@ public class ProtocolActionRule extends ResearchDocumentRuleBase implements Exec
                              KeyConstants.ERROR_PROTOCOL_REVIEW_TYPE_INVALID, new String[] { protocolReviewTypeCode });
         }
         
+        List<ProtocolReviewerBean> reviewers = submitAction.getReviewers();
+        for (int i=0; i<reviewers.size(); i++) {
+            ProtocolReviewerBean reviewer = reviewers.get(i);
+            if (!isReviewerValid(reviewer, i)) {
+                isValid = false;
+            }
+        }
+        
         return isValid;
     }
 
+    /**
+     * This method tests if the fields for a given reviewer have legal values.
+     * @param reviewer
+     * @param reviewerIndex - the index of the reviewer in the list of reviewers that was sent to the client
+     */
+    private boolean isReviewerValid(ProtocolReviewerBean reviewer, int reviewerIndex) {
+        boolean isValid = true;
+        String reviewerTypeCode = reviewer.getReviewerTypeCode();
+        boolean isChecked = reviewer.getChecked();
+        
+        // test if type code is valid
+        if (!StringUtils.isBlank(reviewerTypeCode) && isReviewerTypeInvalid(reviewerTypeCode)) {
+            isValid = false;
+            this.reportError(Constants.PROTOCOL_SUBMIT_ACTION_PROPERTY_KEY + ".reviewer[" + reviewerIndex + "].reviewerTypeCode", KeyConstants.ERROR_PROTOCOL_REVIEWER_TYPE_INVALID, reviewer.getFullName());
+        }
+        
+        // if reviewer checked and type code empty, report an error
+        if (isChecked && StringUtils.isBlank(reviewerTypeCode)) {
+            isValid = false;
+            this.reportError(Constants.PROTOCOL_SUBMIT_ACTION_PROPERTY_KEY + ".reviewer[" + reviewerIndex + "].reviewerTypeCode", KeyConstants.ERROR_PROTOCOL_REVIEWER_NO_TYPE_BUT_REVIEWER_CHECKED, reviewer.getFullName());
+        }
+        
+        // if reviewer unchecked and type code not empty, report an error
+        if (!isChecked && !StringUtils.isBlank(reviewerTypeCode)) {
+            isValid = false;
+            this.reportError(Constants.PROTOCOL_SUBMIT_ACTION_PROPERTY_KEY + ".reviewer[" + reviewerIndex + "].reviewerTypeCode", KeyConstants.ERROR_PROTOCOL_REVIEWER_NOT_CHECKED_BUT_TYPE_SELECTED, reviewer.getFullName());
+        }
+        
+        return isValid;
+    }
+    
     private boolean isSubmissionTypeInvalid(String submissionTypeCode) {
         return !existsUnique(ProtocolSubmissionType.class, "submissionTypeCode", submissionTypeCode);
     }
     
     private boolean isReviewTypeInvalid(String reviewTypeCode) {
         return !existsUnique(ProtocolReviewType.class, "reviewTypeCode", reviewTypeCode);
+    }
+    
+    private boolean isReviewerTypeInvalid(String reviewerTypeCode) {
+        return !existsUnique(ProtocolReviewerType.class, "reviewerTypeCode", reviewerTypeCode);
     }
     
     /**
