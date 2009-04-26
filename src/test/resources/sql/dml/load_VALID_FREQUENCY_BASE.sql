5e530194b03b1ff8d181b3cb610d1e55cb87135e
Geoff McGregor <gmcgrego@iu.edu>
2009-04-26 20:17:01 +0000

    Rice 1.1 upgrade

--- src/test/resources/sql/dml/load_VALID_FREQUENCY_BASE.sql
+++ src/test/resources/sql/dml/load_VALID_FREQUENCY_BASE.sql
@@ -1,18 +1,3 @@
-/*
- Copyright 2006-2008 The Kuali Foundation
-
- Licensed under the Educational Community License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.osedu.org/licenses/ECL-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
 INSERT INTO VALID_FREQUENCY_BASE ( VALID_FREQUENCY_BASE_ID, FREQUENCY_CODE, FREQUENCY_BASE_CODE, UPDATE_TIMESTAMP, UPDATE_USER ) 
 VALUES ( 1, '2', '1', sysdate, user ); 
 INSERT INTO VALID_FREQUENCY_BASE ( VALID_FREQUENCY_BASE_ID, FREQUENCY_CODE, FREQUENCY_BASE_CODE, UPDATE_TIMESTAMP, UPDATE_USER ) 
@@ -123,8 +108,8 @@ INSERT INTO VALID_FREQUENCY_BASE ( VALID_FREQUENCY_BASE_ID, FREQUENCY_CODE, FREQ
 VALUES ( 54, '29', '4', sysdate, user ); 
 INSERT INTO VALID_FREQUENCY_BASE ( VALID_FREQUENCY_BASE_ID, FREQUENCY_CODE, FREQUENCY_BASE_CODE, UPDATE_TIMESTAMP, UPDATE_USER ) 
 VALUES ( 55, '30', '3', sysdate, user ); 
-INSERT INTO VALID_FREQUENCY_BASE ( VALID_FREQUENCY_BASE_ID, FREQUENCY_CODE, FREQUENCY_BASE_CODE, UPDATE_TIMESTAMP, UPDATE_USER ) 
-VALUES ( 55, '30', '4', sysdate, user ); 
+--INSERT INTO VALID_FREQUENCY_BASE ( VALID_FREQUENCY_BASE_ID, FREQUENCY_CODE, FREQUENCY_BASE_CODE, UPDATE_TIMESTAMP, UPDATE_USER ) 
+--VALUES ( 55, '30', '4', sysdate, user ); 
 INSERT INTO VALID_FREQUENCY_BASE ( VALID_FREQUENCY_BASE_ID, FREQUENCY_CODE, FREQUENCY_BASE_CODE, UPDATE_TIMESTAMP, UPDATE_USER ) 
 VALUES ( 56, '31', 1, sysdate, user ); 
 INSERT INTO VALID_FREQUENCY_BASE ( VALID_FREQUENCY_BASE_ID, FREQUENCY_CODE, FREQUENCY_BASE_CODE, UPDATE_TIMESTAMP, UPDATE_USER ) 
