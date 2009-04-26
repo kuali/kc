2c359d11e0b897537f07d4cfca86a51f36b2d279
Geoff McGregor <gmcgrego@iu.edu>
2009-04-26 20:07:28 +0000

    Rice 1.1 upgrade

---
+++ src/main/webapp/kew/scripts/applicationconstants-common.js
@@ -0,0 +1,6 @@
+// ApplicationConstants.jsp
+function clearForm() {
+  dForm = document.forms[0];
+  dForm.elements["constant.applicationConstantName"].value="";
+  dForm.elements["constant.applicationConstantValue"].value="";
+}
---
+++ src/main/webapp/kew/scripts/backdoor-common.js
@@ -0,0 +1,22 @@
+// index.jsp
+function openBrWindow(theURL,winName,features) {
+  window.open(theURL,winName,features);
+}
+
+// SampleRoleClient.jsp
+function post_to_action(action, methodToCall) {
+  document.forms[0].action = action;
+  document.forms[0].methodToCall.value = methodToCall;
+  document.forms[0].submit();
+  return;
+}
+function clearValues(methodToCall, formname, action) {
+  document.forms[formname].methodToCall.value = methodToCall;
+  return post_to_action(formname, action);
+}
+
+function quick_finder(quickFinderLookupable, formname, action) {
+  document.forms[formname].methodToCall.value = 'storeForm';
+  document.forms[formname].quickFinderLookupable.value = quickFinderLookupable;
+  return post_to_action(formname, action);
+}
