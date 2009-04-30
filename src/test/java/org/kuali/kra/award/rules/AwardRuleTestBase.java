4248161a3381d8cecca839c36756a754452fdcc0
Varun Soni <vls45@cornell.edu>
2009-04-30 19:38:17 +0000

    KCAWD-114 - Award Permissions - code review refactorings

--- src/test/java/org/kuali/kra/award/rules/AwardRuleTestBase.java
+++ src/test/java/org/kuali/kra/award/rules/AwardRuleTestBase.java
@@ -21,14 +21,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.kuali.kra.KraTestBase;
 import org.kuali.kra.award.document.AwardDocument;
-import org.kuali.kra.irb.bo.Protocol;
-import org.kuali.kra.irb.bo.ProtocolUnit;
-import org.kuali.kra.irb.document.ProtocolDocument;
-import org.kuali.kra.irb.personnel.ProtocolPerson;
+import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.rice.kew.exception.WorkflowException;
 import org.kuali.rice.kns.UserSession;
 import org.kuali.rice.kns.service.DocumentService;
-import org.kuali.rice.kns.service.KNSServiceLocator;
 import org.kuali.rice.kns.util.ErrorMap;
 import org.kuali.rice.kns.util.ErrorMessage;
 import org.kuali.rice.kns.util.GlobalVariables;
@@ -39,19 +35,8 @@ import org.kuali.rice.kns.util.TypedArrayList;
  * 
  */
 public abstract class AwardRuleTestBase extends KraTestBase {
-
-    protected DocumentService documentService = null;
-    protected static final String DEFAULT_DOCUMENT_DESCRIPTION = "Award Document";
-    protected static final String AWARD_STATUS_STR = "100"; //test of option "Pending/In Progress";
-    protected static final String AWARD_TYPE_CODE_STR = "1";//test of option "Standard";
-    protected static final String AWARD_TITLE_STR = "New protocol test";
-    protected static final String PRINCIPAL_INVESTIGATOR_ID = "000000001";
-    protected static final String PRINCIPAL_INVESTIGATOR_NAME = "Terry Durkin";
-    protected static final String PRINCIPAL_INVESTIGATOR_UNIT = "BL-BL";
-    protected static final String PRINCIPAL_INVESTIGATOR_ROLE = "PI";
-    protected static final String REFERENCE_PERSON_ROLE = "protocolPersonRole";
-    protected static final String REFERENCE_UNIT = "unit";
     
+    protected DocumentService documentService = null;
 
     @Before
     public void setUp() throws Exception {
@@ -59,7 +44,7 @@ public abstract class AwardRuleTestBase extends KraTestBase {
         GlobalVariables.setUserSession(new UserSession("quickstart"));
         GlobalVariables.setErrorMap(new ErrorMap());
         GlobalVariables.setAuditErrorMap(new HashMap());
-        documentService = KNSServiceLocator.getDocumentService();
+        documentService = KraServiceLocator.getService(DocumentService.class);
     }
 
     @After
@@ -72,68 +57,15 @@ public abstract class AwardRuleTestBase extends KraTestBase {
     }
     
     /**
-     * Get a new Protocol Document.
+     * Get a new Award Document.
      * 
-     * @return a new Protocol Document.
+     * @return a new Award Document.
      * @throws WorkflowException
      */
     protected AwardDocument getNewAwardDocument() throws WorkflowException {
         return (AwardDocument) documentService.getNewDocument("AwardDocument");
     }
-
-    /**
-     * This method is to set required fields for Protocol document
-     * @param document
-     */
-    protected void setProtocolRequiredFields(ProtocolDocument document) {
-        Protocol protocol = document.getProtocol();
-        document.getDocumentHeader().setDocumentDescription(DEFAULT_DOCUMENT_DESCRIPTION);
-        protocol.setProtocolTypeCode(AWARD_TYPE_CODE_STR);
-        protocol.setTitle(AWARD_TITLE_STR);
-        
-        ProtocolPerson protocolPerson = getProtocolPerson(PRINCIPAL_INVESTIGATOR_ID, PRINCIPAL_INVESTIGATOR_NAME, PRINCIPAL_INVESTIGATOR_ROLE);
-        
-        ProtocolUnit protocolUnit = new ProtocolUnit();
-        protocolUnit.setUnitNumber(PRINCIPAL_INVESTIGATOR_UNIT);
-        protocolUnit.setLeadUnitFlag(true);
-        protocolUnit.setProtocolNumber("0");
-        protocolUnit.setSequenceNumber(0);
-        protocolUnit.refreshReferenceObject(REFERENCE_UNIT);
-
-        protocol.setLeadUnitForValidation(protocolUnit);
-        protocolPerson.getProtocolUnits().add(protocolUnit);
-        
-        protocol.getProtocolPersons().add(protocolPerson);
-        protocol.setLeadUnitNumber(PRINCIPAL_INVESTIGATOR_UNIT);
-        protocol.setPrincipalInvestigatorId(PRINCIPAL_INVESTIGATOR_ID);
-    }
     
-    /**
-     * This method is to get protocol person details
-     * @param personId
-     * @param personName
-     * @param personRole
-     * @return
-     */
-    protected ProtocolPerson getProtocolPerson(String personId, String personName, String personRole) {
-        ProtocolPerson protocolPerson = new ProtocolPerson();
-        protocolPerson.setPersonId(personId);
-        protocolPerson.setPersonName(personName);
-        protocolPerson.setProtocolPersonRoleId(personRole);
-        protocolPerson.setPreviousPersonRoleId(personRole);
-        protocolPerson.setProtocolNumber("0");
-        protocolPerson.setSequenceNumber(0);
-        protocolPerson.refreshReferenceObject(REFERENCE_PERSON_ROLE);
-        return protocolPerson;
-    }
-    
-    /**
-     * This method is to get protocol person with role PI
-     * @return
-     */
-    protected ProtocolPerson getPrincipalInvestigator() {
-        return getProtocolPerson(PRINCIPAL_INVESTIGATOR_ID, PRINCIPAL_INVESTIGATOR_NAME, PRINCIPAL_INVESTIGATOR_ROLE);
-    }
 
     /**
      * Assert an error.  The Error Map should have one error with the given
