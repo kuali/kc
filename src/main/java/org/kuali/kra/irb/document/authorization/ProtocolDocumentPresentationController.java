ecbf2c66b25641dedcfa15efcc14801fd0b69e71
Lin-Long Shyu <lshyukuali@gmail.com>
2009-05-01 02:02:49 +0000

    rice-upgrade

---
+++ src/main/java/org/kuali/kra/irb/document/authorization/ProtocolDocumentPresentationController.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.document.authorization;
+
+import org.kuali.kra.authorization.ApplicationTask;
+import org.kuali.kra.infrastructure.KeyConstants;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.infrastructure.TaskName;
+import org.kuali.kra.rice.shim.DocumentInitiationAuthorizationException;
+import org.kuali.kra.rice.shim.UniversalUser;
+import org.kuali.kra.service.TaskAuthorizationService;
+import org.kuali.rice.kns.document.authorization.TransactionalDocumentPresentationControllerBase;
+import org.kuali.rice.kns.util.GlobalVariables;
+
+public class ProtocolDocumentPresentationController extends TransactionalDocumentPresentationControllerBase {
+
+    @Override
+    public boolean canInitiate(String documentTypeName) {
+       // super.canInitiate(documentTypeName);
+        UniversalUser user = new UniversalUser(GlobalVariables.getUserSession().getPerson());
+        if (!canCreateProtocol(user)) {
+            throw new DocumentInitiationAuthorizationException(KeyConstants.ERROR_AUTHORIZATION_DOCUMENT_INITIATION, 
+                                                               new String[] { user.getPersonUserIdentifier(), documentTypeName });
+        }
+        return true;
+    }
+    
+    /**
+     * Does the user have permission to create a protocol?
+     * @param user the user
+     * @return true if the user can create a protocol; otherwise false
+     */
+    private boolean canCreateProtocol(UniversalUser user) {
+        String username = user.getPersonUserIdentifier();
+        ApplicationTask task = new ApplicationTask(TaskName.CREATE_PROTOCOL);       
+        TaskAuthorizationService taskAuthenticationService = KraServiceLocator.getService(TaskAuthorizationService.class);
+        return taskAuthenticationService.isAuthorized(username, task);
+    }
+
+}
