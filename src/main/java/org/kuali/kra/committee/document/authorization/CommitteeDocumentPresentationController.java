a2353a7f2093870cc25d05b4f56061b7319b2e45
Lin-Long Shyu <lshyukuali@gmail.com>
2009-05-03 14:30:59 +0000

    krafdbck-2206 : move caninitiate to presentationcontroller

--- src/main/java/org/kuali/kra/committee/document/authorization/CommitteeDocumentPresentationController.java
+++ src/main/java/org/kuali/kra/committee/document/authorization/CommitteeDocumentPresentationController.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.proposaldevelopment.document.authorization;
+package org.kuali.kra.committee.document.authorization;
 
 import org.kuali.kra.authorization.ApplicationTask;
 import org.kuali.kra.infrastructure.KeyConstants;
@@ -21,40 +21,30 @@ import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.infrastructure.TaskName;
 import org.kuali.kra.rice.shim.DocumentInitiationAuthorizationException;
 import org.kuali.kra.rice.shim.UniversalUser;
-import org.kuali.kra.service.KraWorkflowService;
 import org.kuali.kra.service.TaskAuthorizationService;
-import org.kuali.rice.kns.document.Document;
 import org.kuali.rice.kns.document.authorization.TransactionalDocumentPresentationControllerBase;
 import org.kuali.rice.kns.util.GlobalVariables;
 
-public class ProposalDevelopmentDocumentPresentationController extends TransactionalDocumentPresentationControllerBase {
-
-    @Override
-    protected boolean canSave(Document document) {
-        // TODO : rice upgrade 11 - if in workflow, then should not be saved.  Not sure yet.
-        return !KraServiceLocator.getService(KraWorkflowService.class).isInWorkflow(document) && super.canSave(document);
-    }
+public class CommitteeDocumentPresentationController extends TransactionalDocumentPresentationControllerBase {
 
     @Override
     public boolean canInitiate(String documentTypeName) {
         //super.canInitiate(documentTypeName, user);
         UniversalUser user = new UniversalUser(GlobalVariables.getUserSession().getPerson());
-        if (!canCreateProposal(user)) {
+        if (!canCreateCommittee(user)) {
             throw new DocumentInitiationAuthorizationException(KeyConstants.ERROR_AUTHORIZATION_DOCUMENT_INITIATION, 
                                                                new String[] { user.getPersonUserIdentifier(), documentTypeName });
         }
         return true;
     }
-
     /**
-     * Does the user have permission to create a proposal.  Use the Unit Authorization Service to determine
-     * if the user has the CREATE_PROPOSAL permission in any unit.
+     * Does the user have permission to create a committee?
      * @param user the user
-     * @return true if the user has the CREATE_PROPOSAL permission in at least one unit; otherwise false
+     * @return true if the user can create a committee; otherwise false
      */
-    private boolean canCreateProposal(UniversalUser user) {
+    private boolean canCreateCommittee(UniversalUser user) {
         String username = user.getPersonUserIdentifier();
-        ApplicationTask task = new ApplicationTask(TaskName.CREATE_PROPOSAL);       
+        ApplicationTask task = new ApplicationTask(TaskName.ADD_COMMITTEE);       
         TaskAuthorizationService taskAuthenticationService = KraServiceLocator.getService(TaskAuthorizationService.class);
         return taskAuthenticationService.isAuthorized(username, task);
     }
