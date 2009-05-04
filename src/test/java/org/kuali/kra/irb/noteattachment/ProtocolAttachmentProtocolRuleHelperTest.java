36b078fcc76bba248283a6599380cae09aa9db11
Travis Schneeberger <leo.herbie@gmail.com>
2009-05-04 18:23:46 +0000

    KCIRB-204 & KCIRB-204 & KCIRB-208 better coeus compatibility.

--- src/test/java/org/kuali/kra/irb/noteattachment/ProtocolAttachmentProtocolRuleHelperTest.java
+++ src/test/java/org/kuali/kra/irb/noteattachment/ProtocolAttachmentProtocolRuleHelperTest.java
@@ -15,15 +15,22 @@
  */
 package org.kuali.kra.irb.noteattachment;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.kuali.kra.irb.bo.Protocol;
+import org.kuali.kra.irb.test.ProtocolTestUtil;
 import org.kuali.rice.kns.util.ErrorMap;
 import org.kuali.rice.kns.util.GlobalVariables;
+import static org.hamcrest.core.Is.*;
 
 /**
  * Tests for {@link ProtocolAttachmentProtocolRuleHelper ProtocolAttachmentProtocolRuleHelper}.
@@ -38,232 +45,199 @@ public class ProtocolAttachmentProtocolRuleHelperTest {
         GlobalVariables.setErrorMap(new ErrorMap());
     }
     
-    @Test @Ignore public void wip() {}
+    /**
+     * Tests a duplicate type.
+     */
+    @Test
+    public void duplicateTypeInvalid() {
+        ProtocolAttachmentProtocol attachmentToValidate = this.getAttachmentToValidate();
+               
+        Protocol protocol = ProtocolTestUtil.getProtocol(this.context);
+        
+        List<ProtocolAttachmentProtocol> attachments = new ArrayList<ProtocolAttachmentProtocol>();
+        ProtocolAttachmentProtocol attachment1 = new ProtocolAttachmentProtocol();
+        attachment1.setId(2L);
+        attachment1.setType(new ProtocolAttachmentType("1", "a desc"));
+        attachments.add(attachment1);
+        
+        protocol.setAttachmentProtocols(attachments);
+               
+        ProtocolAttachmentService service = this.context.mock(ProtocolAttachmentService.class);
+        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(service);
+        
+        boolean valid = helper.duplicateType(attachmentToValidate, protocol);
+        
+        this.context.assertIsSatisfied();
+        
+        Assert.assertThat("Should not be valid", valid, is(false));
+    }
     
-//    /**
-//     * Tests a duplicate type.
-//     */
-//    @Test
-//    public void duplicateTypeInvalid() {
-//        ProtocolAttachmentProtocol attachmentToValidate = this.getAttachmentToValidate();
-//               
-//        Protocol protocol = ProtocolTestUtil.getProtocol(this.context);
-//        
-//        List<ProtocolAttachmentProtocol> attachments = new ArrayList<ProtocolAttachmentProtocol>();
-//        ProtocolAttachmentProtocol attachment1 = new ProtocolAttachmentProtocol();
-//        attachment1.setId(2L);
-//        attachment1.setTypeCode("1");
-//        attachments.add(attachment1);
-//        
-//        protocol.setAttachmentProtocols(attachments);
-//               
-//        ProtocolAttachmentService service = this.context.mock(ProtocolAttachmentService.class);
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(service);
-//        
-//        boolean valid = helper.duplicateType(attachmentToValidate, protocol);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should not be valid", valid, is(false));
-//    }
-//    
-//    /**
-//     * Tests a unique type is valid.
-//     */
-//    @Test
-//    public void duplicateTypeValidDiffType() {
-//        ProtocolAttachmentProtocol attachmentToValidate = this.getAttachmentToValidate();
-//        
-//        Protocol protocol = ProtocolTestUtil.getProtocol(this.context);
-//        
-//        List<ProtocolAttachmentProtocol> attachments = new ArrayList<ProtocolAttachmentProtocol>();
-//        ProtocolAttachmentProtocol attachment1 = new ProtocolAttachmentProtocol();
-//        attachment1.setId(2L);
-//        attachment1.setTypeCode("2");
-//        attachments.add(attachment1);
-//        
-//        protocol.setAttachmentProtocols(attachments);
-//               
-//        ProtocolAttachmentService service = this.context.mock(ProtocolAttachmentService.class);
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(service);
-//        
-//        boolean valid = helper.duplicateType(attachmentToValidate, protocol);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should be valid", valid, is(true));
-//    }
-//    
-//    /**
-//     * Tests same type, same attachment is valid.
-//     */
-//    @Test
-//    public void duplicateTypeValidSameAttachment() {
-//        ProtocolAttachmentProtocol attachmentToValidate = this.getAttachmentToValidate();
-//        
-//        Protocol protocol = ProtocolTestUtil.getProtocol(this.context);
-//        
-//        List<ProtocolAttachmentProtocol> attachments = new ArrayList<ProtocolAttachmentProtocol>();
-//        ProtocolAttachmentProtocol attachment1 = new ProtocolAttachmentProtocol();
-//        attachment1.setId(1L);
-//        attachment1.setTypeCode("1");
-//        attachments.add(attachment1);
-//        
-//        protocol.setAttachmentProtocols(attachments);
-//               
-//        ProtocolAttachmentService service = this.context.mock(ProtocolAttachmentService.class);
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(service);
-//        
-//        boolean valid = helper.duplicateType(attachmentToValidate, protocol);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should be valid", valid, is(true));
-//    }
-//    
-//    /**
-//     * Tests a valid status with blank code.
-//     */
-//    @Test
-//    public void validStatusBlankCode() {
-//        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
-//        
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
-//        helper.resetPropertyPrefix("fooPrefix");
-//        
-//        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
-//        attachment.setStatusCode(null);
-//                
-//        boolean valid = helper.validStatus(attachment);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should be valid", valid, is(true));
-//    }
-//    
-//    /**
-//     * Tests a valid status with valid code.
-//     */
-//    @Test
-//    public void validStatusValidCode() {
-//        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
-//        
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
-//        helper.resetPropertyPrefix("fooPrefix");
-//        
-//        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
-//        attachment.setStatusCode("1");
-//        
-//        this.context.checking(new Expectations() {{
-//            ProtocolAttachmentStatus status = new ProtocolAttachmentStatus();
-//            status.setCode("1");
-//            status.setDescription("a desc");
-//            
-//            oneOf(paService).getStatusFromCode("1");
-//            will(returnValue(status));
-//        }});
-//        
-//        boolean valid = helper.validStatus(attachment);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should be valid", valid, is(true));
-//    }
-//    
-//    /**
-//     * Tests a valid status with valid code.
-//     */
-//    @Test
-//    public void invalidStatusValidCodeNotFound() {
-//        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
-//        
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
-//        helper.resetPropertyPrefix("fooPrefix");
-//        
-//        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
-//        attachment.setStatusCode("1");
-//        
-//        this.context.checking(new Expectations() {{          
-//            oneOf(paService).getStatusFromCode("1");
-//            will(returnValue(null));
-//        }});
-//        
-//        boolean valid = helper.validStatus(attachment);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should not be valid", valid, is(false));
-//    }
-//    
-//    /**
-//     * Tests a invalid status for submission with null code.
-//     */
-//    @Test
-//    public void invalidStatusValidCodeForSubmissionNull() {
-//        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
-//        
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
-//        helper.resetPropertyPrefix("fooPrefix");
-//        
-//        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
-//        attachment.setStatusCode(null);
-//               
-//        boolean valid = helper.validStatusForSubmission(attachment);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should not be valid", valid, is(false));
-//    }
-//    
-//    /**
-//     * Tests a invalid status for submission with valid code.
-//     */
-//    @Test
-//    public void invalidStatusValidCodeForSubmissionValidCode() {
-//        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
-//        
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
-//        helper.resetPropertyPrefix("fooPrefix");
-//        
-//        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
-//        attachment.setStatusCode("1");
-//        
-//        boolean valid = helper.validStatusForSubmission(attachment);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should not be valid", valid, is(false));
-//    }
-//    
-//    /**
-//     * Tests a valid status for submission with valid code.
-//     */
-//    @Test
-//    public void validStatusValidCodeForSubmission() {
-//        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
-//        
-//        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
-//        helper.resetPropertyPrefix("fooPrefix");
-//        
-//        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
-//        attachment.setStatusCode("11");
-//
-//        boolean valid = helper.validStatusForSubmission(attachment);
-//        
-//        this.context.assertIsSatisfied();
-//        
-//        Assert.assertThat("Should not be valid", valid, is(false));
-//    }
-//    
-//    /**
-//     * Gets a ProtocolAttachmentProtocol to validate.
-//     * @return a ProtocolAttachmentProtocol to validate.
-//     */
-//    private ProtocolAttachmentProtocol getAttachmentToValidate() {
-//        ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
-//        attachment.setId(1L);
-//        attachment.setTypeCode("1");
-//        
-//        return attachment;
-//    }
+    /**
+     * Tests a unique type is valid.
+     */
+    @Test
+    public void duplicateTypeValidDiffType() {
+        ProtocolAttachmentProtocol attachmentToValidate = this.getAttachmentToValidate();
+        
+        Protocol protocol = ProtocolTestUtil.getProtocol(this.context);
+        
+        List<ProtocolAttachmentProtocol> attachments = new ArrayList<ProtocolAttachmentProtocol>();
+        ProtocolAttachmentProtocol attachment1 = new ProtocolAttachmentProtocol();
+        attachment1.setId(2L);
+        attachment1.setType(new ProtocolAttachmentType("2", "a desc"));
+        attachments.add(attachment1);
+        
+        protocol.setAttachmentProtocols(attachments);
+               
+        ProtocolAttachmentService service = this.context.mock(ProtocolAttachmentService.class);
+        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(service);
+        
+        boolean valid = helper.duplicateType(attachmentToValidate, protocol);
+        
+        this.context.assertIsSatisfied();
+        
+        Assert.assertThat("Should be valid", valid, is(true));
+    }
+    
+    /**
+     * Tests same type, same attachment is valid.
+     */
+    @Test
+    public void duplicateTypeValidSameAttachment() {
+        ProtocolAttachmentProtocol attachmentToValidate = this.getAttachmentToValidate();
+        
+        Protocol protocol = ProtocolTestUtil.getProtocol(this.context);
+        
+        List<ProtocolAttachmentProtocol> attachments = new ArrayList<ProtocolAttachmentProtocol>();
+        ProtocolAttachmentProtocol attachment1 = new ProtocolAttachmentProtocol();
+        attachment1.setId(1L);
+        attachment1.setType(new ProtocolAttachmentType("1", "a desc"));
+        attachments.add(attachment1);
+        
+        protocol.setAttachmentProtocols(attachments);
+               
+        ProtocolAttachmentService service = this.context.mock(ProtocolAttachmentService.class);
+        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(service);
+        
+        boolean valid = helper.duplicateType(attachmentToValidate, protocol);
+        
+        this.context.assertIsSatisfied();
+        
+        Assert.assertThat("Should be valid", valid, is(true));
+    }
+    
+    /**
+     * Tests a valid status with blank code.
+     */
+    @Test
+    public void validStatusBlankCode() {
+        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
+        
+        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
+        helper.resetPropertyPrefix("fooPrefix");
+        
+        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
+        attachment.setStatus(null);
+                
+        boolean valid = helper.validStatus(attachment);
+        
+        this.context.assertIsSatisfied();
+        
+        Assert.assertThat("Should be valid", valid, is(true));
+    }
+    
+    /**
+     * Tests a valid status with valid code.
+     */
+    @Test
+    public void validStatus() {
+        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
+        
+        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
+        helper.resetPropertyPrefix("fooPrefix");
+        
+        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
+        attachment.setStatus(new ProtocolAttachmentStatus("1", "a desc"));
+        
+        this.context.checking(new Expectations() {{
+        }});
+        
+        boolean valid = helper.validStatus(attachment);
+        
+        this.context.assertIsSatisfied();
+        
+        Assert.assertThat("Should be valid", valid, is(true));
+    }
+    
+    /**
+     * Tests a invalid status for submission with null.
+     */
+    @Test
+    public void invalidStatusValidCodeForSubmissionNull() {
+        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
+        
+        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
+        helper.resetPropertyPrefix("fooPrefix");
+        
+        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
+        attachment.setStatus(null);
+               
+        boolean valid = helper.validStatusForSubmission(attachment);
+        
+        this.context.assertIsSatisfied();
+        
+        Assert.assertThat("Should not be valid", valid, is(false));
+    }
+    
+    /**
+     * Tests a invalid status for submission with valid code.
+     */
+    @Test
+    public void invalidStatusValidCodeForSubmissionValidCode() {
+        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
+        
+        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
+        helper.resetPropertyPrefix("fooPrefix");
+        
+        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
+        attachment.setStatus(new ProtocolAttachmentStatus("1", "a desc"));
+        
+        boolean valid = helper.validStatusForSubmission(attachment);
+        
+        this.context.assertIsSatisfied();
+        
+        Assert.assertThat("Should not be valid", valid, is(false));
+    }
+    
+    /**
+     * Tests a valid status for submission with valid code.
+     */
+    @Test
+    public void validStatusValidCodeForSubmission() {
+        final ProtocolAttachmentService paService = this.context.mock(ProtocolAttachmentService.class); 
+        
+        ProtocolAttachmentProtocolRuleHelper helper = new ProtocolAttachmentProtocolRuleHelper(paService);
+        helper.resetPropertyPrefix("fooPrefix");
+        
+        final ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
+        attachment.setStatus(new ProtocolAttachmentStatus("9", "a desc"));
+
+        boolean valid = helper.validStatusForSubmission(attachment);
+        
+        this.context.assertIsSatisfied();
+        
+        Assert.assertThat("Should not be valid", valid, is(false));
+    }
+    
+    /**
+     * Gets a ProtocolAttachmentProtocol to validate.
+     * @return a ProtocolAttachmentProtocol to validate.
+     */
+    private ProtocolAttachmentProtocol getAttachmentToValidate() {
+        ProtocolAttachmentProtocol attachment = new ProtocolAttachmentProtocol();
+        attachment.setId(1L);
+        attachment.setType(new ProtocolAttachmentType("1", "a desc"));
+        
+        return attachment;
+    }
 }
--- src/test/resources/sql/dml/load_PROTOCOL_ATTACHMENT_TYPE.sql
+++ src/test/resources/sql/dml/load_PROTOCOL_ATTACHMENT_TYPE.sql
@@ -15,8 +15,9 @@ VALUES ( 7, 'HIPAA Research Authorization Form', sysdate, user );
 INSERT INTO PROTOCOL_ATTACHMENT_TYPE ( TYPE_CD, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER ) 
 VALUES ( 8, 'HIPAA Waiver of Research Authorization Form', sysdate, user );
 INSERT INTO PROTOCOL_ATTACHMENT_TYPE ( TYPE_CD, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER ) 
-VALUES ( 9, 'Adverse Event', sysdate, user );
+VALUES ( 9, 'Other', sysdate, user );
 INSERT INTO PROTOCOL_ATTACHMENT_TYPE ( TYPE_CD, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER ) 
-VALUES ( 10, 'Biography', sysdate, user );
+VALUES ( 10, 'Adverse Event', sysdate, user );
 INSERT INTO PROTOCOL_ATTACHMENT_TYPE ( TYPE_CD, DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER ) 
-VALUES ( 11, 'Other', sysdate, user );
+VALUES ( 11, 'Biography', sysdate, user );
+
