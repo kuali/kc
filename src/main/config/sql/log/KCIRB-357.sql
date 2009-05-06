fcb10299d78b2aba6edcea8299aa9e604f9c98eb
Claus Niesen <cniesen@iastate.edu>
2009-05-06 16:29:39 +0000

    KCIRB-357 Added commit.

--- src/main/config/sql/log/KCIRB-357.sql
+++ src/main/config/sql/log/KCIRB-357.sql
@@ -29,6 +29,8 @@ VALUES ( 1, 'IRB', sysdate, user );
 INSERT INTO GROUP_TYPES ( GROUP_TYPE_CODE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER ) 
 VALUES ( 2, 'Proposal', sysdate, user ); 
 INSERT INTO GROUP_TYPES ( GROUP_TYPE_CODE, GROUP_NAME, UPDATE_TIMESTAMP, UPDATE_USER ) 
-VALUES ( 3, 'Others', sysdate, user ); 
+VALUES ( 3, 'Others', sysdate, user );
+
+COMMIT;
 
 
