1574a5d26cc4103f159840ccd6dceae9dd4ecd13
Travis Schneeberger <leo.herbie@gmail.com>
2009-05-06 14:49:52 +0000

    KCIRB-142

--- src/main/java/org/kuali/kra/irb/actions/copy/ProtocolCopyService.java
+++ src/main/java/org/kuali/kra/irb/actions/copy/ProtocolCopyService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.irb.service;
+package org.kuali.kra.irb.actions.copy;
 
 import org.kuali.kra.irb.document.ProtocolDocument;
 
