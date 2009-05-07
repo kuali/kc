e1acbee4cb43449582e209f2c8a8e417d9efd58c
Jack Frosch <jfrosch@objistics.com>
2009-05-07 02:23:33 +0000

    KCAWD-83 / KCAWD-85

--- src/main/java/org/kuali/kra/proposaldevelopment/bo/InvestigatorCreditType.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/bo/InvestigatorCreditType.java
@@ -27,55 +27,76 @@ import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
  * $Id: InvestigatorCreditType.java,v 1.6 2008-07-23 19:16:37 gmcgrego Exp $
  */
 public class InvestigatorCreditType extends KraPersistableBusinessObjectBase {
+    private static final long serialVersionUID = 2881039955568764530L;
+    
     private String invCreditTypeCode;
     private Boolean addsToHundred;
     private Boolean active;
     private String description;
 
     /**
+     * Default constructor
+     */
+    public InvestigatorCreditType() {
+        
+    }
+    
+    /**
+     * Convenience constructor
+     * @param invCreditTypeCode
+     * @param description
+     */
+    public InvestigatorCreditType(String invCreditTypeCode, String description) {
+        this.invCreditTypeCode = invCreditTypeCode;
+        this.description = description;
+        this.active = true;
+        this.addsToHundred = true;
+    }
+    
+    /**
      * Retrieves the description attribute
      * 
      * @return String
      */
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
     
     /**
      * Assigns the description attribute
      *
      * @param description
      */
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     /**
      * Retrieves the description attribute
      * 
      * @return Boolean
      */
-	public Boolean addsToHundred() {
+    public Boolean addsToHundred() {
         return getAddsToHundred();
-	}
+    }
 
     /**
      * Retrieves the description attribute
      * 
      * @return Boolean
      */
-	public Boolean getAddsToHundred() {
-		return addsToHundred;
-	}
+    public Boolean getAddsToHundred() {
+        return addsToHundred;
+    }
     
     /**
      * Assigns the description attribute
      *
      * @param argAddsToHundred
      */
-	public void setAddsToHundred(Boolean argAddsToHundred) {
-		this.addsToHundred = argAddsToHundred;
-	}
+    public void setAddsToHundred(Boolean argAddsToHundred) {
+        this.addsToHundred = argAddsToHundred;
+    }
     
     /**
      * Gets the value of invCreditTypeCode
@@ -99,20 +120,19 @@ public class InvestigatorCreditType extends KraPersistableBusinessObjectBase {
     /**
      * @see org.kuali.rice.kns.bo.BusinessObjectBase#toStringMapper()
      */
-	@Override 
-	protected LinkedHashMap toStringMapper() {
-   	    LinkedHashMap hashmap = new LinkedHashMap();
-        hashmap.put("invCreditTypeCode", getDescription());
-        hashmap.put("description", getDescription());
-        hashmap.put("addsToHundred", getAddsToHundred());
-		return hashmap;
-	}
+    @Override 
+    protected LinkedHashMap<String, Object> toStringMapper() {
+        LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
+        map.put("invCreditTypeCode", getDescription());
+        map.put("description", getDescription());
+        map.put("addsToHundred", getAddsToHundred());
+        return map;
+    }
 
-	/**
-	 * Read access to the active flag
-	 * 
-	 * @return Boolean
-	 */
+    /**
+     * Read access to the active flag
+     * @return Boolean
+     */
     public Boolean getActive() {
         return active;
     }
@@ -126,4 +146,40 @@ public class InvestigatorCreditType extends KraPersistableBusinessObjectBase {
         this.active = active;
     }
 
+    /**
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int PRIME = 31;
+        int result = 1;
+        result = PRIME * result + ((invCreditTypeCode == null) ? 0 : invCreditTypeCode.hashCode());
+        return result;
+    }
+
+    /**
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof InvestigatorCreditType)) {
+            return false;
+        }
+        InvestigatorCreditType other = (InvestigatorCreditType) obj;
+        if (invCreditTypeCode == null) {
+            if (other.invCreditTypeCode != null) {
+                return false;
+            }
+        } else if (!invCreditTypeCode.equals(other.invCreditTypeCode)) {
+            return false;
+        }
+        return true;
+    }
+    
 }
