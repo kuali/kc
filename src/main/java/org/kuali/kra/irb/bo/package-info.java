774d29c4c2edc7b2bd3f65aeeec40cdbc0eb82d1
Travis Schneeberger <leo.herbie@gmail.com>
2009-05-07 13:53:05 +0000

    who broke the build?

--- src/main/java/org/kuali/kra/irb/bo/package-info.java
+++ src/main/java/org/kuali/kra/irb/bo/package-info.java
@@ -4,5 +4,6 @@
  * @deprecated All of the classes in this package need to be moved to their respective "unit-of-work" package.
  * Do NOT place anymore classes in this package.
  */
-@java.lang.Deprecated()
+//commented out - eclipse's 3.4.x compiler allows it but Sun's 1.5.x compiler does not. 
+//@java.lang.Deprecated()
 package org.kuali.kra.irb.bo;
