f6a4b255e4b6305dd98ad76decd6e2a7ef1ed68e
Don Barre <dbarre@arizona.edu>
2009-05-12 17:48:35 +0000

    KCIRB-245 : Add Values Finder for Protocol Review Type for filtering purposes

---
+++ src/main/java/org/kuali/kra/irb/actions/submit/ProtocolReviewTypeValuesFinder.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.actions.submit;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
+import org.kuali.rice.kns.service.KeyValuesService;
+import org.kuali.rice.kns.web.ui.KeyLabelPair;
+
+/**
+ * Assembles the Protocol Review Types to display in the drop-down menu when
+ * submitting a protocol to the IRB office for review.
+ */
+public class ProtocolReviewTypeValuesFinder extends KeyValuesBase {
+    
+    @SuppressWarnings("unchecked")
+    public List<KeyLabelPair> getKeyValues() {
+        KeyValuesService keyValuesService = (KeyValuesService) KraServiceLocator.getService("keyValuesService");
+        Collection<ProtocolReviewType> protocolReviewTypes = keyValuesService.findAll(ProtocolReviewType.class);
+        List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
+        keyValues.add(new KeyLabelPair("", "select"));
+        for (ProtocolReviewType protocolReviewType : protocolReviewTypes) {
+            keyValues.add(new KeyLabelPair(protocolReviewType.getReviewTypeCode(), protocolReviewType.getDescription()));
+        }
+        return keyValues;
+    }
+}
