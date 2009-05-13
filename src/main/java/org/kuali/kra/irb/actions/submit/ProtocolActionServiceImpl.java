8ed6149d3566090295b650e68f97515a844f0a5f
Kiltesh Patel <kjpatel@ucdavis.edu>
2009-05-13 23:21:12 +0000

    KCIRB-317

--- src/main/java/org/kuali/kra/irb/actions/submit/ProtocolActionServiceImpl.java
+++ src/main/java/org/kuali/kra/irb/actions/submit/ProtocolActionServiceImpl.java
@@ -15,6 +15,11 @@
  */
 package org.kuali.kra.irb.actions.submit;
 
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.kuali.kra.drools.util.DroolsRuleHandler;
 import org.kuali.kra.irb.Protocol;
 import org.kuali.kra.irb.actions.ProtocolAction;
@@ -24,8 +29,13 @@ import org.kuali.rice.kns.service.BusinessObjectService;
 class ProtocolActionServiceImpl {
 
     private BusinessObjectService businessObjectService;
-
-    public void updateProtocolStatus(ProtocolAction protocolActionBo, Protocol protocol) {
+    
+    public void updateProtocolStatus(ProtocolAction protocolActionBo, Protocol protocol) { 
+        runBusinessRules(protocolActionBo, protocol);
+        logAction(protocolActionBo, protocol);
+    }
+    
+    public void runBusinessRules(ProtocolAction protocolActionBo, Protocol protocol) {
         
         String protocolNumberUpper = protocol.getProtocolNumber().toUpperCase();        
         String specialCondition = (protocolNumberUpper.contains("A")? "A" : (protocolNumberUpper.contains("R")? "R" : "NONE"));
@@ -38,16 +48,116 @@ class ProtocolActionServiceImpl {
         DroolsRuleHandler updateHandle = new DroolsRuleHandler("org/kuali/kra/irb/drools/rules/updateProtocolRules.drl");
         updateHandle.executeRules(protocolAction);
         //TODO transaction
-        businessObjectService.save(protocol);
+        businessObjectService.save(protocol);   
     }
 
     public void setBusinessObjectService(BusinessObjectService businessObjectService) {
         this.businessObjectService = businessObjectService;
     }
     
-    private void logAction(ProtocolAction protocolActionBo, Protocol protocol) {
+    public void logAction(ProtocolAction protocolActionBo, Protocol protocol) {
+
+        String comments = getActionComment(protocolActionBo,protocol);
         
+        Timestamp date = getActionDate(protocolActionBo);
         
+        Integer maxActionId = getActionId(protocol);
         
+        Integer maxSequenceNo = getSequenceNumber(protocol);
+        
+        ProtocolAction newActionBo = new ProtocolAction();
+        newActionBo.setProtocolNumber(protocol.getProtocolNumber());
+        newActionBo.setSequenceNumber(maxSequenceNo);
+        newActionBo.setActionId(maxActionId);
+        newActionBo.setProtocolActionTypeCode(protocolActionBo.getProtocolActionTypeCode());
+        newActionBo.setComments(comments.toString());
+        newActionBo.setSubmissionNumber(protocolActionBo.getSubmissionNumber());
+        newActionBo.setActionDate(date);
+        
+        businessObjectService.save(newActionBo);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public Integer getSequenceNumber(Protocol protocol){
+        Map<String, Object> fieldValues = new HashMap<String, Object>();
+        fieldValues.put("protocolNumber", protocol.getProtocolNumber());
+        List<Protocol> protocols = (List<Protocol>)businessObjectService.findMatching(Protocol.class, fieldValues);
+        
+        Integer maxSequenceNo = 0;
+        
+        for(Protocol prtcl: protocols) {
+            if(prtcl.getSequenceNumber() != null && prtcl.getSequenceNumber() > maxSequenceNo) {
+                maxSequenceNo = prtcl.getSequenceNumber();
+            }
+        }
+        if(maxSequenceNo == 0) {
+            maxSequenceNo = protocol.getSequenceNumber();
+        }
+        return maxSequenceNo;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public Integer getActionId(Protocol protocol) {
+        Map<String, Object> fieldValues = new HashMap<String, Object>();
+        fieldValues.put("protocolNumber", protocol.getProtocolNumber());
+        List<ProtocolAction> actions = (List<ProtocolAction>)businessObjectService.findMatching(ProtocolAction.class, fieldValues);
+        
+        Integer maxActionId = 0;
+        
+        for(ProtocolAction action: actions) {
+            if(action.getActionId() != null && action.getActionId() > maxActionId) {
+                maxActionId = action.getActionId();
+            }
+        }
+        return ++maxActionId;
+    }
+    
+    public Timestamp getActionDate(ProtocolAction protocolActionBo) {
+        Timestamp date = new Timestamp(new java.util.Date().getTime());
+        if (null != protocolActionBo.getActionDate())
+            date = protocolActionBo.getActionDate();
+        return date;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public String getActionComment(ProtocolAction protocolActionBo, Protocol protocol) {
+        List<ProtocolAmendRenewal> protocolAmendRenewals = (List<ProtocolAmendRenewal>)businessObjectService.findAll(ProtocolAmendRenewal.class);
+        String actionComments = protocolActionBo.getComments() != null? protocolActionBo.getComments() : "" ;
+        StringBuffer comments = new StringBuffer();
+        
+        Integer max = 0;
+        String maxString = null;
+        if(protocolActionBo.getProtocolActionTypeCode().equalsIgnoreCase("103")) {
+            for(ProtocolAmendRenewal renewal : protocolAmendRenewals) {
+                if(renewal.getProtoAmendRenNumber().substring(0,11).equalsIgnoreCase(protocol.getProtocolNumber()+"A")) {
+                    Integer tmp = new Integer(renewal.getProtoAmendRenNumber().substring(11,14));
+                    if(tmp > max) 
+                        max = tmp;                    
+                }
+            }
+            if (max == 0) maxString = "001";
+            comments.append("Amendment- ").append(maxString != null ? maxString : max).append(" created");
+        }
+        else { 
+            if(protocolActionBo.getProtocolActionTypeCode().equalsIgnoreCase("102")){        
+                for(ProtocolAmendRenewal renewal : protocolAmendRenewals) {
+                    if(renewal.getProtoAmendRenNumber().substring(0,11).equalsIgnoreCase(protocol.getProtocolNumber()+"R")) {
+                        Integer tmp = new Integer(renewal.getProtoAmendRenNumber().substring(11, 14));
+                        if(tmp > max) 
+                            max = tmp; 
+                    }
+                }
+                if (max == 0) maxString = "001";
+                comments.append("Renewal- ").append(maxString != null ? maxString : max).append(" created");
+            } 
+            else {
+                String nbr = protocol.getProtocolNumber().substring(11, 14);
+                if(protocol.getProtocolNumber().substring(10, 11).equalsIgnoreCase("A"))
+                        comments.append("Amendment- ").append(nbr).append(actionComments);
+                else if(protocol.getProtocolNumber().substring(10, 11).equalsIgnoreCase("R"))
+                        comments.append("Renewal- ").append(nbr).append(actionComments);                        
+            }
+        }
+        return comments.toString();
     }
 }
