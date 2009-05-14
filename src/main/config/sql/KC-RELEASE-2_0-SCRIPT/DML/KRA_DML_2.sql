1b20e1e318efd7fc9879b10b4d092da2472ec8c2
Tyler Wilson <tyler.wilson@colostate.edu>
2009-05-14 02:59:28 +0000

    Database Script changes for Rel 2

--- src/main/config/sql/KC-RELEASE-2_0-SCRIPT/DML/KRA_DML_2.sql
+++ src/main/config/sql/KC-RELEASE-2_0-SCRIPT/DML/KRA_DML_2.sql
@@ -1112,4 +1112,19 @@ Insert into KRNS_PARM_T
 Values
 ('KC-PROTOCOL','D','irb.protocol.institute.proposal.linking.enabled', sys_guid(), 1,'CONFG','N','Linking from Award to Protocol Funding source is configurable at impl time','A','WorkflowAdmin','Y');
 
+Insert into KRNS_PARM_T
+   (nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd, grp_nm, ACTV_IND)
+ Values
+   ('KC-PROTOCOL', 'D', 'protocolSelectSubmissionCommittee', sys_guid(), 1, 'CONFG', 'True', 'Implementing institution can decide on whether to allow a committee to be selected upon an IRB submittal', 'A', 'WorkflowAdmin', 'Y');
+
+Insert into KRNS_PARM_T
+   (nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd, grp_nm, ACTV_IND)
+ Values
+   ('KC-PROTOCOL', 'D', 'protocolSelectSubmissionSchedule', sys_guid(), 1, 'CONFG', 'True', 'Implementing institution can decide on whether to allow a committee schedule to be selected upon an IRB submittal', 'A', 'WorkflowAdmin', 'Y');
+
+Insert into KRNS_PARM_T
+   (nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd, grp_nm, ACTV_IND)
+ Values
+   ('KC-PROTOCOL', 'D', 'protocolSelectSubmissionReviewers', sys_guid(), 1, 'CONFG', 'True', 'Implementing institution can decide on whether to allow reviewers to be selected upon an IRB submittal', 'A', 'WorkflowAdmin', 'Y');
+
 commit;
