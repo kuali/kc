c2753b120574de88bcc5847154b9ae5f6c1d678b
Lin-Long Shyu <lshyukuali@gmail.com>
2009-05-15 20:36:58 +0000

    kcirb-17

--- src/main/webapp/scripts/jquery/my_bm_inc.js
+++ src/main/webapp/scripts/jquery/my_bm_inc.js
@@ -1,3 +1,3 @@
-var open_file 	= '../images/tinybutton-hide.gif';
-var closed_file	= '../images/tinybutton-show.gif';
+var open_file 	= 'kr/static/images/tinybutton-hide.gif';
+var closed_file	= 'kr/static/images/tinybutton-show.gif';
 var node_file	= 'my_cc_node.gif';
