14022ae08eb1b03907674fe9938ad7df40b71407
Don Barre <dbarre@arizona.edu>
2009-05-19 21:38:04 +0000

    KCIRB-260 : Add Data Analysis Only UI

--- src/main/java/org/kuali/kra/irb/actions/submit/ProtocolSubmissionType.java
+++ src/main/java/org/kuali/kra/irb/actions/submit/ProtocolSubmissionType.java
@@ -31,6 +31,7 @@ public class ProtocolSubmissionType extends KraPersistableBusinessObjectBase {
     public static final String REQUEST_FOR_SUSPENSION = "110";
     public static final String REQUEST_TO_CLOSE_ENROLLMENT = "111";
     public static final String REQUEST_TO_REOPEN_ENROLLMENT = "114";
+    public static final String REQUEST_FOR_DATA_ANALYSIS_ONLY = "113";
 	
     private String submissionTypeCode; 
 	private String description; 
