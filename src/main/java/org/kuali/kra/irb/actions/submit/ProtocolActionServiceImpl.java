25e252128ca0beccc460d7bd7f36ce7f4409c552
Kiltesh Patel <kjpatel@ucdavis.edu>
2009-05-27 16:47:52 +0000

    KCIRB-317

--- src/main/java/org/kuali/kra/irb/actions/submit/ProtocolActionServiceImpl.java
+++ src/main/java/org/kuali/kra/irb/actions/submit/ProtocolActionServiceImpl.java
@@ -15,50 +15,86 @@
  */
 package org.kuali.kra.irb.actions.submit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.kuali.kra.drools.util.DroolsRuleHandler;
 import org.kuali.kra.irb.Protocol;
 import org.kuali.kra.irb.actions.ProtocolAction;
 import org.kuali.rice.kns.service.BusinessObjectService;
 
 
-public class ProtocolActionServiceImpl {
+public class ProtocolActionServiceImpl implements ProtocolActionService {
 
     private BusinessObjectService businessObjectService;
-    
+
+    private List<String> actions = new ArrayList<String>();
+
+    {
+        String[] actn = { "104", "105", "106", "108", "114", "115", "116", "200", "201", "202", "203", "204", "205", "206", "207",
+                "208", "209", "210", "211", "212", "300", "301", "302", "303", "304", "305", "306" };
+        actions = Arrays.asList(actn);
+    }
+
     public void setBusinessObjectService(BusinessObjectService businessObjectService) {
         this.businessObjectService = businessObjectService;
     }
-    
-    public boolean canPerformAction(String actionTypeCode, Protocol protocol) {    
+
+    /**
+     * @see org.kuali.kra.irb.actions.submit.ProtocolActionService#getActionsAllowed(org.kuali.kra.irb.Protocol)
+     */
+    public List<String> getActionsAllowed(Protocol protocol) {
+
+        List<String> actionList = new ArrayList<String>();
+        for (String actionTypeCode : actions) {
+            if (canPerformAction(actionTypeCode, protocol) && isAuthorizedToPerform(actionTypeCode, protocol)) {
+                actionList.add(actionTypeCode);
+            }
+        }
+        return actionList;
+    }
+
+    // TODO
+    public boolean isAuthorizedToPerform(String actionTypeCode, Protocol protocol) {
+        return true;
+    }
+
+    public boolean canPerformAction(String actionTypeCode, Protocol protocol) {
         String submissionStatusCode = protocol.getProtocolSubmission().getSubmissionStatusCode();
-        String submissionTypeCode = protocol.getProtocolSubmission().getSubmissionTypeCode();       
-        String protocolReviewTypeCode = protocol.getProtocolSubmission().getProtocolReviewTypeCode();       
+        String submissionTypeCode = protocol.getProtocolSubmission().getSubmissionTypeCode();
+        String protocolReviewTypeCode = protocol.getProtocolSubmission().getProtocolReviewTypeCode();
         String protocolStatusCode = protocol.getProtocolStatusCode();
-        ProtocolActionMapping protocolAction = new ProtocolActionMapping(actionTypeCode, submissionStatusCode, submissionTypeCode, protocolReviewTypeCode, protocolStatusCode);
-        
+        ProtocolActionMapping protocolAction = new ProtocolActionMapping(actionTypeCode, submissionStatusCode, submissionTypeCode,
+            protocolReviewTypeCode, protocolStatusCode);
+
         DroolsRuleHandler updateHandle = new DroolsRuleHandler("org/kuali/kra/irb/drools/rules/canPerformProtocolActionRules.drl");
         updateHandle.executeRules(protocolAction);
         return protocolAction.isAllowed();
     }
-    
+
+    /**
+     * @see org.kuali.kra.irb.actions.submit.ProtocolActionService#updateProtocolStatus(org.kuali.kra.irb.actions.ProtocolAction, org.kuali.kra.irb.Protocol)
+     */
     public void updateProtocolStatus(ProtocolAction protocolActionBo, Protocol protocol) {
         runUpdateProtocolRules(protocolActionBo, protocol);
         new ActionLogger().log(protocolActionBo, protocol, businessObjectService);
     }
-    
+
     public void runUpdateProtocolRules(ProtocolAction protocolActionBo, Protocol protocol) {
-        
-        String protocolNumberUpper = protocol.getProtocolNumber().toUpperCase();        
-        String specialCondition = (protocolNumberUpper.contains("A")? "A" : (protocolNumberUpper.contains("R")? "R" : "NONE"));
-        
-        ProtocolActionUpdateMapping protocolAction = new ProtocolActionUpdateMapping(protocolActionBo.getProtocolActionTypeCode(), protocol
-                .getProtocolSubmission().getProtocolSubmissionType().getSubmissionTypeCode(), protocol.getProtocolStatusCode(), specialCondition);
+
+        String protocolNumberUpper = protocol.getProtocolNumber().toUpperCase();
+        String specialCondition = (protocolNumberUpper.contains("A") ? "A" : (protocolNumberUpper.contains("R") ? "R" : "NONE"));
+
+        ProtocolActionUpdateMapping protocolAction = new ProtocolActionUpdateMapping(protocolActionBo.getProtocolActionTypeCode(),
+            protocol.getProtocolSubmission().getProtocolSubmissionType().getSubmissionTypeCode(), protocol.getProtocolStatusCode(),
+            specialCondition);
         protocolAction.setProtocol(protocol);
         protocolAction.setProtocolSubmissionStatus(protocol.getProtocolSubmission().getSubmissionStatus());
 
         DroolsRuleHandler updateHandle = new DroolsRuleHandler("org/kuali/kra/irb/drools/rules/updateProtocolRules.drl");
         updateHandle.executeRules(protocolAction);
-        businessObjectService.save(protocol);   
+        businessObjectService.save(protocol);
     }
 
 }
