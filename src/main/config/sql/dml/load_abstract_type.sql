5bd62e259ca073b94576f4a2b5c0362158a28c81
Geoff McGregor <gmcgrego@iu.edu>
2009-06-17 20:09:43 +0000

    Merge 1.1.1 to trunk

--- src/main/config/sql/dml/load_abstract_type.sql
+++ src/main/config/sql/dml/load_abstract_type.sql
@@ -13,3 +13,5 @@ insert into abstract_type (abstract_type_code,description,update_timestamp,updat
 insert into abstract_type (abstract_type_code,description,update_timestamp,update_user) values('13','Publications',sysdate,'kradev');
 insert into abstract_type (abstract_type_code,description,update_timestamp,update_user) values('14','Reviewers Not to Include',sysdate,'kradev');
 insert into abstract_type (abstract_type_code,description,update_timestamp,update_user) values('15','Deviation Authorization',sysdate,'kradev');
+insert into abstract_type (abstract_type_code,description,update_timestamp,update_user) values('16','Areas Affected',sysdate,'kradev');
+insert into abstract_type (abstract_type_code,description,update_timestamp,update_user) values('17','Relevance',sysdate,'kradev');
