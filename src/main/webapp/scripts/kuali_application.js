efea1a8ad0da8735638baa610794eeebd735a0b4
Geoff McGregor <gmcgrego@iu.edu>
2009-06-17 20:21:49 +0000

    Merge 1.1.1 to trunk

--- src/main/webapp/scripts/kuali_application.js
+++ src/main/webapp/scripts/kuali_application.js
@@ -400,6 +400,49 @@ function loadSponsorName(sponsorCodeFieldName, sponsorNameFieldName ) {
 }
 
 
+/*
+ * Load the Funding Source Name field based on the id and type passed in.
+ */
+function loadFundingSourceNameTitle(fundingTypeCodeFieldName, fundingIdFieldName, fundingNameFieldName, fundingTitleFieldName ) {
+	var fundingTypeCode = DWRUtil.getValue( fundingTypeCodeFieldName );
+	var fundingId = DWRUtil.getValue( fundingIdFieldName );
+	var fundingName = DWRUtil.getValue( fundingNameFieldName );
+	var fundingTitle = DWRUtil.getValue( fundingTitleFieldName );
+
+	if (fundingTypeCode=='') {
+		clearRecipients( fundingNameFieldName, "" );
+		clearRecipients( fundingTitleFieldName, "" );
+	} else {
+		var dwrReply = {
+			callback:function(data) {
+				if ( data != null ) {
+					if ( fundingNameFieldName != null && fundingNameFieldName != "" && data.fundingSourceName!= null) {
+						setRecipientValue( fundingNameFieldName, data.fundingSourceName );
+					} else {
+						setRecipientValue( fundingNameFieldName,  wrapError( "not found" ), true );
+					}
+					if ( fundingTitleFieldName != null && fundingTitleFieldName != "" && data.fundingSourceTitle!=null) {
+						setRecipientValue( fundingTitleFieldName, data.fundingSourceTitle );
+					}
+				} else {
+					if ( fundingNameFieldName != null && fundingNameFieldName != "" ) {
+						setRecipientValue( fundingNameFieldName,  wrapError( "not found" ), true );
+					}
+					if ( fundingTitleFieldName != null && fundingTitleFieldName != "" ) {
+						setRecipientValue( fundingTitleFieldName,  wrapError( "not found" ), true );
+					}
+				}
+			},
+			errorHandler:function( errorMessage ) {
+				window.status = errorMessage;
+				setRecipientValue( fundingNameFieldName,  wrapError( "not found" ), true );
+				setRecipientValue( fundingTitleFieldName,  wrapError( "not found" ), true );
+			}
+		};
+		ProtocolFundingSourceService.calculateProtocolFundingSource(fundingTypeCode, fundingId, fundingName, fundingTitle, dwrReply);
+	}
+}
+ 
 
 /*
  * Load the Budget Category Code based on Object Code(Cost Element)
