ed8e5340fa95a74530faa02a65456c5d4b6ed60d
Geoff McGregor <gmcgrego@iu.edu>
2009-06-17 20:14:37 +0000

    Merge 1.1.1 to trunk

--- src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
+++ src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2008 The Kuali Foundation
+ * Copyright 2006-2009 The Kuali Foundation
  *
  * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,9 @@ package org.kuali.kra.infrastructure;
  */
 public final class KeyConstants {
     
+    public static final String ERROR_INVALID_FORMAT_WITH_FORMAT = "error.invalidFormat.withFormat";
+    public static final String ERROR_REQUIRED = "error.required";
+    
     public static final String ERROR_REQUIRED_FOR_APPROVED_SPECIALREVIEW = "error.required.for.approved.specialReview";
     public static final String ERROR_NOT_APPROVED_SPECIALREVIEW = "error.not.approved.specialReview";
     public static final String ERROR_REQUIRED_SELECT_APPROVAL_STATUS = "error.required.select.approval.status";
@@ -34,8 +37,12 @@ public final class KeyConstants {
     public static final String ERROR_NIH_SPONSOR_PROJECT_TITLE_LENGTH = "error.nih.sponsor.project.title.length";
     public static final String WARNING_EMPTY_DEADLINE_DATE = "warning.empty.deadline.date";
     public static final String WARNING_PAST_DEADLINE_DATE = "warning.past.deadline.date";
+        public static final String ERROR_SPECIAL_REVIEW_DATE_ORDERING = "error.special.review.date.ordering";
     public static final String ERROR_APPROVAL_DATE_BEFORE_APPLICATION_DATE_SPECIALREVIEW = "error.approvalDate.before.applicationDate.for.valid.specialReview";
     public static final String ERROR_START_DATE_AFTER_END_DATE = "error.start.date.after.end.date";
+    
+    // Abstracts and Attachments errors
+    public static final String ERROR_PROPOSAL_ATTACHMENT_NOT_COMPLETE = "error.document.narrativestatuscode.not.complete";
 
     // Proposal Types System Parameter Names
     public static final String PROPOSALDEVELOPMENT_PROPOSALTYPE_NEW = "proposaldevelopment.proposaltype.new";
@@ -60,9 +67,7 @@ public final class KeyConstants {
     public static final String ERROR_SELECT_UNIT="error.select.unit";
     public static final String ERROR_ONE_UNIT="error.one.unit";
     public static final String ERROR_PERCENTAGE="error.percentage";
-    
-    
-    
+    public static final String ERROR_MINLENGTH="error.minlength";
     
     public static final String ERROR_REQUIRED_FOR_FILE_NAME="error.required.for.fileName";
     public static final String ERROR_ABSTRACT_TYPE_NOT_SELECTED = "error.abstractType.notselected";
@@ -110,7 +115,11 @@ public final class KeyConstants {
     public static final String CLEAR_AUDIT_ERRORS_BEFORE_CHANGE_STATUS_TO_COMPLETE = "clear.audit.error.before.change.status.to.complete";
     public static final String ERROR_MULTIPLE_FINAL_BUDGETS = "error.final.multiple";
     public static final String AUDIT_ERROR_NO_BUDGETVERSION_COMPLETE_AND_FINAL = "error.no.budgetVersion.completeAndFinal";
-    
+    public static final String ERROR_BUDGET_STATUS_COMPLETE_WHEN_NOT_MODULER = "error.budgetVersion.budgetStatusCompleteWhenNotModuler";
+
+    //Budget Version warnings
+    public static final String WARNING_BUDGET_VERSION_MODULAR_INVALID_TDC = "warning.budgetVersion.Modular.InvalidTotalDirectCost";
+
     // Budget Personnel constants
     public static final String ERROR_DUPLICATE_BUDGET_PERSON = "error.budgetPerson.duplicate";
     public static final String QUESTION_DELETE_PERSON = "document.question.deletePerson.text";
@@ -136,7 +145,11 @@ public final class KeyConstants {
     public static final String ERROR_LAST_AGGREGATOR = "error.last.aggregator";
     
     public static final String ERROR_REQUIRE_ONE_NARRATIVE_MODIFY="error.narrative.one.modify";
+    public static final String ERROR_REQUIRE_ONE_NARRATIVE_MODIFY_WITH_ARG="error.narrative.one.modify.with.arg";
     public static final String ERROR_NARRATIVE_USER_RIGHT_NO_PERMISSION="error.narrative.no.permission";
+    public static final String ERROR_ONE_AGGREGATOR_MODFIY="error.one.aggregator.modify";
+    public static final String ERROR_ONE_AGGREGATOR_MODIFY_WITH_ARG="error.one.aggregator.modify.with.arg";
+    public static final String ERROR_PERMISSION_VIEWER_ONLY_KEY = "error.viewer.only";
     
     // Authorization
     public static final String AUTHORIZATION_VIOLATION = "error.authorization.violation";
@@ -149,6 +162,7 @@ public final class KeyConstants {
     public static final String ERROR_IF_PROPOSALTYPE_IS_REVISION = "error.s2sopportunity.revisiontype";
     public static final String ERROR_IF_CFDANUMBER_AND_OPPORTUNITY_ID_IS_NULL = "error.s2sopportunity.cfdaNumber_opportunityId_null"; 
     public static final String ERROR_IF_REVISIONTYPE_IS_OTHER = "error.s2sopportunity.revisionTypeOther";
+    public static final String ERROR_REQUIRED_REVISIONTYPE = "error.required.s2sopportunity.revisionType";
     public static final String ERROR_IF_CFDANUMBER_IS_INVALID = "error.s2sopportunity.cfdaNumberInvalid";
     public static final String ERROR_IF_OPPORTUNITY_ID_IS_INVALID = "error.s2sopportunity.opportunityIdInvalid";
     public static final String ERROR_IF_PROPOSAL_TYPE_IS_NEW_AND_S2S_SUBMISSION_TYPE_IS_CHANGED_CORRECTED = "error.s2sopportunity.s2ssubmissiontype";    
@@ -163,6 +177,7 @@ public final class KeyConstants {
     public static final String ERROR_MANDATORY_FORM_NOT_AVAILABLE = "error.mandatory.form.not.available";
     public static final String ERROR_S2SOPPORTUNITY_NOTSELECTED = "error.s2sOpportunity.notSelected";
     public static final String ERROR_RESUBMISSION_PROPOSALTYPE_IS_NEW = "error.resubmission.proposalType.is.new";
+    public static final String ERROR_RESUBMISSION_INVALID_PROPOSALTYPE_SUBMISSIONTYPE = "error.resubmission.invalidProposalTypeSubmissionType";
     public static final String ERROR_RESUBMISSION_OPPORTUNITY_IS_APPLICATION = "error.resubmission.s2sOpportunity.isApplication";
     public static final String ERROR_NOT_SELECTED_SUBMISSION_TYPE = "error.submissionType.notSelected";
     // Grants.gov System Parameters
@@ -182,16 +197,29 @@ public final class KeyConstants {
     public static final String ERROR_LINEITEM_ENDDATE_AFTER_PERIOD_ENDDATE = "error.lineItemEndDate.after.periodEndDate";
     public static final String ERROR_PERSONNELBUDGETLINEITEM_STARTDATE_BEFORE_LINEITEM_STARTDATE = "error.personnelBudgetLinelineItemStartDate.before.lineItemStartDate";
     public static final String ERROR_PERSONNELBUDGETLINEITEM_ENDDATE_AFTER_LINEITEM_ENDDATE = "error.personnelBudgetLineItemEndDate.after.lineItemEndDate";
+    public static final String ERROR_PERCENT_EFFORT_INVALID = "error.percentEffortInvalid";
+    public static final String ERROR_PERCENT_CHARGED_INVALID = "error.percentChargedInvalid";
     public static final String ERROR_PERCENT_EFFORT_LESS_THAN_PERCENT_CHARGED = "error.percentEffort.lessThan.percentCharged";
     public static final String ERROR_COST_ELEMENT_NOT_SELECTED = "error.costElement.notSelected";
+    public static final String ERROR_BUDGET_PERSONNEL_NOT_SELECTED = "error.budgetPersonnel.notSelected";
+    public static final String ERROR_BUDGET_PERSONNEL_INCOMPLETE = "error.budgetPersonnel.incomplete";
     public static final String ERROR_GLOBAL_MESSAGE = "error.global.application";
     public static final String WARNING_PERIOD_COST_LIMIT_EXCEEDED= "warning.periodCostLimit.exceeded";
+    public static final String WARNING_UNRECOVERED_FA_NEGATIVE= "warning.unrecoveredFA.negative";
 
     // special review enhancement
     public static final String ERROR_EXEMPT_NUMBER_SELECTED = "error.exempt.number.selected";
     public static final String ERROR_EXPIRATION_DATE_PAST = "error.expiration.date.past";
     public static final String AUDIT_ERROR_BUDGETPERIOD_START_BEFORE_PROJECT_START_DATE = "error.periodStartDate.before.projectStartDate";
     public static final String AUDIT_ERROR_BUDGETPERIOD_END_AFTER_PROJECT_END_DATE = "error.periodEndDate.after.projectEndDate";
+    public static final String AUDIT_WARNING_BUDGETPERIOD_START_AFTER_PROJECT_START_DATE = "warning.budgetStartDate.after.projectStartDate";
+    public static final String AUDIT_WARNING_BUDGETPERIOD_END_BEFORE_PROJECT_END_DATE = "warning.budgetEndDate.before.projectEndDate";
+
+    public static final String AUDIT_ERROR_BUDGET_DISTRIBUTION_SOURCE_MISSING = "error.budget.distribution.sourceMissing";
+    public static final String AUDIT_ERROR_BUDGET_DISTRIBUTION_FISCALYEAR_MISSING = "error.budget.distribution.fiscalYearMissing";
+    public static final String AUDIT_ERROR_BUDGET_DISTRIBUTION_UNALLOCATED_NOT_ZERO = "error.budget.distribution.unallocatedMustBeZero";
+    public static final String AUDIT_WARNING_BUDGET_DISTRIBUTION_FISCALYEAR_INCONSISTENT = "warning.budget.distribution.fiscalYearInconsistent";
+    public static final String AUDIT_ERROR_BUDGET_DISTRIBUTION_DUPLICATE_UNRECOVERED_FA = "error.budget.distribution.duplicate.unrecoveredFA";
     
     // Change Password
     public static final String ERROR_EMPTY_PASSWORD = "error.password.empty";
@@ -202,6 +230,8 @@ public final class KeyConstants {
     public static final String ERROR_LEAD_UNIT_REQUIRED = "error.leadUnit.required";
     
     public static final String ERROR_RATE_TYPE_NOT_EXIST = "error.rateType.not.exist";
+    public static final String ERROR_RATE_TYPE_NOT_VALID_FOR_TYPE = "error.rateType.not.valid.for.type";
+    public static final String ERROR_RATE_CLASS_NOT_VALID_FOR_TYPE = "error.rateClass.not.valid.for.type";
     
     public static final String ERROR_EFFECTIVE_DATE_OUT_OF_RANGE = "error.effective.date.out.of.range";
     public static final String ERROR_IMCOMPLETE_PERSON_ENTRIES = "error.incomplete.person.entries";
@@ -218,11 +248,13 @@ public final class KeyConstants {
     public static final String QUESTION_SAVE_BUDGET_SUMMARY_FOR_RATE_AND_DATE_CHANGE = "document.question.saveBudgetSummary.forRateAndDateChange.text";
     public static final String QUESTION_DELETE_BUDGET_PERIOD = "document.question.deleteBudgetPeriod.text";
     public static final String QUESTION_SYNCH_BUDGET_RATE = "document.question.synchBudgetRate.text";
+    public static final String QUESTION_NO_RATES_ATTEMPT_SYNCH = "document.question.noRatesSynch.text";
 
     public static final String ERROR_DELETE_PERSON_WITH_PERSONNEL_DETAIL = "error.delete.person.with.personnelDetails";
     public static final String ERROR_DELETE_LINE_ITEM = "error.delete.lineitem";
     public static final String ERROR_INSERT_BUDGET_PERIOD = "error.insert.budget.period";
     public static final String ERROR_APPLY_TO_LATER_PERIODS = "error.applyTo.later.periods";
+    public static final String ERROR_PERSONNELLINEITEM_APPLY_TO_LATER_PERIODS = "error.personnelLineItem.applyTo.later.periods";
 
     public static final String PROPOSAL_DATA_OVERRIDE_SAME_VALUE = "error.proposalData.override.samevalue";
     public static final String PROPOSAL_EDITABLECOLUMN_DATATYPE_MISMATCH = "error.column.datatype.mismatch";
@@ -240,13 +272,32 @@ public final class KeyConstants {
     public static final String ERROR_PERSONNEL_DETAIL_START_DATE = "error.personnel.detail.start.date";
     public static final String ERROR_LINE_ITEM_END_DATE = "error.line.item.end.date";
     public static final String ERROR_LINE_ITEM_START_DATE = "error.line.item.start.date";
-    public static final String QUESTION_DEFAULT_BUDGET_PERIODs = "document.question.defaultBudgetPeriods.text";
+    public static final String QUESTION_DEFAULT_BUDGET_PERIODS = "document.question.defaultBudgetPeriods.text";
     public static final String AUDIT_WARNING_RATE_OUT_OF_SYNC = "warning.rate.outofsync";
     public static final String ERROR_DUPLICATE_PERSON = "error.duplicate.person";
 
     // Budget Version
     public static final String BUDGET_VERSION_EXISTS = "error.budgetVersion.exists";
     
+    public static final String MOVE_UNIT_OWN_DESCENDANTS = "error_move_unit";
+
+    public static final String PERSONNEL_CATEGORY = "P";
+    public static final String CANNOT_SYNC_TO_ZERO_LIMIT = "error.sync.to.zero.limit";
+    public static final String TOTAL_COST_ALREADY_IN_SYNC = "total.cost.already.sync";
+    public static final String INSUFFICIENT_AMOUNT_TO_SYNC = "insufficient.amount.to.sync";
+    public static final String PERSONNEL_LINE_ITEM_EXISTS = "personnelLineItem.exists";
+    
+    // budget personnel details
+    public static final String ERROR_REQUIRED_PERIOD_TYPE = "error.required.periodTypeCode";
+    public static final String ERROR_PERSONNEL_EXISTS = "error.personnelExists";
+    public static final String ERROR_SUMMARY_LINEITEM_EXISTS = "error.summaryLineItemExists";
+    public static final String ERROR_SUMMARY_LINEITEM_EXISTS_ADD_PERSON = "error.summaryLineItemExists.addPerson";
+    public static final String ERROR_PERSON_JOBCODE_CHANGE = "error.person.jobcodeChange";
+    public static final String ERROR_PERSON_JOBCODE_VALUE = "error.person.jobcodeValue";
+    //public static final String ERROR_DUPLICATE_PERSON = "error.duplicate.personnel";
+    public static final String ERROR_JOBCODE_COST_ELEMENT_COMBO_INVALID = "error.jobCode.costElement.invalid";
+    public static final String ERROR_SAVE_JOBCODE_COST_ELEMENT_COMBO_INVALID = "error.save.jobCode.costElement.invalid";
+   
     // Award Approved Equipment
     public static final String ERROR_AWARD_APPROVED_EQUIPMENT_AMOUNT_REQUIRED = "error.required";
     public static final String ERROR_AWARD_APPROVED_EQUIPMENT_ITEM_REQUIRED = ERROR_AWARD_APPROVED_EQUIPMENT_AMOUNT_REQUIRED;
@@ -317,8 +368,7 @@ public final class KeyConstants {
     public static final String ERROR_END_DATE_BEFORE_START_DATE_INDIRECT_COST_RATE = "error.endDate.before.startDate.for.valid.indirectCostRate";
     public static final String ERROR_FISCAL_YEAR_INCORRECT_FORMAT = "error.fiscalYear.IncorrectFormat";
     
-    //Award Reports
-    public static final String ERROR_REQUIRED = "error.required";    
+    //Award Reports   
     public static final String ERROR_REQUIRED_CONTACT_TYPE = "error.required.contact.type";
     public static final String ERROR_REQUIRED_ROLODEX_ID = "error.required.rolodex.id";
     public static final String ERROR_EMPTY_REPORT_TERMS = "error.empty.report.terms";
@@ -456,5 +506,11 @@ public final class KeyConstants {
     // IRB Questions
     public static final String QUESTION_PROTOCOL_CONFIRM_SUBMIT_FOR_REVIEW = "question.protocol.confirmExceedMaxProtocols";
     
-    private KeyConstants() {}
+    /**
+     * private utility class ctor.
+     * @throws UnsupportedOperationException if called.
+     */
+    private KeyConstants() {
+        throw new UnsupportedOperationException("do not call me");
+    }
 }
