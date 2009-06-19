5d088ee27823c268e671d42e38e52371985850d7
Travis Schneeberger <leo.herbie@gmail.com>
2009-06-19 14:57:28 +0000

    removing import KCIRB-142

--- src/main/java/org/kuali/kra/bo/FundingSourceType.java
+++ src/main/java/org/kuali/kra/bo/FundingSourceType.java
@@ -17,8 +17,6 @@ package org.kuali.kra.bo;
 
 import java.util.LinkedHashMap;
 
-import org.kuali.kra.irb.protocol.ProtocolFundingSource;
-
 public class FundingSourceType extends KraPersistableBusinessObjectBase {
 
     /**
