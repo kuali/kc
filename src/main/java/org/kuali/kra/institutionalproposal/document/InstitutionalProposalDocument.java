8892014229a570031a0a54734800f50b8c097854
Jeff Covey <jcovey@rsmart.com>
2009-07-08 17:37:48 +0000

    KRACOEUS-2487: artifacts for changes made during wiring up of front and back end for Institutional Proposal.

--- src/main/java/org/kuali/kra/institutionalproposal/document/InstitutionalProposalDocument.java
+++ src/main/java/org/kuali/kra/institutionalproposal/document/InstitutionalProposalDocument.java
@@ -15,25 +15,116 @@
  */
 package org.kuali.kra.institutionalproposal.document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.kuali.kra.document.ResearchDocumentBase;
+import org.kuali.kra.institutionalproposal.home.InstitutionalProposal;
 
 /**
- * This class...
+ * 
+ * This class represents the InstitutionalProposalDocument Object.
+ * InstitutionalProposalDocument has a 1:1 relationship with InstitutionalProposal Business Object.
+ * We have declared a list of InstitutionalProposal BOs in the InstitutionalProposalDocument at the same time to
+ * get around the OJB anonymous keys issue of primary keys of different data types.
+ * Also we have provided convenient getter and setter methods so that to the outside world;
+ * InstitutionalProposal and InstitutionalProposalDocument can have a 1:1 relationship.
  */
 public class InstitutionalProposalDocument extends ResearchDocumentBase {
 
     /**
+     * Comment for <code>DOCUMENT_TYPE_CODE</code>
+     */
+    public static final String DOCUMENT_TYPE_CODE = "INPR";
+    
+    /**
      * Comment for <code>serialVersionUID</code>
      */
     private static final long serialVersionUID = 5101782927161970631L;
+    
+    
+    private List<InstitutionalProposal> institutionalProposalList;
+    
+    /**
+     * Constructs a InstitutionalProposalDocument object
+     */
+    public InstitutionalProposalDocument(){        
+        super();        
+        init();
+    }
+    
+    /**
+     * 
+     * This method is a convenience method for facilitating a 1:1 relationship between InstitutionalProposalDocument 
+     * and InstitutionalProposal to the outside world - aka a single InstitutionalProposal field associated with InstitutionalProposalDocument
+     * @return
+     */
+    public InstitutionalProposal getInstitutionalProposal() {
+        return institutionalProposalList.get(0);
+    }
+
+    /**
+     * 
+     * This method is a convenience method for facilitating a 1:1 relationship between InstitutionalProposalDocument 
+     * and InstitutionalProposal to the outside world - aka a single InstitutionalProposal field associated with InstitutionalProposalDocument
+     * @param institutionalProposal
+     */
+    public void setInstitutionalProposal(InstitutionalProposal institutionalProposal) {
+        institutionalProposalList.set(0, institutionalProposal);
+    }
+   
+    /**
+     *
+     * @return
+     */
+    public List<InstitutionalProposal> getInstitutionalProposalList() {
+        return institutionalProposalList;
+    }
+
+    /**
+     *
+     * @param institutionalProposalList
+     */
+    public void setInstitutionalProposalList(List<InstitutionalProposal> institutionalProposalList) {
+        this.institutionalProposalList = institutionalProposalList;
+    }
 
     /**
      * @see org.kuali.kra.document.ResearchDocumentBase#getDocumentTypeCode()
      */
     @Override
     public String getDocumentTypeCode() {
-        // TODO Auto-generated method stub
-        return null;
+        return DOCUMENT_TYPE_CODE;
     }
+    
+    protected void init() {
+        institutionalProposalList = new ArrayList<InstitutionalProposal>();
+        institutionalProposalList.add(new InstitutionalProposal());
+        //populateCustomAttributes();
+    }
+    
+    /**
+     * 
+     * @see org.kuali.core.bo.PersistableBusinessObjectBase#buildListOfDeletionAwareLists()
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public List buildListOfDeletionAwareLists() {
+        List managedLists = super.buildListOfDeletionAwareLists();       
+        
+        InstitutionalProposal institutionalProposal = getInstitutionalProposal();
+        
+        return managedLists;
+    }
+    
+    /**
+     * This method populates the customAttributes for this document.
+     */
+    //@Override
+    //public void populateCustomAttributes() {
+        //AwardCustomAttributeService awardCustomAttributeService = KraServiceLocator.getService(AwardCustomAttributeService.class);
+        //Map<String, CustomAttributeDocument> customAttributeDocuments = awardCustomAttributeService.getDefaultAwardCustomAttributeDocuments();
+        //setCustomAttributeDocuments(customAttributeDocuments);
+   // }
 
 }
