11e6b8c988d327768a6a3bb56ca6813bc1930d6d
Tyler Wilson <tyler.wilson@colostate.edu>
2009-07-13 19:22:48 +0000

    Reconfiguring SQL Release scripts.

--- src/main/config/sql/log/KCIRB-421.sql
+++ src/main/config/sql/log/KCIRB-421.sql
@@ -1,4 +1,4 @@
-insert into KIM_PERMISSIONS_T (ID,NAME,DESCRIPTION,NAMESPACE_ID) values(63,'MODIFY_ANY_PROTOCOL','Modify Any Protocol Document', 2); 
-insert into KIM_ROLES_PERMISSIONS_T (ROLE_ID,PERMISSION_ID) values(10,63); 
+insert into KIM_PERMISSIONS_T (ID,NAME,DESCRIPTION,NAMESPACE_ID) values(62,'MODIFY_ANY_PROTOCOL','Modify Any Protocol Document', 2); 
+insert into KIM_ROLES_PERMISSIONS_T (ROLE_ID,PERMISSION_ID) values(10,62); 
 
 commit;
