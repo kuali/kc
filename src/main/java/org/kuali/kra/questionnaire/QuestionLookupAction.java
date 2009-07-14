997adf4fbd10ef5110b3bf7c71dd9b902dbcebb5
Lin-Long Shyu <lshyukuali@gmail.com>
2009-07-14 20:54:59 +0000

    kcirb-372

--- src/main/java/org/kuali/kra/questionnaire/QuestionLookupAction.java
+++ src/main/java/org/kuali/kra/questionnaire/QuestionLookupAction.java
@@ -15,8 +15,57 @@
  */
 package org.kuali.kra.questionnaire;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 import org.kuali.rice.kns.web.struts.action.KualiAction;
 
 public class QuestionLookupAction extends KualiAction {
+
+    @Override
+    public ActionForward refresh(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        // TODO Auto-generated method stub
+        return super.refresh(mapping, form, request, response);
+    }
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        // TODO Auto-generated method stub
+        ActionForward forward = super.execute(mapping, form, request, response);
+        String lookupType = request.getParameter("lookupType");
+        if (StringUtils.isNotBlank(lookupType)) {
+            if (lookupType.equals("single")) {
+                forward = mapping.findForward("singleLookup");
+            } else if (lookupType.equals("multivalue")) {
+                forward = mapping.findForward("multiLookup");
+            }
+        }
+        return forward;
+    }
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, ServletRequest request, ServletResponse response)
+            throws Exception {
+        // TODO Auto-generated method stub
+        ActionForward forward = super.execute(mapping, form, request, response);
+        String lookupType = request.getParameter("lookupType");
+        if (StringUtils.isNotBlank(lookupType)) {
+            if (lookupType.equals("single")) {
+                forward = mapping.findForward("singleLookup");
+            } else if (lookupType.equals("multivalue")) {
+                forward = mapping.findForward("multiLookup");
+            }
+        }
+        return forward;
+
+    }
 
 }
