d5ce3999173a04b0a36d725606d6e235bd9109fd
Claus Niesen <cniesen@iastate.edu>
2009-07-30 14:08:56 +0000

    KCIRB-350 Fixed refresh button for question response type

--- src/main/java/org/kuali/kra/maintenance/KraMaintenanceDocumentAction.java
+++ src/main/java/org/kuali/kra/maintenance/KraMaintenanceDocumentAction.java
@@ -15,8 +15,55 @@
  */
 package org.kuali.kra.maintenance;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.questionnaire.question.Question;
+import org.kuali.rice.kns.document.MaintenanceDocumentBase;
 import org.kuali.rice.kns.web.struts.action.KualiMaintenanceDocumentAction;
+import org.kuali.rice.kns.web.struts.form.KualiMaintenanceForm;
 
 public class KraMaintenanceDocumentAction extends KualiMaintenanceDocumentAction{
+    /**
+     * 
+     * This method gets called upon clicking the refresh button to display the newly selected
+     * question response type when javascript is turned off on the browser. 
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    public ActionForward loadQuestionResponse(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+        KualiMaintenanceForm maintenanceForm = (KualiMaintenanceForm) form;
+        MaintenanceDocumentBase maintenanceDocument = (MaintenanceDocumentBase) maintenanceForm.getDocument();
+        Question question = (Question) maintenanceDocument.getNewMaintainableObject().getBusinessObject();
+        question.refreshReferenceObject("questionType");
+
+        return mapping.findForward(Constants.MAPPING_BASIC );
+    }  
+
+    /**
+     * 
+     * This method gets called upon clicking of refresh pulldown menu buttons on the screen
+     * to populate the drop down menus afresh based on other parameters.
+     * @param mapping
+     * @param form
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    public ActionForward refreshPulldownOptions(ActionMapping mapping, ActionForm form,
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return mapping.findForward(Constants.MAPPING_BASIC );
+    }
 }
