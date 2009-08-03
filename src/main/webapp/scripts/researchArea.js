72a36c5ebf153def3464d249a7442be266d190ed
Lin-Long Shyu <lshyukuali@gmail.com>
2009-08-03 20:35:22 +0000

    kcirb-17

--- src/main/webapp/scripts/researchArea.js
+++ src/main/webapp/scripts/researchArea.js
@@ -11,7 +11,7 @@
       
     $(document).ready(function(){
       $.ajaxSettings.cache = false; 
-      $("#example").treeview({
+      $("#researcharea").treeview({
                  toggle: function() {
                      var idstr=$(this).attr("id").substring(4);
                      var tagId = "listcontrol"+idstr;
@@ -120,10 +120,10 @@
            
            
              if (raExist == 'false') {
-                 var ulTag = $("#example");
+                 var ulTag = $("#researcharea");
                                                             
                  var item_text = trNode.children('td:eq(1)').children('input:eq(0)').attr("value") +" : "+trNode.children('td:eq(2)').children('input:eq(0)').attr("value");
-                 var listitem = setupListItem(item_text);
+                 var listitem = setupListItem(trNode.children('td:eq(1)').children('input:eq(0)').attr("value") , trNode.children('td:eq(2)').children('input:eq(0)').attr("value"));
                  // alert(listitem.html());
               // need this ultag to force to display folder.
               var childUlTag = $('<ul></ul>').attr("id","ul"+i);
@@ -131,7 +131,7 @@
                  listitem.appendTo(ulTag);
                  // alert("ultagid "+ulTag.attr("id").substring(2));
                  // force to display folder icon
-                 $("#example").treeview({
+                 $("#researcharea").treeview({
                      add: listitem
                  // toggle: function() {
                  // var subul=this.getElementsByTagName("ul")[0]
@@ -173,9 +173,15 @@
         },
         success: function(xml){
            // alert("success"+xml);
+ //for (var k=0; k< 10; k++) {       	
            $(xml).find('h3').each(function(){
            var item_text = $(this).text();
            i++;
+           var racode = item_text.substring(0,item_text.indexOf("%3A")).trim();
+           item_text = item_text.replace("%3A",":");
+          // if (i < 4 ) {
+        	//   alert(item_text+"-"+racode);
+          // }	   
            var id = "item"+i;
            var tagId = "listcontrol"+i;
            var divId = "listcontent"+i;
@@ -199,6 +205,9 @@
           }
            var tag = $('<a style = "margin-left:2px;" ></a>').attr("id",tagId).html(idDiv);
            var div = $('<div  class="hierarchydetail" style="margin-top:2px; "></div>').attr("id",divId);
+       	   var hidracode = $('<input type="hidden" id = "racode" name = "racode" />').attr("id",
+    			"racode" + i).attr("name", "racode" + i).attr("value",racode);
+       	   hidracode.appendTo(div);
            // $(document).ready(function () {
                tag.click(
                       function()
@@ -231,15 +240,16 @@
            // need this ultag to force to display folder.
            var childUlTag = $('<ul></ul>').attr("id","ul"+i);
            childUlTag.appendTo(listitem);
-           listitem.appendTo('ul#example');
+           listitem.appendTo('ul#researcharea');
                    // also need this to show 'folder' icon
-           $('#example').treeview({
+           $('#researcharea').treeview({
               add: listitem
               
            });
           // setupListItem(item_text).appendTo('ul#browser');
        
            });
+// } // end test loop 'for'          
         }
        });  
       // return false;
@@ -256,7 +266,7 @@
          var link = $('<a href="#" class="hidedetail"><img src="kr/static/images/tinybutton-hide.gif" align="absmiddle" border="0" width="45" height="15"></a>');
          // var tag = $('<th class="subelementheader"
 			// align="left"></th>').attr("id","raHeader"+i).html(name);
-         var tag = $('<th  style="background:#939393;height:18px;color:#FFFFFF;text-align:left;padding-left:4px;" align="left"></th>').attr("id","raHeader"+i).html(name);
+         var tag = $('<th  style="background:#939393;height:18px;color:#FFFFFF;text-align:left;padding-left:4px;" align="left"></th>').attr("id","raHeader"+id.substring(4)).html(name);
          link.prependTo(tag);
          tag = $('<tr></tr>').html(tag);
          tag = $('<thead></thead>').html(tag);
@@ -280,17 +290,17 @@
                         if ($(liId).parents('li:eq(0)').size() == 0) {
                            parentRACode = '000001';
                         } else {
-                           parentRACode = getResearchAreaCode($(liId).parents('li:eq(0)').children('a:eq(0)').text());
+                           parentRACode = getResearchAreaCode($(liId).parents('li:eq(0)'));
                         }
                         // alert (parentRACode);
-                        addSqlScripts("remove(("+getResearchAreaCode($(liId).children('a:eq(0)').text())+"))");
+                        addSqlScripts("remove(("+getResearchAreaCode($(liId))+"))");
                         if (deletedNodes == '') {
-                            deletedNodes=getResearchAreaCode($(liId).children('a:eq(0)').text());
+                            deletedNodes=getResearchAreaCode($(liId));
                         } else {
-                            deletedNodes=deletedNodes+";"+getResearchAreaCode($(liId).children('a:eq(0)').text());
+                            deletedNodes=deletedNodes+";"+getResearchAreaCode($(liId));
                         }
-                        if (newNodes != ";" && newNodes.indexOf(";"+getResearchAreaCode($(liId).children('a:eq(0)').text())+";") > -1) {
-                        	newNodes = newNodes.replace(";"+getResearchAreaCode($(liId).children('a:eq(0)').text())+";",";");
+                        if (newNodes != ";" && newNodes.indexOf(";"+getResearchAreaCode($(liId))+";") > -1) {
+                        	newNodes = newNodes.replace(";"+getResearchAreaCode($(liId))+";",";");
                             //alert("newnodes "+newNodes);
                         }   
                                    
@@ -321,7 +331,7 @@
                         }   
                         if (removedNode) {
                             removedNode.appendTo(ulTag);
-                            addSqlScripts(getInsertClause(getResearchAreaCode(removedNode.children('a:eq(0)').text()), getResearchAreaCode(name), getResearchAreaDescription(removedNode.children('a:eq(0)').text())));
+                            addSqlScripts(getInsertClause(getResearchAreaCode(removedNode), getResearchAreaCode($("#"+id)), getResearchAreaDescription(getResearchAreaCode($("#"+id)), removedNode.children('a:eq(0)').text())));
                             removedNode = null;
                         } else {
                             var liId = cutNode.attr("id");
@@ -334,13 +344,13 @@
                             if ($("li#"+liId).parents('li:eq(0)').size() == 0) {
                                 parentRACode = '000001';
                             } else {
-                               parentRACode = getResearchAreaCode($("li#"+liId).parents('li:eq(0)').children('a:eq(0)').text());
+                               parentRACode = getResearchAreaCode($("li#"+liId).parents('li:eq(0)'));
                             }
                             
-                            addSqlScripts(getDeleteClause(getResearchAreaCode(cutNode.children('a:eq(0)').text()), parentRACode));
+                            addSqlScripts(getDeleteClause(getResearchAreaCode(cutNode), parentRACode));
                             $("li#"+liId).remove();
                             cutNode.appendTo(ulTag);
-                            addSqlScripts(getInsertClause(getResearchAreaCode(cutNode.children('a:eq(0)').text()), getResearchAreaCode(name), getResearchAreaDescription(cutNode.children('a:eq(0)').text())));
+                            addSqlScripts(getInsertClause(getResearchAreaCode(cutNode), getResearchAreaCode($("#"+id)), getResearchAreaDescription(getResearchAreaCode($("#"+id)),cutNode.children('a:eq(0)').text())));
                             cutNode = null;
                         }
                         ulTag.appendTo(parentNode);
@@ -388,13 +398,13 @@
         // alert(getResearchAreaDescription(name));
       } else {
          // alert($("ul#"+ulTagId).parents('li:eq(0)').children('a:eq(0)').size());
-        tdTag1 = $('<td></td>').html(getResearchAreaCode($("ul#"+ulTagId).parents('li:eq(0)').children('a:eq(0)').text()));
+        tdTag1 = $('<td></td>').html(getResearchAreaCode($("ul#"+ulTagId).parents('li:eq(0)')));
       }
       trTag1.html(tag1);
       tdTag1.appendTo(trTag1);
-      tdTag1 = $('<td></td>').html(getResearchAreaCode(name));
+      tdTag1 = $('<td></td>').html(getResearchAreaCode($("#"+id)));
       tdTag1.appendTo(trTag1);
-      tdTag1 = $('<td></td>').html($('<input type="text" name="m3" style="width:100%;" readonly="true"/>').attr("id","cdesc"+i).attr("value",getResearchAreaDescription(name)));
+      tdTag1 = $('<td></td>').html($('<input type="text" name="m3" style="width:100%;" readonly="true"/>').attr("id","cdesc"+i).attr("value",getResearchAreaDescription(getResearchAreaCode($("#"+id)), name)));
       tdTag1.appendTo(trTag1);
       tag1 = $('<th class="infoline" style="text-align:center;"></th>');
       var editlink = $('<a href="#"><img src="static/images/tinybutton-edit1.gif" width="40" height="15" border="0" title="update"></a>').attr("id","editRA"+i).click(function() {
@@ -402,17 +412,16 @@
             // $("#raHeader"+i) will not work because "i" is evaluated when this
 			// function is called; not when this function is created
             //alert ($(this).attr("id").substring(6));
-            
             var desc = editResearchArea($(this).attr("id").substring(6));
             
             if (desc.length == 0) {
                 alert("Research area can not be empty ");
             } else if ($("#cdesc"+$(this).attr("id").substring(6)).attr("value") != desc) {   
             	$("#cdesc"+$(this).attr("id").substring(6)).attr("value", desc);
-            var newdesc = getResearchAreaCode(header.text())+" : "+desc;
+            var newdesc = getResearchAreaCode($("#item"+$(this).attr("id").substring(6)))+" : "+desc;
             header.html(newdesc);
             $("#itemText"+$(this).attr("id").substring(6)).html(newdesc);
-            addSqlScripts(getUpdateClause(getResearchAreaCode(header.text()), desc));
+            addSqlScripts(getUpdateClause(getResearchAreaCode($("#item"+$(this).attr("id").substring(6))), desc));
             // lots of trouble to update the description on item, so add
 			// additional 'div' tag for this purposes.
             // tried many different ways, include 'replace', but it did not
@@ -428,7 +437,7 @@
     // 4th tr
       var trTag2 = $('<tr></tr>');
       var tag2 = $('<th style="text-align:right;"></th>').html('Add:');
-      var tdTag2 = $('<td></td>').html(getResearchAreaCode(name));
+      var tdTag2 = $('<td></td>').html(getResearchAreaCode($("#"+id)));
       trTag2.html(tag2);
       tdTag2.appendTo(trTag2);
       tdTag2 = $('<td></td>').html($('<input type="text" name="m2" value="" style="width:100%;" maxlength="8" size="8"/>').attr("id","researchCode"+i));
@@ -485,15 +494,15 @@
                  ulTag.appendTo(parentNode); 
                                                             
                  var item_text = trNode.children('td:eq(1)').children('input:eq(0)').attr("value") +" : "+trNode.children('td:eq(2)').children('input:eq(0)').attr("value");
-                 var listitem = setupListItem(item_text);
+                 var listitem = setupListItem(trNode.children('td:eq(1)').children('input:eq(0)').attr("value") ,trNode.children('td:eq(2)').children('input:eq(0)').attr("value"));
                  // alert(listitem.html());
               // need this ultag to force to display folder.
               var childUlTag = $('<ul></ul>').attr("id","ul"+i);
               childUlTag.appendTo(listitem);
                  listitem.appendTo(ulTag);
-                 alert("ultagid "+ulTag.attr("id").substring(2));
+                 //alert("ultagid "+ulTag.attr("id").substring(2));
                  // force to display folder icon
-                 $("#example").treeview({
+                 $("#researcharea").treeview({
                      add: listitem
                  // toggle: function() {
                  // var subul=this.getElementsByTagName("ul")[0]
@@ -504,7 +513,7 @@
                 	 newNodes=newNodes+trNode.children('td:eq(1)').children('input:eq(0)').attr("value")+";";
           
                  // apend to sqlScripts
-                 addSqlScripts(getInsertClause(trNode.children('td:eq(1)').children('input:eq(0)').attr("value"), getResearchAreaCode(name), trNode.children('td:eq(2)').children('input:eq(0)').attr("value")));
+                 addSqlScripts(getInsertClause(trNode.children('td:eq(1)').children('input:eq(0)').attr("value"), getResearchAreaCode($("#"+id)), trNode.children('td:eq(2)').children('input:eq(0)').attr("value")));
                  // alert("sqlScripts = "+sqlScripts);
               }  else {
                    alert ("Research Area Code already exist");
@@ -546,20 +555,21 @@
 	 * set up area of resear list tag. the main table detail is not set up
 	 * initially.
 	 */
-  function setupListItem(name) {
+  function setupListItem(code, name) {
               i++;
+              //alert(code+"-"+name);
               var id1 = "item"+i;
               var tagId = "listcontrol"+i;
               var divId = "listcontent"+i;
               var idDiv;
               if ( jQuery.browser.msie ) { 
-                  idDiv = $('<div></div>').attr("id","itemText"+i).html(name); // for
+                  idDiv = $('<div></div>').attr("id","itemText"+i).html(code +" : "+name); // for
 																				// later
 																				// change
 																				// RA
 																				// description
               } else {
-                  idDiv = $('<span>').attr("id","itemText"+i).html(name); // for
+                  idDiv = $('<span>').attr("id","itemText"+i).html(code +" : "+name); // for
 																			// later
 																			// change
 																			// RA
@@ -567,6 +577,9 @@
               }
               var tag = $('<a style = "margin-left:2px;" ></a>').attr("id",tagId).html(idDiv);
               var detDiv = $('<div  class="hierarchydetail" style="margin-top:2px; "></div>').attr("id",divId);
+          	   var hidracode = $('<input type="hidden" id = "racode" name = "racode" />').attr("id",
+           			"racode" + i).attr("name", "racode" + i).attr("value",code);
+              	   hidracode.appendTo(detDiv);
          // $(document).ready(function () {
               $(tag).click(
                       function()
@@ -574,7 +587,7 @@
                           $(".hierarchydetail:not(#"+divId+")").slideUp(300);
                           if ($(this).siblings('div:eq(1)').children('table:eq(0)').size() == 0) {
                               var idx = $(this).attr("id").substring(11);
-                              tableTag(name, "item"+idx).appendTo($("#listcontent"+idx));
+                              tableTag(code +" : "+name, "item"+idx).appendTo($("#listcontent"+idx));
                               if ($("#"+divId).is(":hidden")) {
                                   // alert(divId + " hidden");
                                   // $("#listcontent"+idx).slideToggle(300);
@@ -587,7 +600,7 @@
                               // $("#"+divId).show();;
                           }  
                           // comment out temporarily
-                          loadChildrenRA(name, tagId);
+                          loadChildrenRA(code +" : "+name, tagId);
                       }
                   );
           // });
@@ -622,7 +635,7 @@
            url: 'researchAreaAjax.do',
            type: 'GET',
            dataType: 'html',
-           data:'researchAreaCode='+getResearchAreaCode(nodeName),
+           data:'researchAreaCode='+getResearchAreaCode(liNode),
            cache: false,
            async: false,
            timeout: 1000,
@@ -631,7 +644,7 @@
            },
            success: function(xml){
               // alert("success"+xml);
-              i++;
+              //i++;
               var ulTag ;
               if (liNode.children('ul').size() == 0) {
                   ulTag = $('<ul class="filetree"></ul>').attr("id","ul"+i);
@@ -649,6 +662,8 @@
               $(xml).find('h3').each(function(){
               var item_text = $(this).text();
               i++;
+              var racode = item_text.substring(0,item_text.indexOf("%3A")).trim();
+              item_text = item_text.replace("%3A",":");
               var id = "item"+i;
               var tagId = "listcontrol"+i;
               var divId = "listcontent"+i;
@@ -670,6 +685,9 @@
           }
               var tag = $('<a style = "margin-left:2px;" ></a>').attr("id",tagId).html(idDiv);
               var detDiv = $('<div  class="hierarchydetail" style="margin-top:2px; "></div>').attr("id",divId);
+         	   var hidracode = $('<input type="hidden" id = "racode" name = "racode" />').attr("id",
+              			"racode" + i).attr("name", "racode" + i).attr("value",racode);
+                 	   hidracode.appendTo(detDiv);
               tag.click(
                       function()
                       {
@@ -702,7 +720,7 @@
               childUlTag.appendTo(listitem);
               listitem.appendTo(ulTag);
               // force to display folder icon
-              $("#example").treeview({
+              $("#researcharea").treeview({
                  add: listitem
                  // toggle: function() {
                  // var subul=this.getElementsByTagName("ul")[0]
@@ -726,20 +744,21 @@
 	 * Utility function to get code from 'code : description' This need to be
 	 * refined because if code contains ':', then this is not working correctly.
 	 */
-  function getResearchAreaCode(nodeName) {
+  function getResearchAreaCode(node) {
    // TODO : this maybe problemmatic because it makes the assumption that
 	// areacode does not contain ":"
-          var endIdx = nodeName.indexOf(":");
-          return nodeName.substring(0, endIdx - 1);    
+//          var endIdx = nodeName.indexOf(":");
+//          return nodeName.substring(0, endIdx - 1);    
+       return $("#racode"+node.attr("id").substring(4)).attr("value");
   }
   
   /*
 	 * similar to getResearchAreaCode, except this is for 'description'
 	 */
-  function getResearchAreaDescription(nodeName) {
+  function getResearchAreaDescription(code, nodeName) {
 
-          var endIdx = nodeName.indexOf(":");
-          // alert(endIdx);
+          var endIdx = nodeName.indexOf(":", code.length);
+          //alert(code.length+"-"+endIdx+"-"+nodeName);
           return nodeName.substring(endIdx+2);    
           // return nodeName;
   }
@@ -830,9 +849,10 @@
            // 10 should be fine to use as check
            sqls[sqlidx++] = sqlScripts;
        }
-       alert ("save"+sqls.length+sqlScripts); 
+       //alert ("save"+sqls.length+sqlScripts); 
              
      for ( var k = 0; k < sqls.length; k++) {
+      if (sqls[k] != '') {	 
        sqlScripts = sqls[k];
        sqlScripts = sqlScripts.replace(/#;#/g, ";;;");
        $("#headermsg").html(""); // clear error message
@@ -862,6 +882,7 @@
               });
             if (retmsg == 'Success') {
            sqlScripts = "";
+           sqls[k]="";
                $('<span id="msg"/>').css("color", "black").html(
                        "Areas of Research saved successfully").appendTo(
                        $("#headermsg"));
@@ -877,7 +898,8 @@
             }	
         }
        });
-    }
+     } 
+    } // end for
           sqlidx = 0;
          
         return false;
