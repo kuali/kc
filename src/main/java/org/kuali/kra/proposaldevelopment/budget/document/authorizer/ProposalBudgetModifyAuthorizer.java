252713dfd35c12b69eb90ad4fa2a5ab5e64d9753
Geo Thomas <geot@mit.edu>
2009-08-10 17:52:05 +0000

    Jira #KCAWD-251 Changing package for ProposalBudget Authorizer

--- src/main/java/org/kuali/kra/proposaldevelopment/budget/document/authorizer/ProposalBudgetModifyAuthorizer.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/budget/document/authorizer/ProposalBudgetModifyAuthorizer.java
@@ -13,22 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.proposaldevelopment.budget.authorizer;
+package org.kuali.kra.proposaldevelopment.budget.document.authorizer;
 
 import org.kuali.kra.authorization.Task;
 import org.kuali.kra.budget.document.BudgetDocument;
 import org.kuali.kra.budget.document.authorization.BudgetTask;
 import org.kuali.kra.budget.document.authorizer.BudgetAuthorizer;
+import org.kuali.kra.budget.versions.BudgetDocumentVersion;
+import org.kuali.kra.budget.versions.BudgetVersionOverview;
 import org.kuali.kra.infrastructure.PermissionConstants;
 import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
 
 /**
- * The Budget View Authorizer checks to see if the user has 
- * the necessary permission to view a specific budget.
+ * The Budget Modify Authorizer checks to see if the user has 
+ * the necessary permission to modify a specific budget.
  *
  * @author Kuali Research Administration Team (kualidev@oncourse.iu.edu)
  */
-public class ProposalBudgetViewAuthorizer extends BudgetAuthorizer {
+public class ProposalBudgetModifyAuthorizer extends BudgetAuthorizer {
  
     /**
      * @see org.kuali.kra.proposaldevelopment.document.authorizer.ProposalAuthorizer#isAuthorized(org.kuali.rice.kns.bo.user.UniversalUser, org.kuali.kra.proposaldevelopment.web.struts.form.ProposalDevelopmentForm)
@@ -40,7 +42,26 @@ public class ProposalBudgetViewAuthorizer extends BudgetAuthorizer {
         BudgetDocument budgetDocument = budgetTask.getBudgetDocument();
         ProposalDevelopmentDocument doc = (ProposalDevelopmentDocument)budgetDocument.getParentDocument();
         
-        return kraWorkflowService.hasWorkflowPermission(username, doc) ||
-               hasProposalPermission(username, doc, PermissionConstants.VIEW_BUDGET);
+        return !kraWorkflowService.isInWorkflow(doc) &&
+               hasProposalPermission(username, doc, PermissionConstants.MODIFY_BUDGET) &&
+              !doc.getDevelopmentProposal().getSubmitFlag();        
+    }
+    
+    /**
+     * If the Budget Document completed or not?
+     * @param proposalDoc the proposal development document
+     * @param budgetDocument the budget document
+     * @return true if completed; otherwise false
+     */
+    private boolean isBudgetCompleted(ProposalDevelopmentDocument proposalDoc, BudgetDocument budgetDocument) {
+        if (!proposalDoc.getDevelopmentProposal().isProposalComplete()) {
+            return false;
+        }
+        for (BudgetDocumentVersion budgetVersion: proposalDoc.getBudgetDocumentVersions()) {
+            if (budgetVersion.getBudgetVersionOverview().isFinalVersionFlag() && budgetVersion.getBudgetVersionOverview().getBudgetVersionNumber().equals(budgetDocument.getBudget().getBudgetVersionNumber())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
