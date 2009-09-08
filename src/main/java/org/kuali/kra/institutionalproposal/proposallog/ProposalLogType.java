23c8cb5f1c7d14809c9ff2c7418c82bb5f7c504d
Geoff McGregor <gmcgrego@iu.edu>
2009-09-08 18:59:51 +0000

    KRACOEUS-2625: Proposal log

---
+++ src/main/java/org/kuali/kra/institutionalproposal/proposallog/ProposalLogType.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2006-2009 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.institutionalproposal.proposallog;
+
+import java.util.LinkedHashMap;
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+public class ProposalLogType extends KraPersistableBusinessObjectBase { 
+    
+    private static final long serialVersionUID = 1L;
+
+    private String proposalLogTypeCode; 
+    private String description; 
+    
+    public ProposalLogType() { 
+
+    } 
+    
+    public String getProposalLogTypeCode() {
+        return proposalLogTypeCode;
+    }
+
+    public void setProposalLogTypeCode(String proposalLogTypeCode) {
+        this.proposalLogTypeCode = proposalLogTypeCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /** {@inheritDoc} */
+    @Override 
+    protected LinkedHashMap<String, Object> toStringMapper() {
+        LinkedHashMap<String, Object> hashMap = new LinkedHashMap<String, Object>();
+        hashMap.put("proposalLogTypeCode", this.getProposalLogTypeCode());
+        hashMap.put("description", this.getDescription());
+        return hashMap;
+    }
+    
+}
