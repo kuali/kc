0384e01325874f9b50573a33a5da05fa1122c9b5
Jack Frosch <jfrosch@objistics.com>
2009-09-21 09:54:05 +0000

    Created package for service adapters and moved existing adapters to that package

---
+++ src/test/java/org/kuali/kra/service/impl/adapters/DocumentServiceAdapter.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.service.impl.adapters;
+
+import java.util.List;
+
+import org.kuali.rice.kew.exception.WorkflowException;
+import org.kuali.rice.kns.bo.AdHocRouteRecipient;
+import org.kuali.rice.kns.bo.Note;
+import org.kuali.rice.kns.bo.PersistableBusinessObject;
+import org.kuali.rice.kns.document.Document;
+import org.kuali.rice.kns.service.DocumentService;
+
+public class DocumentServiceAdapter implements DocumentService {
+
+    public Document acknowledgeDocument(Document document, String annotation, List adHocRecipients) throws WorkflowException {
+        return null;
+    }
+
+    public boolean addNoteToDocument(Document document, Note note) {
+        return false;
+    }
+
+    public Document approveDocument(Document document, String annotation, List adHocRoutingRecipients) throws WorkflowException {
+        return null;
+    }
+
+    public Document blanketApproveDocument(Document document, String annotation, List adHocRecipients) throws WorkflowException {
+        return null;
+    }
+
+    public Document cancelDocument(Document document, String annotation) throws WorkflowException {
+        return null;
+    }
+
+    public Document clearDocumentFyi(Document document, List adHocRecipients) throws WorkflowException {
+        return null;
+    }
+
+    public Note createNoteFromDocument(Document document, String text) throws Exception {
+        return null;
+    }
+
+    public Document disapproveDocument(Document document, String annotation) throws Exception {
+        return null;
+    }
+
+    public boolean documentExists(String documentHeaderId) {
+        return false;
+    }
+
+    public Document getByDocumentHeaderId(String documentHeaderId) throws WorkflowException {
+        return null;
+    }
+
+    public Document getByDocumentHeaderIdSessionless(String documentHeaderId) throws WorkflowException {
+        return null;
+    }
+
+    public List getDocumentsByListOfDocumentHeaderIds(Class clazz, List documentHeaderIds) throws WorkflowException {
+        return null;
+    }
+
+    public Document getNewDocument(String documentTypeName) throws WorkflowException {
+        return null;
+    }
+
+    public Document getNewDocument(Class documentClass) throws WorkflowException {
+        return null;
+    }
+
+    public PersistableBusinessObject getNoteParent(Document document, Note newNote) {
+        return null;
+    }
+
+    public void prepareWorkflowDocument(Document document) throws WorkflowException {
+
+    }
+
+    public Document routeDocument(Document document, String annotation, List adHocRoutingRecipients) throws WorkflowException {
+        return null;
+    }
+
+    public Document saveDocument(Document document) throws WorkflowException {
+        return null;
+    }
+
+    public Document saveDocument(Document document, Class kualiDocumentEventClass) throws WorkflowException {
+        return null;
+    }
+
+    public void sendAdHocRequests(Document document, String annotation, List<AdHocRouteRecipient> adHocRecipients) throws WorkflowException {
+
+    }
+
+    public Document superUserApproveDocument(Document document, String annotation) throws WorkflowException {
+        return null;
+    }
+
+    public Document superUserCancelDocument(Document document, String annotation) throws WorkflowException {
+        return null;
+    }
+
+    public Document superUserDisapproveDocument(Document document, String annotation) throws WorkflowException {
+        return null;
+    }
+
+    public void updateDocument(Document document) {
+
+    }
+}
