23c0cf142f5309fda2aef81415500fccf44f9036
Jack Frosch <jfrosch@objistics.com>
2009-09-21 09:54:05 +0000

    Created package for service adapters and moved existing adapters to that package

--- src/test/java/org/kuali/kra/service/impl/adapters/BusinessObjectServiceAdapter.java
+++ src/test/java/org/kuali/kra/service/impl/adapters/BusinessObjectServiceAdapter.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.service;
+package org.kuali.kra.service.impl.adapters;
 
 import java.util.Collection;
 import java.util.List;
