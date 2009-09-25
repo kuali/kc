77f1f877aaba40f75920c133f5b1849577fb34b1
Tyler Wilson <tyler.wilson@colostate.edu>
2009-09-25 22:48:12 +0000

    Release 2.0 sql updates.

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-2_0-SCRIPT/CONSTRAINTS/FK_COMM_SCHEDULE_ATTENDANCE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-2_0-SCRIPT/CONSTRAINTS/FK_COMM_SCHEDULE_ATTENDANCE.sql
@@ -1,14 +1,4 @@
-ALTER TABLE comm_schedule_attendance 
-    ADD CONSTRAINT fk_csa_schedule_id_new 
-            FOREIGN KEY (schedule_id_new) 
-                REFERENCES comm_schedule (ID) ;
-
-ALTER TABLE comm_schedule_attendance 
-    ADD CONSTRAINT fk_csa_person_id 
-            FOREIGN KEY (person_id) 
-                REFERENCES person (person_id) ;
-
-ALTER TABLE comm_schedule_attendance 
-    ADD CONSTRAINT fk_csa_rolodex_id 
-            FOREIGN KEY (rolodex_id) 
-                REFERENCES rolodex (rolodex_id) ; 
+-- Foreign Key Constraint(s) 
+ALTER TABLE COMM_SCHEDULE_ATTENDANCE 
+    ADD CONSTRAINT FK_COMM_SCHEDULE_ATTENDANCE FOREIGN KEY (SCHEDULE_ID_FK) 
+                REFERENCES COMM_SCHEDULE (ID) ;
