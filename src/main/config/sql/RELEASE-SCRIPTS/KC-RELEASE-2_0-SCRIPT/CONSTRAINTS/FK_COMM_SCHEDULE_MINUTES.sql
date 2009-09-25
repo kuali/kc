0fceee120244f578fdbf9609e219d8b7b0f6ffd4
Tyler Wilson <tyler.wilson@colostate.edu>
2009-09-25 22:48:12 +0000

    Release 2.0 sql updates.

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-2_0-SCRIPT/CONSTRAINTS/FK_COMM_SCHEDULE_MINUTES.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-2_0-SCRIPT/CONSTRAINTS/FK_COMM_SCHEDULE_MINUTES.sql
@@ -1,14 +1,8 @@
-ALTER TABLE comm_schedule_minutes 
-    ADD CONSTRAINT fk_csm_schedule_id_fk 
-            FOREIGN KEY (schedule_id_fk) 
-                REFERENCES COMM_SCHEDULE (ID) ;
+-- Foreign Key Constraint(s) 
+ALTER TABLE COMM_SCHEDULE_MINUTES 
+    ADD CONSTRAINT FK_COMM_SCHEDULE_MINUTES_3 FOREIGN KEY (PROTOCOL_CONTINGENCY_CODE) 
+                REFERENCES PROTOCOL_CONTINGENCY (PROTOCOL_CONTINGENCY_CODE) ;
 
-ALTER TABLE comm_schedule_minutes 
-    ADD CONSTRAINT fk_csm_protocol_id_fk 
-            FOREIGN KEY (protocol_id_fk) 
-                REFERENCES PROTOCOL (protocol_id) ;
-
-ALTER TABLE comm_schedule_minutes 
-    ADD CONSTRAINT fk_csm_submission_id_fk 
-            FOREIGN KEY (submission_id_fk) 
-                REFERENCES PROTOCOL_SUBMISSION (submission_id) ; 
+ALTER TABLE COMM_SCHEDULE_MINUTES 
+    ADD CONSTRAINT FK_COMM_SCHEDULE_MINUTES_2 FOREIGN KEY (MINUTE_ENTRY_TYPE_CODE) 
+                REFERENCES MINUTE_ENTRY_TYPE (MINUTE_ENTRY_TYPE_CODE) ;
