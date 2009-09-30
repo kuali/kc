034fe0e015f598e6574e461e61cccf7e5961bc5a
Douglas Pace <dpace@kuali.org>
2009-09-30 18:22:22 +0000

    KCAWD-374: Missed one link on Institutional Proposal.

--- src/test/resources/sql/dml/load_system_params.sql
+++ src/test/resources/sql/dml/load_system_params.sql
@@ -442,6 +442,10 @@ INSERT INTO krns_PARM_T
 INSERT INTO krns_PARM_T 
 	(nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd) 
 	VALUES 
+	('KC-IP','D','InstitutionalProposalUnitAdministratorHelpUrl',sys_guid(),1,'HELP','default.htm','Institutional Proposal Unit Administrator Help','A');	
+INSERT INTO krns_PARM_T 
+	(nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd) 
+	VALUES 
 	('KC-AWARD','D','awardHelpUrl',sys_guid(),1,'HELP','default.htm','Award Help','A');
 INSERT INTO krns_PARM_T 
 	(nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd) 
