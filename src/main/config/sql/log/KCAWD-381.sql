fb9f141c0b2b5431050f638229c864ca8741db1b
Tyler Wilson <tyler.wilson@colostate.edu>
2009-10-07 04:52:40 +0000

    Release 2.0 sql updates.

---
+++ src/main/config/sql/log/KCAWD-381.sql
@@ -0,0 +1,5 @@
+INSERT INTO krns_PARM_T 
+	(nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd) 
+	VALUES 
+	('KC-IP','D','InstitutionalProposalUnitAdministratorHelpUrl',sys_guid(),1,'HELP','default.htm','Institutional Proposal Unit Administrator Help','A');	
+commit;
