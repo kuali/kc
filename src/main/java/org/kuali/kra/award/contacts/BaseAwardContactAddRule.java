46f0eb13b49dec214a785afe8095a821562e1e1d
Jack Frosch <jfrosch@objistics.com>
2009-10-08 20:32:16 +0000

    KCAWD-269: Improved error handling when adding a contact

--- src/main/java/org/kuali/kra/award/contacts/BaseAwardContactAddRule.java
+++ src/main/java/org/kuali/kra/award/contacts/BaseAwardContactAddRule.java
@@ -21,20 +21,45 @@ import org.kuali.rice.kns.util.GlobalVariables;
  * Common rule processing for adds
  */
 public class BaseAwardContactAddRule {
+    public static final String ERROR_AWARD_CONTACT_REQUIRED = "error.award.contact.person.required";
     public static final String ERROR_AWARD_CONTACT_ROLE_REQUIRED = "error.award.contact.person.role.required";
-    
+
     /**
-     * Verify a contact is slected and role picked
+     * Verify contact AND role are selected
      * @param newContact
      * @return
      */
     boolean checkForSelectedContactAndRole(AwardContact newContact, String errorPath) {
-        boolean valid = newContact.getContact() != null & newContact.getContactRole() != null;
+        return checkForSelectedContact(newContact, errorPath) & checkForSelectedContactRole(newContact, errorPath);
+    }
+
+    /**
+     * Verify a contact is selected
+     * @param newContact
+     * @return
+     */
+    boolean checkForSelectedContact(AwardContact newContact, String errorPath) {
+        boolean valid = newContact.getContact() != null;
         
         if(!valid) {
-            GlobalVariables.getErrorMap().putError(errorPath, ERROR_AWARD_CONTACT_ROLE_REQUIRED);
+            GlobalVariables.getMessageMap().putError(errorPath, ERROR_AWARD_CONTACT_REQUIRED);
         }
         
+        return valid;
+    }
+
+    /**
+     * Verify a contact role is picked
+     * @param newContact
+     * @return
+     */
+    boolean checkForSelectedContactRole(AwardContact newContact, String errorPath) {
+        boolean valid = newContact.getContactRole() != null;
+
+        if(!valid) {
+            GlobalVariables.getMessageMap().putError(errorPath, ERROR_AWARD_CONTACT_ROLE_REQUIRED);
+        }
+
         return valid;
     }
 }
