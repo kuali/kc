c1c66d703e11c0ef1215a3807fc92da2c8e90232
Lin-Long Shyu <lshyukuali@gmail.com>
2009-10-16 23:45:28 +0000

    kcirb-476,kcirb-477,kcirb-478,kcirb-479,kcirb-480,

--- src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
+++ src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
@@ -595,6 +595,7 @@ public final class KeyConstants {
     public static final String ERROR_EMPTY_PERSON = "error.empty.person";
     public static final String ERROR_DUPLICATE_ALTERNATE_FOR = "error.duplicate.alternateFor";
     public static final String ERROR_EMPTY_PROTOCOL = "error.empty.protocol";
+    public static final String ERROR_EMPTY_PROTOCOL_CONTINGENCY = "error.empty.protocol.contingency";
     public static final String ERROR_NON_EMPTY_PROTOCOL = "error.non.empty.protocol";
 
     
