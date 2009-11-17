4d1d2acd9bb8c8d8b92833300d4099bb619b4847
Edward Storm <erstorm@ucdavis.edu>
2009-11-17 17:08:58 +0000

    KRACOEUS-2828:  Adding authorizer for rejecting documents as part of clean-up to allow child documents to be rejected.

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/document/authorizer/RejectProposalAuthorizer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2006-2009 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.document.authorizer;
+
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.infrastructure.PermissionConstants;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
+import org.kuali.kra.proposaldevelopment.document.authorization.ProposalTask;
+import org.kuali.kra.proposaldevelopment.hierarchy.ProposalHierarchyException;
+import org.kuali.kra.proposaldevelopment.hierarchy.service.ProposalHierarchyService;
+import org.kuali.rice.kew.service.WorkflowDocument;
+import org.kuali.rice.kns.workflow.service.KualiWorkflowDocument;
+
+/**
+ * This authorizer determines if the user has the permission
+ * to reject a proposal.  You can reject if:
+ * 1) The document is not at route level 0. ( initiated )
+ * 2) You have an approval pending on the document.
+ * 3) The document state is enroute.
+ * 
+ * 
+ */
+public class RejectProposalAuthorizer extends ProposalAuthorizer {
+
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(RejectProposalAuthorizer.class);
+    /**
+     * @see org.kuali.kra.proposaldevelopment.document.authorizer.ProposalAuthorizer#isAuthorized(org.kuali.rice.kns.bo.user.UniversalUser, org.kuali.kra.proposaldevelopment.web.struts.form.ProposalDevelopmentForm)
+     */
+    public boolean isAuthorized(String username, ProposalTask task) {
+        ProposalDevelopmentDocument doc = task.getDocument();
+        KualiWorkflowDocument workDoc = doc.getDocumentHeader().getWorkflowDocument();
+        return (!workDoc.getRouteHeader().isCompleteRequested()) && (workDoc.getRouteHeader().getDocRouteLevel() > 0 ) && (workDoc.isApprovalRequested()) && (workDoc.stateIsEnroute());
+    }
+    
+    
+}
