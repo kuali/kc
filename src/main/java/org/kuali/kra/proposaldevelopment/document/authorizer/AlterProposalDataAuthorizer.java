80b34d5ca3019747ac8732d69d60b95fbb47820d
Edward Storm <erstorm@ucdavis.edu>
2009-11-20 00:41:15 +0000

    KRACOEUS-2828:  Child proposal with a parent enroute cannot be altered.

--- src/main/java/org/kuali/kra/proposaldevelopment/document/authorizer/AlterProposalDataAuthorizer.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/document/authorizer/AlterProposalDataAuthorizer.java
@@ -15,9 +15,12 @@
  */
 package org.kuali.kra.proposaldevelopment.document.authorizer;
 
+import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.infrastructure.PermissionConstants;
 import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
 import org.kuali.kra.proposaldevelopment.document.authorization.ProposalTask;
+import org.kuali.kra.proposaldevelopment.hierarchy.ProposalHierarchyException;
+import org.kuali.kra.proposaldevelopment.hierarchy.service.ProposalHierarchyService;
 
 /**
  * The Alter Proposal Data Authorizer will only a user to
@@ -26,16 +29,33 @@ import org.kuali.kra.proposaldevelopment.document.authorization.ProposalTask;
  *       1. The proposal has been submitted to workflow and is enroute.
  *       2. The proposal has not been submitted to a sponsor.
  *       3. The user has permission to alter the data for the proposal.
+ *       4. The proposal is not a child of a proposal in approval routing.
  */
 public class AlterProposalDataAuthorizer extends ProposalAuthorizer {
 
+    org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(AlterProposalDataAuthorizer.class);
     /**
      * @see org.kuali.kra.proposaldevelopment.document.authorizer.ProposalAuthorizer#isAuthorized(org.kuali.rice.kns.bo.user.UniversalUser, org.kuali.kra.proposaldevelopment.web.struts.form.ProposalDevelopmentForm)
      */
     public boolean isAuthorized(String userId, ProposalTask task) {
         ProposalDevelopmentDocument doc = task.getDocument();
-        return kraWorkflowService.isEnRoute(doc) &&
-               !doc.getDevelopmentProposal().getSubmitFlag() &&
-               hasProposalPermission(userId, doc, PermissionConstants.ALTER_PROPOSAL_DATA);
-    }
+        //standard is authorized calculation without taking child status into account.
+        boolean ret = kraWorkflowService.isEnRoute(doc) &&
+        !doc.getDevelopmentProposal().getSubmitFlag() &&
+        hasProposalPermission(userId, doc, PermissionConstants.ALTER_PROPOSAL_DATA);
+       
+        //check to see if the parent is enroute, if so deny the edit attempt.
+        if( doc.getDevelopmentProposal().isChild() ) {
+            ProposalHierarchyService hService = KraServiceLocator.getService( ProposalHierarchyService.class);
+            try {
+                if( hService.getParentWorkflowDocument(doc).stateIsEnroute() )
+                    ret = false;
+            } catch (ProposalHierarchyException e) {
+                LOG.error( String.format( "Exception looking up parent of DevelopmentProposal %s, authorizer is going to deny edit access to this child.", doc.getDevelopmentProposal().getProposalNumber()),e );
+                ret = false;
+            }
+        }
+        return ret;
+    }      
+     
 }
