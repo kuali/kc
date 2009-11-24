a726256f2d7aa6b94b671dda270b10fa1c2dc6d4
Travis Schneeberger <leo.herbie@gmail.com>
2009-11-24 15:44:37 +0000

    KRACOEUS-3012

---
+++ src/test/java/org/kuali/kra/document/AllUnitTestsSuite.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.document;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({ActivityTypeMaintenanceDocumentTest.class,
+    BudgetCategoryMaintenanceDocumentTest.class,
+    BudgetCategoryMapMaintenanceDocumentTest.class,
+    BudgetCategoryMappingMaintenanceDocumentTest.class,
+    BudgetCategoryTypeMaintenanceDocumentTest.class,
+    CostElementMaintenanceDocumentTest.class,
+    CustomAttributeDocumentMaintenanceDocumentTest.class,
+    CustomAttributeMaintenanceDocumentTest.class,
+    InstituteLaRateMaintenanceDocumentTest.class,
+    InstituteLaRateMaintenanceDocumentTest.class,
+    OrganizationMaintenanceDocumentTest.class,
+    RateClassMaintenanceDocumentTest.class,
+    RateClassTypeMaintenanceDocumentTest.class,
+    RateTypeMaintenanceDocumentTest.class,
+    ResearchDocumentBaseTest.class,
+    ValidCalcTypeMaintenanceDocumentTest.class,
+    ValidCeRateTypeMaintenanceDocumentTest.class})
+@RunWith(Suite.class)
+public class AllUnitTestsSuite {
+//suite for all doc tests
+}
