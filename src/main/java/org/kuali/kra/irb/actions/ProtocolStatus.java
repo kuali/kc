af315c9a16d1ce3e3931bb0bdd32835937b95901
Don Barre <dbarre@arizona.edu>
2009-12-08 00:16:43 +0000

    KCIRB-534 : Add Expire business logic service layer

--- src/main/java/org/kuali/kra/irb/actions/ProtocolStatus.java
+++ src/main/java/org/kuali/kra/irb/actions/ProtocolStatus.java
@@ -35,6 +35,7 @@ public class ProtocolStatus extends KraPersistableBusinessObjectBase {
     public static final String SUSPENDED_BY_PI = "302";
     public static final String SUSPENDED_BY_DSMB = "311";
     public static final String SUSPENDED_BY_IRB = "308";
+    public static final String EXPIRED = "305";
     
 	private String protocolStatusCode; 
 	private String description; 
