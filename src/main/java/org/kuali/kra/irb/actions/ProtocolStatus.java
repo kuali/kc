1afca980e5429168f4b634cda48ae550312a4516
Don Barre <dbarre@arizona.edu>
2009-12-09 00:04:23 +0000

    KCIRB-529 : Add Close Enrollment business logic in the service layer

--- src/main/java/org/kuali/kra/irb/actions/ProtocolStatus.java
+++ src/main/java/org/kuali/kra/irb/actions/ProtocolStatus.java
@@ -38,6 +38,7 @@ public class ProtocolStatus extends KraPersistableBusinessObjectBase {
     public static final String EXPIRED = "305";
     public static final String CLOSED_ADMINISTRATIVELY = "300";
     public static final String TERMINATED_BY_IRB = "307";
+    public static final String ACTIVE_CLOSED_TO_ENROLLMENT = "201";
     
 	private String protocolStatusCode; 
 	private String description; 
