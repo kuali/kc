8a0ea27d013ac7aa43ab2333d9144abd3f0ad563
Don Barre <dbarre@arizona.edu>
2009-12-10 20:55:50 +0000

    KCIRB-513 : Add Approve business logic in the service layer (fix stupid error)

--- src/main/java/org/kuali/kra/irb/auth/ApproveProtocolAuthorizer.java
+++ src/main/java/org/kuali/kra/irb/auth/ApproveProtocolAuthorizer.java
@@ -27,8 +27,7 @@ public class ApproveProtocolAuthorizer extends ProtocolAuthorizer {
      * @see org.kuali.kra.irb.auth.ProtocolAuthorizer#isAuthorized(java.lang.String, org.kuali.kra.irb.auth.ProtocolTask)
      */
     public boolean isAuthorized(String userId, ProtocolTask task) {
-        return true;
-      //  return canExecuteAction(task.getProtocol(), ProtocolActionType.APPROVED) &&
-            //   hasPermission(userId, task.getProtocol(), PermissionConstants.MAINTAIN_PROTOCOL_SUBMISSIONS);
+        return canExecuteAction(task.getProtocol(), ProtocolActionType.APPROVED) &&
+               hasPermission(userId, task.getProtocol(), PermissionConstants.MAINTAIN_PROTOCOL_SUBMISSIONS);
     }
 }
