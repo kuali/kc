26125d736ad60fbe2070d1e9be2cbb44ee844ed8
Tyler Wilson <tyler.wilson@colostate.edu>
2009-12-18 06:00:21 +0000

    DB Updates

---
+++ src/main/config/sql/log/KCAWD-468.sql
@@ -0,0 +1,5 @@
+update SPONSOR_TERM 
+set DESCRIPTION ='ONR, "Grant General Terms and Conditions for Educational Institutions, Nonprofit Organizations, and State and Local Government"' 
+where SPONSOR_TERM_ID = 157;
+
+commit;
