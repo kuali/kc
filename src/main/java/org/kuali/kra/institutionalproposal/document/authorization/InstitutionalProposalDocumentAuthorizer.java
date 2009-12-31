de0a9f46215afd79e810a4264ff267f22bc4762c
Geoff McGregor <gmcgrego@iu.edu>
2009-12-31 19:32:30 +0000

    KRACOEUS-3155: AuthZ for Institutional Proposal

--- src/main/java/org/kuali/kra/institutionalproposal/document/authorization/InstitutionalProposalDocumentAuthorizer.java
+++ src/main/java/org/kuali/kra/institutionalproposal/document/authorization/InstitutionalProposalDocumentAuthorizer.java
@@ -15,56 +15,72 @@
  */
 package org.kuali.kra.institutionalproposal.document.authorization;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 
-import org.kuali.kra.authorization.KcTransactionalDocumentAuthorizerBase;
-import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.institutionalproposal.document.InstitutionalProposalDocument;
-import org.kuali.kra.service.KraWorkflowService;
-import org.kuali.rice.kim.bo.Person;
-import org.kuali.rice.kns.authorization.AuthorizationConstants;
-import org.kuali.rice.kns.document.Document;
+import org.kuali.kra.kim.bo.KcKimAttributes;
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.document.authorization.TransactionalDocumentAuthorizer;
+import org.kuali.rice.kns.document.authorization.TransactionalDocumentAuthorizerBase;
 
 /**
  * This class is the Institutional Proposal Document Authorizer.  It determines the edit modes and
  * document actions for all institutional proposal documents.
  */
-public class InstitutionalProposalDocumentAuthorizer extends KcTransactionalDocumentAuthorizerBase {
-
-    /**
-     * @see org.kuali.rice.kns.document.authorization.TransactionalDocumentAuthorizer#getEditModes(org.kuali.rice.kns.document.Document, org.kuali.rice.kim.bo.Person, java.util.Set)
-     */
-    public Set<String> getEditModes(Document document, Person user, Set<String> oldEditModes) {
-        Set<String> editModes = new HashSet<String>();
-        InstitutionalProposalDocument institutionalProposalDocument = (InstitutionalProposalDocument) document;
-        if (getKraWorkflowService().isInWorkflow(institutionalProposalDocument) || institutionalProposalDocument.isViewOnly()) {
-            editModes.add(AuthorizationConstants.EditMode.VIEW_ONLY);
-        } else {
-            editModes.add(AuthorizationConstants.EditMode.FULL_ENTRY);
-        }
-        return editModes;
-    }
-
-    /**
-     * @see org.kuali.rice.kns.document.authorization.DocumentAuthorizer#canInitiate(java.lang.String, org.kuali.rice.kim.bo.Person)
-     */
-    public boolean canInitiate(String documentTypeName, Person user) {
-        return true;
-    }
-
-    /**
-     * @see org.kuali.rice.kns.document.authorization.DocumentAuthorizer#canOpen(org.kuali.rice.kns.document.Document, org.kuali.rice.kim.bo.Person)
-     */
-    public boolean canOpen(Document document, Person user) {
-        return true;
-    }
+public class InstitutionalProposalDocumentAuthorizer extends TransactionalDocumentAuthorizerBase 
+		implements TransactionalDocumentAuthorizer {
     
-    /**
-     * @return
-     */
-    protected KraWorkflowService getKraWorkflowService() {
-        return KraServiceLocator.getService(KraWorkflowService.class);
+    @Override
+    protected void addRoleQualification(
+            BusinessObject primaryBusinessObjectOrDocument,
+            Map<String, String> attributes) {
+        super.addRoleQualification(primaryBusinessObjectOrDocument, attributes);
+        InstitutionalProposalDocument institutionalProposalDocument = (InstitutionalProposalDocument) primaryBusinessObjectOrDocument;
+        if (institutionalProposalDocument.getInstitutionalProposal() != null && 
+                institutionalProposalDocument.getInstitutionalProposal().getLeadUnit() != null) {
+            attributes.put(KcKimAttributes.UNIT_NUMBER, institutionalProposalDocument.getInstitutionalProposal().getLeadUnit().getUnitNumber());
+        } else {
+            attributes.put(KcKimAttributes.UNIT_NUMBER, "*");
+        }
     }
     
 }
+
+//public class InstitutionalProposalDocumentAuthorizer extends KcTransactionalDocumentAuthorizerBase {
+//
+//    /**
+//     * @see org.kuali.rice.kns.document.authorization.TransactionalDocumentAuthorizer#getEditModes(org.kuali.rice.kns.document.Document, org.kuali.rice.kim.bo.Person, java.util.Set)
+//     */
+//    public Set<String> getEditModes(Document document, Person user, Set<String> oldEditModes) {
+//        Set<String> editModes = new HashSet<String>();
+//        InstitutionalProposalDocument institutionalProposalDocument = (InstitutionalProposalDocument) document;
+//        if (getKraWorkflowService().isInWorkflow(institutionalProposalDocument) || institutionalProposalDocument.isViewOnly()) {
+//            editModes.add(AuthorizationConstants.EditMode.VIEW_ONLY);
+//        } else {
+//            editModes.add(AuthorizationConstants.EditMode.FULL_ENTRY);
+//        }
+//        return editModes;
+//    }
+//
+//    /**
+//     * @see org.kuali.rice.kns.document.authorization.DocumentAuthorizer#canInitiate(java.lang.String, org.kuali.rice.kim.bo.Person)
+//     */
+//    public boolean canInitiate(String documentTypeName, Person user) {
+//        return true;
+//    }
+//
+//    /**
+//     * @see org.kuali.rice.kns.document.authorization.DocumentAuthorizer#canOpen(org.kuali.rice.kns.document.Document, org.kuali.rice.kim.bo.Person)
+//     */
+//    public boolean canOpen(Document document, Person user) {
+//        return true;
+//    }
+//    
+//    /**
+//     * @return
+//     */
+//    protected KraWorkflowService getKraWorkflowService() {
+//        return KraServiceLocator.getService(KraWorkflowService.class);
+//    }
+//    
+//}
