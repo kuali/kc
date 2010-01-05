3709a571be9cdd8e7416a804bb1d043eefb4ccf1
Claus Niesen <cniesen@iastate.edu>
2010-01-05 17:43:07 +0000

    KCIRB-550 - Changed committee lookup to drop down list - Initial work on adding template add support

---
+++ src/main/java/org/kuali/kra/irb/correspondence/ProtocolCorrespondenceTemplateService.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2006-2009 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.correspondence;
+
+public interface ProtocolCorrespondenceTemplateService {
+
+    /**
+     * 
+     * This method add a ProtocolCorrespondenceTemplate
+     * @param correspondenceType - the protocol correspondence type to which the template is to be added.
+     * @param correspondenceTemplate - the protocol correspondence template to be added
+     */
+    void addProtocolCorrespondenceTemplate(ProtocolCorrespondenceType correspondenceType, ProtocolCorrespondenceTemplate correspondenceTemplate);
+}
