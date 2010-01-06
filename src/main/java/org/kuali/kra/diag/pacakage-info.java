d58e622eeca60c9d5a77549ddbe31c3412ea05b4
Travis Schneeberger <leo.herbie@gmail.com>
2010-01-06 19:24:56 +0000

    fixing JDK 1.5 - JDK 1.6 cross compiler issues

---
+++ src/main/java/org/kuali/kra/diag/pacakage-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * This package contains diagnostic tools.
+ * <p>
+ * Currently some of the classes are renamed temporarily to avoid some cross compiler issues
+ * related to new api methods that were introduced in java 1.6.
+ * </p>
+ */
+package org.kuali.kra.diag;
