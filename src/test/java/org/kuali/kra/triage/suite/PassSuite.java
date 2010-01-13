66084b6a88b5fa2144f141bcf0fd6520f5a3cc8f
Lin-Long Shyu <lshyukuali@gmail.com>
2010-01-13 13:43:42 +0000

    kcirb-379 : add QuestionnaireAnswerServiceTest

--- src/test/java/org/kuali/kra/triage/suite/PassSuite.java
+++ src/test/java/org/kuali/kra/triage/suite/PassSuite.java
@@ -362,8 +362,8 @@ import org.junit.runners.Suite.SuiteClasses;
     org.kuali.kra.committee.web.CommitteeScheduleWebAddSchedulePanelTest.class,
     org.kuali.kra.meeting.MinuteEntryTypeValuesFinderTest.class,
     org.kuali.kra.questionnaire.question.QuestionLookupableHelperServiceTest.class,
-    org.kuali.kra.irb.correspondence.ProtocolCorrespondenceTemplateRuleTest.class
-    
+    org.kuali.kra.irb.correspondence.ProtocolCorrespondenceTemplateRuleTest.class,
+    org.kuali.kra.questionnaire.answer.QuestionnaireAnswerServiceTest.class
 })
 /** these are all the test class where all the test methods are currently passing. */
 public class PassSuite {
