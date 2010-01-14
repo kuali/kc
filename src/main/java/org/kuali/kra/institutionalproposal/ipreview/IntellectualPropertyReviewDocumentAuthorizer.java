0514ac37bddeaa20ffa80b571eecffe6275e10ad
Geoff McGregor <gmcgrego@iu.edu>
2010-01-14 22:46:56 +0000

    KRACOEUS-3254: Repairing the Intellectual Property Review document

---
+++ src/main/java/org/kuali/kra/institutionalproposal/ipreview/IntellectualPropertyReviewDocumentAuthorizer.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.institutionalproposal.ipreview;
+
+import java.util.Map;
+
+import org.kuali.kra.kim.bo.KcKimAttributes;
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.document.authorization.MaintenanceDocumentAuthorizer;
+import org.kuali.rice.kns.document.authorization.MaintenanceDocumentAuthorizerBase;
+
+/**
+ * Custom document authorizer for IntellectualPropertyReview maintenance document.
+ * We use a custom authorizer to add Unit Number qualification for KIM role-based authorizations.
+ */
+public class IntellectualPropertyReviewDocumentAuthorizer 
+    extends MaintenanceDocumentAuthorizerBase implements MaintenanceDocumentAuthorizer {
+    
+    @Override
+    protected void addRoleQualification(
+            BusinessObject primaryBusinessObjectOrDocument,
+            Map<String, String> attributes) {
+        super.addRoleQualification(primaryBusinessObjectOrDocument, attributes);
+        MaintenanceDocument maintenanceDocument = (MaintenanceDocument) primaryBusinessObjectOrDocument;
+        IntellectualPropertyReview ipReview = (IntellectualPropertyReview) maintenanceDocument.getDocumentBusinessObject();
+        if (ipReview.getLeadUnitNumber() != null) {
+            attributes.put(KcKimAttributes.UNIT_NUMBER, ipReview.getLeadUnitNumber());
+        } else {
+            attributes.put(KcKimAttributes.UNIT_NUMBER, "*");
+        }
+    }
+
+}
