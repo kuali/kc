406e6aec16462a7b55f76e7ce0b61d9532a032eb
Jeff Covey <jcovey@rsmart.com>
2010-01-28 23:00:17 +0000

    KRACOEUS-3274

---
+++ src/main/java/org/kuali/kra/service/impl/UnitAdministratorLookupableHelperServiceImpl.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.lookup.HtmlData;
+import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
+
+/**
+ * Lookupable helper service used for proposal log lookup
+ */  
+public class UnitAdministratorLookupableHelperServiceImpl extends KualiLookupableHelperServiceImpl {
+
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 2733736916454475501L;
+    
+    @SuppressWarnings("unchecked")
+    public List<HtmlData> getCustomActionUrls(BusinessObject businessObject, List pkNames){
+        List<HtmlData> htmlDataList = new ArrayList<HtmlData>();
+        htmlDataList = super.getCustomActionUrls(businessObject, pkNames);
+        List<HtmlData> returnHtmlDataList = new ArrayList<HtmlData>();
+        for (HtmlData htmlData : htmlDataList) {
+            if(!(htmlData.getDisplayText().equals("copy") ||
+                    htmlData.getDisplayText().equals("edit"))) {
+                returnHtmlDataList.add(htmlData);
+            }
+        }
+        return returnHtmlDataList;
+    }
+    
+}
