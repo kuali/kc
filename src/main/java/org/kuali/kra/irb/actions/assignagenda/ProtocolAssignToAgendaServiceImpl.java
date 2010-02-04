33d405c0b98aabc92b71123ace77e46d01c3eabc
Jay Hulslander <jdh34@cornell.edu>
2010-02-04 20:40:49 +0000

    updated per checkstyle issues & general clean up

--- src/main/java/org/kuali/kra/irb/actions/assignagenda/ProtocolAssignToAgendaServiceImpl.java
+++ src/main/java/org/kuali/kra/irb/actions/assignagenda/ProtocolAssignToAgendaServiceImpl.java
@@ -34,70 +34,134 @@ import org.kuali.rice.kns.service.DocumentService;
  * This class implements ProtocolAssignToAgendaService.
  */
 public class ProtocolAssignToAgendaServiceImpl implements ProtocolAssignToAgendaService {
-    
+
     private DocumentService documentService;
     private ProtocolActionService protocolActionService;
-    
-    
+
+
     public void setDocumentService(DocumentService documentService) {
         this.documentService = documentService;
     }
-    
+
     public void setProtocolActionService(ProtocolActionService protocolActionService) {
         this.protocolActionService = protocolActionService;
     }
-    
+
     private ProtocolSubmission findSubmission(Protocol protocol) {
         for (ProtocolSubmission submission : protocol.getProtocolSubmissions()) {
-            if (StringUtils.equals(submission.getSubmissionStatusCode(), ProtocolSubmissionStatus.PENDING) 
+            if (StringUtils.equals(submission.getSubmissionStatusCode(), ProtocolSubmissionStatus.PENDING)
                     || StringUtils.equals(submission.getSubmissionStatusCode(), ProtocolSubmissionStatus.SUBMITTED_TO_COMMITTEE)) {
                 return submission;
             }
         }
         return null;
     }
-    
+
     /** {@inheritDoc} */
     public void assignToAgenda(Protocol protocol, ProtocolAssignToAgendaBean actionBean) throws Exception {
-        
+
         ProtocolSubmission submission = findSubmission(protocol);
-        ProtocolAction protocolActionFound = getAssignedToAgendaProtocolAction(protocol);
         if (actionBean.isProtocolAssigned()) {
-            if (protocolActionFound != null) {
-                //add a new protocol action
+            if (!isAssignedToAgenda(protocol)) {
+                // add a new protocol action
                 ProtocolAction protocolAction = new ProtocolAction(protocol, submission, ProtocolActionType.ASSIGN_TO_AGENDA);
                 protocolAction.setComments(actionBean.getComments());
                 protocolActionService.updateProtocolStatus(protocolAction, protocol);
                 protocol.getProtocolActions().add(protocolAction);
                 protocolAction.setActionDate(new Timestamp(actionBean.getScheduleDate().getTime()));
             } else {
-                //update the comment of an existing protocol action
+                // update the comment of an existing protocol action
                 ProtocolAction pa = getAssignedToAgendaProtocolAction(protocol);
                 pa.setComments(actionBean.getComments());
                 documentService.saveDocument(protocol.getProtocolDocument());
                 return;
-            }            
+            }
             documentService.saveDocument(protocol.getProtocolDocument());
-        }else if(!actionBean.isProtocolAssigned() && protocolActionFound != null) {
-            //un assign the protocol            
+        } else if (!actionBean.isProtocolAssigned() && isAssignedToAgenda(protocol)) {
+            // un assign the protocol
             ProtocolAction pa = getAssignedToAgendaProtocolAction(protocol);
             pa.setProtocolActionTypeCode(ProtocolActionType.DISAPPROVED);
             pa.setComments(actionBean.getComments());
             documentService.saveDocument(protocol.getProtocolDocument());
             return;
         }
-        
+
     }
-    
+
     /** {@inheritDoc} */
-    public ProtocolAction getAssignedToAgendaProtocolAction(Protocol protocol) {
+    public boolean isAssignedToAgenda(Protocol protocol) {
+        ProtocolAction pa = getAssignedToAgendaProtocolAction(protocol);
+        // if there is a protocol action return true, otherwise return false
+        return pa != null;
+    }
+
+    /** {@inheritDoc} */
+    public String getAssignToAgendaComments(Protocol protocol) {
+        ProtocolAction pa = getAssignedToAgendaProtocolAction(protocol);
+        if (pa == null) {
+            return "";
+        } else {
+            return pa.getComments();
+        }
+    }
+
+    private ProtocolAction getAssignedToAgendaProtocolAction(Protocol protocol) {
         Iterator<ProtocolAction> i = protocol.getProtocolActions().iterator();
         while (i.hasNext()) {
             ProtocolAction pa = i.next();
-            if (pa.getProtocolActionType().getProtocolActionTypeCode().equals(ProtocolActionType.ASSIGN_TO_AGENDA)) {                
+            if (pa.getProtocolActionType().getProtocolActionTypeCode().equals(ProtocolActionType.ASSIGN_TO_AGENDA)) {
                 return pa;
             }
-        } 
+        }
+        // no proper protocol action found, return null
+        return null;
+    }
+
+    private ProtocolAction getSubmitToIrbProtocolAction(Protocol protocol) {
+        Iterator<ProtocolAction> i = protocol.getProtocolActions().iterator();
+        while (i.hasNext()) {
+            ProtocolAction pa = i.next();
+            if (pa.getProtocolActionType().getProtocolActionTypeCode().equals(ProtocolActionType.SUBMIT_TO_IRB)) {
+                return pa;
+            }
+        }
+        // no proper protocol action found, return null
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    public String getAssignedCommitteeId(Protocol protocol) {
+        ProtocolAction pa = getSubmitToIrbProtocolAction(protocol);
+        if (pa != null) {
+            ProtocolSubmission ps = pa.getProtocolSubmission();
+            if (ps != null) {
+                return ps.getCommitteeId();
+            }
+        }
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    public String getAssignedCommitteeName(Protocol protocol) {
+        ProtocolAction pa = getSubmitToIrbProtocolAction(protocol);
+        if (pa != null) {
+            ProtocolSubmission ps = pa.getProtocolSubmission();
+            if (ps != null) {
+                return ps.getCommittee().getCommitteeName();
+            }
+        }
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    public Date getAssignedScheduleDate(Protocol protocol) {
+        ProtocolAction pa = getSubmitToIrbProtocolAction(protocol);
+        if (pa != null) {
+            ProtocolSubmission ps = pa.getProtocolSubmission();
+            if (ps != null) {
+                return ps.getCommitteeSchedule().getScheduledDate();
+            }
+        }
         return null;
     }
 }
