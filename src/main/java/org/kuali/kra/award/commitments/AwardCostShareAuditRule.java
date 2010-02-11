b421d043704670166261302fc87f0072165482e4
Douglas Pace <dpace@kuali.org>
2010-02-11 23:00:26 +0000

    KCAWD-473: Moving duplicate cost share check to auditing.

---
+++ src/main/java/org/kuali/kra/award/commitments/AwardCostShareAuditRule.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2006-2008 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.award.commitments;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kra.award.document.AwardDocument;
+import org.kuali.kra.award.home.Award;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.KeyConstants;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.lookup.keyvalue.ExtendedPersistableBusinessObjectValuesFinder;
+import org.kuali.rice.kns.document.Document;
+import org.kuali.rice.kns.lookup.keyvalues.KeyValuesFinder;
+import org.kuali.rice.kns.rule.DocumentAuditRule;
+import org.kuali.rice.kns.service.ParameterService;
+import org.kuali.rice.kns.util.AuditCluster;
+import org.kuali.rice.kns.util.AuditError;
+import org.kuali.rice.kns.util.GlobalVariables;
+
+public class AwardCostShareAuditRule implements DocumentAuditRule {
+    private static final String AUDIT_CLUSTER = "costShareAuditErrors";
+    
+    private List<AuditError> auditErrors;
+    
+    public boolean processRunAuditBusinessRules(Document document) {
+        boolean retval = true;
+        AwardDocument awardDocument = (AwardDocument)document;
+        Award award = awardDocument.getAward();
+        retval &= validateCostShareDoesNotViolateUniqueConstraint(award.getAwardCostShares());
+        if (!retval) {
+            reportAndCreateAuditCluster();            
+        }
+        return retval;
+    }
+    
+    /**
+     * This method tests that the Cost Shares do not violate unique constraint on Database table.
+     * @param awardCostShareRuleEvent
+     * @param awardCostShare
+     * @return
+     */
+    public boolean validateCostShareDoesNotViolateUniqueConstraint (List<AwardCostShare> awardCostShares) {
+        boolean valid = true;
+        for (AwardCostShare costShare1 : awardCostShares) {
+            for (AwardCostShare costShare2 : awardCostShares) {
+                if (costShare1 == costShare2) {
+                    continue;
+                } else if (StringUtils.equals(costShare1.getFiscalYear(), costShare2.getFiscalYear()) &&
+                        costShare1.getCostShareTypeCode().equals(costShare2.getCostShareTypeCode()) &&
+                            StringUtils.equalsIgnoreCase(costShare1.getSource(), costShare2.getSource()) &&
+                                StringUtils.equalsIgnoreCase(costShare1.getDestination(), costShare2.getDestination())) {
+                    valid = false;
+                    addAuditError(new AuditError("document.awardList[0].awardCostShares["+awardCostShares.indexOf(costShare1)+"].fiscalYear", 
+                            KeyConstants.ERROR_DUPLICATE_ENTRY,
+                            Constants.MAPPING_AWARD_COMMITMENTS_PAGE+"."+Constants.COST_SHARE_PANEL_ANCHOR));
+                }
+            }
+        }
+        return valid;
+    }
+
+    private void addAuditError(AuditError auditError) {
+        if(auditErrors == null) {
+            auditErrors = new ArrayList<AuditError>();            
+        }
+        auditErrors.add(auditError);
+    }
+
+
+    
+    /**
+     * This method creates and adds the AuditCluster to the Global AuditErrorMap.
+     */
+    @SuppressWarnings("unchecked")
+    protected void reportAndCreateAuditCluster() {
+        if (auditErrors.size() > 0) {
+            GlobalVariables.getAuditErrorMap().put(AUDIT_CLUSTER, new AuditCluster(Constants.COST_SHARE_PANEL_NAME, auditErrors, Constants.AUDIT_ERRORS));
+        }
+    }
+}
