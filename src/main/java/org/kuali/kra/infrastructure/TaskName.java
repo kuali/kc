7d105d240286055e09976310c7397fda2b449734
Edward Storm <erstorm@ucdavis.edu>
2010-03-09 19:54:43 +0000

    KRACOEUS-3600,  KRACOEUS-3625:  App Doc Status for Disapproved and Rejected proposal docuemnts.

--- src/main/java/org/kuali/kra/infrastructure/TaskName.java
+++ src/main/java/org/kuali/kra/infrastructure/TaskName.java
@@ -45,6 +45,7 @@ public interface TaskName {
     public static final String SUBMIT_TO_WORKFLOW = "submitToWorkflow";
     public static final String MAINTAIN_PROPOSAL_HIERARCHY = "maintainProposalHierarchy";
     public static final String PROPOSAL_HIERARCHY_CHILD_WORKFLOW_ACTION="hierarchyChildWorkflowAction";
+    public static final String PROPOSAL_HIERARCHY_CHILD_ACKNOWLEDGE_ACTION="proposal.hierarchyChildAcknowledgeAction";
     
     /*
      * Narrative Tasks.
