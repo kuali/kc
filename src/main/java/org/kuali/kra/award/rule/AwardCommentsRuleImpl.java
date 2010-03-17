c7e915a5de84a2780377a24b8cc115bf496687dc
Edward Storm <erstorm@ucdavis.edu>
2010-03-17 03:18:26 +0000

    KRACOEUS-3145: Addition of ValidAwardBasisPayment, Modification of ValidBasisMethodPayment removing frequency and changing invInstructions to support the necessary three different values.

--- src/main/java/org/kuali/kra/award/rule/AwardCommentsRuleImpl.java
+++ src/main/java/org/kuali/kra/award/rule/AwardCommentsRuleImpl.java
@@ -15,20 +15,28 @@
  */
 package org.kuali.kra.award.rule;
 
+import java.util.Collection;
+
+import org.apache.cxf.common.util.StringUtils;
 import org.kuali.kra.award.document.AwardDocument;
 import org.kuali.kra.award.home.Award;
 import org.kuali.kra.award.home.AwardComment;
+import org.kuali.kra.award.home.InvInstructionsIndicatorConstants;
+import org.kuali.kra.award.home.ValidBasisMethodPayment;
 import org.kuali.kra.award.rule.event.AwardCommentsRuleEvent;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.kra.rules.ResearchDocumentRuleBase;
+import org.kuali.kra.service.ServiceHelper;
+
 
 /**
  * This class implements rules for checking Award comments entered in text boxes.
  */
 public class AwardCommentsRuleImpl extends ResearchDocumentRuleBase implements AwardCommentsRule {
     private static final Integer MAX_COMMENT_LENGTH = 5000;
-
+    private static String[] VALID_BASIS_METHOD_PAYMENT_FINDER_FIELDS = new String[] {"basisOfPaymentCode","methodOfPaymentCode"}; 
+   
     public boolean processAwardCommentsBusinessRules(AwardCommentsRuleEvent awardCommentsRuleEvent) {
         boolean valid = true;
 
@@ -39,6 +47,7 @@ public class AwardCommentsRuleImpl extends ResearchDocumentRuleBase implements A
         valid &= checkAwardComment(awardCommentsRuleEvent, Constants.FANDA_RATE_COMMENT_TYPE_CODE, "awardFandaRateComment.comments");
         valid &= checkAwardComment(awardCommentsRuleEvent, Constants.BENEFITS_RATES_COMMENT_TYPE_CODE, "awardBenefitsRateComment.comments");
         valid &= checkAwardComment(awardCommentsRuleEvent, Constants.PAYMENT_AND_INVOICES_COMMENT_TYPE_CODE, "awardPaymentAndInvoiceRequirementsComments.comments");
+        valid &= checkPaymentAndInvoiceCommentRule( awardCommentsRuleEvent, "awardPaymentAndInvoiceRequirementsComments.comments" );
         
         return valid;
     }
@@ -68,8 +77,47 @@ public class AwardCommentsRuleImpl extends ResearchDocumentRuleBase implements A
         
         return valid;
     }
+
+    
+    @SuppressWarnings("unchecked")
+    private boolean checkPaymentAndInvoiceCommentRule( AwardCommentsRuleEvent awardCommentsRuleEvent, String errorKey ) {
+        boolean valid = true;
+        AwardDocument awardDocument = (AwardDocument)awardCommentsRuleEvent.getDocument();
+        Award award = awardDocument.getAward();
+        AwardComment awardComment = award.findCommentOfSpecifiedType(Constants.PAYMENT_AND_INVOICES_COMMENT_TYPE_CODE);
+        //lookup the ValidBasisMethodPayment object associated with the award.  If none is found for the basis and method we do not 
+        //we do not do the check for the correct value in the invoice comment here as an error will be thrown in another business
+        //rule regarding that case.
+        Collection<ValidBasisMethodPayment> results = getBusinessObjectService().findMatching( ValidBasisMethodPayment.class, ServiceHelper.getInstance().buildCriteriaMap( VALID_BASIS_METHOD_PAYMENT_FINDER_FIELDS, new Object[]{award.getBasisOfPaymentCode(),award.getMethodOfPaymentCode()}));
+        if( results.size() == 1 ) {
+            ValidBasisMethodPayment vbpay = results.iterator().next();
+            InvInstructionsIndicatorConstants indicator = vbpay.getInvInstructionsIndicatorConstant(); 
+            if( indicator != null ) {
+                switch (indicator) {
+                    case Blank:     valid = awardComment==null || StringUtils.isEmpty(awardComment.getComments());
+                                    if( !valid ) {
+                                        String fullErrorPath = awardCommentsRuleEvent.getErrorPathPrefix() + "." + errorKey;
+                                        reportError( fullErrorPath, KeyConstants.ERROR_PAYMENT_AND_INVOICE_COMMENT_NOT_ALLOWED );
+                                    }
+                                    break;
+                    case Mandatory: valid = awardComment!=null && !StringUtils.isEmpty(awardComment.getComments());
+                                    if( !valid ) {
+                                        String fullErrorPath = awardCommentsRuleEvent.getErrorPathPrefix() + "." + errorKey;
+                                        reportError( fullErrorPath, KeyConstants.ERROR_PAYMENT_AND_INVOICE_COMMENT_REQUIRED );
+                                    }
+                                    break;
+                }
+            }
+        } else if ( results.size() > 1 ) {
+            throw new IllegalStateException( String.format( "Found more than one ValidBasisMethodPayment records for basisOfPayment=%s and methodOfPayment=%s", award.getBasisOfPaymentCode(),award.getMethodOfPaymentCode() ));
+        } 
+        return valid;
+    }
     
     private boolean isAwardCommentValid(String comment) {
         return comment==null || comment.length()<=MAX_COMMENT_LENGTH;
     }
+    
+  
+    
 }
--- src/main/webapp/WEB-INF/tags/award/awardPaymentAndInvoices.tag
+++ src/main/webapp/WEB-INF/tags/award/awardPaymentAndInvoices.tag
@@ -19,8 +19,8 @@
 
 <c:set var="awardPaymentAndInvoiceRequirementsCommentAttributes" value="${DataDictionary.AwardComment.attributes}" />
 <c:set var="action" value="awardPaymentReportsAndTerms" />
-
-<kul:tabTop tabTitle="Payment & Invoices" defaultOpen="false" tabErrorKey="document.awardList[0].awardPaymentAndInvoiceRequirementsComments.comments.*,document.awardList[0].paymentsAndInvoices.*,document.award.awardTemplate.PAYMENTS_AND_INVOICES_TAB" auditCluster="paymentAndInvoicesAuditErrors" tabAuditKey="document.paymentsAuditRules*">
+																			
+<kul:tabTop tabTitle="Payment & Invoices" defaultOpen="false" tabErrorKey="document.awardList[0].awardPaymentAndInvoiceRequirementsComments.comments*,document.awardList[0].paymentsAndInvoices.*,document.award.awardTemplate.PAYMENTS_AND_INVOICES_TAB" auditCluster="paymentAndInvoicesAuditErrors" tabAuditKey="document.paymentsAuditRules*">
 	<div class="tab-container" align="center">
 		<kra-a:awardPaymentAndInvoicesTopPanel />	
 		<kra-a:awardReportClasses index="5" reportClassKey="${KualiForm.reportClassForPaymentsAndInvoices.reportClassCode}" reportCodeLabel="* Payment Type"
