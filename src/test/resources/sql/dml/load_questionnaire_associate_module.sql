4fe23902ee2a87c1a5b78a0ffa8910f3acc6fc76
Geoff McGregor <gmcgrego@iu.edu>
2010-03-25 18:00:23 +0000

    KRACOEUS-3556: Change all namespaces to follow pattern KC-*

--- src/test/resources/sql/dml/load_questionnaire_associate_module.sql
+++ src/test/resources/sql/dml/load_questionnaire_associate_module.sql
@@ -3,5 +3,5 @@ INSERT INTO KRNS_NMSPC_T (NMSPC_CD, VER_NBR, NM, ACTV_IND,OBJ_ID) VALUES ('KC-QU
 
 Insert into KRNS_PARM_DTL_TYP_T  ( nmspc_cd, parm_dtl_typ_cd, VER_NBR, NM, ACTV_IND,OBJ_ID) Values  ('KC-QUESTIONNAIRE','P',1,'Permissions','Y','c2210b38-d218-443d-b56c-d78a817a20f3');
 
-INSERT INTO KRNS_PARM_T (NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD,OBJ_ID) VALUES('KC-QUESTIONNAIRE', 'P', 'associateModuleQuestionnairePermission', 'CONFG', 'Modify ProposalDevelopmentDocument:KRA-PD;Modify Protocol:KC-PROTOCOL', 'List of permissions that are allowed to associate a module with questionnaire.', 'A','b84b2cf1-d7a7-4024-ab9c-b05d8b3dbb0c') ;
+INSERT INTO KRNS_PARM_T (NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD,OBJ_ID) VALUES('KC-QUESTIONNAIRE', 'P', 'associateModuleQuestionnairePermission', 'CONFG', 'Modify ProposalDevelopmentDocument:KC-PD;Modify Protocol:KC-PROTOCOL', 'List of permissions that are allowed to associate a module with questionnaire.', 'A','b84b2cf1-d7a7-4024-ab9c-b05d8b3dbb0c') ;
 
