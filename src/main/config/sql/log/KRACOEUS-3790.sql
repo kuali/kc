9555539e49f8a013db7be52f310a5239f640681a
Tyler Wilson <tyler.wilson@colostate.edu>
2010-03-30 20:06:33 +0000

    Release 2.0 sql updates.

---
+++ src/main/config/sql/log/KRACOEUS-3790.sql
@@ -0,0 +1,4 @@
+alter table AWARD_ATTACHMENT modify (file_id number(22));
+alter table PROTOCOL_ATTACHMENT_PERSONNEL modify (file_id number(22));
+alter table PROTOCOL_ATTACHMENT_PROTOCOL modify (file_id number(22));
+
