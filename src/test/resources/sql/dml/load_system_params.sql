0188735b46eeb3a077efa0d601154737b0ebe49a
Douglas Pace <dpace@kuali.org>
2010-04-08 22:21:47 +0000

    KRACOEUS-2765: Adding biosketch attachment to the person extended attributes and integrating it into Development Proposal.

--- src/test/resources/sql/dml/load_system_params.sql
+++ src/test/resources/sql/dml/load_system_params.sql
@@ -383,3 +383,5 @@ insert into KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, OBJ_
 
 insert into KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD) values ('kc', 'KR-WKFLW', 'DocSearchCriteriaDTO', 'DOCUMENT_SEARCH_ROUTE_LOG_POPUP_IND', '7347a46c-c7b3-445b-9e34-90dfe5a82fa1', 1, 'CONFG', 'N', 'Flag to specify if clicking on a Route Log from Document Search will load the Route Log in a new window.', 'A');
 
+insert into KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD) values ('KUALI', 'KC-PD', 'D', 'DEFAULT_BIOGRAPHY_DOCUMENT_TYPE_CODE', '81237ef9-9d29-4a07-9fb8-ebd8aaddc834', 1, 'CONFG', '1', 'Value of the default biography document type code. This is the document type code that will be used when adding new users to a Proposal Development Document when they have an attached Biosketch file.', 'A');
+
