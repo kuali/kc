5af36c8901530cbb6a1984ace3f7e40d2a1e761d
Claus Niesen <cniesen@iastate.edu>
2010-05-07 15:53:32 +0000

    KCIRB-576 History support

---
+++ src/main/java/org/kuali/kra/committee/dao/ojb/CommitteeBatchCorrespondenceDaoOjb.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2006-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.committee.dao.ojb;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.ojb.broker.query.Criteria;
+import org.apache.ojb.broker.query.Query;
+import org.apache.ojb.broker.query.QueryFactory;
+import org.kuali.kra.committee.bo.CommitteeBatchCorrespondence;
+import org.kuali.kra.committee.dao.CommitteeBatchCorrespondenceDao;
+import org.kuali.rice.kns.dao.impl.PlatformAwareDaoBaseOjb;
+import org.kuali.rice.kns.util.OjbCollectionAware;
+
+public class CommitteeBatchCorrespondenceDaoOjb extends PlatformAwareDaoBaseOjb implements OjbCollectionAware, CommitteeBatchCorrespondenceDao {
+
+    private static final Log LOG = LogFactory.getLog(CommitteeBatchCorrespondenceDaoOjb.class);
+
+    private static final String BATCH_CORRESPONDENCE_TYPE_CODE = "batchCorrespondenceTypeCode";
+    private static final String BATCH_RUN_DATE = "batchRunDate";
+
+    @SuppressWarnings("unchecked")
+    public List<CommitteeBatchCorrespondence> getCommitteeBatchCorrespondence(String batchCorrespondenceTypeCode, Date startDate, Date endDate) {
+        Criteria crit = new Criteria();
+        crit.addEqualTo(BATCH_CORRESPONDENCE_TYPE_CODE, batchCorrespondenceTypeCode);
+        if (startDate != null) {
+            crit.addGreaterOrEqualThan(BATCH_RUN_DATE, startDate);
+        }
+        if (endDate != null) {
+            crit.addLessOrEqualThan(BATCH_RUN_DATE, endDate);
+        }
+        Query q = QueryFactory.newQuery(CommitteeBatchCorrespondence.class, crit, true);
+        logQuery(q);
+        return (List<CommitteeBatchCorrespondence>) getPersistenceBrokerTemplate().getCollectionByQuery(q);
+    }
+
+    /**
+     * Logs the Query
+     * @param q the query
+     */
+    private static void logQuery(Query q) {
+        if (LOG.isDebugEnabled()) {
+            LOG.debug(q.toString());
+        }
+    }
+    
+}
