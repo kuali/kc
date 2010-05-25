aee6e53e8e2ff3176998949bbdeb0ed008834ae0
Tyler Wilson <tyler.wilson@colostate.edu>
2010-05-25 14:39:18 +0000

    Moved release 2.0 logs to archive folder

---
+++ src/main/config/sql/log/Release_2_0_logs/KRACOEUS-4018.sql
@@ -0,0 +1,7 @@
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_DTL_TYP_CD,PARM_NM,VER_NBR,PARM_TYP_CD,TXT,PARM_DESC_TXT,CONS_CD,OBJ_ID) 
+VALUES ('KUALI','KC-M','D','sponsorHierarchyHelp',1,'HELP','default.htm?turl=Documents%2Fsponsorhierarchy.htm','Sponsor Hierarchy Help','A','51FFE3D4890B4A9293E46CE58385B536');
+
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_DTL_TYP_CD,PARM_NM,VER_NBR,PARM_TYP_CD,TXT,PARM_DESC_TXT,CONS_CD,OBJ_ID) 
+VALUES ('KUALI','KC-M','D','sponsorHierarchyCreateNewHelp',1,'HELP','default.htm?turl=Documents%2Fsponsorhierarchy.htm','Sponsor Hierarchy Help','A','A169E3D48f8C4A9293E46CE58385B536');
+
+COMMIT;
