d7f3687c76bbd0b9396c8d9ee7623de45ba5f3c3
Travis Schneeberger <leo.herbie@gmail.com>
2010-06-04 20:57:41 +0000

    KRACOEUS-3901: removing generated code.

--- src/main/java/gov/grants/apply/webservices/applicantintegrationservices_v1/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://apply.grants.gov/WebServices/ApplicantIntegrationServices-V1.0", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package gov.grants.apply.webservices.applicantintegrationservices_v1;
