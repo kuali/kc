ec1d785a7a4012e9be7eb4629fa4a45a821ef957
Lin-Long Shyu <lshyukuali@gmail.com>
2010-06-25 16:48:14 +0000

    kcirb-201 : initial commit for implementing 'print' in protocol actions page.

---
+++ src/main/java/org/kuali/kra/irb/actions/print/ProtocolPrintingServiceImpl.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.actions.print;
+
+import org.kuali.kra.printing.Printable;
+import org.kuali.kra.printing.service.impl.PrintingServiceImpl;
+
+/**
+ * 
+ * This class to implement the getProtocolPrintable.
+ */
+public class ProtocolPrintingServiceImpl extends PrintingServiceImpl implements ProtocolPrintingService {
+
+    private static final String ERROR_MESSAGE = "Unknown report type specified";
+    private ProtocolFullProtocolPrint protocolFullProtocolPrint;
+    private ProtocolHistoryPrint protocolHistoryPrint;
+    private ProtocolReviewCommentsPrint protocolReviewCommentsPrint;
+    private ProtocolSummaryViewPrint protocolSummaryViewPrint;
+
+    /**
+     * 
+     * @see org.kuali.kra.irb.actions.print.ProtocolPrintingService#getProtocolPrintable(org.kuali.kra.irb.actions.print.ProtocolPrintType)
+     */
+    public Printable getProtocolPrintable(ProtocolPrintType reportType) {
+        Printable printable = null;
+        
+        switch(reportType) {
+            case PROTOCOL_FULL_PROTOCOL_REPORT :
+                printable = getProtocolFullProtocolPrint();
+                break;
+            case PROTOCOL_PROTOCOL_HISTORY_REPORT :
+                printable = getProtocolHistoryPrint();
+                break;
+            case PROTOCOL_REVIEW_COMMENTS_REPORT :
+                printable = getProtocolReviewCommentsPrint();
+                break;
+            case PROTOCOL_SUMMARY_VIEW_REPORT :
+                printable = getProtocolSummaryViewPrint();
+                break;
+            default :
+                throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+        
+        return printable;
+    }
+    
+    public ProtocolFullProtocolPrint getProtocolFullProtocolPrint() {
+        return protocolFullProtocolPrint;
+    }
+
+    public void setProtocolFullProtocolPrint(ProtocolFullProtocolPrint protocolFullProtocolPrint) {
+        this.protocolFullProtocolPrint = protocolFullProtocolPrint;
+    }
+
+    public ProtocolHistoryPrint getProtocolHistoryPrint() {
+        return protocolHistoryPrint;
+    }
+
+    public void setProtocolHistoryPrint(ProtocolHistoryPrint protocolHistoryPrint) {
+        this.protocolHistoryPrint = protocolHistoryPrint;
+    }
+
+    public ProtocolReviewCommentsPrint getProtocolReviewCommentsPrint() {
+        return protocolReviewCommentsPrint;
+    }
+
+    public void setProtocolReviewCommentsPrint(ProtocolReviewCommentsPrint protocolReviewCommentsPrint) {
+        this.protocolReviewCommentsPrint = protocolReviewCommentsPrint;
+    }
+
+    public ProtocolSummaryViewPrint getProtocolSummaryViewPrint() {
+        return protocolSummaryViewPrint;
+    }
+
+    public void setProtocolSummaryViewPrint(ProtocolSummaryViewPrint protocolSummaryViewPrint) {
+        this.protocolSummaryViewPrint = protocolSummaryViewPrint;
+    }
+
+}
