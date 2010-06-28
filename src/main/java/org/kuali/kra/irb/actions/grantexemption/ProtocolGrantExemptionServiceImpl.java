ea3ac535608a3eeaca21510822c0ef97ea5b30b7
Chitra Chandran <chitra07@msu.edu>
2010-06-28 14:56:45 +0000

    KCIRB-511

--- src/main/java/org/kuali/kra/irb/actions/grantexemption/ProtocolGrantExemptionServiceImpl.java
+++ src/main/java/org/kuali/kra/irb/actions/grantexemption/ProtocolGrantExemptionServiceImpl.java
@@ -15,10 +15,16 @@
  */
 package org.kuali.kra.irb.actions.grantexemption;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.kuali.kra.irb.Protocol;
-import org.kuali.kra.irb.ProtocolVersionService;
+import org.kuali.kra.irb.actions.ProtocolAction;
+import org.kuali.kra.irb.actions.ProtocolActionType;
+import org.kuali.kra.irb.actions.correspondence.ProtocolActionCorrespondenceGenerationService;
 import org.kuali.kra.irb.actions.submit.ProtocolActionService;
-import org.kuali.rice.kns.service.BusinessObjectService;
+import org.kuali.kra.irb.correspondence.ProtocolCorrespondenceTemplate;
+import org.kuali.kra.printing.PrintingException;
 import org.kuali.rice.kns.service.DocumentService;
 
 /**
@@ -27,9 +33,8 @@ import org.kuali.rice.kns.service.DocumentService;
 public class ProtocolGrantExemptionServiceImpl implements ProtocolGrantExemptionService {
 
     private DocumentService documentService;
-    private BusinessObjectService businessObjectService;
     private ProtocolActionService protocolActionService;
-    private ProtocolVersionService protocolVersionService;
+    private ProtocolActionCorrespondenceGenerationService protocolActionCorrespondenceGenerationService;
 
     /**
      * Set the document service.
@@ -40,14 +45,6 @@ public class ProtocolGrantExemptionServiceImpl implements ProtocolGrantExemption
     }
     
     /**
-     * Set the business object service.
-     * @param businessObjectService the business object service
-     */
-    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
-        this.businessObjectService = businessObjectService;
-    }
-    
-    /**
      * Set the Protocol Action Service.
      * @param protocolActionService
      */
@@ -55,18 +52,30 @@ public class ProtocolGrantExemptionServiceImpl implements ProtocolGrantExemption
         this.protocolActionService = protocolActionService;
     }
     
-    /**
-     * Inject Protocol Version Service
-     * @param protocolVersionService
-     */
-    public void setProtocolVersionService(ProtocolVersionService protocolVersionService) {
-        this.protocolVersionService = protocolVersionService;
+    public void setProtocolActionCorrespondenceGenerationService(
+            ProtocolActionCorrespondenceGenerationService protocolActionCorrespondenceGenerationService) {
+        this.protocolActionCorrespondenceGenerationService = protocolActionCorrespondenceGenerationService;
     }
+
+    public void generateCorrespondenceDocumentAndAttach(Protocol protocol) throws PrintingException {
+        List<ProtocolCorrespondenceTemplate> grantExemptionTemplates = 
+            protocolActionCorrespondenceGenerationService.getCorrespondenceTemplates(ProtocolActionType.GRANT_EXEMPTION);
+        protocolActionCorrespondenceGenerationService.generateCorrespondenceDocumentAndAttach(protocol, grantExemptionTemplates, ProtocolActionType.GRANT_EXEMPTION);
+    }    
 
     /**
      * @see org.kuali.kra.irb.actions.grantexemption.ProtocolGrantExemptionService#grantExemption(org.kuali.kra.irb.Protocol, org.kuali.kra.irb.actions.grantexemption.ProtocolGrantExemptionBean)
      */
-    public void grantExemption(Protocol protocol, ProtocolGrantExemptionBean withdrawBean) throws Exception {
-       
+    public void grantExemption(Protocol protocol, ProtocolGrantExemptionBean actionBean) throws Exception {
+        ProtocolAction protocolAction = new ProtocolAction(protocol, null, ProtocolActionType.GRANT_EXEMPTION); 
+        protocolAction.setComments(actionBean.getComments());
+        protocolAction.setActionDate(new Timestamp(actionBean.getActionDate().getTime()));
+        protocol.getProtocolActions().add(protocolAction);
+        protocolActionService.updateProtocolStatus(protocolAction, protocol);
+        
+        protocol.setApprovalDate(actionBean.getApprovalDate());
+        protocol.refreshReferenceObject("protocolStatus");
+        generateCorrespondenceDocumentAndAttach(protocol); 
+        documentService.saveDocument(protocol.getProtocolDocument());
     }
 }
