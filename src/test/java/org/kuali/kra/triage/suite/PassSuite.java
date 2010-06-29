a28aed31e30657993dcb38bc66cca4af53ef27ed
Jay Hulslander <jdh34@cornell.edu>
2010-06-29 14:31:43 +0000

    kcirb-514 implemented test cases for the rule

--- src/test/java/org/kuali/kra/triage/suite/PassSuite.java
+++ src/test/java/org/kuali/kra/triage/suite/PassSuite.java
@@ -443,7 +443,8 @@ import org.junit.runners.Suite.SuiteClasses;
     org.kuali.kra.institutionalproposal.htmlunitwebtest.InstitutionalProposalInstitutionalProposalWebTest.class,
     org.kuali.kra.institutionalproposal.htmlunitwebtest.InstitutionalProposalGraduateStudentWebTest.class,
     org.kuali.kra.proposaldevelopment.web.DocSearchWebTest.class,
-    org.kuali.kra.irb.actions.print.ProtocolPrintingServiceTest.class
+    org.kuali.kra.irb.actions.print.ProtocolPrintingServiceTest.class,
+    org.kuali.kra.irb.actions.decision.CommitteeDecisionRuleTest.class
 
 })
 /** these are all the test class where all the test methods are currently passing. */
