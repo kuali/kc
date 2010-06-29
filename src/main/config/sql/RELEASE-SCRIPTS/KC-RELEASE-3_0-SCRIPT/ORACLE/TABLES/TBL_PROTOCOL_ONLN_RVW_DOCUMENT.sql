5c1924b8f628cccfbd01711aed346c4101cbc3d2
Tyler Wilson <tyler.wilson@colostate.edu>
2010-06-29 13:26:32 +0000

    Release 3.0 sql updates.

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/TABLES/TBL_PROTOCOL_ONLN_RVW_DOCUMENT.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/TABLES/TBL_PROTOCOL_ONLN_RVW_DOCUMENT.sql
@@ -1,6 +1,6 @@
 CREATE TABLE PROTOCOL_ONLN_RVW_DOCUMENT (DOCUMENT_NUMBER NUMBER (10) NOT NULL,
                                          VER_NBR NUMBER (8, 0) DEFAULT 1 NOT NULL,
-                                         OBJ_ID VARCHAR2 (36) DEFAULT SYS_GUID () NOT NULL,
+                                         OBJ_ID VARCHAR2 (36) NOT NULL,
                                          UPDATE_TIMESTAMP DATE NOT NULL,
                                          UPDATE_USER VARCHAR2 (10) NOT NULL) ;
 
