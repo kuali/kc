9b516d5dc2fcda974da3e6bd614085f9797e4af4
Claus Niesen <cniesen@iastate.edu>
2010-07-02 14:24:45 +0000

    KCIRB-685 DB release scripts

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/DML/DML_BS_KRIM_RSP_T.sql
@@ -0,0 +1,4 @@
+INSERT INTO KRIM_RSP_T ( RSP_ID, OBJ_ID, VER_NBR, RSP_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND )
+VALUES ( 1035, UUID(), 1, 1, 'KC-WKFLW', 'IRB Reviewer Approve Online Review', 'Protocol Document - IRB Reviewer approves online review', 'Y' );
+
+COMMIT;
