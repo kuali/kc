4d2290c43681ce3751c5a93b31d33ac1c7857d9f
Claus Niesen <cniesen@iastate.edu>
2010-07-02 14:24:45 +0000

    KCIRB-685 DB release scripts

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/DML/DML_BS_KRIM_TYP_T.sql
@@ -0,0 +1,4 @@
+INSERT INTO KRIM_TYP_T ( KIM_TYP_ID, OBJ_ID, VER_NBR, NM, SRVC_NM, ACTV_IND, NMSPC_CD )
+VALUES ( 1016, UUID(), 1, 'Derived Role: IRB Online Reviewer', 'protocolOnlineReviewRoleTypeService', 'Y', 'KC-WKFLW' );
+
+COMMIT;
