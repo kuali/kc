fc8c59820f576b094486c4d539e105b1948f346c
Tyler Wilson <tyler.wilson@colostate.edu>
2010-07-09 20:10:53 +0000

    Post Release DB scripts to add missing sequences.

---
+++ src/main/config/sql/RELEASE-SCRIPTS/POST-RELEASE-2_0-SCRIPT/POST-2_0-2010-07-09-MYSQL.sql
@@ -0,0 +1,11 @@
+CREATE TABLE SEQUENCE_IPPUCS_ID
+(
+	id bigint(19) not null auto_increment, primary key (id) 
+) ENGINE MyISAM
+/
+
+CREATE TABLE SEQUENCE_PROPOSAL_NUMBER
+(
+	id bigint(19) not null auto_increment, primary key (id)
+) ENGINE MyISAM
+/
