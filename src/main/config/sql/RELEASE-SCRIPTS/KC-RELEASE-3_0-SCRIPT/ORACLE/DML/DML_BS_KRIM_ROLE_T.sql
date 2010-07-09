06535d8d21dcf159b1ba6d090dbb205bf613016a
Claus Niesen <cniesen@iastate.edu>
2010-07-09 16:37:03 +0000

    KCIRB-685 DB release scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/DML/DML_BS_KRIM_ROLE_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/DML/DML_BS_KRIM_ROLE_T.sql
@@ -1,4 +1,4 @@
 INSERT INTO KRIM_ROLE_T ( ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, NMSPC_CD, DESC_TXT, KIM_TYP_ID, ACTV_IND, LAST_UPDT_DT )
-VALUES( 1236, SYS_GUID(), 1, 'IRB Online Reviewer', 'KC-UNT', 'Online Reviewers', 1016, 'Y', sysdate );
+VALUES( KRIM_ROLE_ID_S.NEXTVAL, SYS_GUID(), 1, 'IRB Online Reviewer', 'KC-UNT', 'Online Reviewers', (SELECT KIM_TYP_ID FROM KRIM_TYP_T WHERE NMSPC_CD = 'KC-WKFLW' AND NM = 'Derived Role: IRB Online Reviewer'), 'Y', sysdate );
 
 COMMIT;
