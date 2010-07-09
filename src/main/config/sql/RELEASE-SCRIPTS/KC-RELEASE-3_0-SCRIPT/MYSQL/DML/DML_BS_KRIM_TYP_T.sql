b6c43af2d8532404d77e7ecb26f92c1381e98452
Claus Niesen <cniesen@iastate.edu>
2010-07-09 16:37:03 +0000

    KCIRB-685 DB release scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/DML/DML_BS_KRIM_TYP_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/DML/DML_BS_KRIM_TYP_T.sql
@@ -1,4 +1,6 @@
+INSERT INTO KRIM_TYP_ID_S VALUES (NULL);
+
 INSERT INTO KRIM_TYP_T ( KIM_TYP_ID, OBJ_ID, VER_NBR, NM, SRVC_NM, ACTV_IND, NMSPC_CD )
-VALUES ( 1016, UUID(), 1, 'Derived Role: IRB Online Reviewer', 'protocolOnlineReviewRoleTypeService', 'Y', 'KC-WKFLW' );
+SELECT MAX(ID), UUID(), 1, 'Derived Role: IRB Online Reviewer', 'protocolOnlineReviewRoleTypeService', 'Y', 'KC-WKFLW' FROM KRIM_TYP_ID_S;
 
 COMMIT;
