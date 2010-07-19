3f0311f7ac7eadf2939fa8a8d8be196172a4d384
Travis Schneeberger <leo.herbie@gmail.com>
2010-07-19 17:47:54 +0000

    moving some htmlunit failures to passsuite

--- src/test/java/org/kuali/kra/triage/suite/PassSuite.java
+++ src/test/java/org/kuali/kra/triage/suite/PassSuite.java
@@ -404,7 +404,8 @@ import org.junit.runners.Suite.SuiteClasses;
     org.kuali.kra.committee.web.CommitteeMembershipWebTest.class,
     org.kuali.kra.irb.web.ProtocolFundingSourceWebTest.class,
     org.kuali.kra.irb.web.ProtocolPersonnelWebTest.class,
-    org.kuali.kra.irb.web.ProtocolSpecialReviewWebTest.class
+    org.kuali.kra.irb.web.ProtocolSpecialReviewWebTest.class,
+    org.kuali.kra.irb.web.ProtocolActionsWebTest.class
 
 })
 /** these are all the test class where all the test methods are currently passing. */
