dfe787a348143829c487a93ca570d1d664715b2d
Claus Niesen <cniesen@iastate.edu>
2010-07-19 23:59:03 +0000

    KCIRB-691 DB release scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/DML/DML_BS_KRIM_RSP_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/DML/DML_BS_KRIM_RSP_T.sql
@@ -1,4 +1,7 @@
 INSERT INTO KRIM_RSP_T ( RSP_ID, OBJ_ID, VER_NBR, RSP_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND )
 VALUES ( KRIM_RSP_ID_S.NEXTVAL, SYS_GUID(), 1, 1, 'KC-WKFLW', 'IRB Reviewer Approve Online Review', 'Protocol Document - IRB Reviewer approves online review', 'Y' );
 
+INSERT INTO KRIM_RSP_T ( RSP_ID, OBJ_ID, VER_NBR, RSP_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND )
+VALUES ( KRIM_RSP_ID_S.NEXTVAL, SYS_GUID(), 1, 1, 'KC-WKFLW', 'IRB Admin Approve Online Review', 'Protocol Online Review Document - IRB Admin approves online review', 'Y' );
+
 COMMIT;
