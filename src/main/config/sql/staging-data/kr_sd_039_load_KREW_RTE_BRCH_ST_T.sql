5f5018f309f433ebf54c37ad8cd63f8880b549de
Tyler Wilson <tyler.wilson@colostate.edu>
2010-07-28 04:39:19 +0000

    Staging Data Scripts

--- src/main/config/sql/staging-data/kr_sd_039_load_KREW_RTE_BRCH_ST_T.sql
+++ src/main/config/sql/staging-data/kr_sd_039_load_KREW_RTE_BRCH_ST_T.sql
@@ -1,25 +1,33 @@
 --REM INSERTING into KREW_RTE_BRCH_ST_T
 Insert into KREW_RTE_BRCH_ST_T (RTE_BRCH_ST_ID,RTE_BRCH_ID,KEY_CD,VAL,VER_NBR) 
-values (3673,3671,'System.PostProcessorProcessed','true',2);
+values (3673,3671,'System.PostProcessorProcessed','true',2)
+/
 
 Insert into KREW_RTE_BRCH_ST_T (RTE_BRCH_ST_ID,RTE_BRCH_ID,KEY_CD,VAL,VER_NBR) 
-values (3674,3671,'System.PostProcessorFinal','true',1);
+values (3674,3671,'System.PostProcessorFinal','true',1)
+/
 
 Insert into KREW_RTE_BRCH_ST_T (RTE_BRCH_ST_ID,RTE_BRCH_ID,KEY_CD,VAL,VER_NBR) 
-values (3677,3675,'System.PostProcessorProcessed','true',2);
+values (3677,3675,'System.PostProcessorProcessed','true',2)
+/
 
 Insert into KREW_RTE_BRCH_ST_T (RTE_BRCH_ST_ID,RTE_BRCH_ID,KEY_CD,VAL,VER_NBR) 
-values (3678,3675,'System.PostProcessorFinal','true',1);
+values (3678,3675,'System.PostProcessorFinal','true',1)
+/
 
 Insert into KREW_RTE_BRCH_ST_T (RTE_BRCH_ST_ID,RTE_BRCH_ID,KEY_CD,VAL,VER_NBR) 
-values (3681,3679,'System.PostProcessorProcessed','true',2);
+values (3681,3679,'System.PostProcessorProcessed','true',2)
+/
 
 Insert into KREW_RTE_BRCH_ST_T (RTE_BRCH_ST_ID,RTE_BRCH_ID,KEY_CD,VAL,VER_NBR) 
-values (3682,3679,'System.PostProcessorFinal','true',1);
+values (3682,3679,'System.PostProcessorFinal','true',1)
+/
 
 Insert into KREW_RTE_BRCH_ST_T (RTE_BRCH_ST_ID,RTE_BRCH_ID,KEY_CD,VAL,VER_NBR) 
-values (3685,3683,'System.PostProcessorProcessed','true',2);
+values (3685,3683,'System.PostProcessorProcessed','true',2)
+/
 
 Insert into KREW_RTE_BRCH_ST_T (RTE_BRCH_ST_ID,RTE_BRCH_ID,KEY_CD,VAL,VER_NBR) 
-values (3686,3683,'System.PostProcessorFinal','true',1);
+values (3686,3683,'System.PostProcessorFinal','true',1)
+/
 
