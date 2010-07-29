2ac421aa0e53a6b6f1d2ef2339a271bdea2e299e
Lin-Long Shyu <lshyukuali@gmail.com>
2010-07-29 18:00:05 +0000

    kcirb-777 : protocol notification template maint

---
+++ src/main/java/org/kuali/kra/irb/actions/notification/ProtocolNotificationTemplateAuthorizationService.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.actions.notification;
+
+/**
+ * 
+ * This class is to check whether user have permission to modify or view notification template.
+ */
+public interface ProtocolNotificationTemplateAuthorizationService {
+    /**
+     * 
+     * This method checks if the current user has the specified permission.
+     * @param permissionName the name of the permission
+     * @return true if the user has permission; otherwise false
+     */
+    boolean hasPermission(String permissionName);
+
+}
