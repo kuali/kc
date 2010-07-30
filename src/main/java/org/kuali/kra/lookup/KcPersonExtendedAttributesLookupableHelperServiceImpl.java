8859b36e74cd1f45c71b79942134595ddf76e9bc
Ying Zhou <yingzhou@iastate.edu>
2010-07-30 20:40:44 +0000

    KRACOEUS-4088: added customized lookup to KcPesonExtendedAttributes lookup page

---
+++ src/main/java/org/kuali/kra/lookup/KcPersonExtendedAttributesLookupableHelperServiceImpl.java
@@ -0,0 +1,35 @@
+package org.kuali.kra.lookup;
+
+import java.util.List;
+
+import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
+import org.kuali.rice.kns.web.ui.Field;
+import org.kuali.rice.kns.web.ui.Row;
+
+public class KcPersonExtendedAttributesLookupableHelperServiceImpl extends KualiLookupableHelperServiceImpl {
+
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 8135788972031192656L;
+
+    /**
+     * @see org.kuali.rice.kns.lookup.AbstractLookupableHelperServiceImpl#getRows()
+     */
+    @Override
+    public List<Row> getRows() {
+        List<Row> rows =  super.getRows();
+        for (Row row : rows) {
+            for (Field field : row.getFields()) {
+                if (field.getPropertyName().equals("personId")) {
+                    field.setFieldConversions("principalId:personId");
+                    field.setQuickFinderClassNameImpl("org.kuali.rice.kim.bo.Person");
+                    field.setFieldDirectInquiryEnabled(true);
+                    field.setInquiryParameters("personId:principalId");
+                }
+            }
+        }
+        return rows;
+    }
+
+}
---
+++ src/test/java/org/kuali/kra/committee/service/KcPersonExtendedAttributesLookupableHelperServiceTest.java
@@ -0,0 +1,65 @@
+package org.kuali.kra.committee.service;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.kra.bo.KcPersonExtendedAttributes;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.lookup.KcPersonExtendedAttributesLookupableHelperServiceImpl;
+import org.kuali.kra.test.infrastructure.KcUnitTestBase;
+import org.kuali.rice.kns.UserSession;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.web.ui.Field;
+import org.kuali.rice.kns.web.ui.Row;
+
+public class KcPersonExtendedAttributesLookupableHelperServiceTest extends KcUnitTestBase {
+    private static final int NUMBER_LOOKUP_CRITERIA_FIELDS = 4;
+    KcPersonExtendedAttributesLookupableHelperServiceImpl kcPersonExtendedAttributesLookupableHelperServiceImpl;
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        kcPersonExtendedAttributesLookupableHelperServiceImpl = 
+            (KcPersonExtendedAttributesLookupableHelperServiceImpl) KraServiceLocator
+                .getService("kcPersonExtendedAttributesLookupableHelperService");
+        kcPersonExtendedAttributesLookupableHelperServiceImpl.setBusinessObjectClass(KcPersonExtendedAttributes.class);
+        GlobalVariables.setUserSession(new UserSession("quickstart"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+        kcPersonExtendedAttributesLookupableHelperServiceImpl = null;
+        GlobalVariables.setUserSession(null);
+    }
+
+    /**
+     * 
+     * This method is to test getrows.  The lookup fields will be updated and make sure 
+     * the properties of the field are correct
+     */
+    @Test
+    public void testGetRows() {
+
+        List<Row> rows = kcPersonExtendedAttributesLookupableHelperServiceImpl.getRows();
+        assertEquals(rows.size(), NUMBER_LOOKUP_CRITERIA_FIELDS);
+        for (Row row : rows) {
+            for (Field field : row.getFields()) {
+                if (field.getPropertyName().equals("personId")) {
+                    assertFieldProperties(field, "principalId", "org.kuali.rice.kim.bo.Person");
+                }
+            }
+        }
+    }
+
+    private void assertFieldProperties(Field field, String keyName, String className) {
+        assertEquals(field.getFieldConversions(), keyName + ":" + field.getPropertyName());
+        assertTrue(field.isFieldDirectInquiryEnabled());
+        assertEquals(field.getInquiryParameters(), field.getPropertyName() + ":" + keyName);
+        assertEquals(field.getQuickFinderClassNameImpl(), className);
+
+    }
+
+}
