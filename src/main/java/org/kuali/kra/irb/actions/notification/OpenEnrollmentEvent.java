765bf5011ec798adfc7a9bd2fb992a61f51e94af
Lin-Long Shyu <lshyukuali@gmail.com>
2010-08-02 18:36:31 +0000

    kcirb-263

--- src/main/java/org/kuali/kra/irb/actions/notification/OpenEnrollmentEvent.java
+++ src/main/java/org/kuali/kra/irb/actions/notification/OpenEnrollmentEvent.java
@@ -16,12 +16,9 @@
 package org.kuali.kra.irb.actions.notification;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
-import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.irb.Protocol;
 import org.kuali.kra.irb.actions.ProtocolActionType;
 import org.kuali.kra.irb.personnel.ProtocolPerson;
-import org.kuali.rice.kew.util.XmlHelper;
 import org.w3c.dom.Element;
 
 /**
@@ -29,8 +26,6 @@ import org.w3c.dom.Element;
  * This class is for request to re-open enrollment notification event
  */
 public class OpenEnrollmentEvent extends NotificationEventBase {
-
-    private static final Logger LOG = Logger.getLogger(OpenEnrollmentEvent.class);
 
     public OpenEnrollmentEvent() {
     }
@@ -44,19 +39,7 @@ public class OpenEnrollmentEvent extends NotificationEventBase {
      * @see org.kuali.kra.irb.actions.notification.NotificationEventBase#getRecipients(org.w3c.dom.Element)
      */
     public void getRecipients(Element recipients) {
-        try {
-            if (StringUtils.isNotBlank(getProtocol().getPrincipalInvestigator().getPersonId())) {
-                // rolodex does not have username
-                XmlHelper.appendXml(recipients, "<user>" + getProtocol().getPrincipalInvestigator().getPerson().getUserName()
-                        + "</user>");
-                // recipientUser.setTextContent(protocol.getPrincipalInvestigator().getPerson().getUserName());
-            }
-        }
-        catch (Exception e) {
-            LOG.info("Open Enrollment - get recipeint - exception " + e.getStackTrace());
-
-        }
-        getProtocolActionsNotificationService().addIrbAdminToRecipients(recipients, getProtocol());
+        super.getRecipients(recipients);
     }
 
     /**
@@ -84,10 +67,6 @@ public class OpenEnrollmentEvent extends NotificationEventBase {
                 + " is Open Enrollment";
         return messageBody;
 
-    }
-
-    private ProtocolActionsNotificationService getProtocolActionsNotificationService() {
-        return KraServiceLocator.getService(ProtocolActionsNotificationService.class);
     }
 
     public String getTemplatePath() {
