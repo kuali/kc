f2be5960cebf9b66aa4d53e03d916d992b412776
Edward Storm <erstorm@ucdavis.edu>
2010-08-03 23:07:42 +0000

    KCIRB-795: Added value finder to order ProtocolTypes in select.

---
+++ src/main/java/org/kuali/kra/irb/protocol/ProtocolTypeValuesFinder.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.osedu.org/licenses/ECL-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.protocol;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.lookup.keyvalue.KeyValueFinderService;
+import org.kuali.kra.lookup.keyvalue.PrefixValuesFinder;
+import org.kuali.rice.core.util.KeyLabelPair;
+import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
+import org.kuali.rice.kns.service.KeyValuesService;
+
+public class ProtocolTypeValuesFinder extends KeyValuesBase {
+    KeyValueFinderService keyValueFinderService= (KeyValueFinderService)KraServiceLocator.getService("keyValueFinderService");
+    
+    /**
+     * Constructs the list of Protocol Types.  Each entry
+     * in the list is a &lt;key, value&gt; pair, where the "key" is the unique
+     * status code and the "value" is the textual description that is viewed
+     * by a user.  The list is obtained from the PROTOCOL_TYPE database table
+     * via the "KeyValueFinderService".
+     * 
+     * @return the list of &lt;key, value&gt; pairs of abstract types.  The first entry
+     * is always &lt;"", "select:"&gt;.
+     * @see org.kuali.rice.kns.lookup.keyvalues.KeyValuesFinder#getKeyValues()
+     */
+    public List<KeyLabelPair> getKeyValues() {
+        KeyValuesService keyValuesService = (KeyValuesService) KraServiceLocator.getService("keyValuesService");
+        Collection protocolTypes = keyValuesService.findAllOrderBy(ProtocolType.class,"description",true);
+        List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
+        keyValues.add(0, new KeyLabelPair(PrefixValuesFinder.getPrefixKey(), PrefixValuesFinder.getDefaultPrefixValue()));    
+        for (Iterator iter = protocolTypes.iterator(); iter.hasNext();) {
+            ProtocolType protocolType = (ProtocolType) iter.next();
+            keyValues.add(new KeyLabelPair(protocolType.getProtocolTypeCode().toString(), protocolType.getDescription()));                            
+        }
+        return keyValues;
+    }
+   
+}
