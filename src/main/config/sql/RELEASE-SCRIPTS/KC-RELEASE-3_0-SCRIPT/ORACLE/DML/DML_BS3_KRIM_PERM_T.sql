a00a7961509bce0f310df7f22c2a95f5f67e4884
Tyler Wilson <tyler.wilson@colostate.edu>
2010-08-25 20:48:44 +0000

    Release 3.0 SQL scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/DML/DML_BS3_KRIM_PERM_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/DML/DML_BS3_KRIM_PERM_T.sql
@@ -16,11 +16,23 @@ VALUES (KRIM_PERM_ID_S.NEXTVAL,(SELECT PERM_TMPL_ID FROM KRIM_PERM_TMPL_T WHERE 
 INSERT INTO KRIM_PERM_T (PERM_ID, PERM_TMPL_ID, NM, DESC_TXT, ACTV_IND, NMSPC_CD, OBJ_ID)
 VALUES (KRIM_PERM_ID_S.NEXTVAL, (SELECT PERM_TMPL_ID FROM KRIM_PERM_TMPL_T WHERE NM = 'Open Document' AND NMSPC_CD = 'KR-NS'), 'View Notification Template', NULL, 'Y', 'KC-PROTOCOL', SYS_GUID () ) ;
 
-INSERT INTO KRIM_PERM_T (PERM_ID, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND, VER_NBR, OBJ_ID) 
-VALUES (KRIM_PERM_ID_S.NEXTVAL, (SELECT PERM_TMPL_ID FROM KRIM_PERM_TMPL_T WHERE NM = 'Populate Group' AND NMSPC_CD = 'KR-IDM'), 'KC-SYS', 'Populate KC Groups', 'Authorizes users to modify the information on the Assignees Tab of the Group Document and the Group section of the Membership Tab on the Person Document for groups with namespaces beginning with KC.','Y',1,SYS_GUID()); 
+-- Check for pre-existing permission
+DECLARE
+	DUMMY NUMBER;
+BEGIN
+	SELECT PERM_ID INTO DUMMY FROM KRIM_PERM_T WHERE NM = 'Populate KC Groups' AND NMSPC_CD = 'KC-SYS';
+	EXCEPTION WHEN NO_DATA_FOUND THEN 
+		INSERT INTO KRIM_PERM_T (PERM_ID, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND, VER_NBR, OBJ_ID) 
+		VALUES (KRIM_PERM_ID_S.NEXTVAL, (SELECT PERM_TMPL_ID FROM KRIM_PERM_TMPL_T WHERE NM = 'Populate Group' AND NMSPC_CD = 'KR-IDM'), 'KC-SYS', 'Populate KC Groups', 'Authorizes users to modify the information on the Assignees Tab of the Group Document and the Group section of the Membership Tab on the Person Document for groups with namespaces beginning with KC.','Y',1,SYS_GUID()); 
+END;
+/
 
 Insert into KRIM_PERM_T (PERM_ID,OBJ_ID,VER_NBR,PERM_TMPL_ID,NMSPC_CD,NM,DESC_TXT,ACTV_IND) 
 values (KRIM_PERM_ID_S.NEXTVAL,'3F0FE9D4-83BC-7CC6-5292-49E6F23FA6F0',1, 
         (SELECT T.PERM_TMPL_ID FROM KRIM_PERM_TMPL_T T WHERE T.NMSPC_CD = 'KC-IDM' AND T.NM = 'Perform Document Action'),
         'KC-PROTOCOL','Edit Protocol Billable','can check the billable check box or not','Y');
+
+INSERT INTO KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND)
+VALUES (KRIM_PERM_ID_S.NEXTVAL, SYS_GUID(), 1, (SELECT T.PERM_TMPL_ID FROM KRIM_PERM_TMPL_T T WHERE T.NMSPC_CD = 'KC-IDM' AND T.NM = 'Perform Document Action') , 'KC-PROTOCOL', 'Modify Protocol Submission', 'modify an existing protocol submission', 'Y') ;
+
 COMMIT;
