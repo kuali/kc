02ca534c9f93a876720169b721a195b133948b51
Geo Thomas <geot@mit.edu>
2010-09-01 18:56:18 +0000

    Jira#KRACOEUS-3785

--- src/main/java/org/kuali/kra/s2s/generator/impl/PerformanceSiteV1_2Generator.java
+++ src/main/java/org/kuali/kra/s2s/generator/impl/PerformanceSiteV1_2Generator.java
@@ -44,7 +44,10 @@ import org.kuali.kra.s2s.util.S2SConstants;
 public class PerformanceSiteV1_2Generator extends S2SBaseFormGenerator {
 
 	private static final String QUESTION_ID_FOR_INDIVIDUAL_YNQ = "30";
-	private static final int LOCATION_TYPE_CODE = 2;
+	private static final int PERFORMING_ORG_LOCATION_TYPE_CODE = 2;
+    private static final int OTHER_ORG_LOCATION_TYPE_CODE = 3;
+    private static final int PERFORMANCE_SITE_LOCATION_TYPE_CODE = 4;
+	
 
 	private XmlObject getPerformanceSite() {
 		PerformanceSite12Document performanceSite12Document = PerformanceSite12Document.Factory
@@ -52,46 +55,9 @@ public class PerformanceSiteV1_2Generator extends S2SBaseFormGenerator {
 		PerformanceSite12 performanceSite12 = PerformanceSite12.Factory
 				.newInstance();
 		performanceSite12.setFormVersion(S2SConstants.FORMVERSION_1_2);
-		performanceSite12.setPrimarySite(getPrimarySite());
-		performanceSite12.setOtherSiteArray(getSiteLocationDataTypeArray());
+		setOtherSites(performanceSite12);
 		performanceSite12Document.setPerformanceSite12(performanceSite12);
 		return performanceSite12Document;
-	}
-
-	private SiteLocationDataType getPrimarySite() {
-		SiteLocationDataType siteLocationDataType = SiteLocationDataType.Factory
-				.newInstance();
-		if (pdDoc.getDevelopmentProposal().getPerformingOrganization() != null) {
-			Organization organization = pdDoc.getDevelopmentProposal()
-					.getPerformingOrganization().getOrganization();
-			if (organization != null) {
-				if (organization.getOrganizationName() != null) {
-					siteLocationDataType.setOrganizationName(organization
-							.getOrganizationName());
-				}
-				for (ProposalSite proposalSite : pdDoc.getDevelopmentProposal()
-						.getProposalSites()) {
-					String congressionalDistrict = getCongressionalDistrict(proposalSite);
-					if (congressionalDistrict != null) {
-						siteLocationDataType
-								.setCongressionalDistrictProgramProject(congressionalDistrict);
-						break;
-					}
-				}
-
-				if (organization.getDunsNumber() != null) {
-					siteLocationDataType.setDUNSNumber(organization
-							.getDunsNumber());
-				}
-				setSiteLocationDataType(siteLocationDataType, organization);
-			}
-
-			siteLocationDataType.setAddress(globLibV20Generator
-					.getAddressDataType(pdDoc.getDevelopmentProposal()
-							.getPerformingOrganization().getOrganization()
-							.getRolodex()));
-		}
-		return siteLocationDataType;
 	}
 
 	private void setSiteLocationDataType(
@@ -111,61 +77,53 @@ public class PerformanceSiteV1_2Generator extends S2SBaseFormGenerator {
 		}
 	}
 
-	private SiteLocationDataType[] getSiteLocationDataTypeArray() {
-		List<SiteLocationDataType> siteLocationDataTypeList = new ArrayList<SiteLocationDataType>();
+	private void setOtherSites(PerformanceSite12  performanceSite) {
 		List<ProposalSite> proposalSites = pdDoc.getDevelopmentProposal().getProposalSites();
 		if (proposalSites != null) {
 			Organization organization = null;
-			
+			Rolodex rolodex = null;
+			SiteLocationDataType siteLocationDataType = null;
 			for (ProposalSite proposalSite : proposalSites) {
-				organization = proposalSite.getOrganization();
-				if (organization != null  && LOCATION_TYPE_CODE!=proposalSite.getLocationTypeCode()) {
-					SiteLocationDataType siteLocationOther = SiteLocationDataType.Factory
-							.newInstance();
-					Rolodex rolodex = proposalSite.getRolodex();
-					if (organization.getOrganizationName() != null) {
-						siteLocationOther.setOrganizationName(organization
-								.getOrganizationName());
-					}
-					siteLocationOther.setAddress(globLibV20Generator
-							.getAddressDataType(rolodex));
-
-					if (organization.getDunsNumber() != null) {
-						siteLocationOther.setDUNSNumber(organization
-								.getDunsNumber());
-					}
-
-					String congressionalDistrict = getCongressionalDistrict(proposalSite);
-					if (congressionalDistrict != null) {
-						siteLocationOther
-								.setCongressionalDistrictProgramProject(congressionalDistrict);
-					}
-
-					setSiteLocationDataType(siteLocationOther, organization);
-					siteLocationDataTypeList.add(siteLocationOther);
-				}
+			    switch(proposalSite.getLocationTypeCode()){
+			        case(PERFORMING_ORG_LOCATION_TYPE_CODE):
+		                siteLocationDataType = performanceSite.addNewPrimarySite();
+                        organization = proposalSite.getOrganization();
+                        setSiteLocationDataType(siteLocationDataType, organization);
+                        rolodex = organization.getRolodex();
+                        break;
+			        case(OTHER_ORG_LOCATION_TYPE_CODE):
+			            organization = proposalSite.getOrganization();
+			            rolodex = organization.getRolodex();
+			            siteLocationDataType = performanceSite.addNewOtherSite();
+			            break;
+			        case(PERFORMANCE_SITE_LOCATION_TYPE_CODE):
+			            rolodex = proposalSite.getRolodex();
+			            siteLocationDataType = performanceSite.addNewOtherSite();
+			            break;
+			    }
+			    if(siteLocationDataType!=null){
+                    siteLocationDataType.setOrganizationName(proposalSite.getLocationName());
+                    siteLocationDataType.setAddress(globLibV20Generator.getAddressDataType(rolodex));
+                    if (organization!=null && organization.getDunsNumber() != null) {
+                        siteLocationDataType.setDUNSNumber(organization.getDunsNumber());
+                    }
+                    String congressionalDistrict = getCongressionalDistrict(proposalSite);
+                    if (congressionalDistrict != null) {
+                        siteLocationDataType.setCongressionalDistrictProgramProject(congressionalDistrict);
+                    }
+			    }
 			}
 		}
-		return siteLocationDataTypeList.toArray(new SiteLocationDataType[0]);
 	}
 
 	private String getCongressionalDistrict(ProposalSite proposalSite) {
 		String congDistrictProject = null;
-		if (proposalSite.getLocationTypeCode()==LOCATION_TYPE_CODE) {
-			for (CongressionalDistrict congDistrict : proposalSite
-					.getCongressionalDistricts()) {
-				if (congDistrict.getSiteNumber().equals(
-						proposalSite.getSiteNumber())) {
-					congDistrictProject = congDistrict
-							.getCongressionalDistrict();
-					if (congDistrictProject != null
-							&& congDistrictProject.length() > CONGRESSIONAL_DISTRICT_MAX_LENGTH) {
-						congDistrictProject = congDistrictProject.substring(0,
-								CONGRESSIONAL_DISTRICT_MAX_LENGTH);
-					}
-				}
-			}
-		}
+		for (CongressionalDistrict congDistrict : proposalSite.getCongressionalDistricts()) {
+		    congDistrictProject = congDistrict.getCongressionalDistrict();
+            if (congDistrictProject != null && congDistrictProject.length() > CONGRESSIONAL_DISTRICT_MAX_LENGTH) {
+                congDistrictProject = congDistrictProject.substring(0, CONGRESSIONAL_DISTRICT_MAX_LENGTH);
+            }
+        }
 		return congDistrictProject;
 	}
 
--- src/main/java/org/kuali/kra/s2s/generator/impl/PerformanceSiteV1_3Generator.java
+++ src/main/java/org/kuali/kra/s2s/generator/impl/PerformanceSiteV1_3Generator.java
@@ -15,6 +15,7 @@
  */
 package org.kuali.kra.s2s.generator.impl;
 
+import gov.grants.apply.forms.performanceSite12V12.PerformanceSite12Document.PerformanceSite12;
 import gov.grants.apply.forms.performanceSite13V13.PerformanceSite13Document;
 import gov.grants.apply.forms.performanceSite13V13.SiteLocationDataType;
 import gov.grants.apply.forms.performanceSite13V13.PerformanceSite13Document.PerformanceSite13;
@@ -45,7 +46,9 @@ import org.kuali.kra.s2s.util.S2SConstants;
 public class PerformanceSiteV1_3Generator extends S2SBaseFormGenerator {
 
 	private static final String QUESTION_ID_FOR_INDIVIDUAL_YNQ = "30";
-	private static final int LOCATION_TYPE_CODE = 2;
+    private static final int PERFORMING_ORG_LOCATION_TYPE_CODE = 2;
+    private static final int OTHER_ORG_LOCATION_TYPE_CODE = 3;
+    private static final int PERFORMANCE_SITE_LOCATION_TYPE_CODE = 4;
 
 	private XmlObject getPerformanceSite() {
 		PerformanceSite13Document performanceSite13Document = PerformanceSite13Document.Factory
@@ -53,8 +56,7 @@ public class PerformanceSiteV1_3Generator extends S2SBaseFormGenerator {
 		PerformanceSite13 performanceSite13 = PerformanceSite13.Factory
 				.newInstance();
 		performanceSite13.setFormVersion(S2SConstants.FORMVERSION_1_3);
-		performanceSite13.setPrimarySite(getPrimarySite());
-		performanceSite13.setOtherSiteArray(getSiteLocationDataTypeArray());
+		setOtherSites(performanceSite13);
 		AttachedFileDataType attachedFile = getAttachment();
 		if(attachedFile!=null){
 		    performanceSite13.setAttachedFile(attachedFile);
@@ -63,41 +65,6 @@ public class PerformanceSiteV1_3Generator extends S2SBaseFormGenerator {
 		return performanceSite13Document;
 	}
 
-	private SiteLocationDataType getPrimarySite() {
-		SiteLocationDataType siteLocationDataType = SiteLocationDataType.Factory
-				.newInstance();
-		if (pdDoc.getDevelopmentProposal().getPerformingOrganization() != null) {
-			Organization organization = pdDoc.getDevelopmentProposal()
-					.getPerformingOrganization().getOrganization();
-			if (organization != null) {
-				if (organization.getOrganizationName() != null) {
-					siteLocationDataType.setOrganizationName(organization
-							.getOrganizationName());
-				}
-				for (ProposalSite proposalSite : pdDoc.getDevelopmentProposal()
-						.getProposalSites()) {
-					String congressionalDistrict = getCongressionalDistrict(proposalSite);
-					if (congressionalDistrict != null) {
-						siteLocationDataType
-								.setCongressionalDistrictProgramProject(congressionalDistrict);
-						break;
-					}
-				}
-
-				if (organization.getDunsNumber() != null) {
-					siteLocationDataType.setDUNSNumber(organization
-							.getDunsNumber());
-				}
-				setSiteLocationDataType(siteLocationDataType, organization);
-			}
-
-			siteLocationDataType.setAddress(globLibV20Generator
-					.getAddressDataType(pdDoc.getDevelopmentProposal()
-							.getPerformingOrganization().getOrganization()
-							.getRolodex()));
-		}
-		return siteLocationDataType;
-	}
 
 	private void setSiteLocationDataType(
 			SiteLocationDataType siteLocationDataType, Organization organization) {
@@ -116,64 +83,55 @@ public class PerformanceSiteV1_3Generator extends S2SBaseFormGenerator {
 		}
 	}
 
-	private SiteLocationDataType[] getSiteLocationDataTypeArray() {
-		List<SiteLocationDataType> siteLocationDataTypeList = new ArrayList<SiteLocationDataType>();
-		List<ProposalSite> proposalSites = pdDoc.getDevelopmentProposal().getProposalSites();
-		
-		if (proposalSites != null) {
-			Organization organization = null;
-			
-			for (ProposalSite proposalSite : proposalSites) {
-				organization = proposalSite.getOrganization();
-				if (organization != null && LOCATION_TYPE_CODE!=proposalSite.getLocationTypeCode()) {
-					SiteLocationDataType siteLocationOther = SiteLocationDataType.Factory
-							.newInstance();
-					Rolodex rolodex = proposalSite.getRolodex();
-					if (organization.getOrganizationName() != null) {
-						siteLocationOther.setOrganizationName(organization
-								.getOrganizationName());
-					}
-					siteLocationOther.setAddress(globLibV20Generator
-							.getAddressDataType(rolodex));
+    private void setOtherSites(PerformanceSite13  performanceSite) {
+        List<ProposalSite> proposalSites = pdDoc.getDevelopmentProposal().getProposalSites();
+        if (proposalSites != null) {
+            Organization organization = null;
+            Rolodex rolodex = null;
+            SiteLocationDataType siteLocationDataType = null;
+            for (ProposalSite proposalSite : proposalSites) {
+                switch(proposalSite.getLocationTypeCode()){
+                    case(PERFORMING_ORG_LOCATION_TYPE_CODE):
+                        siteLocationDataType = performanceSite.addNewPrimarySite();
+                        organization = proposalSite.getOrganization();
+                        setSiteLocationDataType(siteLocationDataType, organization);
+                        rolodex = organization.getRolodex();
+                        break;
+                    case(OTHER_ORG_LOCATION_TYPE_CODE):
+                        organization = proposalSite.getOrganization();
+                        rolodex = organization.getRolodex();
+                        siteLocationDataType = performanceSite.addNewOtherSite();
+                        break;
+                    case(PERFORMANCE_SITE_LOCATION_TYPE_CODE):
+                        rolodex = proposalSite.getRolodex();
+                        siteLocationDataType = performanceSite.addNewOtherSite();
+                        break;
+                }
+                if(siteLocationDataType!=null){
+                    siteLocationDataType.setOrganizationName(proposalSite.getLocationName());
+                    siteLocationDataType.setAddress(globLibV20Generator.getAddressDataType(rolodex));
+                    if (organization!=null && organization.getDunsNumber() != null) {
+                        siteLocationDataType.setDUNSNumber(organization.getDunsNumber());
+                    }
+                    String congressionalDistrict = getCongressionalDistrict(proposalSite);
+                    if (congressionalDistrict != null) {
+                        siteLocationDataType.setCongressionalDistrictProgramProject(congressionalDistrict);
+                    }
+                }
+            }
+        }
+    }
 
-					if (organization.getDunsNumber() != null) {
-						siteLocationOther.setDUNSNumber(organization
-								.getDunsNumber());
-					}
-
-					String congressionalDistrict = getCongressionalDistrict(proposalSite);
-					if (congressionalDistrict != null) {
-						siteLocationOther
-								.setCongressionalDistrictProgramProject(congressionalDistrict);
-					}
-
-					setSiteLocationDataType(siteLocationOther, organization);
-					siteLocationDataTypeList.add(siteLocationOther);
-				}
-			}
-		}
-		return siteLocationDataTypeList.toArray(new SiteLocationDataType[0]);
-	}
-
-	private String getCongressionalDistrict(ProposalSite proposalSite) {
-		String congDistrictProject = null;
-		if (proposalSite.getLocationTypeCode()==LOCATION_TYPE_CODE) {
-			for (CongressionalDistrict congDistrict : proposalSite
-					.getCongressionalDistricts()) {
-				if (congDistrict.getSiteNumber().equals(
-						proposalSite.getSiteNumber())) {
-					congDistrictProject = congDistrict
-							.getCongressionalDistrict();
-					if (congDistrictProject != null
-							&& congDistrictProject.length() > CONGRESSIONAL_DISTRICT_MAX_LENGTH) {
-						congDistrictProject = congDistrictProject.substring(0,
-								CONGRESSIONAL_DISTRICT_MAX_LENGTH);
-					}
-				}
-			}
-		}
-		return congDistrictProject;
-	}
+    private String getCongressionalDistrict(ProposalSite proposalSite) {
+        String congDistrictProject = null;
+        for (CongressionalDistrict congDistrict : proposalSite.getCongressionalDistricts()) {
+            congDistrictProject = congDistrict.getCongressionalDistrict();
+            if (congDistrictProject != null && congDistrictProject.length() > CONGRESSIONAL_DISTRICT_MAX_LENGTH) {
+                congDistrictProject = congDistrictProject.substring(0, CONGRESSIONAL_DISTRICT_MAX_LENGTH);
+            }
+        }
+        return congDistrictProject;
+    }
 
 	protected AttachedFileDataType getAttachment(){
 	    return getAttachedFileDataType("40");
