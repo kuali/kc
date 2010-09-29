f0fa2af1c57eccd7a2e83450671bd3f4ca3075bd
Jay Hulslander <jdh34@cornell.edu>
2010-09-29 12:51:00 +0000

    KCIRB-1046 added some error handling

--- src/main/java/org/kuali/kra/irb/noteattachment/ProtocolAttachmentHelper.java
+++ src/main/java/org/kuali/kra/irb/noteattachment/ProtocolAttachmentHelper.java
@@ -244,8 +244,14 @@ public class ProtocolAttachmentHelper {
                 String fieldValue = ((String[]) parameterMap.get(key))[0];
                 if ("T".equals(fieldValue)) {
                     //a refresh button has been clicked, now we just need to update the appropriate attachment status code
-                    int numericVal = Integer.valueOf(key.substring(fieldNameStarter.length()));
-                    this.getProtocol().getAttachmentProtocols().get(numericVal).setDocumentStatusCode("2");
+                    try {
+                        int numericVal = Integer.valueOf(key.substring(fieldNameStarter.length()));
+                        this.getProtocol().getAttachmentProtocols().get(numericVal).setDocumentStatusCode("2");
+                    } catch (Exception e) {
+                        //we could get an exception from the getting of the integer, or from the collection not having the integer in it's index/
+                        //do nothing.
+                        e.printStackTrace();
+                    }
                 }
             }
         }
