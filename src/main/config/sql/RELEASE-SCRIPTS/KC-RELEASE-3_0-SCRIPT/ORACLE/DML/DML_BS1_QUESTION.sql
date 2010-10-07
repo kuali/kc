85e6b1918680963b9f33ef2cb3fb49a106fb166a
Claus Niesen <cniesen@iastate.edu>
2010-10-07 16:07:58 +0000

    KCIRB-1185 Update Question table to use Rice's country bo for the country lookup

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/DML/DML_BS1_QUESTION.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/ORACLE/DML/DML_BS1_QUESTION.sql
@@ -1,5 +1,9 @@
 UPDATE QUESTION
-SET LOOKUP_CLASS   = 'org.kuali.rice.kns.bo.State', LOOKUP_RETURN = 'postalStateName'
+SET LOOKUP_CLASS   = 'org.kuali.rice.kns.bo.StateImpl', LOOKUP_RETURN = 'postalStateName'
 WHERE LOOKUP_CLASS = 'org.kuali.kra.bo.State';
+
+UPDATE QUESTION 
+SET LOOKUP_CLASS = 'org.kuali.rice.kns.bo.CountryImpl', LOOKUP_RETURN = 'postalCountryName' 
+WHERE LOOKUP_CLASS = 'org.kuali.kra.bo.Country';
 
 COMMIT;
