2147b73a5d17678e5ac6c9512ac770a09d5739f7
Tyler Wilson <tyler.wilson@colostate.edu>
2010-10-07 18:47:30 +0000

    Changed comment fields to 3x40 text area.

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/DML/DML_BS2_KRNS_PARM_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/DML/DML_BS2_KRNS_PARM_T.sql
@@ -30,4 +30,6 @@ VALUES ('KC-GEN', 'A', 'NSF_SPONSOR_CODE', 1, 'CONFG', '000500', 'The sponsor co
 insert into krns_parm_t (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID)
 values('KUALI', 'KC-AWARD', 'D', 'AWARD_CREATE_ACCOUNT', 1, 'CONFG', 'ON', 'Defines if the award financial account creation feature is on or off', 'A', UUID());
 
+UPDATE KRNS_PARM_T SET TXT='N' where parm_nm = 'SHOW_BACK_DOOR_LOGIN_IND';
+
 COMMIT;
--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/SEQUENCES/SEQ_ALL.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/SEQUENCES/SEQ_ALL.sql
@@ -24,3 +24,14 @@ CREATE TABLE SEQ_VALID_SUBM_REVW_TYPE_QUAL
 
 drop table sequence_submission_history
 /
+
+drop table KC_COUNTRY_CODES_SEQ
+/
+
+CREATE TABLE SEQ_VALID_NARR_FORMS_ID
+(
+	id bigint(19) not null auto_increment, primary key (id)
+) ENGINE MyISAM
+/
+
+
