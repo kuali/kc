33d8b2eaed6dee0c98206a37bffbd1d1b267afd7
Lin-Long Shyu <lshyukuali@gmail.com>
2010-10-09 21:59:38 +0000

    kcirb-1192

--- src/main/java/org/kuali/kra/irb/ProtocolDocument.java
+++ src/main/java/org/kuali/kra/irb/ProtocolDocument.java
@@ -300,6 +300,7 @@ public class ProtocolDocument extends ResearchDocumentBase implements Copyable, 
         action.setComments(type + "-" + getProtocolNumberIndex() + ": Approved");
         newProtocolDocument.setProtocolWorkflowType(ProtocolWorkflowType.APPROVED);
         newProtocolDocument.getProtocol().getProtocolActions().add(action);
+        newProtocolDocument.getProtocol().setProtocolStatusCode(ProtocolStatus.ACTIVE_OPEN_TO_ENROLLMENT);
         try {
             getDocumentService().saveDocument(newProtocolDocument);
             // blanket approve to make the new protocol document 'final'
