ee989de6be8cec0f186a8b8c61ab8bd79d5de4fe
Claus Niesen <cniesen@iastate.edu>
2010-11-02 20:37:16 +0000

    KCINFR-132  Added database patch - 5/27/2010 (SEQUENCE_AWARD_ID table)

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/SEQUENCES/SEQ_ALL.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/SEQUENCES/SEQ_ALL.sql
@@ -1,3 +1,8 @@
+CREATE TABLE SEQUENCE_AWARD_ID
+(
+ id bigint(19) not null auto_increment, primary key (id) 
+) ENGINE MyISAM;
+
 CREATE TABLE SEQUENCE_IPPUCS_ID
 (
 	id bigint(19) not null auto_increment, primary key (id) 
