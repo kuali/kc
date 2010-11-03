49939b558d477a7e955952fb4d20d8c56cf61815
Lin-Long Shyu <lshyukuali@gmail.com>
2010-11-03 23:33:52 +0000

    kcirb-665

--- src/main/webapp/static/help/urls.js
+++
@@ -1,4 +0,0 @@
-var g_ContentsURL = "Kuali%20Coeus-toc.htm";
-var g_IndexURL = "Kuali%20Coeus-index.htm";
-var g_SearchURL = "Kuali%20Coeus-search.htm";
-var g_FavoritesURL = "Kuali%20Coeus-favorites.htm";
