7a4728a5d9950890a41a8f95179f48bc893e66b8
Claus Niesen <cniesen@iastate.edu>
2010-11-17 19:51:11 +0000

    KCIRB-1249 Don't validate attachments that have been deleted

--- src/main/java/org/kuali/kra/irb/noteattachment/ProtocolAttachmentProtocolRuleHelper.java
+++ src/main/java/org/kuali/kra/irb/noteattachment/ProtocolAttachmentProtocolRuleHelper.java
@@ -89,11 +89,13 @@ class ProtocolAttachmentProtocolRuleHelper {
      * @return true is valid.
      */
     boolean validStatusForSubmission(final ProtocolAttachmentProtocol attachmentProtocol) {
-        if (attachmentProtocol.getStatus() == null || !COMPLETE_STATUS_CODE.equals(attachmentProtocol.getStatus().getCode())) {
-            final AuditError error = new AuditError(this.propertyPrefix + "." + ProtocolAttachmentProtocol.PropertyName.STATUS_CODE,
-                KeyConstants.AUDIT_ERROR_PROTOCOL_ATTACHMENT_STATUS_COMPLETE, NOTE_AND_ATTACHMENT_LINK);
-            this.errorReporter.reportAuditError(error, NOTES_AND_ATTACHMENT_AUDIT_ERRORS_KEY, NOTES_ATTACHMENTS_CLUSTER_LABEL, Constants.AUDIT_ERRORS);
-            return false;
+        if (!StringUtils.equals(attachmentProtocol.getDocumentStatusCode(), "3")) {
+            if (attachmentProtocol.getStatus() == null || !COMPLETE_STATUS_CODE.equals(attachmentProtocol.getStatus().getCode())) {
+                final AuditError error = new AuditError(this.propertyPrefix + "." + ProtocolAttachmentProtocol.PropertyName.STATUS_CODE,
+                    KeyConstants.AUDIT_ERROR_PROTOCOL_ATTACHMENT_STATUS_COMPLETE, NOTE_AND_ATTACHMENT_LINK);
+                this.errorReporter.reportAuditError(error, NOTES_AND_ATTACHMENT_AUDIT_ERRORS_KEY, NOTES_ATTACHMENTS_CLUSTER_LABEL, Constants.AUDIT_ERRORS);
+                return false;
+            }
         }
         return true;
     }
