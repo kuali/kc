16bcad6af119964847763ff1e9f973cde8f4f06e
Tyler Wilson <tyler.wilson@colostate.edu>
2010-11-17 16:47:37 +0000

    Post Release 3.0 SQL scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/DML/DML_BS2_KRNS_PARM_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-SCRIPT/MYSQL/DML/DML_BS2_KRNS_PARM_T.sql
@@ -507,4 +507,15 @@ update krns_parm_t set txt ='default.htm?turl=Documents/proposaltype.htm'
 update krns_parm_t set txt ='default.htm?turl=Documents/keypersonnel.htm' 
        where txt ='default.htm?turl=Documents/userspanel.htm' and parm_nm ='proposalUserHelpUrl';  
  
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/additionalinformation.htm' WHERE PARM_NM = 'protocolAdditionalInformationHelpUrl';
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/addpersonnelattachment.htm' WHERE PARM_NM = 'protocolAddPersonnelAttachmentHelpUrl';	
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/addprotocolattachment.htm' WHERE PARM_NM = 'protocolAddProtocolAttachmentHelpUrl';
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/customdata3.htm' WHERE PARM_NM = 'protocolCustomDataHelp';
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/notes2.htm' WHERE PARM_NM = 'protocolNotesHelpUrl';
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/permissions2.htm' WHERE PARM_NM = 'protocolPermissionsHelp';
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/persondetails1.htm' WHERE PARM_NM = 'protocolPersonnelHelpUrl';
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/requiredfieldsforsavingdocument1.htm' WHERE PARM_NM = 'protocolRequiredFieldsHelpUrl';
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/specialreview3.htm' WHERE PARM_NM = 'protocolSpecialReviewHelp';
+UPDATE KRNS_PARM_T SET TXT = 'default.htm?turl=Documents/statusdates.htm' WHERE PARM_NM = 'protocolStatusAndDatesHelpUrl';
+
 COMMIT;
