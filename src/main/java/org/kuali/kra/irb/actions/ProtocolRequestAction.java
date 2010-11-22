2d81cba78215d08add9f4e7473b07bb78750b904
Kristina Taylor <kbtaylor@uw.edu>
2010-11-22 21:50:50 +0000

    KCIRB-967: Finish refactor on Request actions

--- src/main/java/org/kuali/kra/irb/actions/ProtocolRequestAction.java
+++ src/main/java/org/kuali/kra/irb/actions/ProtocolRequestAction.java
@@ -19,18 +19,64 @@ import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.TaskName;
 
 /**
- * 
  * This class is an enum which set all the properties for each request action.
  * This is referenced by submit action in protocolprotocolactionaction.
  */
 public enum ProtocolRequestAction {
     
-    REQUEST_TO_CLOSE(ProtocolActionType.REQUEST_TO_CLOSE, TaskName.PROTOCOL_REQUEST_CLOSE, Constants.PROTOCOL_CLOSE_REQUEST_PROPERTY_KEY, "protocolCloseRequestBean", "Request to Close"), 
-    REQUEST_TO_CLOSE_ENROLLMENT(ProtocolActionType.REQUEST_TO_CLOSE_ENROLLMENT, TaskName.PROTOCOL_REQUEST_CLOSE_ENROLLMENT, Constants.PROTOCOL_CLOSE_ENROLLMENT_REQUEST_PROPERTY_KEY, "protocolCloseEnrollmentRequestBean", "Request to Close Enrollment"), 
-    REQUEST_TO_REOPEN_ENROLLMENT(ProtocolActionType.REQUEST_TO_REOPEN_ENROLLMENT, TaskName.PROTOCOL_REQUEST_REOPEN_ENROLLMENT, Constants.PROTOCOL_REOPEN_ENROLLMENT_REQUEST_PROPERTY_KEY, "protocolReOpenEnrollmentRequestBean", "Request to Re-open Enrollment"), 
-    REQUEST_FOR_SUSPENSION(ProtocolActionType.REQUEST_FOR_SUSPENSION, TaskName.PROTOCOL_REQUEST_SUSPENSION, Constants.PROTOCOL_SUSPEND_REQUEST_PROPERTY_KEY, "protocolSuspendRequestBean", "Request for Suspension"), 
-    REQUEST_FOR_TERMINATION(ProtocolActionType.REQUEST_FOR_TERMINATION, TaskName.PROTOCOL_REQUEST_TERMINATE, Constants.PROTOCOL_TERMINATE_REQUEST_PROPERTY_KEY, "protocolTerminateRequestBean", "Request for Termination"), 
-    REQUEST_FOR_DATA_ANALYSIS_ONLY(ProtocolActionType.REQUEST_FOR_DATA_ANALYSIS_ONLY, TaskName.PROTOCOL_REQUEST_DATA_ANALYSIS, Constants.PROTOCOL_DATA_ANALYSIS_REQUEST_PROPERTY_KEY, "protocolDataAnalysisRequestBean", "Request for Data Analysis Only"); 
+    /**
+     * Request to close a Protocol.
+     */
+    REQUEST_TO_CLOSE               (ProtocolActionType.REQUEST_TO_CLOSE,
+                                    TaskName.PROTOCOL_REQUEST_CLOSE,
+                                    Constants.PROTOCOL_CLOSE_REQUEST_PROPERTY_KEY, 
+                                    "protocolCloseRequestBean", 
+                                    "Request to Close"), 
+
+    /**
+     * Request to close enrollment to a Protocol.
+     */
+    REQUEST_TO_CLOSE_ENROLLMENT    (ProtocolActionType.REQUEST_TO_CLOSE_ENROLLMENT,
+                                    TaskName.PROTOCOL_REQUEST_CLOSE_ENROLLMENT,
+                                    Constants.PROTOCOL_CLOSE_ENROLLMENT_REQUEST_PROPERTY_KEY, 
+                                    "protocolCloseEnrollmentRequestBean", 
+                                    "Request to Close Enrollment"), 
+                                    
+    /**
+     * Request for data analysis only of a Protocol.
+     */
+    REQUEST_FOR_DATA_ANALYSIS_ONLY (ProtocolActionType.REQUEST_FOR_DATA_ANALYSIS_ONLY, 
+                                    TaskName.PROTOCOL_REQUEST_DATA_ANALYSIS,
+                                    Constants.PROTOCOL_DATA_ANALYSIS_REQUEST_PROPERTY_KEY, 
+                                    "protocolDataAnalysisRequestBean", 
+                                    "Request for Data Analysis Only"),
+    
+    /**
+     * Request to reopen enrollment to a Protocol.
+     */
+    REQUEST_TO_REOPEN_ENROLLMENT   (ProtocolActionType.REQUEST_TO_REOPEN_ENROLLMENT, 
+                                    TaskName.PROTOCOL_REQUEST_REOPEN_ENROLLMENT,
+                                    Constants.PROTOCOL_REOPEN_ENROLLMENT_REQUEST_PROPERTY_KEY, 
+                                    "protocolReOpenEnrollmentRequestBean", 
+                                    "Request to Re-open Enrollment"), 
+                                  
+    /**
+     * Request for suspension of a Protocol.
+     */
+    REQUEST_FOR_SUSPENSION         (ProtocolActionType.REQUEST_FOR_SUSPENSION, 
+                                    TaskName.PROTOCOL_REQUEST_SUSPENSION,
+                                    Constants.PROTOCOL_SUSPEND_REQUEST_PROPERTY_KEY, 
+                                    "protocolSuspendRequestBean", 
+                                    "Request for Suspension"), 
+                                  
+    /**
+     * Request for termination of a Protocol.
+     */
+    REQUEST_FOR_TERMINATION        (ProtocolActionType.REQUEST_FOR_TERMINATION, 
+                                    TaskName.PROTOCOL_REQUEST_TERMINATE,
+                                    Constants.PROTOCOL_TERMINATE_REQUEST_PROPERTY_KEY, 
+                                    "protocolTerminateRequestBean", 
+                                    "Request for Termination");
     
     private final String actionTypeCode;
     private final String taskName;
@@ -39,7 +85,7 @@ public enum ProtocolRequestAction {
     private final String actionName;
 
     
-    ProtocolRequestAction(String actionTypeCode, String taskName,String errorPath,String beanName,String actionName) {
+    private ProtocolRequestAction(String actionTypeCode, String taskName, String errorPath, String beanName, String actionName) {
         this.actionTypeCode = actionTypeCode;
         this.taskName = taskName;
         this.errorPath = errorPath;
@@ -50,7 +96,7 @@ public enum ProtocolRequestAction {
     public String getActionTypeCode() {
         return actionTypeCode;
     }
-
+    
     public String getTaskName() {
         return taskName;
     }
@@ -65,6 +111,44 @@ public enum ProtocolRequestAction {
 
     public String getActionName() {
         return actionName;
+    }
+    
+    /**
+     * Returns the value of this enumeration based on the given taskName.
+     * 
+     * This can return null, which isn't ideal but is necessary since TaskName is not an enum.
+     * @param taskName the name of the task to perform
+     * @return the ProtocolRequestAction corresponding to the taskName
+     */
+    public static ProtocolRequestAction valueOfTaskName(String taskName) {
+        ProtocolRequestAction protocolRequestAction = null;
+        
+        for (ProtocolRequestAction action : values()) {
+            if (action.getTaskName().equals(taskName)) {
+                protocolRequestAction = action;
+            }
+        }
+        
+        return protocolRequestAction;
+    }
+    
+    /**
+     * Returns the value of this enumeration based on the given actionTypeCode.
+     * 
+     * This can return null, which isn't ideal but is necessary since ProtocolActionTypeCode is not an enum.
+     * @param actionTypeCode the action type code
+     * @return the ProtocolRequestAction corresponding to the actionTypeCode
+     */
+    public static ProtocolRequestAction valueOfActionTypeCode(String actionTypeCode) {
+        ProtocolRequestAction protocolRequestAction = null;
+        
+        for (ProtocolRequestAction action : values()) {
+            if (action.getActionTypeCode().equals(actionTypeCode)) {
+                protocolRequestAction = action;
+            }
+        }
+        
+        return protocolRequestAction;
     }
 
 }
