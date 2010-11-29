10aafdc127dca88d6961027a13d36255933a824a
Tyler Wilson <tyler.wilson@colostate.edu>
2010-11-29 21:54:24 +0000

    Release 3.1 Sprint 1 SQL scripts

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1-SCRIPT/MYSQL/DML/DML_BS2_COEUS_SUB_MODULE.sql
@@ -0,0 +1,10 @@
+insert into COEUS_SUB_MODULE (COEUS_SUB_MODULE_ID , MODULE_CODE , SUB_MODULE_CODE , DESCRIPTION, UPDATE_TIMESTAMP ,UPDATE_USER, VER_NBR, OBJ_ID)   values 
+( 1,3,1,'Proposal Budget',NOW(), 'quickstart',1,UUID());      
+ 
+insert into COEUS_SUB_MODULE (COEUS_SUB_MODULE_ID , MODULE_CODE , SUB_MODULE_CODE , DESCRIPTION, UPDATE_TIMESTAMP ,UPDATE_USER, VER_NBR, OBJ_ID)   values 
+( 2,7,1,'Amendment / Renewal',NOW(), 'quickstart',1,UUID());    
+
+insert into COEUS_SUB_MODULE (COEUS_SUB_MODULE_ID , MODULE_CODE , SUB_MODULE_CODE , DESCRIPTION, UPDATE_TIMESTAMP ,UPDATE_USER, VER_NBR, OBJ_ID)   values 
+( 3,7,2,'Protocol Submission',NOW(), 'quickstart',1,UUID());        
+
+COMMIT;
