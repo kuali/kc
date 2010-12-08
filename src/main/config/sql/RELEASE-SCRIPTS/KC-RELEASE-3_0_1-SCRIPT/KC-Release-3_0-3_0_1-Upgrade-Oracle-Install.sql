f2b28a594412bd3c3d373d635d53990c1384a9a4
Tyler Wilson <tyler.wilson@colostate.edu>
2010-12-08 16:53:55 +0000

    Release 3.0.1 SQL scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0_1-SCRIPT/KC-Release-3_0-3_0_1-Upgrade-Oracle-Install.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0_1-SCRIPT/KC-Release-3_0-3_0_1-Upgrade-Oracle-Install.sql
@@ -1,7 +1,6 @@
 set define off 
 set sqlblanklines on 
 spool KC-Release-3_0-3_0_1-Upgrade-Oracle-Install.log
-@ORACLE\DML\DML_DEMO4_SPONSOR_FORM_TEMPLATES.sql 
 @ORACLE\TABLES\TBL_AWARD_CUSTOM_DATA.sql 
 @ORACLE\TABLES\TBL_PROPOSAL_CUSTOM_DATA.sql 
 commit;
