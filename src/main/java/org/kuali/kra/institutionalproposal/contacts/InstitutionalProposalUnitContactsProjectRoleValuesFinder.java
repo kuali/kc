14550112712cdfff9eb1258c45a5e62820af381a
Ying Zhou <yingzhou@iastate.edu>
2010-12-13 16:42:41 +0000

    KRACOEUS-4194: created new values finder for Institutional proposal contacts project role

---
+++ src/main/java/org/kuali/kra/institutionalproposal/contacts/InstitutionalProposalUnitContactsProjectRoleValuesFinder.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.institutionalproposal.contacts;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kra.bo.UnitAdministratorType;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.lookup.keyvalue.ExtendedPersistableBusinessObjectValuesFinder;
+import org.kuali.kra.lookup.keyvalue.PrefixValuesFinder;
+import org.kuali.rice.core.util.KeyLabelPair;
+import org.kuali.rice.kns.service.BusinessObjectService;
+
+public class InstitutionalProposalUnitContactsProjectRoleValuesFinder extends ExtendedPersistableBusinessObjectValuesFinder {
+    
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<KeyLabelPair> getKeyValues(){
+        BusinessObjectService boService = KraServiceLocator.getService(BusinessObjectService.class);
+        Collection<UnitAdministratorType> types = (Collection<UnitAdministratorType>) boService.findAll(UnitAdministratorType.class);
+        List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
+        for (UnitAdministratorType aType: types) {
+            if ( StringUtils.equals(aType.getDefaultGroupFlag(), "U")) {    // only get Unit Contacts
+                keyValues.add(new KeyLabelPair(aType.getUnitAdministratorTypeCode(), aType.getDescription()));
+            }
+        }
+        keyValues.add(0, new KeyLabelPair(PrefixValuesFinder.getPrefixKey(), PrefixValuesFinder.getDefaultPrefixValue()));    
+        
+        return keyValues;
+    }
+}
