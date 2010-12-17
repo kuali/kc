a22bf75ee8ab0b7c0a92d666e8282a1b6965d363
Allen Akers <allen.akers@colostate.edu>
2010-12-17 18:35:22 +0000

    Modifying install script to allow for no TNS name to accommodate automated runs on EC2

--- src/main/config/sql/RELEASE-SCRIPTS/KC_Install.sh
+++ src/main/config/sql/RELEASE-SCRIPTS/KC_Install.sh
@@ -67,6 +67,20 @@ then
 	fi
 fi
 
+if [ "${DBSvrNm}" = "_" ]
+then
+	DBSvrNm=''
+else
+	DBSvrNm="@${DBSvrNm}"
+fi
+
+if [ "${RiceDBSvrNm}" = "_" ]
+then
+	RiceDBSvrNm=''
+else
+	RiceDBSvrNm="@${RiceDBSvrNm}"
+fi
+
 case "${dbtype}" in
 	"ORACLE")
 		if [ "${version}" = "NEW" ]
@@ -74,19 +88,19 @@ case "${dbtype}" in
 			cd KC-RELEASE-2_0-SCRIPT
 			if [ "${mode}" = "BUNDLE" ]
 			then
-				sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KR-Release-1_0_2-Server-Oracle-Install.sql
-				sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KC-Release-2_0-Base-Bundled-Oracle-Install.sql
-				sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KC-Release-2_0-Base-Rice-Oracle-Install.sql
+				sqlplus "${un}"/"${pw}${DBSvrNm}" < KR-Release-1_0_2-Server-Oracle-Install.sql
+				sqlplus "${un}"/"${pw}${DBSvrNm}" < KC-Release-2_0-Base-Bundled-Oracle-Install.sql
+				sqlplus "${un}"/"${pw}${DBSvrNm}" < KC-Release-2_0-Base-Rice-Oracle-Install.sql
 			else
 				if [ "${mode}" = "EMBED" ]
 				then
 					if [ "${InstRice}" = "Y" ]
 					then
-						sqlplus "${Riceun}"/"${Ricepw}"@"${RiceDBSvrNm}" < KR-Release-1_0_2-Server-Oracle-Install.sql
+						sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < KR-Release-1_0_2-Server-Oracle-Install.sql
 					fi
-					sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KR-Release-1_0_2-Client-Oracle-Install.sql
-					sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KC-Release-2_0-Base-Bundled-Oracle-Install.sql
-					sqlplus "${Riceun}"/"${Ricepw}"@"${RiceDBSvrNm}" < KC-Release-2_0-Base-Rice-Oracle-Install.sql
+					sqlplus "${un}"/"${pw}${DBSvrNm}" < KR-Release-1_0_2-Client-Oracle-Install.sql
+					sqlplus "${un}"/"${pw}${DBSvrNm}" < KC-Release-2_0-Base-Bundled-Oracle-Install.sql
+					sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < KC-Release-2_0-Base-Rice-Oracle-Install.sql
 				fi
 			fi
 			mv *.log ../LOGS/
@@ -98,26 +112,26 @@ case "${dbtype}" in
 			cd KC-RELEASE-3_0-SCRIPT
 			if [ "${mode}" = "BUNDLE" ]
 			then
-				sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < RICE-1_0_2-1_0_3/update_final_oracle.sql
-				sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KC-Release-2_0-3_0-Upgrade-Oracle-Install.sql
-				sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KR-Release-2_0-3_0-Upgrade-Oracle-Install.sql
+				sqlplus "${un}"/"${pw}${DBSvrNm}" < RICE-1_0_2-1_0_3/update_final_oracle.sql
+				sqlplus "${un}"/"${pw}${DBSvrNm}" < KC-Release-2_0-3_0-Upgrade-Oracle-Install.sql
+				sqlplus "${un}"/"${pw}${DBSvrNm}" < KR-Release-2_0-3_0-Upgrade-Oracle-Install.sql
 			else 
 				if [ "${mode}" = "EMBED" ]
 				then
 					if [ "${InstRice}" = "Y" ]
 					then
-						sqlplus "${Riceun}"/"${Ricepw}"@"${RiceDBSvrNm}" < RICE-1_0_2-1_0_3/update_final_oracle.sql
+						sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < RICE-1_0_2-1_0_3/update_final_oracle.sql
 					fi
-					sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < RICE-1_0_2-1_0_3/update_client_final_oracle.sql
-					sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KC-Release-2_0-3_0-Upgrade-Oracle-Install.sql
-					sqlplus "${Riceun}"/"${Ricepw}"@"${RiceDBSvrNm}" < KR-Release-2_0-3_0-Upgrade-Oracle-Install.sql
+					sqlplus "${un}"/"${pw}${DBSvrNm}" < RICE-1_0_2-1_0_3/update_client_final_oracle.sql
+					sqlplus "${un}"/"${pw}${DBSvrNm}" < KC-Release-2_0-3_0-Upgrade-Oracle-Install.sql
+					sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < KR-Release-2_0-3_0-Upgrade-Oracle-Install.sql
 				fi
 			fi
 			mv *.log ../LOGS/
 			cd ..
 		fi
 		cd KC-RELEASE-3_0_1-SCRIPT
-		sqlplus "${un}"/"${pw}"@"${DBSvrNm}" < KC-Release-3_0-3_0_1-Upgrade-Oracle-Install.sql
+		sqlplus "${un}"/"${pw}${DBSvrNm}" < KC-Release-3_0-3_0_1-Upgrade-Oracle-Install.sql
 		mv *.log ../LOGS/
 		cd .. ;;
 	"MYSQL")
