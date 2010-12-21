8ad1d40aee1de199bf443ee6c3cf0d0ac23427da
Gayathri Athreya <hsggwk@gmail.com>
2010-12-21 22:16:29 +0000

    KCINFR-81: Implemented budget category lookup service. Since the class Hashmap element ended up being used for this service also, moved it out of institutional unit into a common folder and modified institutional unit service to reflect this change.

--- src/main/java/org/kuali/kra/external/HashMapElement.java
+++ src/main/java/org/kuali/kra/external/HashMapElement.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.external.unit;
+package org.kuali.kra.external;
 
 import java.io.Serializable;
 
