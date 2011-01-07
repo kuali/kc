136c77caef664442d439074bb593295ea119d5ed
Kristina Taylor <kbtaylor@uw.edu>
2011-01-07 17:48:46 +0000

    KCINFR-153: Disabled linking from Protocol Special Review

--- src/main/java/org/kuali/kra/irb/ProtocolDocumentRule.java
+++ src/main/java/org/kuali/kra/irb/ProtocolDocumentRule.java
@@ -198,7 +198,7 @@ public class ProtocolDocumentRule extends ResearchDocumentRuleBase  implements A
     
     private boolean processProtocolSpecialReviewBusinessRules(ProtocolDocument document) {
         List<ProtocolSpecialReview> specialReviews = document.getProtocol().getSpecialReviews();
-        return processRules(new SaveSpecialReviewEvent<ProtocolSpecialReview>(SAVE_SPECIAL_REVIEW_FIELD, document, specialReviews));
+        return processRules(new SaveSpecialReviewEvent<ProtocolSpecialReview>(SAVE_SPECIAL_REVIEW_FIELD, document, specialReviews, false));
     }
 
     public boolean processAddProtocolReferenceBusinessRules(AddProtocolReferenceEvent addProtocolReferenceEvent) {
