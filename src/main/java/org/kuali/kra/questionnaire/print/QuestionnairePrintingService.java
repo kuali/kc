94c891aa44ca73a7e1eb534894310b7d18659bc9
Lin-Long Shyu <lshyukuali@gmail.com>
2011-01-20 17:03:20 +0000

    kcirb-1016

--- src/main/java/org/kuali/kra/questionnaire/print/QuestionnairePrintingService.java
+++ src/main/java/org/kuali/kra/questionnaire/print/QuestionnairePrintingService.java
@@ -78,10 +78,9 @@ public interface QuestionnairePrintingService {
      * This method is to get the printables for the questions selected and printed with protocol summary.
      * @param printableBusinessObject
      * @param questionnairesToPrints
-     * @param selectedQid
      * @return
      */
     List<Printable> getQuestionnairePtintable(KraPersistableBusinessObjectBase printableBusinessObject, 
-            List<QuestionnairePrintOption> questionnairesToPrints, Integer selectedQid);
+            List<QuestionnairePrintOption> questionnairesToPrints);
 
 }
