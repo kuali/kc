c2954b88bfe160dff68b874c31f12412d3e14658
Kristina Taylor <kbtaylor@uw.edu>
2011-01-21 18:28:12 +0000

    KRACOEUS-4287: Integrated into 3.1 release scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP1-SCRIPT/MYSQL/SEQUENCES/SEQ_ALL.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP1-SCRIPT/MYSQL/SEQUENCES/SEQ_ALL.sql
@@ -12,3 +12,8 @@ CREATE TABLE SEQ_AWARD_SYNC_LOG_ID
 (
 	id bigint(19) not null auto_increment, primary key (id) 
 ) ENGINE MyISAM;
+
+CREATE TABLE SEQ_TRAINING_STIPEND_RATES_ID
+(
+    id bigint(19) not null auto_increment, primary key (id) 
+) ENGINE MyISAM;
