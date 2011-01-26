98b5ee6ad7e7df07dffba55c05a0b2d5fb525c89
Claus Niesen <cniesen@iastate.edu>
2011-01-26 18:54:51 +0000

    KCINFR-171 IRBAdmin group assignment & sequence updates

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP1-SCRIPT/MYSQL/DML/DML_BS5_KRIM_GROUP_MBR_T.sql
@@ -0,0 +1,4 @@
+INSERT INTO KRIM_GRP_MBR_ID_S VALUES (null);
+INSERT INTO KRIM_GRP_MBR_T(GRP_MBR_ID, VER_NBR, OBJ_ID, GRP_ID, MBR_ID, MBR_TYP_CD, ACTV_FRM_DT, ACTV_TO_DT, LAST_UPDT_DT) 
+SELECT MAX(ID), 0, UUID(), (select grp_id from krim_grp_t where GRP_NM = 'IRBAdmin' and NMSPC_CD = 'KC-WKFLW'), 
+    (select prncpl_id from krim_prncpl_t where prncpl_nm = 'kr'), 'P', NULL, NULL, NULL FROM KRIM_GRP_MBR_ID_S;
