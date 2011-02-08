06489ebd4bc59ce47fc356fe920c747658edf96a
Gayathri Athreya <hsggwk@gmail.com>
2011-02-08 01:27:14 +0000

    KCINFR-187: Exposing the Effort Reporting Service for KFS as part of the integration effort.

---
+++ src/main/java/org/kuali/kra/external/effortReporting/service/EffortReportingService.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.external.effortReporting.service;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+
+/**
+ * This is the external service that is published to the service bus for
+ * accessing effort reporting information stored in the Kuali Coeus system.
+ * 
+ * @author Kuali Coeus Development Team
+ */
+@WebService(name = "effortReportingService", targetNamespace = "KC")
+@SOAPBinding(style = SOAPBinding.Style.DOCUMENT, use = SOAPBinding.Use.LITERAL, parameterStyle = SOAPBinding.ParameterStyle.WRAPPED)
+public interface EffortReportingService {
+
+    boolean isFederalSponsor(@WebParam(name = "financialAccountNumber") String financialAccountNumber);
+
+    String getProjectDirector(@WebParam(name = "financialAccountNumber") String financialAccountNumber);
+}
