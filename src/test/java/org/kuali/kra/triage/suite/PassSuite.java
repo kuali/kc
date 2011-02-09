a5112a55acb0186d8d74476468c57dcd06a5285c
Douglas Pace <dpace@kuali.org>
2011-02-09 22:22:11 +0000

    KRACOEUS-4307: Changes to the drop down for the budget period search and a unit test for the results.

--- src/test/java/org/kuali/kra/triage/suite/PassSuite.java
+++ src/test/java/org/kuali/kra/triage/suite/PassSuite.java
@@ -486,7 +486,8 @@ import org.junit.runners.Suite.SuiteClasses;
     org.kuali.kra.award.home.fundingproposal.ProjectPersonnelDataFeedCommandTest.class,
     org.kuali.kra.award.home.fundingproposal.SpecialReviewDataFeedCommandTest.class,
     org.kuali.kra.award.lookup.keyvalue.AwardUnitContactTypeValuesFinderTest.class,
-    org.kuali.kra.s2s.generator.impl.PHS398ResTrainProgPlanV1_0GeneratorTest.class
+    org.kuali.kra.s2s.generator.impl.PHS398ResTrainProgPlanV1_0GeneratorTest.class,
+    org.kuali.kra.award.budget.AwardBudgetServiceImplTest.class
 })
 /** these are all the test class where all the test methods are currently passing. */
 public class PassSuite {
