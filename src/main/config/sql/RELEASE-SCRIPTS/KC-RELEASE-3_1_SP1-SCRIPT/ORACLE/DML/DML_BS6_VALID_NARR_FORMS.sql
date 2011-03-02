8c66bbe07e7dd2027929ebd985e654358287c488
Kristina Taylor <kbtaylor@uw.edu>
2011-03-02 21:08:55 +0000

    KRACOEUS-4446: Release 3 Sprint 1 Scripts

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP1-SCRIPT/ORACLE/DML/DML_BS6_VALID_NARR_FORMS.sql
@@ -0,0 +1,44 @@
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',88,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',264)
+;                                                                                                                                                                
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',89,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',265)
+;                                                                                                                                                                                                                                                                                                                    
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',90,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',266)
+;                                                                                                                                                                
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',91,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',267)
+;                                                                                                                                                                
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',92,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',268)
+;                                                                                                                                                                       
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',93,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',269)
+;                                                                                                                                                                       
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',94,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',270)
+;                                                                                                                                                                       
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',96,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',271)
+;                                                                                                                                                                
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',97,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',272)
+;                                                                                                                                                                       
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',98,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',273)
+;                                                                                                                                                                        
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',102,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',274)
+;                                                                                                                                                                        
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',103,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',275)
+;                                                                                                                                                                 
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',104,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',276)
+;                                                                                                                                                                        
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',105,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',277)
+;                                                                                                                                                                        
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',106,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',278)
+;                                                                                                                                                                        
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',107,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',279)
+;                                                                                                                                                                 
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',108,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',280)
+;                                                                                                                                                                        
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',109,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',281)
+;                                                                                                                                                                        
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',110,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',282)
+;                                                                                                                                                                        
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',127,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',263)
+;                                                                                                                                                                 
+insert into VALID_NARR_FORMS (FORM_NAME,MANDATORY,NARRATIVE_TYPE_CODE,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VALID_NARR_FORMS_ID) values ('PHS_Fellowship_Supplemental_1_2-V1.2','N',134,'9D10678A9475AF6EE040DC0A1F8A7B3E',sysdate,'KRDBA',262)
+;
+
+COMMIT;
