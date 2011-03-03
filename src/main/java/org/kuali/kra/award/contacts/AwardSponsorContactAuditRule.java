ab118ee99e952993fb09d5e03b107008af2df12f
Douglas Pace <dpace@kuali.org>
2011-03-03 20:21:25 +0000

    KRACOEUS-4482: Adding validation rule to require at least one sponsor contact.

---
+++ src/main/java/org/kuali/kra/award/contacts/AwardSponsorContactAuditRule.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.award.contacts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kra.award.document.AwardDocument;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.rice.kns.document.Document;
+import org.kuali.rice.kns.rule.DocumentAuditRule;
+import org.kuali.rice.kns.util.AuditCluster;
+import org.kuali.rice.kns.util.AuditError;
+import org.kuali.rice.kns.util.GlobalVariables;
+
+/**
+ * This class processes audit rules (warnings) for the Report Information related
+ * data of the AwardDocument.
+ */
+public class AwardSponsorContactAuditRule implements DocumentAuditRule {
+
+    private static final String AWARD_SPONSOR_CONTACT_LIST_ERROR_KEY = "document.awardList[0].sponsorContact.auditErrors";
+    private static final String ERROR_AWARD_NO_SPONSOR_CONTACTS = "error.awardSponsorContact.none";
+    private static final String CONTACTS_AUDIT_ERRORS = "contactsAuditErrors";
+    private List<AuditError> auditErrors;
+
+    
+    /**
+     * 
+     * Constructs a AwardSponsorContactAuditRule.java. Added so unit test would not
+     * need to call processRunAuditBusinessRules and therefore declare a document.
+     */
+    public AwardSponsorContactAuditRule() {
+        auditErrors = new ArrayList<AuditError>();
+    }
+    
+    /**
+     * @see org.kuali.core.rule.DocumentAuditRule#processRunAuditBusinessRules(org.kuali.core.document.Document)
+     */
+    public boolean processRunAuditBusinessRules(Document document) {
+        boolean valid = true;
+        AwardDocument awardDocument = (AwardDocument)document;
+        auditErrors = new ArrayList<AuditError>();
+        
+        valid &= checkForAtLeastOneSponsorContact(awardDocument.getAward().getSponsorContacts());
+         
+        reportAndCreateAuditCluster();
+        
+        return valid;
+    }
+        
+    /**
+     * This method creates and adds the AuditCluster to the Global AuditErrorMap.
+     */
+    @SuppressWarnings("unchecked")
+    protected void reportAndCreateAuditCluster() {
+        if (auditErrors.size() > 0) {
+            AuditCluster existingErrors = (AuditCluster) GlobalVariables.getAuditErrorMap().get(CONTACTS_AUDIT_ERRORS);
+            if (existingErrors == null) {
+                GlobalVariables.getAuditErrorMap().put(CONTACTS_AUDIT_ERRORS, new AuditCluster(Constants.CONTACTS_PANEL_NAME,
+                                                                                              auditErrors, Constants.AUDIT_ERRORS));
+            } else {
+                existingErrors.getAuditErrorList().addAll(auditErrors);
+            }
+        }
+    }
+    
+    protected boolean checkForAtLeastOneSponsorContact(List<AwardSponsorContact> sponsorContacts) {
+        if (sponsorContacts.isEmpty()) {
+            auditErrors.add(new AuditError(AWARD_SPONSOR_CONTACT_LIST_ERROR_KEY, ERROR_AWARD_NO_SPONSOR_CONTACTS,                    
+                    Constants.MAPPING_AWARD_CONTACTS_PAGE + "." + Constants.CONTACTS_PANEL_ANCHOR));
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
