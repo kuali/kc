f06ce25bc020f982789b051a62772b61014de1ae
Kristina Taylor <kbtaylor@uw.edu>
2011-03-07 22:18:28 +0000

    KCINFR-201: Pass 3 of Selenium 2 integration tests

--- src/test/java/org/kuali/kra/institutionalproposal/web/InstitutionalProposalSeleniumTestBase.java
+++ src/test/java/org/kuali/kra/institutionalproposal/web/InstitutionalProposalSeleniumTestBase.java
@@ -22,7 +22,11 @@ import org.kuali.kra.test.infrastructure.KcSeleniumTestBase;
  */
 public abstract class InstitutionalProposalSeleniumTestBase extends KcSeleniumTestBase {
     
-    private static final String PAGE_TITLE = "Kuali :: KC Institutional Proposal";
+    private static final String PROPOSAL_LOG_PAGE_TITLE = "Kuali :: Proposal Log";
+    
+    private static final String CREATE_PROPOSAL_LOG_LINK_NAME = "Proposal Log";
+    
+    private static final String INSTITUTIONAL_PROPOSAL_PAGE_TITLE = "Kuali :: KC Institutional Proposal";
     
     private static final String CREATE_INSTITUTIONAL_PROPOSAL_LINK_NAME = "Institutional Proposal";
     private static final String HOME_LINK_NAME = "home";
@@ -34,17 +38,48 @@ public abstract class InstitutionalProposalSeleniumTestBase extends KcSeleniumTe
     private static final String ACTIONS_LINK_NAME = "institutionalProposalActions";
     
     private static final String DOCUMENT_DESCRIPTION_ID = "document.documentHeader.documentDescription";
-    private static final String INSTITUTIONAL_PROPOSAL_LIST_PREFIX = "document.institutionalProposalList[0].";
-    private static final String DOCUMENT_STATUS_CODE_ID = INSTITUTIONAL_PROPOSAL_LIST_PREFIX + "statusCode";
-    private static final String DEFAULT_ACTIVITY_TYPE_CODE_ID = INSTITUTIONAL_PROPOSAL_LIST_PREFIX + "activityTypeCode";
-    private static final String DOCUMENT_PROPOSAL_TYPE_CODE_ID = INSTITUTIONAL_PROPOSAL_LIST_PREFIX + "proposalTypeCode";
-    private static final String DOCUMENT_PROJECT_TITLE_ID = INSTITUTIONAL_PROPOSAL_LIST_PREFIX + "title";
-    private static final String DOCUMENT_SPONSOR_CODE_ID = INSTITUTIONAL_PROPOSAL_LIST_PREFIX + "sponsorCode";
+    
+    private static final String PL_DEFAULT_DOCUMENT_DESCRIPTION = "Proposal Log Web Test";
+    private static final String PL_LIST_PREFIX = "document.newMaintainableObject.";
+    private static final String PL_PROPOSAL_TYPE_CODE_ID = PL_LIST_PREFIX + "proposalTypeCode";
+    private static final String PL_TITLE = PL_LIST_PREFIX + "title";
+    private static final String PL_PERSON_USER_NAME = PL_LIST_PREFIX + "person.userName";
+    private static final String PL_LEAD_UNIT = PL_LIST_PREFIX + "leadUnit";
+    
+    private static final String IP_LIST_PREFIX = "document.institutionalProposalList[0].";
+    private static final String IP_STATUS_CODE_ID = "document.institutionalProposal.statusCode";
+    private static final String IP_ACTIVITY_TYPE_CODE_ID = IP_LIST_PREFIX + "activityTypeCode";
+    private static final String IP_PROPOSAL_TYPE_CODE_ID = IP_LIST_PREFIX + "proposalTypeCode";
+    private static final String IP_TITLE_ID = IP_LIST_PREFIX + "title";
+    private static final String IP_SPONSOR_CODE_ID = IP_LIST_PREFIX + "sponsorCode";
+    
+    private static final String PL_DEFAULT_PROPSAL_TYPE = "New";
+    private static final String PL_DEFAULT_TITLE = "Test Project";
+    private static final String PL_DEFAULT_PERSON_USER_NAME = "majors";
+    private static final String PL_DEFAULT_LEAD_UNIT = "000001";
 
-    private static final String DEFAULT_DOCUMENT_DESCRIPTION = "Institutional Proposal Web Test";
-    private static final String DEFAULT_PROJECT_TITLE = "Test Project";
-    private static final String DEFAULT_SPONSOR_CODE = "005891";
-    private static final String ONE = "1";
+    private static final String IP_DEFAULT_DOCUMENT_DESCRIPTION = "Institutional Proposal Web Test";
+    private static final String IP_DEFAULT_STATUS = "Pending";
+    private static final String IP_DEFAULT_PROPOSAL_TYPE = "New";
+    private static final String IP_DEFAULT_ACTIVITY_TYPE = "Research";
+    private static final String IP_DEFAULT_PROJECT_TITLE = "Test Project";
+    private static final String IP_DEFAULT_SPONSOR_CODE = "005891";
+    
+    protected final void createProposalLog() {
+        clickCentralAdminTab();
+        
+        click(CREATE_PROPOSAL_LOG_LINK_NAME);
+        
+        assertTitleContains(PROPOSAL_LOG_PAGE_TITLE);
+        
+        set(DOCUMENT_DESCRIPTION_ID, PL_DEFAULT_DOCUMENT_DESCRIPTION);
+        set(PL_PROPOSAL_TYPE_CODE_ID, PL_DEFAULT_PROPSAL_TYPE);
+        set(PL_TITLE, PL_DEFAULT_TITLE);
+        set(PL_PERSON_USER_NAME, PL_DEFAULT_PERSON_USER_NAME);
+        set(PL_LEAD_UNIT, PL_DEFAULT_LEAD_UNIT);
+        
+        blanketApproveDocument();
+    }
         
     /**
      * Creates a new instance of the Institutional Proposal page, filling in all required values, and saving.
@@ -52,8 +87,12 @@ public abstract class InstitutionalProposalSeleniumTestBase extends KcSeleniumTe
     protected final void createInstitutionalProposal() {
         clickCentralAdminTab();
         
+        createProposalLog();
+        
+        clickCentralAdminTab();
+        
         lookupProposalLog();
-        assertTitleContains(PAGE_TITLE);
+        assertTitleContains(INSTITUTIONAL_PROPOSAL_PAGE_TITLE);
         
         setDefaultRequiredFields();
         
@@ -69,21 +108,25 @@ public abstract class InstitutionalProposalSeleniumTestBase extends KcSeleniumTe
 
         click("methodToCall.search");
         
-        assertTableCellValue("row", 0, 0, "return value");
+        assertTableCellValue("row", 0, 0, "select");
         
-        click("return value");
+        click("select", true);
     }
     
     /**
      * Sets the Institutional Proposal's required fields to legal default values.
      */
     protected void setDefaultRequiredFields() {
-        set(DOCUMENT_DESCRIPTION_ID, DEFAULT_DOCUMENT_DESCRIPTION);
-        set(DOCUMENT_STATUS_CODE_ID, ONE);
-        set(DEFAULT_ACTIVITY_TYPE_CODE_ID, ONE);
-        set(DOCUMENT_PROPOSAL_TYPE_CODE_ID, ONE);
-        set(DOCUMENT_PROJECT_TITLE_ID, DEFAULT_PROJECT_TITLE);
-        set(DOCUMENT_SPONSOR_CODE_ID, DEFAULT_SPONSOR_CODE);
+        set(DOCUMENT_DESCRIPTION_ID, IP_DEFAULT_DOCUMENT_DESCRIPTION);
+        
+        openTab(1);
+        set(IP_STATUS_CODE_ID, IP_DEFAULT_STATUS);
+        set(IP_PROPOSAL_TYPE_CODE_ID, IP_DEFAULT_PROPOSAL_TYPE);
+        set(IP_ACTIVITY_TYPE_CODE_ID, IP_DEFAULT_ACTIVITY_TYPE);
+        set(IP_TITLE_ID, IP_DEFAULT_PROJECT_TITLE);
+        
+        openTab(2);
+        set(IP_SPONSOR_CODE_ID, IP_DEFAULT_SPONSOR_CODE);
     }
     
     /**
