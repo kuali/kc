f4aded118f8348fe45bdac0c00de18f68ff38efc
Kristina Taylor <kbtaylor@uw.edu>
2011-03-14 17:49:19 +0000

    KCINFR-202, KCIRB-1319: Release 3.1 Sprint 2 Scripts

---
+++ src/main/config/sql/log/KCINFR-202.sql
@@ -0,0 +1,2 @@
+insert into krns_parm_t values('KUALI', 'KC-PD', 'Document', 'INVALID_FILE_NAME_CHECK', 1, 'CONFG', '1', 'Set this to 1 if an error should be thrown when invalid characters are found in the file names of attachments or to 2 if a warning should be thrown instead.', 'A', '355488b6-1378-476c-a80e-d4d161c28a11');
+
