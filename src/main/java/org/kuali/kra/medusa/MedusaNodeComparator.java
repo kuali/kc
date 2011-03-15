24050e4fc7cacb473c77916e57d06480a07a7a2a
Ge Zhang <g1zhang@ucsd.edu>
2011-03-15 07:40:20 +0000

    KRACOEUS-4314 Medusa: nodes in each tier should be sorted in ascending order fixed by recursively sort the tree

---
+++ src/main/java/org/kuali/kra/medusa/MedusaNodeComparator.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.medusa;
+
+import java.util.Comparator;
+import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
+
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.kra.award.home.Award;
+import org.kuali.kra.institutionalproposal.home.InstitutionalProposal;
+import org.kuali.kra.proposaldevelopment.bo.DevelopmentProposal;
+import org.kuali.kra.medusa.service.MedusaService;
+
+
+public class MedusaNodeComparator implements Comparator<MedusaNode> {
+
+    public int compare(MedusaNode m1, MedusaNode m2) {
+        return getNodeValue(m1).compareTo(getNodeValue(m2));
+    }   
+
+    private String getNodeValue(MedusaNode m){             
+        String nodeType = m.getType();  
+        BusinessObject mBo = m.getBo();
+        if(!StringUtils.isNotBlank(nodeType) || mBo == null){
+            return "medusa: unknown medusa node type";
+        }
+        else{
+            if(StringUtils.equals(nodeType, "award")){
+                return nodeType + ((Award)mBo).getAwardNumber();
+            }
+            else if(StringUtils.equals(nodeType, "IP"))
+                return nodeType + ((InstitutionalProposal)mBo).getProposalNumber();
+            else if(StringUtils.equals(nodeType, "DP")) 
+                return nodeType + ((DevelopmentProposal)mBo).getProposalNumber();    
+            else
+                return "medusa: unsupported medusa node type";
+        }
+    }
+}
