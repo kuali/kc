e38caa96fb6b5e17962fd88665937bde6de60821
Kristina Taylor <kbtaylor@uw.edu>
2011-03-15 18:50:35 +0000

    KCINFR-223, KCINFR-224: Release 3.1 Sprint 2 Scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP2-SCRIPT/MYSQL/DML/DML_BS7_KRNS_PARM_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP2-SCRIPT/MYSQL/DML/DML_BS7_KRNS_PARM_T.sql
@@ -3,4 +3,8 @@ insert into krns_parm_t values('KUALI', 'KC-PD', 'Document', 'INVALID_FILE_NAME_
 INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID)
     VALUES ('KC', 'KC-GEN', 'All', 'CostShareProjectPeriodNameLabel', 1, 'CONFG', 'Project Period', 'The label of the project period field on cost share screens', 'A', UUID());
 
+INSERT INTO KRNS_PARM_T (NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, obj_id)
+  VALUES('KC-GEN', 'All', 'personrole.nih.coi.mpi', 'CONFG', 'PI/Multiple', 'Description of principal investigator multiple for NIH Proposals', 'A', uuid());
+UPDATE KRNS_PARM_T set TXT = 'Co-Investigator' where PARM_DTL_TYP_CD = 'All' and PARM_NM = 'personrole.nih.coi' and parm_TYP_CD = 'CONFG';
+
 COMMIT;
