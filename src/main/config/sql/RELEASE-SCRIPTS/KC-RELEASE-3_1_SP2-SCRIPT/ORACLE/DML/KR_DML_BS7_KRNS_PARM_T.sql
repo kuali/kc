35c011c4f07757d1ec1e3b70fe0c6ed3a2a9368a
Kristina Taylor <kbtaylor@uw.edu>
2011-03-29 19:56:16 +0000

    NOJIRA: Release 3.1 Sprint 2 Script Fixes

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP2-SCRIPT/ORACLE/DML/KR_DML_BS7_KRNS_PARM_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP2-SCRIPT/ORACLE/DML/KR_DML_BS7_KRNS_PARM_T.sql
@@ -1,10 +1,11 @@
-insert into krns_parm_t values('KUALI', 'KC-PD', 'Document', 'INVALID_FILE_NAME_CHECK', 1, 'CONFG', '1', 'Set this to 1 if an error should be thrown when invalid characters are found in the file names of attachments or to 2 if a warning should be thrown instead.', 'A', '355488b6-1378-476c-a80e-d4d161c28a11');
+insert into krns_parm_t (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID)
+    values('KUALI', 'KC-PD', 'Document', 'INVALID_FILE_NAME_CHECK', 1, 'CONFG', '1', 'Set this to 1 if an error should be thrown when invalid characters are found in the file names of attachments or to 2 if a warning should be thrown instead.', 'A', '355488b6-1378-476c-a80e-d4d161c28a11');
 
 INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID)
     VALUES ('KC', 'KC-GEN', 'All', 'CostShareProjectPeriodNameLabel', 1, 'CONFG', 'Project Period', 'The label of the project period field on cost share screens', 'A', SYS_GUID());
 
-INSERT INTO KRNS_PARM_T (NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, obj_id)
-  VALUES('KC-GEN', 'All', 'personrole.nih.coi.mpi', 'CONFG', 'PI/Multiple', 'Description of principal investigator multiple for NIH Proposals', 'A', sys_guid());
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, obj_id)
+  VALUES('KC', 'KC-GEN', 'All', 'personrole.nih.coi.mpi', 'CONFG', 'PI/Multiple', 'Description of principal investigator multiple for NIH Proposals', 'A', sys_guid());
 UPDATE KRNS_PARM_T set TXT = 'Co-Investigator' where PARM_DTL_TYP_CD = 'All' and PARM_NM = 'personrole.nih.coi' and parm_TYP_CD = 'CONFG';
 
 Insert into KRNS_PARM_T (APPL_NMSPC_CD, nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd)
