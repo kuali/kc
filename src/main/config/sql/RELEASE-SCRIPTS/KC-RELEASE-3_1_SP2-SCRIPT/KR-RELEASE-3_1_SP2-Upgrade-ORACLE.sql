6e8e28edef6958e6f7e05e7b483acdac7f1322d1
Kristina Taylor <kbtaylor@uw.edu>
2011-03-29 15:15:26 +0000

    KCIRB-1332: Release 3.1 Sprint 2 Scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP2-SCRIPT/KR-RELEASE-3_1_SP2-Upgrade-ORACLE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP2-SCRIPT/KR-RELEASE-3_1_SP2-Upgrade-ORACLE.sql
@@ -1,6 +1,4 @@
-set define off
-set sqlblanklines on
-spool KR-RELEASE-3_1_SP2-Upgrade-ORACLE-Install.log
+set define off\nset sqlblanklines on\nspool KR-RELEASE-3_1_SP2-Upgrade-ORACLE-Install.log
 @ORACLE/DML/ORACLE/DML/KR_DML_BS1_KRIM_ID_CLEAN.sql
 @ORACLE/DML/ORACLE/DML/KR_DML_BS2_KRIM_ID_CLEAN.sql
 @ORACLE/DML/ORACLE/DML/KR_DML_BS3_KRIM_PERM_T.sql
@@ -10,5 +8,4 @@ spool KR-RELEASE-3_1_SP2-Upgrade-ORACLE-Install.log
 @ORACLE/DML/ORACLE/DML/KR_DML_BS6_KRIM_ROLE_MBR_T.sql
 @ORACLE/DML/ORACLE/DML/KR_DML_BS7_KRIM_ROLE_MBR_ATTR_DATA_T.sql
 @ORACLE/DML/ORACLE/DML/KR_DML_BS7_KRNS_PARM_T.sql
-commit;
-exit;
+commit;\nexit;
