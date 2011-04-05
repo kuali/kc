abb4b6787af23cb1750c70bc7509b70dfc0ee24c
Kristina Taylor <kbtaylor@uw.edu>
2011-04-05 13:12:19 +0000

    KRACOEUS-4577: Release 3.1 Sprint 3 Scripts

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/KC-RELEASE-3_1_SP3-Upgrade-ORACLE.sql
@@ -0,0 +1,3 @@
+set define off\nset sqlblanklines on\nspool KC-RELEASE-3_1_SP3-Upgrade-ORACLE-Install.log
+@ORACLE/DML/KC_DML_BS1_SPONSOR_FORM_TEMPLATES.sql
+commit;\nexit;
---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/KR-RELEASE-3_1_SP3-Upgrade-ORACLE.sql
@@ -0,0 +1,2 @@
+set define off\nset sqlblanklines on\nspool KR-RELEASE-3_1_SP3-Upgrade-ORACLE-Install.log
+commit;\nexit;
