f1748473fa7f64c293f2c595a51d43caaaa6a794
Gayathri Athreya <hsggwk@gmail.com>
2011-04-12 16:43:21 +0000

    KRACOEUS-4587: Adding SQL from KRACOEUS-4571 to release scripts.

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/KR-RELEASE-3_1_SP3-Upgrade-ORACLE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/KR-RELEASE-3_1_SP3-Upgrade-ORACLE.sql
@@ -1,3 +1,6 @@
-set define off\nset sqlblanklines on\nspool KR-RELEASE-3_1_SP3-Upgrade-ORACLE-Install.log
+set define off
+set sqlblanklines on
+spool KR-RELEASE-3_1_SP3-Upgrade-ORACLE-Install.log
 @ORACLE/DML/KR_DML_BS1_KRNS_PARM_T.sql
-commit;\nexit;
+commit;
+exit;
