fe700253b84265de4a83f2d59ac934a9761e2c34
Kristina Taylor <kbtaylor@uw.edu>
2011-04-15 20:07:55 +0000

    Fixing false kim sequence creation scripts

--- src/main/config/sql/RELEASE-SCRIPTS/INSTALL-SHARED/ORACLE/KR_CLEAN_SEQ_BS.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/INSTALL-SHARED/ORACLE/KR_CLEAN_SEQ_BS.sql
@@ -7,3 +7,6 @@ drop sequence KRIM_PERM_ID_BS_S;
 drop sequence KRIM_ROLE_PERM_ID_BS_S;
 drop sequence KRIM_ATTR_DATA_ID_BS_S;
 drop sequence KRIM_GRP_MBR_ID_BS_S;
+drop sequence KRIM_ROLE_RSP_ID_BS_S;
+drop sequence KRIM_ROLE_ID_BS_S;
+drop sequence KRIM_ROLE_RSP_ACTN_ID_BS_S;
