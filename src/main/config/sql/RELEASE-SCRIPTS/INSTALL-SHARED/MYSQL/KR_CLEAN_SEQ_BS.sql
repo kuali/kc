4a7bcaad45d0ffb9c009e777ef06df284e76cfdd
Kristina Taylor <kbtaylor@uw.edu>
2011-04-18 16:03:56 +0000

    Updating release scripts

---
+++ src/main/config/sql/RELEASE-SCRIPTS/INSTALL-SHARED/MYSQL/KR_CLEAN_SEQ_BS.sql
@@ -0,0 +1,8 @@
+drop table KRIM_ROLE_MBR_ID_BS_S;
+drop table KRIM_PERM_ID_BS_S;
+drop table KRIM_ROLE_PERM_ID_BS_S;
+drop table KRIM_ATTR_DATA_ID_BS_S;
+drop table KRIM_GRP_MBR_ID_BS_S;
+drop table KRIM_ROLE_RSP_ID_BS_S;
+drop table KRIM_ROLE_ID_BS_S;
+drop table KRIM_ROLE_RSP_ACTN_ID_BS_S;
