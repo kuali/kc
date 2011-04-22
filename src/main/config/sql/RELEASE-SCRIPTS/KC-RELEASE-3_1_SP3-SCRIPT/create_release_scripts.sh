da07492ba5662f26a977087d392e2c13bb6ea8f0
Kristina Taylor <kbtaylor@uw.edu>
2011-04-22 19:30:11 +0000

    KCIRB-1352: Release 3.1 Sprint 3 Scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/create_release_scripts.sh
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP3-SCRIPT/create_release_scripts.sh
@@ -11,34 +11,38 @@ sql_dirs='SEQUENCES TABLES DML CONSTRAINTS VIEWS'
 
 make_file()
 {
-	if [ "${thisDB}" = "ORACLE" ]
-	then
-		echo "set define off\nset sqlblanklines on\nspool ${logFile}"
-	fi
-	for thisDir in ${sql_dirs}
-	do
-		for thisFile in `ls ${thisDB}/${thisDir}/${thisApp}_* 2>/dev/null`
-		do
-			if [ "${thisDB}" = "ORACLE" ]
-			then
-				printf '@'
-			else
-				printf '\. '
-			fi
-			echo "${thisFile}"
-		done
-	done
-	echo "commit;\nexit;"
+    if [ "${thisDB}" = "ORACLE" ]
+    then
+        echo "set define off"
+        echo "set sqlblanklines on"
+        echo "spool ${logFile}"
+    fi
+    for thisDir in ${sql_dirs}
+    do
+        for thisFile in `ls ${thisDB}/${thisDir}/${thisApp}_* 2>/dev/null`
+        do
+            if [ "${thisDB}" = "ORACLE" ]
+            then
+                printf '@'
+            else
+                printf '\. '
+            fi
+            echo "${thisFile}"
+        done
+    done
+    echo "commit;"
+    if [ "${thisDB}" = "ORACLE" ]
+    then
+        echo "exit;"
+    fi
 }
 
 for thisApp in ${app_names}
 do
-	for thisDB in ${db_types}
-	do
-		sqlFile="${thisApp}-${base_version}-Upgrade-${thisDB}.sql"
-		logFile="${thisApp}-${base_version}-Upgrade-${thisDB}-Install.log"
-		make_file > ${sqlFile}
-	done
-done	
-
-
+    for thisDB in ${db_types}
+    do
+        sqlFile="${thisApp}-${base_version}-Upgrade-${thisDB}.sql"
+        logFile="${thisApp}-${base_version}-Upgrade-${thisDB}-Install.log"
+        make_file > ${sqlFile}
+    done
+done
