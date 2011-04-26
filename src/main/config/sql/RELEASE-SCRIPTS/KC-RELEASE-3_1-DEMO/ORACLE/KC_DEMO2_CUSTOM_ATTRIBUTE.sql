43dd7e8984d199c8871757a7ef09885828ae89f3
Kristina Taylor <kbtaylor@uw.edu>
2011-04-26 19:35:27 +0000

    Demo data cleanup

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1-DEMO/ORACLE/KC_DEMO2_CUSTOM_ATTRIBUTE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1-DEMO/ORACLE/KC_DEMO2_CUSTOM_ATTRIBUTE.sql
@@ -1,30 +1,30 @@
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (40,'1','asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',1,'Billing Element','billingElement',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (40,'1','asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Billing Element','billingElement',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (30,'1','asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',2,'Cost Sharing Budget','costSharingBudget',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (30,'1','asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Cost Sharing Budget','costSharingBudget',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (6,'2','asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',3,'# of Trainees','numberOfTrainees',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (6,'2','asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'# of Trainees','numberOfTrainees',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (6,'2','Personnel Items for Review',4,'Graduate Student Count','graduateStudentCount',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (6,'2','Personnel Items for Review',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Graduate Student Count','graduateStudentCount',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,LOOKUP_CLASS,LOOKUP_RETURN,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (30,'1','Personnel Items for Review',5,'Tenured','org.kuali.kra.bo.KcPerson','userName','tenured',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (30,'1','Personnel Items for Review',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Tenured','org.kuali.kra.bo.KcPerson','userName','tenured',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (30,'1','Project Details',6,'Export Controls','exportControls',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (30,'1','Project Details',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Export Controls','exportControls',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,LOOKUP_CLASS,LOOKUP_RETURN,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (30,'1','Project Details',7,'Inventions','org.kuali.kra.bo.KcPerson','userName','inventions',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (30,'1','Project Details',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Inventions','org.kuali.kra.bo.KcPerson','userName','inventions',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (10,'3','asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',8,'Local Review Date','localReviewDate',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (10,'3','asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Local Review Date','localReviewDate',SYS_GUID(),TO_DATE( '20080221000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (80,'1','Course Related',9,'Course Name','courseName',SYS_GUID(),TO_DATE( '20090317000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (80,'1','Course Related',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Course Name','courseName',SYS_GUID(),TO_DATE( '20090317000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
 INSERT INTO CUSTOM_ATTRIBUTE (DATA_LENGTH,DATA_TYPE_CODE,GROUP_NAME,ID,LABEL,LOOKUP_CLASS,LOOKUP_RETURN,NAME,OBJ_ID,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR)
-  VALUES (80,'1','Course Related',10,'Instructor Name','org.kuali.kra.bo.KcPerson','fullName','instructorName',SYS_GUID(),TO_DATE( '20090317000000', 'YYYYMMDDHH24MISS' ),'admin',1)
+  VALUES (80,'1','Course Related',SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'Instructor Name','org.kuali.kra.bo.KcPerson','fullName','instructorName',SYS_GUID(),TO_DATE( '20090317000000', 'YYYYMMDDHH24MISS' ),'admin',1)
 /
