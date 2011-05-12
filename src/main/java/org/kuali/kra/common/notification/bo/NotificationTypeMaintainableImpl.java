0c8449c56bc09596ba7d8c194f0020660d366d4e
Kristina Taylor <kbtaylor@uw.edu>
2011-05-12 21:28:56 +0000

    KCINFR-262, KCINFR-263: Initial structure for Notification Type and Notification Type Recipient

---
+++ src/main/java/org/kuali/kra/common/notification/bo/NotificationTypeMaintainableImpl.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.common.notification.bo;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.maintenance.KraMaintainableImpl;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.maintenance.Maintainable;
+import org.kuali.rice.kns.service.SequenceAccessorService;
+import org.kuali.rice.kns.web.ui.Field;
+import org.kuali.rice.kns.web.ui.Row;
+import org.kuali.rice.kns.web.ui.Section;
+
+/**
+ * Maintainable implementation for handling the sequence for Notification Number.
+ */
+public class NotificationTypeMaintainableImpl extends KraMaintainableImpl {
+
+    private static final long serialVersionUID = 5816092888763754328L;
+    
+    private static final String NOTIFICATION_TYPE_ID_SEQUENCE_NAME = "SEQ_NOTIFICATION_TYPE_ID";
+    private static final String NOTIFICATION_TYPE_SECTION_ID = "Edit Notification Types";
+    private static final String NOTIFICATION_TYPE_ID_NAME = "notificationTypeId";
+    
+    private transient SequenceAccessorService sequenceAccessorService;
+    
+    /**
+     * {@inheritDoc}
+     * @see org.kuali.rice.kns.maintenance.KualiMaintainableImpl#setGenerateDefaultValues(java.lang.String)
+     */
+    @Override
+    public void setGenerateDefaultValues(String docTypeName) {
+        super.setGenerateDefaultValues(docTypeName);
+        
+        NotificationType notificationType = (NotificationType) getBusinessObject();
+        Long nextNotificationTypeId = getSequenceAccessorService().getNextAvailableSequenceNumber(NOTIFICATION_TYPE_ID_SEQUENCE_NAME, NotificationType.class);
+        notificationType.setNotificationTypeId(nextNotificationTypeId);
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see org.kuali.kra.maintenance.KraMaintainableImpl#getSections(org.kuali.rice.kns.document.MaintenanceDocument, 
+     *      org.kuali.rice.kns.maintenance.Maintainable)
+     */
+    @SuppressWarnings("unchecked")
+    public List<Section> getSections(MaintenanceDocument document, Maintainable oldMaintainable) {
+        List<Section> sections = super.getSections(document, oldMaintainable);
+        
+        for (Section section : sections) {
+            if (StringUtils.equals(section.getSectionId(), NOTIFICATION_TYPE_SECTION_ID)) {
+                for (Row row : section.getRows()) {
+                    for (Field field : row.getFields()) {
+                        if (StringUtils.equals(field.getPropertyName(), NOTIFICATION_TYPE_ID_NAME)) {
+                            field.setReadOnly(true);
+                        }
+                    }
+                }
+            }
+        }
+
+        return sections;
+    }
+    
+    public SequenceAccessorService getSequenceAccessorService() {
+        if (sequenceAccessorService == null) {
+            sequenceAccessorService = KraServiceLocator.getService(SequenceAccessorService.class);
+        }
+        return sequenceAccessorService;
+    }
+
+    public void setSequenceAccessorService(SequenceAccessorService sequenceAccessorService) {
+        this.sequenceAccessorService = sequenceAccessorService;
+    }
+
+}
