2456f01aba78ec0efdb6a1f71b89762456ec7d15
Lin-Long Shyu <lshyukuali@gmail.com>
2011-05-16 16:54:45 +0000

    kracoeus-4690

--- src/main/java/org/kuali/kra/questionnaire/answer/AnswerComparator.java
+++ src/main/java/org/kuali/kra/questionnaire/answer/AnswerComparator.java
@@ -21,6 +21,9 @@ import java.util.Comparator;
 import java.util.List;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.kuali.kra.irb.actions.abandon.ProtocolAbandonServiceImpl;
 
 /**
  * 
@@ -28,48 +31,60 @@ import org.apache.commons.lang.ObjectUtils;
  * on questionnaire questions order
  */
 public class AnswerComparator implements Comparator<Answer>  {
-
+    private static final Log LOG = LogFactory.getLog(AnswerComparator.class);
+    
     public int compare(Answer ans1, Answer argAnswer) {
-//        if ((ans1.getQuestionnaireQuestionsIdFk() == 5883 && argAnswer.getQuestionnaireQuestionsIdFk() == 5893)
-//                || (ans1.getQuestionnaireQuestionsIdFk() == 5893 && argAnswer.getQuestionnaireQuestionsIdFk() == 5883)) {
-//            System.out.println("sort print matched 5893 & 5883 ");
-//        }
 
         int retVal = 0;
        if (ObjectUtils.equals(ans1.getQuestionNumber(), argAnswer.getQuestionNumber())) {
-            retVal =  ans1.getAnswerNumber().compareTo(argAnswer.getAnswerNumber());
+           // question with multiple answers.  then compare answer number
+           retVal =  ans1.getAnswerNumber().compareTo(argAnswer.getAnswerNumber());
         } else if (ObjectUtils.equals(ans1.getQuestionnaireQuestion().getParentQuestionNumber(), argAnswer.getQuestionnaireQuestion()
                 .getParentQuestionNumber())) {
-            //retVal = this.getQuestionNumber().compareTo(argAnswer.getQuestionNumber());
+            // questions with same parent
             retVal = ans1.getQuestionnaireQuestion().getQuestionSeqNumber().compareTo(argAnswer.getQuestionnaireQuestion().getQuestionSeqNumber());
         } else if (ObjectUtils.equals(ans1.getQuestionnaireQuestion().getParentQuestionNumber(), argAnswer.getQuestionNumber())) {
+            // argAnswer is the parent question of ans1
             retVal = 1;
         } else if (ObjectUtils.equals(ans1.getQuestionNumber(), argAnswer.getQuestionnaireQuestion().getParentQuestionNumber())) {
+            // ans1 is the parent question of argAnswer
             retVal = -1;
         } else if (ans1.getQuestionnaireQuestion().getParentQuestionNumber() == 0
                 && argAnswer.getQuestionnaireQuestion().getParentQuestionNumber() != 0) {
+            // ans1 is the root question
             retVal = ans1.getQuestionnaireQuestion().getQuestionSeqNumber().compareTo(getRootAnswer(argAnswer).getQuestionnaireQuestion().getQuestionSeqNumber());
+            if (retVal == 0) {
+                // ans1 is the root question of argAnswer
+                retVal = -1;
+            }
         } else if (ans1.getQuestionnaireQuestion().getParentQuestionNumber() != 0
                 && argAnswer.getQuestionnaireQuestion().getParentQuestionNumber() == 0) {
+            // argAnswer is the root question
             retVal = getRootAnswer(ans1).getQuestionnaireQuestion().getQuestionSeqNumber().compareTo(argAnswer.getQuestionnaireQuestion().getQuestionSeqNumber());
+            if (retVal == 0) {
+                // argAnswer is the root question of ans1
+                retVal = 1;
+            }
         } else if (ans1.getQuestionnaireQuestion().getParentQuestionNumber() != 0
                 && argAnswer.getQuestionnaireQuestion().getParentQuestionNumber() != 0) {
+            // both are not root question
             if (ObjectUtils.equals(getRootAnswer(ans1).getQuestionNumber(), getRootAnswer(argAnswer).getQuestionNumber())) {
+                // both has the same root question
                 retVal = compareAtSameDepth(ans1, argAnswer);
             } else {
                 retVal = getRootAnswer(ans1).getQuestionnaireQuestion().getQuestionSeqNumber().compareTo(getRootAnswer(argAnswer).getQuestionnaireQuestion().getQuestionSeqNumber());
             }
         } else {
+            // set up log now to see why it comes to here
+            LOG.info("no comparison matched "+ans1.getQuestionnaireQuestionsIdFk()+"-"+argAnswer.getQuestionnaireQuestionsIdFk());
             retVal = 0;
         }
-//       if (ans1.getQuestionnaireQuestionsIdFk() == 5893 || ans1.getQuestionnaireQuestionsIdFk() == 5883
-//               || argAnswer.getQuestionnaireQuestionsIdFk() == 5893 || argAnswer.getQuestionnaireQuestionsIdFk() == 5883) {
-//           System.out.println("sort print "+ans1.getQuestionnaireQuestionsIdFk()+"-"+argAnswer.getQuestionnaireQuestionsIdFk()+"--"+
-//                   ans1.getQuestionNumber()+"-"+ argAnswer.getQuestionnaireQuestion().getParentQuestionNumber()+"---"+
-//                   argAnswer.getQuestionNumber()+"-"+argAnswer.getQuestion().getQuestion()+" ret val ="+retVal);
-//       }
         return retVal;
     }
+    
+    /*
+     * get the root answer for the selected answer
+     */
     private Answer getRootAnswer(Answer argAnswer) {
 
         Answer thisAnswer = argAnswer;
@@ -79,19 +94,20 @@ public class AnswerComparator implements Comparator<Answer>  {
         return thisAnswer;
     }
     
+    /*
+     * compare question answer if they neither is the root question, but they have the same
+     * root question; ie, they are from the same question tree.
+     * 
+     */
     private int compareAtSameDepth(Answer thisAnswer, Answer argAnswer) {
-//        if (thisAnswer.getQuestionnaireQuestionsIdFk() == 5893 || thisAnswer.getQuestionnaireQuestionsIdFk() == 5883
-//                || argAnswer.getQuestionnaireQuestionsIdFk() == 5893 || argAnswer.getQuestionnaireQuestionsIdFk() == 5883) {
-//            System.out.println("sort print compareAtSameDepth " + thisAnswer.getQuestionnaireQuestionsIdFk() + "-"
-//                    + argAnswer.getQuestionnaireQuestionsIdFk() + "--" + thisAnswer.getQuestionNumber() + "-"
-//                    + argAnswer.getQuestionnaireQuestion().getParentQuestionNumber() + "---" + argAnswer.getQuestionNumber());
-//        }
         int retVal = 0;
 
         List<Answer> ancestors1 = getAncestors(thisAnswer);
         List<Answer> ancestors2 = getAncestors(argAnswer);
+        // comparing by starting from root.  Whenever, it shows that they are in different branch
+        // then use the split nodes sequence number to decide the order.
         if (ancestors1.size() <= ancestors2.size()) {
-            retVal = 1;
+            retVal = -1;
             for (int i = 0; i < ancestors1.size(); i++) {
 
                 if (!ObjectUtils.equals(ancestors1.get(i).getQuestionNumber(), ancestors2.get(i).getQuestionNumber())) {
@@ -105,7 +121,7 @@ public class AnswerComparator implements Comparator<Answer>  {
         }
         else {
             for (int i = 0; i < ancestors2.size(); i++) {
-                retVal = -1;
+                retVal = 1;
 
                 if (!ObjectUtils.equals(ancestors2.get(i).getQuestionNumber(), ancestors1.get(i).getQuestionNumber())) {
                     retVal = ancestors1.get(i).getQuestionnaireQuestion().getQuestionSeqNumber().compareTo(
@@ -118,26 +134,11 @@ public class AnswerComparator implements Comparator<Answer>  {
 
         }
         return retVal;
-        // int depth = 1;
-        // while (getAncestor(thisAnswer,depth).getQuestionNumber().equals(getAncestor(argAnswer,depth).getQuestionNumber())) {
-        // depth++;
-        // }
-        // return
-        // getAncestor(thisAnswer,depth).getQuestionnaireQuestion().getQuestionSeqNumber().compareTo(getAncestor(argAnswer,depth).getQuestionnaireQuestion().getQuestionSeqNumber());
-    }
-    private Answer getAncestor(Answer argAnswer, int depth) {
-        List<Answer> answers = new ArrayList<Answer>();
-        answers.add(argAnswer);
-        Answer thisAnswer = argAnswer;
-        int internalDepth = depth;
-        while (thisAnswer.getQuestionnaireQuestion().getParentQuestionNumber() > 0) {
-            thisAnswer = thisAnswer.getParentAnswer().get(0);
-            answers.add(thisAnswer);
-            ++internalDepth;
-        }
-        return answers.get(answers.size() - (internalDepth));
     }
 
+    /*
+     * get all the ancestors of this qnswer's question.  then reverse to list from root to leaf question.
+     */
     private List<Answer> getAncestors(Answer argAnswer) {
         List<Answer> answers = new ArrayList<Answer>();
         answers.add(argAnswer);
