ae8510575ed7fb5c8209b0e320c5474b271d5c2d
Kristina Taylor <kbtaylor@uw.edu>
2011-05-18 19:20:11 +0000

    KCINFR-285: Release 3.1 Sprint 4 Scripts

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/KR-RELEASE-3_1_SP4-Upgrade-MYSQL.sql
@@ -0,0 +1,3 @@
+\. MYSQL/DML/KR_DML_BS1_KRNS_PARM_T.sql
+commit;
+exit
---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/KR-RELEASE-3_1_SP4-Upgrade-ORACLE.sql
@@ -0,0 +1,6 @@
+set define off
+set sqlblanklines on
+spool KR-RELEASE-3_1_SP4-Upgrade-ORACLE-Install.log
+@ORACLE/DML/KR_DML_BS1_KRNS_PARM_T.sql
+commit;
+exit
