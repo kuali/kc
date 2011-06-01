c4c90017b120a88cd52a9ed8c45740f6dc26e036
Kristina Taylor <kbtaylor@uw.edu>
2011-06-01 19:54:08 +0000

    KCINFR-299: Release 3.1 Sprint 4 Scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/KR-RELEASE-3_1_SP4-Upgrade-ORACLE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/KR-RELEASE-3_1_SP4-Upgrade-ORACLE.sql
@@ -1,6 +1,8 @@
 set define off
 set sqlblanklines on
 spool KR-RELEASE-3_1_SP4-Upgrade-ORACLE-Install.log
+@ORACLE/DML/KR_DML_BS1_KRIM_PERM_T.sql
 @ORACLE/DML/KR_DML_BS1_KRNS_PARM_T.sql
+@ORACLE/DML/KR_DML_BS2_KRIM_ROLE_PERM_T.sql
 commit;
 exit
---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/ORACLE/DML/KR_DML_BS1_KRIM_PERM_T.sql
@@ -0,0 +1,2 @@
+INSERT INTO KRIM_PERM_T (PERM_ID,PERM_TMPL_ID,NMSPC_CD,NM,DESC_TXT,ACTV_IND,OBJ_ID,VER_NBR)
+  VALUES (KRIM_PERM_ID_BS_S.NEXTVAL,(SELECT PERM_TMPL_ID FROM KRIM_PERM_TMPL_T WHERE NM = 'Full Unmask Field'),'KC-SYS','View Active Special Review Types','Can view all active special review types','Y',SYS_GUID(),1);
