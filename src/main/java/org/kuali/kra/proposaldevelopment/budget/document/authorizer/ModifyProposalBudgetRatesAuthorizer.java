9f3b8b802e16760e5dbff2c996691a59ba0f30c2
Ge Zhang <g1zhang@ucsd.edu>
2011-06-15 07:26:25 +0000

    KRACOEUS-4729

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/budget/document/authorizer/ModifyProposalBudgetRatesAuthorizer.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.budget.document.authorizer;
+
+import org.kuali.kra.budget.document.BudgetDocument;
+import org.kuali.kra.budget.document.authorization.BudgetTask;
+import org.kuali.kra.budget.document.authorizer.BudgetAuthorizer;
+import org.kuali.kra.budget.versions.BudgetDocumentVersion;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.infrastructure.PermissionConstants;
+import org.kuali.kra.kew.KraDocumentRejectionService;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
+
+public class ModifyProposalBudgetRatesAuthorizer extends BudgetAuthorizer {
+    
+    /**
+     * @see org.kuali.kra.proposaldevelopment.document.authorizer.ProposalAuthorizer#isAuthorized(org.kuali.rice.kns.bo.user.UniversalUser, org.kuali.kra.proposaldevelopment.web.struts.form.ProposalDevelopmentForm)
+     */
+    public boolean isAuthorized(String userId, BudgetTask task) {
+        System.out.println("**********checking MDPR permission*********************");
+        KraDocumentRejectionService documentRejectionService = KraServiceLocator.getService(KraDocumentRejectionService.class);
+        BudgetDocument budgetDocument = task.getBudgetDocument();
+        ProposalDevelopmentDocument doc = (ProposalDevelopmentDocument)budgetDocument.getParentDocument();
+        boolean rejectedDocument = documentRejectionService.isDocumentOnInitialNode(doc.getDocumentNumber());
+        
+      System.out.println("++++++++++++++++++++ "+ !kraWorkflowService.isInWorkflow(doc));
+      System.out.println("++++++++++++++++++++ "+ rejectedDocument);
+      System.out.println("++++++++++++++++++++ "+ hasParentPermission(userId, doc, PermissionConstants.MODIFY_PROPOSAL_RATES));
+      System.out.println("++++++++++++++++++++ "+ !doc.getDevelopmentProposal().getSubmitFlag());
+        
+        return (!kraWorkflowService.isInWorkflow(doc) || rejectedDocument) &&
+                hasParentPermission(userId, doc, PermissionConstants.MODIFY_PROPOSAL_RATES) &&!doc.getDevelopmentProposal().getSubmitFlag(); 
+    }
+}
