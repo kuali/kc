5519d8a37061ada3e7bb50ffa72f434135b64081
Gayathri Athreya <hsggwk@gmail.com>
2011-06-16 22:09:27 +0000

    KCINFR-279: Added to Release 3.1 sprint 4 scripts.

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/ORACLE/DML/KR_DML_BS1_KRNS_PARM_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/ORACLE/DML/KR_DML_BS1_KRNS_PARM_T.sql
@@ -13,3 +13,7 @@ update krns_parm_t set txt='1,9' where parm_nm='scope.sync.PAYMENTS_AND_INVOICES
 
 INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID)
   VALUES ('KC', 'KC-GEN', 'A', 'ALLOW_PROPOSAL_PERSON_TO_OVERRIDE_KC_PERSON_EXTENDED_ATTRIBUTES', 1, 'CONFG', 'Y', 'If Y then the proposal person citizenship type is used, if N then the kc extended attributes citizenship type is used', 'A', SYS_GUID());
+
+delete from krns_parm_t where PARM_NM = 'AWARD_CREATE_ACCOUNT';
+
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID) VALUES ('KC', 'KC-AWARD', 'Document', 'FIN_SYSTEM_INTEGRATION_ON', 1, 'CONFG', 'OFF', 'Parameter to set the financial system integration feature ON or OFF.', 'A', SYS_GUID());
