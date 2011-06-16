f8e52176bc138b1453262d6034edecbb9077e52c
Kristina Taylor <kbtaylor@uw.edu>
2011-06-16 20:03:59 +0000

    KRACOEUS-4829: Release 3.1 Sprint 4 Scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/MYSQL/DML/KR_DML_BS1_KRNS_PARM_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/MYSQL/DML/KR_DML_BS1_KRNS_PARM_T.sql
@@ -11,4 +11,7 @@ update KRNS_PARM_T set TXT='6' where PARM_NM='budgetPersonDefaultAppointmentType
 
 update krns_parm_t set txt='1,9' where parm_nm='scope.sync.PAYMENTS_AND_INVOICES_TAB.AwardComment.commentTypeCode';
 
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID)
+  VALUES ('KC', 'KC-GEN', 'A', 'ALLOW_PROPOSAL_PERSON_TO_OVERRIDE_KC_PERSON_EXTENDED_ATTRIBUTES', 1, 'CONFG', 'Y', 'If Y then the proposal person citizenship type is used, if N then the kc extended attributes citizenship type is used', 'A', UUID());
+  
 COMMIT;
