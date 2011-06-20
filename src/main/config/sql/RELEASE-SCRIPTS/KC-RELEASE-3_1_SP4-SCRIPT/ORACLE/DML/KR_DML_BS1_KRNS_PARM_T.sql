4521bf316388413feefbe8f655443cf02bb33abc
Gayathri Athreya <hsggwk@gmail.com>
2011-06-20 20:47:12 +0000

    KCINFR-162, KRACOEUS-4119: Adding to Release 3.1 sprint 4 scripts.

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/ORACLE/DML/KR_DML_BS1_KRNS_PARM_T.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP4-SCRIPT/ORACLE/DML/KR_DML_BS1_KRNS_PARM_T.sql
@@ -50,3 +50,13 @@ INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, PARM
   
 INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID) 
   VALUES ('KC', 'KC-GEN', 'All', 'SPONSOR_HIERARCHY_FOR_PRINTING', 1, 'CONFG', 'Printing', 'The name of the Sponsor Hierarchy used for Sponsor Form selection.', 'A', SYS_GUID());
+
+insert into krns_parm_t (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID) values ('KC', 'KC-AWARD', 'Document', 'CFDA_BATCH_JOB_CRON_EXPRESSION', 1, 'CONFG', '0 0 6 * * ?', 'Parameter to set the cron expression for the CFDA batch job', 'A', SYS_GUID());
+
+insert into krns_parm_t (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID) values ('KC', 'KC-AWARD', 'Document', 'CFDA_GOV_URL', 1, 'CONFG', 'ftp://ftp.cfda.gov/programs', 'Url of the CFDA FTP site for the CFDA batch job', 'A', SYS_GUID());
+
+insert into krns_parm_t (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID) values ('KC', 'KC-AWARD', 'Document', 'CFDA_BATCH_NOTIFICATION_RECIPIENT', 1, 'CONFG', '', 'Principal name of the person that should receive notifications when the CFDA batch job runs', 'A', SYS_GUID());
+
+insert into krns_parm_t (APPL_NMSPC_CD, NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD, OBJ_ID) values('KC', 'KC-AWARD', 'Document', 'CFDA_BATCH_JOB_CRON_START_TIME', 1, 'CONFG', '', 'Start time of the CFDA job', 'A', SYS_GUID());  
+  
+insert into KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_TYP_CD,TXT,PARM_DESC_TXT,CONS_CD,OBJ_ID) values ('KC','KC-GEN','A','POST_DOCTORAL_COSTELEMENT','CONFG','400390','PostDoctoral CostElement', 'A',sys_guid());
