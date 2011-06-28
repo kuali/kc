50d8ba154cf770829065255e48c3542f1cdf5692
Kristina Taylor <kbtaylor@uw.edu>
2011-06-28 21:09:04 +0000

    Moving references to quickstart from bootstrap to demo

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP2-SCRIPT/MYSQL/DML/KR_DML_BS6_KRIM_ROLE_MBR_T.sql
+++
@@ -1,9 +0,0 @@
--- assign Time and Money modifier role to quickstart
-INSERT INTO KRIM_ROLE_MBR_ID_BS_S VALUES (NULL);
-insert into krim_role_mbr_t (role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values ((SELECT MAX(ID) FROM KRIM_ROLE_MBR_ID_BS_S), 1, uuid(), (select ROLE_ID from KRIM_ROLE_T where ROLE_NM='Time And Money Modifier'), '10000000001', 'P');
-
--- assign Award budget viewer role to awdbudviewer
-INSERT INTO KRIM_ROLE_MBR_ID_BS_S VALUES (NULL);
-insert into krim_role_mbr_t (role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values ((SELECT MAX(ID) FROM KRIM_ROLE_MBR_ID_BS_S), 1, uuid(), (select ROLE_ID from KRIM_ROLE_T where ROLE_NM='Award Budget Viewer'), '10000000063', 'P');
-
-COMMIT;
--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_SP2-SCRIPT/ORACLE/DML/KR_DML_BS6_KRIM_ROLE_MBR_T.sql
+++
@@ -1,7 +0,0 @@
--- assign Time and Money modifier role to quickstart
-insert into krim_role_mbr_t (role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values (KRIM_ROLE_MBR_ID_BS_S.nextval, 1, sys_guid(), (select ROLE_ID from KRIM_ROLE_T where ROLE_NM='Time And Money Modifier'), '10000000001', 'P');
-
--- assign Award budget viewer role to awdbudviewer
-insert into krim_role_mbr_t (role_mbr_id, ver_nbr, obj_id, role_id, mbr_id, mbr_typ_cd) values (KRIM_ROLE_MBR_ID_BS_S.nextval, 1, sys_guid(), (select ROLE_ID from KRIM_ROLE_T where ROLE_NM='Award Budget Viewer'), '10000000063', 'P');
-
-COMMIT;
