abd208cf0439c4e43e867a5471be9dc4173b8f43
Kristina Taylor <kbtaylor@uw.edu>
2011-06-29 20:04:00 +0000

    Moving reference to quickstart to demo data and removing extra group membership reference

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0_1-SCRIPT/MYSQL/DML/DML_BS1_KRIM_GRP_MBR_T.sql
+++
@@ -1,6 +0,0 @@
-insert into krim_grp_mbr_id_s values (null);
-
-insert into krim_grp_mbr_t (GRP_MBR_ID, GRP_ID, mbr_id, MBR_TYP_CD, VER_NBR, OBJ_ID)
-select max(id), (select grp_id from krim_grp_t where nmspc_cd = 'KC-WKFLW' and grp_nm = 'KcAdmin'),'admin','P',1,uuid() from krim_grp_mbr_id_s;
-
-commit;
