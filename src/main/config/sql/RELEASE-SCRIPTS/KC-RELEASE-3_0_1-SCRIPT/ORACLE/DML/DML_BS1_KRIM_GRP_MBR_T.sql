3dd2d27e8a0679d3f94658fbd856b9878d1c4cae
Kristina Taylor <kbtaylor@uw.edu>
2011-06-29 20:04:00 +0000

    Moving reference to quickstart to demo data and removing extra group membership reference

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0_1-SCRIPT/ORACLE/DML/DML_BS1_KRIM_GRP_MBR_T.sql
+++
@@ -1,4 +0,0 @@
-insert into krim_grp_mbr_t (GRP_MBR_ID, GRP_ID, mbr_id, MBR_TYP_CD, VER_NBR, OBJ_ID)
-values (krim_grp_mbr_id_s.nextval, (select grp_id from krim_grp_t where nmspc_cd = 'KC-WKFLW' and grp_nm = 'KcAdmin'),'admin','P',1,sys_guid());
-
-commit;
