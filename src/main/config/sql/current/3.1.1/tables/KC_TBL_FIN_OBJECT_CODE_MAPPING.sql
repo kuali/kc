8ea83df26d24748f855898fa7639cbd5b4f4299e
Gayathri Athreya <hsggwk@gmail.com>
2011-07-20 18:13:27 +0000

    KCINFR-39: Committing the sql necessary for the Budget Adjustment client.

---
+++ src/main/config/sql/current/3.1.1/tables/KC_TBL_FIN_OBJECT_CODE_MAPPING.sql
@@ -0,0 +1,9 @@
+CREATE TABLE FIN_OBJECT_CODE_MAPPING 
+  ( 
+    FIN_OBJECT_CODE varchar2(10) not null,
+    RATE_TYPE_CODE varchar2(3) not null,
+    OBJ_ID VARCHAR2(36),
+    UNIT_NUMBER VARCHAR2(8) not null,
+    RATE_CLASS_CODE VARCHAR2(3) not null,
+    ACTIVITY_TYPE_CODE VARCHAR2(3)
+  );
