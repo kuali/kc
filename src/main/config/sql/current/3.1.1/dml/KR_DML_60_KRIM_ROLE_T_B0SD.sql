ebc6ea63bd6b93e4f6ba2a6c6a20e3c3b162482e
Lin-Long Shyu <lshyukuali@gmail.com>
2011-07-27 15:04:07 +0000

    remove and revise later

--- src/main/config/sql/current/3.1.1/dml/KR_DML_60_KRIM_ROLE_T_B0SD.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO KRIM_ROLE_T (ACTV_IND,DESC_TXT,KIM_TYP_ID,LAST_UPDT_DT,NMSPC_CD,OBJ_ID,ROLE_ID,ROLE_NM,VER_NBR)
-  VALUES ('Y','Funding Source Monitor',(select KIM_TYP_ID from KRIM_TYP_T where nm = 'UnitHierarchy'),sysdate,'KC-UNT',sys_guid(),KRIM_ROLE_ID_S.nextval,'Funding Source Monitor',1);
--- src/main/config/sql/current/3.1.1/dml/KR_DML_62_KRIM_ROLE_MBR_T_B0SD.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO KRIM_ROLE_MBR_T (LAST_UPDT_DT,MBR_ID,MBR_TYP_CD,OBJ_ID,ROLE_ID,ROLE_MBR_ID,VER_NBR)
-  VALUES (SYSDATE,
-(select role_id from KRIM_ROLE_T where NMSPC_CD = 'KC-UNT' AND role_nm = 'IRB Administrator'),'R',SYS_GUID(),
-(select role_id from KRIM_ROLE_T where NMSPC_CD = 'KC-UNT' AND role_nm = 'Funding Source Monitor'),KRIM_ROLE_MBR_ID_S.nextval,1);
-
-INSERT INTO KRIM_ROLE_MBR_T (LAST_UPDT_DT,MBR_ID,MBR_TYP_CD,OBJ_ID,ROLE_ID,ROLE_MBR_ID,VER_NBR)
-  VALUES (SYSDATE,
-(select role_id from KRIM_ROLE_T where NMSPC_CD = 'KC-ADM' AND role_nm = 'OSP Administrator'),'R',SYS_GUID(),
-(select role_id from KRIM_ROLE_T where NMSPC_CD = 'KC-UNT' AND role_nm = 'Funding Source Monitor'),KRIM_ROLE_MBR_ID_S.nextval,1);
