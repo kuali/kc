153f9b74691db60e151a66dbeea96d082eae017b
Sweta Sheth <ssheth@vivantech.com>
2011-08-16 09:06:29 +0000

    KRACOEUS-4304: Added code to make 'Current & Pending Support' link working.

---
+++ src/main/java/org/kuali/kra/proposaldevelopment/web/struts/form/CurrentOrPendingReportForm.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.proposaldevelopment.web.struts.form;
+
+import org.kuali.kra.common.web.struts.form.ReportHelperBean;
+import org.kuali.kra.common.web.struts.form.ReportHelperBeanContainer;
+import org.kuali.kra.web.struts.form.BudgetVersionFormBase;
+import org.kuali.rice.kns.web.struts.form.KualiForm;
+import org.kuali.rice.kns.web.struts.form.KualiTransactionalDocumentFormBase;
+
+public class CurrentOrPendingReportForm extends KualiForm implements ReportHelperBeanContainer{
+    
+    private ReportHelperBean reportHelperBean;
+    
+    public CurrentOrPendingReportForm(){
+        super();
+        reportHelperBean = new ReportHelperBean();
+    }
+    
+    public void setReportHelperBean(ReportHelperBean reportHelperBean) {
+        this.reportHelperBean = reportHelperBean;
+    }
+
+    public ReportHelperBean getReportHelperBean() {
+         return reportHelperBean;
+    }
+
+}
--- src/main/webapp/WEB-INF/tags/rice-portal/channel/researcherPersonnel.tag
+++ src/main/webapp/WEB-INF/tags/rice-portal/channel/researcherPersonnel.tag
@@ -19,7 +19,7 @@
 <div class="body">
   <ul class="chan">
     <li>Degree Information</li>
-    <li>Current &amp; Pending Support</li>
+   <li><portal:portalLink displayTitle="false" title="Current & Pending Support" url="currentOrPendingReport.do">Current &amp; Pending Support</portal:portalLink></li>
     <li>Bio-sketches</li>
     <li>All my Training</li>
   </ul>
