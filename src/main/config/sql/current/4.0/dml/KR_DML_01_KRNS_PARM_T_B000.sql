54e36c64b361606e432c993f93a92bbd175f609d
Lin-Long Shyu <lshyukuali@gmail.com>
2011-08-21 12:48:31 +0000

    kccoi-2

---
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRNS_PARM_T_B000.sql
@@ -0,0 +1,18 @@
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
+    VALUES ('KUALI','KC-COIDISCLOSURE','HELP','D','coiDisclosureHelp','Coi Disclosure Page Help','default.htm?turl=Documents/coidisclosure.htm','A',SYS_GUID(),1)
+/
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
+    VALUES ('KUALI','KC-COIDISCLOSURE','HELP','D','disclosureActionsHelp','Coi Disclosure Actions Page Help','default.htm?turl=Documents/coidisclosureactions.htm','A',SYS_GUID(),1)
+/
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
+    VALUES ('KUALI','KC-COIDISCLOSURE','HELP','D','disclosureCommitteeHelp','Coi Committee Page Help','default.htm?turl=Documents/coicommittee.htm','A',SYS_GUID(),1)
+/
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
+    VALUES ('KUALI','KC-COIDISCLOSURE','HELP','D','coiFinancialInterestsHelp','Coi Financial Interests Page Help','default.htm?turl=Documents/coifinancialinterests.htm','A',SYS_GUID(),1)
+/
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
+    VALUES ('KUALI','KC-COIDISCLOSURE','HELP','D','coiNotesAndAttachmentsHelp','Coi Note and Attachment Page Help','default.htm?turl=Documents/coinoteandattachment.htm','A',SYS_GUID(),1)
+/
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
+    VALUES ('KUALI','KC-COIDISCLOSURE','HELP','D','coiCertificationHelp','Coi CertificationPage Help','default.htm?turl=Documents/coicertification.htm','A',SYS_GUID(),1)
+/
