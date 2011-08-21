ef4ec47987f7a9b958799c9d59a60c1f01aa5394
Lin-Long Shyu <lshyukuali@gmail.com>
2011-08-21 13:19:18 +0000

    kccoi-2 : initial commit

---
+++ src/main/java/org/kuali/kra/coi/CoiAction.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.coi;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.TaskName;
+import org.kuali.kra.irb.ProtocolForm;
+import org.kuali.kra.irb.auth.ProtocolTask;
+import org.kuali.kra.web.struts.action.AuditActionHelper;
+import org.kuali.kra.web.struts.action.KraTransactionalDocumentActionBase;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.KNSConstants;
+
+public abstract class CoiAction extends KraTransactionalDocumentActionBase {
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        final ActionForward forward = super.execute(mapping, form, request, response);
+        if(GlobalVariables.getAuditErrorMap().isEmpty()) {
+            new AuditActionHelper().auditConditionally((CoiDisclosureForm) form);
+        }
+        
+        return forward;
+    }
+    
+    public ActionForward disclosure(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+        return mapping.findForward("disclosure");
+    }
+    public ActionForward committee(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+        return mapping.findForward("committee");
+    }
+    public ActionForward financialInterests(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+        return mapping.findForward("financialInterests");
+    }
+    public ActionForward certification(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+        return mapping.findForward("certification");
+    }
+    public ActionForward noteAndAttachment(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+        return mapping.findForward("noteAndAttachment");
+    }
+    public ActionForward disclosureActions(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+        return mapping.findForward("disclosureActions");
+    }
+
+    @Override
+    public final ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+        throws Exception {
+        
+        ActionForward actionForward = mapping.findForward(Constants.MAPPING_BASIC);
+        // TODO : fill in detail here
+        //actionForward = super.save(mapping, form, request, response);
+        
+        return actionForward;
+    }
+
+}
---
+++ src/main/java/org/kuali/kra/coi/CoiDisclosureDocumentRule.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.coi;
+
+import org.kuali.kra.rule.BusinessRuleInterface;
+import org.kuali.kra.rule.event.KraDocumentEventBaseExtension;
+import org.kuali.kra.rules.ResearchDocumentRuleBase;
+import org.kuali.rice.kns.document.Document;
+
+/**
+ * 
+ * This class is the rule class for coidisclosuredocument
+ */
+public class CoiDisclosureDocumentRule extends ResearchDocumentRuleBase  implements BusinessRuleInterface {
+
+    
+    @Override
+    protected boolean processCustomRouteDocumentBusinessRules(Document document) {
+        boolean retval = true;
+        retval &= super.processCustomRouteDocumentBusinessRules(document);
+        
+        return retval;
+    }
+
+    @Override
+    protected boolean processCustomSaveDocumentBusinessRules(Document document) {
+        if (!(document instanceof CoiDisclosureDocument)) {
+            return false;
+        }
+
+        // TODO : uncomment following when we start to implement disclosure
+//        MessageMap errorMap = GlobalVariables.getMessageMap();
+//        errorMap.addToErrorPath(DOCUMENT_ERROR_PATH);
+//        getDictionaryValidationService().validateDocumentAndUpdatableReferencesRecursively(
+//               document, getMaxDictionaryValidationDepth(),
+//               VALIDATION_REQUIRED, CHOMP_LAST_LETTER_S_FROM_COLLECTION_NAME);
+//        errorMap.removeFromErrorPath(DOCUMENT_ERROR_PATH);
+
+        boolean valid = true;
+        CoiDisclosureDocument coiDisclosureDocument = (CoiDisclosureDocument) document;
+        return valid;
+    }
+
+    /**
+     * 
+     * @see org.kuali.kra.rule.BusinessRuleInterface#processRules(org.kuali.kra.rule.event.KraDocumentEventBaseExtension)
+     */
+    public boolean processRules(KraDocumentEventBaseExtension event) {
+        boolean retVal = false;
+        retVal = event.getRule().processRules(event);
+        return retVal;
+    }
+
+}
