57cbea48b44b1965c394a27301940fa50b2851f3
Lin-Long Shyu <lshyukuali@gmail.com>
2011-08-21 13:19:18 +0000

    kccoi-2 : initial commit

---
+++ src/main/java/org/kuali/kra/coi/CoiDisclosureDocument.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.coi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kra.document.ResearchDocumentBase;
+import org.kuali.rice.kns.document.Copyable;
+import org.kuali.rice.kns.document.SessionDocument;
+
+/**
+ * 
+ * This class is Coi disclosure document class
+ */
+public class CoiDisclosureDocument extends ResearchDocumentBase implements Copyable, SessionDocument { 
+    
+    public static final String DOCUMENT_TYPE_CODE = "COI";
+    private List<CoiDisclosure> coiDisclosureList;
+    
+    /**
+     * Constructs a CoiDisclosureDocument object.
+     */
+    public CoiDisclosureDocument() { 
+        super();
+        coiDisclosureList = new ArrayList<CoiDisclosure>();
+        CoiDisclosure newCoiDisclosure = new CoiDisclosure();
+        newCoiDisclosure.setCoiDisclosureDocument(this);
+        coiDisclosureList.add(newCoiDisclosure);
+
+    }
+
+    public List<CoiDisclosure> getCoiDisclosureList() {
+        return coiDisclosureList;
+    }
+
+    public void setCoiDisclosureList(List<CoiDisclosure> coiDisclosureList) {
+        this.coiDisclosureList = coiDisclosureList;
+    }
+
+    @Override
+    public String getDocumentTypeCode() {
+        // TODO Auto-generated method stub
+        return DOCUMENT_TYPE_CODE;
+    } 
+    
+    /**
+     * 
+     * This method is a convenience method for facilitating a 1:1 relationship between CoiDisclosureDocument 
+     * and CoiDisclosure to the outside world - aka a single CoiDisclosure field associated with CoiDocument
+     * @return
+     */
+    public CoiDisclosure getCoiDisclosure() {
+        if (coiDisclosureList.size() == 0) return null;
+        return coiDisclosureList.get(0);
+    }
+
+    /**
+     * 
+     * This method is a convenience method for facilitating a 1:1 relationship between CoiDocument 
+     * and CoiDisclosure to the outside world - aka a single CoiDisclosure field associated with CoiDocument
+     * @param coiDisclosure
+     */
+    public void setCoiDisclosure(CoiDisclosure coiDisclosure) {
+        coiDisclosureList.set(0, coiDisclosure);
+    }
+
+ 
+}
