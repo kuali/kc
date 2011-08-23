be6151f7bdb2035e441fc3415dabd3e8067bceb5
Allen Akers <allen.akers@colostate.edu>
2011-08-23 16:13:35 +0000

    Updating syntax for MySQL

--- src/main/config/sql/current_mysql/3.1.1/dml/KC_DML_01_KCIRB-1449_B000.sql
+++ src/main/config/sql/current_mysql/3.1.1/dml/KC_DML_01_KCIRB-1449_B000.sql
@@ -1,24 +1,20 @@
-DELIMITER /
-
 INSERT INTO PROTOCOL_ACTION_TYPE (PROTOCOL_ACTION_TYPE_CODE, DESCRIPTION, TRIGGER_SUBMISSION, TRIGGER_CORRESPONDENCE, FINAL_ACTION_FOR_BATCH_CORRESP, UPDATE_TIMESTAMP,UPDATE_USER, obj_id) 
-VALUES ('904', 'Funding Source', 'N', 'N', 'N', now(), 'admin', uuid())
+VALUES ('904', 'Funding Source', 'N', 'N', 'N', str_to_date('09/21/2010', '%m/%d/%Y'), 'admin', UUID())
 /
 
-INSERT INTO SEQ_NOTIFICATION_TYPE_ID VALUES (null)
+insert into SEQ_NOTIFICATION_TYPE_ID values(NULL)
 /
 insert into notification_type
 (NOTIFICATION_TYPE_ID,MODULE_CODE,ACTION_CODE,DESCRIPTION,SUBJECT,MESSAGE,PROMPT_USER,SEND_NOTIFICATION,SYSTEM_GENERATED,UPDATE_USER,    
  UPDATE_TIMESTAMP,VER_NBR,OBJ_ID ) values(                             
- (SELECT MAX(ID) FROM SEQ_NOTIFICATION_TYPE_ID),7,904,'Funding Source','{MESSAGE_SUBJECT}','{MESSAGE_BODY}',  'N','Y','Y','admin',now(),1,
-uuid())
+ last_insert_id(),7,904,'Funding Source','{MESSAGE_SUBJECT}','{MESSAGE_BODY}',  'N','Y','Y','admin',NOW(),1,
+UUID())
 /
 
-INSERT INTO SEQ_NOTIFICATION_TYPE_ID VALUES (null)
+insert into SEQ_NOTIFICATION_TYPE_ID values(NULL)
 /
 insert into NOTIFICATION_TYPE_RECIPIENT(
 NOTIFICATION_TYPE_RECIPIENT_ID,NOTIFICATION_TYPE_ID,ROLE_name,ROLE_QUALIFIER,TO_OR_CC,UPDATE_USER,UPDATE_TIMESTAMP,VER_NBR,OBJ_ID)
-values((SELECT MAX(ID) FROM SEQ_NOTIFICATION_TYPE_ID),(select NOTIFICATION_TYPE_ID from notification_type where DESCRIPTION = 'Funding Source'),'KC-UNT:Funding Source Monitor','unitNumber', 'T','admin',now(),1, 
-uuid())
+values(last_insert_id(),(select NOTIFICATION_TYPE_ID from notification_type where DESCRIPTION = 'Funding Source'),'KC-UNT:Funding Source Monitor','unitNumber', 'T','admin',NOW(),1, 
+UUID())
 /
-
-
