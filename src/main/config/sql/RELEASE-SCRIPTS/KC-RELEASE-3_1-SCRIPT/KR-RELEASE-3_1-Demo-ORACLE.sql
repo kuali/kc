fb3d26de135a95ef959f20813d079ed7150d3f6c
Allen Akers <allen.akers@colostate.edu>
2011-08-24 17:01:47 +0000

    Updating release scripts

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1-SCRIPT/KR-RELEASE-3_1-Demo-ORACLE.sql
@@ -0,0 +1,22 @@
+set define off
+set sqlblanklines on
+spool KR-RELEASE-3_1-Demo-ORACLE-Install.log
+@../../current/3.1/dml/KR_DML_01_KREN_PRODCR_T_000D.sql
+@../../current/3.1/dml/KR_DML_01_KRIM_ENTITY_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_01_KRNS_CAMPUS_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_02_KRIM_ENTITY_ENT_TYP_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_02_KRIM_PRNCPL_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_03_KREW_DOC_HDR_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_03_KRIM_ENTITY_ADDR_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_03_KRIM_ENTITY_AFLTN_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_03_KRIM_ENTITY_EMAIL_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_03_KRIM_ENTITY_EMP_INFO_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_03_KRIM_ENTITY_PHONE_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_04_KREW_ACTN_TKN_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_04_KREW_DOC_HDR_CNTNT_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_04_KREW_RTE_BRCH_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_04_KRIM_GRP_MBR_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_04_KRIM_ROLE_MBR_T_0TSD.sql
+@../../current/3.1/dml/KR_DML_05_KREW_ACTN_RQST_T_0TSD.sql
+commit;
+exit
--- src/main/config/sql/RELEASE-SCRIPTS/KC_Install_Demo.sh
+++ src/main/config/sql/RELEASE-SCRIPTS/KC_Install_Demo.sh
@@ -90,7 +90,7 @@ fi
 
 case "${dbtype}" in
 	"ORACLE")
-        cd KC-RELEASE-3_1-DEMO
+        cd KC-RELEASE-3_1-SCRIPT
 		sqlplus "${un}"/"${pw}${DBSvrNm}" < KRC-RELEASE-3_1-Demo-ORACLE.sql		
 		sqlplus "${un}"/"${pw}${DBSvrNm}" < KC-RELEASE-3_1-Demo-ORACLE.sql
 		sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < KR-RELEASE-3_1-Demo-ORACLE.sql
@@ -99,7 +99,7 @@ case "${dbtype}" in
         cd .. ;;
 		
 	"MYSQL")
-        cd KC-RELEASE-3_1-DEMO
+        cd KC-RELEASE-3_1-SCRIPT
 		mysql -u ${un} -p${pw} -D ${DBSvrNm} -s -f < KRC-RELEASE-3_1-Demo-MYSQL.sql > KRC-RELEASE-3_1-Demo-MYSQL-Install.log 2>&1
 		mysql -u ${un} -p${pw} -D ${DBSvrNm} -s -f < KC-RELEASE-3_1-Demo-MYSQL.sql > KC-RELEASE-3_1-Demo-MYSQL-Install.log 2>&1		
         mysql -u ${Riceun} -p${Ricepw} -D ${RiceDBSvrNm} -s -f < KR-RELEASE-3_1-Demo-MYSQL.sql > KR-RELEASE-3_1-Demo-MYSQL-Install.log 2>&1
