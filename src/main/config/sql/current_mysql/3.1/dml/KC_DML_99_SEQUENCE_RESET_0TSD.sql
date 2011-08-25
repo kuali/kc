3ed8602d7661826679767f49a832d2cc96f99d56
Allen Akers <allen.akers@colostate.edu>
2011-08-25 18:06:27 +0000

    Adding MySQL versions of demo scripts

---
+++ src/main/config/sql/current_mysql/3.1/dml/KC_DML_99_SEQUENCE_RESET_0TSD.sql
@@ -0,0 +1,36 @@
+DELIMITER /
+declare
+    MaxTblVal INTEGER;
+begin
+    select max(QUESTION_ID) + 1 into MaxTblVal from QUESTION;
+    execute immediate 'DROP SEQUENCE SEQ_QUESTION_ID';
+    execute immediate 'CREATE SEQUENCE SEQ_QUESTION_ID START WITH ' || MaxTblVal || 
+        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
+end;
+/
+declare
+    MaxTblVal INTEGER;
+begin
+    select max(QUESTIONNAIRE_ID) + 1 into MaxTblVal from QUESTIONNAIRE;
+    execute immediate 'DROP SEQUENCE SEQ_QUESTIONNAIRE_ID';
+    execute immediate 'CREATE SEQUENCE SEQ_QUESTIONNAIRE_ID START WITH ' || MaxTblVal || 
+        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
+end;
+/
+declare
+    MaxTblVal INTEGER;
+begin
+    select max(ID) + 1 into MaxTblVal from 
+        ((select max(QUESTION_REF_ID) as ID from QUESTION)
+        union (select max(QUESTION_EXPLANATION_ID) as ID from QUESTION_EXPLANATION)
+        union (select max(QUESTIONNAIRE_ANSWER_HEADER_ID) as ID from QUESTIONNAIRE_ANSWER_HEADER)
+        union (select max(QUESTIONNAIRE_ANSWER_ID) as ID from QUESTIONNAIRE_ANSWER)
+        union (select max(QUESTIONNAIRE_REF_ID) as ID from QUESTIONNAIRE)
+        union (select max(QUESTIONNAIRE_QUESTIONS_ID) as ID from QUESTIONNAIRE_QUESTIONS)
+        union (select max(QUESTIONNAIRE_USAGE_ID) as ID from QUESTIONNAIRE_USAGE));
+    execute immediate 'DROP SEQUENCE SEQ_QUESTIONNAIRE_REF_ID';
+    execute immediate 'CREATE SEQUENCE SEQ_QUESTIONNAIRE_REF_ID START WITH ' || MaxTblVal || 
+        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
+end;
+/
+DELIMITER ;
