d921dd51d008b91f02ad231f9985742dced00017
Allen Akers <allen.akers@colostate.edu>
2011-08-26 16:54:11 +0000

    Updating MySQL release scripts

--- src/main/config/sql/current_mysql/3.1/dml/KC_DML_99_SEQUENCE_RESET_0TSD.sql
+++ src/main/config/sql/current_mysql/3.1/dml/KC_DML_99_SEQUENCE_RESET_0TSD.sql
@@ -1,36 +1,52 @@
-DELIMITER /
-declare
-    MaxTblVal INTEGER;
-begin
-    select max(QUESTION_ID) + 1 into MaxTblVal from QUESTION;
-    execute immediate 'DROP SEQUENCE SEQ_QUESTION_ID';
-    execute immediate 'CREATE SEQUENCE SEQ_QUESTION_ID START WITH ' || MaxTblVal || 
-        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
-end;
-/
-declare
-    MaxTblVal INTEGER;
-begin
-    select max(QUESTIONNAIRE_ID) + 1 into MaxTblVal from QUESTIONNAIRE;
-    execute immediate 'DROP SEQUENCE SEQ_QUESTIONNAIRE_ID';
-    execute immediate 'CREATE SEQUENCE SEQ_QUESTIONNAIRE_ID START WITH ' || MaxTblVal || 
-        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
-end;
-/
-declare
-    MaxTblVal INTEGER;
-begin
-    select max(ID) + 1 into MaxTblVal from 
-        ((select max(QUESTION_REF_ID) as ID from QUESTION)
-        union (select max(QUESTION_EXPLANATION_ID) as ID from QUESTION_EXPLANATION)
-        union (select max(QUESTIONNAIRE_ANSWER_HEADER_ID) as ID from QUESTIONNAIRE_ANSWER_HEADER)
-        union (select max(QUESTIONNAIRE_ANSWER_ID) as ID from QUESTIONNAIRE_ANSWER)
-        union (select max(QUESTIONNAIRE_REF_ID) as ID from QUESTIONNAIRE)
-        union (select max(QUESTIONNAIRE_QUESTIONS_ID) as ID from QUESTIONNAIRE_QUESTIONS)
-        union (select max(QUESTIONNAIRE_USAGE_ID) as ID from QUESTIONNAIRE_USAGE));
-    execute immediate 'DROP SEQUENCE SEQ_QUESTIONNAIRE_REF_ID';
-    execute immediate 'CREATE SEQUENCE SEQ_QUESTIONNAIRE_REF_ID START WITH ' || MaxTblVal || 
-        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
-end;
-/
 DELIMITER ;
+
+drop procedure if exists p;
+
+delimiter //
+create procedure p ()
+begin
+  declare l_new_seq INT;
+
+  select ifnull(max(cast(QUESTION_ID as SIGNED)),'1') + 1 into l_new_seq from QUESTION;
+  set @create_seq := 'CREATE TABLE SEQ_QUESTION_ID (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
+  prepare create_seq_stmt from @create_seq;
+  execute create_seq_stmt;
+  deallocate prepare create_seq_stmt;
+  set @alter_seq := concat('ALTER TABLE SEQ_QUESTION_ID auto_increment = ', l_new_seq);
+  prepare alter_seq_stmt from @alter_seq;
+  execute alter_seq_stmt;
+  deallocate prepare alter_seq_stmt;
+  
+  select ifnull(max(cast(QUESTIONNAIRE_ID as SIGNED)),'1') + 1 into l_new_seq from QUESTIONNAIRE;
+  set @create_seq := 'CREATE TABLE SEQ_QUESTIONNAIRE_ID (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
+  prepare create_seq_stmt from @create_seq;
+  execute create_seq_stmt;
+  deallocate prepare create_seq_stmt;
+  set @alter_seq := concat('ALTER TABLE SEQ_QUESTIONNAIRE_ID auto_increment = ', l_new_seq);
+  prepare alter_seq_stmt from @alter_seq;
+  execute alter_seq_stmt;
+  deallocate prepare alter_seq_stmt;
+
+  select ifnull(max(cast(ID as SIGNED)),'1') + 1 into l_new_seq from 
+    (select max(cast(QUESTION_REF_ID as SIGNED)) as ID from QUESTION 
+      union select max(cast(QUESTION_EXPLANATION_ID as SIGNED)) as ID from QUESTION_EXPLANATION
+      union select max(cast(QUESTIONNAIRE_ANSWER_HEADER_ID as SIGNED)) as ID from QUESTIONNAIRE_ANSWER_HEADER
+      union select max(cast(QUESTIONNAIRE_ANSWER_ID as SIGNED)) as ID from QUESTIONNAIRE_ANSWER
+      union select max(cast(QUESTIONNAIRE_REF_ID as SIGNED)) as ID from QUESTIONNAIRE
+      union select max(cast(QUESTIONNAIRE_QUESTIONS_ID as SIGNED)) as ID from QUESTIONNAIRE_QUESTIONS
+      union select max(cast(QUESTIONNAIRE_USAGE_ID as SIGNED)) as ID from QUESTIONNAIRE_USAGE) as t1;
+  set @create_seq := 'CREATE TABLE SEQ_QUESTIONNAIRE_REF_ID (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
+  prepare create_seq_stmt from @create_seq;
+  execute create_seq_stmt;
+  deallocate prepare create_seq_stmt;
+  set @alter_seq := concat('ALTER TABLE SEQ_QUESTIONNAIRE_REF_ID auto_increment = ', l_new_seq);
+  prepare alter_seq_stmt from @alter_seq;
+  execute alter_seq_stmt;
+  deallocate prepare alter_seq_stmt;
+  
+end //
+delimiter ;
+
+call p ();
+
+drop procedure if exists p;
--- src/main/config/sql/current_mysql/3.1/dml/KR_DML_99_SEQUENCE_RESET_00S0.sql
+++ src/main/config/sql/current_mysql/3.1/dml/KR_DML_99_SEQUENCE_RESET_00S0.sql
@@ -1,14 +1,27 @@
-DELIMITER /
-declare
-    MaxTblVal INTEGER;
+delimiter ;
+drop procedure if exists p;
+
+delimiter //
+create procedure p ()
 begin
-    select max(ID) + 1 into MaxTblVal from 
-        ((select max(DOC_HDR_ID) as ID from KREW_DOC_HDR_T)
-        union (select max(DOC_TYP_ID) as ID from KREW_DOC_TYP_T) 
-        union (select max(APP_DOC_STAT_TRAN_ID) as ID from KREW_APP_DOC_STAT_TRAN_T));
-    execute immediate 'DROP SEQUENCE KREW_DOC_HDR_S';
-    execute immediate 'CREATE SEQUENCE KREW_DOC_HDR_S START WITH ' || MaxTblVal || 
-        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
-end;
-/
-DELIMITER ;
+  declare l_new_seq INT;
+
+  select ifnull(max(cast(ID as SIGNED)),'1') + 1 into l_new_seq from 
+    (select max(cast(DOC_HDR_ID as SIGNED)) as ID from KREW_DOC_HDR_T
+      union select max(cast(DOC_TYP_ID as SIGNED)) as ID from KREW_DOC_TYP_T
+      union select max(cast(APP_DOC_STAT_TRAN_ID as SIGNED)) as ID from KREW_APP_DOC_STAT_TRAN_T) as t1;
+  set @create_seq := 'CREATE TABLE KREW_DOC_HDR_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
+  prepare create_seq_stmt from @create_seq;
+  execute create_seq_stmt;
+  deallocate prepare create_seq_stmt;
+  set @alter_seq := concat('ALTER TABLE KREW_DOC_HDR_S auto_increment = ', l_new_seq);
+  prepare alter_seq_stmt from @alter_seq;
+  execute alter_seq_stmt;
+  deallocate prepare alter_seq_stmt;
+  
+end //
+delimiter ;
+
+call p ();
+
+drop procedure if exists p;
