2968158d55d5934dce580e2e7f0638f532e05a72
Kristina Taylor <kbtaylor@uw.edu>
2011-08-30 14:27:47 +0000

    Correcting script for impex

--- src/main/config/sql/current/3.1.1/dml/KR_DML_01_KRACOEUS-4357_B000.sql
+++ src/main/config/sql/current/3.1.1/dml/KR_DML_01_KRACOEUS-4357_B000.sql
@@ -6,5 +6,5 @@ INSERT INTO KRNS_PARM_T(APPL_NMSPC_CD,NMSPC_CD,PARM_DTL_TYP_CD,PARM_NM,VER_NBR,P
 	VALUES('KC', 'KC-AWARD', 'Document', 'closeoutReportTypeInvoice', 1, 'CONFG', '6', 'This system parameter maps the CloseoutReportType Invoice(closeoutReoprtTypeCode=6) with ReportClass Payment/Invoice(reportClassCode=6). If this system parameter is changed - the corresponding values in CloseoutReportType and ReportClass tables should be updated as well.', 'A', SYS_GUID())
 /
 
-UPDATE CLOSEOUT_REPORT_TYPE SET DESCRIPTION = 'Financial' where CLOSEOUT_REPORT_CODE = 1
+UPDATE CLOSEOUT_REPORT_TYPE SET DESCRIPTION = 'Financial' where CLOSEOUT_REPORT_CODE = '1'
 /
