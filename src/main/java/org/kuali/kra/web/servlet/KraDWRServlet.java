26a3445178323482d25273f86971e1172ec1e1a9
Chuck Tharp <grover.tharp@mail.wvu.edu>
2011-09-07 13:32:00 +0000

    KCIRB-1505: Assign to Committee shows "undefined" instead of list of dates.

--- src/main/java/org/kuali/kra/web/servlet/KraDWRServlet.java
+++ src/main/java/org/kuali/kra/web/servlet/KraDWRServlet.java
@@ -15,21 +15,31 @@
  */
 package org.kuali.kra.web.servlet;
 
+import java.io.IOException;
 import java.io.InputStream;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.rice.core.util.ClassLoaderUtils;
+import org.kuali.rice.kns.UserSession;
+import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.util.KNSConstants;
 import org.kuali.rice.kns.util.spring.NamedOrderedListBean;
 import org.kuali.rice.kns.web.servlet.KualiDWRServlet;
-import org.kuali.kra.infrastructure.Constants;
 import org.springframework.core.io.DefaultResourceLoader;
 
 import uk.ltd.getahead.dwr.Configuration;
 
 public class KraDWRServlet extends KualiDWRServlet {
+
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = 1L;
 
     @Override
     public void configure(ServletConfig servletConfig, Configuration configuration) throws ServletException {
@@ -54,6 +64,18 @@ public class KraDWRServlet extends KualiDWRServlet {
                 }
             }
         }
-
     }
+    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        UserSession userSession = null;
+        boolean sessionexist = req.getSession(false) != null && req.getSession(false).getAttribute(KNSConstants.USER_SESSION_KEY) != null;
+        if (sessionexist && GlobalVariables.getUserSession() == null) {
+            userSession = (UserSession) req.getSession().getAttribute(KNSConstants.USER_SESSION_KEY);
+            GlobalVariables.setUserSession(userSession);
+        }
+        super.doPost(req, resp);
+    }
+    
 }
