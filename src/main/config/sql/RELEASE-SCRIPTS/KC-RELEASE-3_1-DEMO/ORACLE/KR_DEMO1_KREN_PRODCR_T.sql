6148e99297161700795db552228d70cbaf0f27ad
Allen Akers <allen.akers@colostate.edu>
2011-09-08 20:47:36 +0000

    Updating release scripts and documentation

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1-DEMO/ORACLE/KR_DEMO1_KREN_PRODCR_T.sql
+++
@@ -1,8 +0,0 @@
--- Notification Producer Demo Data
-
-INSERT INTO KREN_PRODCR_T (PRODCR_ID,NM,DESC_TXT,CNTCT_INFO,VER_NBR) 
-  VALUES (2,'University Library System','This producer represents messages sent from the University Library system.','kuali-ken-testing@cornell.edu',1)
-/
-INSERT INTO KREN_PRODCR_T (PRODCR_ID,NM,DESC_TXT,CNTCT_INFO,VER_NBR) 
-  VALUES (3,'University Events Office','This producer represents messages sent from the University Events system.','kuali-ken-testing@cornell.edu',1)
-/
