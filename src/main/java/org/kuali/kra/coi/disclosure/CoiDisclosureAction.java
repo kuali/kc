82aa9cb0891c2cc98c978e336fa677ce75351b4b
Lin-Long Shyu <lshyukuali@gmail.com>
2011-09-09 12:17:04 +0000

    kccoi-1 : initial commit for coi disclosure reporter/units

--- src/main/java/org/kuali/kra/coi/disclosure/CoiDisclosureAction.java
+++ src/main/java/org/kuali/kra/coi/disclosure/CoiDisclosureAction.java
@@ -15,8 +15,87 @@
  */
 package org.kuali.kra.coi.disclosure;
 
+import java.sql.Date;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
 import org.kuali.kra.coi.CoiAction;
+import org.kuali.kra.coi.CoiDisclosure;
+import org.kuali.kra.coi.CoiDisclosureDocument;
+import org.kuali.kra.coi.CoiDisclosureForm;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.rule.event.KraDocumentEventBaseExtension;
+import org.kuali.rice.kns.service.SequenceAccessorService;
+import org.kuali.rice.kns.util.DateUtils;
 
 public class CoiDisclosureAction extends CoiAction {
+
+   
+    public ActionForward addDisclosurePersonUnit(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        CoiDisclosureForm coiDisclosureForm = (CoiDisclosureForm) form;
+        DisclosureHelper disclosureHelper = coiDisclosureForm.getDisclosureHelper();
+        if (checkRule(new AddDisclosurePersonUnitEvent("disclosureHelper", disclosureHelper.getNewDisclosurePersonUnit(),
+                ((CoiDisclosureDocument)coiDisclosureForm.getDocument()).getCoiDisclosure().getDisclosureReporter().getDisclosurePersonUnits()))) {
+            getCoiDisclosureService().addDisclosurePersonUnit(
+                    ((CoiDisclosureDocument)coiDisclosureForm.getDocument()).getCoiDisclosure().getDisclosureReporter(),
+                    disclosureHelper.getNewDisclosurePersonUnit());
+            disclosureHelper.setNewDisclosurePersonUnit(new DisclosurePersonUnit());
+        }
+        return mapping.findForward(Constants.MAPPING_BASIC);
+    }
+
+    public ActionForward deleteDisclosurePersonUnit(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        CoiDisclosureForm coiDisclosureForm = (CoiDisclosureForm) form;
+        DisclosureHelper disclosureHelper = coiDisclosureForm.getDisclosureHelper();
+        getCoiDisclosureService().deleteDisclosurePersonUnit(((CoiDisclosureDocument)coiDisclosureForm.getDocument()).getCoiDisclosure().getDisclosureReporter(), disclosureHelper.getDeletedUnits(), getSelectedLine(request));
+        return mapping.findForward(Constants.MAPPING_BASIC);
+    }
+
+    private CoiDisclosureService getCoiDisclosureService() {
+        return KraServiceLocator.getService(CoiDisclosureService.class);
+    }
+
+    private boolean checkRule(KraDocumentEventBaseExtension event) {
+        return event.getRule().processRules(event);
+    }
+
+    @Override
+    public final ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+        throws Exception {
+        
+        ActionForward actionForward = mapping.findForward(Constants.MAPPING_BASIC);
+        CoiDisclosureForm coiDisclosureForm = (CoiDisclosureForm) form;
+        CoiDisclosure coiDisclosure = ((CoiDisclosureDocument)coiDisclosureForm.getDocument()).getCoiDisclosure();
+        if (coiDisclosure.getCoiDisclosureId() == null) {
+            coiDisclosure.initRequiredFields();            
+        } else {
+            getCoiDisclosureService().resetLeadUnit(coiDisclosure.getDisclosureReporter());
+        }
+        actionForward = super.save(mapping, form, request, response);
+        
+        return actionForward;
+    }
+
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        // TODO Auto-generated method stub
+        ActionForward actionForward = super.execute(mapping, form, request, response);
+        CoiDisclosureForm coiDisclosureForm = (CoiDisclosureForm) form;
+        ((CoiDisclosureDocument)coiDisclosureForm.getDocument()).getCoiDisclosure().initSelectedUnit();
+        return actionForward;
+
+    }
+
 
 }
---
+++ src/main/java/org/kuali/kra/coi/disclosure/CoiDisclosureServiceImpl.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.coi.disclosure;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.kuali.kra.bo.KcPerson;
+import org.kuali.kra.coi.personfinancialentity.FinancialEntityReporter;
+import org.kuali.kra.coi.personfinancialentity.FinancialEntityUnit;
+import org.kuali.kra.service.KcPersonService;
+import org.kuali.rice.kns.service.BusinessObjectService;
+
+public class CoiDisclosureServiceImpl implements CoiDisclosureService {
+
+    private BusinessObjectService businessObjectService;
+    private KcPersonService kcPersonService;
+
+    @SuppressWarnings("rawtypes")
+    public DisclosurePerson getDisclosureReporter(String personId, Long coiDisclosureId) {
+        List<DisclosurePerson> reporters = new ArrayList<DisclosurePerson>();
+        if (coiDisclosureId != null) {
+            Map fieldValues = new HashMap();
+            fieldValues.put("personId", personId);
+            fieldValues.put("personRoleId", "COIR");
+            fieldValues.put("coiDisclosureId", coiDisclosureId);
+
+            reporters = (List<DisclosurePerson>) businessObjectService.findMatching(DisclosurePerson.class, fieldValues);
+        }
+        if (reporters.isEmpty()) {
+            DisclosurePerson reporter = new DisclosurePerson();
+            reporter.setDisclosurePersonUnits(new ArrayList<DisclosurePersonUnit>());
+            reporter.setPersonId(personId);
+            reporter.setPersonRoleId("COIR");
+            DisclosurePersonUnit leadUnit = createLeadUnit(personId);
+            if (leadUnit != null) {
+                reporter.getDisclosurePersonUnits().add(leadUnit);
+            }
+            return reporter;
+        }
+        else {
+            int i = 0;
+            for (DisclosurePersonUnit disclosurePersonUnit : reporters.get(0).getDisclosurePersonUnits()) {
+                if (disclosurePersonUnit.isLeadUnitFlag()) {
+                    reporters.get(0).setSelectedUnit(i);
+                    break;
+                }
+                i++;
+            }
+        }
+        return reporters.get(0);
+    }
+
+    public void addDisclosurePersonUnit(DisclosurePerson disclosurePerson , DisclosurePersonUnit newDisclosurePersonUnit) {
+        
+        List<DisclosurePersonUnit> disclosurePersonUnits = disclosurePerson.getDisclosurePersonUnits();
+        if (newDisclosurePersonUnit.isLeadUnitFlag()) {
+            resetLeadUnit(disclosurePersonUnits);
+            disclosurePerson.setSelectedUnit(disclosurePersonUnits.size());
+        }
+        disclosurePersonUnits.add(newDisclosurePersonUnit);
+    }
+
+    public void deleteDisclosurePersonUnit(DisclosurePerson disclosurePerson,List<DisclosurePersonUnit> deletedUnits, int unitIndex) {
+        
+        List<DisclosurePersonUnit> disclosurePersonUnits = disclosurePerson.getDisclosurePersonUnits();
+        DisclosurePersonUnit deletedUnit = disclosurePersonUnits.get(unitIndex);
+        if (deletedUnit.getDisclosurePersonUnitsId() != null) {
+            deletedUnits.add(deletedUnit);
+        }
+        disclosurePersonUnits.remove(unitIndex);
+        if (deletedUnit.isLeadUnitFlag() && !disclosurePersonUnits.isEmpty()) {
+            disclosurePersonUnits.get(0).setLeadUnitFlag(true);
+            disclosurePerson.setSelectedUnit(0);
+        }
+    }
+
+    public void resetLeadUnit(DisclosurePerson disclosurePerson) {
+        List<DisclosurePersonUnit> disclosurePersonUnits = disclosurePerson.getDisclosurePersonUnits();
+        if (CollectionUtils.isNotEmpty(disclosurePersonUnits)) {
+            resetLeadUnit(disclosurePersonUnits);
+            disclosurePersonUnits.get(disclosurePerson.getSelectedUnit()).setLeadUnitFlag(true);
+        }
+    }
+
+
+    private void resetLeadUnit(List<DisclosurePersonUnit> disclosurePersonUnits) {
+        for (DisclosurePersonUnit  disclosurePersonUnit : disclosurePersonUnits) {
+            disclosurePersonUnit.setLeadUnitFlag(false);
+        }
+        
+    }
+
+    private DisclosurePersonUnit createLeadUnit(String personId) {
+
+        DisclosurePersonUnit leadUnit = null;
+        KcPerson kcPerson = kcPersonService.getKcPersonByPersonId(personId);
+        if (kcPerson != null && kcPerson.getUnit() != null) {
+            leadUnit = new DisclosurePersonUnit();
+            leadUnit.setLeadUnitFlag(true);
+            leadUnit.setUnitNumber(kcPerson.getUnit().getUnitNumber());
+            leadUnit.setUnitName(kcPerson.getUnit().getUnitName());
+            leadUnit.setPersonId(personId);
+        }
+        return leadUnit;
+    }
+
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+
+    public void setKcPersonService(KcPersonService kcPersonService) {
+        this.kcPersonService = kcPersonService;
+    }
+
+}
