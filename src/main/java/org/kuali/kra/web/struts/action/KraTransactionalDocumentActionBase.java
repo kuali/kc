f313e67382018f97004046bba0b0cdd427b98a53
Douglas Pace <dpace@kuali.org>
2011-09-15 22:17:34 +0000

    KRACOEUS-4970: Adding links to attachments and refactoring the 'getValidHeaderString' method into the shared parent.

--- src/main/java/org/kuali/kra/web/struts/action/KraTransactionalDocumentActionBase.java
+++ src/main/java/org/kuali/kra/web/struts/action/KraTransactionalDocumentActionBase.java
@@ -35,6 +35,8 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
+import javax.mail.internet.HeaderTokenizer;
+import javax.mail.internet.MimeUtility;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
@@ -904,4 +906,25 @@ public class KraTransactionalDocumentActionBase extends KualiTransactionalDocume
             return mapping.findForward(Constants.MAPPING_BASIC);
         }
     }
+    
+    /**
+     * Quotes a string that follows RFC 822 and is valid to include in an http header.
+     * 
+     * <p>
+     * This really should be a part of {@link org.kuali.rice.kns.util.WebUtils WebUtils}.
+     * <p>
+     * 
+     * For example: without this method, file names with spaces will not show up to the client correctly.
+     * 
+     * <p>
+     * This method is not doing a Base64 encode just a quoted printable character otherwise we would have
+     * to set the encoding type on the header.
+     * <p>
+     * 
+     * @param s the original string
+     * @return the modified header string
+     */
+    protected static String getValidHeaderString(String s) {
+        return MimeUtility.quote(s, HeaderTokenizer.MIME);
+    }    
 }
