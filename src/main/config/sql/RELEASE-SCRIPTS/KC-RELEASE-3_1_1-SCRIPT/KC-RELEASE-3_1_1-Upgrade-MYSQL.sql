927efa02f877854178afd973a04a713b106b150d
Allen Akers <allen.akers@colostate.edu>
2011-09-20 15:42:56 +0000

    Adding 3.1.1 release script folder

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_1-SCRIPT/KC-RELEASE-3_1_1-Upgrade-MYSQL.sql
@@ -0,0 +1,26 @@
+\. ./../../current_mysql/3.1.1/sequences/KC_SEQ_SEQ_CUSTOM_ATTRIBUTE.sql
+\. ./../../current_mysql/3.1.1/sequences/KC_SEQ_SEQ_FIN_OBJECT_CODE_MAPPING_ID.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_AWARD.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_AWARD_IDC_RATE.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_BUDGET.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_BUDGET_DETAILS.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_COST_ELEMENT.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_EPS_PROP_PERSON_EXT.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_FIN_IDC_TYPE_CODE.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_FIN_OBJECT_CODE_MAPPING.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_PERSON_EXT_T.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_RATE_CLASS.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_REVIEWER_ATTACHMENTS.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_STATE_CODE.sql
+\. ./../../current_mysql/3.1.1/tables/KC_TBL_USER_TABLES_TEMP.sql
+\. ./../../current_mysql/3.1.1/dml/KC_DML_01_KCINFR-347_B000.sql
+\. ./../../current_mysql/3.1.1/dml/KC_DML_01_KCINFR-348_B000.sql
+\. ./../../current_mysql/3.1.1/dml/KC_DML_01_KCINFR-350_B000.sql
+\. ./../../current_mysql/3.1.1/dml/KC_DML_01_KCIRB-1448_B000.sql
+\. ./../../current_mysql/3.1.1/dml/KC_DML_01_KCIRB-1449_B000.sql
+\. ./../../current_mysql/3.1.1/dml/KC_DML_01_KRACOEUS-4357_B000.sql
+\. ./../../current_mysql/3.1.1/constraints/KC_FK_FIN_IDC_TYPE_CODE.sql
+\. ./../../current_mysql/3.1.1/constraints/KC_FK_FIN_OBJECT_CODE_MAPPING.sql
+\. ./../../current_mysql/3.1.1/constraints/KC_UQ_FIN_OBJECT_CODE_MAPPING.sql
+commit;
+exit
