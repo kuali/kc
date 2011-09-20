0241e41b31e919431f8cc07faa0091e6abda27be
Kristina Taylor <kbtaylor@uw.edu>
2011-09-20 19:31:16 +0000

    Script corrections

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCCOI-2_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCCOI-2_B000.sql
@@ -1,3 +1,7 @@
+INSERT INTO KRNS_NMSPC_T (ACTV_IND,NM,NMSPC_CD,APPL_NMSPC_CD,OBJ_ID,VER_NBR)
+  VALUES ('Y','KC Coi Disclosure','KC-COIDISCLOSURE','KC',sys_guid(),1)
+/
+
 INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
     VALUES ('KC','KC-COIDISCLOSURE','HELP','Document','coiDisclosureHelp','Coi Disclosure Page Help','default.htm?turl=Documents/coidisclosure.htm','A',SYS_GUID(),1)
 /
