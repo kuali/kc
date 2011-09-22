531aec8959c7621a759052b169e9362ed0270069
Lin-Long Shyu <lshyukuali@gmail.com>
2011-09-22 16:02:47 +0000

    kccoi-4

--- src/test/java/org/kuali/kra/coi/personfinancialentity/FinancialEntityServiceTest.java
+++ src/test/java/org/kuali/kra/coi/personfinancialentity/FinancialEntityServiceTest.java
@@ -16,7 +16,6 @@
 package org.kuali.kra.coi.personfinancialentity;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,6 +30,7 @@ import org.junit.Test;
 import org.kuali.kra.bo.KcPerson;
 import org.kuali.kra.bo.Unit;
 import org.kuali.kra.service.KcPersonService;
+import org.kuali.kra.service.VersioningService;
 import org.kuali.rice.kns.service.BusinessObjectService;
 
 public class FinancialEntityServiceTest {
@@ -38,6 +38,21 @@ public class FinancialEntityServiceTest {
     private static final String  UNIT_NUMBER = "000001";
     private static final String  UNIT_NAME = "University";
     private static final String  ENTITY_NUMBER = "1";
+    private static final String  GROUP_NAME_1 = "Group 1";
+    private static final String  GROUP_NAME_2 = "Group 2";
+    private static final String  COLUMN_NAME_11 = "COLUMN_NAME_11";
+    private static final String  COLUMN_LABEL_11 = "Column 11";
+    private static final String  COLUMN_NAME_12 = "COLUMN_NAME_12";
+    private static final String  COLUMN_LABEL_12 = "Column 12";
+    private static final String  COLUMN_NAME_21 = "COLUMN_NAME_21";
+    private static final String  COLUMN_LABEL_21 = "Column 21";
+    private static final String  GUITYPE_DROPDOWN = "DROPDOWN";
+    private static final String  GUITYPE_CHECKBOX = "CHECKBOX";
+    private static final String  LOOKUP_ARGUIMENT= "Argument 1";
+    private static final Integer  SORT_ID_1= 1;
+    private static final Integer  SORT_ID_2= 2;
+    private static final String  COMMENT_1= "Comment 1";
+
     Mockery context = new JUnit4Mockery();
 
     @Before
@@ -168,7 +183,288 @@ public class FinancialEntityServiceTest {
 
     }
      
-  
+    @Test
+    public void testGetFinancialEntityRelationshipTypes() throws Exception {
+        FinancialEntityServiceImpl financialEntityService = new FinancialEntityServiceImpl();
+        final BusinessObjectService businessObjectService = context.mock(BusinessObjectService.class);
+        
+        final List<FinIntEntityRelType> relationTypes = new ArrayList<FinIntEntityRelType>();
+        relationTypes.add(getFinIntEntityRelType("1", 1, true));
+        relationTypes.add(getFinIntEntityRelType("2", 2, true));
+        context.checking(new Expectations() {
+            {
+                Map fieldValues = new HashMap();
+                fieldValues.put("active", "Y");
+                one(businessObjectService).findMatchingOrderBy(FinIntEntityRelType.class, fieldValues, "sortId", true);
+                will(returnValue(relationTypes));
+
+
+            }
+        });
+        financialEntityService.setBusinessObjectService(businessObjectService);
+        
+        List<FinIntEntityRelType> returnRelationTypes = financialEntityService.getFinancialEntityRelationshipTypes();
+        Assert.assertEquals(returnRelationTypes.size(), 2);
+        Assert.assertEquals(returnRelationTypes.get(0).getRelationshipTypeCode(), "1");
+        Assert.assertEquals(returnRelationTypes.get(0).getSortId(), new Integer(1));
+        Assert.assertEquals(returnRelationTypes.get(1).getRelationshipTypeCode(), "2");
+        Assert.assertEquals(returnRelationTypes.get(1).getSortId(), new Integer(2));
+
+    }
+     
+    @Test
+    public void testGetFinancialEntityDataMatrix() throws Exception {
+        FinancialEntityServiceImpl financialEntityService = new FinancialEntityServiceImpl();
+        final BusinessObjectService businessObjectService = context.mock(BusinessObjectService.class);
+        
+        final List<FinIntEntityRelType> relationTypes = new ArrayList<FinIntEntityRelType>();
+        relationTypes.add(getFinIntEntityRelType("1", 1, true));
+        relationTypes.add(getFinIntEntityRelType("2", 2, true));
+        context.checking(new Expectations() {
+            {
+                Map fieldValues = new HashMap();
+                fieldValues.put("active", "Y");
+                one(businessObjectService).findMatchingOrderBy(FinIntEntityRelType.class, fieldValues, "sortId", true);
+                will(returnValue(relationTypes));
+
+
+            }
+        });
+        
+        final List<FinEntitiesDataGroup> dataGroups = getDataGroups();
+        context.checking(new Expectations() {
+            {
+                one(businessObjectService).findAll(FinEntitiesDataGroup.class);
+                will(returnValue(dataGroups));
+
+
+            }
+        });
+
+        financialEntityService.setBusinessObjectService(businessObjectService);
+        
+        List<FinEntityDataMatrixBean> dataMatrixs = financialEntityService.getFinancialEntityDataMatrix();
+        Assert.assertEquals(dataMatrixs.size(), 3);
+        Assert.assertEquals(dataMatrixs.get(0).getColumnName(), COLUMN_NAME_11);
+        Assert.assertEquals(dataMatrixs.get(1).getColumnName(), COLUMN_NAME_12);
+        Assert.assertEquals(dataMatrixs.get(2).getColumnName(), COLUMN_NAME_21);
+        Assert.assertEquals(dataMatrixs.get(0).getColumnLabel(), COLUMN_LABEL_11);
+        Assert.assertEquals(dataMatrixs.get(1).getColumnLabel(), COLUMN_LABEL_12);
+        Assert.assertEquals(dataMatrixs.get(2).getColumnLabel(), COLUMN_LABEL_21);
+        Assert.assertEquals(dataMatrixs.get(0).getGuiType(), GUITYPE_DROPDOWN);
+        Assert.assertEquals(dataMatrixs.get(1).getGuiType(), GUITYPE_CHECKBOX);
+        Assert.assertEquals(dataMatrixs.get(2).getGuiType(), GUITYPE_CHECKBOX);
+        Assert.assertEquals(dataMatrixs.get(0).getLookupArgument(), LOOKUP_ARGUIMENT);
+        Assert.assertEquals(dataMatrixs.get(1).getLookupArgument(), "");
+        Assert.assertEquals(dataMatrixs.get(2).getLookupArgument(), "");
+
+ 
+    }
+     
+    @Test
+    public void testGetFinDisclosureDetails() throws Exception {
+        FinancialEntityServiceImpl financialEntityService = new FinancialEntityServiceImpl();
+        final BusinessObjectService businessObjectService = context.mock(BusinessObjectService.class);
+        
+        final List<FinIntEntityRelType> relationTypes = new ArrayList<FinIntEntityRelType>();
+        relationTypes.add(getFinIntEntityRelType("1", 1, true));
+        relationTypes.add(getFinIntEntityRelType("2", 2, true));
+        context.checking(new Expectations() {
+            {
+                Map fieldValues = new HashMap();
+                fieldValues.put("active", "Y");
+                one(businessObjectService).findMatchingOrderBy(FinIntEntityRelType.class, fieldValues, "sortId", true);
+                will(returnValue(relationTypes));
+
+
+            }
+        });
+        
+        final List<FinEntitiesDataGroup> dataGroups = getDataGroups();
+        context.checking(new Expectations() {
+            {
+                one(businessObjectService).findAll(FinEntitiesDataGroup.class);
+                will(returnValue(dataGroups));
+
+
+            }
+        });
+
+        financialEntityService.setBusinessObjectService(businessObjectService);
+        List<FinEntityDataMatrixBean> dataMatrixs = financialEntityService.getFinancialEntityDataMatrix();
+        dataMatrixs.get(0).getRelationshipTypeBeans().get(0).setStringValue("1");
+        dataMatrixs.get(1).getRelationshipTypeBeans().get(1).setBooleanValue(true);
+        dataMatrixs.get(2).setComments(COMMENT_1);
+
+        List<PersonFinIntDisclDet>  disclosureDetails = financialEntityService.getFinDisclosureDetails(dataMatrixs,"1",1);
+        Assert.assertEquals(disclosureDetails.size(), 4);
+        Assert.assertEquals(disclosureDetails.get(0).getColumnName(), COLUMN_NAME_11);
+        Assert.assertEquals(disclosureDetails.get(1).getColumnName(), COLUMN_NAME_12);
+        Assert.assertEquals(disclosureDetails.get(2).getColumnName(), COLUMN_NAME_21);
+        Assert.assertEquals(disclosureDetails.get(3).getColumnName(), COLUMN_NAME_21);
+        Assert.assertEquals(disclosureDetails.get(0).getColumnValue(), "1");
+        Assert.assertEquals(disclosureDetails.get(1).getColumnValue(), "1");
+        Assert.assertEquals(disclosureDetails.get(2).getComments(), COMMENT_1);
+        Assert.assertEquals(disclosureDetails.get(3).getComments(), COMMENT_1);
+
+ 
+    }
+     
+    @Test
+    public void testGetFinancialEntityDataMatrixForEdit() throws Exception {
+        FinancialEntityServiceImpl financialEntityService = new FinancialEntityServiceImpl();
+        final BusinessObjectService businessObjectService = context.mock(BusinessObjectService.class);
+        
+        final List<FinIntEntityRelType> relationTypes = new ArrayList<FinIntEntityRelType>();
+        relationTypes.add(getFinIntEntityRelType("1", 1, true));
+        relationTypes.add(getFinIntEntityRelType("2", 2, true));
+        context.checking(new Expectations() {
+            {
+                Map fieldValues = new HashMap();
+                fieldValues.put("active", "Y");
+                one(businessObjectService).findMatchingOrderBy(FinIntEntityRelType.class, fieldValues, "sortId", true);
+                will(returnValue(relationTypes));
+                one(businessObjectService).findMatchingOrderBy(FinIntEntityRelType.class, fieldValues, "sortId", true);
+                will(returnValue(relationTypes));
+
+
+            }
+        });
+        
+        final List<FinEntitiesDataGroup> dataGroups = getDataGroups();
+        context.checking(new Expectations() {
+            {
+                one(businessObjectService).findAll(FinEntitiesDataGroup.class);
+                will(returnValue(dataGroups));
+                one(businessObjectService).findAll(FinEntitiesDataGroup.class);
+                will(returnValue(dataGroups));
+
+
+            }
+        });
+
+        financialEntityService.setBusinessObjectService(businessObjectService);
+        List<FinEntityDataMatrixBean> dataMatrixs = financialEntityService.getFinancialEntityDataMatrix();
+        dataMatrixs.get(0).getRelationshipTypeBeans().get(0).setStringValue("1");
+        dataMatrixs.get(1).getRelationshipTypeBeans().get(1).setBooleanValue(true);
+        dataMatrixs.get(2).setComments(COMMENT_1);
+
+        List<PersonFinIntDisclDet>  disclosureDetails = financialEntityService.getFinDisclosureDetails(dataMatrixs,"1",1);
+        List<FinEntityDataMatrixBean> matrixBeans = financialEntityService.getFinancialEntityDataMatrixForEdit(disclosureDetails);
+        Assert.assertEquals(matrixBeans.size(), 3);
+ 
+    }
+
+    @Test
+   public void testVersionPersonFinintDisclosures() throws Exception {
+       FinancialEntityServiceImpl financialEntityService = new FinancialEntityServiceImpl();
+       final VersioningService versioningService = context.mock(VersioningService.class);
+       final BusinessObjectService businessObjectService = context.mock(BusinessObjectService.class);
+       final PersonFinIntDisclosure personFinIntDisclosure = new PersonFinIntDisclosure();
+       personFinIntDisclosure.setPersonId(PERSON_ID);
+       personFinIntDisclosure.setEntityNumber(ENTITY_NUMBER);
+       personFinIntDisclosure.setSequenceNumber(1);
+       personFinIntDisclosure.setStatusCode(2);
+       personFinIntDisclosure.setCurrentFlag(true);
+       personFinIntDisclosure.setPersonFinIntDisclosureId(1L);
+       final List<FinancialEntityContactInfo> contactInfos = new ArrayList<FinancialEntityContactInfo>();
+       FinancialEntityContactInfo contactInfo = new FinancialEntityContactInfo();
+       contactInfo.setPersonFinIntDisclosureId(1L);
+       contactInfo.setFinancialEntityContactInfoId(2L);
+       contactInfos.add(contactInfo);
+       personFinIntDisclosure.setFinEntityContactInfos(contactInfos);
+       context.checking(new Expectations() {
+           {
+               final PersonFinIntDisclosure newPersonFinIntDisclosure = getPersonFinancialEntityDiscl();     
+               one(versioningService).createNewVersion(personFinIntDisclosure);
+               will(returnValue(newPersonFinIntDisclosure));
+
+
+           }
+       });
+       context.checking(new Expectations() {
+           {
+               Map<String, Object> fieldValues = new HashMap<String, Object>();
+               fieldValues.put("personFinIntDisclosureId", 1L);
+               one(businessObjectService).findByPrimaryKey(PersonFinIntDisclosure.class, fieldValues);
+               will(returnValue(personFinIntDisclosure));
+               one(businessObjectService).save(personFinIntDisclosure);
+               
+
+
+           }
+       });
+
+       financialEntityService.setBusinessObjectService(businessObjectService);
+       financialEntityService.setVersioningService(versioningService);
+       PersonFinIntDisclosure newDisclosure = financialEntityService.versionPersonFinintDisclosure(personFinIntDisclosure, new ArrayList<FinEntityDataMatrixBean>());
+       Assert.assertEquals(newDisclosure.getSequenceNumber(), new Integer(2));
+       Assert.assertNull(newDisclosure.getFinEntityContactInfos().get(0).getPersonFinIntDisclosureId());
+       Assert.assertNull(newDisclosure.getFinEntityContactInfos().get(0).getFinancialEntityContactInfoId());
+       Assert.assertTrue(newDisclosure.isCurrentFlag());
+       Assert.assertFalse(personFinIntDisclosure.isCurrentFlag());
+   }
+    
+    private PersonFinIntDisclosure getPersonFinancialEntityDiscl() {
+        PersonFinIntDisclosure personFinIntDisclosure = new PersonFinIntDisclosure();
+        personFinIntDisclosure.setPersonId(PERSON_ID);
+        personFinIntDisclosure.setEntityNumber(ENTITY_NUMBER);
+        personFinIntDisclosure.setSequenceNumber(2);
+        personFinIntDisclosure.setStatusCode(2);
+        personFinIntDisclosure.setCurrentFlag(true);
+        final List<FinancialEntityContactInfo> contactInfos = new ArrayList<FinancialEntityContactInfo>();
+        FinancialEntityContactInfo contactInfo = new FinancialEntityContactInfo();
+        contactInfo.setPersonFinIntDisclosureId(1L);
+        contactInfo.setFinancialEntityContactInfoId(2L);
+        contactInfos.add(contactInfo);
+        personFinIntDisclosure.setFinEntityContactInfos(contactInfos);
+        return personFinIntDisclosure;
+    }
+    
+    private List<FinEntitiesDataGroup> getDataGroups() {
+        List<FinEntitiesDataGroup> dataGroups = new ArrayList<FinEntitiesDataGroup>();    
+        FinEntitiesDataGroup dataGroup1 = new FinEntitiesDataGroup();
+        dataGroup1.setDataGroupId(1);
+        dataGroup1.setDataGroupName(GROUP_NAME_1);
+        dataGroup1.setDataGroupSortId(SORT_ID_1);
+        dataGroup1.setFinEntitiesDataMatrixs(new ArrayList<FinEntitiesDataMatrix>());
+        dataGroup1.getFinEntitiesDataMatrixs().add(getFinEntitiesDataMatrix(COLUMN_NAME_11, GUITYPE_DROPDOWN, LOOKUP_ARGUIMENT, COLUMN_LABEL_11));
+        dataGroup1.getFinEntitiesDataMatrixs().add(getFinEntitiesDataMatrix(COLUMN_NAME_12, GUITYPE_CHECKBOX, "", COLUMN_LABEL_12));
+        dataGroups.add(dataGroup1);
+        FinEntitiesDataGroup dataGroup2 = new FinEntitiesDataGroup();
+        dataGroup2.setDataGroupId(2);
+        dataGroup2.setDataGroupSortId(SORT_ID_2);
+        dataGroup2.setDataGroupName(GROUP_NAME_2);
+        dataGroup2.setFinEntitiesDataMatrixs(new ArrayList<FinEntitiesDataMatrix>());
+        dataGroup2.getFinEntitiesDataMatrixs().add(getFinEntitiesDataMatrix(COLUMN_NAME_21, GUITYPE_CHECKBOX, "", COLUMN_LABEL_21));
+        dataGroup2.getFinEntitiesDataMatrixs().add(getFinEntitiesDataMatrix(COLUMN_NAME_12, GUITYPE_CHECKBOX, "", COLUMN_LABEL_12));
+        // this inactive one will not be included
+        dataGroup2.getFinEntitiesDataMatrixs().get(1).setStatusFlag(false);
+        dataGroups.add(dataGroup2);
+        
+        return dataGroups;
+        
+    }
+    
+    private FinEntitiesDataMatrix getFinEntitiesDataMatrix(String columnName,String guiType,String lookupArgument,String columnLabel) {
+        FinEntitiesDataMatrix dataMatrix = new FinEntitiesDataMatrix();
+        dataMatrix.setColumnName(columnName);
+        dataMatrix.setGuiType(guiType);
+        dataMatrix.setLookupArgument(lookupArgument);
+        dataMatrix.setColumnLabel(columnLabel);
+        dataMatrix.setStatusFlag(true);
+        return dataMatrix;
+    }  
+    
+     private FinIntEntityRelType getFinIntEntityRelType(String code, Integer sortId, boolean active) {
+         FinIntEntityRelType finIntEntityRelType = new FinIntEntityRelType();
+         finIntEntityRelType.setRelationshipTypeCode(code);
+         finIntEntityRelType.setSortId(sortId);
+         finIntEntityRelType.setActive(active);
+         finIntEntityRelType.setDescription("Description"+code);
+         return finIntEntityRelType;
+     }
+     
      private KcPersonService getMockKcPersonService() {
          // not really referenced yet
          final KcPersonService kcPersonService = context.mock(KcPersonService.class);
