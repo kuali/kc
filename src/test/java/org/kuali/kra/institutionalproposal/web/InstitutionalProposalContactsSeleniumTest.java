31b3579adc75f71e1c57c7dd6abd777435b9b2f2
Kristina Taylor <kbtaylor@uw.edu>
2011-09-29 15:30:23 +0000

    Moved IP web tests to Selenium

---
+++ src/test/java/org/kuali/kra/institutionalproposal/web/InstitutionalProposalContactsSeleniumTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.institutionalproposal.web;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.kra.test.infrastructure.KcSeleniumTestBase;
+
+public class InstitutionalProposalContactsSeleniumTest extends KcSeleniumTestBase {
+    
+    private static final String BEAN_PREFIX = "projectPersonnelBean.";
+    
+    private static final String PERSON_ID_TAG = BEAN_PREFIX + "personId";
+    private static final String ROLODEX_ID_TAG = BEAN_PREFIX + "rolodexId";
+    
+    private static final String PERSON_ID_ID = "personId";
+    private static final String ROLODEX_ID_ID = "rolodexId";
+    private static final String CONTACT_ROLE_CODE = BEAN_PREFIX + "contactRoleCode";
+    private static final String KEY_PERSON_ROLE = BEAN_PREFIX + "newInstitutionalProposalContact.keyPersonRole";
+
+    private static final String PERSON_ID = "10000000002";
+    private static final String ROLODEX_ID = "1727";
+    
+    private static final String EMPLOYEE_FULL_NAME = "Joe  Tester";
+    private static final String ROLODEX_FULL_NAME = "Ho, Pauline";
+    private static final String CO_INVESTIGATOR_CONTACT_ROLE_CODE = "Co-Investigator";
+    private static final String KEY_PERSON_CONTACT_ROLE_CODE = "Key Person";
+    private static final String OBSERVER_KEY_PERSON_ROLE = "Observer";
+    
+    private static final String ADD_PROJECT_PERSON_BUTTON = "methodToCall.addProjectPerson";
+    private static final String DELETE_PROJECT_PERSON_BUTTON = "methodToCall.deleteProjectPerson.line%d";
+    
+    private InstitutionalProposalSeleniumHelper helper;
+    
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        
+        helper = InstitutionalProposalSeleniumHelper.instance(driver);
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        helper = null;
+        
+        super.tearDown();
+    }
+    
+    @Test
+    public void testAddRemoveEmployeeContact() throws Exception {
+        helper.createInstitutionalProposal();
+        helper.clickInstitutionalProposalContactsPage();
+        
+        helper.openTab("ProjectPersonnel");
+        helper.lookup(PERSON_ID_TAG, PERSON_ID_ID, PERSON_ID);
+        
+        helper.set(CONTACT_ROLE_CODE, CO_INVESTIGATOR_CONTACT_ROLE_CODE);
+        helper.click(ADD_PROJECT_PERSON_BUTTON);
+        
+        helper.assertNoPageErrors();
+        helper.assertPageContains(EMPLOYEE_FULL_NAME);
+        
+        helper.click(String.format(DELETE_PROJECT_PERSON_BUTTON, 1));
+        helper.assertPageDoesNotContain(EMPLOYEE_FULL_NAME);
+    }
+
+    @Test
+    public void testAddRemoveRolodexContact() throws Exception {
+        helper.createInstitutionalProposal();
+        helper.clickInstitutionalProposalContactsPage();
+        
+        helper.openTab("ProjectPersonnel");
+        helper.lookup(ROLODEX_ID_TAG, ROLODEX_ID_ID, ROLODEX_ID);
+        
+        helper.set(CONTACT_ROLE_CODE, KEY_PERSON_CONTACT_ROLE_CODE);
+        helper.set(KEY_PERSON_ROLE, OBSERVER_KEY_PERSON_ROLE);
+        helper.click(ADD_PROJECT_PERSON_BUTTON);
+        
+        helper.assertNoPageErrors();
+        helper.assertPageContains(ROLODEX_FULL_NAME);
+        
+        helper.click(String.format(DELETE_PROJECT_PERSON_BUTTON, 1));
+        helper.assertPageDoesNotContain(ROLODEX_FULL_NAME);
+    }
+
+}
---
+++ src/test/java/org/kuali/kra/institutionalproposal/web/InstitutionalProposalSpecialReviewSeleniumTest.java
@@ -0,0 +1,240 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.institutionalproposal.web;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.test.infrastructure.KcSeleniumTestBase;
+
+public class InstitutionalProposalSpecialReviewSeleniumTest extends KcSeleniumTestBase {
+    
+    private static final String HELPER_PREFIX = "specialReviewHelper.newSpecialReview.";
+    private static final String LIST_PREFIX = "document.institutionalProposalList[0].specialReviews[%d].";
+    
+    private static final String SPECIAL_REVIEW_TYPE_CODE_ID = "specialReviewTypeCode";
+    private static final String APPROVAL_TYPE_CODE_ID = "approvalTypeCode";
+    private static final String PROTOCOL_NUMBER_ID = "protocolNumber";
+    private static final String APPLICATION_DATE_ID = "applicationDate";
+    private static final String APPROVAL_DATE_ID = "approvalDate";
+    private static final String EXPIRATION_DATE_ID = "expirationDate";
+    private static final String EXEMPTION_TYPE_CODES_ID = "exemptionTypeCodes";
+    private static final String COMMENTS_ID = "comments";
+    private static final String HELPER_SPECIAL_REVIEW_TYPE_CODE_ID = HELPER_PREFIX + SPECIAL_REVIEW_TYPE_CODE_ID;
+    private static final String HELPER_APPROVAL_TYPE_CODE_ID = HELPER_PREFIX + APPROVAL_TYPE_CODE_ID;
+    private static final String HELPER_PROTOCOL_NUMBER_ID = HELPER_PREFIX + PROTOCOL_NUMBER_ID;
+    private static final String HELPER_APPLICATION_DATE_ID = HELPER_PREFIX + APPLICATION_DATE_ID;
+    private static final String HELPER_APPROVAL_DATE_ID = HELPER_PREFIX + APPROVAL_DATE_ID;
+    private static final String HELPER_EXPIRATION_DATE_ID = HELPER_PREFIX + EXPIRATION_DATE_ID;
+    private static final String HELPER_EXEMPTION_TYPE_CODES_ID = HELPER_PREFIX + EXEMPTION_TYPE_CODES_ID;
+    private static final String HELPER_COMMENTS_ID = HELPER_PREFIX + COMMENTS_ID;
+    private static final String LIST_SPECIAL_REVIEW_TYPE_CODE_ID = LIST_PREFIX + SPECIAL_REVIEW_TYPE_CODE_ID;
+    private static final String LIST_APPROVAL_TYPE_CODE_ID = LIST_PREFIX + APPROVAL_TYPE_CODE_ID;
+    private static final String LIST_PROTOCOL_NUMBER_ID = LIST_PREFIX + PROTOCOL_NUMBER_ID;
+    private static final String LIST_APPLICATION_DATE_ID = LIST_PREFIX + APPLICATION_DATE_ID;
+    private static final String LIST_APPROVAL_DATE_ID = LIST_PREFIX + APPROVAL_DATE_ID;
+    private static final String LIST_EXPIRATION_DATE_ID = LIST_PREFIX + EXPIRATION_DATE_ID;
+    private static final String LIST_EXEMPTION_TYPE_CODES_ID = LIST_PREFIX + EXEMPTION_TYPE_CODES_ID;
+    private static final String LIST_COMMENTS_ID = LIST_PREFIX + COMMENTS_ID;
+        
+    private static final String SPECIAL_REVIEW_TYPE_ANIMAL_USAGE_NAME = "Animal Usage";
+    private static final String APPROVAL_TYPE_APPROVED_NAME = "Approved";
+    private static final String APPROVAL_TYPE_EXEMPT_NAME = "Exempt";
+    private static final String PROTOCOL_NUMBER = "1000000001";
+    private static final String APPLICATION_DATE = "01/01/2009";
+    private static final String APPROVAL_DATE = "02/01/2009";
+    private static final String EXPIRATION_DATE = "03/01/2009";
+    private static final String COMMENTS = "This is a test";
+    private static final String EXEMPTION_TYPE_E1_NAME = "E1";
+    
+    private static final String ADD_SPECIAL_REVIEW_BUTTON = "methodToCall.addSpecialReview";
+    private static final String DELETE_SPECIAL_REVIEW_BUTTON = "methodToCall.deleteSpecialReview.line%d";
+    
+    private InstitutionalProposalSeleniumHelper helper;
+    
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        
+        helper = InstitutionalProposalSeleniumHelper.instance(driver);
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        helper = null;
+        
+        super.tearDown();
+    }
+    
+    private class Review {
+        String type;
+        String approvalStatus;
+        String protocolNumber;
+        String applicationDate;
+        String approvalDate;
+        String expirationDate;
+        List<String> exemptionTypeCodes;
+        String comments;
+        
+        public Review(String type, String approvalStatus, String protocolNumber, String applicationDate, String approvalDate, String expirationDate, 
+                      List<String> exemptionTypeCodes, String comments) {
+            this.type = type;
+            this.approvalStatus = approvalStatus;
+            this.protocolNumber = protocolNumber;
+            this.applicationDate = applicationDate;
+            this.approvalDate = approvalDate;
+            this.expirationDate = expirationDate;
+            this.exemptionTypeCodes = exemptionTypeCodes;
+            this.comments = comments;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return true;
+        }
+        
+        @Override
+        public int hashCode() {
+            return 1;
+        }
+    }
+
+    /**
+     * Verify that we can successfully add an approved special review and then verify that it is indeed saved to the database.
+     */
+    @Test
+    public void testAddApprovedSpecialReview() throws Exception {
+        helper.createInstitutionalProposal();
+        helper.clickInstitutionalProposalSpecialReviewPage();
+        
+        setApprovedSpecialReviewFields();
+        helper.click(ADD_SPECIAL_REVIEW_BUTTON);
+        helper.assertNoPageErrors();
+        
+        List<Review> reviews = new ArrayList<Review>();
+        reviews.add(
+                new Review(SPECIAL_REVIEW_TYPE_ANIMAL_USAGE_NAME, APPROVAL_TYPE_APPROVED_NAME, PROTOCOL_NUMBER, APPLICATION_DATE, APPROVAL_DATE, 
+                           EXPIRATION_DATE, Collections.<String>emptyList(), COMMENTS));
+        checkTable(reviews);
+        
+        helper.closeAndSearchDocument();
+        helper.clickInstitutionalProposalSpecialReviewPage();
+        checkTable(reviews);
+    }
+    
+    /**
+     * Verify that we can successfully add an exempt special review and then verify that it is indeed saved to the database.
+     */
+    @Test
+    public void testAddExemptSpecialReview() throws Exception {
+        helper.createInstitutionalProposal();
+        helper.clickInstitutionalProposalSpecialReviewPage();
+        
+        setExemptSpecialReviewFields();
+        helper.click(ADD_SPECIAL_REVIEW_BUTTON);
+        helper.assertNoPageErrors();
+        
+        List<Review> reviews = new ArrayList<Review>();
+        reviews.add(
+                new Review(SPECIAL_REVIEW_TYPE_ANIMAL_USAGE_NAME, APPROVAL_TYPE_EXEMPT_NAME, Constants.EMPTY_STRING, APPLICATION_DATE, Constants.EMPTY_STRING, 
+                           EXPIRATION_DATE, Collections.singletonList(EXEMPTION_TYPE_E1_NAME), COMMENTS));
+        checkTable(reviews);
+        
+        helper.closeAndSearchDocument();
+        helper.clickInstitutionalProposalSpecialReviewPage();
+        checkTable(reviews);
+    }
+    
+    /**
+     * Verify that we can delete a special review entry and that the change is reflected in the database.
+     */
+    @Test
+    public void testDeleteSpecialReview() throws Exception {
+        helper.createInstitutionalProposal();
+        helper.clickInstitutionalProposalSpecialReviewPage();
+        
+        setApprovedSpecialReviewFields();
+        helper.click(ADD_SPECIAL_REVIEW_BUTTON);
+        helper.assertNoPageErrors();
+        
+        helper.closeAndSearchDocument();
+        helper.clickInstitutionalProposalSpecialReviewPage();
+        
+        helper.click(String.format(DELETE_SPECIAL_REVIEW_BUTTON, 0));
+        helper.clickYesAnswer();
+        List<Review> reviews = new ArrayList<Review>();
+        checkTable(reviews);
+        
+        helper.closeAndSearchDocument();
+        helper.clickInstitutionalProposalSpecialReviewPage();
+        checkTable(reviews);
+    }
+    
+    /**
+     * Try adding a special review without setting the type and approval status.  We should get back two errors.
+     */
+    @Test
+    public void testAddErrorSpecialReview() throws Exception {
+        helper.createInstitutionalProposal();
+        helper.clickInstitutionalProposalSpecialReviewPage();
+        
+        helper.click(ADD_SPECIAL_REVIEW_BUTTON);
+        helper.assertErrorCount("tab-SpecialReview-div", 2);
+    }
+    
+    private void setApprovedSpecialReviewFields() throws IOException {
+        helper.set(HELPER_SPECIAL_REVIEW_TYPE_CODE_ID, SPECIAL_REVIEW_TYPE_ANIMAL_USAGE_NAME);
+        helper.set(HELPER_APPROVAL_TYPE_CODE_ID, APPROVAL_TYPE_APPROVED_NAME);
+        helper.set(HELPER_PROTOCOL_NUMBER_ID, PROTOCOL_NUMBER);
+        helper.set(HELPER_APPLICATION_DATE_ID, APPLICATION_DATE);
+        helper.set(HELPER_APPROVAL_DATE_ID, APPROVAL_DATE);
+        helper.set(HELPER_EXPIRATION_DATE_ID, EXPIRATION_DATE);
+        helper.set(HELPER_COMMENTS_ID, COMMENTS);
+    }
+    
+    private void setExemptSpecialReviewFields() {
+        helper.set(HELPER_SPECIAL_REVIEW_TYPE_CODE_ID, SPECIAL_REVIEW_TYPE_ANIMAL_USAGE_NAME);
+        helper.set(HELPER_APPROVAL_TYPE_CODE_ID, APPROVAL_TYPE_EXEMPT_NAME);
+        helper.set(HELPER_APPLICATION_DATE_ID, APPLICATION_DATE);
+        helper.set(HELPER_EXPIRATION_DATE_ID, EXPIRATION_DATE);
+        helper.set(HELPER_EXEMPTION_TYPE_CODES_ID, EXEMPTION_TYPE_E1_NAME);
+        helper.set(HELPER_COMMENTS_ID, COMMENTS);
+    }
+    
+    private void checkTable(List<Review> reviews) {
+        int index = 0;
+        for (Review review : reviews) {
+            helper.assertSelectedOptionsContains(String.format(LIST_SPECIAL_REVIEW_TYPE_CODE_ID, index), review.type);
+            helper.assertSelectedOptionsContains(String.format(LIST_APPROVAL_TYPE_CODE_ID, index), review.approvalStatus);
+            helper.assertElementContains(String.format(LIST_PROTOCOL_NUMBER_ID, index), review.protocolNumber);
+            helper.assertElementContains(String.format(LIST_APPLICATION_DATE_ID, index), review.applicationDate);
+            helper.assertElementContains(String.format(LIST_APPROVAL_DATE_ID, index), review.approvalDate);
+            helper.assertElementContains(String.format(LIST_EXPIRATION_DATE_ID, index), review.expirationDate);
+            for (String exemptionTypeCode : review.exemptionTypeCodes) {
+                helper.assertSelectedOptionsContains(String.format(LIST_EXEMPTION_TYPE_CODES_ID, index), exemptionTypeCode);
+            }
+            helper.assertElementContains(String.format(LIST_COMMENTS_ID, index), review.comments);
+            index++;
+        }
+    }
+    
+}
