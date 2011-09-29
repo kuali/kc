0f896b67bbb55d8fcdf43778bbb6f10213a813f7
Lin-Long Shyu <lshyukuali@gmail.com>
2011-09-29 13:22:17 +0000

    kccoi-4 : change in FE selenium test

--- src/test/java/org/kuali/kra/coi/personfinancialentity/FinancialEntitySeleniumHelper.java
+++ src/test/java/org/kuali/kra/coi/personfinancialentity/FinancialEntitySeleniumHelper.java
@@ -17,108 +17,109 @@ package org.kuali.kra.coi.personfinancialentity;
 
 import java.util.Date;
 
+import org.apache.commons.lang.StringUtils;
 import org.kuali.kra.test.infrastructure.KcSeleniumHelper;
 import org.openqa.selenium.WebDriver;
 
 public class FinancialEntitySeleniumHelper extends KcSeleniumHelper {
     private static FinancialEntitySeleniumHelper helper;
-    
+
     public static FinancialEntitySeleniumHelper instance(WebDriver driver) {
+
         if (helper == null) {
             helper = new FinancialEntitySeleniumHelper(driver);
         }
         return helper;
     }
-    
+
     private FinancialEntitySeleniumHelper(WebDriver driver) {
         super(driver);
     }
 
-    public void FinancialEntityTest() {
-     //   open("/kc-dev/portal.jsp");
+    public void FinancialEntityCompleteTest() {
+
         click("Financial Entities");
-     //   waitForPageToLoad("30000");
-     //   login( "jtester");
-     //   click("css=input[type=\"submit\"]");
-  //      waitForPageToLoad("30000");
-  //      selectFrame("iframeportlet");
         click("methodToCall.editNew");
- //       waitForPageToLoad("30000");
+        setupFields();
+        waitForSponsorCodeAjax();
+        click("methodToCall.submit.new.anchor");
+        assertNoPageErrors();
+
+        click("methodToCall.editList");
+        int lastEntityIndex = findLastEntity();
+        click("methodToCall.editFinancialEntity.line" + lastEntityIndex);
+        set("financialEntityHelper.activeFinancialEntities[" + lastEntityIndex + "].principalBusinessActivity", "test company - v2");
+
+        click("methodToCall.save.line" + lastEntityIndex + ".anchor");
+        checkEditActionOK(lastEntityIndex);
+        
+        click("methodToCall.submit.line" + lastEntityIndex + ".anchor");
+        checkEditActionOK(lastEntityIndex);
+
+        click("methodToCall.inactivateFinancialEntity.line" + lastEntityIndex + ".anchor");
+        set("reason", "deactivate");
+        click("methodToCall.processAnswer.button0");
+        assertNoPageErrors();
+
+        click("methodToCall.activateFinancialEntity.line0.anchor");
+        assertNoPageErrors();
+
+    }
+
+    private void checkEditActionOK(int lastEntityIndex) {
+        assertNoPageErrors();
+        assertElementContains("financialEntityHelper.activeFinancialEntities[" + lastEntityIndex + "].sponsorCode", "005770");
+        assertElementContains("financialEntityHelper.activeFinancialEntities[" + lastEntityIndex + "].principalBusinessActivity",
+                "test company - v2");
+        
+    }
+    
+    /*
+     * set up required and some testing data
+     */
+    private void setupFields() {
         set("financialEntityHelper.newPersonFinancialEntity.entityName", String.valueOf(new Date().getTime()));
         click("financialEntityHelper.newPersonFinancialEntity.sponsorCode");
         set("financialEntityHelper.newPersonFinancialEntity.sponsorCode", "005770");
         set("financialEntityHelper.newPersonFinancialEntity.entityTypeCode", "1");
-        set("financialEntityHelper.newPersonFinancialEntity.principalBusinessActivity", "search company");
+        set("financialEntityHelper.newPersonFinancialEntity.principalBusinessActivity", "test company");
         set("financialEntityHelper.newRelationDetails[0].relationshipTypeBeans[0].stringValue", "1");
         click("financialEntityHelper.newRelationDetails[1].relationshipTypeBeans[0].booleanValue");
-        click("methodToCall.submit.new.anchor");
-     //   waitForPageToLoad("30000");
-        click("methodToCall.editList");
-    //    waitForPageToLoad("30000");
-    }
-
-    public void FinancialEntityCompleteTest() {
-        //   waitForPageToLoad("30000");
-        //   login( "jtester");
-        //   click("css=input[type=\"submit\"]");
-     //      waitForPageToLoad("30000");
-     //      selectFrame("iframeportlet");
-        click("Financial Entities");
-          click("methodToCall.editNew");
-    //       waitForPageToLoad("30000");
-           set("financialEntityHelper.newPersonFinancialEntity.entityName", String.valueOf(new Date().getTime()));
-           click("financialEntityHelper.newPersonFinancialEntity.sponsorCode");
-           set("financialEntityHelper.newPersonFinancialEntity.sponsorCode", "005770");
-           set("financialEntityHelper.newPersonFinancialEntity.entityTypeCode", "1");
-           set("financialEntityHelper.newPersonFinancialEntity.principalBusinessActivity", "search company");
-           set("financialEntityHelper.newRelationDetails[0].relationshipTypeBeans[0].stringValue", "1");
-           click("financialEntityHelper.newRelationDetails[1].relationshipTypeBeans[0].booleanValue");
-           click("methodToCall.submit.new.anchor");
-           assertNoPageErrors() ;
-           click("methodToCall.editList");
-       // waitForPageToLoad("30000");
-        int lastEntityIndex = findLastEntity();
-        click("methodToCall.editFinancialEntity.line"+lastEntityIndex);
-        //waitForPageToLoad("30000");
-        set("financialEntityHelper.activeFinancialEntities["+lastEntityIndex+"].principalBusinessActivity", "test company - v2");
-        click("methodToCall.save.line"+lastEntityIndex+".anchor");
-        assertNoPageErrors() ;
-        assertElementContains("financialEntityHelper.activeFinancialEntities["+lastEntityIndex+"].sponsorCode","005770");
-        assertElementContains("financialEntityHelper.activeFinancialEntities["+lastEntityIndex+"].principalBusinessActivity", "test company - v2");
-        // waitForPageToLoad("30000");
-        click("methodToCall.submit.line"+lastEntityIndex+".anchor");
-        assertNoPageErrors() ;
-        assertElementContains("financialEntityHelper.activeFinancialEntities["+lastEntityIndex+"].sponsorCode","005770");
-        assertElementContains("financialEntityHelper.activeFinancialEntities["+lastEntityIndex+"].principalBusinessActivity", "test company - v2");
-      //  waitForPageToLoad("30000");
-        click("methodToCall.inactivateFinancialEntity.line"+lastEntityIndex+".anchor");
-      //  waitForPageToLoad("30000");
-        set("reason", "deactivate");
-        click("methodToCall.processAnswer.button0");
-        assertNoPageErrors() ;
-    //    waitForPageToLoad("30000");
-        // this should be ok, unless last test failed after deactivate before activate ?
-        //  anyway, there is at least one in the inactive list
-        click("methodToCall.activateFinancialEntity.line0.anchor");
-        assertNoPageErrors() ;
-        //waitForPageToLoad("30000");
-
+    
     }
     
+    /*
+     * utility method for local test.  Local test does not refresh db after each test, so
+     * there might be several financial entity.  find the last one to 'edit'
+     */
     private int findLastEntity() {
         // this is for local testing which does not have db refreshed after every test
         int i = 0;
         String message = "";
         while (i < 1000) {
             try {
-            message = get("methodToCall.editFinancialEntity.line"+i);
-            } catch (AssertionError  e) {
-            //if (message.endsWith(" not found")) {
-                return (i-1);
-            //}
+                message = get("methodToCall.editFinancialEntity.line" + i);
             }
-            i ++ ;
+            catch (AssertionError e) {
+                // if (message.endsWith(" not found")) {
+                return (i - 1);
+                // }
+            }
+            i++;
         }
         return i;
+    }
+    
+    private void waitForSponsorCodeAjax() {
+        // use "i" as safety valve to prevent infinite loop, in case something went wrong
+        // no more than 5 sec wait.
+        int i = 0;
+        while (i++ < 10 && StringUtils.isBlank(get("financialEntityHelper.newPersonFinancialEntity.finEntityContactInfos[0].postalCode"))) {
+            try {
+               wait(500L);
+            } catch (Exception e) {
+                
+            }
+        }
     }
 }
