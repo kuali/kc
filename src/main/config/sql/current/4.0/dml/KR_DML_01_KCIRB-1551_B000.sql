97900eb996406f98ba9308e588c0280e76104d54
Lin-Long Shyu <lshyukuali@gmail.com>
2011-10-13 15:47:45 +0000

    kcirb-1551 : the update with international char is not working for impex

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCIRB-1551_B000.sql
+++
@@ -1,9 +0,0 @@
-update KR_COUNTRY_T set POSTAL_CNTRY_NM = 'Ã…land Islands' where POSTAL_CNTRY_CD='AX'
-/
-update KR_COUNTRY_T set POSTAL_CNTRY_NM = 'CÃ´te D''Ivoire' where POSTAL_CNTRY_CD='CI'
-/
-update KR_COUNTRY_T set POSTAL_CNTRY_NM = 'RÃ©union' where POSTAL_CNTRY_CD='RE'
-/
-update KR_COUNTRY_T set POSTAL_CNTRY_NM = 'Saint BarthÃ©lemy' where POSTAL_CNTRY_CD='BL'
-/
-
