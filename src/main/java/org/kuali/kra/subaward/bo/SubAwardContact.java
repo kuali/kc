98e9c88cca243ebb4190c207572b6ac46b56ded2
Geo Thomas <geot@mit.edu>
2011-10-17 23:30:51 +0000

    Jira#KRACOEUS-4956,KRACOEUS-4958,KRACOEUS-4957 Subaward module files - first commit.

---
+++ src/main/java/org/kuali/kra/subaward/bo/SubAwardContact.java
@@ -0,0 +1,266 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.subaward.bo;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+import java.util.LinkedHashMap;
+import org.kuali.kra.award.home.ContactType;
+import org.kuali.kra.bo.Rolodex;
+import org.kuali.kra.subaward.bo.SubAward;
+
+public class SubAwardContact extends KraPersistableBusinessObjectBase { 
+    
+    private static final long serialVersionUID = 1L;
+
+    private Integer subAwardContactId; 
+    private Integer subAwardId; 
+    private String subAwardCode; 
+    private Integer sequenceNumber; 
+    private String contactTypeCode; 
+    private Integer rolodexId;
+   
+
+    private ContactType contactType; 
+    private Rolodex rolodex; 
+    private SubAward subAward;
+    
+    
+    private String rolodexName;  
+    private String sponsorCode;
+    private String organization;
+    private String addressLine1;
+    private String addressLine2;
+    private String addressLine3;
+    private String city;   
+    private String county;
+    private String state;
+    private String postalCode;
+    private String countryCode;
+    private String phoneNumber;
+    private String emailAddress;
+    private String faxNumber;
+    private String comments;
+    
+    public SubAwardContact() { 
+
+    } 
+    
+    public Integer getSubAwardContactId() {
+        return subAwardContactId;
+    }
+
+    public void setSubAwardContactId(Integer subAwardContactId) {
+        this.subAwardContactId = subAwardContactId;
+    }
+
+    public Integer getSubAwardId() {
+        return subAwardId;
+    }
+
+    public void setSubAwardId(Integer subAwardId) {
+        this.subAwardId = subAwardId;
+    }
+
+    public String getSubAwardCode() {
+        return subAwardCode;
+    }
+
+    public void setSubAwardCode(String subAwardCode) {
+        this.subAwardCode = subAwardCode;
+    }
+
+    public Integer getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public void setSequenceNumber(Integer sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    public String getContactTypeCode() {
+        return contactTypeCode;
+    }
+
+    public void setContactTypeCode(String contactTypeCode) {
+        this.contactTypeCode = contactTypeCode;
+    }
+
+    public Integer getRolodexId() {
+        return rolodexId;
+    }
+
+    public void setRolodexId(Integer rolodexId) {
+        this.rolodexId = rolodexId;
+    }
+
+    public ContactType getContactType() {
+        return contactType;
+    }
+
+    public void setContactType(ContactType contactType) {
+        this.contactType = contactType;
+    }
+
+    public Rolodex getRolodex() {
+        return rolodex;
+    }
+
+    public void setRolodex(Rolodex rolodex) {
+        this.rolodex = rolodex;
+    }
+
+    public SubAward getSubAward() {
+        return subAward;
+    }
+
+    public void setSubAward(SubAward subAward) {
+        this.subAward = subAward;
+    }  
+
+    public String getRolodexName() {
+        return rolodexName;
+    }
+
+    public void setRolodexName(String rolodexName) {
+        this.rolodexName = rolodexName;
+    }
+
+    public String getSponsorCode() {
+        return sponsorCode;
+    }
+
+    public void setSponsorCode(String sponsorCode) {
+        this.sponsorCode = sponsorCode;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+    
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getAddressLine3() {
+        return addressLine3;
+    }
+
+    public void setAddressLine3(String addressLine3) {
+        this.addressLine3 = addressLine3;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    /** {@inheritDoc} */
+    @Override 
+    protected LinkedHashMap<String, Object> toStringMapper() {
+        LinkedHashMap<String, Object> hashMap = new LinkedHashMap<String, Object>();
+        hashMap.put("subAwardContactId", this.getSubAwardContactId());
+        hashMap.put("subAwardId", this.getSubAwardId());
+        hashMap.put("subAwardCode", this.getSubAwardCode());
+        hashMap.put("sequenceNumber", this.getSequenceNumber());
+        hashMap.put("contactTypeCode", this.getContactTypeCode());
+        hashMap.put("rolodexId", this.getRolodexId());
+        return hashMap;
+    }
+    
+}
---
+++ src/main/java/org/kuali/kra/subaward/web/struts/action/SubAwardAmountInfoAction.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.subaward.web.struts.action;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.kuali.kra.infrastructure.Constants;
+import org.kuali.kra.subaward.SubAwardForm;
+import org.kuali.kra.subaward.bo.SubAward;
+import org.kuali.kra.subaward.bo.SubAwardAmountInfo;
+import org.kuali.kra.subaward.document.SubAwardDocument;
+import org.kuali.kra.subaward.subawardrule.SubAwardDocumentRule;
+
+public class SubAwardAmountInfoAction extends SubAwardAction{
+    
+    @Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, ServletRequest request, ServletResponse response) throws Exception {
+        ActionForward actionForward = super.execute(mapping, form, request, response);
+        return actionForward;
+    }
+    
+    @Override
+    public ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        SubAwardForm subAwardForm = (SubAwardForm) form;
+        SubAward subAward = subAwardForm.getSubAwardDocument().getSubAward();
+        ActionForward forward;
+            forward = super.save(mapping, form, request, response);
+        return forward;
+    }
+    
+    public ActionForward reload(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        SubAwardForm subAwardForm = (SubAwardForm) form;
+        ActionForward forward = super.reload(mapping, form, request, response);
+        return forward;
+    }
+    
+    public ActionForward addAmountInfo(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        SubAwardForm subAwardForm = (SubAwardForm) form;
+        SubAwardAmountInfo amountInfo=subAwardForm.getNewSubAwardAmountInfo();       
+       
+        if(new SubAwardDocumentRule().processAddSubAwardAmountInfoBusinessRules(amountInfo)){   
+            addAmountInfoToSubAward(subAwardForm.getSubAwardDocument().getSubAward(),amountInfo); 
+            subAwardForm.setNewSubAwardAmountInfo(new SubAwardAmountInfo());
+        }
+        return mapping.findForward(Constants.MAPPING_AMOUNT_INFO_PAGE);        
+     }
+    boolean addAmountInfoToSubAward(SubAward subAward,SubAwardAmountInfo amountInfo){
+        amountInfo.setSubAward(subAward);    
+        return subAward.getSubAwardAmountInfoList().add(amountInfo);
+    }
+    
+    public ActionForward deleteAmountInfo(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        SubAwardForm subAwardForm = (SubAwardForm)form;
+        SubAwardDocument subAwardDocument = subAwardForm.getSubAwardDocument();
+        int selectedLineNumber = getSelectedLine(request);
+        subAwardDocument.getSubAward().getSubAwardAmountInfoList().remove(selectedLineNumber);
+        return mapping.findForward(Constants.MAPPING_AMOUNT_INFO_PAGE);
+    }
+
+}
