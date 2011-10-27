36b1d7044a435191cc7d46c2f87fe23c96f30415
Darius Robinson <drobinson@rsmart.com>
2011-10-27 19:33:27 +0000

    KCCOI-23 Create COI Reviewer Maintenance Document and BO

---
+++ src/main/java/org/kuali/kra/coi/CoiReviewer.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.coi;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+public class CoiReviewer extends KraPersistableBusinessObjectBase {
+
+    private static final long serialVersionUID = 8787122160668915118L;
+    
+    private String reviewerCode;
+    private String description;
+    
+    
+    public String getReviewerCode() {
+        return reviewerCode;
+    }
+    
+    public void setReviewerCode(String reviewerCode) {
+        this.reviewerCode = reviewerCode;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see org.kuali.rice.kns.bo.BusinessObjectBase#toStringMapper()
+     */
+    @Override
+    protected LinkedHashMap toStringMapper() {
+        LinkedHashMap<String, Object> hashMap = new LinkedHashMap<String, Object>();
+        hashMap.put("reviewerCode", this.getReviewerCode());
+        hashMap.put("description", this.getDescription());
+        return hashMap;
+    }
+}
