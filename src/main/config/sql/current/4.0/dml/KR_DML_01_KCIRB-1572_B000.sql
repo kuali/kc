007e361d9abf43ab716820c638cd022a3bcffb7a
Gayathri Athreya <hsggwk@gmail.com>
2011-10-29 04:09:34 +0000

    KCIRB-1572: Adding help icon to summary and history panel.

---
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCIRB-1572_B000.sql
@@ -0,0 +1,3 @@
+INSERT INTO krns_PARM_T (NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD)
+VALUES('KC-PROTOCOL', 'Document', 'protocolSummaryAndHistoryHelp', SYS_GUID () , 1, 'HELP', 'default.htm?turl=Documents/summaryhistory.htm', 'Summary and History Help', 'A')
+/
