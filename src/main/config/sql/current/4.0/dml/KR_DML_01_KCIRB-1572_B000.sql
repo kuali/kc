40c64f096549d79840879f889c70536ec09a679c
Gayathri Athreya <hsggwk@gmail.com>
2011-10-29 17:32:02 +0000

    KCIRB-1575: Adding protocol history help.

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCIRB-1572_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCIRB-1572_B000.sql
@@ -1,3 +1,7 @@
 INSERT INTO krns_PARM_T (NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD)
 VALUES('KC-PROTOCOL', 'Document', 'protocolSummaryAndHistoryHelp', SYS_GUID () , 1, 'HELP', 'default.htm?turl=Documents/summaryhistory.htm', 'Summary and History Help', 'A')
 /
+INSERT INTO krns_PARM_T (NMSPC_CD, PARM_DTL_TYP_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, TXT, PARM_DESC_TXT, CONS_CD)
+VALUES('KC-PROTOCOL', 'Document', 'protocolSummaryHelp', SYS_GUID () , 1, 'HELP', 'default.htm?turl=Documents/summary4.htm', 'Summary Help', 'A')
+/
+
