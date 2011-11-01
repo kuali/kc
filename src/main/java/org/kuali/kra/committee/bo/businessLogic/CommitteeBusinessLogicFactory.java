457e90b86a8c59de5f029847ce26e69f4e5d1fb2
Neeraj Khanolkar <neerajsk@iastate.edu>
2011-11-01 23:44:06 +0000

    KCCOI-17: Adding business logic wrappers and their factories (with the spring bean wiring)  for the COI/IRB committee domain.

---
+++ src/main/java/org/kuali/kra/committee/bo/businessLogic/CommitteeBusinessLogicFactory.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.committee.bo.businessLogic;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kra.committee.bo.Committee;
+import org.kuali.kra.committee.bo.CommitteeType;
+
+public class CommitteeBusinessLogicFactory {
+
+    private CommitteeCollaboratorFactoryGroup committeeCollaboratorFactoryGroup;
+
+
+    public void setCommitteeCollaboratorFactoryGroup(CommitteeCollaboratorFactoryGroup committeeCollaboratorFactoryGroup) {
+        this.committeeCollaboratorFactoryGroup = committeeCollaboratorFactoryGroup;
+    }
+
+    public CommitteeCollaboratorFactoryGroup getCommitteeCollaboratorFactoryGroup() {
+        return committeeCollaboratorFactoryGroup;
+    }
+
+
+    public CommitteeBusinessLogic getBusinessLogicFor(Committee committee) {
+        CommitteeBusinessLogic retVal = null;
+
+        // return appropriate subclass of committee business logic based on committee type
+        if (StringUtils.equals(committee.getCommitteeTypeCode(), CommitteeType.IRB_TYPE_CODE)) {
+            retVal = new IRBCommitteeBusinessLogic(committee, getCommitteeCollaboratorFactoryGroup());
+        }
+        else if (StringUtils.equals(committee.getCommitteeTypeCode(), CommitteeType.COI_TYPE_CODE)) {
+            retVal = new COICommitteeBusinessLogic(committee, getCommitteeCollaboratorFactoryGroup());
+        }
+        else {
+            retVal = new CommitteeBusinessLogic(committee, getCommitteeCollaboratorFactoryGroup()) {
+
+                @Override
+                public boolean validateCommitteeResearchAreas() {
+                    return false;
+                }
+                
+            };
+        }
+
+        return retVal;
+    }
+
+}
