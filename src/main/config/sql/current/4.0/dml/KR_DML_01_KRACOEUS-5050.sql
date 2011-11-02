79e14936a33f812854fb666b9b794f8f48e3198c
Ted Standley <hwstandley@ucdavis.edu>
2011-11-02 22:43:43 +0000

    KRACOEUS-5050:	Negotiation: Custom Attribute Document appears to be missing Document Type Code for Negotiation.

---
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5050.sql
@@ -0,0 +1,4 @@
+UPDATE KRNS_PARM_T
+SET txt = 'AWRD=Award;INPR=Institutional Proposal;NGT=Negotiation;PRDV=Proposal Development;PROT=Protocol'
+WHERE txt = 'AWRD=Award;INPR=Institutional Proposal;PRDV=Proposal Development;PROT=Protocol'
+/
