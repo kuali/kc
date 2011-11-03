542236566f0dd4d8a5395b83a5186222e2715006
Neeraj Khanolkar <neerajsk@iastate.edu>
2011-11-03 20:42:21 +0000

    KCCOI-15: Maintenance docs and BOs for the new COI committee artifacts.

---
+++ src/main/java/org/kuali/kra/coi/CoiCommitteeRoleType.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ *
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.osedu.org/licenses/ECL-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.coi;
+
+import java.util.LinkedHashMap;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+/**
+ * 
+ * This class implements the membership role object.
+ * 
+ * @author Kuali Research Administration Team (kc.dev@kuali.org)
+ */
+public class CoiCommitteeRoleType extends KraPersistableBusinessObjectBase { 
+    private String roleTypeCode; 
+    private String description; 
+    
+    public CoiCommitteeRoleType() { 
+
+    }    
+    
+    public void setRoleTypeCode(String roleTypeCode) {
+        this.roleTypeCode = roleTypeCode;
+    }
+
+    public String getRoleTypeCode() {
+        return roleTypeCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }  
+    
+    @Override 
+    protected LinkedHashMap<String,Object> toStringMapper() {
+        LinkedHashMap<String,Object> hashMap = new LinkedHashMap<String,Object>();
+        hashMap.put("roleTypeCode", getRoleTypeCode());
+        hashMap.put("description", getDescription());
+        return hashMap;
+    }
+    
+}
