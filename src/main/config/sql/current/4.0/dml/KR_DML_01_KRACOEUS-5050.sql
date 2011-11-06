583d11a482858093e4b522b12bb9b7ecd9da02c9
Ted Standley <hwstandley@ucdavis.edu>
2011-11-06 01:37:08 +0000

    remove file KR_DML_KRACOEUS-5050.sql

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5050.sql
+++
@@ -1,4 +0,0 @@
-UPDATE KRNS_PARM_T
-SET txt = 'AWRD=Award;INPR=Institutional Proposal;NGT=Negotiation;PRDV=Proposal Development;PROT=Protocol'
-WHERE txt = 'AWRD=Award;INPR=Institutional Proposal;PRDV=Proposal Development;PROT=Protocol'
-/
