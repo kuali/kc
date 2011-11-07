ff9a2615bd8d1b321781b336cf400f5a23bda264
Lin-Long Shyu <lshyukuali@gmail.com>
2011-11-07 14:44:38 +0000

    kccoi-36, kccoi-37, kccoi-38 : add projecttype to discdetail table and active status parameters

---
+++ src/main/config/sql/current/4.0/dml/KC_DML_01_KCCOI-37_B000.sql
@@ -0,0 +1,3 @@
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_DTL_TYP_CD,PARM_NM,VER_NBR,PARM_TYP_CD,TXT,PARM_DESC_TXT,CONS_CD,OBJ_ID) 
+VALUES ('KC','KC-PROTOCOL','Document','PROTOCOL_DISCLOSE_STATUS_CODES',1,'CONFG','100;101;200;201;202','Protocol status codes a coi disclosure event will be considered active.','A',sys_guid())
+/
