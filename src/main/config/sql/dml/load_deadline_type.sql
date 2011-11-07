1fdcf50aa5bb003b22c0cf9f51fd3e8b732c4e00
Kristina Taylor <kbtaylor@uw.edu>
2011-11-07 18:23:17 +0000

    Removing extraneous and outdated sql folders/files

--- src/main/config/sql/dml/load_deadline_type.sql
+++
@@ -1,3 +0,0 @@
-insert into deadline_type (deadline_type_code,description,update_timestamp,update_user) values('P','Postmark',sysdate,'kradev');
-insert into deadline_type (deadline_type_code,description,update_timestamp,update_user) values('T','Target',sysdate,'kradev');
-insert into deadline_type (deadline_type_code,description,update_timestamp,update_user) values('R','Receipt',sysdate,'kradev');
--- src/main/config/sql/dml/load_person.sql
+++
@@ -1,6 +0,0 @@
-insert into PERSON_EXT_T (PERSON_ID, OFFICE_LOCATION, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR)
-       values ('000000000', 'Kuali Foundation', SYSDATE,'KRADEV','1'); 
-
-insert into krim_entity_emp_info_t (entity_emp_id, obj_id, ver_nbr, entity_id, prmry_ind, actv_ind, last_updt_dt, prmry_dept_cd)
-       values (KRIM_ENTITY_EMP_ID_S.nextval, SYS_GUID(), 1, (SELECT p.entity_id FROM KRIM_PRNCPL_T p WHERE p.prncpl_id = '000000000'), 'Y', 'Y', SYSDATE, '000001');
-
