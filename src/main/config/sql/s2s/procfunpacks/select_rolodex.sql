2a34705069160b9cb9f713e263f5b237c3d98405
Kristina Taylor <kbtaylor@uw.edu>
2011-11-08 17:23:40 +0000

    Removing extraneous and outdated sql folders/files, moving log files from s2s to log folder

--- src/main/config/sql/s2s/procfunpacks/select_rolodex.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE PROCEDURE          SELECT_ROLODEX ( al_RolodexId in number, acur_rolodex IN OUT result_sets.cur_rolodex) is
-
-begin
-
-
-open acur_rolodex for select * from osp$rolodex where rolodex_id = al_RolodexId;
-end;
-/
-
