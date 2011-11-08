0b55edd34d7f5ab679aa7bea1a0ae99e4f537040
Kristina Taylor <kbtaylor@uw.edu>
2011-11-08 17:23:40 +0000

    Removing extraneous and outdated sql folders/files, moving log files from s2s to log folder

--- src/main/config/sql/s2s/views/OSP$SCIENCE_CODE.sql
+++
@@ -1,4 +0,0 @@
-
-CREATE OR REPLACE VIEW OSP$SCIENCE_CODE ( SCIENCE_CODE, 
-DESCRIPTION, UPDATE_TIMESTAMP, UPDATE_USER ) AS select science_keyword_code, description, update_timestamp, update_user
-from science_keyword;
