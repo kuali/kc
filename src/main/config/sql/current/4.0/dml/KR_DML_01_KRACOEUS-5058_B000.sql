7771cf28f9e8ab59bf49b34bcaf8bdbe0d0007a6
Ted Standley <hwstandley@ucdavis.edu>
2011-11-09 19:12:59 +0000

    KRACOEUS-5058:  Negotiation: Update Negotiation Agreement Type & Negotiation Association Type maintenance document header.

---
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5058_B000.sql
@@ -0,0 +1,8 @@
+UPDATE KREW_DOC_TYP_T 
+SET LBL = 'Negotiation Agreement Type'
+WHERE DOC_TYP_NM = 'NegotiationAgreementTypeMaintenanceDocument'
+/
+UPDATE KREW_DOC_TYP_T 
+SET LBL = 'Negotiation Association Type'
+WHERE DOC_TYP_NM = 'NegotiationAssociationTypeMaintenanceDocument'
+/
