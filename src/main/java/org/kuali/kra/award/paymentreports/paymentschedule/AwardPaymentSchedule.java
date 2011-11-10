fa0eff6650f5546a0f67a6eda6a3d645f48c3cb4
Jay Hulslander <jdh34@cornell.edu>
2011-11-10 20:12:49 +0000

    KRACOEUS-5002

--- src/main/java/org/kuali/kra/award/paymentreports/paymentschedule/AwardPaymentSchedule.java
+++ src/main/java/org/kuali/kra/award/paymentreports/paymentschedule/AwardPaymentSchedule.java
@@ -16,14 +16,20 @@
 package org.kuali.kra.award.paymentreports.paymentschedule;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.LinkedHashMap;
 
 import org.kuali.kra.award.AwardAssociate;
+import org.kuali.kra.award.paymentreports.ReportStatus;
+import org.kuali.kra.bo.KcPerson;
 import org.kuali.rice.kns.util.KualiDecimal;
 
 /**
  * 
  * This class represents the AwardPaymentSchedule business object.
+ */
+/**
+ * This class...
  */
 public class AwardPaymentSchedule extends AwardAssociate { 
     
@@ -39,13 +45,24 @@ public class AwardPaymentSchedule extends AwardAssociate {
     private String invoiceNumber; 
     private String statusDescription;
     private String status;
+    private Integer overdue;
+    private String lastUpdateUser;
+    private Timestamp lastUpdateTimestamp;
+    private String reportStatusCode;
+    private String submittedByPersonId;
+    /**
+     * submittedByPerson is not persisted!
+     */
+    private KcPerson submittedByPerson;
+    
+    
+    private ReportStatus reportStatus;
     
     /**
      * 
      * Constructs a AwardPaymentSchedule.java.
      */
     public AwardPaymentSchedule() { 
-
     }
     
     public AwardPaymentSchedule(Date dueDate, String awardNumber, Integer sequenceNumber){
@@ -185,27 +202,130 @@ public class AwardPaymentSchedule extends AwardAssociate {
      */
     public boolean equals(AwardPaymentSchedule awardPaymentSchedule) {
         if (getAwardNumber() == null) {
-            if (awardPaymentSchedule.getAwardNumber() != null){
+            if (awardPaymentSchedule.getAwardNumber() != null) {
                 return false;
             }   
-        }else if (!getAwardNumber().equals(awardPaymentSchedule.getAwardNumber())){
+        } else if (!getAwardNumber().equals(awardPaymentSchedule.getAwardNumber())) {
             return false;
         }   
         if (dueDate == null) {
-            if (awardPaymentSchedule.dueDate != null){
+            if (awardPaymentSchedule.dueDate != null) {
                 return false;
             }   
-        }else if (!dueDate.equals(awardPaymentSchedule.dueDate)){
+        } else if (!dueDate.equals(awardPaymentSchedule.dueDate)) {
             return false;
         }   
         if (getSequenceNumber() == null) {
-            if (awardPaymentSchedule.getSequenceNumber() != null){
+            if (awardPaymentSchedule.getSequenceNumber() != null) {
                 return false;
             }   
-        }else if (!getSequenceNumber().equals(awardPaymentSchedule.getSequenceNumber())){
+        } else if (!getSequenceNumber().equals(awardPaymentSchedule.getSequenceNumber())) {
             return false;
         }   
         return true;
     }
     
+    public boolean checkForUpdates (AwardPaymentSchedule awardPaymentSchedule) {
+        boolean retVal = true;
+        if (awardPaymentSchedule == null || !objectCompare(this.getAwardPaymentScheduleId(), awardPaymentSchedule.getAwardPaymentScheduleId())) {
+            throw new IllegalArgumentException("The passed in award payment schedule is null, or it has a different AwardPaymentScheduleId than this object.");
+        }
+        retVal &= objectCompare(this.getInvoiceNumber(), awardPaymentSchedule.getInvoiceNumber());
+        retVal &= objectCompare(this.getDueDate(), awardPaymentSchedule.getDueDate());
+        retVal &= objectCompare(this.getOverdue(), awardPaymentSchedule.getOverdue());
+        retVal &= objectCompare(this.getStatusDescription(), awardPaymentSchedule.getStatusDescription());
+        retVal &= objectCompare(this.getStatus(), awardPaymentSchedule.getStatus());
+        retVal &= objectCompare(this.getAmount(), awardPaymentSchedule.getAmount());
+        retVal &= objectCompare(this.getSubmittedBy(), awardPaymentSchedule.getSubmittedBy());
+        retVal &= objectCompare(this.getSubmitDate(), awardPaymentSchedule.getSubmitDate());
+        return !retVal;
+    }
+    
+    /**
+     * 
+     * This method returns true if both objects are the same.  If both objects are null, they are the same.  If both objects are not
+     * null and the object's equality method returns true, they are the same.  All other cases, they are not the same.
+     * @param ob1
+     * @param ob2
+     * @return
+     */
+    private boolean objectCompare(Object ob1, Object ob2) {
+        boolean retVal = false;
+        if (ob1 == null && ob2 == null) {
+            retVal = true; 
+        } else  if (ob1 != null && ob2 != null && ob1.equals(ob2)) {
+            retVal = true;
+        }
+        return retVal;
+    }
+
+    public Integer getOverdue() {
+        return overdue;
+    }
+
+    public void setOverdue(Integer overdue) {
+        this.overdue = overdue;
+    }
+
+    public String getLastUpdateUser() {
+        return lastUpdateUser;
+    }
+
+    public void setLastUpdateUser(String lastUpdateUser) {
+        this.lastUpdateUser = lastUpdateUser;
+    }
+
+    public Timestamp getLastUpdateTimestamp() {
+        return lastUpdateTimestamp;
+    }
+
+    public void setLastUpdateTimestamp(Timestamp lastUpdateTimestamp) {
+        this.lastUpdateTimestamp = lastUpdateTimestamp;
+    }
+
+    public String getReportStatusCode() {
+        return reportStatusCode;
+    }
+
+    public void setReportStatusCode(String reportStatusCode) {
+        this.reportStatusCode = reportStatusCode;
+    }
+
+    public String getSubmittedByPersonId() {
+        return submittedByPersonId;
+    }
+
+    public void setSubmittedByPersonId(String submittedByPersonId) {
+        this.submittedByPersonId = submittedByPersonId;
+    }
+    
+    /**
+     * 
+     * This method returns a KC Person for the getSubmittedByPersonId().
+     * @return
+     */
+    public KcPerson getSubmittedByPerson() {
+        if (submittedByPerson == null && getSubmittedByPersonId() != null) {
+            submittedByPerson = this.getKcPersonService().getKcPersonByPersonId(getSubmittedByPersonId());
+        }
+        return submittedByPerson;
+    }
+    
+    public String getSubmittedByPersonUsername() {
+        KcPerson submitter = getSubmittedByPerson();
+        return submitter == null ? "" : submitter.getUserName();
+    }
+    
+    public String getSubmittedByPersonFullname() {
+        KcPerson submitter = getSubmittedByPerson();
+        return submitter == null ? "" : submitter.getFullName();
+    }
+
+    public ReportStatus getReportStatus() {
+        return reportStatus;
+    }
+
+    public void setReportStatus(ReportStatus reportStatus) {
+        this.reportStatus = reportStatus;
+    }
 }
