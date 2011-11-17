8d97ddabeafcd63e19ae351354434db9fe250678
Chuck Tharp <grover.tharp@mail.wvu.edu>
2011-11-17 14:43:55 +0000

    KCIRB-1596: Create notification for "Delete Review" (oops)

--- src/main/config/sql/current/4.0/dml/KC_DML_01_KCIRB-1596_B000.sql
+++ src/main/config/sql/current/4.0/dml/KC_DML_01_KCIRB-1596_B000.sql
@@ -7,14 +7,14 @@ insert into notification_type
  SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,7,905,'Delete Review','Protocol {PROTOCOL_NUMBER} Deleted','The IRB protocol number <a title="" target="_self" href="../kew/DocHandler.do?command=displayDocSearchView&amp;docId={DOCUMENT_NUMBER}">{PROTOCOL_NUMBER}</a>, Principal Investigator {PI_NAME} has had the action "Delete" performed on it.<br />The action was executed by {USER_FULLNAME}.  Additional information and further actions can be accessed through the Kuali Coeus system.','N','Y','Y','admin',sysdate,1,sys_guid())
 /
 insert into NOTIFICATION_TYPE_RECIPIENT
-(NOTIFICATION_TYPE_RECIPIENT_ID,NOTIFICATION_TYPE_ID,ROLE_name,ROLE_QUALIFIER,TO_OR_CC,UPDATE_USER,UPDATE_TIMESTAMP,VER_NBR,OBJ_ID)
-values(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,(select NOTIFICATION_TYPE_ID from notification_type where DESCRIPTION = 'Delete Review'),'KC-PROTOCOL:IRB Online Reviewer','submissionId', 'T','admin',sysdate,1,sys_guid())
+(NOTIFICATION_TYPE_RECIPIENT_ID,NOTIFICATION_TYPE_ID,ROLE_name,TO_OR_CC,UPDATE_USER,UPDATE_TIMESTAMP,VER_NBR,OBJ_ID)
+values(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,(select NOTIFICATION_TYPE_ID from notification_type where DESCRIPTION = 'Delete Review'),'KC-PROTOCOL:IRB Online Reviewer','T','admin',sysdate,1,sys_guid())
 /
 insert into NOTIFICATION_TYPE_RECIPIENT
-(NOTIFICATION_TYPE_RECIPIENT_ID,NOTIFICATION_TYPE_ID,ROLE_name,ROLE_QUALIFIER,TO_OR_CC,UPDATE_USER,UPDATE_TIMESTAMP,VER_NBR,OBJ_ID)
-values(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,(select NOTIFICATION_TYPE_ID from notification_type where DESCRIPTION = 'Delete Review'),'KC-PROTOCOL:IRB Online Reviewer','protocolOnlineReviewId', 'T','admin',sysdate,1,sys_guid())
+(NOTIFICATION_TYPE_RECIPIENT_ID,NOTIFICATION_TYPE_ID,ROLE_name,TO_OR_CC,UPDATE_USER,UPDATE_TIMESTAMP,VER_NBR,OBJ_ID)
+values(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,(select NOTIFICATION_TYPE_ID from notification_type where DESCRIPTION = 'Delete Review'),'KC-PROTOCOL:Protocol Aggregator','T','admin',sysdate,1,sys_guid())
 /
 insert into NOTIFICATION_TYPE_RECIPIENT
-(NOTIFICATION_TYPE_RECIPIENT_ID,NOTIFICATION_TYPE_ID,ROLE_name,ROLE_QUALIFIER,TO_OR_CC,UPDATE_USER,UPDATE_TIMESTAMP,VER_NBR,OBJ_ID)
-values(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,(select NOTIFICATION_TYPE_ID from notification_type where DESCRIPTION = 'Delete Review'),'KC-PROTOCOL:IRB Online Reviewer','protocolLeadUnitNumber', 'T','admin',sysdate,1,sys_guid())
+(NOTIFICATION_TYPE_RECIPIENT_ID,NOTIFICATION_TYPE_ID,ROLE_name,TO_OR_CC,UPDATE_USER,UPDATE_TIMESTAMP,VER_NBR,OBJ_ID)
+values(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,(select NOTIFICATION_TYPE_ID from notification_type where DESCRIPTION = 'Delete Review'),'KC-PROTOCOL:PI','T','admin',sysdate,1,sys_guid())
 /
