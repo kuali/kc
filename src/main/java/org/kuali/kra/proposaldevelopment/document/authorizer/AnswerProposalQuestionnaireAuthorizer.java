6ca7803ca19f03dfcb42745381ee858ffaf05912
Ted Standley <hwstandley@ucdavis.edu>
2011-11-22 01:12:00 +0000

    KRACOEUS-5080: After rejecting the PD during routing, the eDoc is returned to the initiator. The initiator can edit fields on the various tabs, but not on the Questions tab

--- src/main/java/org/kuali/kra/proposaldevelopment/document/authorizer/AnswerProposalQuestionnaireAuthorizer.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/document/authorizer/AnswerProposalQuestionnaireAuthorizer.java
@@ -15,10 +15,14 @@
  */
 package org.kuali.kra.proposaldevelopment.document.authorizer;
 
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.kew.KraDocumentRejectionService;
+import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
 import org.kuali.kra.proposaldevelopment.document.authorization.ProposalTask;
 import org.kuali.rice.kns.document.Document;
 import org.kuali.rice.kns.document.authorization.PessimisticLock;
 import org.kuali.rice.kns.util.GlobalVariables;
+import org.kuali.rice.kns.workflow.service.KualiWorkflowDocument;
 
 
 /**
@@ -29,13 +33,15 @@ public class AnswerProposalQuestionnaireAuthorizer extends ProposalAuthorizer {
 
   
     /**
-     * @see org.kuali.kra.proposaldevelopment.document.authorizer.ProposalAuthorizer#isAuthorized(java.lang.String, org.kuali.kra.proposaldevelopment.document.authorization.ProposalTask)
+     * @see org.kuali.kra.proposaldevelopment.document.authorizer.ProposalAuthorizer#isAuthorized(java.lang.St  ring, org.kuali.kra.proposaldevelopment.document.authorization.ProposalTask)
      */
     public boolean isAuthorized(String userId, ProposalTask task) {
-
+        ProposalDevelopmentDocument doc = task.getDocument();
+        KualiWorkflowDocument wfd=doc.getDocumentHeader().getWorkflowDocument();
+        boolean hasBeenRejected=KraServiceLocator.getService(KraDocumentRejectionService.class).isDocumentOnInitialNode(doc);
         return !task.getDocument().isViewOnly()
                 && !isPessimisticLocked(task.getDocument())
-                && !kraWorkflowService.isInWorkflow(task.getDocument());
+                && (!kraWorkflowService.isInWorkflow(task.getDocument()) || hasBeenRejected);
         // kcirb-876 : Answer questionnaire permission is not needed   
            //     && hasPermission(userId, task.getProtocol(), PermissionConstants.ANSWER_PROTOCOL_QUESTIONNAIRE);
     }
