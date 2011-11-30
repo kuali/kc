1dfaa9031b7680e5cf90376fd59bdff4ba4a267f
Douglas Pace <dpace@kuali.org>
2011-11-30 18:43:33 +0000

    KRACOEUS-4985, KRACOEUS-4982: Fix parm desc to reflect which item is used as default status and add missing parm type to resolve issues with using Parameter maint doc.

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-4936_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-4936_B000.sql
@@ -2,8 +2,12 @@ INSERT INTO KRNS_NMSPC_T (ACTV_IND,NM,NMSPC_CD,APPL_NMSPC_CD,OBJ_ID,VER_NBR)
   VALUES ('Y','KC Negotiation','KC-NEGOTIATION','KC',sys_guid(),1)
 /
 
+INSERT INTO KRNS_PARM_DTL_TYP_T (NMSPC_CD, PARM_DTL_TYP_CD, NM, ACTV_IND, OBJ_ID, VER_NBR)
+	VALUES ('KC-NEGOTIATION', 'Document', 'Document', 'Y', sys_guid(), 1)
+/
+
 INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
-    VALUES ('KC', 'KC-NEGOTIATION', 'CONFG', 'Document', 'negotiationInProgressStatusCodes', 'A comma delimited list of the negotiation status codes that are considered in progress.', 'IP', 'A', SYS_GUID(), 1)
+    VALUES ('KC', 'KC-NEGOTIATION', 'CONFG', 'Document', 'negotiationInProgressStatusCodes', 'A comma delimited list of the negotiation status codes that are considered in progress. The first status defined here will be used as the default status for new negotiations.', 'IP', 'A', SYS_GUID(), 1)
 /
 
 INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,NMSPC_CD,PARM_TYP_CD,PARM_DTL_TYP_CD,PARM_NM,PARM_DESC_TXT,TXT,CONS_CD,OBJ_ID,VER_NBR)
