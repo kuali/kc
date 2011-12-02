1b744df6b50b794251d98809bd492b0b8bb7375f
Ge Zhang <g1zhang@ucsd.edu>
2011-12-02 06:19:47 +0000

    KRACOEUS-5083

--- src/main/config/sql/current/4.0/dml/KC_DML_01_KRACOEUS-5083_B000.sql
+++ src/main/config/sql/current/4.0/dml/KC_DML_01_KRACOEUS-5083_B000.sql
@@ -2,16 +2,16 @@ DECLARE
   max_id number;
   max_sq number;
 BEGIN
-  select max(id) into max_id from test1;
-  select max(id) into max_sq from test2;
+  select max(PROPOSAL_PERSON_ID) into max_id from PROPOSAL_PERSONS;
+  select max(PROPOSAL_PERSON_UNIT_ID) into max_sq from PROPOSAL_PERSON_UNITS;
   if max_id is null then max_id:=0; end if;
   if max_sq is null then max_sq:=0; end if;
   select GREATEST (max_id, max_sq)  into max_id from dual;
-  select TEST_S.NEXTVAL into max_sq from dual;
+  select SEQ_PROPOSAL_PROPOSAL_ID.NEXTVAL into max_sq from dual;
   IF max_sq < max_id THEN 
    max_sq := max_id - max_sq;
-   EXECUTE IMMEDIATE 'alter sequence TEST_S increment by ' || max_sq; 
-   EXECUTE IMMEDIATE 'select TEST_S.NEXTVAL from dual' INTO max_id;
-   EXECUTE IMMEDIATE 'alter sequence TEST_S increment by 1';
+   EXECUTE IMMEDIATE 'alter sequence SEQ_PROPOSAL_PROPOSAL_ID increment by ' || max_sq; 
+   EXECUTE IMMEDIATE 'select SEQ_PROPOSAL_PROPOSAL_ID.NEXTVAL from dual' INTO max_id;
+   EXECUTE IMMEDIATE 'alter sequence SEQ_PROPOSAL_PROPOSAL_ID increment by 1';
   END IF;
 END;
