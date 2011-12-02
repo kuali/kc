e154320a18e4238db978e6dfca5793e0db71588d
Ge Zhang <g1zhang@ucsd.edu>
2011-12-02 07:30:36 +0000

    KRACOEUS-5083

--- src/main/config/sql/current/4.0/dml/KC_DML_01_KRACOEUS-5083_B000.sql
+++ src/main/config/sql/current/4.0/dml/KC_DML_01_KRACOEUS-5083_B000.sql
@@ -1,17 +1,15 @@
-DECLARE 
-  max_id number;
-  max_sq number;
+DECLARE   max_id NUMBER; max_sq NUMBER;
 BEGIN
-  select max(PROPOSAL_PERSON_ID) into max_id from PROPOSAL_PERSONS;
-  select max(PROPOSAL_PERSON_UNIT_ID) into max_sq from PROPOSAL_PERSON_UNITS;
-  if max_id is null then max_id:=0; end if;
-  if max_sq is null then max_sq:=0; end if;
-  select GREATEST (max_id, max_sq)  into max_id from dual;
-  select SEQ_PROPOSAL_PROPOSAL_ID.NEXTVAL into max_sq from dual;
+  SELECT max(PROPOSAL_PERSON_ID) INTO max_id FROM PROPOSAL_PERSONS;
+  SELECT max(PROPOSAL_PERSON_UNIT_ID) INTO max_sq FROM PROPOSAL_PERSON_UNITS;
+  IF max_id is null THEN max_id:=0; END IF;
+  IF max_sq is null THEN max_sq:=0; END IF;
+  SELECT GREATEST (max_id, max_sq) INTO max_id FROM dual;
+  SELECT SEQ_PROPOSAL_PROPOSAL_ID.NEXTVAL INTO max_sq FROM dual;
   IF max_sq < max_id THEN 
    max_sq := max_id - max_sq;
-   EXECUTE IMMEDIATE 'alter sequence SEQ_PROPOSAL_PROPOSAL_ID increment by ' || max_sq; 
-   EXECUTE IMMEDIATE 'select SEQ_PROPOSAL_PROPOSAL_ID.NEXTVAL from dual' INTO max_id;
-   EXECUTE IMMEDIATE 'alter sequence SEQ_PROPOSAL_PROPOSAL_ID increment by 1';
+   EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_PROPOSAL_PROPOSAL_ID INCREMENT BY ' || max_sq; 
+   EXECUTE IMMEDIATE 'SELECT SEQ_PROPOSAL_PROPOSAL_ID.NEXTVAL FROM DUAL' INTO max_id;
+   EXECUTE IMMEDIATE 'ALTER SEQUENCE SEQ_PROPOSAL_PROPOSAL_ID INCREMENT BY 1';
   END IF;
 END;
