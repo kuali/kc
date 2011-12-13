95fa40f04665e34e24c29f2e93de2ce4d8c5e042
Kristina Taylor <kbtaylor@uw.edu>
2011-12-13 19:20:02 +0000

    MySQL Scripts and other minor script corrections for 4.0

--- src/main/config/sql/current/4.0/dml/KR_DML_03_KCIRB-1568_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_03_KCIRB-1568_B000.sql
@@ -1,4 +1,4 @@
---create new roles
+-- create new roles
 INSERT INTO KRIM_ROLE_T (ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, NMSPC_CD, DESC_TXT, KIM_TYP_ID, ACTV_IND, LAST_UPDT_DT)
 VALUES (KRIM_ROLE_PERM_ID_BS_S.NEXTVAL, SYS_GUID(), 1, 'Organization Correspondent', 'KC-PROTOCOL', 'The Organization Correspondent role', (SELECT KIM_TYP_ID FROM KRIM_TYP_T WHERE SRVC_NM = 'irbCorrespondentDerivedRoleTypeService'), 'Y', SYSDATE)
 /
