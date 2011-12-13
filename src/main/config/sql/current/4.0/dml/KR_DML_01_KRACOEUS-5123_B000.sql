66566efc2d05f15284ae5d6be3d570c4bb27232d
Ted Standley <hwstandley@ucdavis.edu>
2011-12-13 17:49:44 +0000

    KRACOEUS-5123: changed file name from KC to KR and added SYS_GUID() to script

---
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5123_B000.sql
@@ -0,0 +1,15 @@
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,CONS_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_DTL_TYP_CD,PARM_NM,PARM_TYP_CD,TXT,VER_NBR)
+  VALUES ('KC','A','KC-NEGOTIATION',SYS_GUID(),'A Negotiation Documet Overview','Document','negotiationNegotiationHelp','HELP','default.htm?turl=Documents/negotiation1.htm',1)
+/
+
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,CONS_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_DTL_TYP_CD,PARM_NM,PARM_TYP_CD,TXT,VER_NBR)
+  VALUES ('KC','A','KC-NEGOTIATION',SYS_GUID(),'A Negotiation Description','Document','negotiation','HELP','default.htm?turl=Documents/negotiation2.htm',1)
+/
+
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,CONS_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_DTL_TYP_CD,PARM_NM,PARM_TYP_CD,TXT,VER_NBR)
+  VALUES ('KC','A','KC-NEGOTIATION',SYS_GUID(),'Negotiation Activities and Attachments','Document','negotiationActivitiesHelp','HELP','default.htm?turl=Documents/activitiesattachments.htm',1)
+/
+
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,CONS_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_DTL_TYP_CD,PARM_NM,PARM_TYP_CD,TXT,VER_NBR)
+  VALUES ('KC','A','KC-NEGOTIATION',SYS_GUID(),'Negotiation Medusa','Document','negotiationMedusaHelp','HELP','default.htm?turl=Documents/medusa5.htm',1)
+/
