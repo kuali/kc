3fc760b2865e5faf19c1ea5c16bfb3a3d46848e7
Ted Standley <hwstandley@ucdavis.edu>
2011-12-14 22:47:45 +0000

    KRACOEUS-5088: Proposal Development: Online Help Icon on Other Organizations Subpanel Links to Incorrect Help Document

---
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5088_B000.sql
@@ -0,0 +1,5 @@
+delimiter /
+
+INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,CONS_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_DTL_TYP_CD,PARM_NM,PARM_TYP_CD,TXT,VER_NBR)
+VALUES ('KC','A','KC-PD',SYS_GUID(),'This is the other organizations','Document','otherOrganizationDocumentHelpUrl','HELP','default.htm?turl=Documents/otherorganizations.htm','1')
+/
