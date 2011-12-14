cd77e84134d64183882e55069994c7fe637d0074
Ted Standley <hwstandley@ucdavis.edu>
2011-12-14 23:38:37 +0000

    KRACOEUS-5086: correct syntax errors in scripts

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5086_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5086_B000.sql
@@ -1,6 +1,3 @@
-delimiter /
-
-
 INSERT INTO KRNS_PARM_T (APPL_NMSPC_CD,CONS_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_DTL_TYP_CD,PARM_NM,PARM_TYP_CD,TXT,VER_NBR)
 VALUES ('KC','A','KC-PD',SYS_GUID(),'This is the institutional Fields Conditionally Required','Document','institutionalFieldsConditionallyRequiredHelpUrl','HELP','default.htm?turl=Documents/institutionalfieldsconditionallyrequired.htm','1')
 /
