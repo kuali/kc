65db06247d6378a0d305cc6e15a563ddb001643a
Kristina Taylor <kbtaylor@uw.edu>
2011-12-14 19:07:51 +0000

    Minor script correction for 4.0

--- src/main/config/sql/current/4.0/dml/KC_DML_01_KRACOEUS-5071_B000.sql
+++ src/main/config/sql/current/4.0/dml/KC_DML_01_KRACOEUS-5071_B000.sql
@@ -1,9 +1,9 @@
 insert into VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_TIMESTAMP,UPDATE_USER,OBJ_ID) 
-	values (SEQ_VALID_NARR_FORMS_ID.nextval,'PHS398_ModularBudget_1_2-V1.2',35,'N',sysdate,'admin',sys_guid())
+	values (SEQ_VALID_NARR_FORMS_ID.NEXTVAL,'PHS398_ModularBudget_1_2-V1.2',35,'N',sysdate,'admin',sys_guid())
 /
 insert into VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_TIMESTAMP,UPDATE_USER,OBJ_ID) 
-	values (SEQ_VALID_NARR_FORMS_ID.nextval,'PHS398_ModularBudget_1_2-V1.2',36,'N',sysdate,'admin',sys_guid())
+	values (SEQ_VALID_NARR_FORMS_ID.NEXTVAL,'PHS398_ModularBudget_1_2-V1.2',36,'N',sysdate,'admin',sys_guid())
 /
 insert into VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_TIMESTAMP,UPDATE_USER,OBJ_ID) 
-	values (SEQ_VALID_NARR_FORMS_ID.nextval,'PHS398_ModularBudget_1_2-V1.2',37,'N',sysdate,'admin',sys_guid())
+	values (SEQ_VALID_NARR_FORMS_ID.NEXTVAL,'PHS398_ModularBudget_1_2-V1.2',37,'N',sysdate,'admin',sys_guid())
 /
