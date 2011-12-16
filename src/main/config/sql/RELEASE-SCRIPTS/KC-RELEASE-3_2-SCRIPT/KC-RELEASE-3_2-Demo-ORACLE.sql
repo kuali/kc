c537f21ec3ac34dc91f9ff786413761ee21b0a31
Kristina Taylor <kbtaylor@uw.edu>
2011-12-16 15:39:40 +0000

    MySQL Scripts for 3.2

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_2-SCRIPT/KC-RELEASE-3_2-Demo-ORACLE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_2-SCRIPT/KC-RELEASE-3_2-Demo-ORACLE.sql
@@ -3,6 +3,5 @@ set sqlblanklines on
 spool KC-RELEASE-3_2-Demo-ORACLE-Install.log
 @../../current/3.2/dml/KC_DML_01_KRACOEUS-4937_0TSD.sql
 @../../current/3.2/dml/KC_DML_01_KRACOEUS-5144_00SD.sql
-@../../current/3.2/dml/KC_DML_01_WATERMARK_000D.sql
 commit;
 exit
