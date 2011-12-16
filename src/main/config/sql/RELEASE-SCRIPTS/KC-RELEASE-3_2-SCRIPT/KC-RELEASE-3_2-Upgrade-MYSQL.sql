0a40fac188d31db20867e4adb0ffdbab7dbfc738
Kristina Taylor <kbtaylor@uw.edu>
2011-12-16 14:43:41 +0000

    MySQL Scripts for 3.2

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_2-SCRIPT/KC-RELEASE-3_2-Upgrade-MYSQL.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_2-SCRIPT/KC-RELEASE-3_2-Upgrade-MYSQL.sql
@@ -121,6 +121,7 @@
 \. ./../../current_mysql/3.2/dml/KC_DML_01_KRACOEUS-4930_B000.sql
 \. ./../../current_mysql/3.2/dml/KC_DML_01_KRACOEUS-4937_B000.sql
 \. ./../../current_mysql/3.2/dml/KC_DML_01_KRACOEUS-4956_B000.sql
+\. ./../../current_mysql/3.2/dml/KC_DML_01_KRACOEUS-4962_B000.sql
 \. ./../../current_mysql/3.2/dml/KC_DML_01_KRACOEUS-4969_B000.sql
 \. ./../../current_mysql/3.2/dml/KC_DML_01_KRACOEUS-4979_B000.sql
 \. ./../../current_mysql/3.2/dml/KC_DML_01_KRACOEUS-4998_B000.sql
