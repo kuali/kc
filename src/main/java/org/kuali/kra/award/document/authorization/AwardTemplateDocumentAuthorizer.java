2d7772e76bd300a1cfe867470081e6cae90dc806
Sweta Sheth <ssheth@vivantech.com>
2011-12-28 08:29:47 +0000

    KRACOEUS-5100 : Added 'Modify Sponsor Template' role and 'create', 'modify','view','delete' permission for Sponsor Template

---
+++ src/main/java/org/kuali/kra/award/document/authorization/AwardTemplateDocumentAuthorizer.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.award.document.authorization;
+
+import java.util.Map;
+
+import org.kuali.kra.kim.bo.KcKimAttributes;
+import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.document.authorization.MaintenanceDocumentAuthorizer;
+import org.kuali.rice.kns.document.authorization.MaintenanceDocumentAuthorizerBase;
+
+public class AwardTemplateDocumentAuthorizer extends MaintenanceDocumentAuthorizerBase implements MaintenanceDocumentAuthorizer {
+    
+    @Override
+    protected void addRoleQualification(
+            BusinessObject primaryBusinessObjectOrDocument,
+            Map<String, String> attributes) {
+        super.addRoleQualification(primaryBusinessObjectOrDocument, attributes);
+        attributes.put(KcKimAttributes.UNIT_NUMBER, "*");
+    }
+}
