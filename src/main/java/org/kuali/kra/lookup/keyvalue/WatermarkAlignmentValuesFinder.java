722427d4cd38782a14eb76bbb1b9cee7f3c25c13
Geo Thomas <geot@mit.edu>
2012-01-03 20:14:18 +0000

    jira#KCIRB-1628,KRACOEUS-5077

---
+++ src/main/java/org/kuali/kra/lookup/keyvalue/WatermarkAlignmentValuesFinder.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.lookup.keyvalue;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.kuali.kra.util.watermark.WatermarkConstants;
+import org.kuali.rice.core.util.KeyLabelPair;
+import org.kuali.rice.kns.lookup.keyvalues.KeyValuesBase;
+
+public class WatermarkAlignmentValuesFinder extends KeyValuesBase {
+    public List<KeyLabelPair> getKeyValues() {
+        List<KeyLabelPair> keyValues = new ArrayList<KeyLabelPair>();
+        keyValues.add(new KeyLabelPair("LEFT", WatermarkConstants.ALIGN_RIGHT));
+        keyValues.add(new KeyLabelPair("CENTER", WatermarkConstants.ALIGN_CENTER));
+        keyValues.add(new KeyLabelPair("RIGHT", WatermarkConstants.ALIGN_LEFT ));
+        return keyValues;
+    }
+}
