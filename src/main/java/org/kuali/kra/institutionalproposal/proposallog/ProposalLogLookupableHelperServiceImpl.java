0bd599b0110f54402137d7bd4191e6bba2043bab
Sweta Sheth <ssheth@vivantech.com>
2012-01-13 12:17:01 +0000

    KRACOEUS-5176 : Added code to filter search result for ProosalLog Lookup according to assigned role and permissions

--- src/main/java/org/kuali/kra/institutionalproposal/proposallog/ProposalLogLookupableHelperServiceImpl.java
+++ src/main/java/org/kuali/kra/institutionalproposal/proposallog/ProposalLogLookupableHelperServiceImpl.java
@@ -17,6 +17,8 @@ package org.kuali.kra.institutionalproposal.proposallog;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -25,17 +27,33 @@ import org.apache.commons.lang.StringUtils;
 import org.kuali.kra.bo.KcPerson;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.institutionalproposal.document.InstitutionalProposalDocument;
+import org.kuali.kra.institutionalproposal.document.authorization.InstitutionalProposalDocumentAuthorizer;
+import org.kuali.kra.institutionalproposal.home.InstitutionalProposal;
+import org.kuali.kra.questionnaire.Questionnaire;
 import org.kuali.kra.service.KcPersonService;
+import org.kuali.rice.kew.doctype.bo.DocumentType;
+import org.kuali.rice.kew.exception.WorkflowException;
+import org.kuali.rice.kew.routeheader.DocumentRouteHeaderValue;
+import org.kuali.rice.kew.util.KEWConstants;
+import org.kuali.rice.kew.util.KEWPropertyConstants;
+import org.kuali.rice.kew.web.session.UserSession;
+import org.kuali.rice.kim.bo.Person;
 import org.kuali.rice.kns.bo.BusinessObject;
+import org.kuali.rice.kns.document.Document;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.kns.document.MaintenanceDocumentBase;
 import org.kuali.rice.kns.lookup.HtmlData;
 import org.kuali.rice.kns.lookup.KualiLookupableHelperServiceImpl;
 import org.kuali.rice.kns.lookup.HtmlData.AnchorHtmlData;
+import org.kuali.rice.kns.service.DocumentService;
 import org.kuali.rice.kns.util.GlobalVariables;
 import org.kuali.rice.kns.util.KNSConstants;
 import org.kuali.rice.kns.util.UrlFactory;
 import org.kuali.rice.kns.web.struts.form.LookupForm;
 import org.kuali.rice.kns.web.ui.Field;
 import org.kuali.rice.kns.web.ui.Row;
+
 
 /**
  * Lookupable helper service used for proposal log lookup
@@ -46,8 +64,13 @@ public class ProposalLogLookupableHelperServiceImpl extends KualiLookupableHelpe
     
     private static final String USERNAME_FIELD = "person.userName";
     private static final String STATUS_PENDING = "1";
+    private static final String DOC_ROUTE_STATUS = "docRouteStatus";
     
     private boolean isLookupForProposalCreation;
+    private DocumentService documentService;
+    public void setDocumentService(DocumentService documentService) {
+        this.documentService = documentService;
+    }
     
     /*
      * We want to allow users to query on principal name instead of person id, 
@@ -78,15 +101,57 @@ public class ProposalLogLookupableHelperServiceImpl extends KualiLookupableHelpe
     @Override
     public List<? extends BusinessObject> getSearchResults(Map<String, String> fieldValues) {
         checkIsLookupForProposalCreation(fieldValues);
-        List<? extends BusinessObject> results = super.getSearchResults(fieldValues);
+        List<ProposalLog> results = (List<ProposalLog>)super.getSearchResults(fieldValues);
         String returnLocation = fieldValues.get("backLocation");
+       List<ProposalLog> searchList = filterForPermissions(results);
         if (StringUtils.containsIgnoreCase(returnLocation, "negotiationNegotiation")) {
-            return cleanSearchResultsForNegotiationLookup(results);
+            return cleanSearchResultsForNegotiationLookup(searchList);
         } else {
-            return results;
+            return searchList;
         }
     }
-    
+    protected List<ProposalLog> filterForPermissions(List<ProposalLog> results) {
+        Person user = UserSession.getAuthenticatedUser().getPerson();
+        Map<String, Object> fieldValues = new HashMap<String, Object>();
+        List<ProposalLog> proposalLogs = new ArrayList<ProposalLog>();
+        ProposalLogDocumentAuthorizer authorizer = new ProposalLogDocumentAuthorizer();
+        List<String> proposalIds = new ArrayList<String>();
+        fieldValues.put(KEWPropertyConstants.NAME, getMaintenanceDocumentDictionaryService().getDocumentTypeName(
+                ProposalLog.class));
+        List<Long> docTypeIds = new ArrayList<Long>();
+        for (DocumentType docType : (List<DocumentType>) getBusinessObjectService().findMatching(DocumentType.class, fieldValues)) {
+            docTypeIds.add(docType.getDocumentTypeId());
+        }
+        fieldValues.clear();
+        fieldValues.put(KEWPropertyConstants.DOCUMENT_TYPE_ID, docTypeIds);
+        fieldValues.put(DOC_ROUTE_STATUS, KEWConstants.ROUTE_HEADER_FINAL_CD);
+        List<DocumentRouteHeaderValue> docHeaders = (List<DocumentRouteHeaderValue>) getBusinessObjectService().findMatching(DocumentRouteHeaderValue.class, fieldValues);
+        try {
+            for (DocumentRouteHeaderValue docHeader : docHeaders) {
+                MaintenanceDocumentBase doc = (MaintenanceDocumentBase) documentService.getByDocumentHeaderId(docHeader
+                        .getRouteHeaderId().toString());
+                for(ProposalLog proposalLog : results){
+                    ProposalLog proposalLogDoc = (ProposalLog)doc.getNewMaintainableObject().getBusinessObject();
+                    if(proposalLog.getProposalNumber() != null && proposalLogDoc.getProposalNumber() != null && proposalLog.getProposalNumber().equalsIgnoreCase(proposalLogDoc.getProposalNumber())){
+                        if(!(proposalIds.contains(proposalLog.getProposalNumber()))){
+                            proposalIds.add(proposalLog.getProposalNumber());
+                            if(authorizer.canOpen(doc, user)){
+                                proposalLogs.add(proposalLog);
+                            }
+                        }
+                       
+                    }
+                            
+                }
+                
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        
+        return proposalLogs;
+    }
+        
     private List<ProposalLog> cleanSearchResultsForNegotiationLookup(Collection searchResults) {
         List<ProposalLog> newResults = new ArrayList<ProposalLog>();
         for (ProposalLog pl : (List<ProposalLog>) searchResults) {
