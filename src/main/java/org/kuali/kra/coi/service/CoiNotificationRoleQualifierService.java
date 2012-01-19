659907d15f1b99e01440955ca16cbb0fe4f6cf72
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-01-19 21:44:47 +0000

    KCCOI-98: Implement new notification for Disclosure Certified event (moved services to different packages)

--- src/main/java/org/kuali/kra/coi/service/CoiNotificationRoleQualifierService.java
+++ src/main/java/org/kuali/kra/coi/service/CoiNotificationRoleQualifierService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.coi.notification;
+package org.kuali.kra.coi.service;
 
 import org.kuali.kra.common.notification.service.KcNotificationRoleQualifierService;
 
