2dba4673f932a3c9245c2cbed662edd814582d35
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-01-19 19:44:24 +0000

    KCCOI-98, KCIRB-1664: Remove hardcoded indices

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCIRB-1664_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCIRB-1664_B000.sql
@@ -1,3 +1,3 @@
 INSERT INTO KRIM_ROLE_T (ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, NMSPC_CD, DESC_TXT, KIM_TYP_ID, ACTV_IND, LAST_UPDT_DT)
-             VALUES (1006, SYS_GUID(), 1, 'Committee Administrator', 'KC-COMMITTEE', 'Committee Administrator', (SELECT KIM_TYP_ID FROM KRIM_TYP_T WHERE NM='Unit'), 'Y', SYSDATE)
+             VALUES (KRIM_ROLE_ID_BS_S.NEXTVAL, SYS_GUID(), 1, 'Committee Administrator', 'KC-COMMITTEE', 'Committee Administrator', (SELECT KIM_TYP_ID FROM KRIM_TYP_T WHERE NM='Unit'), 'Y', SYSDATE)
 /
