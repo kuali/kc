4bff6efc766ebd809160a10b5b7d9e55db5a3e5b
Polus Dev1 <polusdev1@polussoftware.com>
2012-01-20 08:51:52 +0000

    commmit after merge

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5175_B000.sql
+++
@@ -1,4 +0,0 @@
-UPDATE KRNS_PARM_T
-SET TXT = 'AWRD=Award;INPR=Institutional Proposal;NGT=Negotiation;PRDV=Proposal Development;PROT=Protocol;SAWD=SubAward'
-where NMSPC_CD = 'KC-GEN' and PARM_NM = 'customAttributeDocumentType'
-/
