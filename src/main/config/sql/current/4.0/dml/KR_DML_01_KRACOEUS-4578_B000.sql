8ddbcc50119a5137fac0f9a14291c0f1ea39c16e
Polus Dev1 <polusdev1@polussoftware.com>
2012-01-20 08:51:52 +0000

    commmit after merge

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-4578_B000.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO KRNS_PARM_T (nmspc_cd, parm_dtl_typ_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, txt, parm_desc_txt, cons_cd)  VALUES ('KC-B','Document','enableSalaryInflationAnniversaryDate',sys_guid(),1,'CONFG','1','Enable the option to calculate the inflation using Salary Anniversary Date','A')
-/
-COMMIT
-/
