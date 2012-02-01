f11574a9d68a28066c12473bc525a15db96391ce
Douglas Pace <dpace@kuali.org>
2012-02-01 21:48:03 +0000

    Removing overly verbose comment.

--- src/main/java/org/kuali/kra/award/contacts/AwardCentralAdminContact.java
+++ src/main/java/org/kuali/kra/award/contacts/AwardCentralAdminContact.java
@@ -16,39 +16,10 @@
 package org.kuali.kra.award.contacts;
 
 /**
- * This class is a complete hack to satisfy the DataDictionary requirements in this DataDictionary method:
- * 
- * public DocumentEntry getDocumentEntry(String documentTypeDDKey ) {
-        if (StringUtils.isBlank(documentTypeDDKey)) {
-            throw new IllegalArgumentException("invalid (blank) documentTypeName");
-        }
-        if ( LOG.isDebugEnabled() ) {
-            LOG.debug("calling getDocumentEntry by documentTypeName '" + documentTypeDDKey + "'");
-        }
-
-        DocumentEntry de = documentEntries.get(documentTypeDDKey);  
-        
-        if ( de == null ) {
-            try {
-                Class clazz = Class.forName( documentTypeDDKey );
-                de = documentEntriesByBusinessObjectClass.get(clazz);
-                if ( de == null ) {
-                    de = documentEntriesByMaintainableClass.get(clazz);
-                }
-            } catch ( ClassNotFoundException ex ) {
-                LOG.warn( "Unable to find document entry for key: " + documentTypeDDKey );
-            }
-        }
-        
-        return de;
-    }
-    
-    Since the Award Central Admin contact type uses the same BO as the Unit Contact type, 
-    the DD can't find the AwardCentralAdminContact DD entry when it does the 
-    de = documentEntriesByBusinessObjectClass.get(clazz) lookup
-    
-    When will the world ever learn that every conceivable problem can't be solved through a centrally managed solution conceived 
-    by a handful of people, even really smart people?
+ * This class is a minor hack to satisfy the DataDictionary requirements.
+ *  Since the Award Central Admin contact type uses the same BO as the Unit Contact type, 
+ *  the DD can't find the AwardCentralAdminContact DD entry when it does  
+ *  de = documentEntriesByBusinessObjectClass.get(clazz) lookup
  */
 public class AwardCentralAdminContact extends AwardUnitContact {
     private static final long serialVersionUID = 2430642816335021874L;
