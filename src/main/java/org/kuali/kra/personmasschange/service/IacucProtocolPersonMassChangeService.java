9c7d318196747dbbdf6634203d4e8eb56e56b0b8
Kristina Taylor <kbtaylor@uw.edu>
2012-02-07 20:18:46 +0000

    KCINFR-436: Updates to PMC for Coeus 4.5 FE

---
+++ src/main/java/org/kuali/kra/personmasschange/service/IacucProtocolPersonMassChangeService.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.personmasschange.service;
+
+import java.util.List;
+
+import org.kuali.kra.iacuc.IacucProtocol;
+import org.kuali.kra.personmasschange.bo.PersonMassChange;
+
+/**
+ * Defines the service interface for performing a Person Mass Change on IACUC Protocols.
+ */
+public interface IacucProtocolPersonMassChangeService {
+    
+    /**
+     * Returns the IACUC Protocols that would have a Person Mass Change performed on them.
+     * 
+     * @param personMassChange the Person Mass Change to be performed
+     * @return the IACUC Protocols that would have a Person Mass Change performed on them
+     */
+    List<IacucProtocol> getIacucProtocolChangeCandidates(PersonMassChange personMassChange);
+    
+    /**
+     * Performs the Person Mass Change on the IACUC Protocols.
+     * 
+     * @param personMassChange the Person Mass Change to be performed
+     * @param iacucProtocolChangeCandidates the IACUC Protocols to perform a Person Mass Change on
+     */
+    void performPersonMassChange(PersonMassChange personMassChange, List<IacucProtocol> iacucProtocolChangeCandidates);
+
+}
