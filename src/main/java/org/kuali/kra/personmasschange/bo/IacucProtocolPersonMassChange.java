5eb340cdf19739257fd73fe156880ee80d803247
Kristina Taylor <kbtaylor@uw.edu>
2012-02-07 21:32:41 +0000

    KCINFR-436: Updates to PMC for Coeus 4.5 FE

---
+++ src/main/java/org/kuali/kra/personmasschange/bo/IacucProtocolPersonMassChange.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.personmasschange.bo;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+public class IacucProtocolPersonMassChange extends KraPersistableBusinessObjectBase {
+
+    private static final long serialVersionUID = 6966837084945864548L;
+    
+    private long protocolPersonMassChangeId;
+    private long personMassChangeId;
+    private boolean investigator;
+    private boolean keyStudyPerson;
+    private boolean correspondents;
+    
+    private PersonMassChange personMassChange;
+
+    public long getProtocolPersonMassChangeId() {
+        return protocolPersonMassChangeId;
+    }
+
+    public void setProtocolPersonMassChangeId(long protocolPersonMassChangeId) {
+        this.protocolPersonMassChangeId = protocolPersonMassChangeId;
+    }
+
+    public long getPersonMassChangeId() {
+        return personMassChangeId;
+    }
+
+    public void setPersonMassChangeId(long personMassChangeId) {
+        this.personMassChangeId = personMassChangeId;
+    }
+
+    public boolean isInvestigator() {
+        return investigator;
+    }
+    
+    public void setInvestigator(boolean investigator) {
+        this.investigator = investigator;
+    }
+    
+    public boolean isKeyStudyPerson() {
+        return keyStudyPerson;
+    }
+    
+    public void setKeyStudyPerson(boolean keyStudyPerson) {
+        this.keyStudyPerson = keyStudyPerson;
+    }
+    
+    public boolean isCorrespondents() {
+        return correspondents;
+    }
+    
+    public void setCorrespondents(boolean correspondents) {
+        this.correspondents = correspondents;
+    }
+    
+    public PersonMassChange getPersonMassChange() {
+        return personMassChange;
+    }
+
+    public void setPersonMassChange(PersonMassChange personMassChange) {
+        this.personMassChange = personMassChange;
+    }
+
+}
