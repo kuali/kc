609fb95ed04aafa4c97ee8c6c4d34abd101e330a
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-02-12 01:33:34 +0000

    KCINFR-464: Fixup help links

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-464_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-464_B000.sql
@@ -1,40 +1,40 @@
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','financialEntityHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/financialentities.htm','Financial Entity Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','financialEntity1Help',SYS_GUID(),1,'HELP','default.htm?turl=Documents/financialentity1.htm','Financial Entity Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','financialEntityNewHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/newfinancialentity.htm','New Financial Entity Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','financialEntityReporterHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/reporter3.htm','Financial Entity Reporter Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','financialEntityMyHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/myfinancialentities.htm','My Financial Entities Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','disclosureReporterHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/reporter1.htm','Disclosure Reporter Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','coiDisclosure1Help',SYS_GUID(),1,'HELP','default.htm?turl=Documents/disclosure1.htm','Disclosure Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','financialEntityUnitHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/unitdetails3.htm','Financial Entity Reporter Unit Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COIDISCLOSURE','Document','financialEntityRelationshipHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/relationshipdetails.htm','Relationship Details Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-PROTOCOL','Document','protocolAddPersonnelHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/addpersonnel.htm','Add Personnel Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-PROTOCOL','Document','protocolActionCopyHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/copytonewdocument1.htm','Copy Protocol Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COMMITTEE','Document','committeeAddScheduleHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/addtoschedule.htm','Add To Schedule Help','A','KC')
 /
-INSERT INTO KRCR_PARM_T (appl_id, nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd)
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-COMMITTEE','Document','committeeSchedule2Help',SYS_GUID(),1,'HELP','default.htm?turl=Documents/schedule2.htm','Schedule Help','A','KC')
 /
 UPDATE KRCR_PARM_T SET VAL='default.htm?turl=Documents/committee1.htm' WHERE PARM_NM='disclosureCommitteeHelp'
