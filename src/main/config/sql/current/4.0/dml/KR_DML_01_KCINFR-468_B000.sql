5bf82f14e1eb83b667593c81a29d66c5ef3b73f2
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-02-14 17:02:55 +0000

    KCINFR-468: Fixup help links

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-468_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-468_B000.sql
@@ -1,5 +1,5 @@
 INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
-VALUES ('KC-UNT','Document','financialEntityHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/unithierarchy.htm','Unit Hierarchy Help','A','KC')
+VALUES ('KC-UNT','Document','unitHierarchyHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/unithierarchy.htm','Unit Hierarchy Help','A','KC')
 /
 COMMIT
 /
