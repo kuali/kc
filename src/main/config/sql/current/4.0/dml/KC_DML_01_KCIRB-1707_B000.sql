ae6c3e20d0d25a8ec7c0b26997b91927872954cb
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-02-21 02:12:42 +0000

    KCIRB-1707: Changes to Watermark maint docs are not saved, nor do they have reasonable defaults.

---
+++ src/main/config/sql/current/4.0/dml/KC_DML_01_KCIRB-1707_B000.sql
@@ -0,0 +1,10 @@
+UPDATE WATERMARK SET position='HEADER'
+/
+UPDATE WATERMARK SET position_font='25'
+/
+UPDATE WATERMARK SET alignment='CENTER'
+/
+UPDATE WATERMARK SET watermark_text='Deferred' where watermark_text='Defered'
+/
+commit
+/
