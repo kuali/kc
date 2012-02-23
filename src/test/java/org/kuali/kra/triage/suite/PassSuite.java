eef3f2b01860091d7486d2d274bb1e0131324160
Darius Robinson <drobinson@rsmart.com>
2012-02-23 15:58:19 +0000

    Temporarily Removing AddManualProjectRuleTest until it can be completely reworked

--- src/test/java/org/kuali/kra/triage/suite/PassSuite.java
+++ src/test/java/org/kuali/kra/triage/suite/PassSuite.java
@@ -446,7 +446,6 @@ import org.junit.runners.Suite.SuiteClasses;
     org.kuali.kra.coi.personfinancialentity.FinEntitiesDataGroupMaintenanceDocumentRuleTest.class,
     org.kuali.kra.coi.personfinancialentity.FinEntitiesDataMatrixMaintenanceDocumentRuleTest.class,
     org.kuali.kra.coi.personfinancialentity.SaveFinancialEntityRuleTest.class,
-    org.kuali.kra.coi.disclosure.AddManualProjectRuleTest.class,
     org.kuali.kra.coi.disclosure.CoiDisclosureServiceTest.class,
     org.kuali.kra.coi.actions.CoiDisclosureActionServiceTest.class,
     org.kuali.kra.negotiations.rules.NegotiationActivityRuleTest.class,
