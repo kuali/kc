77ded0b59c6c22977ff43134ebd6cb35b032b074
Ted Standley <hwstandley@ucdavis.edu>
2012-02-23 00:02:04 +0000

    KCINFR-490: correct syntax errors

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-490_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-490_B000.sql
@@ -1,27 +1,27 @@
 INSERT INTO KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalDevelopmentperformancesitelocationsHelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/performancesitelocations.htm','PD Performance site locations Document Help','A','KC')
+VALUES ('KC-PD','Document','proposalDevelopmentperformancesitelocationsHelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/performancesitelocations.htm','PD Performance site locations Document Help','A','KC')
 /
 INSERT INTO KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalDevelopmentpersonnelattachmentsHelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/personnelattachments.htm','PD Personnel Attachment Document Help','A','KC')
+VALUES ('KC-PD','Document','proposalDevelopmentpersonnelattachmentsHelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/personnelattachments.htm','PD Personnel Attachment Document Help','A','KC')
 /
 INSERT INTO KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalDevelopmentinternalattachmentsHelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/internalattachments.htm','PD Internal Attachment Document Help','A','KC')
+VALUES ('KC-PD','Document','proposalDevelopmentinternalattachmentsHelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/internalattachments.htm','PD Internal Attachment Document Help','A','KC')
 /
 INSERT INTO KRCR_PARM_TKRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalDevelopmentnotesHelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/notes.htm','PD Notes Document Help','A','KC')
+VALUES ('KC-PD','Document','proposalDevelopmentnotesHelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/notes.htm','PD Notes Document Help','A','KC')
 /
 INSERT INTO KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalHierarchyHelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/proposalhierarchy.htm','Proposal Hierarchy Document Help','A','KC')
+VALUES ('KC-PD','Document','proposalHierarchyHelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/proposalhierarchy.htm','Proposal Hierarchy Document Help','A','KC')
 /
 INSERT INTO KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalDevelopmentDataValidation1HelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/datavalidation1.htm','PD Data Validation Help','A','KC')
+VALUES ('KC-PD','Document','proposalDevelopmentDataValidation1HelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/datavalidation1.htm','PD Data Validation Help','A','KC')
 /
 INSERT INTO KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalDevelopmentPrintFormsHelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/print.htm','PD Print Forms Help','A','KC')
+VALUES ('KC-PD','Document','proposalDevelopmentPrintFormsHelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/print.htm','PD Print Forms Help','A','KC')
 /
 INSERT INTO KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalDevelopmentCopyCriteriaHelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/copytonewdocument.htm','Proposal Copy Criteria Help','A','KC')
+VALUES ('KC-PD','Document','proposalDevelopmentCopyCriteriaHelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/copytonewdocument.htm','Proposal Copy Criteria Help','A','KC')
 /
 INSERT INTO KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) 
-VALUES ('KC-PD','Document','proposalDevelopmentMedusaHelpUrl',sys_guid()(),1,'HELP','default.htm?turl=Documents/medusa.htm','PD medusa Document Help','A','KC')
+VALUES ('KC-PD','Document','proposalDevelopmentMedusaHelpUrl',SYS_GUID(),1,'HELP','default.htm?turl=Documents/medusa.htm','PD medusa Document Help','A','KC')
 /
