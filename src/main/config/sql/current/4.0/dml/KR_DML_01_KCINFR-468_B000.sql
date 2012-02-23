64e581ed393b886f341935e8ab1df0103145971e
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-02-23 20:35:37 +0000

    KCINFR-468: Fixup help links

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-468_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-468_B000.sql
@@ -4,5 +4,8 @@ VALUES ('KC-UNT','Document','unitHierarchyHelp',SYS_GUID(),1,'HELP','default.htm
 INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
 VALUES ('KC-M','Document','batchCorrespondenceHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/batchcorrespondence1.htm','Batch Correspondence Help','A','KC')
 /
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
+VALUES ('KC-M','Document','researchAreaHelp',SYS_GUID(),1,'HELP','default.htm?turl=Documents/researchareas.htm','Research Areas Help','A','KC')
+/
 COMMIT
 /
