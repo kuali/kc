7ca78cc29ee7dca3cef241661b82dad90f9ee17d
Kristina Taylor <kbtaylor@uw.edu>
2012-02-24 18:00:18 +0000

    KCINFR-461: Adding delete links to all maintenance documents, fixing file locations

--- src/main/java/org/kuali/kra/common/notification/NotificationLookupableHelperServiceImpl.java
+++ src/main/java/org/kuali/kra/common/notification/NotificationLookupableHelperServiceImpl.java
@@ -49,18 +49,20 @@ public class NotificationLookupableHelperServiceImpl extends KualiLookupableHelp
         boolean hasModifyPermission = getNotificationAuthorizationService().hasPermission(PermissionConstants.MODIFY_NOTIFICATION);
         boolean hasViewPermission = hasModifyPermission || getNotificationAuthorizationService().hasPermission(PermissionConstants.VIEW_NOTIFICATION);
         if (hasModifyPermission) {
-            AnchorHtmlData htmlData = getUrlData(businessObject, KRADConstants.MAINTENANCE_EDIT_METHOD_TO_CALL, pkNames);
-            htmlDataList.add(htmlData);
+            AnchorHtmlData editHtmlData = getUrlData(businessObject, KRADConstants.MAINTENANCE_EDIT_METHOD_TO_CALL, pkNames);
+            htmlDataList.add(editHtmlData);
 
-            AnchorHtmlData htmlData1 = getUrlData(businessObject, KRADConstants.MAINTENANCE_COPY_METHOD_TO_CALL, pkNames);
-            htmlDataList.add(htmlData1);
+            AnchorHtmlData copyHtmlData = getUrlData(businessObject, KRADConstants.MAINTENANCE_COPY_METHOD_TO_CALL, pkNames);
+            htmlDataList.add(copyHtmlData);
+            
+            AnchorHtmlData deleteHtmlData = getUrlData(businessObject, KRADConstants.MAINTENANCE_DELETE_METHOD_TO_CALL, pkNames);
+            htmlDataList.add(deleteHtmlData);
         } 
         // if user can view question, then if doc number exists, use doc service to view, otherwise open for editing in read-only mode
         if (hasViewPermission) {
-            AnchorHtmlData htmlData2 = new AnchorHtmlData();
-            htmlData2 = getUrlData(businessObject, KRADConstants.MAINTENANCE_EDIT_METHOD_TO_CALL, pkNames);
-            htmlData2.setDisplayText(VIEW);
-            htmlDataList.add(htmlData2);
+            AnchorHtmlData viewHtmlData = getUrlData(businessObject, KRADConstants.MAINTENANCE_EDIT_METHOD_TO_CALL, pkNames);
+            viewHtmlData.setDisplayText(VIEW);
+            htmlDataList.add(viewHtmlData);
         }
         return htmlDataList;
     }
