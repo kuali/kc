cd421d724475985ced37d0e090fab4bd45c43aae
Ted Standley <hwstandley@ucdavis.edu>
2012-02-24 21:56:25 +0000

    KCINFR-493: integrity constraints

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-493_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCINFR-493_B000.sql
@@ -5,8 +5,12 @@ WHERE PARM_NM = 'subAwardHomeHelpUrl'
  AND CMPNT_CD = 'Document' 
  AND NMSPC_CD = 'KC-SUBAWARD'
 /
-INSERT INTO KRCR_PARM_T (APPL_ID,CMPNT_CD,EVAL_OPRTR_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_NM,PARM_TYP_CD,VAL,VER_NBR)
-  VALUES ('KC','Document','A','KC-SUBAWARD',SYS_GUID(),'Subaward Actions Help','subAwardActionsHelpUrl','HELP','default.htm?turl=Documents/subawardactions.htm',1)
+UPDATE KRCR_PARM_T
+SET VAL = 'default.htm?turl=Documents/subawardactions.htm',
+     PARM_DESC_TXT = 'Subaward Actions Help'
+WHERE PARM_NM = 'subAwardActionsHelpUrl'
+ AND CMPNT_CD = 'Document' 
+ AND NMSPC_CD = 'KC-SUBAWARD'
 /
 INSERT INTO KRCR_PARM_T (APPL_ID,CMPNT_CD,EVAL_OPRTR_CD,NMSPC_CD,OBJ_ID,PARM_DESC_TXT,PARM_NM,PARM_TYP_CD,VAL,VER_NBR)
   VALUES ('KC','Document','A','KC-SUBAWARD',SYS_GUID(),'Subaward Financial Help','subAwardFinancialHelpUrl','HELP','default.htm?turl=Documents/financial1.htm',1)
