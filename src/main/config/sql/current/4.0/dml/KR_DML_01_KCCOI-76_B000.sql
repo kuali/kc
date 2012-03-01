d40a9d96e55676f7e46d72d893eb63622bdc8065
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-03-01 19:58:24 +0000

    KCCOI-76: Add/correct help links for COI

---
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KCCOI-76_B000.sql
@@ -0,0 +1,9 @@
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
+VALUES ('KC-COIDISCLOSURE', 'Document', 'coiDisclosureReviewerHelp', SYS_GUID(), 1, 'HELP', 'default.htm?turl=Documents/revieweractions2.htm','Disclosure Reviewer Actions Help', 'A', 'KC')
+/
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
+VALUES ('KC-COIDISCLOSURE', 'Document', 'coiDisclosuremanualHelp', SYS_GUID(), 1, 'HELP', 'default.htm?turl=Documents/manualevent.htm','Manual Event and Financial Entities Help', 'A', 'KC')
+/
+INSERT INTO KRCR_PARM_T (nmspc_cd, cmpnt_cd, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
+VALUES ('KC-COIDISCLOSURE', 'Document', 'coiAdministratorActionHelp', SYS_GUID(), 1, 'HELP', 'default.htm?turl=Documents/administratoraction2.htm','Administrator Actions Help', 'A', 'KC')
+/
