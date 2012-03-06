eb04d2759a98c03e647092f1e25a7b82fd4c1def
Douglas Pace <dpace@kuali.org>
2012-03-06 00:45:45 +0000

    KRACOEUS-5369 : Fixing js error causing proposal hierarchy to fail.

--- src/main/webapp/scripts/proposalHierarchy.js
+++ src/main/webapp/scripts/proposalHierarchy.js
@@ -1,22 +1,22 @@
-$(document).ready( function() {
+jq(document).ready( function() {
 
-	$("input[name*=toggleTab]").click( function() {
-		var tabBody = $(this).parents("table.tab").next().children(".tab-container");
+	jq("input[name*=toggleTab]").click( function() {
+		var tabBody = jq(this).parents("table.tab").next().children(".tab-container");
 		if (this.title.indexOf('close')==0 && tabBody.html().indexOf('<!-- Summary Not Loaded -->')>-1) {			
 			var proposalNumber = tabBody.attr("id").split("-")[1];
 			tabBody.html('Summary Loading <img src="static/images/jquery/ajax-loader.gif" alt="Summary Loading"/>');
-			$.ajax({
+			jq.ajax({
 				url: "proposalDevelopmentHierarchy.do",
 				data: "methodToCall=loadProposalSummary&proposalNumberToSummarize="+proposalNumber,
 				type: "POST",
 				success: function(proposalHtml) {
 					tabBody.html(proposalHtml);
 					tabBody.find("div#budgetSummaries").find("input[name*=toggleTab]").click( function() {
-						var innerTabBody = $(this).parent().next().children(".innerTab-container");
+						var innerTabBody = jq(this).parent().next().children(".innerTab-container");
 						if (this.title.indexOf('close')==0 && innerTabBody.html().indexOf('<!-- Summary Not Loaded -->')>-1) {
 							var budgetNumber = innerTabBody.attr("id").split("-")[1];
 							innerTabBody.html('<table cellpadding=0 cellspacing=0 summary=""><tr><th width="100%">Summary Loading <img src="static/images/jquery/ajax-loader.gif" alt="Summary Loading"/></th></tr></table>');
-							$.ajax({
+							jq.ajax({
 								url: "proposalDevelopmentHierarchy.do",
 								data: "methodToCall=loadBudgetSummary&budgetNumberToSummarize="+budgetNumber,
 								type: "POST",
