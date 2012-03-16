6a3f48ae11ba8685ed3203bc1e7fea8fdf11e83c
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-03-16 01:56:04 +0000

    KCIRB-1770: Implement Protocol Suspended by DSMB notification

---
+++ src/main/java/org/kuali/kra/irb/actions/notification/ProtocolSuspendedByDSMBNotificationRenderer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.irb.actions.notification;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import org.kuali.kra.irb.Protocol;
+import org.kuali.kra.irb.notification.IRBNotificationRenderer;
+
+/**
+ * Renders additional fields for the Protocol Suspended by DSMB notification.
+ */
+public class ProtocolSuspendedByDSMBNotificationRenderer extends IRBNotificationRenderer {
+
+    private static final long serialVersionUID = 4288325928624683600L;
+
+    /**
+     * Constructs a Protocol Suspended by DSMB notification renderer.
+     * @param protocol
+     * @param actionComments
+     */
+    public ProtocolSuspendedByDSMBNotificationRenderer(Protocol protocol) {
+        super(protocol);
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see org.kuali.kra.common.notification.NotificationContext#replaceContextVariables(java.lang.String)
+     */
+    @Override
+    public Map<String, String> getDefaultReplacementParameters() {
+        Map<String, String> params = super.getDefaultReplacementParameters();
+        params.put("{SUSPENDED_DATE}", (new SimpleDateFormat("d'-'MMM'-'yyyy")).format(new Date()));
+        return params;
+    }
+}
