7cc4c4821c46cb9ba4b1ff05fd3cf6a4e79a4b9c
Vineeth A V <vineeth@polussoftware.com>
2012-03-19 14:51:08 +0000

    KRACOEUS-5418

---
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5418_B000.sql
@@ -0,0 +1,5 @@
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','s2sBudgetCategoryCodePersonnel',SYS_GUID(),1,'CONFG','1','Code corresponding to budget category code personnel','A','KC')
+/
+commit
+/
