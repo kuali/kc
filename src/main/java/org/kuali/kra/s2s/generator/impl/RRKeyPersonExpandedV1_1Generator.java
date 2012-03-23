b04fc874a997644d9d8a4f159b963f5acf16c8b8
Neethu Jose <neethu@polussoftware.com>
2012-03-23 16:33:04 +0000

    KRACOEUS-5242 , KRACOEUS-5348

--- src/main/java/org/kuali/kra/s2s/generator/impl/RRKeyPersonExpandedV1_1Generator.java
+++ src/main/java/org/kuali/kra/s2s/generator/impl/RRKeyPersonExpandedV1_1Generator.java
@@ -31,12 +31,15 @@ import java.util.Collections;
 import java.util.List;
 
 import org.apache.xmlbeans.XmlObject;
+import org.kuali.kra.bo.KcPerson;
 import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.proposaldevelopment.bo.DevelopmentProposal;
 import org.kuali.kra.proposaldevelopment.bo.Narrative;
 import org.kuali.kra.proposaldevelopment.bo.ProposalPerson;
 import org.kuali.kra.proposaldevelopment.bo.ProposalPersonComparator;
 import org.kuali.kra.proposaldevelopment.document.ProposalDevelopmentDocument;
 import org.kuali.kra.s2s.util.S2SConstants;
+import org.kuali.kra.service.KcPersonService;
 import org.kuali.kra.service.SponsorService;
 
 /**
@@ -162,20 +165,18 @@ public class RRKeyPersonExpandedV1_1Generator extends
 						.getApplicantOrganization().getOrganization()
 						.getOrganizationName());
 			}
-			String departmentName = null;
-			if (pdDoc.getDevelopmentProposal().getOwnedByUnit() != null) {
-				departmentName = pdDoc.getDevelopmentProposal()
-						.getOwnedByUnit().getUnitName();
-				if (departmentName != null) {
-					if (departmentName.length() > DEPARTMENT_NAME_MAX_LENGTH) {
-						profile.setDepartmentName(departmentName.substring(0,
-								DEPARTMENT_NAME_MAX_LENGTH));
-					} else {
-						profile.setDepartmentName(departmentName);
-					}
-				}
-			}
-			String divisionName = s2sUtilService.getDivisionName(pdDoc);
+			if(PI.getHomeUnit() != null) {
+                KcPersonService kcPersonService = KraServiceLocator.getService(KcPersonService.class);
+                KcPerson kcPersons = kcPersonService.getKcPersonByPersonId(PI.getPersonId());
+                String departmentName =  kcPersons.getOrganizationIdentifier();
+                profile.setDepartmentName(departmentName);
+            }
+            else
+            {
+                DevelopmentProposal developmentProposal = pdDoc.getDevelopmentProposal();
+                profile.setDepartmentName(developmentProposal.getOwnedByUnit().getUnitName());
+            }
+			String divisionName = PI.getDivision();
 			if (divisionName != null) {
 				profile.setDivisionName(divisionName);
 			}
@@ -264,21 +265,18 @@ public class RRKeyPersonExpandedV1_1Generator extends
 							.getApplicantOrganization().getOrganization()
 							.getOrganizationName());
 				}
-				String departmentName = null;
-				if (pdDoc.getDevelopmentProposal().getOwnedByUnit() != null) {
-					departmentName = pdDoc.getDevelopmentProposal()
-							.getOwnedByUnit().getUnitName();
-					if (departmentName != null) {
-						if (departmentName.length() > DEPARTMENT_NAME_MAX_LENGTH) {
-							profileKeyPerson.setDepartmentName(departmentName
-									.substring(0,
-											DEPARTMENT_NAME_MAX_LENGTH - 1));
-						} else {
-							profileKeyPerson.setDepartmentName(departmentName);
-						}
-					}
-				}
-				String divisionName = s2sUtilService.getDivisionName(pdDoc);
+				if(keyPerson.getHomeUnit() != null) {
+                    KcPersonService kcPersonService = KraServiceLocator.getService(KcPersonService.class);
+                    KcPerson kcPersons = kcPersonService.getKcPersonByPersonId(keyPerson.getPersonId());
+                    String departmentName =  kcPersons.getOrganizationIdentifier();
+                    profileKeyPerson.setDepartmentName(departmentName);
+                }
+                else
+                {
+                    DevelopmentProposal developmentProposal = pdDoc.getDevelopmentProposal();
+                    profileKeyPerson.setDepartmentName(developmentProposal.getOwnedByUnit().getUnitName());
+                }
+				String divisionName = keyPerson.getDivision();
 				if (divisionName != null) {
 					profileKeyPerson.setDivisionName(divisionName);
 				}
