c271a4ab49fc9f0e9e56d521c49e5ca87ad0c245
Kristina Taylor <kbtaylor@uw.edu>
2012-03-29 21:45:47 +0000

    KC Release 4.0 Script Corrections

--- src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5238_B000.sql
+++ src/main/config/sql/current/4.0/dml/KR_DML_01_KRACOEUS-5238_B000.sql
@@ -1,14 +1,14 @@
 INSERT INTO KRIM_ROLE_T (ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, DESC_TXT, NMSPC_CD, KIM_TYP_ID, ACTV_IND, LAST_UPDT_DT) 
-VALUES (KRIM_ROLE_ID_BS_S.nextval, SYS_GUID(), 1, 'Access_Proposal_Person_Institutional_Salaries', 'Access Proposal Person Institutional Salaries', 'KC-PD', (SELECT KIM_TYP_ID FROM KRIM_TYP_T WHERE NMSPC_CD = 'KC-SYS' AND NM ='Unit'), 'Y', SYSDATE)
+VALUES (KRIM_ROLE_ID_BS_S.NEXTVAL, SYS_GUID(), 1, 'Access_Proposal_Person_Institutional_Salaries', 'Access Proposal Person Institutional Salaries', 'KC-PD', (SELECT KIM_TYP_ID FROM KRIM_TYP_T WHERE NMSPC_CD = 'KC-SYS' AND NM ='Unit'), 'Y', SYSDATE)
 /
 INSERT INTO KRIM_ROLE_T (ROLE_ID, OBJ_ID, VER_NBR, ROLE_NM, DESC_TXT, NMSPC_CD, KIM_TYP_ID, ACTV_IND, LAST_UPDT_DT) 
-	VALUES (KRIM_ROLE_ID_BS_S.nextval, SYS_GUID(), 1, 'View Institutionally Maintained Salaries', 
+	VALUES (KRIM_ROLE_ID_BS_S.NEXTVAL, SYS_GUID(), 1, 'View Institutionally Maintained Salaries', 
 			'View Institutionally Maintained Salaries', 'KC-PD', 
 			(SELECT KIM_TYP_ID FROM KRIM_TYP_T WHERE NMSPC_CD = 'KC-SYS' AND NM ='Unit'), 
 			'Y', SYSDATE)
 /
 INSERT INTO KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND) 
-	VALUES (KRIM_PERM_ID_BS_S.nextval, SYS_GUID(), 1, 
+	VALUES (KRIM_PERM_ID_BS_S.NEXTVAL, SYS_GUID(), 1, 
 			(select perm_tmpl_id from KRIM_PERM_T where nm='Modify Budget'), 
 			'KC-PD', 'VIEW_INSTITUTIONAL_SALARIES', 'VIEW INSTITUTIONAL BUDGET SALARIES', 'Y')
 /
@@ -27,7 +27,7 @@ INSERT INTO KRIM_PERM_ATTR_DATA_T (ATTR_DATA_ID, OBJ_ID, VER_NBR, PERM_ID, KIM_T
          'ProposalDevelopmentDocument')
 /
 INSERT INTO KRIM_PERM_T (PERM_ID, OBJ_ID, VER_NBR, PERM_TMPL_ID, NMSPC_CD, NM, DESC_TXT, ACTV_IND) 
-	VALUES (KRIM_PERM_ID_BS_S.nextval, SYS_GUID(), 1, 
+	VALUES (KRIM_PERM_ID_BS_S.NEXTVAL, SYS_GUID(), 1, 
 			(select perm_tmpl_id from KRIM_PERM_T where nm='Modify Budget'), 
 			'KC-PD', 'VIEW_PROP_PERSON_INST_SALARIES', 'VIEW PROPOSAL PERSON INST SALARIES', 'Y')
 /
