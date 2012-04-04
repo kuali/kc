f34330ab030510854f4aa5193a8b4aac06dffc00
Allen Akers <allen.akers@colostate.edu>
2012-04-04 13:43:56 +0000

    Merging 4.0 branch changes to trunk

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-4_0-SCRIPT/KC_RICE-RELEASE-4_0-Upgrade-MYSQL.sql
@@ -0,0 +1,3 @@
+\. ./../../current_mysql/4.0/rice/KC_RICE_01_QUESTION_B000.sql
+commit;
+exit
--- src/main/config/sql/RELEASE-SCRIPTS/KC_Install.sh
+++ src/main/config/sql/RELEASE-SCRIPTS/KC_Install.sh
@@ -46,7 +46,7 @@ fi
 
 dbtype=`getChoice 'Enter Database Type' ORACLE MYSQL`
 
-version=`getChoice 'Enter Currently Installed Version' NEW 3.0 3.0.1 3.1 3.1.1`
+version=`getChoice 'Enter Currently Installed Version' NEW 3.0 3.0.1 3.1 3.1.1 3.2`
 
 un=`getAnswer 'Enter KC Database Username'`
 
@@ -189,7 +189,22 @@ case "${dbtype}" in
             sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < KR-RELEASE-3_2-Upgrade-ORACLE.sql
             mv *.log ../LOGS/
             cd ..
-        fi  
+        fi
+        
+        if [ "${version}" = "3.2" ] || [ "${version}" = "3.1.1" ] || [ "${version}" = "3.1" ] || [ "${version}" = "3.0.1" ] || [ "${version}" = "3.0" ] || [ "${version}" = "NEW" ]
+        then
+            cd KC-RELEASE-4_0-SCRIPT
+            sqlplus "${un}"/"${pw}${DBSvrNm}" < KRC_RICE-RELEASE-4_0-Upgrade-ORACLE.sql
+            sqlplus "${un}"/"${pw}${DBSvrNm}" < KC_RICE-RELEASE-4_0-Upgrade-ORACLE.sql
+            if [ "${InstRice}" = "Y" ] || [ "${mode}" = "BUNDLE" ]
+            then
+                sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < KR_RICE-RELEASE-4_0-Upgrade-ORACLE.sql
+            fi
+            sqlplus "${un}"/"${pw}${DBSvrNm}" < KC-RELEASE-4_0-Upgrade-ORACLE.sql
+            sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < KR-RELEASE-4_0-Upgrade-ORACLE.sql
+            mv *.log ../LOGS/
+            cd ..
+        fi 
 		
 		cd KC-RELEASE-3_0-CLEAN/oracle
 		sqlplus "${Riceun}"/"${Ricepw}${RiceDBSvrNm}" < krrelease/datasql/KR_00_CLEAN_SEQ_BS.sql
@@ -287,6 +302,21 @@ case "${dbtype}" in
             cd KC-RELEASE-3_2-SCRIPT
             mysql -u ${un} -p${pw} -D ${DBSvrNm} -s -f < KC-RELEASE-3_2-Upgrade-MYSQL.sql > KC-RELEASE-3_2-Upgrade-MYSQL-Install.log 2>&1
             mysql -u ${Riceun} -p${Ricepw} -D ${RiceDBSvrNm} -s -f < KR-RELEASE-3_2-Upgrade-MYSQL.sql > KR-RELEASE-3_2-Upgrade-MYSQL-Install.log 2>&1
+            mv *.log ../LOGS/
+            cd ..
+        fi
+        
+        if [ "${version}" = "3.2" ] || [ "${version}" = "3.1.1" ] || [ "${version}" = "3.1" ] || [ "${version}" = "3.0.1" ] || [ "${version}" = "3.0" ] || [ "${version}" = "NEW" ]
+        then
+            cd KC-RELEASE-4_0-SCRIPT
+            mysql -u ${un} -p${pw} -D ${DBSvrNm} -s -f < KRC_RICE-RELEASE-4_0-Upgrade-MYSQL.sql > KRC_RICE-RELEASE-4_0-Upgrade-MYSQL-Install.log 2>&1
+            mysql -u ${un} -p${pw} -D ${DBSvrNm} -s -f < KC_RICE-RELEASE-4_0-Upgrade-MYSQL.sql > KC_RICE-RELEASE-4_0-Upgrade-MYSQL-Install.log 2>&1
+            if [ "${InstRice}" = "Y" ] || [ "${mode}" = "BUNDLE" ]
+            then
+                mysql -u ${Riceun} -p${Ricepw} -D ${RiceDBSvrNm} -s -f < KR_RICE-RELEASE-4_0-Upgrade-MYSQL.sql > KR_RICE-RELEASE-4_0-Upgrade-MYSQL-Install.log 2>&1
+            fi
+            mysql -u ${un} -p${pw} -D ${DBSvrNm} -s -f < KC-RELEASE-4_0-Upgrade-MYSQL.sql > KC-RELEASE-4_0-Upgrade-MYSQL-Install.log 2>&1
+            mysql -u ${Riceun} -p${Ricepw} -D ${RiceDBSvrNm} -s -f < KR-RELEASE-4_0-Upgrade-MYSQL.sql > KR-RELEASE-4_0-Upgrade-MYSQL-Install.log 2>&1
             mv *.log ../LOGS/
             cd ..
         fi
