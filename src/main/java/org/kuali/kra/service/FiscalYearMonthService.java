526933d649c94296a4de82f54f1f8bd9a6edc663
Jay Hulslander <jdh34@cornell.edu>
2012-04-06 17:49:47 +0000

    KRACOEUS-4757

---
+++ src/main/java/org/kuali/kra/service/FiscalYearMonthService.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.service;
+
+/**
+ * 
+ * This class...
+ */
+public interface FiscalYearMonthService {
+    
+    /**
+     * 
+     * This method returns the Integer value of the current month in the fiscal year.  January is 0, December is 11.
+     * @return
+     */
+    Integer getCurrentFiscalMonth();
+    
+    /**
+     * 
+     * This method returns the Integer value of the current month in the fiscal year.  January is 1, December is 12.
+     * @return
+     */
+    Integer getCurrentFiscalMonthForDisplay();
+    
+    /**
+     * 
+     * This method returns the current fiscal year.
+     * @return
+     */
+    Integer getCurrentFiscalYear();
+    
+}
