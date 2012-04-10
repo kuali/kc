67e283a6f478989892dff86e90ac6be8f7886648
Douglas Pace <dpace@kuali.org>
2012-04-10 23:36:48 +0000

    KRACOEUS-4374, KRACOEUS-4365 : Auto submit to sponsor on final approval of prop dev is the approving user has the appropriate permissions.

---
+++ src/main/config/sql/current/5.0/dml/KC_DML_01_KRACOEUS-4374_B000.sql
@@ -0,0 +1,3 @@
+INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+VALUES ('KC-PD', 'Document', 'autoSubmitToSponsorOnFinalApproval', SYS_GUID(), 1, 'CONFG', 'Y', 'This value determines whether the proposal will automatically submit to sponsor when the person performing the final approval also has the appropriate permissions.', 'A', 'KC')
+/
