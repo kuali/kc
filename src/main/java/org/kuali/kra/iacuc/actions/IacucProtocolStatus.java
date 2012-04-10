d8750f9974970c82ccdbccae5f5b9f9c95d47436
Lin-Long Shyu <lshyukuali@gmail.com>
2012-04-10 16:02:33 +0000

    kciac-48

---
+++ src/main/java/org/kuali/kra/iacuc/actions/IacucProtocolStatus.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.iacuc.actions;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+public class IacucProtocolStatus extends KraPersistableBusinessObjectBase { 
+    
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = -3782529092395093228L;
+    private Integer protocolStatusCode; 
+    private String description; 
+        
+    public IacucProtocolStatus() { 
+
+    } 
+    
+    public Integer getProtocolStatusCode() {
+        return protocolStatusCode;
+    }
+
+    public void setProtocolStatusCode(Integer protocolStatusCode) {
+        this.protocolStatusCode = protocolStatusCode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+}
