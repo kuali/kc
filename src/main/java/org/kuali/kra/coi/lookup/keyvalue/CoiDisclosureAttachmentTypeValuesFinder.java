273669d5be4f61f9acb5724550db29e6d9ac4a6f
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-04-20 17:29:41 +0000

    KCCOI-242: Link COI Attachment Type to Disclosures

---
+++ src/main/java/org/kuali/kra/coi/lookup/keyvalue/CoiDisclosureAttachmentTypeValuesFinder.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.coi.lookup.keyvalue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.kuali.kra.coi.CoiAttachmentType;
+import org.kuali.kra.coi.CoiDisclosureForm;
+import org.kuali.kra.coi.personfinancialentity.FinancialEntityService;
+import org.kuali.kra.coi.personfinancialentity.PersonFinIntDisclosure;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.rice.core.api.util.ConcreteKeyValue;
+import org.kuali.rice.core.api.util.KeyValue;
+import org.kuali.rice.kns.util.KNSGlobalVariables;
+import org.kuali.rice.krad.keyvalues.KeyValuesBase;
+import org.kuali.rice.krad.service.KeyValuesService;
+import org.kuali.rice.krad.util.GlobalVariables;
+
+public class CoiDisclosureAttachmentTypeValuesFinder extends KeyValuesBase {
+
+    private static KeyValuesService keyValuesService;
+
+    /*
+     * get list of COI attachment types
+     */
+    public List<KeyValue> getKeyValues() {
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        Collection<CoiAttachmentType> cdaTypes = this.getKeyValuesService().findAll(CoiAttachmentType.class);
+        for (CoiAttachmentType cdaType : cdaTypes) {
+            keyValues.add(new ConcreteKeyValue(cdaType.getCode(), cdaType.getDescription())); 
+        }
+        return keyValues;
+    }
+
+    /**
+     * 
+     * This method returns an instance of CommitteeService.
+     * @return KeyValuesService
+     */
+    public KeyValuesService getKeyValuesService() {
+        if (keyValuesService == null) {
+            keyValuesService = KraServiceLocator.getService(KeyValuesService.class);
+        }
+        return keyValuesService;
+    }
+
+}
