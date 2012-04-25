d9c38c8a6aa56b303e858e4b681e868a2db29750
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-04-25 00:45:26 +0000

    KCIAC-16: Implement Questionnaire functionality in IACUC - lots of commits

--- src/main/config/sql/current/5.0/dml/KR_DML_02_KCIAC-16_B000.sql
+++ src/main/config/sql/current/5.0/dml/KR_DML_02_KCIAC-16_B000.sql
@@ -1,3 +1,6 @@
+UPDATE KRCE_PARM_T SET VAL='Maintain Questionnaire Usage:KC-IACUC;Modify Protocol:KC-PROTOCOL;Maintain Questionnaire Usage:KC-PD;Maintain Questionnaire Usage:KC-PROTOCOL;Edit Institutional Proposal:KC-IP;Maintain Coi Disclosure:KC-COIDISCLOSURE'
+WHERE NMSPC_CD='KC-QUESTIONNAIRE' and CMPNT_CD='P'
+/
 INSERT INTO KRIM_PERM_T (PERM_ID,OBJ_ID,VER_NBR,PERM_TMPL_ID,NMSPC_CD,NM,DESC_TXT,ACTV_IND)
 VALUES (KRIM_PERM_ID_BS_S.nextval,SYS_GUID(),1,(SELECT PERM_TMPL_ID from KRIM_PERM_TMPL_T WHERE NMSPC_CD='KC-IDM' AND NM='Questionnaire Permission'),'KC-IACUC','Maintain Questionnaire Usage','Add/Edit Questionnaire Usages for IACUC Protocol','Y')
 /
