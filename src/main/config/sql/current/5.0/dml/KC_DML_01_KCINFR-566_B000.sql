2d6e4aa3241eff27c127ba7d93789d04aca9cb47
Kristina Taylor <kbtaylor@uw.edu>
2012-05-02 21:16:51 +0000

    Correcting scripts

--- src/main/config/sql/current/5.0/dml/KC_DML_01_KCINFR-566_B000.sql
+++ src/main/config/sql/current/5.0/dml/KC_DML_01_KCINFR-566_B000.sql
@@ -1,4 +1,4 @@
 insert into KC_KRMS_TERM_FUNCTION values (SEQ_KC_KRMS_TERM_FUNCTION_ID.nextval,'KC1009','IS_FEDERAL_SPONSOR','java.lang.String',null,sysdate,'admin',1,sys_guid())
 /
-insert into KC_KRMS_TERM_FUN_PARAM_SPEC values (SEQ_KC_KRMS_TERM_FUN_PARM_ID.nextval,SEQ_KC_KRMS_TERM_FUNCTION_ID.curval,'sponsorCode','java.lang.Object',1,sysdate,'admin',1,sys_guid())
+insert into KC_KRMS_TERM_FUN_PARAM_SPEC values (SEQ_KC_KRMS_TERM_FUN_PARM_ID.nextval,SEQ_KC_KRMS_TERM_FUNCTION_ID.currval,'sponsorCode','java.lang.Object',1,sysdate,'admin',1,sys_guid())
 /
