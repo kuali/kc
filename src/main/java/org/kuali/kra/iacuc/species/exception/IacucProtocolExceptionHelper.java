f2ed3829f2329db20df4dc929f3133ae72985c62
Rajeev Mancheril <rajeev@genesistec.com>
2012-05-02 18:18:46 +0000

    KCIAC-60 IACUC Exception Tab

---
+++ src/main/java/org/kuali/kra/iacuc/species/exception/IacucProtocolExceptionHelper.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.iacuc.species.exception;
+
+import java.io.Serializable;
+
+import org.kuali.kra.iacuc.IacucProtocol;
+import org.kuali.kra.iacuc.IacucProtocolDocument;
+import org.kuali.kra.iacuc.IacucProtocolForm;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.service.TaskAuthorizationService;
+import org.kuali.rice.krad.util.GlobalVariables;
+
+public class IacucProtocolExceptionHelper implements Serializable{
+
+    /**
+     * Comment for <code>serialVersionUID</code>
+     */
+    private static final long serialVersionUID = -2090976351003068814L;
+
+    protected IacucProtocolForm form;
+    
+    protected boolean modifyProtocolException;
+    protected IacucProtocolException newIacucProtocolException;
+
+    public IacucProtocolExceptionHelper(IacucProtocolForm form) {
+        setForm(form); 
+        setNewIacucProtocolException(new IacucProtocolException());
+    }    
+    
+    public void prepareView() {
+        //getForm().populateEditableFields();
+        initializePermission(getProtocol());
+
+    }
+
+    protected IacucProtocol getProtocol() {
+        IacucProtocolDocument document = form.getIacucProtocolDocument();
+        if (document == null || document.getProtocol() == null) {
+            throw new IllegalArgumentException("invalid (null) Iacuc ProtocolDocument in ProtocolForm");
+        }
+        return document.getIacucProtocol();
+    }
+    
+    public IacucProtocolForm getForm() {
+        return form;
+    }
+
+    public void setForm(IacucProtocolForm form) {
+        this.form = form;
+    }
+
+    public boolean isModifyProtocolException() {
+        return modifyProtocolException;
+    }
+
+    public void setModifyProtocolException(boolean modifyProtocolException) {
+        this.modifyProtocolException = modifyProtocolException;
+    }
+
+    public IacucProtocolException getNewIacucProtocolException() {
+        return newIacucProtocolException;
+    }
+
+    public void setNewIacucProtocolException(IacucProtocolException newIacucProtocolException) {
+        this.newIacucProtocolException = newIacucProtocolException;
+    }
+
+    protected void initializePermission(IacucProtocol protocol) {
+        //IacucProtocolTask task = new IacucProtocolTask(TaskName.MODIFY_PROTOCOL_EXCEPTION, protocol);
+        //modifyProtocolException = getTaskAuthorizationService().isAuthorized(getUserIdentifier(), task);
+        modifyProtocolException = true;
+    }
+    
+    protected TaskAuthorizationService getTaskAuthorizationService() {
+        return KraServiceLocator.getService(TaskAuthorizationService.class);
+    }
+
+    protected String getUserIdentifier() {
+        return GlobalVariables.getUserSession().getPrincipalId();
+    }
+
+}
