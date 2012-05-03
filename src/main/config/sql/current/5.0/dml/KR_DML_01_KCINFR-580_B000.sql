416b8fcfb24c04761686c52d48cfa715d95ccb8c
Geoff McGregor <gmcgrego@iu.edu>
2012-05-03 21:11:29 +0000

    KCINFR-523: KRMS integration - more bootstrap data

---
+++ src/main/config/sql/current/5.0/dml/KR_DML_01_KCINFR-580_B000.sql
@@ -0,0 +1,3 @@
+-- create default PeopleFlow type for KC documents
+insert into krew_typ_t (TYP_ID, NM, NMSPC_CD, SRVC_NM, ACTV, VER_NBR) values ('KC1000', 'KC Basic PeopleFlow', 'KC-WKFLW', null, 'Y', 1)
+/
