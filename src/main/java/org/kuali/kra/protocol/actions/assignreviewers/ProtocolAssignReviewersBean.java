f32809150ccf728e8323762c3213006fdc5cba71
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-05-10 12:45:41 +0000

    KCIAC-30: More work establishing baseline for IACUC Actions: Authorization, more beans, etc.

---
+++ src/main/java/org/kuali/kra/protocol/actions/assignreviewers/ProtocolAssignReviewersBean.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.protocol.actions.assignreviewers;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.kra.committee.bo.CommitteeMembership;
+import org.kuali.kra.committee.service.CommitteeService;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.irb.actions.ActionHelper;
+import org.kuali.kra.irb.actions.ProtocolActionBean;
+import org.kuali.kra.irb.actions.submit.ProtocolReviewType;
+import org.kuali.kra.irb.actions.submit.ProtocolReviewerBean;
+import org.kuali.kra.irb.actions.submit.ProtocolSubmission;
+import org.kuali.kra.irb.onlinereview.ProtocolOnlineReview;
+
+/**
+ * This class is really just a "form" for assigning a protocol
+ * to one or more reviewers.
+ */
+public class ProtocolAssignReviewersBean extends ProtocolActionBean implements Serializable {
+
+    private static final long serialVersionUID = -3769655019628462999L;
+    
+    private String currentCommitteeId = null;
+    private String currentScheduleId = null;
+    private List<ProtocolReviewerBean> reviewers = new ArrayList<ProtocolReviewerBean>();
+    
+    /**
+     * Constructs a ProtocolAssignReviewersBean.
+     * @param actionHelper Reference back to the action helper for this bean
+     */
+    public ProtocolAssignReviewersBean(ActionHelper actionHelper) {
+        super(actionHelper);
+    }
+    
+    /**
+     * Create the list of reviewers based upon the current committee
+     * and schedule, and assigns their reviewer types if any have been saved in the past
+     */
+    public void prepareView() {
+        ProtocolSubmission submission = getProtocol().getProtocolSubmission();
+        if (submission != null) {
+            String committeeId = submission.getCommitteeId();
+            String scheduleId = submission.getScheduleId();
+            
+            if (!StringUtils.equals(committeeId, currentCommitteeId) || !StringUtils.equals(scheduleId, currentScheduleId)) {
+                currentCommitteeId = committeeId;
+                currentScheduleId = scheduleId;
+                reviewers.clear();
+                if (!StringUtils.isBlank(committeeId) && (!StringUtils.isBlank(scheduleId) || isExpeditedSubmission(submission))) {
+                    List<CommitteeMembership> members = getProtocol().filterOutProtocolPersonnel(getCommitteeService().getAvailableMembers(committeeId, scheduleId));
+                    for (CommitteeMembership member : members) {
+                        reviewers.add(new ProtocolReviewerBean(member));
+                    }
+                    
+                    for (ProtocolOnlineReview review : submission.getProtocolOnlineReviews()) {
+                        if (review.isActive()) {
+                            for (ProtocolReviewerBean reviewerBean : reviewers) {
+                                if (reviewerBean.isProtocolReviewerBeanForReviewer(review.getProtocolReviewer())) {
+                                    reviewerBean.setReviewerTypeCode(review.getProtocolReviewer().getReviewerTypeCode());
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+//TODO:IACUC    private ProtocolAssignReviewersService getProtocolAssignReviewersService() {
+//        return KraServiceLocator.getService(ProtocolAssignReviewersService.class);
+//    }
+    
+    private CommitteeService getCommitteeService() {
+        return KraServiceLocator.getService(CommitteeService.class);
+    }
+    
+    public List<ProtocolReviewerBean> getReviewers() {
+        return reviewers;
+    }
+    
+    public ProtocolReviewerBean getReviewer(int i) {
+        return reviewers.get(i);
+    }
+    
+    /**
+     * We display the reviewers in two columns.  These are the
+     * reviewers in the left column.
+     * @return
+     */
+    public List<ProtocolReviewerBean> getLeftReviewers() {
+        List<ProtocolReviewerBean> leftReviewers = new ArrayList<ProtocolReviewerBean>();
+        for (int i = 0; i < (reviewers.size() + 1) / 2; i++) {
+            leftReviewers.add(reviewers.get(i));
+        }
+        return leftReviewers;
+    }
+    
+    /**
+     * We display the reviewers in two columns.  These are the
+     * reviewers in the right column.
+     * @return
+     */
+    public List<ProtocolReviewerBean> getRightReviewers() {
+        List<ProtocolReviewerBean> rightReviewers = new ArrayList<ProtocolReviewerBean>();
+        for (int i = (reviewers.size() + 1) / 2; i < reviewers.size(); i++) {
+            rightReviewers.add(reviewers.get(i));
+        }
+        return rightReviewers;
+    }
+    
+    private boolean isExpeditedSubmission(ProtocolSubmission submission) {
+        return submission != null && ProtocolReviewType.EXPEDITED_REVIEW_TYPE_CODE.equals(submission.getProtocolReviewTypeCode());
+    }
+}
