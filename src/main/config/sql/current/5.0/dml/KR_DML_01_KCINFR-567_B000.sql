8299c1855dd9a2bf6de34373f969a2d41805257f
Lin-Long Shyu <lshyukuali@gmail.com>
2012-05-13 23:09:45 +0000

    kcinfr-567 : param for questionnaire branching agenda name

---
+++ src/main/config/sql/current/5.0/dml/KR_DML_01_KCINFR-567_B000.sql
@@ -0,0 +1,3 @@
+INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+VALUES ('KC-PD', 'Document', 'QUESTIONNAIRE_BRANCHING_AGENDA', SYS_GUID(), 1, 'CONFG', 'Development Proposal Branching Questionnaire', 'This param is the agenda name for questionnaire branching rule related to development proposal', 'A', 'KC')
+/
