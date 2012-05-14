3820a0d5b037cd44091ecde41a35c3b55bcbe07e
Jay Hulslander <jdh34@cornell.edu>
2012-05-14 20:11:41 +0000

    KRACOEUS-4757

--- src/main/java/org/kuali/kra/service/impl/FiscalYearMonthServiceImpl.java
+++ src/main/java/org/kuali/kra/service/impl/FiscalYearMonthServiceImpl.java
@@ -75,32 +75,58 @@ public class FiscalYearMonthServiceImpl implements FiscalYearMonthService {
             calendar = Calendar.getInstance();
         }
         Integer fiscalStartMonth = getFiscalYearMonth();
+        System.err.println("fiscalStartMonth: " + fiscalStartMonth);
         //assuming July is the fiscal start month         
         if (calendar.get(Calendar.MONTH) == fiscalStartMonth.intValue()) {
             //July 1st, 2012, is the 1st month of FY 2013 
-            data.put(MONTH_KEY, Calendar.JANUARY);
-            if (fiscalStartMonth.equals(0)) {
+            data.put(MONTH_KEY, findMonth(fiscalStartMonth, calendar.get(Calendar.MONTH)));
+            if (fiscalStartMonth.equals(Calendar.JANUARY)) {
                 data.put(YEAR_KEY, calendar.get(Calendar.YEAR));
             } else {
                 data.put(YEAR_KEY, calendar.get(Calendar.YEAR) + 1);
             }
         } else if (calendar.get(Calendar.MONTH) > fiscalStartMonth.intValue()) {
             //August 1st 2012, is the second month of FY 2013
-            
-            data.put(MONTH_KEY, calendar.get(Calendar.MONTH) - fiscalStartMonth);
-            if (fiscalStartMonth.equals(0)) {
+            data.put(MONTH_KEY, findMonth(fiscalStartMonth, calendar.get(Calendar.MONTH)));
+            if (fiscalStartMonth.equals(Calendar.JANUARY)) {
                 data.put(YEAR_KEY, calendar.get(Calendar.YEAR));
             } else {
                 data.put(YEAR_KEY, calendar.get(Calendar.YEAR) + 1);
             }
         } else {
             //June 1st 2012, is the 12th month of FY 2012
-            
-            data.put(MONTH_KEY, fiscalStartMonth + calendar.get(Calendar.MONTH));
+            data.put(MONTH_KEY, findMonth(fiscalStartMonth, calendar.get(Calendar.MONTH)));
             data.put(YEAR_KEY, calendar.get(Calendar.YEAR));
         }        
         return data;
     }
+    
+    private int findMonth(int startingMonth, int currentMonth) {
+        /**
+         * We are building an array of integers.  The array position number is the fiscal month position of the calendar month.
+         * The array values are the calendar months.  So an example with a fiscal year starting in September would be as follows:
+         * YEAR[0] = Calendar.September
+         * Year[1] = Calendar.October
+         * Year[11 = Calendar.August
+         */
+        int nextMonth = startingMonth;
+        int[] YEAR = {0, 1, 2, 3, 4, 5, 6 ,7, 8, 9, 10, 11};
+        for (int i = 0; i < 12; i++) {
+            YEAR[i] = nextMonth;
+            if (nextMonth == 11) {
+                nextMonth = 0;
+            } else {
+                nextMonth++;
+            }
+        }
+        for (int i : YEAR) {
+            if (YEAR[i] == currentMonth) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("Could not find the current month: " + currentMonth);
+    }
+    
     
     /**
      * 
