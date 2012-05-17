f1f996b480fd467a49fe1e01cfac39b99c199c7c
Allen Akers <allen.akers@colostate.edu>
2012-05-17 22:57:44 +0000

    Hack to see if sequences were not properly updated for kc400dba

--- src/main/config/sql/current/4.0/dml/KC_DML_31101_CUSTOM_ATTRIBUTE_0TS0.sql
+++ src/main/config/sql/current/4.0/dml/KC_DML_31101_CUSTOM_ATTRIBUTE_0TS0.sql
@@ -1,3 +1,14 @@
+DECLARE temp NUMBER;
+maxAttributeId NUMBER;
+BEGIN
+        SELECT COUNT(*) INTO temp FROM user_sequences WHERE sequence_name = 'SEQ_CUSTOM_ATTRIBUTE';
+        IF temp = 0 THEN 
+        	select nvl(max(ID),0) into maxAttributeId from CUSTOM_ATTRIBUTE; 
+        	EXECUTE IMMEDIATE 'CREATE SEQUENCE SEQ_CUSTOM_ATTRIBUTE  MINVALUE 1 MAXVALUE 999999999999 INCREMENT BY 1 START WITH '||(maxAttributeId+1)||' NOCACHE  ORDER  NOCYCLE'; 
+		END IF;
+END;
+/
+
 insert into CUSTOM_ATTRIBUTE (ID,NAME,LABEL,DATA_TYPE_CODE,DATA_LENGTH,DEFAULT_VALUE,LOOKUP_CLASS,LOOKUP_RETURN,GROUP_NAME,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR,OBJ_ID)
 values (SEQ_CUSTOM_ATTRIBUTE.NEXTVAL,'ARRA_FUNDING','ARRA Funding','1',3,null,'org.kuali.kra.bo.ArgValueLookup','yes_no_flag','Other',sysdate,'admin',0,sys_guid())
 /
