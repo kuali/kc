a47e642a2e67a432ac39bc66117610d35a28da6c
Geoff McGregor <gmcgrego@iu.edu>
2012-05-23 18:11:07 +0000

    oops

--- src/main/java/org/kuali/kra/service/impl/FiscalYearMonthServiceImpl.java
+++ src/main/java/org/kuali/kra/service/impl/FiscalYearMonthServiceImpl.java
@@ -134,7 +134,7 @@ public class FiscalYearMonthServiceImpl implements FiscalYearMonthService {
      */
     protected Integer getFiscalYearMonth() {
         Parameter parm = this.getParameterService().getParameter(KC_GENERAL_NAMESPACE, DOCUMENT_COMPONENT_NAME, FISCAL_YEAR_MONTH_PARAMETER_NAME);
-        return Integer.parseInt("7");
+        return Integer.parseInt(parm.getValue());
     }
 
     public ParameterService getParameterService() {
