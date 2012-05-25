23bc780b024c1c687c1ae75f03b4f622d320e222
Rajeev Mancheril <rajeev@genesistec.com>
2012-05-25 17:12:20 +0000

    KCIAC-152 Create Procedure Category Maintenance

---
+++ src/main/java/org/kuali/kra/iacuc/procedures/IacucProcedureCategory.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.iacuc.procedures;
+
+import org.kuali.kra.bo.KraPersistableBusinessObjectBase;
+
+public class IacucProcedureCategory extends KraPersistableBusinessObjectBase { 
+    
+    private static final long serialVersionUID = 1L;
+
+    private Integer procedureCategoryCode; 
+    private String procedureCategory; 
+    private boolean active;
+    
+    public IacucProcedureCategory() { 
+
+    } 
+    
+    public Integer getProcedureCategoryCode() {
+        return procedureCategoryCode;
+    }
+
+    public void setProcedureCategoryCode(Integer procedureCategoryCode) {
+        this.procedureCategoryCode = procedureCategoryCode;
+    }
+
+    public String getProcedureCategory() {
+        return procedureCategory;
+    }
+
+    public void setProcedureCategory(String procedureCategory) {
+        this.procedureCategory = procedureCategory;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+    /**  {@inheritDoc} */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((this.procedureCategoryCode == null) ? 0 : this.procedureCategoryCode.hashCode());
+        result = prime * result + ((this.procedureCategory == null) ? 0 : this.procedureCategory.hashCode());
+        return result;
+    }
+
+    /**  {@inheritDoc} */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (this.getClass() != obj.getClass()) {
+            return false;
+        }
+        IacucProcedureCategory other = (IacucProcedureCategory) obj;
+        if (this.procedureCategoryCode == null) {
+            if (other.procedureCategoryCode != null) {
+                return false;
+            }
+        } else if (!this.procedureCategoryCode.equals(other.procedureCategoryCode)) {
+            return false;
+        }
+        if (this.procedureCategory == null) {
+            if (other.procedureCategory != null) {
+                return false;
+            }
+        } else if (!this.procedureCategory.equals(other.procedureCategory)) {
+            return false;
+        }
+        return true;
+    }
+    
+}
--- src/main/webapp/WEB-INF/tags/rice-portal/channel/maintenanceCompliance.tag
+++ src/main/webapp/WEB-INF/tags/rice-portal/channel/maintenanceCompliance.tag
@@ -76,6 +76,7 @@
     <li><portal:portalLink displayTitle="true" title="Iacuc Submission Type" url="${ConfigProperties.application.url}/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.kra.iacuc.actions.submit.IacucProtocolSubmissionType&docFormKey=88888888&returnLocation=${ConfigProperties.application.url}/portal.do&hideReturnLink=true" /></li>
     <li><portal:portalLink displayTitle="true" title="Iacuc Submission Type Qualifier" url="${ConfigProperties.application.url}/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.kra.iacuc.actions.submit.IacucProtocolSubmissionQualifierType&docFormKey=88888888&returnLocation=${ConfigProperties.application.url}/portal.do&hideReturnLink=true" /></li>
     <li><portal:portalLink displayTitle="true" title="Iacuc Protocol Person Role" url="${ConfigProperties.application.url}/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.kra.iacuc.personnel.IacucProtocolPersonRole&docFormKey=88888888&returnLocation=${ConfigProperties.application.url}/portal.do&hideReturnLink=true" /></li>
+    <li><portal:portalLink displayTitle="true" title="Iacuc Procedure Category" url="${ConfigProperties.application.url}/kr/lookup.do?methodToCall=start&businessObjectClassName=org.kuali.kra.iacuc.procedures.IacucProcedureCategory&docFormKey=88888888&returnLocation=${ConfigProperties.application.url}/portal.do&hideReturnLink=true" /></li>
     
     
   <ul>
