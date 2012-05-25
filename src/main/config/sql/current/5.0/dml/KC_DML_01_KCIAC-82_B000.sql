121f8b26bd840fca9ddbd35dc5bdd1d9ad564f84
Kristina Taylor <kbtaylor@uw.edu>
2012-05-25 18:21:03 +0000

    KCIAC-82: Moving scripts to correct place

--- src/main/config/sql/current/5.0/dml/KC_DML_01_KCIAC-82_B000.sql
+++
@@ -1,9 +0,0 @@
--- remove these constraint, so this table can be shared by irb & iacuc
-ALTER TABLE COMM_SCHEDULE_MINUTES
-	drop  CONSTRAINT FK_COMM_SCHEDULE_MINUTES_FK4
-/
-ALTER TABLE COMM_SCHEDULE_MINUTES
-	drop CONSTRAINT COMM_SCHEDULE_MINUTES_PRO_FK1
-/
-
-
