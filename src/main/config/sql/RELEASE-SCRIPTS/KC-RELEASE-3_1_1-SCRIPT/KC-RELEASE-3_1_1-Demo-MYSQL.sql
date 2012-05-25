daaf840fd8cbe4ee76a10093a4041ba1bd2cb414
Kristina Taylor <kbtaylor@uw.edu>
2012-05-25 14:51:20 +0000

    Updating demo data locations for new structure and adding MySQL TSD data

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_1-SCRIPT/KC-RELEASE-3_1_1-Demo-MYSQL.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_1-SCRIPT/KC-RELEASE-3_1_1-Demo-MYSQL.sql
@@ -1,6 +1,6 @@
-\. ./../../current_mysql/3.1.1/dml/KC_DML_01_RATE_CLASS_00SD.sql
-\. ./../../current_mysql/3.1.1/dml/KC_DML_01_SPONSOR_FORM_TEMPLATES_00SD.sql
-\. ./../../current_mysql/3.1.1/dml/KC_DML_02_INSTITUTE_RATES_00SD.sql
-\. ./../../current_mysql/3.1.1/dml/KC_DML_03_VALID_CE_RATE_TYPES_00SD.sql
+\. ./../../current_mysql/4.0/dml/KC_DML_31101_RATE_CLASS_00SD.sql
+\. ./../../current_mysql/4.0/dml/KC_DML_31101_SPONSOR_FORM_TEMPLATES_00SD.sql
+\. ./../../current_mysql/4.0/dml/KC_DML_31102_INSTITUTE_RATES_00SD.sql
+\. ./../../current_mysql/4.0/dml/KC_DML_31103_VALID_CE_RATE_TYPES_00SD.sql
 commit;
 exit
---
+++ src/main/config/sql/current_mysql/4.0/dml/KC_DML_31005_AWARD_SYNC_CHANGE_00S0.sql
@@ -0,0 +1,34 @@
+DELIMITER /
+INSERT INTO SEQ_AWARD_SYNC_CHANGE_ID VALUES(NULL)
+/
+INSERT INTO AWARD_SYNC_CHANGE (AWARD_SYNC_CHANGE_ID,AWARD_ID,CLASS_NAME,ATTRIBUTE_NAME,OBJECT_DESC,DATA_DESC,SYNC_TYPE,SYNC_DESCEND,SYNC_FABRICATED_DESCEND,SYNC_COST_SHARE_DESCEND,XML_CHANGES,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR,OBJ_ID)
+VALUES ((SELECT (MAX(ID)) FROM SEQ_AWARD_SYNC_CHANGE_ID),(SELECT AWARD_ID FROM AWARD WHERE AWARD_NUMBER = '001002-00001' AND SEQUENCE_NUMBER = 2),'org.kuali.kra.award.home.Award','sponsorCode','Sponsor','000340 : NIH','A','ALL','Y','N',
+'<org.kuali.kra.award.awardhierarchy.sync.AwardSyncXmlExport>
+<className>org.kuali.kra.award.home.Award</className>
+<addIfNotFound>true</addIfNotFound>
+<keys/>
+<values>
+<entry>
+<string>sponsorCode</string>
+<string>000340</string>
+</entry>
+</values>
+</org.kuali.kra.award.awardhierarchy.sync.AwardSyncXmlExport>',NOW(),'quickstart',0,UUID())
+/
+INSERT INTO SEQ_AWARD_SYNC_CHANGE_ID VALUES(NULL)
+/
+INSERT INTO AWARD_SYNC_CHANGE (AWARD_SYNC_CHANGE_ID,AWARD_ID,CLASS_NAME,ATTRIBUTE_NAME,OBJECT_DESC,DATA_DESC,SYNC_TYPE,SYNC_DESCEND,SYNC_FABRICATED_DESCEND,SYNC_COST_SHARE_DESCEND,XML_CHANGES,UPDATE_TIMESTAMP,UPDATE_USER,VER_NBR,OBJ_ID)
+VALUES ((SELECT (MAX(ID)) FROM SEQ_AWARD_SYNC_CHANGE_ID),(SELECT AWARD_ID FROM AWARD WHERE AWARD_NUMBER = '001002-00001' AND SEQUENCE_NUMBER = 3),'org.kuali.kra.award.home.Award','sponsorCode','Sponsor','000500 : NSF','A','ALL','Y','N',
+'<org.kuali.kra.award.awardhierarchy.sync.AwardSyncXmlExport>
+<className>org.kuali.kra.award.home.Award</className>
+<addIfNotFound>true</addIfNotFound>
+<keys/>
+<values>
+<entry>
+<string>sponsorCode</string>
+<string>000500</string>
+</entry>
+</values>
+</org.kuali.kra.award.awardhierarchy.sync.AwardSyncXmlExport>',NOW(),'quickstart',0,UUID())
+/
+DELIMITER ;
