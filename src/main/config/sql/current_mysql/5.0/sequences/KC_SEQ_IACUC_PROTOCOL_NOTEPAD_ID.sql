ebe2561463bdc1fa5b739eee6b1f995bbcbc45aa
Kristina Taylor <kbtaylor@uw.edu>
2012-05-29 20:36:20 +0000

    Release 5.0 Scripts: Oracle script corrections and converted MySQL scripts

---
+++ src/main/config/sql/current_mysql/5.0/sequences/KC_SEQ_IACUC_PROTOCOL_NOTEPAD_ID.sql
@@ -0,0 +1,8 @@
+DELIMITER /
+CREATE TABLE SEQ_IACUC_PROTOCOL_NOTEPAD_ID (
+  id bigint(19) not null auto_increment, primary key (id)
+) ENGINE MyISAM
+/
+ALTER TABLE SEQ_IACUC_PROTOCOL_NOTEPAD_ID auto_increment = 1
+/
+DELIMITER ;
