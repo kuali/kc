d775748806e60ae7ebc294f1bc3711aaa274e4f8
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-05-29 14:02:04 +0000

    KCCOI-284: Financial Entity attachments cannot be opened for viewing if the name contains spaces

--- src/main/java/org/kuali/kra/printing/util/PrintingUtils.java
+++ src/main/java/org/kuali/kra/printing/util/PrintingUtils.java
@@ -326,22 +326,21 @@ public class PrintingUtils {
         }
     }
 
-    public static void streamToResponse(byte[] fileContents, String fileName, String fileContentType, HttpServletResponse response)
-    throws Exception {
+    public static void streamToResponse(byte[] fileContents, String fileName, String fileContentType, HttpServletResponse response) throws Exception {
         ByteArrayOutputStream baos = null;
         try {
             baos = new ByteArrayOutputStream(fileContents.length);
             baos.write(fileContents);
-            baos.close();
-            response.addHeader("Content-Disposition", "attachment;filename=\"" + fileName + "\"");
-            response.addHeader("Content-Transfer-Encoding", "binary");
-            response.setContentType(fileContentType);
-            response.setContentLength(fileContents.length);
-            response.getOutputStream().write(baos.toByteArray());
-            response.getOutputStream().flush();
-            baos = null;
-        } catch (IOException ioEx) {
-            throw new RuntimeException("IOException occurred while downloading attachment", ioEx);
+            WebUtils.saveMimeOutputStreamAsFile(response, fileContentType, baos, fileName);
+        } finally {
+            try {
+                if (baos != null) {
+                    baos.close();
+                    baos = null;
+                }
+            } catch (IOException ioEx) {
+                throw new RuntimeException("IOException occurred while downloading attachment", ioEx);
+            }
         }
     }
 
