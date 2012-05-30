c7cf52779efb4d89d8e5ab5de60fe1dd8301e786
Geoff McGregor <gmcgrego@iu.edu>
2012-05-30 20:12:25 +0000

    KCINFR-523: Award hooks for KRMS

--- src/main/java/org/kuali/kra/award/AwardFactBuilderServiceImpl.java
+++ src/main/java/org/kuali/kra/award/AwardFactBuilderServiceImpl.java
@@ -13,16 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.irb.rules;
+package org.kuali.kra.award;
 
 import java.io.ByteArrayInputStream;
 
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 
+import org.kuali.kra.award.document.AwardDocument;
 import org.kuali.kra.bo.CoeusModule;
-import org.kuali.kra.irb.ProtocolDocument;
-import org.kuali.kra.krms.KcKrmsConstants;
 import org.kuali.rice.core.api.exception.RiceRuntimeException;
 import org.kuali.rice.core.api.util.xml.XmlHelper;
 import org.kuali.rice.kew.api.exception.WorkflowException;
@@ -32,55 +31,30 @@ import org.kuali.rice.krad.service.DocumentService;
 import org.kuali.rice.krms.api.engine.Facts;
 import org.w3c.dom.Document;
 
-public class IrbProtocolFactBuilderServiceImpl implements IrbProtocolFactBuilderService {
+public class AwardFactBuilderServiceImpl implements AwardFactBuilderService {
     
     private BusinessObjectService businessObjectService;
-    private DocumentService documentService;;
+    private DocumentService documentService;
     
     public void addFacts(Facts.Builder factsBuilder, String docContent) {
         String documentNumber = getElementValue(docContent, "//documentNumber");
         try {
-            ProtocolDocument protocolDocument = (ProtocolDocument) getDocumentService().getByDocumentHeaderId(documentNumber);
-            addFacts(factsBuilder, protocolDocument);
-        }catch (WorkflowException e) {
+            AwardDocument awardDocument = (AwardDocument) getDocumentService().getByDocumentHeaderId(documentNumber);
+            addFacts(factsBuilder, awardDocument);
+        } catch (WorkflowException e) {
             throw new RuntimeException(e);
         }
     }
     
-    public void addFacts(Facts.Builder factsBuilder, ProtocolDocument protocolDocument) {
-        factsBuilder.addFact(KcKrmsConstants.IrbProtocol.PROTOCOL_REFERENCE_NUMBER_1, protocolDocument.getProtocol().getReferenceNumber1());
-        factsBuilder.addFact(KcKrmsConstants.IrbProtocol.PROTOCOL_REFERENCE_NUMBER_2, protocolDocument.getProtocol().getReferenceNumber2());
-        factsBuilder.addFact(KcKrmsConstants.IrbProtocol.FDA_APPLICATION_NUMBER, protocolDocument.getProtocol().getFdaApplicationNumber());
+    public void addFacts(Facts.Builder factsBuilder, AwardDocument awardDocument) {
         
         // Functions
-        // All persons training completed
-        // All Protocols
-        // Application date
-        // Areas of research - do areas of research contain specified?  user input 1 area of research
-        // Expiration date
-        // Funding source sponsor
-        // Funding source unit
-        // Is amendment
-        // Is renewal
-        // PI training complete
-        // Protocol campus
-        // Protocol document type
-        // Protocol lead unit
-        // Protocol lead unit below
-        // Protocol non-faculty PI
-        // Protocol organization
-        // Protocol organization changed
-        // Protocol PI changed
-        // Protocol PI is specified
-        // Protocol special review type
-        // Protocol submission type
-        // Protocol submitted by PI
-        // Protocol type
-        // Subject type
+        // Award document type
+        // Signature required
     
         // Questionnaire Prereqs
-        factsBuilder.addFact("moduleCode", CoeusModule.IRB_MODULE_CODE);
-        factsBuilder.addFact("moduleItemKey", protocolDocument.getProtocol().getProtocolNumber());
+        factsBuilder.addFact("moduleCode", CoeusModule.AWARD_MODULE_CODE);
+        factsBuilder.addFact("moduleItemKey", awardDocument.getAward().getAwardNumber());
     }
     
     protected String getElementValue(String docContent, String xpathExpression) {
