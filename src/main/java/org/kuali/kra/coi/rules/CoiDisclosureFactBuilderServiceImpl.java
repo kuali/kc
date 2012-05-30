c072d0235eb0991f20ffddc64eaaf2421939db1d
Geoff McGregor <gmcgrego@iu.edu>
2012-05-30 02:01:56 +0000

    KCINFR-523: Fact builder services

---
+++ src/main/java/org/kuali/kra/coi/rules/CoiDisclosureFactBuilderServiceImpl.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.coi.rules;
+
+import java.io.ByteArrayInputStream;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+
+import org.kuali.kra.bo.CoeusModule;
+import org.kuali.kra.coi.CoiDisclosureDocument;
+import org.kuali.rice.core.api.exception.RiceRuntimeException;
+import org.kuali.rice.core.api.util.xml.XmlHelper;
+import org.kuali.rice.kew.api.exception.WorkflowException;
+import org.kuali.rice.kew.rule.xmlrouting.XPathHelper;
+import org.kuali.rice.krad.service.BusinessObjectService;
+import org.kuali.rice.krad.service.DocumentService;
+import org.kuali.rice.krms.api.engine.Facts;
+import org.w3c.dom.Document;
+
+public class CoiDisclosureFactBuilderServiceImpl implements CoiDisclosureFactBuilderService {
+    
+    private BusinessObjectService businessObjectService;
+    private DocumentService documentService;;
+    
+    public void addFacts(Facts.Builder factsBuilder, String docContent) {
+        String documentNumber = getElementValue(docContent, "//documentNumber");
+        try {
+            CoiDisclosureDocument disclosureDocument = (CoiDisclosureDocument) getDocumentService().getByDocumentHeaderId(documentNumber);
+            addFacts(factsBuilder, disclosureDocument);
+        }catch (WorkflowException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public void addFacts(Facts.Builder factsBuilder, CoiDisclosureDocument disclosureDocument) {
+    
+        // Questionnaire Prereqs
+        factsBuilder.addFact("moduleCode", CoeusModule.COI_DISCLOSURE_MODULE_CODE);
+        factsBuilder.addFact("moduleItemKey", disclosureDocument.getCoiDisclosure().getCoiDisclosureNumber());
+    }
+    
+    protected String getElementValue(String docContent, String xpathExpression) {
+        try {
+            Document document = XmlHelper.trimXml(new ByteArrayInputStream(docContent.getBytes()));
+
+            XPath xpath = XPathHelper.newXPath();
+            String value = (String) xpath.evaluate(xpathExpression, document, XPathConstants.STRING);
+
+            return value;
+
+        } catch (Exception e) {
+            throw new RiceRuntimeException();
+        }
+    }
+
+    public BusinessObjectService getBusinessObjectService() {
+        return businessObjectService;
+    }
+
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+
+    public DocumentService getDocumentService() {
+        return documentService;
+    }
+
+    public void setDocumentService(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+}
