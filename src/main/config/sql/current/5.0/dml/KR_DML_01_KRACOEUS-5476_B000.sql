3d74c8b55d7ab115ff446bfd314c493740f1e971
Neethu Jose <neethu@polussoftware.com>
2012-05-30 18:37:48 +0000

    KRACOEUS-5476

---
+++ src/main/config/sql/current/5.0/dml/KR_DML_01_KRACOEUS-5476_B000.sql
@@ -0,0 +1,32 @@
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','propSummaryDisclaimerText',SYS_GUID(),1,'CONFG','Approval signifies that the proposed project fits within the academic framework and resources of the unit, requirements for new or renovated facilitiesspace have been discussed with the appropriate people, contributions listed will be met by the departmentcollege unless otherwise approved, that Conflict of Interest requirements have been addressed, and that Sponsored Programs may process the proposal','Display Proposal Summary Disclaimer Text','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableProposalSummaryPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Proposal Summary panel','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableBudgetSummaryPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Budget Summary panel','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableKeyPersonnelPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Key Personnel panel','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableSummaryPrintPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Summary Print Forms panel','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableSummaryKeywordsPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Summary Keywords panel','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableSummaryAttachmentsPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Summary Attachments panel','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableSummaryQuestionsPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Summary Questions panel','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableCustomDataInfoPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Custom Data Infor panel','A','KC')
+/
+INSERT INTO krcr_parm_t(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
+VALUES ('KC-PD','Document','enableSpecialReviewPanel',SYS_GUID(),1,'CONFG','Y','Flag to display Special Review panel','A','KC')
+/
+commit
+/
