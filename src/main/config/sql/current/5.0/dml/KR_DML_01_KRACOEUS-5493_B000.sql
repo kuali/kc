2b38a6e0fd466d6b4ea5d581a8a8002a7bc10570
Geo Thomas <geot@mit.edu>
2012-05-30 04:02:37 +0000

    Jira#KRACOEUS-5493,KCINFR-566

---
+++ src/main/config/sql/current/5.0/dml/KR_DML_01_KRACOEUS-5493_B000.sql
@@ -0,0 +1,16 @@
+insert into KRMS_TERM_SPEC_T values('KC1020','specifiedGGForm','java.lang.String','Y',1,'Specified GG Form','KC-PD')
+/
+insert into KRMS_TERM_T values ('KC1020','KC1020',1,'Specified GG Form')
+/
+insert into KRMS_TERM_PARM_T values ('KC1020','KC1020',1,'Specified GG Form')
+/
+insert into KRMS_CNTXT_VLD_TERM_SPEC_T values ('KC1020','KC-PD-CONTEXT','KC1020','Y')
+/
+insert into KRMS_TERM_RSLVR_T values ('KC1002','KC-PD','javaFunctionResolver','KC1001','KC1020','Y',1)
+/
+insert into KRMS_TERM_RSLVR_PARM_SPEC_T (TERM_RSLVR_PARM_SPEC_ID, TERM_RSLVR_ID, NM, VER_NBR) 
+values ('KC1002', (select TERM_RSLVR_ID from KRMS_TERM_RSLVR_T where NM='javaFunctionResolver' and NMSPC_CD='KC-PD'), 'GrantsGov Form Name', 1)
+/
+insert into KRMS_TERM_SPEC_CTGRY_T (TERM_SPEC_ID, CTGRY_ID) values ((select TERM_SPEC_ID from KRMS_TERM_SPEC_T where NMSPC_CD='KC-PD' and NM='specifiedGGForm'), 
+(select CTGRY_ID from KRMS_CTGRY_T where NMSPC_CD='KC-PD' and NM='Questionnaire'))
+/
