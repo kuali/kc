abd4d98098b2798310dd1ff2ed9d81ca1dfee2f9
Geo Thomas <geot@mit.edu>
2012-05-31 01:11:17 +0000

    Jira#KRACOEUS-5493 - fixing script

--- src/main/config/sql/current/5.0/dml/KR_DML_03_KRACOEUS-5493_B000.sql
+++ src/main/config/sql/current/5.0/dml/KR_DML_03_KRACOEUS-5493_B000.sql
@@ -5,7 +5,7 @@ insert into KRMS_CNTXT_VLD_TERM_SPEC_T values ('KC2000','KC-PD-CONTEXT','KC2000'
 insert into KRMS_TERM_RSLVR_T values ('KC2000','KC-PD','Java Function Resolver','KC1001','KC2000','Y',1)
 /
 insert into KRMS_TERM_RSLVR_PARM_SPEC_T (TERM_RSLVR_PARM_SPEC_ID, TERM_RSLVR_ID, NM, VER_NBR) 
-values ('KC2000', (select TERM_RSLVR_ID from KRMS_TERM_RSLVR_T where NM='javaFunctionResolver' and NMSPC_CD='KC-PD'), 'GrantsGov Form Name', 1)
+values ('KC2000', (select TERM_RSLVR_ID from KRMS_TERM_RSLVR_T where NM='Java Function Resolver' and NMSPC_CD='KC-PD'), 'GrantsGov Form Name', 1)
 /
 insert into KRMS_TERM_SPEC_CTGRY_T (TERM_SPEC_ID, CTGRY_ID) values ((select TERM_SPEC_ID from KRMS_TERM_SPEC_T where NMSPC_CD='KC-PD' and NM='specifiedGGForm'), 
 	(select CTGRY_ID from KRMS_CTGRY_T where NMSPC_CD='KC-PD' and NM='Function'))
