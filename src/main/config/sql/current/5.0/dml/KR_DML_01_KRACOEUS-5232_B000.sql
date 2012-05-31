b35e51733b4416b73c16bb2d7a1164493ca4f493
Geo Thomas <geot@mit.edu>
2012-05-31 03:12:11 +0000

    removing formulated cost scripts

--- src/main/config/sql/current/5.0/dml/KR_DML_01_KRACOEUS-5232_B000.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KC-B', 'Document', 'enableFormulatedCostCalc', SYS_GUID(), 1, 'CONFG', 'Y', 'This is to enable formulated cost calculation', 'A', 'KC')
-/
-INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KC-B', 'Document', 'formulatedCostElements', SYS_GUID(), 1, 'CONFG', '421598,420726', 'Cost elements mentioned here are used as Formulated Cost Elements', 'A', 'KC')
-/
