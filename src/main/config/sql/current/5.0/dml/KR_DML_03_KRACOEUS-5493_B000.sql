b1928a30885c9af9c61a8d92bc2f3184c4ff73fd
Geo Thomas <geot@mit.edu>
2012-05-31 00:51:49 +0000

    Jira#KRACOEUS-5493 - DB script for s2s questionnaire business rules

--- src/main/config/sql/current/5.0/dml/KR_DML_03_KRACOEUS-5493_B000.sql
+++ src/main/config/sql/current/5.0/dml/KR_DML_03_KRACOEUS-5493_B000.sql
@@ -1,10 +1,8 @@
 insert into KRMS_TERM_SPEC_T values('KC2000','specifiedGGForm','java.lang.String','Y',1,'Specified GG Form','KC-PD')
 /
-insert into KRMS_TERM_T values ('KC2000','KC2000',1,'Specified GG Form')
-/
 insert into KRMS_CNTXT_VLD_TERM_SPEC_T values ('KC2000','KC-PD-CONTEXT','KC2000','Y')
 /
-insert into KRMS_TERM_RSLVR_T values ('KC2000','KC-PD','javaFunctionResolver','KC1001','KC2000','Y',1)
+insert into KRMS_TERM_RSLVR_T values ('KC2000','KC-PD','Java Function Resolver','KC1001','KC2000','Y',1)
 /
 insert into KRMS_TERM_RSLVR_PARM_SPEC_T (TERM_RSLVR_PARM_SPEC_ID, TERM_RSLVR_ID, NM, VER_NBR) 
 values ('KC2000', (select TERM_RSLVR_ID from KRMS_TERM_RSLVR_T where NM='javaFunctionResolver' and NMSPC_CD='KC-PD'), 'GrantsGov Form Name', 1)
