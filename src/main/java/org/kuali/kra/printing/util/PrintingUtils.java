c2fe00322fe821686ea3cfca1443467f2d9a447c
Douglas Pace <dpace@kuali.org>
2012-06-01 00:45:28 +0000

    KRACOEUS-5006 : Reverting previous commit for report tracking print due to stack trace.

--- src/main/java/org/kuali/kra/printing/util/PrintingUtils.java
+++ src/main/java/org/kuali/kra/printing/util/PrintingUtils.java
@@ -27,7 +27,6 @@ import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 
 import org.kuali.kra.award.printing.AwardPrintType;
-import org.kuali.kra.award.paymentreports.awardreports.reporting.service.ReportTrackingType;
 import org.kuali.kra.bo.CommentType;
 import org.kuali.kra.budget.printing.BudgetPrintType;
 import org.kuali.kra.coi.print.CoiDisclosureType;
@@ -81,7 +80,6 @@ public class PrintingUtils {
 	private static final String COMMENT_TYPE_CODE_PARAMETER = "commentTypeCode";
 	private static final String  XSL_PRINT_NEGOTIATION_ACTIVITY_REPORT ="NegotiationActivityReport.xsl";
 	private static final String XSL_COI_APPROVED_DISCLOSURE = "ApprovedDisclosure.xsl";
-	private static final String XSL_AWARD_REPORT_TRACKING = "AwardReportingRequirements.xsl";
 
 	/**
 	 * This method fetches system constant parameters
@@ -212,16 +210,11 @@ public class PrintingUtils {
         } else if (reportType.equals(CommitteeReportType.FUTURE_SCHEDULED_MEETINGS.getCommitteeReportType())) {
             xsl = XSL_FUTURE_SCHEDULED_MEETINGS;
         } 
-	 else if (reportType.equals(ReportTrackingType.
-	 AWARD_REPORT_TRACKING.getReportTrackingType())) {
-        xsl = XSL_AWARD_REPORT_TRACKING;
-    } 
         else if (reportType
                 .equals(NegotiationActivityPrintType.NEGOTIATION_ACTIVITY_REPORT
                         .getNegotiationActivityPrintType())) {
             xsl = XSL_PRINT_NEGOTIATION_ACTIVITY_REPORT;
         }  
-		
         else if (ProtocolPrintType.getReportTypes().contains(reportType)) {
             for (ProtocolPrintType protocolPrintType : ProtocolPrintType.values()) {
                 if (reportType.equals(protocolPrintType.getProtocolPrintType())) {
