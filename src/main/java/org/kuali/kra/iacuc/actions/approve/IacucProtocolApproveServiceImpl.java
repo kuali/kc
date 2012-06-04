4345811ff9a6be8a6f16d13c875490acf5aad203
Neeraj Khanolkar <neerajsk@iastate.edu>
2012-06-04 21:08:07 +0000

    KCIAC-140: Setting up the services and beans for admin approval action.

---
+++ src/main/java/org/kuali/kra/iacuc/actions/approve/IacucProtocolApproveServiceImpl.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.iacuc.actions.approve;
+
+import org.kuali.kra.iacuc.IacucProtocol;
+import org.kuali.kra.iacuc.actions.IacucProtocolAction;
+import org.kuali.kra.iacuc.actions.IacucProtocolActionType;
+import org.kuali.kra.protocol.Protocol;
+import org.kuali.kra.protocol.actions.ProtocolAction;
+import org.kuali.kra.protocol.actions.approve.ProtocolApproveBean;
+import org.kuali.kra.protocol.actions.approve.ProtocolApproveServiceImpl;
+
+public class IacucProtocolApproveServiceImpl extends ProtocolApproveServiceImpl implements IacucProtocolApproveService {
+
+    private static final String FULL_APPROVAL_FINALIZE_OLR_ANNOTATION = "Online Review finalized as part of full approval action on protocol.";
+    
+    
+    /**
+     * {@inheritDoc}
+     * @see org.kuali.kra.protocol.actions.approve.ProtocolApproveService#grantFullApproval(org.kuali.kra.protocol.Protocol, 
+     *      org.kuali.kra.protocol.actions.approve.ProtocolApproveBean)
+     */
+    public void grantFullApproval(Protocol protocol, ProtocolApproveBean actionBean) throws Exception {
+        generateProtocolActionAndAttach(protocol, actionBean, IacucProtocolActionType.IACUC_APPROVED);   
+
+        if (protocol.getApprovalDate() == null) {
+            protocol.setApprovalDate(actionBean.getApprovalDate());
+        }
+        if (protocol.isRenewal()) {
+            protocol.setLastApprovalDate(actionBean.getApprovalDate());
+        }
+        
+        finalizeReviewsAndSave(protocol, IacucProtocolActionType.IACUC_APPROVED, FULL_APPROVAL_FINALIZE_OLR_ANNOTATION);
+    }  
+
+    
+    @Override
+    protected String getProtocolActionTypeCodeForAdminApprovalHook() {
+        return IacucProtocolActionType.ADMINISTRATIVE_APPROVAL;
+    }
+
+    
+    @Override
+    protected String getProtocolActionTypeCodeForResponseApprovalHook() {
+        return IacucProtocolActionType.RESPONSE_APPROVAL;
+    }
+    
+    
+    @Override
+    protected ProtocolAction getNewProtocolActionInstanceHook(Protocol protocol, Object object, String protocolActionTypeCode) {
+        return new IacucProtocolAction((IacucProtocol)protocol, null, protocolActionTypeCode);
+    }
+
+    
+}
