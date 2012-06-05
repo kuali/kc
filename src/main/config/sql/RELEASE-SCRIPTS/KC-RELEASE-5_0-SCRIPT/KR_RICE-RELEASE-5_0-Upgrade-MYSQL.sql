075e9bc56dbb7aed931f88431e934c99df193a11
Allen Akers <allen.akers@colostate.edu>
2012-06-05 13:44:37 +0000

    Removing Rice upgrade script that was causing problems with existing installs and was removed from the Rice project for 2.1

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_0-SCRIPT/KR_RICE-RELEASE-5_0-Upgrade-MYSQL.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_0-SCRIPT/KR_RICE-RELEASE-5_0-Upgrade-MYSQL.sql
@@ -9,7 +9,6 @@
 \. ./../../current_mysql/5.0/rice/KR_RICE_09_2.0.0-2.1.0_2012-04-16_B000.sql
 \. ./../../current_mysql/5.0/rice/KR_RICE_10_2.0.0-2.1.0_2012-04-16b_B000.sql
 \. ./../../current_mysql/5.0/rice/KR_RICE_11_2.0.0-2.1.0_2012-04-19_B000.sql
-\. ./../../current_mysql/5.0/rice/KR_RICE_12_2.0.0-2.1.0_2012-04-24_B000.sql
 \. ./../../current_mysql/5.0/rice/KR_RICE_13_2.0.0-2.1.0_2012-04-25_B000.sql
 \. ./../../current_mysql/5.0/rice/KR_RICE_14_2.0.0-2.1.0_2012-05-11_B000.sql
 \. ./../../current_mysql/5.0/rice/KR_RICE_15_2.0.0-2.1.0_2012-05-17_B000.sql
