bd09c491be3fdde154519f5ba2520edf6eb580ea
Jay Hulslander <jdh34@cornell.edu>
2012-06-07 19:55:08 +0000

    KRACOEUS-5511

--- src/main/java/org/kuali/kra/questionnaire/QuestionnaireMaintenanceDocumentRule.java
+++ src/main/java/org/kuali/kra/questionnaire/QuestionnaireMaintenanceDocumentRule.java
@@ -15,12 +15,18 @@
  */
 package org.kuali.kra.questionnaire;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.lang.StringUtils;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.rice.core.api.util.RiceKeyConstants;
 import org.kuali.rice.kns.document.MaintenanceDocument;
 import org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase;
+import org.kuali.rice.krad.service.BusinessObjectService;
 import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.util.MessageMap;
 
@@ -29,6 +35,8 @@ import org.kuali.rice.krad.util.MessageMap;
  * This class is to provide rule validation when questionnaire is to be approved.
  */
 public class QuestionnaireMaintenanceDocumentRule extends MaintenanceDocumentRuleBase {
+    
+    public static final String ALREADY_EDITED_ERROR = "error.questionnaire.alreadyEdited";
 
     /**
      * Constructs a CustomAttributeMaintenanceDocumentRule.java.
@@ -95,11 +103,48 @@ public class QuestionnaireMaintenanceDocumentRule extends MaintenanceDocumentRul
                 valid = false;
             }
         }
+        
+        if (newQuestionnaire.getObjectId() != null 
+                && checkForLatestQuuestionnaireSequenceNumber(newQuestionnaire.getQuestionnaireId(), newQuestionnaire.getSequenceNumber())) {
+            errorMap.putError("document.newMaintainableObject.businessObject.name", ALREADY_EDITED_ERROR, "");
+            valid = false;
+        }
+        
         return valid;
 
     }
+    
+    /**
+     * 
+     * This method determines if the passed in sequence number is the latest sequence for the passed in questionnaireId.
+     * @param questionnnaireId
+     * @param sequenceNumber
+     * @return
+     */
+    protected boolean checkForLatestQuuestionnaireSequenceNumber(String questionnnaireId, Integer sequenceNumber) {
+        Map fieldValues = new HashMap();
+        fieldValues.put("QUESTIONNAIRE_ID", questionnnaireId);
+        boolean sortAscending = true;
+        Collection<Questionnaire> questionnaires = getBusinessObjectService().findMatchingOrderBy(Questionnaire.class, fieldValues, "SEQUENCE_NUMBER", sortAscending);
+        boolean foundCurrentSequence = false;
+        boolean foundLargerSequence = false;
+        for (Questionnaire questionnarire : questionnaires) {
+            if (questionnarire.getSequenceNumber().equals(sequenceNumber)) {
+                foundCurrentSequence = true;
+            }
+            if (questionnarire.getSequenceNumber() > sequenceNumber) {
+                foundLargerSequence = true;
+            }
+        }
+        return foundCurrentSequence && !foundLargerSequence;
+    }
+    
     private QuestionnaireService getQuestionnaireService() {
         return KraServiceLocator.getService(QuestionnaireService.class);
+    }
+    
+    private BusinessObjectService getBusinessObjectService() {
+        return KraServiceLocator.getService(BusinessObjectService.class);
     }
 
 }
