8171fa730f6dd787965dda688c9038db1960f710
Gayathri Athreya <hsggwk@gmail.com>
2012-07-02 19:48:11 +0000

    Rice upgrade: 2.1 to 2.1.1 revision 33181

--- src/main/java/org/kuali/kra/coi/service/impl/CoiDisclosureReviewerDerivedRoleTypeServiceImpl.java
+++ src/main/java/org/kuali/kra/coi/service/impl/CoiDisclosureReviewerDerivedRoleTypeServiceImpl.java
@@ -28,6 +28,7 @@ import org.kuali.kra.coi.CoiUserRole;
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.infrastructure.RoleConstants;
 import org.kuali.kra.service.KcPersonService;
+import org.kuali.rice.core.api.exception.RiceIllegalArgumentException;
 import org.kuali.rice.core.api.membership.MemberType;
 import org.kuali.rice.kim.api.role.RoleMembership;
 import org.kuali.rice.kns.kim.role.DerivedRoleTypeServiceBase;
@@ -128,5 +129,21 @@ public class CoiDisclosureReviewerDerivedRoleTypeServiceImpl extends DerivedRole
     
     public void setKcPersonService(KcPersonService kcPersonService) {
         this.kcPersonService = kcPersonService;
+    }
+    
+    /*
+     * Should override if derivedRoles should not to be cached.
+     */
+    @Override
+    public boolean dynamicRoleMembership(String namespaceCode, String roleName) {
+        if (StringUtils.isBlank(namespaceCode)) {
+            throw new RiceIllegalArgumentException("namespaceCode was null or blank");
+        }
+
+        if (StringUtils.isBlank(roleName)) {
+            throw new RiceIllegalArgumentException("roleName was null or blank");
+        }
+
+        return true;
     }
 }
