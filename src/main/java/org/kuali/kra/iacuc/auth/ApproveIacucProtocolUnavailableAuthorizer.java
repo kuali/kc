80db57544624e2223094c5f024cee5df4ed99931
Neeraj Khanolkar <neerajsk@iastate.edu>
2012-07-06 23:28:29 +0000

    KCIAC-172: Added approve action functionality which is wired up to the RCD motion of approve. Also fixed issues with forwarding from the notification editor.

--- src/main/java/org/kuali/kra/iacuc/auth/ApproveIacucProtocolUnavailableAuthorizer.java
+++ src/main/java/org/kuali/kra/iacuc/auth/ApproveIacucProtocolUnavailableAuthorizer.java
@@ -13,39 +13,39 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.protocol.auth;
 
-import org.kuali.kra.committee.bo.CommitteeDecisionMotionType;
+package org.kuali.kra.iacuc.auth;
+
+import org.kuali.kra.common.committee.bo.CommitteeDecisionMotionType;
+import org.kuali.kra.iacuc.actions.IacucProtocolAction;
+import org.kuali.kra.iacuc.actions.IacucProtocolActionType;
+import org.kuali.kra.iacuc.actions.submit.IacucProtocolSubmission;
 import org.kuali.kra.infrastructure.PermissionConstants;
-import org.kuali.kra.irb.actions.ProtocolAction;
-import org.kuali.kra.irb.actions.ProtocolActionType;
-import org.kuali.kra.irb.actions.submit.ProtocolSubmission;
 
 /**
  * Is the user allowed to approve protocols and the action is currently unavailable?
  */
-public class ApproveProtocolUnavailableAuthorizer extends ProtocolAuthorizer {
+public class ApproveIacucProtocolUnavailableAuthorizer extends IacucProtocolAuthorizer {
 
     /**
      * {@inheritDoc}
      * @see org.kuali.kra.protocol.auth.ProtocolAuthorizer#isAuthorized(java.lang.String, org.kuali.kra.protocol.auth.ProtocolTask)
      */
-    public boolean isAuthorized(String userId, ProtocolTask task) {
-        /* -- commented as part of GENERATED CODE need to verify
-        ProtocolAction lastAction = task.getProtocol().getLastProtocolAction();
-        ProtocolSubmission lastSubmission = task.getProtocol().getProtocolSubmission();
+    public boolean isAuthorized(String userId, IacucProtocolTask task) {
+        IacucProtocolAction lastAction = (IacucProtocolAction) task.getProtocol().getLastProtocolAction();
+        IacucProtocolSubmission lastSubmission = (IacucProtocolSubmission) task.getProtocol().getProtocolSubmission();
         
-        return !canPerform(lastAction, lastSubmission) && 
-               hasPermission(userId, task.getProtocol(), PermissionConstants.MAINTAIN_PROTOCOL_SUBMISSIONS);
-               */
-        return false;
+        return !(canPerform(lastAction, lastSubmission) &&
+                 canExecuteAction(task.getProtocol(), IacucProtocolActionType.IACUC_APPROVED)) && 
+                 hasPermission(userId, task.getProtocol(), PermissionConstants.PERFORM_IACUC_ACTIONS_ON_PROTO);
     }
     
-    private boolean canPerform(ProtocolAction lastAction, ProtocolSubmission lastSubmission) {
+    
+    private boolean canPerform(IacucProtocolAction lastAction, IacucProtocolSubmission lastSubmission) {
         boolean canPerform = false;
         
         if (lastAction != null && lastSubmission != null) {
-            canPerform = ProtocolActionType.RECORD_COMMITTEE_DECISION.equals(lastAction.getProtocolActionTypeCode()) 
+            canPerform = IacucProtocolActionType.RECORD_COMMITTEE_DECISION.equals(lastAction.getProtocolActionTypeCode()) 
                       && CommitteeDecisionMotionType.APPROVE.equals(lastSubmission.getCommitteeDecisionMotionTypeCode());
         }
         
