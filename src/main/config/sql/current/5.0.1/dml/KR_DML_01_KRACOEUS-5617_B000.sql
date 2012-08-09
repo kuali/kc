39983ef2483d409f429d828f3d68db98c11ddc03
Anand Meher <akmeher@ucdavis.edu>
2012-08-09 17:34:41 +0000

    KRACOEUS-5617:Fixed the sql script to create parameter for proposal summary

--- src/main/config/sql/current/5.0.1/dml/KR_DML_01_KRACOEUS-5617_B000.sql
+++ src/main/config/sql/current/5.0.1/dml/KR_DML_01_KRACOEUS-5617_B000.sql
@@ -1,8 +1,3 @@
 INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
 VALUES ('KC-PD', 'Document', 'proposaldevelopment.proposal.summary.title', SYS_GUID(), 1, 'CONFG', 'Proposal Summary', 'Default value of Proposal Summary title', 'A', 'KC')
 /
-
-select * from krcr_parm_t where parm_nm='propSummarySpecialReview';
-update krcr_parm_t set val='4,5' where parm_nm='propSummarySpecialReview';
-
-select * from krcr_parm_t where parm_nm like 'valid%';
