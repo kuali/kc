7058d2e32d622d2670daff311f6d60d2c91c2246
Douglas Pace <dpace@kuali.org>
2012-08-14 18:24:18 +0000

    KCIAC-352 : Iacuc Correspondence Generation related services.

---
+++ src/main/java/org/kuali/kra/protocol/actions/correspondence/ProtocolActionCorrespondenceGenerationServiceImpl.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.protocol.actions.correspondence;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.kuali.kra.protocol.Protocol;
+import org.kuali.kra.protocol.actions.ProtocolAction;
+import org.kuali.kra.protocol.correspondence.ProtocolCorrespondence;
+import org.kuali.kra.protocol.correspondence.ProtocolCorrespondenceTemplate;
+import org.kuali.kra.printing.PrintingException;
+import org.kuali.kra.printing.service.PrintingService;
+import org.kuali.kra.proposaldevelopment.bo.AttachmentDataSource;
+import org.kuali.rice.core.api.datetime.DateTimeService;
+import org.kuali.rice.krad.service.BusinessObjectService;
+import org.kuali.rice.krad.util.GlobalVariables;
+
+/**
+ * 
+ * This class deals with making a protocol attachment from a template based on an action.
+ */
+public abstract class ProtocolActionCorrespondenceGenerationServiceImpl implements ProtocolActionCorrespondenceGenerationService {
+    
+    private BusinessObjectService businessObjectService;
+    private PrintingService printingService;
+    private ProtocolActionTypeToCorrespondenceTemplateService protocolActionTypeToCorrespondenceTemplateService;
+    private DateTimeService dateTimeService;
+
+    
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+    
+    protected BusinessObjectService getBusinessObjectService() {
+        return this.businessObjectService;
+    }
+    
+    public void setPrintingService(PrintingService printingService) {
+        this.printingService = printingService;
+    }
+    
+    protected PrintingService getPrintingService() {
+        return printingService;
+    }
+    
+    public void setProtocolActionTypeToCorrespondenceTemplateService(ProtocolActionTypeToCorrespondenceTemplateService protocolActionTypeToCorrespondenceTemplateService) {
+        this.protocolActionTypeToCorrespondenceTemplateService = protocolActionTypeToCorrespondenceTemplateService;
+    }
+    
+    protected abstract ProtocolCorrespondence getProtocolCorrespondenceHook();
+    
+    protected void buildAndAttachProtocolCorrespondence(Protocol protocol, byte[] data, String correspTypeCode) {
+        
+        ProtocolCorrespondence protocolCorrespondence = new ProtocolCorrespondence();
+        protocolCorrespondence.setProtocol(protocol);
+        protocolCorrespondence.setProtocolId(protocol.getProtocolId());
+        protocolCorrespondence.setProtocolNumber(protocol.getProtocolNumber());
+        protocolCorrespondence.setSequenceNumber(protocol.getSequenceNumber());
+        protocolCorrespondence.setProtoCorrespTypeCode(correspTypeCode);
+        
+        ProtocolAction lastAction = protocol.getLastProtocolAction();
+        protocolCorrespondence.setProtocolAction(lastAction);
+        protocolCorrespondence.setActionIdFk(lastAction.getProtocolActionId());
+        protocolCorrespondence.setCorrespondence(data); 
+        protocolCorrespondence.setActionId(lastAction.getActionId());
+
+        //What is Final flag used for? ANSWER: the final flag is used by the IRB admin to denote correspondences 
+        //that are ready to be sent/published to the PI etc.
+        protocolCorrespondence.setFinalFlag(false);
+        protocolCorrespondence.setCreateUser(GlobalVariables.getUserSession().getPrincipalName());
+        protocolCorrespondence.setCreateTimestamp(dateTimeService.getCurrentTimestamp());
+       
+        if(lastAction.getProtocolCorrespondences() == null) {
+            List<ProtocolCorrespondence> correspondences = new ArrayList<ProtocolCorrespondence>();
+            correspondences.add(protocolCorrespondence);
+            lastAction.setProtocolCorrespondences(correspondences);
+            protocol.refreshReferenceObject("protocolSubmissions");
+        } else {
+            lastAction.getProtocolCorrespondences().add(protocolCorrespondence);
+        }
+        
+        getBusinessObjectService().save(protocolCorrespondence);
+        
+        getBusinessObjectService().save(protocol);
+    }
+    
+    /**{@inheritDoc}**/
+    public void generateCorrespondenceDocumentAndAttach(AbstractProtocolActionsCorrespondence printableCorrespondence) 
+            throws PrintingException {
+            if (printableCorrespondence.getXSLTemplates().size() > 0) {
+                //there are templates in play, lets do some printing and attaching            
+                Protocol protocol = printableCorrespondence.getProtocol();
+                AttachmentDataSource ads = this.printingService.print(printableCorrespondence);
+                if (ads.getContent().length > 0) {
+                    //only need to do this, if there is actually printable correspondence to save
+                    //this may not be the case if a bad template is put in place, or under certain testing conditions.
+                    buildAndAttachProtocolCorrespondence(protocol, ads.getContent(), printableCorrespondence.getProtoCorrespTypeCode());
+                }
+            }
+        }
+        
+    public AttachmentDataSource reGenerateCorrespondenceDocument(AbstractProtocolActionsCorrespondence printableCorrespondence) 
+            throws PrintingException {
+            if (printableCorrespondence.getXSLTemplates().size() > 0) {
+                //there are templates in play, lets do some printing and attaching            
+                Protocol protocol = printableCorrespondence.getProtocol();
+                return this.printingService.print(printableCorrespondence);
+            }
+            return null;
+        }
+        
+    /**{@inheritDoc}**/
+    public List<ProtocolCorrespondenceTemplate> getCorrespondenceTemplates(String actionType) {
+        List<ProtocolCorrespondenceTemplate> templates = 
+            this.protocolActionTypeToCorrespondenceTemplateService.getTemplatesByProtocolAction(actionType);
+        return templates;
+    }
+
+    public void setDateTimeService(DateTimeService dateTimeService) {
+        this.dateTimeService = dateTimeService;
+    }
+}
