73ef6ae2349a95e8fb275781487aa7c1fdfcdc86
Anand Meher <akmeher@ucdavis.edu>
2012-08-14 21:38:26 +0000

    KRACOEUS-5617: Add system parameter to display title for Approver View

--- src/main/java/org/kuali/kra/proposaldevelopment/bo/DevelopmentProposal.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/bo/DevelopmentProposal.java
@@ -2228,10 +2228,4 @@ public KrmsRulesContext getKrmsRulesContext() {
     return (KrmsRulesContext) getProposalDocument();
 }
 
-public String getProposalSummaryTitle() {
-    String proposalSummaryTitle = this.getParameterService().getParameterValueAsString(
-           ProposalDevelopmentDocument.class, Constants.PARAMETER_PROPOSAL_SUMMARY_TITLE);
-    return proposalSummaryTitle;
-}
-
 }
