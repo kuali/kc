926318c7aeec217be3b6717b125cbf3efc817f23
Anand Meher <akmeher@ucdavis.edu>
2012-08-16 16:27:10 +0000

    KCIAC-333: Modified sql script to use namespace KC-IACUC

--- src/main/config/sql/current/5.0.1/dml/KR_DML_01_KCIAC-333_B000.sql
+++ src/main/config/sql/current/5.0.1/dml/KR_DML_01_KCIAC-333_B000.sql
@@ -1,11 +1,11 @@
 INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KC-PD', 'Document', 'iacuc.protocol.development.proposal.linking.enabled', SYS_GUID(), 1, 'CONFG', 'Y', 'Linking from Development Proposal to IACUC PROTOCOL Funding source is configurable at impl time', 'A', 'KC')
+VALUES ('KC-IACUC', 'Document', 'iacuc.protocol.proposal.development.linking.enabled', SYS_GUID(), 1, 'CONFG', 'Y', 'Linking from Proposal Development to IACUC PROTOCOL Funding source is configurable at impl time', 'A', 'KC')
 /
 
 INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KC-PD', 'Document', 'iacuc.protocol.institute.proposal.linking.enabled', SYS_GUID(), 1, 'CONFG', 'Y', 'Linking from Institution Proposal to IACUC PROTOCOL Funding source is configurable at impl time', 'A', 'KC')
+VALUES ('KC-IACUC', 'Document', 'iacuc.protocol.institute.proposal.linking.enabled', SYS_GUID(), 1, 'CONFG', 'Y', 'Linking from Institution Proposal to IACUC PROTOCOL Funding source is configurable at impl time', 'A', 'KC')
 /
 
 INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KC-PD', 'Document', 'iacuc.protocol.award.proposal.linking.enabled', SYS_GUID(), 1, 'CONFG', 'Y', 'Linking from Award to IACUC PROTOCOL Funding source is configurable at impl time', 'A', 'KC')
+VALUES ('KC-IACUC', 'Document', 'iacuc.protocol.award.linking.enabled', SYS_GUID(), 1, 'CONFG', 'Y', 'Linking from Award to IACUC PROTOCOL Funding source is configurable at impl time', 'A', 'KC')
 /
