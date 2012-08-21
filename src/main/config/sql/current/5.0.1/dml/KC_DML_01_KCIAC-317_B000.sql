9469e31479b69ee8f5039abceeb842b64ece85ef
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-08-21 23:56:57 +0000

    KCIAC-317: Implement notification for IACUC Review Not Required

--- src/main/config/sql/current/5.0.1/dml/KC_DML_01_KCIAC-317_B000.sql
+++ src/main/config/sql/current/5.0.1/dml/KC_DML_01_KCIAC-317_B000.sql
@@ -1,6 +1,3 @@
-INSERT INTO IACUC_PROTOCOL_ACTION_TYPE ( PROTOCOL_ACTION_TYPE_CODE, DESCRIPTION, TRIGGER_SUBMISSION, TRIGGER_CORRESPONDENCE, UPDATE_TIMESTAMP, UPDATE_USER, OBJ_ID )
-       VALUES ( '207', 'IACUC Protocol Review Not Required', 'N', 'N', SYSDATE, 'admin', SYS_GUID() )
-/
 INSERT INTO NOTIFICATION_TYPE (NOTIFICATION_TYPE_ID, MODULE_CODE, ACTION_CODE, DESCRIPTION, SUBJECT, MESSAGE, PROMPT_USER, SEND_NOTIFICATION, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID)
        VALUES (SEQ_NOTIFICATION_TYPE_ID.NEXTVAL, (SELECT MODULE_CODE FROM COEUS_MODULE WHERE DESCRIPTION='IACUC Protocol'),'207','IACUC Protocol Review Not Required','IACUC Protocol {PROTOCOL_NUMBER} Review Not Required Action','The protocol number <a title="" target="_self" href="{DOCUMENT_PREFIX}/kew/DocHandler.do?command=displayDocSearchView&amp;docId={DOCUMENT_NUMBER}">{PROTOCOL_NUMBER}</a>, Principal Investigator {PI_NAME} has had the action "Review Not Required" performed on it.<br />The action was executed by {USER_FULLNAME}.  Determination is due by {DUE_DATE}. Additional information and further actions can be accessed through the Kuali Coeus system.','Y','N','admin',SYSDATE,1,SYS_GUID())
 /
