0b5f8a4e7831a3a62427ceac7bb7e10e5b1bb78e
Neeraj Khanolkar <neerajsk@iastate.edu>
2012-08-30 00:41:46 +0000

    KCIAC-381:Switched the membership rolodex reference to Iacuc person rolodex, and added dummy fields to the Iacuc person rolodex BO to maintain compatibility with the kc person BO properties during rendering of the membership contact information tag.

--- src/main/java/org/kuali/kra/common/committee/bo/CommitteeMembership.java
+++ src/main/java/org/kuali/kra/common/committee/bo/CommitteeMembership.java
@@ -22,8 +22,8 @@ import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
 import org.kuali.kra.bo.KcPerson;
+import org.kuali.kra.iacuc.personnel.IacucProtocolPersonRolodex;
 import org.kuali.kra.infrastructure.KraServiceLocator;
-import org.kuali.kra.protocol.personnel.ProtocolPersonRolodex;
 import org.kuali.kra.service.KcPersonService;
 import org.kuali.kra.util.DateUtils;
 
@@ -68,7 +68,7 @@ public class CommitteeMembership extends CommitteeAssociate {
 
     private CommitteeMembershipType membershipType;
 
-    private ProtocolPersonRolodex rolodex;
+    private IacucProtocolPersonRolodex rolodex;
 
     private boolean delete;
 
@@ -241,11 +241,11 @@ public class CommitteeMembership extends CommitteeAssociate {
         return this.kcPersonService;
     }
 
-    public ProtocolPersonRolodex getRolodex() {
+    public IacucProtocolPersonRolodex getRolodex() {
         return rolodex;
     }
 
-    public void setRolodex(ProtocolPersonRolodex rolodex) {
+    public void setRolodex(IacucProtocolPersonRolodex rolodex) {
         this.rolodex = rolodex;
     }
 
