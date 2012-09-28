143699c43fa292d8d34795ec59dd07bdb427e2b8
Gayathri Athreya <hsggwk@gmail.com>
2012-09-28 23:17:23 +0000

    KCINFR-675: Set the cfda job to run every 2 hrs on intg to enable testing.

--- src/main/config/sql/current/5.1.0/dml/kcintg/KR_DML_01_KCINFR-675_00S0.sql
+++ src/main/config/sql/current/5.1.0/dml/kcintg/KR_DML_01_KCINFR-675_00S0.sql
@@ -2,4 +2,5 @@ update KRCR_PARM_T set VAL = 'ON' where parm_nm = 'FIN_SYSTEM_INTEGRATION_ON'
 /
 update KRCR_PARM_T set VAL = '2' where parm_nm = 'enable.award.FnA.validation'
 /
-
+update KRCR_PARM_T set VAL = '0 0 0/2 * * ?' WHERE PARM_NM = 'CFDA_BATCH_JOB_CRON_EXPRESSION'
+/
