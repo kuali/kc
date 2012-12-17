54d46ff6ab7983d85dad17f9129df01a329b9210
Geo Thomas <geot@mit.edu>
2012-12-17 19:10:11 +0000

    jira#KRACOEUS-5886

--- src/main/config/sql/current/5.1.0/dml/KR_DML_01_KRACOEUS-5886_0TSD.sql
+++ src/main/config/sql/current/5.1.0/dml/KR_DML_01_KRACOEUS-5886_0TSD.sql
@@ -2,6 +2,6 @@ insert into krim_role_mbr_t
 (role_mbr_id, role_id, mbr_id, mbr_typ_cd, last_updt_dt, ver_nbr, obj_id)
 values ((select (max(to_number(role_mbr_id)) + 1) from krim_role_mbr_t where role_mbr_id is not NULL and to_number(role_mbr_id) < 10000),
         (select role_id from krim_role_t where role_nm = 'Kuali Rules Management System Administrator' and nmspc_cd = 'KR-RULE'),
-        (select prncpl_id from krim_prncpl_t where prncpl_nm = 'admin'),
+        (select prncpl_id from krim_prncpl_t where prncpl_nm = 'quickstart'),
         'P', current_date, 1, sys_guid())
 /
