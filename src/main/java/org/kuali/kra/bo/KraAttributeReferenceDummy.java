df140557d249c08afed04d78a7a0befa9e8e102f
Chuck Tharp <grover.tharp@mail.wvu.edu>
2012-12-21 04:24:31 +0000

    KRACOEUS-6047: Document header missing labels for Committee ID and Committee Name

--- src/main/java/org/kuali/kra/bo/KraAttributeReferenceDummy.java
+++ src/main/java/org/kuali/kra/bo/KraAttributeReferenceDummy.java
@@ -43,7 +43,8 @@ public class KraAttributeReferenceDummy extends AttributeReferenceDummy {
     private Long sortId;
     private String reporter;
     private Timestamp createTimestamp;
-
+    private String committeeId;
+    private String committeeName;
 
     /**
      * Gets the value of city
@@ -230,6 +231,22 @@ public class KraAttributeReferenceDummy extends AttributeReferenceDummy {
 
     public void setCreateTimestamp(Timestamp createTimestamp) {
         this.createTimestamp = createTimestamp;
+    }
+
+    public String getCommitteeId() {
+        return committeeId;
+    }
+
+    public void setCommitteeId(String committeeId) {
+        this.committeeId = committeeId;
+    }
+
+    public String getCommitteeName() {
+        return committeeName;
+    }
+
+    public void setCommitteeName(String committeeName) {
+        this.committeeName = committeeName;
     }
 
 }
