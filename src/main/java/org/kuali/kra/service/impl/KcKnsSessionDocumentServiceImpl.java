ce8963c7ed940fd7bf422f746bcb673ddf16c765
Gayathri Athreya <hsggwk@gmail.com>
2012-12-25 23:41:47 +0000

    KCINFR-698: Overriding Rice's session document service.

---
+++ src/main/java/org/kuali/kra/service/impl/KcKnsSessionDocumentServiceImpl.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.service.impl;
+
+import java.sql.Timestamp;
+
+import org.kuali.rice.kew.api.WorkflowDocument;
+import org.kuali.rice.kns.service.SessionDocumentService;
+import org.kuali.rice.kns.web.struts.form.KualiDocumentFormBase;
+import org.kuali.rice.krad.UserSession;
+
+public class KcKnsSessionDocumentServiceImpl implements SessionDocumentService {
+
+    @Override
+    public WorkflowDocument getDocumentFromSession(UserSession userSession, String docId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addDocumentToUserSession(UserSession userSession, WorkflowDocument document) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void purgeDocumentForm(String documentNumber, String docFormKey, UserSession userSession, String ipAddress) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void purgeAllSessionDocuments(Timestamp expirationDate) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public KualiDocumentFormBase getDocumentForm(String documentNumber, String docFormKey, UserSession userSession, String ipAddress) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setDocumentForm(KualiDocumentFormBase form, UserSession userSession, String ipAddress) {
+        // TODO Auto-generated method stub
+        
+    }
+
+   
+
+}
