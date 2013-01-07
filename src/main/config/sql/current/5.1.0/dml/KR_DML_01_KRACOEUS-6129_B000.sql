2a3aec7347f7f078563a4060c8ea6fcb12828afd
Douglas Pace <dpace@kuali.org>
2013-01-07 18:59:26 +0000

    KRACOEUS-6129 : Hide PD param renamed and used as the default description when populated.

--- src/main/config/sql/current/5.1.0/dml/KR_DML_01_KRACOEUS-6129_B000.sql
+++ src/main/config/sql/current/5.1.0/dml/KR_DML_01_KRACOEUS-6129_B000.sql
@@ -1,4 +1,4 @@
 INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KC-PD', 'Document', 'HIDE_PROP_DEV_DOC_DESCRIPTION_PANEL', SYS_GUID(), 1, 'CONFG', 'Y', 
-	'Set to Y to hide the document description panel in Proposal Development and default the document description. N to turn off this behavior.', 'A', 'KC')
+VALUES ('KC-PD', 'Document', 'HIDE_AND_DEFAULT_PROP_DEV_DOC_DESC', SYS_GUID(), 1, 'CONFG', 'Proposal created: ', 
+	'When set to a non-blank value, will hide the document description panel on Proposal Development documents and default the document description to the value specified with the current timestamp appended. When empty, the document description panel will be displayed and the user will need to enter a value.', 'A', 'KC')
 /
