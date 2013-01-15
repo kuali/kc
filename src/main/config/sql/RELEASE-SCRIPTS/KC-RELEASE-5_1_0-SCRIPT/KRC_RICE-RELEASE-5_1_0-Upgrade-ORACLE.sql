82d6144da1f247752592d664c1ace37393f25ce7
Allen Akers <allen.akers@colostate.edu>
2013-01-15 20:06:16 +0000

    Updating install scripts in preparation for Boston F2F

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_1_0-SCRIPT/KRC_RICE-RELEASE-5_1_0-Upgrade-ORACLE.sql
@@ -0,0 +1,6 @@
+set define off
+set sqlblanklines on
+spool KRC_RICE-RELEASE-5_1_0-Upgrade-ORACLE-Install.log
+@../../current/5.1.0/rice/KRC_RICE_01_2.1.2-2.1.3_2012-12-12_B000.sql
+commit;
+exit
---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_1_0-SCRIPT/KR_RICE-RELEASE-5_1_0-Upgrade-ORACLE.sql
@@ -0,0 +1,14 @@
+set define off
+set sqlblanklines on
+spool KR_RICE-RELEASE-5_1_0-Upgrade-ORACLE-Install.log
+@../../current/5.1.0/rice/KR_RICE_01_2.1.0-2.1.2_2012-09-26_B000.sql
+@../../current/5.1.0/rice/KR_RICE_02_2.1.0-2.1.2_2012-10-12_B000.sql
+@../../current/5.1.0/rice/KR_RICE_03_2.1.0-2.1.2_2012-10-17_B000.sql
+@../../current/5.1.0/rice/KR_RICE_04_2.1.0-2.1.2_2012-10-19_B000.sql
+@../../current/5.1.0/rice/KR_RICE_05_2.1.0-2.1.2_2012-10-25_B000.sql
+@../../current/5.1.0/rice/KR_RICE_06_2.1.2-2.1.3_2012-10-23_B000.sql
+@../../current/5.1.0/rice/KR_RICE_08_2.1.2-2.1.3_2012-11-15_B000.sql
+@../../current/5.1.0/rice/KR_RICE_09_2.1.2-2.1.3_2012-11-28_B000.sql
+@../../current/5.1.0/rice/KR_RICE_10_2.1.2-2.1.3_2012-12-12_B000.sql
+commit;
+exit
