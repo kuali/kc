3d6e179c8b24558e34e026d2a05006471842810a
Allen Akers <allen.akers@colostate.edu>
2013-01-24 20:47:13 +0000

    Changing delimiter lines for CI build

--- src/main/config/sql/current_mysql/5.0/rice/KR_RICE_99_SEQ_RESET_BS.sql
+++ src/main/config/sql/current_mysql/5.0/rice/KR_RICE_99_SEQ_RESET_BS.sql
@@ -1,6 +1,6 @@
-drop procedure if exists p;
+drop procedure if exists p
+/
 
-delimiter //
 create procedure p ()
 begin
   declare l_new_seq INT;
@@ -162,9 +162,10 @@ begin
   prepare alter_seq_stmt from @alter_seq;
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
-end //
-delimiter ;
+end /
 
-call p ();
+call p ()
+/
 
-drop procedure if exists p;
+drop procedure if exists p
+/
