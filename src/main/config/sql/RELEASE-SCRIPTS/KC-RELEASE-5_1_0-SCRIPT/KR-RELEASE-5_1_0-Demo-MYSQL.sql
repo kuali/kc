2cd75e3a2079f74da88282e9c6d4a6cfdf586076
Allen Akers <allen.akers@colostate.edu>
2013-01-24 19:04:55 +0000

    First pass at converting existing Oracle sql to MySQL

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_1_0-SCRIPT/KR-RELEASE-5_1_0-Demo-MYSQL.sql
@@ -0,0 +1,5 @@
+\. ./../../current_mysql/5.1.0/dml/KR_DML_01_KRACOEUS-5886_0TSD.sql
+\. ./../../current_mysql/5.1.0/dml/KR_DML_01_KRACOEUS-5892_00SD.sql
+\. ./../../current_mysql/5.1.0/dml/KR_DML_02_KRACOEUS-6078_00SD.sql
+commit;
+exit
