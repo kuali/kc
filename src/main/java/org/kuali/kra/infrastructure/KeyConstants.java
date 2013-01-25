8411e378b43c059144e5664fb7aeacff06acdfcb
Chuck Tharp <grover.tharp@mail.wvu.edu>
2013-01-25 16:43:23 +0000

    KRAFDBCK-8005: Change error messages to match panel text

--- src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
+++ src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
@@ -405,7 +405,9 @@ public final class KeyConstants {
     public static final String ERROR_ANTICIPATED_AMOUNT_NEGATIVE = "error.award.anticipated.negative";
     public static final String ERROR_DUPLICATE_AWARD_TRANSFERRING_SPONSOR = "error.sponsor.duplicate";
     public static final String ERROR_AWARD_EFFECTIVE_DATE = "error.award.effective.date";
+    public static final String ERROR_AWARD_EFFECTIVE_DATE_TOTAL = "error.award.effective.date.total";
     public static final String ERROR_OBLIGATION_EXPIRATION_DATE = "error.obligation.expiration.date";
+    public static final String ERROR_OBLIGATION_EXPIRATION_DATE_TOTAL = "error.obligation.expiration.date.total";
     public static final String ERROR_INVALID_CFDA = "error.award.cfda.invalid";
     public static final String ERROR_AWARD_EFFECTIVE_DATE_TIME_AND_MONEY = "error.award.effective.date.timeAndMoney";
     
