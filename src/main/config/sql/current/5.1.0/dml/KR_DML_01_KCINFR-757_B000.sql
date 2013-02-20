353b9da6d285a920d8760dac7330901d722ca711
Gayathri Athreya <hsggwk@gmail.com>
2013-02-20 00:17:28 +0000

    KCINFR-764: Adding url namespaces to services that were left out.

--- src/main/config/sql/current/5.1.0/dml/KR_DML_01_KCINFR-757_B000.sql
+++ src/main/config/sql/current/5.1.0/dml/KR_DML_01_KCINFR-757_B000.sql
@@ -1,2 +1,12 @@
 update KRIM_TYP_T set SRVC_NM = '{http://kc.kuali.org/core/v5_0}'||SRVC_NM where NM LIKE 'Derived Role%' AND NMSPC_CD LIKE 'KC-%'
 /
+update KRIM_TYP_T set SRVC_NM = '{http://kc.kuali.org/core/v5_0}'||SRVC_NM where SRVC_NM = 'unitHierarchyRoleTypeService'
+/
+update KRIM_TYP_T set SRVC_NM = '{http://kc.kuali.org/core/v5_0}'||SRVC_NM where SRVC_NM = 'unitRoleTypeService'
+/
+update KRIM_TYP_T set SRVC_NM = '{http://kc.kuali.org/core/v5_0}'||SRVC_NM where SRVC_NM = 'protocolApproverRoleTypeService'
+/
+update KRMS_TYP_T set SRVC_NM = '{http://kc.kuali.org/core/v5_0}'||SRVC_NM where NMSPC_CD like '%KC-%'
+/
+update KREW_TYP_T set SRVC_NM = '{http://kc.kuali.org/core/v5_0}'||SRVC_NM where NMSPC_CD like '%KC-%'
+/
