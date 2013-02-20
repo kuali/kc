f695c7c029594fb9f4ce9dde09c03de0c9136c98
Gayathri Athreya <hsggwk@gmail.com>
2013-02-20 00:17:28 +0000

    KCINFR-764: Adding url namespaces to services that were left out.

--- src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KCINFR-757_B000.sql
+++ src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KCINFR-757_B000.sql
@@ -1,4 +1,14 @@
 DELIMITER /
 update KRIM_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where NM LIKE 'Derived Role%' AND NMSPC_CD LIKE 'KC-%'
 /
+update KRIM_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where SRVC_NM = 'unitHierarchyRoleTypeService'
+/
+update KRIM_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where SRVC_NM = 'unitRoleTypeService'
+/
+update KRIM_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where SRVC_NM = 'protocolApproverRoleTypeService'
+/
+update KRMS_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where NMSPC_CD like '%KC-%'
+/
+update KREW_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where NMSPC_CD like '%KC-%'
+/
 DELIMITER ;
