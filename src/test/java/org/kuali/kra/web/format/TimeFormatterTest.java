4a670937e50c96d2b6fa3cc53b0807db9a592447
Jay Hulslander <jdh34@cornell.edu>
2013-02-22 15:06:55 +0000

    KRACOEUS-5694

---
+++ src/test/java/org/kuali/kra/web/format/TimeFormatterTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.web.format;
+
+import static org.junit.Assert.assertEquals;
+
+import org.drools.command.assertion.AssertEquals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TimeFormatterTest {
+    
+    private TimeFormatter formatter;
+    private static String[][] valueChecks = { { "11:59 AM", "11:59 AM" }, { "01:59 AM", "1:59 AM" }, { "1:59 AM", "1:59 AM" },
+        { "1AM", "1:00 AM" }, { "3:00", "3:00 AM" }, { "3", "3:00 AM" }, { "12:00", "12:00 PM" }, { "17:00", "5:00 PM" },
+        { "1730", "5:30 PM" }, { "0800", "8:00 AM" }, { "5P", "5:00 PM" }, { "6:00a", "6:00 AM" }, { "6:00AM", "6:00 AM" },
+        { "8.30", "8:30 AM" }, { "2 p.m.", "2:00 PM" }, { "00:00", "12:00 AM" }, {null, ""}, {"23:36 pm", "11:36 PM"}, {"23:36 aM", "11:36 PM"} };
+    
+    @Before
+    public void setUp() throws Exception {
+        formatter = new TimeFormatter();
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        formatter = null;
+    }
+    
+    @Test
+    public void testConvertToObject() {
+        for (String[] values : valueChecks) {
+            String canonicalized = (String) formatter.convertToObject(values[0]);
+            //System.err.println("Original Value: '" + values[0] + "'  calculated value: '" + canonicalized + "'  expected value: '" + values[1] + "'");
+            assertEquals(values[1], canonicalized);
+        }
+    }
+}
