898bc39f5e14e55a803a8970b9b27b491d521fee
Geo Thomas <geot@mit.edu>
2013-02-25 20:56:00 +0000

    jira#KRACOEUS-5873 - Pre commit of refactored code to implement krms term functions by using KRMS Type service

---
+++ src/main/java/org/kuali/kra/krms/service/impl/FunctionTermResolverTypeServiceBase.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2005-2010 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.krms.service.impl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.krms.FunctionTermResolver;
+import org.kuali.rice.krad.service.BusinessObjectService;
+import org.kuali.rice.krms.api.engine.TermResolver;
+import org.kuali.rice.krms.api.repository.function.FunctionDefinition;
+import org.kuali.rice.krms.api.repository.function.FunctionParameterDefinition;
+import org.kuali.rice.krms.api.repository.function.FunctionRepositoryService;
+import org.kuali.rice.krms.api.repository.term.TermResolverDefinition;
+import org.kuali.rice.krms.framework.type.TermResolverTypeService;
+
+/**
+ * 
+ * This class implements <code>TermResolverTypeService</code> for resolving Stored Functions or Java Functions as Terms.
+ */
+public abstract class FunctionTermResolverTypeServiceBase implements TermResolverTypeService {
+    private BusinessObjectService businessObjectService;
+    public BusinessObjectService getBusinessObjectService() {
+        return businessObjectService;
+    }
+    public void setBusinessObjectService(BusinessObjectService businessObjectService) {
+        this.businessObjectService = businessObjectService;
+    }
+    public abstract FunctionTermResolver createFunctionResolver(List<String> orderedInputParams, Set<String> parameterNames, 
+                                                                    String output, FunctionDefinition functionTerm);
+    
+    @Override
+    public TermResolver<?> loadTermResolver(TermResolverDefinition termResolverDefinition) {
+        String functionId = termResolverDefinition.getOutput().getName();
+        FunctionRepositoryService functionRepositoryService = KraServiceLocator.getService("functionRepositoryService");
+        FunctionDefinition functionTerm = functionRepositoryService.getFunction(functionId);
+        List<String> params = getFunctionParameters(functionTerm);
+        Set<String> paramNames = termResolverDefinition.getParameterNames();
+        return createFunctionResolver(params,paramNames,functionId,functionTerm);
+    }
+    private List<String> getFunctionParameters(FunctionDefinition functionTerm) {
+        List<FunctionParameterDefinition> functionParams = functionTerm.getParameters();
+        List<FunctionParameterDefinition> modifiableParams = new ArrayList<FunctionParameterDefinition>(functionParams);
+        Collections.sort(modifiableParams, new ParamComparator());
+        List<String> params = new ArrayList<String>();
+        for (FunctionParameterDefinition termResolverFunctionParamDefinition : modifiableParams) {
+            params.add(termResolverFunctionParamDefinition.getName());
+        }
+        return params;
+    }
+    class  ParamComparator implements Comparator<FunctionParameterDefinition> {
+        @Override
+        public int compare(FunctionParameterDefinition param1, FunctionParameterDefinition param2) {
+            return param1.getSequenceNumber().compareTo(param2.getSequenceNumber());
+        }
+
+    }
+}
