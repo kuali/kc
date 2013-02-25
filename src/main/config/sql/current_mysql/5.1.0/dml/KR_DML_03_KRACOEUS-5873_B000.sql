3b459cbfd9525614bcddee5a4744e6416ac2caca
Geo Thomas <geot@mit.edu>
2013-02-25 21:09:36 +0000

    jira#KRACOEUS-5873

--- src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_03_KRACOEUS-5873_B000.sql
+++ src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_03_KRACOEUS-5873_B000.sql
@@ -1,12 +1,4 @@
 DELIMITER /
-update KRMS_TYP_T set SRVC_NM='{http://kc.kuali.org/core/v5_0}javaFunctionTermResolverTypeService' where SRVC_NM = 'functionTermResolverTypeService'
-/
-insert into KRMS_TYP_T values ('KC1005','Stored Function Term Resolver Type Service','KC-PD','{http://kc.kuali.org/core/v5_0}storedFunctionTermResolverTypeService','Y',1)
-/
-insert into KRMS_TYP_T values ('KC1006','ProposalDevelopment Java Function Term Service','KC-PD','krmsPropDevJavaFunctionTermService','Y',1)
-/
-insert into KRMS_TYP_T values ('KC1007','IRB Java Function Term Service','KC-PD','krmsIrbJavaFunctionTermService','Y',1)
-/
 update KRMS_TERM_SPEC_T A set NM = (select FUNC_ID from KRMS_FUNC_T B where B.NM=A.NM) 
   where exists (select FUNC_ID from KRMS_FUNC_T C where C.NM=A.NM)
 /
