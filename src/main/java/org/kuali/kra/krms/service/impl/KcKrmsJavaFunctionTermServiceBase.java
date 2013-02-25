7789cf854d5925055691b2302d1a824fb79aa605
Geo Thomas <geot@mit.edu>
2013-02-25 20:56:00 +0000

    jira#KRACOEUS-5873 - Pre commit of refactored code to implement krms term functions by using KRMS Type service

--- src/main/java/org/kuali/kra/krms/service/impl/KcKrmsJavaFunctionTermServiceBase.java
+++ src/main/java/org/kuali/kra/krms/service/impl/KcKrmsJavaFunctionTermServiceBase.java
@@ -24,24 +24,25 @@ import org.kuali.rice.krad.service.DocumentService;
 public abstract class KcKrmsJavaFunctionTermServiceBase {
     public static final String TRUE = "true";
     public static final String FALSE = "false";
-    public static final String[] restrictedElements = {" ", "`", "@", "#", "!", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{", "}", 
-            "|", "\\", "/", "?", "<", ">", ",", ";", ":", "'", "\"", "`", "+"};    
+    public static final String[] restrictedElements = { " ", "`", "@", "#", "!", "$", "%", "^", "&", "*", "(", ")", "[", "]", "{",
+            "}", "|", "\\", "/", "?", "<", ">", ",", ";", ":", "'", "\"", "`", "+" };
     private BusinessObjectService businessObjectService;
     private UnitService unitService;
     private ParameterService parameterService;
     private DocumentService documentService;
-    
+
     protected String[] buildArrayFromCommaList(String commaList) {
-        String[] newArray = commaList.split(","); //MIT Equity Interests
-        if(commaList!=null && newArray.length==0){
-            newArray = new String[]{commaList.trim()};
+        String[] newArray = commaList.split(","); // MIT Equity Interests
+        if (commaList != null && newArray.length == 0) {
+            newArray = new String[] { commaList.trim() };
         }
         return newArray;
     }
-    
+
     /**
      * 
      * This method returns 'true' if 'stringToCheck' does not contain a special character, otherwise returns 'false'.
+     * 
      * @param stringToCheck
      * @return
      */
@@ -53,7 +54,7 @@ public abstract class KcKrmsJavaFunctionTermServiceBase {
         }
         return TRUE;
     }
-    
+
     public BusinessObjectService getBusinessObjectService() {
         return businessObjectService;
     }
@@ -61,7 +62,7 @@ public abstract class KcKrmsJavaFunctionTermServiceBase {
     public void setBusinessObjectService(BusinessObjectService businessObjectService) {
         this.businessObjectService = businessObjectService;
     }
-    
+
     public UnitService getUnitService() {
         return unitService;
     }
