a323f0fba55b00508b6ac3da4bf2c1a208264f03
Chris Denne <chris.denne@colostate.edu>
2013-03-07 18:47:13 +0000

    KRACOEUS-6267

--- src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KRACOEUS-6267_0TSD.sql
+++ src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KRACOEUS-6267_0TSD.sql
@@ -1,6 +1,6 @@
 DELIMITER /
 UPDATE KRIM_ENTITY_EMAIL_T 
 SET EMAIL_ADDR = 'kc.grants.gov@kuali.org' 
-WHERE ENTITY_ID=(select ENTITY_ID from krdev.KRIM_PRNCPL_T where PRNCPL_NM='quickstart')
+WHERE ENTITY_ID=(select ENTITY_ID from KRIM_PRNCPL_T where PRNCPL_NM='quickstart')
 /
 DELIMITER ;
