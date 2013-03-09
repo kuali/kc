22866eb8602a7a0d2e215a352e7b5d23f97e5089
Gayathri Athreya <hsggwk@gmail.com>
2013-03-09 20:10:26 +0000

    KCINFR-764: Removing addition to CONTEXT in KRMS_TYP_T

--- src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KCINFR-757_B000.sql
+++ src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KCINFR-757_B000.sql
@@ -7,7 +7,9 @@ update KRIM_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM
 /
 update KRIM_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where SRVC_NM = 'protocolApproverRoleTypeService'
 /
-update KRMS_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where NMSPC_CD like '%KC-KRMS%'
+update KRMS_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where NM= 'Unit Agenda'
+/
+update KRMS_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where NM= 'Question Term Resolver Type Service'
 /
 update KREW_TYP_T set SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}',SRVC_NM) where NMSPC_CD like '%KC-%'
 /
