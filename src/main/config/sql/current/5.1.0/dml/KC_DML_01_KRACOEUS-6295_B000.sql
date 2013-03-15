dc649555035cbee2d3f1e08553d3fbca9f3e119a
Chuck Tharp <grover.tharp@mail.wvu.edu>
2013-03-15 14:53:56 +0000

    KRACOEUS-6295: Tweak text for Special Review notifications in Award

--- src/main/config/sql/current/5.1.0/dml/KC_DML_01_KRACOEUS-6295_B000.sql
+++ src/main/config/sql/current/5.1.0/dml/KC_DML_01_KRACOEUS-6295_B000.sql
@@ -1,9 +1,13 @@
+UPDATE NOTIFICATION_TYPE SET SUBJECT='Human Subjects Usage Special Review Inserted - {AWARD_NUMBER}' WHERE MODULE_CODE='1' AND ACTION_CODE='552'
+/
 UPDATE NOTIFICATION_TYPE SET MESSAGE='Human Subjects Special Review Inserted - {AWARD_NUMBER}' WHERE MODULE_CODE='1' AND ACTION_CODE='552'
+/
+UPDATE NOTIFICATION_TYPE SET SUBJECT='Human Subjects Usage Special Review Removed - {AWARD_NUMBER}' WHERE MODULE_CODE='1' AND ACTION_CODE='553'
 /
 UPDATE NOTIFICATION_TYPE SET MESSAGE='Human Subjects Special Review Removed - {AWARD_NUMBER}' WHERE MODULE_CODE='1' AND ACTION_CODE='553'
 /
 INSERT INTO NOTIFICATION_TYPE (NOTIFICATION_TYPE_ID, MODULE_CODE, ACTION_CODE, DESCRIPTION, SUBJECT, MESSAGE, PROMPT_USER, SEND_NOTIFICATION, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID)
-       VALUES(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,1,554,'Sent when a protocol is added as a special review in an award, that will result in IACUC linking.','Animal Usage Special Review Inserted - {AWARD_NUMBER}','Animal Usage Special Review Inserted - {AWARD_NUMBER}','Y','Y','admin',SYSDATE,1,SYS_GUID())
+       VALUES(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,1,554,'Sent when an IACUC protocol is added as a special review in an award, that will result in IACUC linking.','Animal Usage Special Review Inserted - {AWARD_NUMBER}','Animal Usage Special Review Inserted - {AWARD_NUMBER}','Y','Y','admin',SYSDATE,1,SYS_GUID())
 /
 INSERT INTO NOTIFICATION_TYPE (NOTIFICATION_TYPE_ID, MODULE_CODE, ACTION_CODE, DESCRIPTION, SUBJECT, MESSAGE, PROMPT_USER, SEND_NOTIFICATION, UPDATE_USER, UPDATE_TIMESTAMP, VER_NBR, OBJ_ID)
        VALUES(SEQ_NOTIFICATION_TYPE_ID.NEXTVAL,1,555,'Send when a linked IACUC protocol is removed from the award.','Animal Usage Special Review Removed - {AWARD_NUMBER}','Animal Usage Special Review Removed - {AWARD_NUMBER}','Y','Y','admin',SYSDATE,1,SYS_GUID())
