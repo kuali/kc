cbae23f5b42f56f71c21a9a3d487101787f96ed3
Chuck Tharp <grover.tharp@mail.wvu.edu>
2013-03-15 02:41:17 +0000

    KRACOEUS-6308: Fix unit test for number of Award attributes

--- src/test/java/org/kuali/kra/award/home/AwardTest.java
+++ src/test/java/org/kuali/kra/award/home/AwardTest.java
@@ -39,7 +39,7 @@ import org.kuali.rice.core.api.util.type.KualiDecimal;
 public class AwardTest { 
     private static final double DELTA = 0.001;
 
-    private static final int AWARD_ATTRIBUTES_COUNT = 125;
+    private static final int AWARD_ATTRIBUTES_COUNT = 129;
     private static final String PERSON_ID = "1001";
     private static final String KP_PERSON_ID = "1002";
     private static final int ROLODEX_ID = 1002;
