d5c2212725a4f11963947220ab5d419214845e92
Douglas Pace <dpace@kuali.org>
2013-03-18 18:16:40 +0000

    KRACOEUS-5762 : Fix staged sql for irb workflow authorizer to not affect IACUC.

--- src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KRACOEUS-5762_0TS0.sql
+++ src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KRACOEUS-5762_0TS0.sql
@@ -1,5 +1,5 @@
 DELIMITER /
-update KREW_DOC_TYP_T set AUTHORIZER = 'org.kuali.kra.irb.auth.ProtocolWorkflowDocumentAuthorizer' where  DOC_TYP_NM LIKE '%ProtocolDocument%' and CUR_IND = 1
+update KREW_DOC_TYP_T set AUTHORIZER = 'org.kuali.kra.irb.auth.ProtocolWorkflowDocumentAuthorizer' where  DOC_TYP_NM = 'ProtocolDocument' and CUR_IND = 1
 /
 
 DELIMITER ;
