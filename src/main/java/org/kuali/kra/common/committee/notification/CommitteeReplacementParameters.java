47be26ed0910aff124a10e1064ffcccfe25aa4fb
Jisha Mariam Saju <jisha@polussoftware.com>
2013-03-22 14:28:31 +0000

    KRACOEUS-6331

--- src/main/java/org/kuali/kra/common/committee/notification/CommitteeReplacementParameters.java
+++ src/main/java/org/kuali/kra/common/committee/notification/CommitteeReplacementParameters.java
@@ -27,13 +27,14 @@ public class CommitteeReplacementParameters {
     public static final String ACTION_TAKEN = "{ACTION_TAKEN}";
     public static final String LAST_ACTION_DATE = "{LAST_ACTION_DATE}";
     public static final String OBJECT_INDEX = "{OBJECT_INDEX}";
-
+    public static final String SCHEDULE_ID = "{SCHEDULE_ID}";
     
     public static final String[] REPLACEMENT_PARAMETERS = new String[] { DOCUMENT_NUMBER,
                                                                          SEQUENCE_NUMBER, 
                                                                          COMMITTEE_NAME,
                                                                          ACTION_TAKEN, 
                                                                          LAST_ACTION_DATE,
-                                                                         OBJECT_INDEX
+                                                                         OBJECT_INDEX,
+                                                                         SCHEDULE_ID
                                                                        };
 }
