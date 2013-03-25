c66d5da7859135030856e52208dbfc6800f2106d
Allen Akers <allen.akers@colostate.edu>
2013-03-25 16:49:22 +0000

    Modified release scripts

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_1_0-SCRIPT/KR_RICE-RELEASE-5_1_0-Upgrade-MYSQL.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_1_0-SCRIPT/KR_RICE-RELEASE-5_1_0-Upgrade-MYSQL.sql
@@ -8,5 +8,9 @@
 \. ./../../current_mysql/5.1.0/rice/KR_RICE_08_2.1.2-2.1.3_2012-11-15_B000.sql
 \. ./../../current_mysql/5.1.0/rice/KR_RICE_09_2.1.2-2.1.3_2012-11-28_B000.sql
 \. ./../../current_mysql/5.1.0/rice/KR_RICE_10_2.1.2-2.1.3_2012-12-12_B000.sql
+\. ./../../current_mysql/5.1.0/rice/KR_RICE_11_2.1.2-2.1.3_2013-01-09_B000.sql
+\. ./../../current_mysql/5.1.0/rice/KR_RICE_12_2.1.2-2.1.3_2013-01-16_B000.sql
+\. ./../../current_mysql/5.1.0/rice/KR_RICE_13_2.1.2-2.1.3_2013-01-14_B000.sql
+\. ./../../current_mysql/5.1.0/rice/KR_RICE_14_2.1.2-2.1.3_2013-02-19_B000.sql
 commit;
 exit
