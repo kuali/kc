bff02d44d2c7100383500d916a45d5c95e422f01
Allen Akers <allen.akers@colostate.edu>
2013-03-28 22:47:49 +0000

    Updating Oracle database release scripts for separately upgraded Rice

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_1_1-SCRIPT/oracle/dml/SR/KR_DML_02_KRACOEUS-4414_B000.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO KRCR_PARM_T (nmspc_cd, CMPNT_CD, parm_nm, OBJ_ID, VER_NBR, parm_typ_cd, VAL, parm_desc_txt, EVAL_OPRTR_CD)  VALUES ('KC-B','Document','ENABLE_COST_SHARE_SUBMIT',sys_guid(),1,'CONFG','1','Enable schools the option to submit or not-submit cost sharing at the summary and \/ or line item levels','A')
+/
+COMMIT
+/
+
---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_2-SCRIPT/oracle/dml/SR/KR_DML_01_KCIRB-1475_B000.sql
@@ -0,0 +1,7 @@
+--
+--
+
+
+INSERT INTO KRCR_PARM_T (APPL_ID,NMSPC_CD      ,PARM_TYP_CD,CMPNT_CD,PARM_NM                       ,PARM_DESC_TXT                    ,VAL                                    ,EVAL_OPRTR_CD,OBJ_ID    ,VER_NBR)
+   VALUES               ('KC'         ,'KC-PROTOCOL' ,'HELP'     ,'Document'     ,'protocolPermissionsUsersHelp','Protocol Permissions Users Help','default.htm?turl=Documents/users1.htm','A'    ,SYS_GUID(),1      )
+/
