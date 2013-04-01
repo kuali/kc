e53f70d23ff143a600842107a77fe632e813163e
Allen Akers <allen.akers@colostate.edu>
2013-04-01 03:16:52 +0000

    Updating release scripts

--- src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KRACOEUS-5873_B000.sql
+++ src/main/config/sql/current_mysql/5.1.0/dml/KR_DML_01_KRACOEUS-5873_B000.sql
@@ -5,8 +5,6 @@ insert into KRMS_TYP_T values ('KC1005','Stored Function Term Resolver Type Serv
 /
 insert into KRMS_TYP_T values ('KC1006','ProposalDevelopment Java Function Term Service','KC-PD','krmsPropDevJavaFunctionTermService','Y',1)
 /
-insert into KRMS_TYP_T values ('KC1007','IRB Java Function Term Service','KC-PROTOCOL','krmsIrbJavaFunctionTermService','Y',1)
-/
 insert into KRMS_FUNC_T (FUNC_ID,NM,DESC_TXT,RTRN_TYP,VER_NBR,ACTV,TYP_ID,NMSPC_CD) values ('KC1001','IS_SPONSOR_FEDERAL','Is Sponsor Federal','java.lang.String',1,'Y','KC1005','KC-PD')
 /
 insert into KRMS_FUNC_T (FUNC_ID,NM,DESC_TXT,RTRN_TYP,VER_NBR,ACTV,TYP_ID,NMSPC_CD) values ('KC1002','specifiedGGForm','Specified GG Form','java.lang.String',1,'Y','KC1006','KC-PD')
