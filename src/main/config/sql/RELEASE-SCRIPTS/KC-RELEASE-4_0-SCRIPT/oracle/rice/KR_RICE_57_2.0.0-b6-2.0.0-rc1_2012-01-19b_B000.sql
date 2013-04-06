47795274234b63b21a5f20614f5d891f8315bd81
Allen Akers <allen.akers@colostate.edu>
2013-04-06 13:27:41 +0000

    Updating copyright notices

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-4_0-SCRIPT/oracle/rice/KR_RICE_57_2.0.0-b6-2.0.0-rc1_2012-01-19b_B000.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-4_0-SCRIPT/oracle/rice/KR_RICE_57_2.0.0-b6-2.0.0-rc1_2012-01-19b_B000.sql
@@ -1,5 +1,5 @@
 --
--- Copyright 2005-2012 The Kuali Foundation
+-- Copyright 2005-2013 The Kuali Foundation
 --
 -- Licensed under the Educational Community License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
@@ -894,7 +894,7 @@ insert into krms_typ_attr_t (TYP_ATTR_ID, SEQ_NO, TYP_ID, ATTR_DEFN_ID) values (
 /
 
 --
--- Copyright 2005-2012 The Kuali Foundation
+-- Copyright 2005-2013 The Kuali Foundation
 --
 --
 
