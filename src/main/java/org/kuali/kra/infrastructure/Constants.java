7fd8ed71424b1850ec4422e813b39bbb80f01eb9
Gayathri Athreya <hsggwk@gmail.com>
2013-04-23 22:15:11 +0000

    KCINFR-797: changing namespace uri for version compatibility. Also changing some hardcoded krms namespace uris

--- src/main/java/org/kuali/kra/infrastructure/Constants.java
+++ src/main/java/org/kuali/kra/infrastructure/Constants.java
@@ -24,6 +24,8 @@ public interface Constants {
 
     public static final String BOOTSTRAP_SPRING_FILE = "kc.bootstrap.spring.file";
     public static final String KC_CORE_SERVICE_NAMESPACE = "http://kc.kuali.org/core/v5_0";
+    public static final String FINANCIAL_INTEGRATION_KC_SERVICE_NAMESPACE = "http://kc.kuali.org/kc-kfs/v2_0";
+    public static final String FINANCIAL_SYSTEM_SERVICE_NAMESPACE = "http://kfs.kuali.org/kc-kfs/v2_0";
     public static final String KRA_SESSION_KEY = "kra.session";
     public static final String APP_CONTEXT_KEY = "app.context.name";
     public static final String DATASOURCE = "kraDataSource";
