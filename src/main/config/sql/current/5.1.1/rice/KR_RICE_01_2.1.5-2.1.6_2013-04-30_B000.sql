7c23be21d3fd9e90cc5baa826dc08f2d54bdaf35
Gayathri Athreya <hsggwk@gmail.com>
2013-05-14 13:48:34 +0000

    KCINFR-809: Rice upgrade from Rice 2.1.5 to Rice 2.1.7 revision 39133

---
+++ src/main/config/sql/current/5.1.1/rice/KR_RICE_01_2.1.5-2.1.6_2013-04-30_B000.sql
@@ -0,0 +1,24 @@
+--
+-- Copyright 2005-2013 The Kuali Foundation
+--
+-- Licensed under the Educational Community License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+-- http://www.opensource.org/licenses/ecl2.php
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+
+--
+-- KULRICE-9288  - Column 'SESN_ID' cannot be null causes issues
+--
+
+UPDATE KRNS_PESSIMISTIC_LOCK_T SET SESN_ID = 'undefined' WHERE SESN_ID IS NULL OR SESN_ID = ''
+/
+ALTER TABLE KRNS_PESSIMISTIC_LOCK_T MODIFY (SESN_ID DEFAULT 'undefined')
+/
