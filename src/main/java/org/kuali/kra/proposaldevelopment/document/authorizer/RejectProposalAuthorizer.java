dd796b9b27914bf2121817309da97573e5cb70e4
Travis Schneeberger <leo.herbie@gmail.com>
2013-09-03 14:07:34 +0000

    organize import to remove dep on struts Form, org imports

--- src/main/java/org/kuali/kra/proposaldevelopment/document/authorizer/RejectProposalAuthorizer.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/document/authorizer/RejectProposalAuthorizer.java
@@ -33,9 +33,7 @@ import org.kuali.rice.kew.api.WorkflowDocument;
 public class RejectProposalAuthorizer extends ProposalAuthorizer {
 
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(RejectProposalAuthorizer.class);
-    /**
-     * @see org.kuali.kra.proposaldevelopment.document.authorizer.ProposalAuthorizer#isAuthorized(org.kuali.rice.kns.bo.user.UniversalUser, org.kuali.kra.proposaldevelopment.web.struts.form.ProposalDevelopmentForm)
-     */
+
     public boolean isAuthorized(String username, ProposalTask task) {
         ProposalDevelopmentDocument doc = task.getDocument();
         WorkflowDocument workDoc = doc.getDocumentHeader().getWorkflowDocument();
