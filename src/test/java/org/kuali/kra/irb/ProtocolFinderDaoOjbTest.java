7ea9df3962fea1f7de64aa7d7be4a14d2a8a237f
Travis Schneeberger <leo.herbie@gmail.com>
2013-09-13 21:24:32 +0000

    KCINFR-851 fixing failing test

--- src/test/java/org/kuali/kra/irb/ProtocolFinderDaoOjbTest.java
+++ src/test/java/org/kuali/kra/irb/ProtocolFinderDaoOjbTest.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.kuali.kra.infrastructure.KraServiceLocator;
 import org.kuali.kra.irb.test.ProtocolFactory;
@@ -29,7 +30,7 @@ import org.kuali.rice.krad.util.GlobalVariables;
 
 public class ProtocolFinderDaoOjbTest extends KcUnitTestBase {
     
-    private static final String PROTOCOL_NUMBER = "0906000001";
+    private static final String PROTOCOL_NUMBER = "0906000003";
     private static final String PROTOCOL_NUMBER2 = "0906000002";
 
     private ProtocolFinderDao protocolFinder;
@@ -47,26 +48,6 @@ public class ProtocolFinderDaoOjbTest extends KcUnitTestBase {
         super.tearDown();
     }
     
-    /**
-     * Add "two" protocols.  One of the two protocols will have two versions.  For each of
-     * the "two" protocols, we should get back the latest version.
-     */
-    @Test
-    public void testFinder() throws WorkflowException {
-        ProtocolFactory.createProtocolDocument(PROTOCOL_NUMBER, 1);
-        ProtocolDocument protocolDocument1 = ProtocolFactory.createProtocolDocument(PROTOCOL_NUMBER, 2);
-        
-        ProtocolDocument protocolDocument2 = ProtocolFactory.createProtocolDocument(PROTOCOL_NUMBER2, 1);
-       
-        Protocol protocol = protocolFinder.findCurrentProtocolByNumber(PROTOCOL_NUMBER2);
-        assertNotNull(protocol);
-        assertEquals(protocolDocument2.getProtocol().getProtocolId(), protocol.getProtocolId());
-        
-        protocol = protocolFinder.findCurrentProtocolByNumber(PROTOCOL_NUMBER);
-        assertNotNull(protocol);
-        assertEquals(protocolDocument1.getProtocol().getProtocolId(), protocol.getProtocolId());
-    }
-    
     @SuppressWarnings("unchecked")
     @Test
     public void testFindProtocol() throws WorkflowException {
@@ -80,4 +61,23 @@ public class ProtocolFinderDaoOjbTest extends KcUnitTestBase {
         assertEquals(PROTOCOL_NUMBER, protocols.get(0).getProtocolNumber());
     }
 
+    /**
+     * Add "two" protocols.  One of the two protocols will have two versions.  For each of
+     * the "two" protocols, we should get back the latest version.
+     */
+    @Test
+    public void testFinder() throws WorkflowException {
+        ProtocolFactory.createProtocolDocument(PROTOCOL_NUMBER, 1);
+        ProtocolDocument protocolDocument1 = ProtocolFactory.createProtocolDocument(PROTOCOL_NUMBER, 2);
+
+        ProtocolDocument protocolDocument2 = ProtocolFactory.createProtocolDocument(PROTOCOL_NUMBER2, 1);
+
+        Protocol protocol = protocolFinder.findCurrentProtocolByNumber(PROTOCOL_NUMBER2);
+        assertNotNull(protocol);
+        assertEquals(protocolDocument2.getProtocol().getProtocolId(), protocol.getProtocolId());
+
+        protocol = protocolFinder.findCurrentProtocolByNumber(PROTOCOL_NUMBER);
+        assertNotNull(protocol);
+        assertEquals(protocolDocument1.getProtocol().getProtocolId(), protocol.getProtocolId());
+    }
 }
