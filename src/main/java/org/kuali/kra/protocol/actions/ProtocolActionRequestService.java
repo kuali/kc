ae2f744b162cfda85657bb7cf80c32c6c4d22e30
Rajeev Mancheril <rajeev@genesistec.com>
2013-09-13 20:47:14 +0000

    KRACOEUS-6651 Trigger correspondence for every action IRB/IACUC

--- src/main/java/org/kuali/kra/protocol/actions/ProtocolActionRequestService.java
+++ src/main/java/org/kuali/kra/protocol/actions/ProtocolActionRequestService.java
@@ -17,6 +17,8 @@ package org.kuali.kra.protocol.actions;
 
 import org.kuali.kra.protocol.ProtocolBase;
 import org.kuali.kra.protocol.ProtocolFormBase;
+import org.kuali.kra.protocol.correspondence.ProtocolCorrespondence;
+import org.kuali.kra.protocol.notification.ProtocolNotificationRequestBeanBase;
 
 public interface ProtocolActionRequestService {
 
@@ -26,4 +28,16 @@ public interface ProtocolActionRequestService {
 
     public void recalledInRouting(ProtocolBase protocol) throws Exception;
 
+    public boolean checkToSendNotification(ProtocolFormBase protocolForm, ProtocolNotificationRequestBeanBase notificationRequestBean, String promptAfterNotification);
+    
+    /**
+     * This method checks the document state to see if something has changed between the time the user
+     * loaded the document to when they clicked on an action.
+     * @param protocolForm
+     * @return
+     */
+    public boolean hasDocumentStateChanged(ProtocolFormBase protocolForm);
+    
+    public ProtocolCorrespondence getProtocolCorrespondence (ProtocolFormBase protocolForm, String forwardName, ProtocolNotificationRequestBeanBase notificationRequestBean, boolean holdingPage);
+    
 }
