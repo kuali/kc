4fbfbc30a9d50bd18629fa147f2e826b99223059
Allen Akers <allen.akers@colostate.edu>
2013-11-23 21:55:16 +0000

    Deleting unneeded file

--- src/main/config/sql/current/5.2.0/dml/KR_DML_02_KRACOEUS-6763_B000.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM KRIM_TYP_T WHERE NM = 'Derived Role: IACUC Correspondent'
-/
-
-DELETE FROM KRIM_ROLE_T WHERE ROLE_NM = 'IACUC Unit Correspondent'
-/
-
-DELETE FROM KRIM_ROLE_T WHERE ROLE_NM = 'IACUC Organization Correspondent'
-/
