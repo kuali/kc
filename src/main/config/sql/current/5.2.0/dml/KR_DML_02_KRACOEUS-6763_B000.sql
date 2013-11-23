1d89c133cfefde1f48d60fd2b08403a463dd6ed3
David Godfrey <dgodfrey@iastate.edu>
2013-11-23 18:51:49 +0000

    KRACOEUS-6763: correcting sequence / id naming.

---
+++ src/main/config/sql/current/5.2.0/dml/KR_DML_02_KRACOEUS-6763_B000.sql
@@ -0,0 +1,8 @@
+DELETE FROM KRIM_TYP_T WHERE NM = 'Derived Role: IACUC Correspondent'
+/
+
+DELETE FROM KRIM_ROLE_T WHERE ROLE_NM = 'IACUC Unit Correspondent'
+/
+
+DELETE FROM KRIM_ROLE_T WHERE ROLE_NM = 'IACUC Organization Correspondent'
+/
