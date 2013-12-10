d0fa99baa26c7fef359304513339b72b927c22ee
Douglas Pace <dpace@kuali.org>
2013-12-10 00:16:21 +0000

    KRACOEUS-6594 : Manage credit split and units when changing roles within award. Allow changing from key person to investigator and visa-versa without causing problems in credit split.

--- src/main/java/org/kuali/kra/award/contacts/AwardPerson.java
+++ src/main/java/org/kuali/kra/award/contacts/AwardPerson.java
@@ -16,6 +16,7 @@
 package org.kuali.kra.award.contacts;
 
 import org.apache.commons.lang.ObjectUtils;
+import org.apache.commons.lang.StringUtils;
 import org.kuali.kra.award.awardhierarchy.sync.AwardSyncableProperty;
 import org.kuali.kra.award.home.ContactRole;
 import org.kuali.kra.bo.AbstractProjectPerson;
@@ -66,6 +67,8 @@ public class AwardPerson extends AwardContact implements PersonRolodex, Comparab
 
     @AwardSyncableProperty
     private boolean multiplePi;
+    
+    private transient boolean roleChanged;
 
     public AwardPerson() {
         super();
@@ -99,17 +102,6 @@ public class AwardPerson extends AwardContact implements PersonRolodex, Comparab
     public void add(AwardPersonUnit awardPersonUnit) {
         units.add(awardPersonUnit);
         awardPersonUnit.setAwardPerson(this);
-    }
-
-    /**
-     * @see org.kuali.core.bo.PersistableBusinessObjectBase#buildListOfDeletionAwareLists()
-     */
-    @SuppressWarnings("unchecked")
-    @Override
-    public List buildListOfDeletionAwareLists() {
-        List managedLists = super.buildListOfDeletionAwareLists();
-        managedLists.add(getUnits());
-        return managedLists;
     }
 
     /**
@@ -380,4 +372,34 @@ public class AwardPerson extends AwardContact implements PersonRolodex, Comparab
     public boolean getIsRolodexPerson() {
         return this.getRolodex() != null;
     }
+    
+    @Override
+    public void setContactRoleCode(String roleCode) {
+        if (!StringUtils.equals(roleCode, this.roleCode)) {
+            updateBasedOnRoleChange();
+            //used by AwardContactsAction to work around repopulation of units due to credit split being on page and posted with
+            //role change.
+            roleChanged = true;
+        }
+        super.setContactRoleCode(roleCode);
+    }
+    
+    public void updateBasedOnRoleChange() {
+        if (ProposalPersonRole.KEY_PERSON.equals(roleCode)) {
+            this.setOptInUnitStatus(true);
+        } else {
+            if (this.getPerson() != null && this.getPerson().getUnit() != null && this.getUnits().isEmpty()) {
+                this.add(new AwardPersonUnit(this, this.getPerson().getUnit(), true));
+            }                
+        }        
+    }
+
+    public boolean isRoleChanged() {
+        return roleChanged;
+    }
+
+    public void setRoleChanged(boolean roleChanged) {
+        this.roleChanged = roleChanged;
+    }
+    
 }
