b026e9e60763f47d53c757117ff72f1e60148e46
David Godfrey <dgodfrey@iastate.edu>
2013-12-10 04:21:08 +0000

    KRACOEUS-5621: refactored code

--- src/main/java/org/kuali/kra/proposaldevelopment/rules/KeyPersonnelAuditRule.java
+++ src/main/java/org/kuali/kra/proposaldevelopment/rules/KeyPersonnelAuditRule.java
@@ -29,13 +29,12 @@ import org.kuali.kra.questionnaire.answer.AnswerHeader;
 import org.kuali.kra.questionnaire.answer.QuestionnaireAnswerService;
 import org.kuali.kra.rules.ResearchDocumentRuleBase;
 import org.kuali.kra.service.SponsorService;
-import org.kuali.rice.coreservice.api.parameter.Parameter;
-import org.kuali.rice.coreservice.framework.parameter.ParameterService;
 import org.kuali.rice.kns.util.AuditCluster;
 import org.kuali.rice.kns.util.AuditError;
 import org.kuali.rice.kns.util.KNSGlobalVariables;
 import org.kuali.rice.krad.document.Document;
 import org.kuali.rice.krad.rules.rule.DocumentAuditRule;
+import org.kuali.rice.krad.util.GlobalVariables;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
@@ -55,9 +54,6 @@ import static org.kuali.kra.infrastructure.KraServiceLocator.getService;
  */
 public class KeyPersonnelAuditRule extends ResearchDocumentRuleBase implements DocumentAuditRule {
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(KeyPersonnelAuditRule.class);
-
-    private static final String BEFORE_SUBMIT = "BS";
-    private String BEFORE_APPROVE = "BA";
     
     /**
      * 
@@ -70,7 +66,7 @@ public class KeyPersonnelAuditRule extends ResearchDocumentRuleBase implements D
         if (!hasPrincipalInvestigator(pd)) {
             retval = false;
 
-            getAuditErrors(AUDIT_ERRORS).add(new AuditError(PRINCIPAL_INVESTIGATOR_KEY , ERROR_INVESTIGATOR_LOWBOUND, KEY_PERSONNEL_PAGE + "." + KEY_PERSONNEL_PANEL_ANCHOR));
+            getAuditErrors().add(new AuditError(PRINCIPAL_INVESTIGATOR_KEY , ERROR_INVESTIGATOR_LOWBOUND, KEY_PERSONNEL_PAGE + "." + KEY_PERSONNEL_PANEL_ANCHOR));
         }
         // Include normal save document business rules
         retval &= new ProposalDevelopmentKeyPersonsRule().processCustomSaveDocumentBusinessRules(pd);
@@ -90,7 +86,7 @@ public class KeyPersonnelAuditRule extends ResearchDocumentRuleBase implements D
                 hasInvestigator = true;
             }
         }
-        retval &= validateYesNoQuestions((ProposalDevelopmentDocument) document);
+        //retval &= validateYesNoQuestions((ProposalDevelopmentDocument) document);
         
         if (hasInvestigator) {
             retval &= validateCreditSplit((ProposalDevelopmentDocument) document);
@@ -102,104 +98,11 @@ public class KeyPersonnelAuditRule extends ResearchDocumentRuleBase implements D
     private boolean validateEraCommonUserName(ProposalPerson person, int personCount) {
         boolean retval = true;
         if (person.getEraCommonsUserName() == null) {
-            getAuditErrors(AUDIT_ERRORS).add(new AuditError(
+            getAuditErrors().add(new AuditError(
                     "document.developmentProposalList[0].proposalPersons[" + personCount + "].eraCommonUserName", 
                         ERROR_ERA_COMMON_USER_NAME, KEY_PERSONNEL_PAGE + "." + KEY_PERSONNEL_PANEL_ANCHOR, new String[]{person.getFullName()}));
             retval = false;
         }
-        return retval;
-    }
-       
-    /**
-     * Yes/No questions have to be submitted to Grants.gov on document route. If the submitter has not completed the certifications,
-     * errors should be displayed in audit mode. This does validation by iterating over each investigator until the PI is found.
-     * The PI Yes/No questions are then checked for completeness. 
-     * 
-     * @param document
-     * @return boolean true if the submitter has not completed the certifications
-     */
-    private boolean validateYesNoQuestions(ProposalDevelopmentDocument document) {
-        boolean retval = true;
-        int count = 0;
-        AuditError error = null;
-        final String errorStarter = "proposalPersonQuestionnaireHelpers[";
-        final String errorFinish = "].answerHeaders[0].answers[0].answer";
-        final String errorLink = KEY_PERSONNEL_PAGE + "." + KEY_PERSONNEL_PANEL_ANCHOR;
-        String questionnaireAuditDeferral = ProposalDevelopmentUtils.getProposalDevelopmentDocumentParameter(ProposalDevelopmentUtils.KEY_PERSON_CERTIFICATION_DEFERRAL_PARM);
-            
-            for (ProposalPerson person : document.getDevelopmentProposal().getProposalPersons()) {
-                if (shouldValidateYesNoQuestions(person) && !validateYesNoQuestions(person)) {
-                    String errorKey = errorStarter + count + errorFinish;
-
-                    if(questionnaireAuditDeferral.equals(BEFORE_SUBMIT)) {
-                        retval = false;
-                        //display error in the audit pane
-                        error = new AuditError(errorKey, ERROR_PROPOSAL_PERSON_CERTIFICATION_INCOMPLETE, 
-                                errorLink, new String[]{person.getFullName()});
-                        getAuditErrors(AUDIT_ERRORS).add(error);
-                        
-                        //display error within the tab.
-                        reportError(errorKey, ERROR_PROPOSAL_PERSON_CERTIFICATION_INCOMPLETE,
-                                new String[]{person.getFullName()});
-                    }
-                    else if(questionnaireAuditDeferral.equals(BEFORE_APPROVE)){
-                        //Only a warning in this instance. Questionnaire must be answered before the
-                        //user can submit to sponsor, or approve the proposal, in the case of PI/COI/KP
-                        error = new AuditError(errorKey, ERROR_PROPOSAL_PERSON_CERTIFICATION_INCOMPLETE,
-                                errorLink, new String[]{person.getFullName()});
-
-                        getAuditErrors(AUDIT_WARNINGS).add(error);
-                        
-                        reportWarning(errorKey, ERROR_PROPOSAL_PERSON_CERTIFICATION_INCOMPLETE, new String[]{person.getFullName()});
-                        retval = true;
-                    }
-                }
-                count++;
-            }
-        return retval;
-    }
-    
-    private boolean shouldValidateYesNoQuestions(ProposalPerson person) {
-        boolean retval = false;
-        if (person.getProposalPersonRoleId().equals(CO_INVESTIGATOR_ROLE)
-                || person.getProposalPersonRoleId().equals(PRINCIPAL_INVESTIGATOR_ROLE)) {
-            retval = true;
-        }
-        else if (person.getProposalPersonRoleId().equals(KEY_PERSON_ROLE) && isNotBlank(person.getOptInCertificationStatus())
-                && person.getOptInCertificationStatus().equals("Y")) {
-            retval = true;
-        }
-        return retval;
-    }
-    
-    
-    /**
-     * Yes/No questions have to be submitted to Grants.gov on document route. If the submitter has not completed the certifications,
-     * errors should be displayed in audit mode.<br/> 
-     * <br/>
-     * This method differs from <code>{@link #validateYesNoQuestions(ProposalDevelopmentDocument)}</code> that it refers to a specific person.
-     * If any one of the Yes/No Questions is not completed, then this check will fail.
-     * 
-     * 
-     * @param investigator Proposal Investigator
-     * @return true if the given PI's Yes/No Questions are completed
-     */
-    private boolean validateYesNoQuestions(ProposalPerson investigator) {
-        boolean retval = true;
-        
-        ProposalPersonModuleQuestionnaireBean bean = new ProposalPersonModuleQuestionnaireBean(investigator.getDevelopmentProposal(), investigator);
-        List<AnswerHeader> headers = KraServiceLocator.getService(QuestionnaireAnswerService.class).getQuestionnaireAnswer(bean);
-        
-        for (AnswerHeader head : headers) {
-            retval &= head.getCompleted();
-        }
-        /*
-        for (ProposalPersonYnq question : investigator.getProposalPersonYnqs()) {
-           
-            retval &= isNotBlank(question.getAnswer());
-        }
-        */
-               
         return retval;
     }
 
@@ -321,11 +224,11 @@ public class KeyPersonnelAuditRule extends ResearchDocumentRuleBase implements D
      * 
      * @return List of AuditError instances
      */
-    private List<AuditError> getAuditErrors(String category) {
+    private List<AuditError> getAuditErrors() {
         List<AuditError> auditErrors = new ArrayList<AuditError>();
         
         if (!KNSGlobalVariables.getAuditErrorMap().containsKey("keyPersonnelAuditErrors")) {
-           KNSGlobalVariables.getAuditErrorMap().put("keyPersonnelAuditErrors", new AuditCluster(KEY_PERSONNEL_PANEL_NAME, auditErrors, category));
+           KNSGlobalVariables.getAuditErrorMap().put("keyPersonnelAuditErrors", new AuditCluster(KEY_PERSONNEL_PANEL_NAME, auditErrors, AUDIT_ERRORS));
         }
         else {
             auditErrors = ((AuditCluster)KNSGlobalVariables.getAuditErrorMap().get("keyPersonnelAuditErrors")).getAuditErrorList();
@@ -352,4 +255,5 @@ public class KeyPersonnelAuditRule extends ResearchDocumentRuleBase implements D
         
         return retval;
     }
+
 }
