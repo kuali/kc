9177999b7b4025d896367ca814895626fdf70d24
Douglas Pace <dpace@kuali.org>
2013-12-17 23:52:57 +0000

    Fix more unit tests post rice upgrade.

--- src/main/java/org/kuali/kra/irb/protocol/ProtocolNumberServiceImpl.java
+++ src/main/java/org/kuali/kra/irb/protocol/ProtocolNumberServiceImpl.java
@@ -29,67 +29,6 @@ public class ProtocolNumberServiceImpl extends ProtocolNumberServiceImplBase imp
     private static final String ZERO = "0";
     private static final String SEQUENCE_NAME = "SEQ_PROTOCOL_ID";
     private static final int MAX_NUMBER = 1000000;
-    
-    private SequenceAccessorService sequenceAccessorService;
-    
-    /**
-     * Set the Sequence Accessor Service.
-     * @param sequenceAccessorService the Sequence Accessor Service
-     */
-    public void setSequenceAccessorService(SequenceAccessorService sequenceAccessorService) {
-        this.sequenceAccessorService = sequenceAccessorService;
-    }
-    
-    /**
-     * @see org.kuali.kra.irb.protocol.ProtocolNumberService#generateProtocolNumber()
-     */
-    public String generateProtocolNumber() {
-        Calendar calendar = Calendar.getInstance();
-        return getYear(calendar) + getMonth(calendar) + getNextNumber();
-    }
-    
-    /**
-     * Get the current year, only the last two digits.
-     * @param calendar the current time
-     * @return the year as 2 digits as a string
-     */
-    protected String getYear(Calendar calendar) {
-        int year = calendar.get(Calendar.YEAR);
-        String s = Integer.toString(year).substring(2);
-        if (s.length() == 1) {
-            s = ZERO + s;
-        }
-        return s;
-    }
-    
-    /**
-     * Get the current month, always 2 digits.
-     * @param calendar the current time
-     * @return the month as 2 digits, e.g. 03 is March
-     */
-    protected String getMonth(Calendar calendar) {
-        int month = calendar.get(Calendar.MONTH) + 1;
-        String s = Integer.toString(month);
-        if (s.length() == 1) {
-            s = ZERO + s;
-        }
-        return s;
-    }
-    
-    /**
-     * Get the next sequence number which is always 6 digits,
-     * including leading zeros.  
-     * @return the next sequence number
-     */
-    protected String getNextNumber() {
-        Long nextNumber = getSequenceNumber() % MAX_NUMBER;
-        String s = nextNumber.toString();
-        int length = s.length();
-        for (int i = 0; i < 6 - length; i++) {
-            s = ZERO + s;
-        }
-        return s;
-    }
 
     @Override
     protected String getSequenceNameHook() {
