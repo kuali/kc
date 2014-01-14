0b2da7a970e94c2c378e97f42793ddd2198fad05
Allen Akers <allen.akers@colostate.edu>
2014-01-14 22:29:13 +0000

    Fixing MySQL release scripts

--- src/main/config/sql/current_mysql/5.2.0/dml/KR_DML_01_KCINFR-830_B000.sql
+++ src/main/config/sql/current_mysql/5.2.0/dml/KR_DML_01_KCINFR-830_B000.sql
@@ -1,3 +1,6 @@
+DELIMITER /
+
 update KRIM_ROLE_T set KIM_TYP_ID = (select KIM_TYP_ID from KRIM_TYP_T where NM = 'Default') where ROLE_NM = 'Modify Training'
 /
 
+DELIMITER ;
--- src/main/config/sql/current_mysql/5.2.0/dml/KR_DML_01_KRACOEUS-6720_B000.sql
+++ src/main/config/sql/current_mysql/5.2.0/dml/KR_DML_01_KRACOEUS-6720_B000.sql
@@ -1,3 +1,5 @@
+DELIMITER /
+
 UPDATE KRMS_FUNC_T SET DESC_TXT='Checks if the opportunity is already selected for the proposal' WHERE NM='proposalGrantsRule'
 /
 UPDATE KRMS_TERM_SPEC_T SET DESC_TXT='Is S2S Submission' WHERE nm=(select FUNC_ID from KRMS_FUNC_T where NM='proposalGrantsRule')
@@ -6,3 +8,5 @@ UPDATE KRMS_TERM_T SET DESC_TXT='Is S2S Submission' WHERE TERM_SPEC_ID=(select T
 /
 commit
 /
+
+DELIMITER ;
