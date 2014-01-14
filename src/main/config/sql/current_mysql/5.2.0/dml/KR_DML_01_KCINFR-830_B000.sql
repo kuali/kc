3e3da47cac2c9414c250d21cc95dc071fd49a133
Gayathri Athreya <hsggwk@gmail.com>
2014-01-14 23:21:20 +0000

    KCINFR-985: Fixing mysql scripts. Adding namespace qualification that was missing.

--- src/main/config/sql/current_mysql/5.2.0/dml/KR_DML_01_KCINFR-830_B000.sql
+++ src/main/config/sql/current_mysql/5.2.0/dml/KR_DML_01_KCINFR-830_B000.sql
@@ -1,6 +1,6 @@
 DELIMITER /
 
-update KRIM_ROLE_T set KIM_TYP_ID = (select KIM_TYP_ID from KRIM_TYP_T where NM = 'Default') where ROLE_NM = 'Modify Training'
+update KRIM_ROLE_T set KIM_TYP_ID = (select KIM_TYP_ID from KRIM_TYP_T where NM = 'Default' and NMSPC_CD = 'KUALI') where ROLE_NM = 'Modify Training'
 /
 
 DELIMITER ;
