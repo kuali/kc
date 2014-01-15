9df7468d8766984331eeb3d14cfcefe5901f6dcb
David Godfrey <dgodfrey@iastate.edu>
2014-01-15 17:11:25 +0000

    KRACOEUS-6636: Initial commit, more work needed to display qn for all submit actions, as well as displaying correct version/sequence of qn for past submit actions.

--- src/main/java/org/kuali/kra/iacuc/questionnaire/IacucSubmissionQuestionnaireHelper.java
+++ src/main/java/org/kuali/kra/iacuc/questionnaire/IacucSubmissionQuestionnaireHelper.java
@@ -16,7 +16,9 @@
 package org.kuali.kra.iacuc.questionnaire;
 
 import org.kuali.kra.bo.CoeusModule;
+import org.kuali.kra.bo.CoeusSubModule;
 import org.kuali.kra.protocol.ProtocolBase;
+import org.kuali.kra.protocol.questionnaire.ProtocolModuleQuestionnaireBeanBase;
 import org.kuali.kra.protocol.questionnaire.ProtocolSubmissionQuestionnaireHelper;
 
 public class IacucSubmissionQuestionnaireHelper extends ProtocolSubmissionQuestionnaireHelper {
@@ -33,6 +35,12 @@ public class IacucSubmissionQuestionnaireHelper extends ProtocolSubmissionQuesti
     @Override
     public String getModuleCode() {
         return CoeusModule.IACUC_PROTOCOL_MODULE_CODE;
+    }
+
+    @Override
+    public ProtocolModuleQuestionnaireBeanBase getBaseProtocolModuleQuestionnaireBean(String sequenceNumber) {
+        return new IacucProtocolModuleQuestionnaireBean(CoeusModule.IACUC_PROTOCOL_MODULE_CODE, getProtocol().getProtocolNumber(), CoeusSubModule.ZERO_SUBMODULE, sequenceNumber, 
+                getProtocol().getProtocolDocument().getDocumentHeader().getWorkflowDocument().isApproved());
     }
 
 }
