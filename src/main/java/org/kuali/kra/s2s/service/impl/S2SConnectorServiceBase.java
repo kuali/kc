75adeb350fc0c29e84637b9223f9232ea5a3a8df
Allen Akers <allen.akers@colostate.edu>
2014-01-17 21:27:56 +0000

    Updating copyright notices

--- src/main/java/org/kuali/kra/s2s/service/impl/S2SConnectorServiceBase.java
+++ src/main/java/org/kuali/kra/s2s/service/impl/S2SConnectorServiceBase.java
@@ -1,5 +1,5 @@
 /*
-kc * Copyright 2005-2013 The Kuali Foundation.
+kc * Copyright 2005-2014 The Kuali Foundation.
  * 
  * Licensed under the Educational Community License, Version 1.0 (the "License");
  * you may not use this file except in compliance with the License.
