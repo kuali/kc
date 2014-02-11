cbfb09b9689761e556e7c55b8b44a5ee5db1074e
gathreya <hsggwk@gmail.com>
2014-02-10 19:33:37 -0500

    KRACOEUS-6939: Removing unused imports.

--- src/main/java/org/kuali/kra/iacuc/customdata/IacucProtocolCustomDataHelper.java
+++ src/main/java/org/kuali/kra/iacuc/customdata/IacucProtocolCustomDataHelper.java
@@ -21,10 +21,8 @@ import org.kuali.kra.iacuc.IacucProtocolDocument;
 import org.kuali.kra.iacuc.IacucProtocolForm;
 import org.kuali.kra.iacuc.auth.IacucProtocolTask;
 import org.kuali.kra.infrastructure.TaskName;
-import org.kuali.kra.protocol.ProtocolDocumentBase;
 import org.kuali.kra.protocol.auth.ProtocolTaskBase;
 import org.kuali.kra.protocol.customdata.ProtocolCustomDataHelperBase;
-import org.kuali.rice.krad.document.Document;
 
 import java.util.List;
 import java.util.Map;
