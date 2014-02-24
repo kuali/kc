6f75ecf1b6b3dc719d3f65f1201f18b21aa638ff
David Godfrey <dgodfrey@iastate.edu>
2014-02-23 18:54:09 -0600

    KRACOEUS-7030: repackage rolodex maintainable

--- coeus-code/src/main/java/org/kuali/coeus/common/framework/rolodex/RolodexMaintainableImpl.java
+++ coeus-code/src/main/java/org/kuali/coeus/common/framework/rolodex/RolodexMaintainableImpl.java
@@ -13,10 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.bo;
+package org.kuali.coeus.common.framework.rolodex;
 
 import org.apache.commons.lang.StringUtils;
-import org.kuali.coeus.common.framework.rolodex.Rolodex;
 import org.kuali.coeus.sys.framework.service.KcServiceLocator;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.maintenance.KraMaintainableImpl;
