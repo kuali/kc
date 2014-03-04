0bfae33c5a764d7234bb3cc29dece680d65f78d0
gathreya <hsggwk@gmail.com>
2014-03-03 23:41:04 -0500

    KRACOEUS-6906:Committing again. Fixing issue of auto-generated F&A amounts blocking user from opening T&M document.

--- src/main/java/org/kuali/kra/award/web/struts/action/AwardAction.java
+++ src/main/java/org/kuali/kra/award/web/struts/action/AwardAction.java
@@ -48,6 +48,7 @@ import org.kuali.kra.award.paymentreports.awardreports.AwardReportTerm;
 import org.kuali.kra.award.paymentreports.awardreports.AwardReportTermRecipient;
 import org.kuali.kra.award.paymentreports.awardreports.reporting.service.ReportTrackingService;
 import org.kuali.kra.award.paymentreports.closeout.CloseoutReportTypeValuesFinder;
+import org.kuali.kra.award.timeandmoney.AwardDirectFandADistribution;
 import org.kuali.kra.award.version.service.AwardVersionService;
 import org.kuali.kra.bo.versioning.VersionHistory;
 import org.kuali.kra.bo.versioning.VersionStatus;
@@ -519,17 +520,6 @@ public class AwardAction extends BudgetParentActionBase {
         getAwardService().updateAwardSequenceStatus(award, VersionStatus.PENDING);
         getVersionHistoryService().updateVersionHistory(award, VersionStatus.PENDING, userId);
         
-        // set awardDirectFandADistributions on award
-        if(isNewAward(awardForm) && !(award.getAwardEffectiveDate() == null)){
-            String autoGenerate = getParameterService().getParameterValueAsString(Constants.PARAMETER_MODULE_AWARD, ParameterConstants.DOCUMENT_COMPONENT, KeyConstants.AUTO_GENERATE_TIME_MONEY_FUNDS_DIST_PERIODS); 
-            if (!StringUtils.equalsIgnoreCase(autoGenerate, "N")) {
-                AwardDirectFandADistributionService awardDirectFandADistributionService = getAwardDirectFandADistributionService();
-                awardForm.getAwardDocument().getAward().setAwardDirectFandADistributions
-                                    (awardDirectFandADistributionService.
-                                            generateDefaultAwardDirectFandADistributionPeriods(awardForm.getAwardDocument().getAward()));
-            }
-        }
-        
         if(!awardForm.getAwardDocument().isDocumentSaveAfterVersioning()) {
             awardForm.getAwardHierarchyBean().createDefaultAwardHierarchy();
             awardForm.getAwardHierarchyBean().saveHierarchyChanges();            
@@ -746,6 +736,21 @@ public class AwardAction extends BudgetParentActionBase {
     }
 
 
+    protected void generateDirectFandADistribution(Award award) {
+        if(!(award.getAwardEffectiveDate() == null)) {
+            // delete entries that were added during previous T&M initiations but the doc cancelled.
+            getBusinessObjectService().delete(award.getAwardDirectFandADistributions());
+            
+            Boolean autoGenerate = getParameterService().getParameterValueAsBoolean(Constants.PARAMETER_MODULE_AWARD, ParameterConstants.DOCUMENT_COMPONENT, 
+                                                                                    KeyConstants.AUTO_GENERATE_TIME_MONEY_FUNDS_DIST_PERIODS); 
+            if (autoGenerate) {
+                AwardDirectFandADistributionService awardDirectFandADistributionService = getAwardDirectFandADistributionService();
+                award.setAwardDirectFandADistributions
+                                    (awardDirectFandADistributionService.
+                                            generateDefaultAwardDirectFandADistributionPeriods(award));
+            }
+        }
+    }
     
     @SuppressWarnings({ "deprecation", "unchecked" })
     public ActionForward timeAndMoney(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception{
@@ -787,7 +792,11 @@ public class AwardAction extends BudgetParentActionBase {
             if(timeAndMoneyDocuments.size() > 0 && timeAndMoneyDocument != null) {
                 firstTimeAndMoneyDocCreation = Boolean.FALSE;
             }
-    
+            
+            if (timeAndMoneyDocument == null) {
+                generateDirectFandADistribution(currentAward);
+            }
+
             if(firstTimeAndMoneyDocCreation){
                 timeAndMoneyDocument = (TimeAndMoneyDocument) documentService.getNewDocument(TimeAndMoneyDocument.class);
                 timeAndMoneyDocument.getDocumentHeader().setDocumentDescription("timeandmoney document");
@@ -817,7 +826,7 @@ public class AwardAction extends BudgetParentActionBase {
             String forward = buildForwardUrl(routeHeaderId);
             actionForward = new ActionForward(forward, true);
             //add this to session and leverage in T&M for return to award action.
-            GlobalVariables.getUserSession  ().addObject(Constants.AWARD_DOCUMENT_STRING_FOR_SESSION + "-" + timeAndMoneyDocument.getDocumentNumber(), awardDocument.getDocumentNumber());            
+            GlobalVariables.getUserSession().addObject(Constants.AWARD_DOCUMENT_STRING_FOR_SESSION + "-" + timeAndMoneyDocument.getDocumentNumber(), awardDocument.getDocumentNumber());            
         } else {
             actionForward = mapping.findForward(Constants.MAPPING_AWARD_BASIC);
         }
