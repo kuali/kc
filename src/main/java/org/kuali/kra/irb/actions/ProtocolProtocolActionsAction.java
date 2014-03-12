0170a8513f49f4ea8c838a53e0e8d0d5ebccaf2f
rmancher <rajeev@genesistec.com>
2014-03-12 14:46:59 -0400

    KRACOEUS-6888,IRB - Modify submission - update to set committee and schedule,post agenda and when assigned initially

--- src/main/java/org/kuali/kra/irb/actions/ProtocolProtocolActionsAction.java
+++ src/main/java/org/kuali/kra/irb/actions/ProtocolProtocolActionsAction.java
@@ -69,6 +69,7 @@ import org.kuali.kra.irb.actions.request.ProtocolRequestBean;
 import org.kuali.kra.irb.actions.reviewcomments.*;
 import org.kuali.kra.irb.actions.risklevel.*;
 import org.kuali.kra.irb.actions.submit.ProtocolSubmission;
+import org.kuali.kra.irb.actions.submit.ProtocolSubmissionStatus;
 import org.kuali.kra.irb.actions.submit.ProtocolSubmitAction;
 import org.kuali.kra.irb.actions.submit.ProtocolSubmitActionEvent;
 import org.kuali.kra.irb.actions.submit.ValidProtocolActionAction;
@@ -1802,7 +1803,11 @@ public class ProtocolProtocolActionsAction extends ProtocolAction implements Aud
                     }
         
                     if (performAssignment) {
-                        getProtocolAssignCmtSchedService().assignToCommitteeAndSchedulePostAgendaAssignment((Protocol) protocolForm.getProtocolDocument().getProtocol(), actionBean);
+                        if(StringUtils.equals(protocolSubmission.getSubmissionStatusCode(), ProtocolSubmissionStatus.IN_AGENDA)) {
+                            getProtocolAssignCmtSchedService().assignToCommitteeAndSchedulePostAgendaAssignment((Protocol) protocolForm.getProtocolDocument().getProtocol(), actionBean);
+                        }else {
+                            getProtocolAssignCmtSchedService().assignToCommitteeAndSchedule((Protocol) protocolForm.getProtocolDocument().getProtocol(), actionBean);
+                        }
                     }
                         
                 }
