7397f98280aff286eddb340b5d518478e68d5417
blackcathacker <douglas.pace@gmail.com>
2014-03-14 11:44:35 -0700

    KRACOEUS-7005 : Fix sql for oracle.

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/tables/KC_TBL_EPS_PROP_PERSON_EXT.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/tables/KC_TBL_EPS_PROP_PERSON_EXT.sql
@@ -4,5 +4,5 @@ alter table EPS_PROP_PERSON_EXT drop primary key
 alter table EPS_PROP_PERSON_EXT add primary key (PROPOSAL_NUMBER, PROP_PERSON_NUMBER)
 /
 
-alter table EPS_PROP_PERSON_EXT drop PROP_PERSON_ROLE_ID
+alter table EPS_PROP_PERSON_EXT drop column PROP_PERSON_ROLE_ID
 /
--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/tables/KC_TBL_EPS_PROP_PERSON_EXT.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/tables/KC_TBL_EPS_PROP_PERSON_EXT.sql
@@ -5,6 +5,6 @@ alter table EPS_PROP_PERSON_EXT drop primary key
 alter table EPS_PROP_PERSON_EXT add primary key (PROPOSAL_NUMBER, PROP_PERSON_NUMBER)
 /
 
-alter table EPS_PROP_PERSON_EXT drop PROP_PERSON_ROLE_ID
+alter table EPS_PROP_PERSON_EXT drop column PROP_PERSON_ROLE_ID
 / 
 DELIMITER ;
