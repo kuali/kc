f91aa964d5444d6102186e57791360be68d88478
yyuan1 <yyuan1@kuali.org>
2014-03-20 22:47:41 -0400

    KRACOEUS-6956 :  move ProposalDevelopmentPermissionsAction, addProposalUserEvent to auth perm

--- coeus-code/src/main/java/org/kuali/coeus/propdev/impl/auth/perm/AddProposalUserEvent.java
+++ coeus-code/src/main/java/org/kuali/coeus/propdev/impl/auth/perm/AddProposalUserEvent.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.proposaldevelopment.rule.event;
+package org.kuali.coeus.propdev.impl.auth.perm;
 
 import org.apache.commons.lang3.StringUtils;
 import org.kuali.coeus.sys.framework.rule.KcDocumentEventBase;
