55ef06ff9c81dbee75d9996b9329ec89c50bda31
David Godfrey <dgodfrey@iastate.edu>
2014-03-24 12:25:43 -0500

    KRACOEUS-7051: repackage award-specific version history bo

--- coeus-code/src/main/java/org/kuali/coeus/common/framework/version/history/VersionHistorySearchBo.java
+++ coeus-code/src/main/java/org/kuali/coeus/common/framework/version/history/VersionHistorySearchBo.java
@@ -13,10 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.bo.versioning;
+package org.kuali.coeus.common.framework.version.history;
 
 import org.kuali.coeus.common.framework.sequence.owner.SequenceOwner;
 import org.kuali.coeus.sys.framework.model.KcPersistableBusinessObjectBase;
+import org.kuali.kra.bo.versioning.VersionStatus;
 
 import java.sql.Date;
 
