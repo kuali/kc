6d6ae0fedfc0e63e3123f84c65dec310796b1ece
mrudulpolus <mrudul@polussoftware.com>
2014-03-26 21:23:29 +0530

    KRACOEUS-7003

---
+++ coeus-code/src/main/java/org/kuali/kra/proposaldevelopment/web/krad/ProposalDevelopmentDeliveryInfoController.java
@@ -0,0 +1,28 @@
+package org.kuali.kra.proposaldevelopment.web.krad;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.kuali.kra.proposaldevelopment.bo.DevelopmentProposal;
+import org.kuali.rice.krad.web.form.DocumentFormBase;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ProposalDevelopmentDeliveryInfoController extends ProposalDevelopmentControllerBase{
+
+	@RequestMapping(value = "/proposalDevelopment", params = "methodToCall=clearMailingNameAddress")
+	   public ModelAndView clearMailingNameAddress(@ModelAttribute("KualiForm") DocumentFormBase form, BindingResult result,
+	           HttpServletRequest request, HttpServletResponse response) throws Exception {
+		   ProposalDevelopmentDocumentForm pdForm = (ProposalDevelopmentDocumentForm) form;
+		   DevelopmentProposal developmentProposal = pdForm.getProposalDevelopmentDocument().getDevelopmentProposal();
+	       if (developmentProposal.getRolodex() != null) {
+	           developmentProposal.setMailingAddressId(null);
+	           developmentProposal.setRolodex(null);
+	       }
+	       return getTransactionalDocumentControllerService().refresh(form, result, request, response);
+	   }
+}
