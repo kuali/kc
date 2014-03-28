8cf946c1d558c51ff354ff61a5ff012c6a39bf26
yyuan1 <yyuan1@kuali.org>
2014-03-28 13:12:20 -0400

    KRACOEUS-6956 : repack propdev action

--- coeus-code/src/main/java/org/kuali/kra/proposaldevelopment/web/struts/action/ProposalDevelopmentApproverViewAction.java
+++ coeus-code/src/main/java/org/kuali/kra/proposaldevelopment/web/struts/action/ProposalDevelopmentApproverViewAction.java
@@ -23,6 +23,7 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.kuali.coeus.common.specialreview.impl.service.SpecialReviewService;
+import org.kuali.coeus.propdev.impl.action.ProposalDevelopmentActionsAction;
 import org.kuali.coeus.sys.framework.service.KcServiceLocator;
 import org.kuali.kra.budget.core.Budget;
 import org.kuali.kra.budget.document.BudgetDocument;
