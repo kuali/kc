d818ccf0868a0c9bcea6403d7dfbd381ec55f66d
Travis Schneberger <leo.herbie@gmail.com>
2014-04-01 18:14:54 -0400

    KRACOEUS-7095: fixing routing

--- coeus-code/src/main/java/org/kuali/coeus/sys/impl/workflow/KcPostProcessorServiceImpl.java
+++ coeus-code/src/main/java/org/kuali/coeus/sys/impl/workflow/KcPostProcessorServiceImpl.java
@@ -25,11 +25,14 @@ import org.kuali.rice.kew.api.document.WorkflowDocumentService;
 import org.kuali.rice.kew.api.exception.WorkflowException;
 import org.kuali.rice.kew.framework.postprocessor.*;
 import org.kuali.rice.krad.UserSession;
+import org.kuali.rice.krad.datadictionary.DocumentEntry;
 import org.kuali.rice.krad.document.Document;
+import org.kuali.rice.krad.service.DocumentDictionaryService;
 import org.kuali.rice.krad.service.DocumentService;
 import org.kuali.rice.krad.service.PostProcessorService;
 import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.util.KRADConstants;
+import org.kuali.rice.krad.util.LegacyUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -61,21 +64,25 @@ public class KcPostProcessorServiceImpl implements PostProcessorService {
     @Qualifier("documentService")
     private DocumentService documentService;
 
+    @Autowired
+    @Qualifier("documentDictionaryService")
+    private DocumentDictionaryService documentDictionaryService;
+
     protected void performKCWorkaround(DocumentRouteStatusChange statusChangeEvent) throws Exception {
         try {
             establishPostProcessorUserSession();
-            
+
             String routeHeaderId = statusChangeEvent.getDocumentId();
-            
+
             List<ActionTaken> actionsTaken = workflowDocumentService.getActionsTaken(routeHeaderId);
-            
+
             ActionTaken lastActionTaken = null;
             for (ActionTaken actionTaken : actionsTaken) {
                 if (lastActionTaken == null || actionTaken.getActionDate().toDate().after(lastActionTaken.getActionDate().toDate())) {
                     lastActionTaken = actionTaken;
                 }
             }
-            
+
             if (lastActionTaken != null) {
                 String lastPrincipalId = lastActionTaken.getPrincipalId();
                 GlobalVariables.getUserSession().addObject(LAST_ACTION_PRINCIPAL_ID, (Object) lastPrincipalId);
@@ -86,10 +93,10 @@ public class KcPostProcessorServiceImpl implements PostProcessorService {
             throw new RuntimeException("post processor caught exception while handling route status change: " + e.getMessage(), e);
         }
     }
-    
+
     @Override
     public ProcessDocReport doRouteStatusChange(final DocumentRouteStatusChange statusChangeEvent) throws Exception {
-        return GlobalVariables.doInNewGlobalVariables(establishPostProcessorUserSession(),
+        return doInContext(statusChangeEvent.getDocumentId(),
                 new Callable<ProcessDocReport>() {
                     public ProcessDocReport call() throws Exception {
                         try {
@@ -172,18 +179,16 @@ public class KcPostProcessorServiceImpl implements PostProcessorService {
         return postProcessorService.getDocumentIdsToLock(lockingEvent);
     }
 
-    /* Replicating utilitity methods from super class */
-    /* will request for those to be made protected */
-    private void logAndRethrow(String changeType, Exception e) throws RuntimeException {
+    /* Replicating utilitity methods from rice post processor service */
+    protected void logAndRethrow(String changeType, Exception e) throws RuntimeException {
         LOG.error("caught exception while handling " + changeType + " change", e);
         logOptimisticDetails(5, e);
 
         throw new RuntimeException("post processor caught exception while handling " + changeType + " change: " + e.getMessage(), e);
     }
 
-    /* Replicating utilitity methods from super class */
-    /* will request for those to be made protected */
-    private void logOptimisticDetails(int depth, Throwable t) {
+    /* Replicating utilitity methods from rice post processor service */
+    protected void logOptimisticDetails(int depth, Throwable t) {
         if ((depth > 0) && (t != null)) {
             if (t instanceof OptimisticLockException) {
                 OptimisticLockException o = (OptimisticLockException) t;
@@ -199,6 +204,7 @@ public class KcPostProcessorServiceImpl implements PostProcessorService {
         }
     }
 
+    /* Replicating utilitity methods from rice post processor service */
     protected UserSession establishPostProcessorUserSession() throws WorkflowException {
         if (GlobalVariables.getUserSession() == null) {
             return new UserSession(KRADConstants.SYSTEM_USER);
@@ -206,12 +212,66 @@ public class KcPostProcessorServiceImpl implements PostProcessorService {
             return GlobalVariables.getUserSession();
         }
     }
-    
+
+    /* Replicating utilitity methods from rice post processor service */
+    protected <T> T doInContext(String documentId, Callable<T> callable) throws Exception {
+        boolean inLegacyContext = establishLegacyDataContextIfNeccesary(documentId);
+        try {
+            return GlobalVariables.doInNewGlobalVariables(establishPostProcessorUserSession(), callable);
+        } finally {
+            clearLegacyDataContextIfExists(inLegacyContext);
+        }
+    }
+
+    /* Replicating utilitity methods from rice post processor service */
+    protected boolean establishLegacyDataContextIfNeccesary(String documentId) {
+        String documentTypeName = workflowDocumentService.getDocumentTypeName(documentId);
+        DocumentEntry documentEntry = documentDictionaryService.getDocumentEntry(documentTypeName);
+
+        if (LegacyUtils.isKnsDocumentEntry(documentEntry)) {
+            LegacyUtils.beginLegacyContext();
+
+            return true;
+        }
+        return false;
+    }
+
+    /* Replicating utilitity methods from rice post processor service */
+    protected void clearLegacyDataContextIfExists(boolean inLegacyContext) {
+        if (inLegacyContext) {
+            LegacyUtils.endLegacyContext();
+        }
+    }
+
     public void setWorkflowDocumentService(WorkflowDocumentService workflowDocumentService) {
         this.workflowDocumentService = workflowDocumentService;
     }
 
     public void setPostProcessorService(PostProcessorService postProcessorService) {
         this.postProcessorService = postProcessorService;
+    }
+
+    public WorkflowDocumentService getWorkflowDocumentService() {
+        return workflowDocumentService;
+    }
+
+    public PostProcessorService getPostProcessorService() {
+        return postProcessorService;
+    }
+
+    public DocumentService getDocumentService() {
+        return documentService;
+    }
+
+    public void setDocumentService(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    public DocumentDictionaryService getDocumentDictionaryService() {
+        return documentDictionaryService;
+    }
+
+    public void setDocumentDictionaryService(DocumentDictionaryService documentDictionaryService) {
+        this.documentDictionaryService = documentDictionaryService;
     }
 }
