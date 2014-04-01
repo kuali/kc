899c66c03d7fa9d1b68c2b0ebfefd2e7647284ee
Travis Schneberger <leo.herbie@gmail.com>
2014-04-01 09:22:58 -0400

    KRACOEUS-7120: fixing javadoc

--- coeus-code/src/main/java/org/kuali/kra/s2s/depend/BudgetSubAwardsService.java
+++ coeus-code/src/main/java/org/kuali/kra/s2s/depend/BudgetSubAwardsService.java
@@ -10,7 +10,7 @@ public interface BudgetSubAwardsService {
     /**
      * Finds the {@link BudgetSubAwards}s by budgetId.  Will return an empty
      * List if no items exist.
-     * @param budgetId the budget Id.  Cannot be blank.
+     * @param budgetId the budget Id.  Cannot be null.
      * @return a list of {@link BudgetSubAwards}s or an empty list.
      * @throws IllegalArgumentException if the budgetId is null
      */
@@ -19,7 +19,7 @@ public interface BudgetSubAwardsService {
     /**
      * Finds the {@link BudgetSubAwards}s by budgetId and namespace.  Will return an empty
      * List if no items exist.
-     * @param budgetId the budget Id.  Cannot be blank.
+     * @param budgetId the budget Id.  Cannot be null.
      * @param namespace the namespace.  Cannot be blank.
      * @return a list of {@link BudgetSubAwards}s or an empty list.
      * @throws IllegalArgumentException if the budgetId is null, if the namespace is blank
@@ -29,7 +29,7 @@ public interface BudgetSubAwardsService {
     /**
      * Finds the {@link BudgetSubAwards}s by budgetId and where the namespace is null.
      * Will return an empty List if no items exist.
-     * @param budgetId the budget Id.  Cannot be blank.
+     * @param budgetId the budget Id.  Cannot be null.
      * @return a list of {@link BudgetSubAwards}s or an empty list.
      * @throws IllegalArgumentException if the budgetId is null
      */
