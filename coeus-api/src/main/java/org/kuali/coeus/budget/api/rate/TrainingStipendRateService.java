5d2d476160500f40a538bb31444c0012fb47aef3
Travis Schneberger <leo.herbie@gmail.com>
2014-04-04 20:26:12 -0400

    KRACOEUS-7120: removing unused method, fixing javadoc a per code review

--- coeus-api/src/main/java/org/kuali/coeus/budget/api/rate/TrainingStipendRateService.java
+++ coeus-api/src/main/java/org/kuali/coeus/budget/api/rate/TrainingStipendRateService.java
@@ -7,12 +7,6 @@ import java.util.List;
 public interface TrainingStipendRateService {
 
     /**
-     * Retrieves all the {@link TrainingStipendRateContract}s.  Will return an empty. List if no items exist.
-     * @return a list of {@link TrainingStipendRateContract}s or an empty list.
-     */
-    List<? extends TrainingStipendRateContract> findAllTrainingStipendRates();
-
-    /**
      * This method finds the closest matching {@link TrainingStipendRateContract} based on the following rules.
      *
      * The Training Stipend Rate's effective date must be less than or equal to the passed in effective date.
@@ -22,8 +16,8 @@ public interface TrainingStipendRateService {
      * If at the end of this match, multiple Training Stipend Rates match then the one with the latest effective date
      * is returned.  This is considered the "closest match".
      *
-     * Note: that this method on considered the date not the time for date comparisons.  Also note that a TrainingStipendRate
-     * without an effective date will never be considered a match.
+     * Note: that this method only considers the date portion of a {@link Date} for effective date comparisons.
+     * Also note that a TrainingStipendRate without an effective date will never be considered a match.
      *
      * @param effectiveDate the effective date of the training stipend rate must be less than or equal to.  Cannot be null.
      * @param careerLevel the career level of the training stipend rate. Cannot be blank.
