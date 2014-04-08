49d6318a8d24a7e56fe293f5b360aea13e9d7369
David Godfrey <dgodfrey@iastate.edu>
2014-04-08 16:41:42 -0500

    KRACOEUS-7051: repackage CitizenshipType BO/Service & base Correspondent

--- coeus-code/src/main/java/org/kuali/coeus/common/impl/person/attr/CitizenshipTypeServiceImpl.java
+++ coeus-code/src/main/java/org/kuali/coeus/common/impl/person/attr/CitizenshipTypeServiceImpl.java
@@ -13,13 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.service.impl;
+package org.kuali.coeus.common.impl.person.attr;
 
 import gov.grants.apply.forms.phs398CareerDevelopmentAwardSup11V11.CitizenshipDataType;
 import gov.grants.apply.forms.phs398CareerDevelopmentAwardSup11V11.CitizenshipDataType.Enum;
-import org.kuali.kra.bo.CitizenshipType;
+
+import org.kuali.coeus.common.framework.person.attr.CitizenshipType;
+import org.kuali.coeus.common.framework.person.attr.CitizenshipTypeService;
 import org.kuali.kra.infrastructure.CitizenshipTypes;
-import org.kuali.kra.service.CitizenshipTypeService;
 
 /**
  * 
