16ff2fa4fe1320529ac58d66160f8cbf1b6b9c49
David Godfrey <dgodfrey@iastate.edu>
2014-04-09 17:06:41 -0500

    KRACOEUS-7051: deprecating keyword service method; see KRACOEUS-7148

--- coeus-code/src/main/java/org/kuali/coeus/common/impl/keyword/KeywordsServiceImpl.java
+++ coeus-code/src/main/java/org/kuali/coeus/common/impl/keyword/KeywordsServiceImpl.java
@@ -23,18 +23,17 @@ import org.kuali.coeus.common.framework.keyword.ScienceKeyword;
 import org.kuali.coeus.sys.framework.model.MultiLookupForm;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.rice.core.api.criteria.QueryByCriteria;
+import org.kuali.rice.kns.service.KNSServiceLocator;
 import org.kuali.rice.krad.bo.PersistableBusinessObject;
-import org.kuali.rice.krad.service.BusinessObjectService;
+import org.kuali.rice.krad.data.DataObjectService;
 import org.kuali.rice.krad.util.GlobalVariables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 /**
  * This class is the implementation of KeywordsService to handle the requests from keywords panel in general
@@ -44,10 +43,6 @@ import java.util.Map;
 public class KeywordsServiceImpl implements KeywordsService {
 	
     private static final org.apache.commons.logging.Log LOG = org.apache.commons.logging.LogFactory.getLog(KeywordsServiceImpl.class);
-    
-    @Autowired
-    @Qualifier("businessObjectService")
-    private BusinessObjectService businessObjectService;
     
     @Override
     public void addKeyword(KeywordsManager document, ScienceKeyword scienceKeyword) {
@@ -87,6 +82,7 @@ public class KeywordsServiceImpl implements KeywordsService {
      * Method to add keywords into keywords list associated with particular BO/Document
      * @see org.kuali.coeus.common.framework.keyword.KeywordsService#addKeywords(org.kuali.coeus.common.framework.keyword.KeywordsManager, org.kuali.coeus.sys.framework.model.MultiLookupForm)
      */
+    @Deprecated
     public void addKeywords(KeywordsManager document, MultiLookupForm multiLookUpForm) {
         try{
             // check to see if we are coming back from a lookup
@@ -96,10 +92,8 @@ public class KeywordsServiceImpl implements KeywordsService {
                 String lookupResultsSequenceNumber = multiLookUpForm.getLookupResultsSequenceNumber();//implement MultiLookupFormSupport
                 if (StringUtils.isNotBlank(lookupResultsSequenceNumber)) {
                     Class lookupResultsBOClass = Class.forName(multiLookUpForm.getLookupResultsBOClassName());
-                    QueryByCriteria criteria = QueryByCriteria.Builder.forAttribute("lookupResultsSequenceNumber", lookupResultsSequenceNumber).build();
-                    Map<String,Object> critMap = new HashMap<String,Object>();
-                    critMap.put("lookupResultsSequenceNumber", lookupResultsSequenceNumber);
-                    Collection<PersistableBusinessObject> rawValues = businessObjectService.findMatching(lookupResultsBOClass, critMap);
+                    Collection<PersistableBusinessObject> rawValues = KNSServiceLocator.getLookupResultsService()
+                    		.retrieveSelectedResultBOs(lookupResultsSequenceNumber, lookupResultsBOClass, GlobalVariables.getUserSession().getPrincipalId());
                     if (lookupResultsBOClass.isAssignableFrom(ScienceKeyword.class)) {
                         for (Iterator iter = rawValues.iterator(); iter.hasNext();) {
                             ScienceKeyword scienceKeyword = (ScienceKeyword) iter.next();
