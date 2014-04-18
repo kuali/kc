a5d7dd17eb0389c239417df7392f54c556fe15c6
yyuan1 <yyuan1@kuali.org>
2014-04-17 21:19:28 -0400

    KRACOEUS-6956 : repack propdev remove the added sql file(s) for the authrozer change made at the table level

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/dml/KR_DML_01_KRACOEUS-6956_0TS0.sql
+++
@@ -1,2 +0,0 @@
-update KREW_DOC_TYP_T set AUTHORIZER = 'org.kuali.coeus.propdev.impl.auth.ProposalDevelopmentWorkflowDocumentAuthorizer' where  DOC_TYP_NM LIKE '%ProposalDevelopmentDocument%' and CUR_IND = 1
-/
--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/dml/KR_DML_01_KRACOEUS-6956_0TS0.sql
+++
@@ -1,5 +0,0 @@
-DELIMITER /
-update KREW_DOC_TYP_T set AUTHORIZER = 'org.kuali.coeus.propdev.impl.auth.ProposalDevelopmentWorkflowDocumentAuthorizer' where  DOC_TYP_NM LIKE '%ProposalDevelopmentDocument%' and CUR_IND = 1
-/
-
-DELIMITER ;
