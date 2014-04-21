19030746748afc4ed34f6435768d4080828f9b3d
nkimble <nkk4@cornell.edu>
2014-04-21 09:06:43 -0400

    KRACOEUS-7150: Updated service namespace for service name iacucCorrespondentDerivedRoleTypeService to fix STE generated when role maintenance document is finalized.

---
+++ src/main/config/sql/current_mysql/5.2.1/dml/KR_DML_01_KRACOEUS-7150_B000.sql
@@ -0,0 +1,8 @@
+DELIMITER 
+/
+
+UPDATE KRIM_TYP_T SET SRVC_NM = CONCAT('{http://kc.kuali.org/core/v5_0}', SRVC_NM) 
+WHERE NM = 'Derived Role: IACUC Correspondent' AND SRVC_NM = 'iacucCorrespondentDerivedRoleTypeService' AND NMSPC_CD = 'KC-IACUC'
+/
+
+DELIMITER ;
