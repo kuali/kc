c3babba3175a5a3b7fe5db916b7df7178ff0a2e4
blackcathacker <douglas.pace@gmail.com>
2014-04-22 12:52:41 -0700

    KRACOEUS-7109 : Initial Multi-PI to real role work.

---
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/sequences/KC_SEQ_EPS_PROP_PERSON_ROLE.sql
@@ -0,0 +1,9 @@
+DELIMITER /
+CREATE TABLE SEQ_EPS_PROP_PERSON_ROLE (
+		id bigint(19) not null auto_increment, primary key (id)
+) ENGINE MyISAM
+/
+ALTER TABLE SEQ_EPS_PROP_PERSON_ROLE auto_increment = 1
+/
+
+DELIMITER ;
