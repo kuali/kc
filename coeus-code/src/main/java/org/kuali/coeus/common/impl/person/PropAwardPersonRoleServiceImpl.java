710f1253f21cdee42573e28fff976ece46daab3b
blackcathacker <douglas.pace@gmail.com>
2014-04-24 12:20:41 -0700

    KRACOEUS-7109 : Implement All NIH Sponsor param

--- coeus-code/src/main/java/org/kuali/coeus/common/impl/person/PropAwardPersonRoleServiceImpl.java
+++ coeus-code/src/main/java/org/kuali/coeus/common/impl/person/PropAwardPersonRoleServiceImpl.java
@@ -1,12 +1,13 @@
 package org.kuali.coeus.common.impl.person;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.kuali.coeus.common.api.sponsor.hierarchy.SponsorHierarchyService;
 import org.kuali.coeus.common.framework.person.PropAwardPersonRole;
 import org.kuali.coeus.common.framework.person.PropAwardPersonRoleService;
+import org.kuali.coeus.propdev.impl.core.ProposalDevelopmentDocument;
+import org.kuali.kra.infrastructure.Constants;
 import org.kuali.rice.core.api.criteria.PredicateFactory;
 import org.kuali.rice.core.api.criteria.QueryByCriteria;
 import org.kuali.rice.coreservice.framework.parameter.ParameterService;
@@ -19,6 +20,8 @@ import org.springframework.stereotype.Service;
 public class PropAwardPersonRoleServiceImpl implements PropAwardPersonRoleService {
 	
 	private static final String SPONSOR_HIERARCHIES_PARM = "PERSON_ROLE_SPONSOR_HIERARCHIES";
+	private static final String ALL_SPONSOR_HIERARCHY_NIH_MULTI_PI = "ALL_SPONSOR_HIERARCHY_NIH_MULTI_PI";
+	private static final String NIH_MULTIPLE_PI_HIERARCHY = "NIH Multiple PI";
 
 	@Autowired
 	@Qualifier("dataObjectService")
@@ -51,7 +54,7 @@ public class PropAwardPersonRoleServiceImpl implements PropAwardPersonRoleServic
             throw new IllegalArgumentException("The sponsorCode cannot be blank");
         }
         
-		String hierarchyName = getSponsorHierarchyLevel(sponsorCode);
+		String hierarchyName = getSponsorHierarchy(sponsorCode);
 		QueryByCriteria.Builder criteria = 
 				QueryByCriteria.Builder.create().setPredicates(PredicateFactory.equal("code", roleCode), PredicateFactory.equal("sponsorHierarchyName", hierarchyName));
 		return getDataObjectService().findUnique(PropAwardPersonRole.class, criteria.build());
@@ -63,11 +66,20 @@ public class PropAwardPersonRoleServiceImpl implements PropAwardPersonRoleServic
             throw new IllegalArgumentException("The sponsorCode cannot be blank");
         }
 
-		String hierarchyName = getSponsorHierarchyLevel(sponsorCode);
+		String hierarchyName = getSponsorHierarchy(sponsorCode);
 		return getDataObjectService().findMatching(PropAwardPersonRole.class, QueryByCriteria.Builder.forAttribute("sponsorHierarchyName", hierarchyName).build()).getResults();
 	}
 	
-	protected String getSponsorHierarchyLevel(String sponsorCode) {
+	@Override
+	public boolean isNihOtherSignificantContributerEnabledForSponsor(
+			String sponsorCode) {
+        return areAllSponsorsMultiPi() || getSponsorHierarchyService().isSponsorNihOsc(sponsorCode);
+	}	
+	
+	protected String getSponsorHierarchy(String sponsorCode) {
+		if (areAllSponsorsMultiPi()) {
+			return NIH_MULTIPLE_PI_HIERARCHY;
+		}
 		for (String hierarchyName : getRoleHierarchies()) {
 			if (getSponsorHierarchyService().isSponsorInHierarchy(sponsorCode, hierarchyName)) {
 				return hierarchyName;
@@ -76,8 +88,12 @@ public class PropAwardPersonRoleServiceImpl implements PropAwardPersonRoleServic
 		return DEFAULT_SPONSOR_HIERARCHY_NAME;
 	}
 	
+	protected Boolean areAllSponsorsMultiPi() {
+		return getParameterService().getParameterValueAsBoolean(Constants.MODULE_NAMESPACE_PROPOSAL_DEVELOPMENT, Constants.PARAMETER_COMPONENT_DOCUMENT, ALL_SPONSOR_HIERARCHY_NIH_MULTI_PI);
+	}
+	
 	protected Collection<String> getRoleHierarchies() {
-		return getParameterService().getParameterValuesAsString("KC-GEN", "All", SPONSOR_HIERARCHIES_PARM);
+		return getParameterService().getParameterValuesAsString(Constants.KC_GENERIC_PARAMETER_NAMESPACE, Constants.KC_ALL_PARAMETER_DETAIL_TYPE_CODE, SPONSOR_HIERARCHIES_PARM);
 	}
 	
 	protected DataObjectService getDataObjectService() {
@@ -103,5 +119,5 @@ public class PropAwardPersonRoleServiceImpl implements PropAwardPersonRoleServic
 
 	public void setParameterService(ParameterService parameterService) {
 		this.parameterService = parameterService;
-	}	
+	}
 }
