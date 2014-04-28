b67babe3984b929e92b3d367d7a66e71b3699c15
aakers <allen.akers@colostate.edu>
2014-04-28 09:36:27 -0400

    First cut at sql release scripts for 5.2.1

---
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_2_1-SCRIPT/KR-RELEASE-5_2_1-Upgrade-ORACLE.sql
@@ -0,0 +1,18 @@
+set define off
+set sqlblanklines on
+spool KR-RELEASE-5_2_1-Upgrade-ORACLE-Install.log
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-6640_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-6774_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-6848_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-6862_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-6960_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-6980_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7031_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7046_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7150_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7152_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7174_B000.sql
+@../../current/5.2.1/dml/KR_DML_02_KRACOEUS-6848_B000.sql
+@../../current/5.2.1/dml/KR_DML_03_KRACOEUS-6848_B000.sql
+commit;
+exit
