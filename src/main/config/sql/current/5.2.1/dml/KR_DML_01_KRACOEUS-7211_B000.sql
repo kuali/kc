f92f2ab9e4d1a30fb0ab08a6afc14b0221a5ed8e
nkimble <nkk4@cornell.edu>
2014-05-01 14:21:26 -0400

    KRACOEUS-7211: Database parameter additions and tag file changes to allow the help pages to show.

---
+++ src/main/config/sql/current/5.2.1/dml/KR_DML_01_KRACOEUS-7211_B000.sql
@@ -0,0 +1,11 @@
+INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+VALUES ('KC-COMMITTEE', 'Document', 'committeeGenerateBatchCorrespondenceHelpUrl', SYS_GUID(), 1, 'HELP', 'default.htm?turl=Documents/generatebatchcorrespondence.htm', 'Committee Generate Batch Correspondence Help', 'A', 'KC')
+/
+
+INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+VALUES ('KC-COMMITTEE', 'Document', 'committeeGeneratedBatchCorrespondenceHelpUrl', SYS_GUID(), 1, 'HELP', 'default.htm?turl=Documents/generatedbatchcorrespondence.htm', 'Committee Generated Batch Correspondence Help', 'A', 'KC')
+/
+
+INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+VALUES ('KC-COMMITTEE', 'Document', 'committeeBatchCorrespondenceHistoryHelpUrl', SYS_GUID(), 1, 'HELP', 'default.htm?turl=Documents/batchcorrespondencehistory.htm', 'Committee Batch Correspondence History Help', 'A', 'KC')
+/
