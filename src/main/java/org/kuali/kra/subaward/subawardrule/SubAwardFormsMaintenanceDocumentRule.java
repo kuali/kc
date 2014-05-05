8327a216db611bc0ddfca42a4e1e21010577f30e
gathreya <hsggwk@gmail.com>
2014-05-05 19:56:37 -0400

    KRACOEUS-7213: Fixing multiple issues with the subaward forms table.

---
+++ src/main/java/org/kuali/kra/subaward/subawardrule/SubAwardFormsMaintenanceDocumentRule.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2005-2014 The Kuali Foundation
+ * 
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kuali.kra.subaward.subawardrule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kuali.kra.coi.CoiDisclosureStatus;
+import org.kuali.kra.coi.CoiDispositionStatus;
+import org.kuali.kra.infrastructure.KeyConstants;
+import org.kuali.kra.infrastructure.KraServiceLocator;
+import org.kuali.kra.rules.KraMaintenanceDocumentRuleBase;
+import org.kuali.kra.subaward.bo.SubAwardForms;
+import org.kuali.rice.kns.document.MaintenanceDocument;
+import org.kuali.rice.krad.service.BusinessObjectService;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADConstants;
+
+public class SubAwardFormsMaintenanceDocumentRule extends KraMaintenanceDocumentRuleBase {
+    
+    private static final String TEMPLATE_FILE_FIELD_NAME = "templateFile";
+
+    /**
+     * 
+     * @see org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase#processCustomRouteDocumentBusinessRules(org.kuali.rice.kns.document.MaintenanceDocument)
+     */
+
+    protected boolean processCustomRouteDocumentBusinessRules(MaintenanceDocument document) {
+        return isDocumentValidForSave(document);
+    }
+
+
+    /**
+     * @see org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase#processCustomSaveDocumentBusinessRules(org.kuali.rice.kns.document.MaintenanceDocument)
+     */
+    @Override
+    public boolean processCustomSaveDocumentBusinessRules(MaintenanceDocument document) {
+        return isDocumentValidForSave(document);
+    }
+
+    /**
+     * 
+     * @see org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase#processCustomApproveDocumentBusinessRules(org.kuali.rice.kns.document.MaintenanceDocument)
+     */
+    @Override
+    public boolean processCustomApproveDocumentBusinessRules(MaintenanceDocument document) {
+        return isDocumentValidForSave(document);
+    }
+
+    /**
+     * 
+     * @see org.kuali.rice.kns.maintenance.rules.MaintenanceDocumentRuleBase#isDocumentValidForSave(org.kuali.rice.kns.document.MaintenanceDocument)
+     */
+    public boolean isDocumentValidForSave(MaintenanceDocument document) {
+        boolean result = super.isDocumentValidForSave(document);
+        final SubAwardForms subAwardForms = (SubAwardForms) document.getNewMaintainableObject().getDataObject();
+        String fileName = subAwardForms.getFileName() == null ? subAwardForms.getTemplateFile().getFileName() : subAwardForms.getFileName();
+        if (!fileName.endsWith(".xsl")) {
+            result = false;
+            putFieldError(TEMPLATE_FILE_FIELD_NAME, KeyConstants.SUBAWARD_ERROR_INVALID_FILE_TYPE);
+        }
+        return result;
+    }
+    
+}
