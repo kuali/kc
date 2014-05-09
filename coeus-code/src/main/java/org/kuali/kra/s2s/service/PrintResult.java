71e860f61e1a63ae77261dd02005c550a388f24a
Travis Schneberger <leo.herbie@gmail.com>
2014-05-09 09:07:30 -0400

    KCINFR-979: removing PrintServiceImpl's use of GlobalVariables

---
+++ coeus-code/src/main/java/org/kuali/kra/s2s/service/PrintResult.java
@@ -0,0 +1,27 @@
+package org.kuali.kra.s2s.service;
+
+import org.kuali.coeus.sys.api.model.KcFile;
+import org.kuali.kra.s2s.util.AuditError;
+
+import java.util.List;
+
+public class PrintResult {
+    private KcFile file;
+    private List<AuditError> errors;
+
+    public KcFile getFile() {
+        return file;
+    }
+
+    public void setFile(KcFile file) {
+        this.file = file;
+    }
+
+    public List<AuditError> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<AuditError> errors) {
+        this.errors = errors;
+    }
+}
