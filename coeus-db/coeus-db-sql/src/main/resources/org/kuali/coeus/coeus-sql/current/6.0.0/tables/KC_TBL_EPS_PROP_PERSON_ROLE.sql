4c79933c01b1b24b97598b24ab92271f0640242e
blackcathacker <douglas.pace@gmail.com>
2014-05-12 20:07:53 -0700

    KRACOEUS-7109 : Code review changes and oracle sql fixes

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/tables/KC_TBL_EPS_PROP_PERSON_ROLE.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/tables/KC_TBL_EPS_PROP_PERSON_ROLE.sql
@@ -1,4 +1,4 @@
-alter table AWARD_PERSONS drop foreign key FK_PROP_PERS_ROLE_AWD_PERSONS
+alter table AWARD_PERSONS drop constraint FK_PROP_PERS_ROLE_AWD_PERSONS
 /
 
 alter table EPS_PROP_PERSON_ROLE drop primary key
@@ -10,23 +10,29 @@ alter table EPS_PROP_PERSON_ROLE add PROP_PERSON_ROLE_CODE varchar2(12)
 update EPS_PROP_PERSON_ROLE set PROP_PERSON_ROLE_CODE = PROP_PERSON_ROLE_ID
 /
 
-alter table EPS_PROP_PERSON_ROLE modify column PROP_PERSON_ROLE_CODE varchar2(12) not null
+alter table EPS_PROP_PERSON_ROLE modify PROP_PERSON_ROLE_CODE varchar2(12) not null
 /
 
-alter table EPS_PROP_PERSON_ROLE add SPONSOR_HIERARCHY_NAME varchar2(50) not null default 'DEFAULT'
+alter table EPS_PROP_PERSON_ROLE add SPONSOR_HIERARCHY_NAME varchar2(50) default 'DEFAULT' not null
 /
 
-update EPS_PROP_PERSON_ROLE set PROP_PERSON_ROLE_ID = (select SEQ_EPS_PROP_PERSON_ROLE.nextval from dual)
+update EPS_PROP_PERSON_ROLE set PROP_PERSON_ROLE_ID = null
 /
 
-alter table EPS_PROP_PERSON_ROLE modify column PROP_PERSON_ROLE_ID number(12,0) not null
+alter table EPS_PROP_PERSON_ROLE modify PROP_PERSON_ROLE_ID number(12,0)
+/
+
+update EPS_PROP_PERSON_ROLE set PROP_PERSON_ROLE_ID = SEQ_EPS_PROP_PERSON_ROLE.nextval
+/
+
+alter table EPS_PROP_PERSON_ROLE modify PROP_PERSON_ROLE_ID number(12,0) not null
 /
 
 alter table EPS_PROP_PERSON_ROLE add primary key (PROP_PERSON_ROLE_ID)
 /
 
-alter table EPS_PROP_PERSON_ROLE add constraint unique EPS_PROP_PERSON_ROLE_U1 (PROP_PERSON_ROLE_CODE, SPONSOR_HIERARCHY_NAME)
+alter table EPS_PROP_PERSON_ROLE add constraint EPS_PROP_PERSON_ROLE_U1 unique (PROP_PERSON_ROLE_CODE, SPONSOR_HIERARCHY_NAME)
 /
 
-alter table EPS_PROP_PERSON_ROLE add READ_ONLY_ROLE char(1) not null default 'N'
+alter table EPS_PROP_PERSON_ROLE add READ_ONLY_ROLE char(1) default 'N' not null
 /
