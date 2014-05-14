d804924e57e5b38ebac15b87d3f9a5458404e8fe
gathreya <hsggwk@gmail.com>
2014-05-14 18:37:51 -0400

    KCINFR-1018: Committing SQL fixes

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_2_1-SCRIPT/KR-RELEASE-5_2_1-Upgrade-ORACLE.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-5_2_1-SCRIPT/KR-RELEASE-5_2_1-Upgrade-ORACLE.sql
@@ -15,7 +15,9 @@ spool KR-RELEASE-5_2_1-Upgrade-ORACLE-Install.log
 @../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7207_B000.sql
 @../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7211_B000.sql
 @../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7214_B000.sql
+@../../current/5.2.1/dml/KR_DML_01_KRACOEUS-7230_B000.sql
 @../../current/5.2.1/dml/KR_DML_02_KRACOEUS-6848_B000.sql
+@../../current/5.2.1/dml/KR_DML_02_KRACOEUS-7444_B000.sql
 @../../current/5.2.1/dml/KR_DML_03_KRACOEUS-6848_B000.sql
 commit;
 exit
