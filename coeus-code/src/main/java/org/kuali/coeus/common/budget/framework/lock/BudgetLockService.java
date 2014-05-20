36afaca361f6b63df777e3c36efc72ec8b4c0698
yyuan1 <yyuan1@kuali.org>
2014-05-19 23:36:13 -0400

    KRACOEUS-6955 : common budget lock

--- coeus-code/src/main/java/org/kuali/coeus/common/budget/framework/lock/BudgetLockService.java
+++ coeus-code/src/main/java/org/kuali/coeus/common/budget/framework/lock/BudgetLockService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.budget.service;
+package org.kuali.coeus.common.budget.framework.lock;
 
 import org.kuali.rice.krad.service.PessimisticLockService;
 
