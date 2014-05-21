c33116a6d02a31cbdf15f589f4e18d5d640f24c3
aakers <allen.akers@colostate.edu>
2014-05-20 20:26:25 -0400

    Fixing error with null values

--- src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-CLEAN/mysql/krrelease/datasql/KR_00_SEQ_BS.sql
+++ src/main/config/sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-CLEAN/mysql/krrelease/datasql/KR_00_SEQ_BS.sql
@@ -30,7 +30,7 @@ create procedure p ()
 begin
   declare l_new_seq INT;
   
-  select max(numericVal(KIM_ATTR_DEFN_ID)) + 1 into l_new_seq from KRIM_ATTR_DEFN_T where numericVal(KIM_ATTR_DEFN_ID) < 10000;
+  select ifnull(max(numericVal(KIM_ATTR_DEFN_ID)) + 1,1) into l_new_seq from KRIM_ATTR_DEFN_T where numericVal(KIM_ATTR_DEFN_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_ATTR_DEFN_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -40,7 +40,7 @@ begin
   execute alter_seq_stmt; 
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(GRP_ID)) + 1 into l_new_seq from KRIM_GRP_T where numericVal(GRP_ID) < 10000;
+  select ifnull(max(numericVal(GRP_ID)) + 1,1) into l_new_seq from KRIM_GRP_T where numericVal(GRP_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_GRP_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -50,7 +50,7 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(GRP_MBR_ID)) + 1 into l_new_seq from KRIM_GRP_MBR_T where numericVal(GRP_MBR_ID) < 10000;
+  select ifnull(max(numericVal(GRP_MBR_ID)) + 1,1) into l_new_seq from KRIM_GRP_MBR_T where numericVal(GRP_MBR_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_GRP_MBR_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -60,7 +60,7 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(PERM_TMPL_ID)) + 1 into l_new_seq from KRIM_PERM_TMPL_T where numericVal(PERM_TMPL_ID) < 10000;
+  select ifnull(max(numericVal(PERM_TMPL_ID)) + 1,1) into l_new_seq from KRIM_PERM_TMPL_T where numericVal(PERM_TMPL_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_PERM_TMPL_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -70,7 +70,7 @@ begin
   execute alter_seq_stmt; 
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(PERM_ID)) + 1 into l_new_seq from KRIM_PERM_T where numericVal(PERM_ID) < 10000;
+  select ifnull(max(numericVal(PERM_ID)) + 1,1) into l_new_seq from KRIM_PERM_T where numericVal(PERM_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_PERM_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -80,7 +80,7 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(ROLE_ID)) + 1 into l_new_seq from KRIM_ROLE_T where numericVal(ROLE_ID) < 10000;
+  select ifnull(max(numericVal(ROLE_ID)) + 1,1) into l_new_seq from KRIM_ROLE_T where numericVal(ROLE_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_ROLE_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -90,7 +90,7 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(ROLE_MBR_ID)) + 1 into l_new_seq from KRIM_ROLE_MBR_T where numericVal(ROLE_MBR_ID) < 10000;
+  select ifnull(max(numericVal(ROLE_MBR_ID)) + 1,1) into l_new_seq from KRIM_ROLE_MBR_T where numericVal(ROLE_MBR_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_ROLE_MBR_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -100,7 +100,7 @@ begin
   execute alter_seq_stmt; 
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(RSP_ID)) + 1 into l_new_seq from KRIM_RSP_T where numericVal(RSP_ID) < 10000;
+  select ifnull(max(numericVal(RSP_ID)) + 1,1) into l_new_seq from KRIM_RSP_T where numericVal(RSP_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_RSP_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -110,7 +110,7 @@ begin
   execute alter_seq_stmt; 
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(ROLE_PERM_ID)) + 1 into l_new_seq from KRIM_ROLE_PERM_T where numericVal(ROLE_PERM_ID) < 10000;
+  select ifnull(max(numericVal(ROLE_PERM_ID)) + 1,1) into l_new_seq from KRIM_ROLE_PERM_T where numericVal(ROLE_PERM_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_ROLE_PERM_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -120,7 +120,7 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
 
-  select max(numericVal(ROLE_RSP_ID)) + 1 into l_new_seq from KRIM_ROLE_RSP_T where numericVal(ROLE_RSP_ID) < 10000;
+  select ifnull(max(numericVal(ROLE_RSP_ID)) + 1,1) into l_new_seq from KRIM_ROLE_RSP_T where numericVal(ROLE_RSP_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_ROLE_RSP_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -130,7 +130,7 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
           
-  select max(numericVal(ROLE_RSP_ACTN_ID)) + 1 into l_new_seq from KRIM_ROLE_RSP_ACTN_T where numericVal(ROLE_RSP_ACTN_ID) < 10000;
+  select ifnull(max(numericVal(ROLE_RSP_ACTN_ID)) + 1,1) into l_new_seq from KRIM_ROLE_RSP_ACTN_T where numericVal(ROLE_RSP_ACTN_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_ROLE_RSP_ACTN_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -140,11 +140,11 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(ID)) + 1 into l_new_seq from 
-    ((select max(numericVal(ATTR_DATA_ID)) as ID from KRIM_RSP_ATTR_DATA_T where numericVal(ATTR_DATA_ID) < 10000) 
-      union (select max(numericVal(ATTR_DATA_ID)) as ID from KRIM_DLGN_MBR_ATTR_DATA_T where numericVal(ATTR_DATA_ID) < 10000)
-      union (select max(numericVal(ATTR_DATA_ID)) as ID from KRIM_ROLE_MBR_ATTR_DATA_T where numericVal(ATTR_DATA_ID) < 10000)
-      union (select max(numericVal(ATTR_DATA_ID)) as ID from KRIM_PERM_ATTR_DATA_T where numericVal(ATTR_DATA_ID) < 10000)) as t1;
+  select ifnull(max(numericVal(ID)) + 1,1) into l_new_seq from 
+    ((select ifnull(max(numericVal(ATTR_DATA_ID)),0) as ID from KRIM_RSP_ATTR_DATA_T where numericVal(ATTR_DATA_ID) < 10000) 
+      union (select ifnull(max(numericVal(ATTR_DATA_ID)),0) as ID from KRIM_DLGN_MBR_ATTR_DATA_T where numericVal(ATTR_DATA_ID) < 10000)
+      union (select ifnull(max(numericVal(ATTR_DATA_ID)),0) as ID from KRIM_ROLE_MBR_ATTR_DATA_T where numericVal(ATTR_DATA_ID) < 10000)
+      union (select ifnull(max(numericVal(ATTR_DATA_ID)),0) as ID from KRIM_PERM_ATTR_DATA_T where numericVal(ATTR_DATA_ID) < 10000)) as t1;
   set @create_seq := 'CREATE TABLE KRIM_ATTR_DATA_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -154,7 +154,7 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(KIM_TYP_ID)) + 1 into l_new_seq from KRIM_TYP_T where numericVal(KIM_TYP_ID) < 10000;
+  select ifnull(max(numericVal(KIM_TYP_ID)) + 1,1) into l_new_seq from KRIM_TYP_T where numericVal(KIM_TYP_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_TYP_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
@@ -164,7 +164,7 @@ begin
   execute alter_seq_stmt;
   deallocate prepare alter_seq_stmt;
   
-  select max(numericVal(KIM_TYP_ATTR_ID)) + 1 into l_new_seq from KRIM_TYP_ATTR_T where numericVal(KIM_TYP_ATTR_ID) < 10000;
+  select ifnull(max(numericVal(KIM_TYP_ATTR_ID)) + 1,1) into l_new_seq from KRIM_TYP_ATTR_T where numericVal(KIM_TYP_ATTR_ID) < 10000;
   set @create_seq := 'CREATE TABLE KRIM_TYP_ATTR_ID_BS_S (id bigint(19) not null auto_increment, primary key (id)) ENGINE MyISAM';
   prepare create_seq_stmt from @create_seq;
   execute create_seq_stmt;
