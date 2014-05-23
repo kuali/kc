7e88bdfd2670dceaca3efc5572abc04731dd0b6b
yyuan1 <yyuan1@kuali.org>
2014-05-23 13:07:31 -0400

    KRACOEUS-6955 : common budget rate repack --fix typo

--- coeus-code/src/main/java/org/kuali/coeus/common/budget/framework/rate/AbstractBudgetRate.java
+++ coeus-code/src/main/java/org/kuali/coeus/common/budget/framework/rate/AbstractBudgetRate.java
@@ -6,7 +6,7 @@
  * You may obtain a copy of the License at
  * 
  * http://www.osedu.org/licenses/ECL-2.0
- * X
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
