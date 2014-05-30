bbd5e9fb7996fb3aacd0408e6152faeddd2daa74
blackcathacker <douglas.pace@gmail.com>
2014-05-29 20:24:15 -0700

    Jira#KRACOEUS-7252 - DB Script

---
+++ src/main/config/sql/current_mysql/5.2.1/sequences/KC_SEQ_S2S_USER_ATTD_FORM_FILE_ID.sql
@@ -0,0 +1,10 @@
+DELIMITER /
+
+CREATE TABLE SEQ_S2S_USER_ATTD_FORM_FILE_ID (
+		id bigint(19) not null auto_increment, primary key (id)
+) ENGINE MyISAM
+/
+ALTER TABLE SEQ_S2S_USER_ATTD_FORM_FILE_ID auto_increment = 1
+/
+
+DELIMITER ;
