2fb0728cbb3fdcde85346b6bab88869bb3f33960
Travis Schneberger <leo.herbie@gmail.com>
2014-06-17 14:50:47 -0400

    KCINFR-979: renaming attribute

--- coeus-api/src/main/java/org/kuali/coeus/propdev/api/specialreview/ProposalSpecialReviewContract.java
+++ coeus-api/src/main/java/org/kuali/coeus/propdev/api/specialreview/ProposalSpecialReviewContract.java
@@ -1,12 +1,11 @@
 package org.kuali.coeus.propdev.api.specialreview;
 
 import org.kuali.coeus.propdev.api.core.NumberedProposal;
+import org.kuali.coeus.sys.api.model.IdentifiableNumeric;
 
 import java.util.List;
 
-public interface ProposalSpecialReviewContract extends NumberedProposal {
-
-    Long getProposalSpecialReviewId();
+public interface ProposalSpecialReviewContract extends NumberedProposal, IdentifiableNumeric {
 
     String getHierarchyProposalNumber();
 
