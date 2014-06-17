3df6dac3eb0e612547ea11957b79778b139de17f
jhulslander <jdh34@cornell.edu>
2014-06-17 09:28:15 -0400

    Revert "Saving stuff, will revert"
    This reverts commit 1d2ced205940be5041b6920dd89668afbe5a9de3.

--- coeus-code/src/main/java/org/kuali/coeus/propdev/impl/person/ProposalDevelopmentPersonnelController.java
+++ coeus-code/src/main/java/org/kuali/coeus/propdev/impl/person/ProposalDevelopmentPersonnelController.java
@@ -21,7 +21,6 @@ import org.kuali.coeus.common.framework.rolodex.Rolodex;
 import org.kuali.coeus.propdev.impl.core.ProposalDevelopmentControllerBase;
 import org.kuali.coeus.propdev.impl.core.ProposalDevelopmentDocument;
 import org.kuali.coeus.propdev.impl.core.ProposalDevelopmentDocumentForm;
-import org.kuali.coeus.propdev.impl.person.question.ProposalPersonQuestionnaireHelper;
 import org.kuali.kra.questionnaire.answer.AnswerHeader;
 import org.kuali.rice.kns.lookup.LookupableHelperService;
 import org.kuali.rice.krad.web.form.DocumentFormBase;
@@ -35,7 +34,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -67,17 +65,8 @@ public class ProposalDevelopmentPersonnelController extends ProposalDevelopmentC
    public ModelAndView savePersonnel(@ModelAttribute("KualiForm") DocumentFormBase form, BindingResult result,
            HttpServletRequest request, HttpServletResponse response) throws Exception {
        ProposalDevelopmentDocumentForm pdForm = (ProposalDevelopmentDocumentForm) form;
-       ModelAndView mv=  super.save(pdForm, result, request, response);
        saveAnswerHeaders(pdForm);
-       
-       //just needed until questionnaire gets JPAed	   
-	   for (ProposalPerson person : ((ProposalDevelopmentDocumentForm) form).getProposalDevelopmentDocument().getDevelopmentProposal().getProposalPersons()) {
-		   ProposalPersonQuestionnaireHelper qh = new ProposalPersonQuestionnaireHelper(person);
-		   qh.populateAnswers();
-		   //person.setQuestionnaireHelper(qh);
-	   }
-       
-       return mv;
+       return super.save(pdForm, result, request, response);
    }
    
    @RequestMapping(value = "/proposalDevelopment", params="methodToCall=performPersonnelSearch")
