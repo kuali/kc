c0c3667c3fcaf23ba272f3052ab799dd5ea4a028
Travis Schneberger <leo.herbie@gmail.com>
2014-06-18 14:43:09 -0400

    KCINFR-979: proposal type api

---
+++ coeus-api/src/main/java/org/kuali/coeus/common/api/type/ProposalTypeContract.java
@@ -0,0 +1,7 @@
+package org.kuali.coeus.common.api.type;
+
+import org.kuali.coeus.sys.api.model.Coded;
+import org.kuali.coeus.sys.api.model.Describable;
+
+public interface ProposalTypeContract extends Coded, Describable {
+}
