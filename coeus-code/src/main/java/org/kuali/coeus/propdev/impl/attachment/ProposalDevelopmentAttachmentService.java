6bc11e6a7a40c2ff2fafe46750adc80626acc9af
Joe Williams <jwillia@iastate.edu>
2014-06-23 11:04:47 -0500

    KRACOEUS-6634:standardize attachment file names

---
+++ coeus-code/src/main/java/org/kuali/coeus/propdev/impl/attachment/ProposalDevelopmentAttachmentService.java
@@ -0,0 +1,8 @@
+package org.kuali.coeus.propdev.impl.attachment;
+
+
+import org.kuali.coeus.propdev.impl.core.DevelopmentProposal;
+
+public interface ProposalDevelopmentAttachmentService {
+    public void standardizeAttachments(DevelopmentProposal developmentProposal);
+}
