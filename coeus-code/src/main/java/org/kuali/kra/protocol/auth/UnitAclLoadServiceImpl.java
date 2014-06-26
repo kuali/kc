3f2f5072e5510763ae4228c6852086bae55c6ff2
yyuan1 <yyuan1@kuali.org>
2014-06-26 13:37:55 -0400

    KRACOEUS-6955 move *Acl* classes to kra protocol auth for later repackaging

--- coeus-code/src/main/java/org/kuali/kra/protocol/auth/UnitAclLoadServiceImpl.java
+++ coeus-code/src/main/java/org/kuali/kra/protocol/auth/UnitAclLoadServiceImpl.java
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.kuali.kra.service.impl;
+package org.kuali.kra.protocol.auth;
 
 import org.apache.commons.lang3.StringUtils;
 import org.kuali.coeus.sys.framework.auth.SystemAuthorizationService;
 import org.kuali.coeus.sys.framework.auth.perm.KcAuthorizationService;
 import org.kuali.coeus.sys.framework.auth.perm.Permissionable;
-import org.kuali.kra.service.UnitAclLoadService;
+import org.kuali.kra.protocol.auth.UnitAclLoadService;
 import org.kuali.rice.kim.api.role.Role;
 import org.kuali.rice.kim.api.role.RoleMembership;
 import org.kuali.rice.kim.api.role.RoleService;
