da38377f9f297a83ccdc28003e36c922a6c1e915
Travis Schneberger <leo.herbie@gmail.com>
2014-06-26 10:15:32 -0400

    KCINFR-979: matching interfaces

--- coeus-api/src/main/java/org/kuali/coeus/propdev/api/person/ProposalPersonContract.java
+++ coeus-api/src/main/java/org/kuali/coeus/propdev/api/person/ProposalPersonContract.java
@@ -2,13 +2,14 @@ package org.kuali.coeus.propdev.api.person;
 
 import org.kuali.coeus.common.api.person.KcPersonContract;
 import org.kuali.coeus.common.api.person.attr.CitizenshipTypeContract;
+import org.kuali.coeus.propdev.api.hierarchy.HierarchicalProposal;
 import org.kuali.coeus.propdev.api.person.creditsplit.ProposalPersonCreditSplitContract;
 import org.kuali.coeus.sys.api.model.ScaleTwoDecimal;
 
 import java.util.Date;
 import java.util.List;
 
-public interface ProposalPersonContract extends ProposalPersonLink {
+public interface ProposalPersonContract extends ProposalPersonLink, HierarchicalProposal {
     
     boolean getConflictOfInterestFlag();
 
@@ -31,10 +32,6 @@ public interface ProposalPersonContract extends ProposalPersonLink {
     String getProjectRole();
     
     Integer getOrdinalPosition();
-    
-    String getHierarchyProposalNumber();
-    
-    Boolean getHiddenInHierarchy();
     
     String getPersonId();
     
