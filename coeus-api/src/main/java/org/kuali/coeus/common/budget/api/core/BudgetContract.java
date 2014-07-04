b77e35e2a7655ec1a45e910e40c739258572edbc
Travis Schneberger <leo.herbie@gmail.com>
2014-07-03 21:19:36 -0400

    KCINFR-979: code review comments

--- coeus-api/src/main/java/org/kuali/coeus/common/budget/api/core/BudgetContract.java
+++ coeus-api/src/main/java/org/kuali/coeus/common/budget/api/core/BudgetContract.java
@@ -9,16 +9,55 @@ import org.kuali.coeus.common.budget.api.rate.BudgetLaRateContract;
 import org.kuali.coeus.common.budget.api.rate.BudgetRateContract;
 import org.kuali.coeus.common.budget.api.rate.RateClassContract;
 import org.kuali.coeus.propdev.api.budget.subaward.BudgetSubAwardsContract;
+import org.kuali.coeus.sys.api.model.DocumentNumbered;
+import org.kuali.coeus.sys.api.model.ScaleTwoDecimal;
 
+import java.util.Date;
 import java.util.List;
 
-public interface BudgetContract extends AbstractBudgetContract {
-    
+public interface BudgetContract extends IdentifiableBudget, DocumentNumbered {
+
+    Integer getBudgetVersionNumber();
+
+    ScaleTwoDecimal getCostSharingAmount();
+
+    Date getEndDate();
+
+    Date getStartDate();
+
+    Boolean getFinalVersionFlag();
+
+    String getOhRateTypeCode();
+
+    ScaleTwoDecimal getResidualFunds();
+
+    ScaleTwoDecimal getTotalCost();
+
+    ScaleTwoDecimal getTotalDirectCost();
+
+    ScaleTwoDecimal getTotalIndirectCost();
+
+    ScaleTwoDecimal getTotalCostLimit();
+
+    ScaleTwoDecimal getTotalDirectCostLimit();
+
+    ScaleTwoDecimal getUnderrecoveryAmount();
+
+    String getComments();
+
+    Boolean getModularBudgetFlag();
+
+    String getOnOffCampusFlag();
+
+    Boolean getSubmitCostSharingFlag();
+
+    RateClassContract getUrRateClass();
+
+    RateClassContract getRateClass();
+
     String getBudgetJustification();
 
     String getBudgetAdjustmentDocumentNumber();
-    
-    RateClassContract getRateClass();
 
     List<? extends BudgetRateContract> getBudgetRates();
     
