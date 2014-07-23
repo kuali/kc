f41b937a4f41ac79ae705448166f2b86ac3ebb49
Joe Williams <jwillia@iastate.edu>
2014-07-22 21:09:16 -0600

    KRACOUES-7115:s2s form page

--- coeus-code/src/main/java/org/kuali/coeus/propdev/impl/s2s/ProposalDevelopmentS2SController.java
+++ coeus-code/src/main/java/org/kuali/coeus/propdev/impl/s2s/ProposalDevelopmentS2SController.java
@@ -15,16 +15,25 @@
  */
 package org.kuali.coeus.propdev.impl.s2s;
 
+
 import org.apache.commons.lang3.StringUtils;
 import org.kuali.coeus.propdev.impl.core.ProposalDevelopmentControllerBase;
 import org.kuali.coeus.propdev.impl.core.ProposalDevelopmentDocument;
 import org.kuali.coeus.propdev.impl.core.ProposalDevelopmentDocumentForm;
 import org.kuali.coeus.propdev.impl.s2s.connect.S2sCommunicationException;
+import org.kuali.coeus.s2sgen.api.print.FormPrintResult;
+import org.kuali.coeus.s2sgen.api.print.FormPrintService;
+import org.kuali.coeus.sys.api.model.KcFile;
 import org.kuali.coeus.sys.framework.gv.GlobalVariableService;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.KeyConstants;
 import org.kuali.coeus.propdev.impl.core.DevelopmentProposal;
+import org.kuali.rice.kns.util.AuditCluster;
+import org.kuali.rice.kns.util.AuditError;
+import org.kuali.rice.kns.util.KNSGlobalVariables;
 import org.kuali.rice.krad.data.DataObjectService;
+import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -33,16 +42,16 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 @Controller
 public class ProposalDevelopmentS2SController extends ProposalDevelopmentControllerBase {
+
+    private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(ProposalDevelopmentS2SController.class);
 
     @Autowired
     @Qualifier("s2sSubmissionService")
@@ -51,6 +60,12 @@ public class ProposalDevelopmentS2SController extends ProposalDevelopmentControl
     @Autowired
     @Qualifier("globalVariableService")
     private GlobalVariableService globalVariableService;
+
+    @Autowired
+    @Qualifier("formPrintService")
+    private FormPrintService formPrintService;
+
+    private static final String ERROR_NO_GRANTS_GOV_FORM_SELECTED = "error.proposalDevelopment.no.grants.gov.form.selected";
 
     @RequestMapping(value = "/proposalDevelopment", params={"methodToCall=refresh", "refreshCaller=S2sOpportunity-LookupView"})
    public ModelAndView refresh(@ModelAttribute("KualiForm") ProposalDevelopmentDocumentForm form, BindingResult result, HttpServletRequest request, HttpServletResponse response)
@@ -106,6 +121,7 @@ public class ProposalDevelopmentS2SController extends ProposalDevelopmentControl
            globalVariableService.getMessageMap().putError(Constants.NO_FIELD, ex.getErrorKey(),ex.getMessageWithParams());
            proposal.setS2sOpportunity(new S2sOpportunity());
        }
+       super.save(form,result,request,response);
        return getRefreshControllerService().refresh(form);
    }
    
@@ -122,6 +138,87 @@ public class ProposalDevelopmentS2SController extends ProposalDevelopmentControl
        return getRefreshControllerService().refresh(form);
    }
 
+    @RequestMapping(value = "/proposalDevelopment", params={"methodToCall=printForms"})
+        public ModelAndView printForms(@ModelAttribute("KualiForm") ProposalDevelopmentDocumentForm form, BindingResult result, HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        ProposalDevelopmentDocument proposalDevelopmentDocument = form.getProposalDevelopmentDocument();
+        boolean grantsGovErrorExists = false;
+
+        if(proposalDevelopmentDocument.getDevelopmentProposal().getSelectedS2sOppForms().isEmpty()){
+            getGlobalVariableService().getMessageMap().putError("noKey", ERROR_NO_GRANTS_GOV_FORM_SELECTED);
+            return getModelAndViewService().getModelAndView(form);
+        }
+        FormPrintResult formPrintResult = getFormPrintService().printForm(proposalDevelopmentDocument);
+        setValidationErrorMessage(formPrintResult.getErrors());
+        KcFile attachmentDataSource = formPrintResult.getFile();
+        if(attachmentDataSource==null || attachmentDataSource.getData()==null || attachmentDataSource.getData().length==0){
+            grantsGovErrorExists = copyAuditErrorsToPage(Constants.GRANTSGOV_ERRORS, "grantsGovFormValidationErrors");
+            if(grantsGovErrorExists){
+                getGlobalVariableService().getMessageMap().putError("grantsGovFormValidationErrors", KeyConstants.VALIDATTION_ERRORS_BEFORE_GRANTS_GOV_SUBMISSION);
+            }
+            return getModelAndViewService().getModelAndView(form);
+        }
+        if (proposalDevelopmentDocument.getDevelopmentProposal().getGrantsGovSelectFlag()) {
+            File grantsGovXmlDirectoryFile = getS2sSubmissionService().getGrantsGovSavedFile(proposalDevelopmentDocument);
+            byte[] bytes = new byte[(int) grantsGovXmlDirectoryFile.length()];
+            long size = bytes.length;
+            try (ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes)) {
+                KRADUtils.addAttachmentToResponse(response, inputStream, "binary/octet-stream", grantsGovXmlDirectoryFile.getName() + ".zip", size);
+                response.flushBuffer();
+            }
+            proposalDevelopmentDocument.getDevelopmentProposal().setGrantsGovSelectFlag(false);
+            return getModelAndViewService().getModelAndView(form);
+        }
+        streamToResponse(attachmentDataSource, response);
+        return getModelAndViewService().getModelAndView(form);
+    }
+
+    protected void setValidationErrorMessage(List<org.kuali.coeus.s2sgen.api.core.AuditError> errors) {
+        if (errors != null) {
+            LOG.info("Error list size:" + errors.size() + errors.toString());
+            List<org.kuali.rice.kns.util.AuditError> auditErrors = new ArrayList<>();
+            for (org.kuali.coeus.s2sgen.api.core.AuditError error : errors) {
+                auditErrors.add(new org.kuali.rice.kns.util.AuditError(error.getErrorKey(),
+                        Constants.GRANTS_GOV_GENERIC_ERROR_KEY, error.getLink(),
+                        new String[]{error.getMessageKey()}));
+            }
+            if (!auditErrors.isEmpty()) {
+                KNSGlobalVariables.getAuditErrorMap().put(
+                        "grantsGovAuditErrors",
+                        new AuditCluster(Constants.GRANTS_GOV_OPPORTUNITY_PANEL,
+                                auditErrors, Constants.GRANTSGOV_ERRORS)
+                );
+            }
+        }
+    }
+
+    protected void streamToResponse(KcFile attachmentDataSource, HttpServletResponse response) throws Exception {
+            byte[] data = attachmentDataSource.getData();
+            long size = data.length;
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(data)) {
+            KRADUtils.addAttachmentToResponse(response,inputStream,attachmentDataSource.getType(),attachmentDataSource.getName(),size);
+            response.flushBuffer();
+        }
+    }
+
+    protected boolean copyAuditErrorsToPage(String auditClusterCategory, String errorkey) {
+        boolean auditClusterFound = false;
+        Iterator<String> iter = KNSGlobalVariables.getAuditErrorMap().keySet().iterator();
+        while (iter.hasNext()) {
+            String errorKey = (String) iter.next();
+            AuditCluster auditCluster = (AuditCluster)KNSGlobalVariables.getAuditErrorMap().get(errorKey);
+            if(auditClusterCategory == null || StringUtils.equalsIgnoreCase(auditCluster.getCategory(), auditClusterCategory)){
+                auditClusterFound = true;
+                for (Object error : auditCluster.getAuditErrorList()) {
+                    AuditError auditError = (AuditError)error;
+                    getGlobalVariableService().getMessageMap().putError(errorKey == null ? auditError.getErrorKey() : errorKey,
+                            auditError.getMessageKey(), auditError.getParams());
+                }
+            }
+        }
+        return auditClusterFound;
+    }
+
     public S2sSubmissionService getS2sSubmissionService() {
         return s2sSubmissionService;
     }
@@ -136,5 +233,13 @@ public class ProposalDevelopmentS2SController extends ProposalDevelopmentControl
 
     public void setGlobalVariableService(GlobalVariableService globalVariableService) {
         this.globalVariableService = globalVariableService;
+    }
+
+    public FormPrintService getFormPrintService() {
+        return formPrintService;
+    }
+
+    public void setFormPrintService(FormPrintService formPrintService) {
+        this.formPrintService = formPrintService;
     }
 }
