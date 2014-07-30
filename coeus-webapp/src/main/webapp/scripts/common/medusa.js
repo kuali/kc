b5306c78eb90e9ac8101da0d1d55bcd9011b3029
Jay Hulslander <jdh34@cornell.edu>
2014-07-30 16:29:14 -0400

    KRACOEUS-7196 fixing javascript bug

--- coeus-webapp/src/main/webapp/scripts/common/medusa.js
+++ coeus-webapp/src/main/webapp/scripts/common/medusa.js
@@ -3,7 +3,8 @@ Kc.Medusa = Kc.Medusa || {};
 (function(namespace, $) {
 	namespace.minimizeMedusaTrees = function() {	
 		$('div.jstree li div.row').hide();
-		$('div.jstree li a').on('click', function()
+		$('div.jstree li a').off('click.medusa');
+		$('div.jstree li a').on('click.medusa', function()
 				{ $(this).parent().find('div.row').toggle(); return false;} );
 	}
 })(Kc.Medusa, jQuery);
