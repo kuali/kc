9cfc69430631a8add6b6745aa697657391f51f9f
blackcathacker <douglas.pace@gmail.com>
2014-08-04 16:10:22 -0400

    KRACOEUS-7714 : Fix personnel page in constants

--- coeus-code/src/main/java/org/kuali/coeus/propdev/impl/budget/core/ProposalBudgetConstants.java
+++ coeus-code/src/main/java/org/kuali/coeus/propdev/impl/budget/core/ProposalBudgetConstants.java
@@ -4,7 +4,7 @@ public class ProposalBudgetConstants {
 
 	public static class KradConstants {
 		public static final String BUDGET_DEFAULT_VIEW = "PropBudget-DefaultView";
-		public static final String PERSONNEL_PAGE_ID = "PropBudget-PersonnelPage";
+		public static final String PERSONNEL_PAGE_ID = "PropBudget-ProjectPersonnelPage";
 		public static final String PERIODS_AND_TOTALS_PAGE_ID = "PropBudget-PeriodsPage";
 	}
 }
