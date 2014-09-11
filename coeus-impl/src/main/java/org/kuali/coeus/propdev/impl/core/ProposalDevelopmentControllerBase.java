b1dc8a2fa0d12f0d11b1cccbf444544cf3db7c1f
Jamey Decker <jdecker@rsmart.com>
2014-09-11 12:12:32 -0700

    KRACOEUS-7472: Adding Super Users Actions tab.

--- coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/core/ProposalDevelopmentControllerBase.java
+++ coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/core/ProposalDevelopmentControllerBase.java
@@ -20,6 +20,7 @@ import org.kuali.coeus.common.framework.keyword.ScienceKeyword;
 import org.kuali.coeus.common.questionnaire.framework.answer.AnswerHeader;
 import org.kuali.coeus.common.framework.compliance.core.SaveDocumentSpecialReviewEvent;
 import org.kuali.coeus.propdev.impl.attachment.ProposalDevelopmentAttachmentService;
+import org.kuali.coeus.propdev.impl.datavalidation.ProposalDevelopmentDataValidationItem;
 import org.kuali.coeus.propdev.impl.docperm.ProposalDevelopmentPermissionsHelper;
 import org.kuali.coeus.propdev.impl.docperm.ProposalRoleTemplateService;
 import org.kuali.coeus.propdev.impl.keyword.PropScienceKeyword;
@@ -390,6 +391,24 @@ public abstract class ProposalDevelopmentControllerBase {
             }
             return null;
         }
+    }
+
+    public boolean proposalValidToRoute(ProposalDevelopmentDocumentForm form) {
+        boolean isValid = true;
+        form.setAuditActivated(true);
+        List<ProposalDevelopmentDataValidationItem> dataValidationItems = ((ProposalDevelopmentViewHelperServiceImpl)form.getViewHelperService())
+                .populateDataValidation(form,form.getView().getViewIndex());
+        if(dataValidationItems != null && dataValidationItems.size() > 0 ) {
+            for(ProposalDevelopmentDataValidationItem validationItem : dataValidationItems) {
+                if (StringUtils.equalsIgnoreCase(validationItem.getSeverity(), Constants.AUDIT_ERRORS)) {
+                    isValid = false;
+                    form.setDataValidationItems(dataValidationItems);
+                    break;
+                }
+            }
+        }
+        getGlobalVariableService().getMessageMap().clearErrorMessages();
+        return isValid;
     }
 
     protected ScienceKeyword getScienceKeyword(Object element) {
