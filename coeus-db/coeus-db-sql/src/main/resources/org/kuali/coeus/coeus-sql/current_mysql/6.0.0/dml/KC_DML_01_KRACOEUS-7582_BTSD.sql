6bdf5941dfab6bd789f41a7dfcfa77057f4cd67a
Travis Schneberger <leo.herbie@gmail.com>
2014-09-17 14:09:59 -0400

    KRACOEUS-7582: removing document level role

---
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/dml/KC_DML_01_KRACOEUS-7582_BTSD.sql
@@ -0,0 +1,6 @@
+DELIMITER /
+--This will not be needed in our final install scripts
+DELETE FROM KRIM_ROLE_T WHERE ROLE_NM = 'View Institutionally Maintained Salaries Document Level' AND NMSPC_CD = 'KC-PD'
+/
+
+DELIMITER ;
