731c337ffc2855781d1e59f604b4a8ece4a40c76
Jamey Decker <jamey@Jameys-MacBook-Pro.local>
2014-09-20 21:39:01 -0700

    KRACOEUS-7916: Creating a toggle to allow the attachment file name to be shown and updated.

---
+++ coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/attachment/ProposalDevelopmentAttachment.java
@@ -0,0 +1,10 @@
+package org.kuali.coeus.propdev.impl.attachment;
+
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ProposalDevelopmentAttachment {
+    public MultipartFile getMultipartFile();
+
+    public void setMultipartFile(MultipartFile multipartFile);
+}
