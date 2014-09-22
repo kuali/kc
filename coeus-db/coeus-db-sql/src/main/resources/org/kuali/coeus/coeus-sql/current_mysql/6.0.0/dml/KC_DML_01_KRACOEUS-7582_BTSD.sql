95ccc67142a3a57ed51df150ff92f9f3706c78a5
Travis Schneberger <leo.herbie@gmail.com>
2014-09-22 15:38:58 -0400

    KRACOEUS-7582: code review comments

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/dml/KC_DML_01_KRACOEUS-7582_BTSD.sql
+++
@@ -1,6 +0,0 @@
-DELIMITER /
---This will not be needed in our final install scripts
-DELETE FROM KRIM_ROLE_T WHERE ROLE_NM = 'View Institutionally Maintained Salaries Document Level' AND NMSPC_CD = 'KC-PD'
-/
-
-DELIMITER ;
