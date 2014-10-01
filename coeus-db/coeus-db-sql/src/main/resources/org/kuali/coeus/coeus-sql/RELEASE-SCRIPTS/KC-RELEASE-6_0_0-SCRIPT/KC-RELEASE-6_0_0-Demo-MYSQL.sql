df29e71e0b0934c5d0337f8746f884b49a80e9be
Geo Thomas <geot@mit.edu>
2014-10-01 15:26:45 -0400

    release script for 6.0 sprint 5

---
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/RELEASE-SCRIPTS/KC-RELEASE-6_0_0-SCRIPT/KC-RELEASE-6_0_0-Demo-MYSQL.sql
@@ -0,0 +1,6 @@
+\. ./../../current_mysql/6.0.0/dml/KC_DML_01_KRACOEUS-6956_BSTD.sql
+\. ./../../current_mysql/6.0.0/dml/KC_DML_01_KRACOEUS-7120_BTSD.sql
+\. ./../../current_mysql/6.0.0/dml/KC_DML_01_KRACOEUS-7356_0TSD.sql
+\. ./../../current_mysql/6.0.0/dml/KC_DML_01_KRACOEUS-7659_00SD.sql
+commit;
+exit
---
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/RELEASE-SCRIPTS/KC-RELEASE-6_0_0-SCRIPT/KR-RELEASE-6_0_0-Demo-MYSQL.sql
@@ -0,0 +1,4 @@
+\. ./../../current_mysql/6.0.0/dml/KR_DML_01_KRACOEUS-6294_BTSD.sql
+\. ./../../current_mysql/6.0.0/dml/KR_DML_02_KRACOEUS-6294_BTSD.sql
+commit;
+exit
