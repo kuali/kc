0318d50de48b5644f76e3b4f3f966f58a54df201
Travis Schneberger <leo.herbie@gmail.com>
2014-10-10 10:21:06 -0400

    KRACOEUS-6954: fix mysql script

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/dml/KR_DML_01_KRACOEUS-6954_B000.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/dml/KR_DML_01_KRACOEUS-6954_B000.sql
@@ -1,4 +1,8 @@
+DELIMITER /
+
 DELETE FROM KRCR_PARM_T WHERE NMSPC_CD = 'KC-PD' AND PARM_NM = 'pessimisticLocking.timeout'
 /
 UPDATE KRCR_PARM_T SET NMSPC_CD = 'KC-SYS', CMPNT_CD = 'All' WHERE NMSPC_CD = 'KC-PD' AND PARM_NM in ('pessimisticLocking.cronExpression', 'pessimisticLocking.expirationAge')
 /
+
+DELIMITER ;
