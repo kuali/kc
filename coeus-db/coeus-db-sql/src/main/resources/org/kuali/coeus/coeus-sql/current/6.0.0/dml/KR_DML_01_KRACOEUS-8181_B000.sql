e32a98d742d338118b0397db51d3375cc64320f3
Travis Schneberger <leo.herbie@gmail.com>
2014-10-30 14:36:39 -0400

    KRACOEUS-8181: code review comments

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/dml/KR_DML_01_KRACOEUS-8181_B000.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/dml/KR_DML_01_KRACOEUS-8181_B000.sql
@@ -1,3 +1,3 @@
 INSERT INTO KRCR_PARM_T(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
-VALUES ('KC-GEN','All','ALLOW_CLEAR_PESSIMISTIC_LOCK',SYS_GUID(),1,'CONFG','Y','Allows pessimistic locks to be cleared conveniently when a message is displayed. Do not use in production. (Y|N)','A','KC')
+VALUES ('KC-GEN','All','ALLOW_CLEAR_PESSIMISTIC_LOCK',SYS_GUID(),1,'CONFG','N','Allows pessimistic locks to be cleared conveniently when a message is displayed. Do not use in production. (Y|N)','A','KC')
 /
