90405ee9c0d0faaeb7a25c80a2e1ca280cc1dbd5
anumole <anumole@polus177.polusindia.com>
2014-11-28 15:00:42 +0530

    RESKC-1442: Provide option to number Inst Prop based on FY and FM

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1612_008__fy_based_ip.sql
@@ -0,0 +1,24 @@
+--
+-- Kuali Coeus, a comprehensive research administration system for higher education.
+--
+-- Copyright 2005-2016 Kuali, Inc.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+CREATE SEQUENCE SEQ_IP_FY_BASED_ID INCREMENT BY 1 START WITH 1 MAXVALUE 9999 NOCACHE;
+
+insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values ('KC-PD','Document','GENERATE_IP_BASED_ON_FY',sys_guid(),1,'CONFG','N','On enabling this parameter,IP Number will be generated based on the fiscal year.','A','KUALI');
+
+
