dcd37f0e1b1fb01e5caff8f7cb33c3d49d88317c
jamey-rsmart <jdecker@navmp.com>
2014-12-05 15:46:20 -0700

    KRACOEUS-8452: Added missing IP properties in Medusa.

---
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/framework/model/KcYNBooleanEditor.java
@@ -0,0 +1,21 @@
+package org.kuali.coeus.sys.framework.model;
+
+import org.kuali.rice.krad.web.bind.UifBooleanEditor;
+
+public class KcYNBooleanEditor extends UifBooleanEditor {
+    @Override
+    public String getAsText() {
+        if(this.getValue() == null) {
+            return "";
+        }
+        if (this.getValue() instanceof String){
+            setAsText(this.getValue().toString());
+        }
+        if(((Boolean)this.getValue()).booleanValue()) {
+            return "Yes";
+        }
+        else {
+            return "No";
+        }
+    }
+}
