335035a65f1ede2dfa0008016b3fc3d7d9f31d4c
Travis Schneeberger <leo.herbie@gmail.com>
2014-12-11 17:52:44 -0500

    Update KC_DML_01_KRACOEUS-8454_B000.sql

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/dml/KC_DML_01_KRACOEUS-8454_B000.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/dml/KC_DML_01_KRACOEUS-8454_B000.sql
@@ -1,4 +1,3 @@
-
 update NOTIFICATION_TYPE_RECIPIENT t1 set t1.ROLE_NAME = ( 
     select t2.NMSPC_CD || ':'|| t2.ROLE_NM from KRIM_ROLE_T t2 where t2.NMSPC_CD='KC-PD' and upper(t2.ROLE_NM) = upper('Aggregator Document Level')
   )
@@ -7,4 +6,4 @@ where t1.NOTIFICATION_TYPE_ID = (
     (select t4.MODULE_CODE from COEUS_MODULE t4 where upper( t4.DESCRIPTION )= upper('Development Proposal'))
     and t3.ACTION_CODE='103'
  )
- /
+/
