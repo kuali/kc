4f7b75de6070b5af8222bcf5e5f2bb0ca8e04f41
bhutchinson <bryan.hutchinson@cornell.edu>
2014-12-15 10:57:09 -0500

    KFSTI-672 apply Java/resource/web changes

--- coeus-impl/src/main/java/org/kuali/coeus/common/framework/sponsor/Sponsor.java
+++ coeus-impl/src/main/java/org/kuali/coeus/common/framework/sponsor/Sponsor.java
@@ -19,6 +19,7 @@ import org.kuali.coeus.common.api.sponsor.SponsorContract;
 import org.kuali.coeus.common.framework.rolodex.Rolodex;
 import org.kuali.coeus.common.framework.unit.Unit;
 import org.kuali.coeus.sys.framework.model.KcPersistableBusinessObjectBase;
+import org.kuali.kra.external.dunningcampaign.DunningCampaign;
 import org.kuali.rice.krad.data.jpa.PortableSequenceGenerator;
 import org.kuali.rice.krad.data.jpa.converters.BooleanYNConverter;
 
@@ -78,6 +79,20 @@ public class Sponsor extends KcPersistableBusinessObjectBase implements SponsorC
 
     @Column(name = "CREATE_USER")
     private String createUser;
+
+    @Column(name = "DUNNING_CAMPAIGN_ID")
+    private String dunningCampaignId;
+
+    @OneToOne(mappedBy = "dunningCampaign", orphanRemoval = true, cascade = { CascadeType.ALL })
+    @JoinColumn(name = "DUNNING_CAMPAIGN_ID", referencedColumnName = "DUNNING_CAMPAIGN_ID", insertable = false, updatable = false)
+    private DunningCampaign dunningCampaign;
+
+    @Column(name = "CUSTOMER_NUMBER")
+    private String customerNumber;
+
+    private String customerExists;
+
+    private String customerTypeCode;
 
     @Column(name = "ACTV_IND")
     @Convert(converter = BooleanYNConverter.class)
@@ -282,5 +297,45 @@ public class Sponsor extends KcPersistableBusinessObjectBase implements SponsorC
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    public String getDunningCampaignId() {
+        return dunningCampaignId;
+    }
+
+    public void setDunningCampaignId(String dunningCampaignId) {
+        this.dunningCampaignId = dunningCampaignId;
+    }
+
+    public DunningCampaign getDunningCampaign() {
+        return dunningCampaign;
+    }
+
+    public void setDunningCampaign(DunningCampaign dunningCampaign) {
+        this.dunningCampaign = dunningCampaign;
+    }
+
+    public String getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getCustomerExists() {
+        return customerExists;
+    }
+
+    public void setCustomerExists(String customerExists) {
+        this.customerExists = customerExists;
+    }
+
+    public String getCustomerTypeCode() {
+        return customerTypeCode;
+    }
+
+    public void setCustomerTypeCode(String customerTypeCode) {
+        this.customerTypeCode = customerTypeCode;
     }
 }
