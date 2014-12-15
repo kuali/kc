62fa5af958f3dc676eb2accd716aec6351778cdc
bhutchinson <bryan.hutchinson@cornell.edu>
2014-12-15 11:09:56 -0500

    KFSTI-673 add new db files

---
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/dml/KR_DML_01_KFSMI-11535_B000.sql
@@ -0,0 +1,7 @@
+DELIMITER /
+
+INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
+VALUES ('KC-GEN', 'All', 'STATE_SPONSOR_TYPE', UUID(), 1, 'CONFG', '1', 'Specifies the Sponsor Type Code that designates a state agency.', 'A', 'KC')
+/
+
+DELIMITER ;
