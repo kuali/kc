e7600d666b96a42b039365c168fabce5e76b2aa4
kkronenb <kevin.kronenbitter@colostate.edu>
2014-12-16 14:20:40 -0700

    KFSTI-699 Change hardcoded valuesfinder to an enum

--- coeus-impl/src/main/java/org/kuali/kra/external/sponsor/SponsorMaintainableImpl.java
+++ coeus-impl/src/main/java/org/kuali/kra/external/sponsor/SponsorMaintainableImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.commons.lang3.StringUtils;
 import org.kuali.coeus.common.framework.sponsor.Sponsor;
 import org.kuali.coeus.sys.framework.service.KcServiceLocator;
+import org.kuali.kra.external.customercreation.CustomerConstants;
 import org.kuali.kra.external.customercreation.CustomerCreationClient;
 import org.kuali.rice.kew.api.WorkflowDocument;
 import org.kuali.rice.kim.api.identity.Person;
@@ -20,8 +21,7 @@ public class SponsorMaintainableImpl extends org.kuali.coeus.common.impl.sponsor
         WorkflowDocument workflowDocument = documentHeader.getWorkflowDocument();
         Sponsor sponsor = (Sponsor) getDataObject();
         sponsor.refreshReferenceObject("rolodex");
-        if (workflowDocument.isProcessed()
-        		&& StringUtils.equals(sponsor.getCustomerExists(), "N")) {
+        if (workflowDocument.isProcessed() && StringUtils.equals(sponsor.getCustomerExists(), CustomerConstants.CustomerOptions.Types.NEW.getCode())) {
         	Person initiator = this.getPersonService().getPerson(workflowDocument.getInitiatorPrincipalId());
         	List<String> errors = getCustomerCreationClient().createCustomer((Sponsor) getDataObject(), initiator.getPrincipalName());
         	if (errors != null && !errors.isEmpty()) {
