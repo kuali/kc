37bbdfd03bd70dcd732f4084136ea8cbd8195bd1
Joe Williams <jwillia@iastate.edu>
2014-12-18 12:58:54 -0600

    KRACOEUS-8573:fix to display person names on personnel attachment in parent proposals

--- coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/core/ProposalDevelopmentViewHelperServiceImpl.java
+++ coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/core/ProposalDevelopmentViewHelperServiceImpl.java
@@ -73,6 +73,7 @@ import org.kuali.coeus.propdev.impl.specialreview.ProposalSpecialReview;
 import org.kuali.coeus.propdev.impl.attachment.LegacyNarrativeService;
 import org.kuali.coeus.propdev.impl.docperm.ProposalUserRoles;
 import org.kuali.rice.core.api.datetime.DateTimeService;
+import org.kuali.rice.kim.api.identity.Person;
 import org.kuali.rice.kim.api.identity.PersonService;
 import org.kuali.rice.krad.util.*;
 import org.kuali.rice.krad.bo.Note;
@@ -984,5 +985,12 @@ public class ProposalDevelopmentViewHelperServiceImpl extends KcViewHelperServic
         String openTab = form.getDefaultOpenTab();
         form.setDefaultOpenTab("");
         return openTab;
+    }
+
+    public String getPropPersonName(String personId) {
+        Person person= getPersonService().getPerson(personId);
+        final String middleName = person.getMiddleName() != null ? person.getMiddleName() + " " : "";
+
+        return (person.getFirstName() + " " + middleName + person.getLastName()).trim();
     }
 }
