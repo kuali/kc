d5174dae40c9e7aefb2d6b777871a156cdc444c9
sasipolus <sasi.kumar@polussoftware.com>
2014-12-23 17:06:25 +0530

    KRACOEUS-8436 PD UXI: S2S Form Print - Select All selects all Available forms, not all Included forms

--- coeus-webapp/src/main/webapp/scripts/propdev/s2s.js
+++ coeus-webapp/src/main/webapp/scripts/propdev/s2s.js
@@ -10,4 +10,26 @@ Kc.PropDev.s2s = Kc.PropDev.s2s || {};
             $("input[name='document.developmentProposal.s2sOpportunity.revisionOtherDescription']").val("");
         }
     };
+    namespace.selectAllIncludedForms = function(document) {
+    	var count = 0;
+        var include = true;
+        $.each(document, function(index, element) {
+            if(element.type == 'checkbox') {
+    			if (element.name == 'document.developmentProposal.s2sOpportunity.s2sOppForms['+count+'].include') {
+    				include = element.checked;
+                }
+                if (element.name == 'document.developmentProposal.s2sOpportunity.s2sOppForms['+count+'].selectToPrint') {
+                    if(element.disabled == true) {
+                    	element.checked = false;
+                    } else if(include == false) {
+                    	element.checked = false;
+                    } else {
+                    	element.checked = true;
+                    }
+                    include = true;
+                    count++;
+                }
+            }
+    	});
+    };
 })(Kc.PropDev.s2s, jQuery);
