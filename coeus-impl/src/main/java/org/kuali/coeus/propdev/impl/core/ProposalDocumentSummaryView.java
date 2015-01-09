d9db48405f067fb7a7211aa302f2948710146ab8
bsmith83 <kubsmith@ucdavis.edu>
2015-01-08 17:23:21 -0800

    [KRACOEUS-8695] Fix for proposal summary view

---
+++ coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/core/ProposalDocumentSummaryView.java
@@ -0,0 +1,46 @@
+package org.kuali.coeus.propdev.impl.core;
+
+import org.apache.commons.lang.StringUtils;
+import org.kuali.rice.kew.api.KewApiServiceLocator;
+import org.kuali.rice.kew.api.doctype.DocumentType;
+import org.kuali.rice.krad.datadictionary.DocumentEntry;
+import org.kuali.rice.krad.document.DocumentRequestAuthorizationCache;
+import org.kuali.rice.krad.document.DocumentViewAuthorizerBase;
+import org.kuali.rice.krad.document.DocumentViewPresentationControllerBase;
+import org.kuali.rice.krad.service.KRADServiceLocatorWeb;
+import org.kuali.rice.krad.uif.UifConstants;
+import org.kuali.rice.krad.uif.view.FormView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * View for a ProposalDocument that is used as a summary view on the action list, used to workaround one
+ * view per document type limitation.
+ */
+public class ProposalDocumentSummaryView extends ProposalDocumentView{
+
+    public ProposalDocumentSummaryView () {
+   		super();
+
+        setRequestAuthorizationCacheClass(DocumentRequestAuthorizationCache.class);
+   	}
+
+    @Override
+    public void performInitialization(Object model) {
+        super.performInitialization(model);
+
+        setObjectPathToConcreteClassMapping(new HashMap<String, Class<?>>());
+        getObjectPathToConcreteClassMapping().put("document", ProposalDevelopmentDocument.class);
+
+        // Workaround for non-set additionalScriptFiles files
+        setAdditionalScriptFiles(new ArrayList<String>());
+    }
+
+    protected DocumentEntry getDocumentEntryForView() {
+        DocumentEntry documentEntry = KRADServiceLocatorWeb.getDocumentDictionaryService().getDocumentEntryByClass(
+                ProposalDevelopmentDocument.class);
+
+        return documentEntry;
+    }
+}
