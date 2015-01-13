067584b67db87c614fe801c05f2f177f73e87836
blackcathacker <douglas.pace@gmail.com>
2015-01-13 14:23:51 -0800

    KRACOEUS-8724 : Mysql sql fixes

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/tables/KC_TBL_EPS_PROPOSAL_BUDGET_EXT.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/tables/KC_TBL_EPS_PROPOSAL_BUDGET_EXT.sql
@@ -31,5 +31,6 @@ update EPS_PROPOSAL_BUDGET_EXT prop_budget set STATUS_CODE = '2' where STATUS_CO
 /
 
 alter table EPS_PROPOSAL_BUDGET_EXT modify column STATUS_CODE char(1) not null
+/
 
 DELIMITER ;
