2b72d4ff6bcbb02a60547849f77408b76737c428
Travis Schneberger <leo.herbie@gmail.com>
2015-01-22 22:27:50 -0500

    KRACOEUS-8733: adding oracle jar to oracle profile, creating a verifying to ensure if on the oracle platform the jar is present.

---
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/impl/KcConfigVerifier.java
@@ -0,0 +1,77 @@
+package org.kuali.coeus.sys.impl;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.kuali.rice.core.api.config.property.ConfigurationService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("kcConfigVerifier")
+public class KcConfigVerifier implements InitializingBean {
+
+    private static final Log LOG = LogFactory.getLog(KcConfigVerifier.class);
+
+    private static final String ORACLE_PLATFORM_NM = "Oracle";
+    private static final String ORACLE_CLASS_NAME = "org.eclipse.persistence.platform.database.oracle.Oracle11Platform";
+    private static final String KC_CONFIG_VERIFIER_HARD_ERROR_CFG_NM = "kc.config.verifier.hard.error";
+    private static final String SERVER_DATASOURCE_PLATFORM_CFG_NM = "server.datasource.platform";
+    private static final String DATASOURCE_PLATFORM_CFG_NM = "datasource.platform";
+    private static final String MISSING_LIB_MESSAGE_FOR_ORACLE = "Oracle platform detected but org.eclipse.persistence:org.eclipse.persistence.oracle:jar is not found on the classpath";
+    private static final String INCLUDED_LIB_MESSAGE_FOR_NON_ORACLE = "Non-Oracle platform detected but org.eclipse.persistence:org.eclipse.persistence.oracle:jar is found on the classpath";
+
+
+    @Autowired
+    @Qualifier("kualiConfigurationService")
+    private ConfigurationService configurationService;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        verifyOracleConfiguration();
+    }
+
+    protected void verifyOracleConfiguration() {
+        if (oraclePlatform() && !oracleJpaLibraryAvailable()) {
+            if (hardError()) {
+                throw new RuntimeException(MISSING_LIB_MESSAGE_FOR_ORACLE);
+            } else {
+                LOG.fatal(MISSING_LIB_MESSAGE_FOR_ORACLE);
+            }
+        } else if (!oraclePlatform() && oracleJpaLibraryAvailable()) {
+            LOG.warn(INCLUDED_LIB_MESSAGE_FOR_NON_ORACLE);
+        }
+    }
+
+    protected boolean oracleJpaLibraryAvailable() {
+        try {
+            Class.forName(ORACLE_CLASS_NAME);
+            return true;
+        } catch (ClassNotFoundException e) {
+           return false;
+        } catch (NoClassDefFoundError e) {
+            //class is available but it cannot be initialized because of missing oracle drivers
+            return true;
+        }
+
+    }
+
+    protected boolean oraclePlatform() {
+        final String serverPlatform = configurationService.getPropertyValueAsString(SERVER_DATASOURCE_PLATFORM_CFG_NM);
+        final String clientPlatform = configurationService.getPropertyValueAsString(DATASOURCE_PLATFORM_CFG_NM);
+
+        return (serverPlatform != null && serverPlatform.contains(ORACLE_PLATFORM_NM)) || (clientPlatform != null && clientPlatform.contains(ORACLE_PLATFORM_NM));
+    }
+
+    protected boolean hardError() {
+        return configurationService.getPropertyValueAsBoolean(KC_CONFIG_VERIFIER_HARD_ERROR_CFG_NM);
+    }
+
+    public ConfigurationService getConfigurationService() {
+        return configurationService;
+    }
+
+    public void setConfigurationService(ConfigurationService configurationService) {
+        this.configurationService = configurationService;
+    }
+}
