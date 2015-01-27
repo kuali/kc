3cac0efc0722ba58e38e204aea64886973254035
Travis Schneberger <leo.herbie@gmail.com>
2015-01-27 13:13:58 -0500

    KRACOEUS-8747: changing description of param

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/dml/KR_DML_01_KRACOEUS-8181_B000.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current_mysql/6.0.0/dml/KR_DML_01_KRACOEUS-8181_B000.sql
@@ -1,5 +1,5 @@
 DELIMITER /
 INSERT INTO KRCR_PARM_T(NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID)
-VALUES ('KC-GEN','All','ALLOW_CLEAR_PESSIMISTIC_LOCK',UUID(),1,'CONFG','N','Allows pessimistic locks to be cleared conveniently when a message is displayed. Do not use in production. (Y|N)','A','KC')
+VALUES ('KC-GEN','All','ALLOW_CLEAR_PESSIMISTIC_LOCK',UUID(),1,'CONFG','N','Possible values are N or Y. When set to Y, allows pessimistic locks to be cleared conveniently when a message is displayed. Do not use in production. This parameter should be set as Y for testing purposes only.','A','KC')
 /
 DELIMITER ;
