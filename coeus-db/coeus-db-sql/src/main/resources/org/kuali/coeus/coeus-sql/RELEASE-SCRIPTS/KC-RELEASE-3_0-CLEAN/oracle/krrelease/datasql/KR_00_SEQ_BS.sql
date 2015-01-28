26c6fe5beb5ccf742121dc10f52444b732392ba6
aakers <allen.akers@colostate.edu>
2015-01-28 11:11:30 -0700

    Moving responsibility-related sequences to the bottom to minimize broken syntax impact on later scripts

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-CLEAN/oracle/krrelease/datasql/KR_00_SEQ_BS.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/RELEASE-SCRIPTS/KC-RELEASE-3_0-CLEAN/oracle/krrelease/datasql/KR_00_SEQ_BS.sql
@@ -5,6 +5,10 @@ spool KR_SEQ_BS-Oracle-Install.log
 create or replace function numeric_val (p_string in varchar2) 
 return NUMBER as l_number number;
 begin
+	if p_string is null
+	then
+		return 0;
+	end if;
     return to_number(REGEXP_REPLACE(p_string, '[^0-9]+', ''));
 end;
 /
@@ -39,22 +43,10 @@ begin
     select max(numeric_val(ROLE_MBR_ID)) + 1 into l_new_seq from KRIM_ROLE_MBR_T where numeric_val(ROLE_MBR_ID) < 10000;
     execute immediate 'CREATE SEQUENCE KRIM_ROLE_MBR_ID_BS_S START WITH ' || l_new_seq || 
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
-        
-    select max(numeric_val(RSP_ID)) + 1 into l_new_seq from KRIM_RSP_T where numeric_val(RSP_ID) < 10000;
-    execute immediate 'CREATE SEQUENCE KRIM_RSP_ID_BS_S START WITH ' || l_new_seq || 
-        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
 	select max(numeric_val(ROLE_PERM_ID)) + 1 into l_new_seq from KRIM_ROLE_PERM_T where numeric_val(ROLE_PERM_ID) < 10000;
 	execute immediate 'CREATE SEQUENCE KRIM_ROLE_PERM_ID_BS_S START WITH ' || l_new_seq || 
 		' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
-    
-    select max(numeric_val(ROLE_RSP_ID)) + 1 into l_new_seq from KRIM_ROLE_RSP_T where numeric_val(ROLE_RSP_ID) < 10000;
-    execute immediate 'CREATE SEQUENCE KRIM_ROLE_RSP_ID_BS_S START WITH ' || l_new_seq || 
-        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
-        
-    select max(numeric_val(ROLE_RSP_ACTN_ID)) + 1 into l_new_seq from KRIM_ROLE_RSP_ACTN_T where numeric_val(ROLE_RSP_ACTN_ID) < 10000;
-    execute immediate 'CREATE SEQUENCE KRIM_ROLE_RSP_ACTN_ID_BS_S START WITH ' || l_new_seq || 
-        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 
 	select max(numeric_val(ID)) + 1 into l_new_seq from 
 		((select max(numeric_val(ATTR_DATA_ID)) as ID from KRIM_RSP_ATTR_DATA_T where numeric_val(ATTR_DATA_ID) < 10000)
@@ -70,6 +62,18 @@ begin
         
     select max(numeric_val(KIM_TYP_ATTR_ID)) + 1 into l_new_seq from KRIM_TYP_ATTR_T where numeric_val(KIM_TYP_ATTR_ID) < 10000;
     execute immediate 'CREATE SEQUENCE KRIM_TYP_ATTR_ID_BS_S START WITH ' || l_new_seq || 
+        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
+        
+    select max(numeric_val(RSP_ID)) + 1 into l_new_seq from KRIM_RSP_T where numeric_val(RSP_ID) < 10000;
+    execute immediate 'CREATE SEQUENCE KRIM_RSP_ID_BS_S START WITH ' || l_new_seq || 
+        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
+        
+    select max(numeric_val(ROLE_RSP_ID)) + 1 into l_new_seq from KRIM_ROLE_RSP_T where numeric_val(ROLE_RSP_ID) < 10000;
+    execute immediate 'CREATE SEQUENCE KRIM_ROLE_RSP_ID_BS_S START WITH ' || l_new_seq || 
+        ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
+        
+    select max(numeric_val(ROLE_RSP_ACTN_ID)) + 1 into l_new_seq from KRIM_ROLE_RSP_ACTN_T where numeric_val(ROLE_RSP_ACTN_ID) < 10000;
+    execute immediate 'CREATE SEQUENCE KRIM_ROLE_RSP_ACTN_ID_BS_S START WITH ' || l_new_seq || 
         ' MAXVALUE 99999999 MINVALUE 1 NOCYCLE NOCACHE NOORDER';
 end;
 /
