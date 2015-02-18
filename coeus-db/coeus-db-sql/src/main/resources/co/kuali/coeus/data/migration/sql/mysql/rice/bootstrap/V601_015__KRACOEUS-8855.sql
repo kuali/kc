a9aa1c154a1c26ddefd29c47a5656dde06922025
blackcathacker <douglas.pace@gmail.com>
2015-02-17 16:09:27 -0800

    KRACOEUS-8855:Make PD KEW doc standalone and repackage KEW related class The PD KEW File that was being used by CX was apparently dependent on the previous foundation KEW file being ingested. I have copied relevant elements from it such that this file can be ingested into an empty database.

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/mysql/rice/bootstrap/V601_015__KRACOEUS-8855.sql
@@ -0,0 +1,1 @@
+update KREW_RULE_ATTR_T set cls_nm = 'org.kuali.coeus.sys.impl.workflow.CopyCustomActionListAttribute' where cls_nm = 'org.kuali.kra.workflow.CopyCustomActionListAttribute';
--- coeus-impl/src/main/java/org/kuali/coeus/sys/impl/workflow/CopyCustomActionListAttribute.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/impl/workflow/CopyCustomActionListAttribute.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.kuali.kra.workflow;
+package org.kuali.coeus.sys.impl.workflow;
 
 import org.kuali.rice.kew.actionlist.CustomActionListAttribute;
 import org.kuali.rice.kew.api.action.ActionItem;
