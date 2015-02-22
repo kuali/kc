cf65617ece8b605feba4a9fe92e7bf31d7ef3728
blackcathacker <douglas.pace@gmail.com>
2015-02-22 15:34:14 -0800

    KRACOEUS-8891:Health Check Page Simple health check page that return 200 on success and 417 on failure. Does simple query against the KC and Rice database to confirm they are accessible

--- coeus-impl/src/main/java/org/kuali/coeus/sys/framework/config/KcConfigurer.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/framework/config/KcConfigurer.java
@@ -19,6 +19,9 @@
 package org.kuali.coeus.sys.framework.config;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.kuali.rice.core.api.config.ConfigurationException;
 import org.kuali.rice.core.api.config.module.RunMode;
 import org.kuali.rice.core.api.config.property.ConfigContext;
 import org.kuali.rice.core.api.resourceloader.ResourceLoader;
@@ -31,10 +34,13 @@ import org.springframework.web.servlet.DispatcherServlet;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
+
 import java.util.Collections;
 import java.util.List;
 
 public class KcConfigurer extends ModuleConfigurer {
+	
+	protected final Log LOG = LogFactory.getLog(KcConfigurer.class);
     
     private String bootstrapSpringFile;
     private String dispatchServletName;
@@ -61,6 +67,17 @@ public class KcConfigurer extends ModuleConfigurer {
     public List<String> getAdditionalSpringFiles() {
         final String files = ConfigContext.getCurrentContextConfig().getProperty("kc." + getModuleName() + ".additionalSpringFiles");
         return files == null ? Collections.<String>emptyList() : parseFileList(files);
+    }
+    
+    @Override
+    public RunMode getRunMode() {
+    	try {
+    		return super.getRunMode();
+    	} catch (ConfigurationException e) {
+    		//for KC modules assume LOCAL if not provided since thats the only valid run mode anyway
+    		LOG.info("Assuming LOCAL mode for " + getModuleName() + " as one wasn't provided.", e);
+    		return RunMode.LOCAL; 
+    	}
     }
     
     @Override
