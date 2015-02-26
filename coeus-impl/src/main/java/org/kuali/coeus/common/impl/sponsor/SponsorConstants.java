aa7e7969c3b76b78a0c1f91e18139028512b0320
Travis Schneberger <leo.herbie@gmail.com>
2015-02-26 08:26:31 -0500

    KRACOEUS-8909: make sponsor maintenance work without KFS integration

---
+++ coeus-impl/src/main/java/org/kuali/coeus/common/impl/sponsor/SponsorConstants.java
@@ -0,0 +1,10 @@
+package org.kuali.coeus.common.impl.sponsor;
+
+public final class SponsorConstants {
+
+    public static final String FIN_SYSTEM_INTEGRATION_ON_SPONSOR = "FIN_SYSTEM_INTEGRATION_ON_SPONSOR";
+
+    private SponsorConstants() {
+        throw new UnsupportedOperationException("do not call");
+    }
+}
