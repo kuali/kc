b448141bf75a59eafbec9841f78423dc6a5b2519
Joe Williams <jwillia@iastate.edu>
2015-05-26 10:07:24 -0500

    Adds Parameters For Comment Type Codes

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/mysql/rice/bootstrap/V1506_001__ParametersForCommentTypes.sql
@@ -0,0 +1,39 @@
+--
+-- Kuali Coeus, a comprehensive research administration system for higher education.
+--
+-- Copyright 2005-2015 Kuali, Inc.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','PROPOSAL_IP_REVIEW_COMMENT_CODE',UUID(),1,'CONFG','16','Comment type code for IP review comments','A','KC');
+
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','PROPOSAL_SUMMARY_COMMENT_CODE',UUID(),1,'CONFG','12','Comment type code for Proposal Summary','A','KC');
+
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','SPECIAL_RATE_COMMENT_CODE',UUID(),1,'CONFG','7','Comment type code for Special rate comments in awards','A','KC');
+
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','SPECIAL_REVIEW_COMMENT_CODE',UUID(),1,'CONFG','10','Comment type code for Special review comments','A','KC');
+
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','FISCAL_REPORT_COMMENT_CODE',UUID(),1,'CONFG','3','Code of value Fiscal Reports Comment','A','KC');
+
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','INDIRECT_COST_COMMENT_CODE',UUID(),1,'CONFG','8','Comment type code for Indirect cost Comments','A','KC');
+
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','INVOICE_INSTRUCTION_COMMENT_CODE',UUID(),1,'CONFG','1','Comment type code for Invoice instructions','A','KC');
