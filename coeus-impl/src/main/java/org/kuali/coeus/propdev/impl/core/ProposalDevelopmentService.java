6232384dccb99a8a2d7a193eaed3ec46acff4930
Travis Schneberger <leo.herbie@gmail.com>
2015-06-02 14:27:54 -0400

    fix delete proposal, code cleanup, remove unused code

--- coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/core/ProposalDevelopmentService.java
+++ coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/core/ProposalDevelopmentService.java
@@ -27,64 +27,40 @@ import java.util.List;
 
 public interface ProposalDevelopmentService {
     
-    public void initializeUnitOrganizationLocation(ProposalDevelopmentDocument proposalDevelopmentDocument);
+    void initializeUnitOrganizationLocation(ProposalDevelopmentDocument proposalDevelopmentDocument);
     
     /**
      * This method initializes the siteNumber property on proposal sites that don't have it set yet.
-     * @param proposalDevelopmentDocument
      */
-    public void initializeProposalSiteNumbers(ProposalDevelopmentDocument proposalDevelopmentDocument);
+    void initializeProposalSiteNumbers(ProposalDevelopmentDocument proposalDevelopmentDocument);
     
    /**
      * This method returns a Map of Units for which the user represented by the userId passed in has the role Proposal Aggregator
      * @param userId unique identifer representing the user whose units will be returned
      * @return A Map in the form of Unit Number, Unit Name representing the units for which the userId passed in has the Proposal Aggregator role.
      */
-    public List<Unit> getDefaultModifyProposalUnitsForUser(String userId);
+    List<Unit> getDefaultModifyProposalUnitsForUser(String userId);
     
-    public String populateBudgetEditableFieldMetaDataForAjaxCall(String proposalNumber, String documentNumber, String editableFieldDBColumn);
+    Object getBudgetFieldValueFromDBColumnName(String documentNumber, String dbColumnName);
 
-    public Object getBudgetFieldValueFromDBColumnName(String documentNumber, String dbColumnName);
-    
-    public String getDataOverrideLookupDisplayReturnValue( String lookupClassName );
-
-    
     /**
-     * For the proposal, based on the hierarchy status and sponsor code is grants gov enabled 
-     * @param proposal
-     * @return
+     * For the proposal, based on the hierarchy status and sponsor code is grants gov enabled
      */
-    public boolean isGrantsGovEnabledForProposal(DevelopmentProposal proposal);
+    boolean isGrantsGovEnabledForProposal(DevelopmentProposal proposal);
     /**
      *
      * Deletes the proposal and any budgets associated with this document and then cancels document
-     * @param proposalDocument
-     * @throws WorkflowException
      */
-    public ProposalDevelopmentDocument deleteProposal(ProposalDevelopmentDocument proposalDocument) throws WorkflowException;
+    ProposalDevelopmentDocument deleteProposal(ProposalDevelopmentDocument proposalDocument) throws WorkflowException;
     
-    public Budget getFinalBudget (DevelopmentProposal proposal);
+    Budget getFinalBudget (DevelopmentProposal proposal);
     
+    InstitutionalProposal getInstitutionalProposal(String devProposalNumber);
 
-
-    public InstitutionalProposal getInstitutionalProposal(String devProposalNumber);
-    
-
-    public static final String PROPOSAL_NARRATIVE_TYPE_GROUP = "proposalNarrativeTypeGroup";
-    public static final String DELIVERY_INFO_DISPLAY_INDICATOR = "deliveryInfoDisplayIndicator";
-    public static final String PROPOSAL_SUMMARY_INDICATOR = "enableProposalSummaryPanel";
-    public static final String BUDGET_SUMMARY_INDICATOR = "enableBudgetSummaryPanel";
-    public static final String KEY_PERSONNEL_INDICATOR = "enableKeyPersonnelPanel";
-    public static final String SPECIAL_REVIEW_INDICATOR = "enableSpecialReviewPanel";
-    public static final String SUMMARY_PRINT_FORMS_INDICATOR = "enableSummaryPrintPanel";
-    public static final String CUSTOM_DATA_INFO_INDICATOR = "enableCustomDataInfoPanel";
-    public static final String SUMMARY_QUESTIONS_INDICATOR = "enableSummaryQuestionsPanel";
-    public static final String SUMMARY_ATTACHMENTS_INDICATOR = "enableSummaryAttachmentsPanel";
-    public static final String SUMMARY_KEYWORDS_INDICATOR = "enableSummaryKeywordsPanel";
-    public static final String PROPOSAL_SUMMARY_DISCLAIMER_INDICATOR = "propSummaryDisclaimerText";
-
-    public static final String SUMMARY_SPECIAL_REVIEW_LIST = "proposal.summary.validSpecialReviewCodes";
-    public static final String PROPOSAL_NARRATIVE_TYPE_GROUP2 = "proposalNarrativeTypeGroup";
+    String BUDGET_SUMMARY_INDICATOR = "enableBudgetSummaryPanel";
+    String SUMMARY_QUESTIONS_INDICATOR = "enableSummaryQuestionsPanel";
+    String SUMMARY_ATTACHMENTS_INDICATOR = "enableSummaryAttachmentsPanel";
+    String SUMMARY_KEYWORDS_INDICATOR = "enableSummaryKeywordsPanel";
 
     /**
      * Get the units that the user has tht can create proposal.
@@ -94,8 +70,8 @@ public interface ProposalDevelopmentService {
      * @param userId the user's username
      * @return the list of units the user has this permission in
      */
-    public List<Unit> getUnitsForCreateProposal(String userId);
+    List<Unit> getUnitsForCreateProposal(String userId);
     
-    public boolean autogenerateInstitutionalProposal();
+    boolean autogenerateInstitutionalProposal();
     
 }
