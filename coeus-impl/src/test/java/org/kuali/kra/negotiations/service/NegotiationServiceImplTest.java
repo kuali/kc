dc576524f2d67bfa87e0be0f8caef2c8c2c8bb80
blackcathacker <douglas.pace@gmail.com>
2015-06-05 17:16:08 -0700

    RESOPS-189:Fix NPE that occurred with certain activity date combo
    When a Negotation has activities that are in the past in the same location such that the later activity starts before the previous activity ends and does not have an end date, building the history for the activities will cause an NPE to be thrown during JSP page rendering(not displayed to the user). Any further activity will produce an error similar to java.lang.RuntimeException: Error occured while trying to create a new instance for class interface java.util.List.
    
    The underlying NPE was occurring here. java.lang.NullPointerException
            at org.kuali.kra.negotiations.service.NegotiationServiceImpl.getNegotiationActivityHistoryLineBeans(NegotiationServiceImpl.java:294)
            at org.kuali.kra.negotiations.web.struts.form.NegotiationForm.getNegotiationActivityHistoryLineBeans(NegotiationForm.java:292)

---
+++ coeus-impl/src/test/java/org/kuali/kra/negotiations/service/NegotiationServiceImplTest.java
@@ -0,0 +1,314 @@
+package org.kuali.kra.negotiations.service;
+
+import static org.junit.Assert.*;
+import static org.kuali.kra.negotiations.service.NegotiationServiceImpl.ZERO_DAYS_LONG_STRING;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+import org.kuali.kra.negotiations.bo.NegotiationActivity;
+import org.kuali.kra.negotiations.bo.NegotiationActivityHistoryLineBean;
+import org.kuali.kra.negotiations.bo.NegotiationActivityType;
+import org.kuali.kra.negotiations.bo.NegotiationLocation;
+
+public class NegotiationServiceImplTest {
+
+	private NegotiationServiceImpl negotiationService;
+	private NegotiationActivityType activityType1;
+	private NegotiationLocation location1;
+	private NegotiationLocation location2;
+	
+	@Before
+	public void setup() {
+		negotiationService = new NegotiationServiceImpl();
+		
+		activityType1 = new NegotiationActivityType();
+		activityType1.setCode("1");
+		activityType1.setDescription("Activity Type 1");
+		
+		location1 = new NegotiationLocation();
+		location1.setCode("1");
+		location1.setDescription("Location 1");
+		
+		location2 = new NegotiationLocation();
+		location2.setCode("2");
+		location2.setDescription("Location 2");
+		
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_emptyList() {
+		negotiationService.getNegotiationActivityHistoryLineBeans(new ArrayList<NegotiationActivity>());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_oneActivity_noStartDate() {
+		NegotiationActivity activity = new NegotiationActivity();
+		activity.setLocation(location1);
+		activity.setActivityType(activityType1);
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(1, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals("", historyBeans.get(0).getLocationDays());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_oneActivity_noEndDate() {
+		int numberOfDaysAgo = 10;
+		NegotiationActivity activity = new NegotiationActivity();
+		activity.setLocation(location1);
+		activity.setActivityType(activityType1);
+		activity.setStartDate(new Date(DateTime.now().minusDays(numberOfDaysAgo).getMillis()));
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(1, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals(String.valueOf(numberOfDaysAgo), historyBeans.get(0).getLocationDays());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_oneActivity() {
+		NegotiationActivity activity = new NegotiationActivity();
+		activity.setLocation(location1);
+		activity.setActivityType(activityType1);
+		int numberOfDaysAgo = 10;
+		int activityLength1 = 2;
+		DateTime startDate1 = DateTime.now().minusDays(numberOfDaysAgo);
+		activity.setStartDate(new Date(startDate1.getMillis()));
+		DateTime endDate1 = startDate1.plusDays(activityLength1);
+		activity.setEndDate(new Date(endDate1.getMillis()));
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(1, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals(String.valueOf(activityLength1), historyBeans.get(0).getLocationDays());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_twoActivities_sameLocation_contiguousDates() {
+		int numberOfDaysAgo = 10;
+		int activityLength1 = 2;
+		int activityLength2 = 3;
+		
+		NegotiationActivity activity1 = new NegotiationActivity();
+		activity1.setLocation(location1);
+		activity1.setActivityType(activityType1);
+
+		DateTime startDate1 = DateTime.now().minusDays(numberOfDaysAgo);
+		activity1.setStartDate(new Date(startDate1.getMillis()));
+		DateTime endDate1 = startDate1.plusDays(activityLength1);
+		activity1.setEndDate(new Date(endDate1.getMillis()));
+		
+		NegotiationActivity activity2 = new NegotiationActivity();
+		activity2.setLocation(location1);
+		activity2.setActivityType(activityType1);
+		DateTime startDate2 = startDate1.plusDays(activityLength1+1);
+		activity2.setStartDate(new Date(startDate2.getMillis()));
+		DateTime endDate2 = startDate2.plusDays(activityLength2);
+		activity2.setEndDate(new Date(endDate2.getMillis()));
+		
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity1);
+		activities.add(activity2);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(2, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals(String.valueOf(activityLength1), historyBeans.get(0).getLocationDays());
+		assertEquals(location1.getDescription(), historyBeans.get(1).getLocation());
+		assertEquals(String.valueOf(activityLength2), historyBeans.get(1).getLocationDays());
+		DateTime effectiveStartDate = endDate1.plusDays(1);
+		assertEquals(effectiveStartDate.toDate(), historyBeans.get(1).getEfectiveLocationStartDate());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_twoActivities_sameLocation_noEndDates() {
+		int numberOfDaysAgo = 10;
+		
+		NegotiationActivity activity1 = new NegotiationActivity();
+		activity1.setLocation(location1);
+		activity1.setActivityType(activityType1);
+
+		DateTime startDate1 = DateTime.now().minusDays(numberOfDaysAgo);
+		activity1.setStartDate(new Date(startDate1.getMillis()));
+		
+		NegotiationActivity activity2 = new NegotiationActivity();
+		activity2.setLocation(location1);
+		activity2.setActivityType(activityType1);
+		DateTime startDate2 = startDate1.plusDays(3);
+		activity2.setStartDate(new Date(startDate2.getMillis()));
+		
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity1);
+		activities.add(activity2);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(2, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals(String.valueOf(numberOfDaysAgo), historyBeans.get(0).getLocationDays());
+		assertEquals(location1.getDescription(), historyBeans.get(1).getLocation());
+		assertEquals(ZERO_DAYS_LONG_STRING, historyBeans.get(1).getLocationDays());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_twoActivities_sameLocation_noFinalEndDate() {
+		int numberOfDaysAgo = 10;
+		int activityLength1 = 2;
+		
+		NegotiationActivity activity1 = new NegotiationActivity();
+		activity1.setLocation(location1);
+		activity1.setActivityType(activityType1);
+
+		DateTime startDate1 = DateTime.now().minusDays(numberOfDaysAgo);
+		activity1.setStartDate(new Date(startDate1.getMillis()));
+		DateTime endDate1 = startDate1.plusDays(activityLength1);
+		activity1.setEndDate(new Date(endDate1.getMillis()));
+		
+		NegotiationActivity activity2 = new NegotiationActivity();
+		activity2.setLocation(location1);
+		activity2.setActivityType(activityType1);
+		DateTime startDate2 = endDate1.plusDays(1);
+		activity2.setStartDate(new Date(startDate2.getMillis()));
+		
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity1);
+		activities.add(activity2);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(2, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals(String.valueOf(activityLength1), historyBeans.get(0).getLocationDays());
+		assertEquals(location1.getDescription(), historyBeans.get(1).getLocation());
+		assertEquals(NegotiationActivity.getNumberOfDays(new Date(startDate2.getMillis()), new Date(DateTime.now().getMillis())), historyBeans.get(1).getLocationDays());
+		DateTime effectiveStartDate = endDate1.plusDays(1);
+		assertEquals(effectiveStartDate.toDate(), historyBeans.get(1).getEfectiveLocationStartDate());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_twoActivities_sameLocation_overLappingDates() {
+		int numberOfDaysAgo = 30;
+		int activityLength1 = 15;
+		int activityLength2 = 10;
+		
+		NegotiationActivity activity1 = new NegotiationActivity();
+		activity1.setLocation(location1);
+		activity1.setActivityType(activityType1);
+
+		DateTime startDate1 = DateTime.now().minusDays(numberOfDaysAgo);
+		activity1.setStartDate(new Date(startDate1.getMillis()));
+		DateTime endDate1 = startDate1.plusDays(activityLength1);
+		activity1.setEndDate(new Date(endDate1.getMillis()));
+		
+		NegotiationActivity activity2 = new NegotiationActivity();
+		activity2.setLocation(location1);
+		activity2.setActivityType(activityType1);
+		DateTime startDate2 = startDate1.plusDays(8);
+		activity2.setStartDate(new Date(startDate2.getMillis()));
+		DateTime endDate2 = startDate2.plusDays(activityLength2);
+		activity2.setEndDate(new Date(endDate2.getMillis()));
+		
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity1);
+		activities.add(activity2);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(2, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals(String.valueOf(activityLength1), historyBeans.get(0).getLocationDays());
+		assertEquals(location1.getDescription(), historyBeans.get(1).getLocation());
+		assertEquals(NegotiationActivity.getNumberOfDays(new Date(endDate1.plusDays(1).getMillis()), new Date(endDate2.getMillis())), historyBeans.get(1).getLocationDays());
+		DateTime effectiveStartDate = endDate1.plusDays(1);
+		assertEquals(effectiveStartDate.toDate(), historyBeans.get(1).getEfectiveLocationStartDate());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_twoActivities_sameLocation_inclusiveDates() {
+		int numberOfDaysAgo = 30;
+		int activityLength1 = 15;
+		int activityLength2 = 10;
+		
+		NegotiationActivity activity1 = new NegotiationActivity();
+		activity1.setLocation(location1);
+		activity1.setActivityType(activityType1);
+
+		DateTime startDate1 = DateTime.now().minusDays(numberOfDaysAgo);
+		activity1.setStartDate(new Date(startDate1.getMillis()));
+		DateTime endDate1 = startDate1.plusDays(activityLength1);
+		activity1.setEndDate(new Date(endDate1.getMillis()));
+		
+		NegotiationActivity activity2 = new NegotiationActivity();
+		activity2.setLocation(location1);
+		activity2.setActivityType(activityType1);
+		DateTime startDate2 = startDate1.plusDays(1);
+		activity2.setStartDate(new Date(startDate2.getMillis()));
+		DateTime endDate2 = startDate2.plusDays(activityLength2);
+		activity2.setEndDate(new Date(endDate2.getMillis()));
+		
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity1);
+		activities.add(activity2);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(2, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals(String.valueOf(activityLength1), historyBeans.get(0).getLocationDays());
+		assertEquals(location1.getDescription(), historyBeans.get(1).getLocation());
+		assertEquals(ZERO_DAYS_LONG_STRING, historyBeans.get(1).getLocationDays());
+		assertNull(historyBeans.get(1).getEfectiveLocationStartDate());
+	}
+	
+	@Test
+	public void testHistoryBeanGeneration_twoActivities_twoLocations() {
+		int numberOfDaysAgo = 10;
+		int activityLength1 = 2;
+		int activityLength2 = 5;
+		
+		NegotiationActivity activity1 = new NegotiationActivity();
+		activity1.setLocation(location1);
+		activity1.setActivityType(activityType1);
+
+		DateTime startDate1 = DateTime.now().minusDays(numberOfDaysAgo);
+		activity1.setStartDate(new Date(startDate1.getMillis()));
+		DateTime endDate1 = startDate1.plusDays(activityLength1);
+		activity1.setEndDate(new Date(endDate1.getMillis()));
+		
+		NegotiationActivity activity2 = new NegotiationActivity();
+		activity2.setLocation(location2);
+		activity2.setActivityType(activityType1);
+		DateTime startDate2 = endDate1.minusDays(1);
+		activity2.setStartDate(new Date(startDate2.getMillis()));
+		activity2.setEndDate(new Date(startDate2.plusDays(activityLength2).getMillis()));
+		
+		List<NegotiationActivity> activities = new ArrayList<>();
+		activities.add(activity1);
+		activities.add(activity2);
+		
+		List<NegotiationActivityHistoryLineBean> historyBeans = negotiationService.getNegotiationActivityHistoryLineBeans(activities);
+		
+		assertEquals(3, historyBeans.size());
+		assertEquals(location1.getDescription(), historyBeans.get(0).getLocation());
+		assertEquals(String.valueOf(activityLength1), historyBeans.get(0).getLocationDays());
+		assertEquals(location2.getDescription(), historyBeans.get(2).getLocation());
+		assertEquals(String.valueOf(activityLength2), historyBeans.get(2).getLocationDays());
+		assertEquals(activity2.getStartDate(), historyBeans.get(2).getEfectiveLocationStartDate());
+	}
+}
