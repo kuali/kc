a21ce042d412b09fb9c8f88cc6090dc87449f5dc
Travis Schneeberger <leo.herbie@gmail.com>
2015-06-11 11:21:04 -0400

    RESKC-540: making log messages use one log statement (avoids interleave), make perf filter & monitoring filter use the same exclude configuration, make monitoring verbose logging configurable.

--- coeus-impl/src/main/java/org/kuali/coeus/sys/framework/controller/interceptor/RequestLoggingFilter.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/framework/controller/interceptor/RequestLoggingFilter.java
@@ -107,43 +107,39 @@ public class RequestLoggingFilter implements Filter {
      */
     private void doFilter(HttpServletRequest request, 
                           HttpServletResponse response) throws IOException, ServletException {
-        if (isInfoNotAllowed()) {
-            return;
-        }        
-        
-        MDC.put("clientIp", request.getRemoteAddr());
-                
-        LOG.info("Current User :" + request.getRemoteUser());
-        
-        //RequestURI & methodToCall (request parameter) are logged to trace user actions
-        LOG.info("RequestURI : " + request.getRequestURI());
-        LOG.info(getRequestParametersMessage(request, SENSITIVE_FILEDS_FILTER, paramsToLogInfoByDefault));
-        
-        if (LOG.isDebugEnabled()) {
 
-            LOG.debug("\n***************************** HEADERS **********************************\n"
-                      + getRequestHeadersMessage(request));
-            LOG.debug("\n***************************** PARAMETERS *******************************\n"
-                      + getRequestParametersMessage(request, SENSITIVE_FILEDS_FILTER));
-            LOG.debug("\n***************************** ATTRIBUTES *******************************\n"
-                      + getRequestAttributesMessage(request));
+        MDC.put("clientIp", request.getRemoteAddr());
+        if (LOG.isInfoEnabled()) {
+            final String logMessage = "Current User: " + request.getRemoteUser()
+                    + ", RequestURI: " + request.getRequestURI()
+                    + ", Logged-By-Default Parameters: " + getRequestParametersMessage(request, SENSITIVE_FILEDS_FILTER, paramsToLogInfoByDefault);
+
+            LOG.info(logMessage);
         }
+
+        if (LOG.isDebugEnabled()) {
+            final String logMessage = "\n***************************** HEADERS **********************************\n"
+                + getRequestHeadersMessage(request)
+                + "\n***************************** PARAMETERS *******************************\n"
+                      + getRequestParametersMessage(request, SENSITIVE_FILEDS_FILTER)
+                + "\n***************************** ATTRIBUTES *******************************\n"
+                      + getRequestAttributesMessage(request);
+            LOG.debug(logMessage);
+        }
+
     }
 
     /**
      * Constructs a log message that displays HTTP header information belonging to the given
      * {@link HttpServletRequest} instance. This method uses two nested loops to iterate headers
      * and then iterate through header values because a header may have one or more values.
-     *
-     * @param request
-     * @return Log message
      */
     private String getRequestHeadersMessage(HttpServletRequest request) {
         StringBuilder retval = new StringBuilder();
         for (Enumeration<String> headerNames = request.getHeaderNames();
              headerNames.hasMoreElements();) {
             String headerName = headerNames.nextElement();
-            retval.append(headerName).append(": {").toString();
+            retval.append(headerName).append(": {");
 
             for (Enumeration<String> headerValues = request.getHeaders(headerName); 
                  headerValues.hasMoreElements();) {
@@ -166,9 +162,7 @@ public class RequestLoggingFilter implements Filter {
      * {@link HttpServletRequest} instance. It also provides {@link Boolean} flag
      * to filter out sensitive field info from logging. Method is also enhanced to provide
      * info on selected parameters, which can be passed along {@link String} array.
-     * 
-     * @param request
-     * @param sensitivefieldsfilter 
+     *
      * @param params (Partial parameter names can also be used, Note: NO wildcard characters)
      * @return Log message
      */
@@ -209,13 +203,11 @@ public class RequestLoggingFilter implements Filter {
      * This method constructs and returns message info for passed parameter. Method also
      * iterates over parameterValues returned from request because a parameter may have 
      * one or more values.
-     * @param request
-     * @param parameterName
      * @return Parameter Info
      */
     private String getRequestParameterMessage(HttpServletRequest request, String parameterName) {
         StringBuilder retval = new StringBuilder();
-        retval.append(parameterName).append(": {").toString();
+        retval.append(parameterName).append(": {");
 
         for (String parameterValue : request.getParameterValues(parameterName)) {
             retval.append(parameterValue);
@@ -231,7 +223,6 @@ public class RequestLoggingFilter implements Filter {
      * Constructs a log message that displays attribute information belonging to the given
      * {@link HttpServletRequest} instance. 
      *
-     * @param request
      * @return Log message
      */
     private String getRequestAttributesMessage(HttpServletRequest request) {
@@ -240,20 +231,9 @@ public class RequestLoggingFilter implements Filter {
              attributeNames.hasMoreElements();) {
             String attributeName = attributeNames.nextElement();
             retval.append(attributeName).append(": ")
-                .append(request.getAttribute(attributeName)).append("\n").toString();
+                .append(request.getAttribute(attributeName)).append("\n");
         }
         
         return retval.toString();
-    }    
-
-    /**
-     * Determine if the logging is allowed by using {@link Log#isInfoEnabled()}. Currently the <code>INFO</code> level
-     * is required for logging here. It is possible that by extending this class and overriding {@link #isInfoNotAllowed()} the
-     * required log level can be adjusted.
-     *
-     * @return the value of {@link Log#isInfoEnabled()} directly.
-     */
-    protected boolean isInfoNotAllowed() {
-        return !LOG.isInfoEnabled();
     }
 }
