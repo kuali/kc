67bdc5b372e0efb052ceaadec83923d586db78f3
vineeth <vineeth@mit.edu>
2015-09-18 11:22:54 -0400

    RESKC-809:PD: System should enforce Proposal and Budget Start/End dates match prior to submitting-issue fixed

--- coeus-impl/src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
+++ coeus-impl/src/main/java/org/kuali/kra/infrastructure/KeyConstants.java
@@ -172,6 +172,7 @@ public final class KeyConstants {
     public static final String QUESTION_TOTALCOSTLIMIT_CHANGED = "document.question.totalCostLimit.changed";
     public static final String ERROR_BUDGET_REJECT_NO_REASON = "error.budget.reject.noReason";
     public static final String ERROR_COMPLETE_BUDGET_LOCK = "error.completeBudget.locked";
+    public static final String ERROR_BUDGET_DATES_NOT_MATCH_PROPOSAL_DATES = "error.budget.dates.not.match.proposal.dates";
     //Budget Version warnings
     public static final String WARNING_BUDGET_VERSION_MODULAR_INVALID_TDC = "warning.budgetVersion.Modular.InvalidTotalDirectCost";
 
