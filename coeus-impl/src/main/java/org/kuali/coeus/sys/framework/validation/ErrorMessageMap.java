4a7dccc784eb3097fbc4040c82d8d8a5ab631106
blackcathacker <douglas.pace@gmail.com>
2015-11-12 10:38:08 -0600

    adding missing copyrights #norelease

--- coeus-impl/src/main/java/org/kuali/coeus/sys/framework/validation/ErrorMessageMap.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/framework/validation/ErrorMessageMap.java
@@ -1,8 +1,25 @@
+/*
+ * Kuali Coeus, a comprehensive research administration system for higher education.
+ * 
+ * Copyright 2005-2015 Kuali, Inc.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.kuali.coeus.sys.framework.validation;
 
-import org.apache.commons.collections4.map.HashedMap;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +44,7 @@ public class ErrorMessageMap {
 
     public void addError(String key, String value) {
         if (errors == null) {
-            errors = new HashedMap<>();
+            errors = new HashMap<>();
         }
 
         List<String> values = errors.get(key);
