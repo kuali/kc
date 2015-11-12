aba1746a986675c1ede8963cf73a3384a837fea0
blackcathacker <douglas.pace@gmail.com>
2015-11-12 14:24:37 -0600

    update simplecrudrestcontroller to be non-abstract to support eventually spring/auto configuration

--- coeus-impl/src/main/java/org/kuali/coeus/common/budget/impl/core/category/BudgetCategoryController.java
+++ coeus-impl/src/main/java/org/kuali/coeus/common/budget/impl/core/category/BudgetCategoryController.java
@@ -18,40 +18,20 @@
  */
 package org.kuali.coeus.common.budget.impl.core.category;
 
-import java.util.Collections;
-import java.util.Map;
 
 import org.kuali.coeus.common.budget.framework.core.category.BudgetCategory;
-import org.kuali.coeus.common.budget.framework.core.category.BudgetCategoryType;
 import org.kuali.coeus.sys.framework.controller.rest.SimpleCrudRestController;
-import org.kuali.coeus.sys.framework.rest.DataDictionaryValidationException;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.kra.infrastructure.PermissionConstants;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import static org.kuali.coeus.sys.framework.util.CollectionUtils.entry;
 
 @Controller(value="budgetCategoryController")
-@RequestMapping(value="/api/v1/budget-categories/")
+@RequestMapping(value="/api/v1/budget-categories")
 public class BudgetCategoryController extends SimpleCrudRestController<BudgetCategory, BudgetCategoryDto> {
 
-	@Override
-	protected Class<BudgetCategoryDto> getDtoClass() {
-		return BudgetCategoryDto.class;
-	}
-
-	@Override
-	protected Class<BudgetCategory> getDoClass() {
-		return BudgetCategory.class;
-	}
-
-	@Override
-	protected Map.Entry<String, String> getPermission() {
-		return entry(Constants.MODULE_NAMESPACE_MAINTENANCE, PermissionConstants.MAINTAIN_INSTITUTE_RATES);
-	}
-
-	@Override
-	protected Object getPrimaryKeyFromDto(BudgetCategoryDto dataObject) {
-		return dataObject.getCode();
+	public BudgetCategoryController() {
+		super(BudgetCategory.class, BudgetCategoryDto.class, "code", 
+				Constants.MODULE_NAMESPACE_MAINTENANCE, PermissionConstants.MAINTAIN_INSTITUTE_RATES);
 	}
 }
