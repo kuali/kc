5d8f026039d6213ea2c9e8bd7a191b0e81bd0820
rmancher <rajeev@genesistec.com>
2015-11-23 09:59:52 -0500

    RESKC-1015:Committee Edit - Performance issue IRB committee document has huge performance issue. Also volume of data is high. This is when committee document is edited to make changes and routed. Edit a committee and submit/blanket approve the document takes a while to complete the process and finalize the document. Adding index to help boost performance.

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/mysql/kc/bootstrap/V1511_012__RESKC-1015.sql
@@ -0,0 +1,28 @@
+--
+-- Kuali Coeus, a comprehensive research administration system for higher education.
+--
+-- Copyright 2005-2015 Kuali, Inc.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+CREATE INDEX COMM_MEMBERSHIPS_COM_IDX
+ON COMM_MEMBERSHIPS (COMMITTEE_ID_FK);
+
+CREATE INDEX COMM_SCHEDULE_COM_IDX
+   ON COMM_SCHEDULE (COMMITTEE_ID_FK);
+   
+CREATE INDEX COMM_SCHEDULE_ATT_SCH_IDX
+ON COMM_SCHEDULE_ATTACHMENTS (SCHEDULE_ID_FK);
+   
