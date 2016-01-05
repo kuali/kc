66e2601fc59ccf88d64547982e7e78ddbd376188
blackcathacker <douglas.pace@gmail.com>
2016-01-04 17:13:17 -0800

    RESKC-1064:Rebase audit onto other rest changes #norelease

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1601_004__RestAuditLog.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1601_004__RestAuditLog.sql
@@ -1,7 +1,7 @@
 --
 -- Kuali Coeus, a comprehensive research administration system for higher education.
 --
--- Copyright 2005-2015 Kuali, Inc.
+-- Copyright 2005-2016 Kuali, Inc.
 --
 -- This program is free software: you can redistribute it and/or modify
 -- it under the terms of the GNU Affero General Public License as
@@ -17,17 +17,14 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-create table SEQ_REST_AUDIT_LOG_ID
-(
-	id bigint(19) not null auto_increment, primary key (id)
-) ENGINE MyISAM;
+create sequence SEQ_REST_AUDIT_LOG_ID INCREMENT BY 1 START WITH 1 NOCACHE;
 
 create table REST_AUDIT_LOG (
-	ID decimal(22,0) primary key,
-	USERNAME varchar(200) not null,
+	ID number(22,0) primary key,
+	USERNAME varchar2(200) not null,
 	UPDATE_TIMESTAMP timestamp not null,
-	CLASS_NAME varchar(4000) not null,
-	ADDED_JSON longtext,
-	MODIFIED_JSON longtext,
-	DELETED_JSON longtext
+	CLASS_NAME varchar2(4000) not null,
+	ADDED_JSON clob,
+	MODIFIED_JSON clob,
+	DELETED_JSON clob
 );
