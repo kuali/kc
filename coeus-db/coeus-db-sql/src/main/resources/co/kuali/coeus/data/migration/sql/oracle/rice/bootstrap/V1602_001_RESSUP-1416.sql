b5c0cca85e16087be7fed83fdf58d4c9775bc417
Peter Brown <petebrow@140-182-72-151.ssl-vpn.indiana.edu>
2016-02-02 09:54:28 -0500

    [RESSUP-1416] Adding sql for new rice parameter. Parameter value is empty by accepts a list of Award Transaction Type Codes

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1602_001_RESSUP-1416.sql
@@ -0,0 +1,3 @@
+INSERT INTO krcr_parm_t (nmspc_cd, cmpnt_cd, parm_nm, obj_id, ver_nbr, parm_typ_cd, val, parm_desc_txt, eval_oprtr_cd, appl_id)
+ VALUES ('KC-AWARD', 'Document', 'AwardTransactionTypeAllowedValues', sys_guid(), 1, 'CONFG', '', 'Parameter defining allowed Award Transaction Types in Award drop down', 'A', 'KC');
+ 
