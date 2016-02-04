b4f4d018b40b73a0d3f7a4311a44f0edf7917bdc
blackcathacker <douglas.pace@gmail.com>
2016-02-04 15:49:40 -0800

    RESKC-1120:Fix oracle parameter statement using UUID #norelease

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1602_003__new_breakupinterval_rate_calc_param.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/rice/bootstrap/V1602_003__new_breakupinterval_rate_calc_param.sql
@@ -17,4 +17,4 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 INSERT INTO KRCR_PARM_T (NMSPC_CD, CMPNT_CD, PARM_NM, OBJ_ID, VER_NBR, PARM_TYP_CD, VAL, PARM_DESC_TXT, EVAL_OPRTR_CD, APPL_ID)
-VALUES ('KC-B', 'Document', 'breakupServiceUseNewCalculation', UUID(), 1, 'CONFG', 'N', 'Determines whether or not to enable a new rate calculator within the breakup service. The new calculator should be functionally equivilent to the previous one, but resolves some edge cases. It is also still under testing and therefore not enabled by default. Set to Y to enable, N to disable', 'A', 'KC');
+VALUES ('KC-B', 'Document', 'breakupServiceUseNewCalculation', SYS_GUID(), 1, 'CONFG', 'N', 'Determines whether or not to enable a new rate calculator within the breakup service. The new calculator should be functionally equivilent to the previous one, but resolves some edge cases. It is also still under testing and therefore not enabled by default. Set to Y to enable, N to disable', 'A', 'KC');
