0f9185d994ba45ba93000afdc9aad4bdb8dba4ae
blackcathacker <douglas.pace@gmail.com>
2016-03-11 16:00:04 -0800

    fixing broken integration test #norelease

--- coeus-it/src/test/java/org/kuali/kra/questionnaire/QuestionnaireLookupableHelperServiceTest.java
+++ coeus-it/src/test/java/org/kuali/kra/questionnaire/QuestionnaireLookupableHelperServiceTest.java
@@ -90,7 +90,7 @@ public class QuestionnaireLookupableHelperServiceTest extends KcIntegrationTestB
         maintDocument.getNewMaintainableObject().setMaintenanceAction(KRADConstants.MAINTENANCE_NEW_ACTION);
         documentService.routeDocument(maintDocument, null, null);
         List<Questionnaire> searchResults = (List<Questionnaire>) questionnaireLookupableHelperServiceImpl.getSearchResults(new HashMap());
-        assertEquals(19 , searchResults.size());
+        assertEquals(20 , searchResults.size());
         
         Questionnaire test1 = null;
         Questionnaire test2 = null;
