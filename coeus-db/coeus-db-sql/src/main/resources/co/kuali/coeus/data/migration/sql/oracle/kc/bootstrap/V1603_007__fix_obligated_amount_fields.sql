c5e9617b0091faa154e9653734b5ead9520a6976
Geo Thomas <geot@mit.edu>
2016-03-15 15:12:59 -0400

    RESKC-904: Changing the script to address the issue in oracle, which restricts to alter the column to decrease its scale precision

--- coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1603_007__fix_obligated_amount_fields.sql
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1603_007__fix_obligated_amount_fields.sql
@@ -17,9 +17,42 @@
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 --
 
-alter table AWARD_BUDGET_DETAILS_EXT modify (obligated_amount number(12,2));
-alter table AWD_BGT_DET_CAL_AMTS_EXT modify (obligated_amount number(12,2));
-alter table AWD_BUDGET_PER_DET_EXT modify (obligated_amount number(12,2));
-alter table AWD_BUDGET_PER_CAL_AMTS_EXT modify (obligated_amount number(12,2));
-alter table award_budget_period_ext modify (obligated_amount number(12,2));
-alter table award_budget_period_ext modify (total_fringe_amount number(12,2));
+alter table AWARD_BUDGET_DETAILS_EXT add obligated_amount_new number(12,2);
+update AWARD_BUDGET_DETAILS_EXT set obligated_amount_new=obligated_amount where BUDGET_DETAILS_ID in
+    (select BUDGET_DETAILS_ID from AWARD_BUDGET_DETAILS_EXT);
+update AWARD_BUDGET_DETAILS_EXT set obligated_amount=null;
+alter table AWARD_BUDGET_DETAILS_EXT drop column obligated_amount;
+alter table AWARD_BUDGET_DETAILS_EXT rename column obligated_amount_new to obligated_amount;
+
+alter table AWD_BGT_DET_CAL_AMTS_EXT add obligated_amount_new number(12,2);
+update AWD_BGT_DET_CAL_AMTS_EXT set obligated_amount_new=obligated_amount where BUDGET_DETAILS_CAL_AMTS_ID in
+    (select BUDGET_DETAILS_CAL_AMTS_ID from AWD_BGT_DET_CAL_AMTS_EXT);
+update AWD_BGT_DET_CAL_AMTS_EXT set obligated_amount=null;
+alter table AWD_BGT_DET_CAL_AMTS_EXT drop column obligated_amount;
+alter table AWD_BGT_DET_CAL_AMTS_EXT rename column obligated_amount_new to obligated_amount;
+
+alter table AWD_BUDGET_PER_DET_EXT add obligated_amount_new number(12,2);
+update AWD_BUDGET_PER_DET_EXT set obligated_amount_new=obligated_amount where BUDGET_PERSONNEL_DETAILS_ID in
+    (select BUDGET_PERSONNEL_DETAILS_ID from AWD_BUDGET_PER_DET_EXT);
+update AWD_BUDGET_PER_DET_EXT set obligated_amount=null;
+alter table AWD_BUDGET_PER_DET_EXT drop column obligated_amount;
+alter table AWD_BUDGET_PER_DET_EXT rename column obligated_amount_new to obligated_amount;
+
+alter table AWD_BUDGET_PER_CAL_AMTS_EXT add obligated_amount_new number(12,2);
+update AWD_BUDGET_PER_CAL_AMTS_EXT set obligated_amount_new=obligated_amount where BUDGET_PERSONNEL_CAL_AMTS_ID in
+    (select BUDGET_PERSONNEL_CAL_AMTS_ID from AWD_BUDGET_PER_CAL_AMTS_EXT);
+update AWD_BUDGET_PER_CAL_AMTS_EXT set obligated_amount=null;
+alter table AWD_BUDGET_PER_CAL_AMTS_EXT drop column obligated_amount;
+alter table AWD_BUDGET_PER_CAL_AMTS_EXT rename column obligated_amount_new to obligated_amount;
+
+alter table award_budget_period_ext add obligated_amount_new number(12,2);
+alter table award_budget_period_ext add total_fringe_amount_new number(12,2);
+update award_budget_period_ext set obligated_amount_new=obligated_amount,
+                                    total_fringe_amount_new = total_fringe_amount
+          where BUDGET_PERIOD_NUMBER in (select BUDGET_PERIOD_NUMBER from award_budget_period_ext);
+update award_budget_period_ext set obligated_amount=null,total_fringe_amount=null;
+alter table award_budget_period_ext drop column obligated_amount;
+alter table award_budget_period_ext drop column total_fringe_amount;
+alter table award_budget_period_ext rename column obligated_amount_new to obligated_amount;
+alter table award_budget_period_ext rename column total_fringe_amount_new to total_fringe_amount;
+
