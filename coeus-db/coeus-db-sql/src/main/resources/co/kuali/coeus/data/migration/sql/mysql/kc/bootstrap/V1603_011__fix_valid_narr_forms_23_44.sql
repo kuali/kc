be95be966552c2894461bfb0f87068662e475e3f
Travis Schneeberger <leo.herbie@gmail.com>
2016-03-17 12:54:03 -0400

    RESKC-1217: fix the valid_narr_forms records for narrative type 23 and 44.

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/mysql/kc/bootstrap/V1603_011__fix_valid_narr_forms_23_44.sql
@@ -0,0 +1,50 @@
+--
+-- Kuali Coeus, a comprehensive research administration system for higher education.
+--
+-- Copyright 2005-2016 Kuali, Inc.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+delete from valid_narr_forms where narrative_type_code in ('23', '44');
+
+INSERT INTO SEQ_VALID_NARR_FORMS_ID VALUES(NULL);
+
+INSERT INTO VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_USER,UPDATE_TIMESTAMP,OBJ_ID)
+VALUES ((SELECT (MAX(ID)) FROM SEQ_VALID_NARR_FORMS_ID),'PHS398_ResearchPlan-V1.0','23','N','admin',NOW(),UUID());
+
+INSERT INTO SEQ_VALID_NARR_FORMS_ID VALUES(NULL);
+
+INSERT INTO VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_USER,UPDATE_TIMESTAMP,OBJ_ID)
+VALUES ((SELECT (MAX(ID)) FROM SEQ_VALID_NARR_FORMS_ID),'PHS398_ResearchPlan-V1.1','23','N','admin',NOW(),UUID());
+
+INSERT INTO SEQ_VALID_NARR_FORMS_ID VALUES(NULL);
+
+INSERT INTO VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_USER,UPDATE_TIMESTAMP,OBJ_ID)
+VALUES ((SELECT (MAX(ID)) FROM SEQ_VALID_NARR_FORMS_ID),'PHS398_ResearchPlan_1_2-V1.2','23','N','admin',NOW(),UUID());
+
+INSERT INTO SEQ_VALID_NARR_FORMS_ID VALUES(NULL);
+
+INSERT INTO VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_USER,UPDATE_TIMESTAMP,OBJ_ID)
+VALUES ((SELECT (MAX(ID)) FROM SEQ_VALID_NARR_FORMS_ID),'PHS398_ResearchPlan_1_3','44','N','admin',NOW(),UUID());
+
+INSERT INTO SEQ_VALID_NARR_FORMS_ID VALUES(NULL);
+
+INSERT INTO VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_USER,UPDATE_TIMESTAMP,OBJ_ID)
+VALUES ((SELECT (MAX(ID)) FROM SEQ_VALID_NARR_FORMS_ID),'PHS398_ResearchPlan_2_0','44','N','admin',NOW(),UUID());
+
+INSERT INTO SEQ_VALID_NARR_FORMS_ID VALUES(NULL);
+
+INSERT INTO VALID_NARR_FORMS (VALID_NARR_FORMS_ID,FORM_NAME,NARRATIVE_TYPE_CODE,MANDATORY,UPDATE_USER,UPDATE_TIMESTAMP,OBJ_ID)
+VALUES ((SELECT (MAX(ID)) FROM SEQ_VALID_NARR_FORMS_ID),'PHS398_ResearchPlan_3_0','44','N','admin',NOW(),UUID());
