7712b8e2391ff4c7c506e3a75035e3f8feba2e86
Travis Schneeberger <leo.herbie@gmail.com>
2016-03-31 15:40:14 -0400

    RESKC-1227: Question 149 is not entered properly. It is a child of Q 5.             It is supposed to be a child of Q6.             With this set-up, if user answers Yes - stem cells will be used, they MUST enter cell lines in Q149, else the questionnaire will not update to Complete.

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/mysql/kc/bootstrap/V1604_008__fix_questionnaire_for_form.sql
@@ -0,0 +1,22 @@
+--
+-- Kuali Coeus, a comprehensive research administration system for higher education.
+--
+-- Copyright 2005-2016 Kuali, Inc.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+update questionnaire_questions set parent_question_number = 8 where
+  QUESTIONNAIRE_REF_ID_FK = (select QUESTIONNAIRE_REF_ID from questionnaire where QUESTIONNAIRE_ID = -1) and
+  QUESTION_REF_ID_FK in (select QUESTION_REF_ID from question where QUESTION_ID in (149));
