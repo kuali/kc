fc154efce363b2c3f134dccd759ae3aa3bec012b
blackcathacker <douglas.pace@gmail.com>
2016-04-25 12:23:12 -0700

    RESOPS-812:Fix budget rates build, misc bug fix and permission correction

--- coeus-webapp/src/main/jsfrontend/gulpfile.js
+++ coeus-webapp/src/main/jsfrontend/gulpfile.js
@@ -1,18 +1,18 @@
 /*
  * Kuali Coeus, a comprehensive research administration system for higher education.
- * 
+ *
  * Copyright 2005-2016 Kuali, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -41,8 +41,7 @@ gulp.task('webpack', function (callback) {
 		new webpack.DefinePlugin({
 			"process.env" : { "NODE_ENV" : JSON.stringify("production") }
 		}),
-		new webpack.optimize.DedupePlugin(),
-		new webpack.optimize.UglifyJsPlugin()
+		new webpack.optimize.DedupePlugin()
 	);
 
 	webpack(localWebpackConfig, function(err, stats) {
@@ -62,19 +61,5 @@ gulp.task('clients', ['webpack', 'static-assets']);
 gulp.task('dev-build', ['static-assets','webpack:build-dev'], function() {
 	gulp.watch(['instituteRates/**/*', 'client/**/*'], ['static-assets', 'webpack:build-dev']);
 });
-
-gulp.task('webpack:build-dev', function(callback) {
-	var myDevConfig = Object.create(webpackConfig);
-	myDevConfig.devtool = "sourcemap";
-	myDevConfig.debug = true;
-
-	var devWebpackCompiler = webpack(myDevConfig);
-	devWebpackCompiler.run(function(err, stats) {
-		if (err) throw new gutil.PluginError('webpack:build-dev', err);
-		gutil.log('[webpack:build-dev]', stats.toString({ color: true }));
-		callback();
-	});
-});
-
 
 gulp.task('default', ['docs', 'clients']);
