7e0cb8c41a0040ae062b5697476a98f2928e947d
Travis Schneeberger <leo.herbie@gmail.com>
2016-05-13 20:41:58 -0400

    RESOPS-858: handle date strings that are empty strings.

--- coeus-impl/src/main/java/org/kuali/coeus/sys/framework/controller/rest/CustomEditors/CustomSqlDateEditor.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/framework/controller/rest/CustomEditors/CustomSqlDateEditor.java
@@ -1,5 +1,7 @@
 package org.kuali.coeus.sys.framework.controller.rest.CustomEditors;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.beans.PropertyEditorSupport;
 import java.sql.Date;
 import java.text.ParseException;
@@ -7,15 +9,21 @@ import java.text.SimpleDateFormat;
 
 public class CustomSqlDateEditor extends PropertyEditorSupport {
 
+    @Override
     public void setAsText(String value) {
         setValue(value);
     }
 
+    @Override
     public Object getValue() {
         SimpleDateFormat source = new SimpleDateFormat("MM/dd/yyyy");
         SimpleDateFormat target = new SimpleDateFormat("yyyy-MM-dd");
 
         String oldDate = super.getValue().toString();
+        if (super.getValue() == null || StringUtils.isBlank(oldDate)) {
+            return null;
+        }
+
         String newDate;
         try {
             newDate = target.format(source.parse(oldDate));
--- coeus-impl/src/main/java/org/kuali/coeus/sys/framework/controller/rest/CustomEditors/CustomSqlTimestampEditor.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/framework/controller/rest/CustomEditors/CustomSqlTimestampEditor.java
@@ -1,16 +1,24 @@
 package org.kuali.coeus.sys.framework.controller.rest.CustomEditors;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.beans.PropertyEditorSupport;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 
 public class CustomSqlTimestampEditor extends PropertyEditorSupport {
 
+    @Override
     public void setAsText(String value) {
         setValue(value);
     }
 
+    @Override
     public Object getValue() {
+        if (super.getValue() == null || StringUtils.isBlank(super.getValue().toString())) {
+            return null;
+        }
+
         return Timestamp.valueOf(super.getValue().toString());
     }
 
