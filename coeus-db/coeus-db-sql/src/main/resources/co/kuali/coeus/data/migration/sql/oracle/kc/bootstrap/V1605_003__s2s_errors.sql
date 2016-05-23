63fa79471aed80034d45fa523412c5a5534e5861
Travis Schneeberger <leo.herbie@gmail.com>
2016-05-23 16:46:24 -0400

    RESKC-1325: added error messages for several s2s generators, upgrade s2s

---
+++ coeus-db/coeus-db-sql/src/main/resources/co/kuali/coeus/data/migration/sql/oracle/kc/bootstrap/V1605_003__s2s_errors.sql
@@ -0,0 +1,27 @@
+--
+-- Kuali Coeus, a comprehensive research administration system for higher education.
+--
+-- Copyright 2005-2016 Kuali, Inc.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as
+-- published by the Free Software Foundation, either version 3 of the
+-- License, or (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+INSERT INTO S2S_ERROR (S2S_ERROR_ID, MESSAGE_KEY, MESSAGE, FIX_LINK, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+VALUES (SEQ_S2S_ERROR_ID.NEXTVAL, '/GrantApplication/Forms/PHS398_ResearchPlan_3_0/ResearchPlanAttachments/ResearchStrategy', 'Research Strategy attachment is required for Research Plan.', 'abstractsAttachments',SYSDATE,'admin',1,SYS_GUID());
+
+INSERT INTO S2S_ERROR (S2S_ERROR_ID, MESSAGE_KEY, MESSAGE, FIX_LINK, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+VALUES (SEQ_S2S_ERROR_ID.NEXTVAL, '/GrantApplication/Forms/PHS398_ResearchTrainingProgramPlan_3_0/ResearchTrainingProgramPlanAttachments', 'Program Plan attachment is required for Research Training Program Plan.', 'abstractsAttachments',SYSDATE,'admin',1,SYS_GUID());
+
+INSERT INTO S2S_ERROR (S2S_ERROR_ID, MESSAGE_KEY, MESSAGE, FIX_LINK, UPDATE_TIMESTAMP, UPDATE_USER, VER_NBR, OBJ_ID)
+VALUES (SEQ_S2S_ERROR_ID.NEXTVAL, '/GrantApplication/Forms/PHS398_CareerDevelopmentAwardSup_3_0/CareerDevelopmentAwardAttachments/ResearchStrategy', 'Research Strategy attachment is required for Career Development Award Supplemental.', 'abstractsAttachments',SYSDATE,'admin',1,SYS_GUID());
