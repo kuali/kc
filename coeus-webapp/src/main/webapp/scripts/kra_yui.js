325367cc46bb9110d4e4a9c7abec65786557eda7
Travis Schneeberger <leo.herbie@gmail.com>
2016-05-25 10:00:39 -0400

    RESKC-1246: sorting and truncating the unit list in a way that displays correctly in the unit hierarchy UI.  Correcting client javascript errors.  Minor code cleanup.

--- coeus-webapp/src/main/webapp/scripts/kra_yui.js
+++ coeus-webapp/src/main/webapp/scripts/kra_yui.js
@@ -32,12 +32,13 @@
 	var units = document.getElementById("units").value;
 	var depth = document.getElementById("depth").value;
 	var unit_0_array=units.split(";1;");
-	
+	var unit_array = unit_0_array.slice(0);
+
 	// following is a temp solution to align the action buttons. The buttons is still not 100% align.  need a better solution
-    var table_1 = "<table width=\"800\"><tr><td width=\"60%\">";
+
     var widthGap=18;
     var table_1 = "<table style=\"width:1080px\"><tr><td style=\"width:760px\">";
-    var table_2 = "<table style=\"width:1062px\"><tr><td style=\"width:742px\">";
+
 
     var tree;    
         function buildTree() {
@@ -48,16 +49,15 @@
            	var root = tree.getRoot();
            	// top node
 			var tempNode = new YAHOO.widget.HTMLNode( table_1 + unit_0_array[0] + "</td><td style=\"width:320px\">"+ setupMaintenanceButtons(unit_0_array[0])+"</td></tr></table>", root, true, true);
-			//var tempNode = new YAHOO.widget.HTMLNode( "<td style=\"width:742px\">" + unit_0_array[0] + "</td> <td style=\"width:320px\">"+ setupMaintenanceButtons(unit_0_array[0])+"</td>", root, true, true);
            	tempNode.contentStyle="icon-page";
             unit_0_array[0] = tempNode; // save node for later parent reference
            	for (var i=1 ; i < unit_0_array.length;  i++) {
            	    // set up the rest of the nodes
-           	    // data structure is like 1-IU-UNIV : unit name
-           	    // "1-" indicated that it's parent is "1" in the array list
-                var idx = unit_0_array[i].indexOf("-");
-                var parentIdx=unit_0_array[i].substring(0,idx);
-                var nodeLabel=unit_0_array[i].substring(idx+1, unit_0_array[i].length);
+           	    // data structure is like 1$-$IU-UNIV : unit name
+           	    // "1$-$" indicated that it's parent is "1" in the array list
+                var idx = unit_0_array[i].indexOf("$-$");
+                var parentIdx=findIndex(unit_0_array[i].substring(0,idx), unit_array);
+                var nodeLabel=unit_0_array[i].substring(idx+3, unit_0_array[i].length);
 
 				var tempNode_1 = new YAHOO.widget.HTMLNode( "<table style=\"width:"+String(1080-(unit_0_array[parentIdx].depth+1)*widthGap)+"px\"><tr><td style=\"width:"+String(760-(unit_0_array[parentIdx].depth+1)*widthGap)+"px\">" + nodeLabel + "</td><td style=\"width:320px\">"+ setupMaintenanceButtons(nodeLabel)+"</td></tr></table>", unit_0_array[parentIdx], true, true);
 				if (tempNode_1.depth == depth - 1) {
@@ -67,14 +67,22 @@
 					tempNode_1.expanded=false;
 				}
            		tempNode_1.contentStyle="icon-page";
-                unit_0_array[i]=tempNode_1;		
-                //alert(tempNode_1.getNodeHtml());	 	
+                unit_0_array[i]=tempNode_1;
            }
            
            //render tree with these toplevel nodes; all descendants of these nodes
            //will be generated as needed by the dynamic loader.
            tree.draw();
         }
+
+	    function findIndex(parentIdx, unit_array) {
+			for (var i = 1; i < unit_array.length; i++) {
+				if (unit_array[i].indexOf("$-$" + parentIdx) > -1) {
+					return i;
+				}
+			}
+			return 0;
+		}
 
 		function loadUnitName(node) {
 		   // The ajax code to load node dynamically.  so far it is working fine without the yui connection manager
@@ -90,8 +98,6 @@
 				               	unitNum+=1;
 			 				}
 							
-						} else {
-							//alert ("data is null");
 						}
 						 node.loadComplete();
 						
