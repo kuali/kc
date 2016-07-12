5adeda2621fe2d969f1f21afc68fa09bd91c7f39
Gayathri Athreya <gathreya@email.arizona.edu>
2016-07-12 12:13:29 -0700

    fix integration tests. (#1852)

--- coeus-impl/src/main/java/org/kuali/coeus/award/dto/AwardBudgetExtDto.java
+++ coeus-impl/src/main/java/org/kuali/coeus/award/dto/AwardBudgetExtDto.java
@@ -1,11 +1,7 @@
 package org.kuali.coeus.award.dto;
 
 import com.codiform.moo.annotation.CollectionProperty;
-import com.codiform.moo.annotation.Property;
-import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonProperty;
-import org.kuali.coeus.common.budget.framework.rate.BudgetLaRate;
-import org.kuali.coeus.common.budget.framework.rate.BudgetRate;
 import org.kuali.coeus.sys.api.model.ScaleTwoDecimal;
 import org.kuali.kra.award.budget.AwardBudgetLimit;
 
@@ -18,25 +14,25 @@ public class AwardBudgetExtDto extends AwardBudgetGeneralInfoDto {
     private ScaleTwoDecimal totalCostInclPrev;
     @JsonProperty(value="budgetRates")
     @CollectionProperty(source="budgetRates", itemClass= BudgetRateDto.class)
-    private List<BudgetRate> budgetRates;
+    private List<BudgetRateDto> budgetRates;
     @JsonProperty(value="budgetLaRates")
     @CollectionProperty(source="budgetLaRates", itemClass= BudgetRateDto.class)
-    private List<BudgetLaRate> budgetLaRates;
+    private List<BudgetRateDto> budgetLaRates;
     @JsonProperty(value="budgetPeriods")
     @CollectionProperty(source="budgetPeriods", itemClass= BudgetPeriodDto.class)
     private List<BudgetPeriodDto> budgetPeriods;
     @JsonProperty(value="awardBudgetLimits")
     @CollectionProperty(source="awardBudgetLimits", itemClass= AwardBudgetLimitDto.class)
-    private List<AwardBudgetLimit> awardBudgetLimits;
+    private List<AwardBudgetLimitDto> awardBudgetLimits;
     @JsonProperty(value="budgetPersons")
     @CollectionProperty(source="budgetPersons", itemClass= BudgetPersonDto.class)
     private List<BudgetPersonDto> budgetPersons;
 
-    public List<AwardBudgetLimit> getAwardBudgetLimits() {
+    public List<AwardBudgetLimitDto> getAwardBudgetLimits() {
         return awardBudgetLimits;
     }
 
-    public void setAwardBudgetLimits(List<AwardBudgetLimit> awardBudgetLimits) {
+    public void setAwardBudgetLimits(List<AwardBudgetLimitDto> awardBudgetLimits) {
         this.awardBudgetLimits = awardBudgetLimits;
     }
 
