81c04e8a727a592ea08d9f692a2ca64a64b3a0a5
Travis Schneeberger <leo.herbie@gmail.com>
2016-10-06 12:08:26 -0400

    RESOPS-1129: fixing data dictionary overrides, allowing docs to be opened from doc search, making sure delete functions correctly with runtime application enabled.  making sure inactivation works correctly with runtime application enabled.  Fixing validation when a file is not uploaded (happens when copying or editing existing objects).

--- coeus-impl/src/main/java/org/kuali/coeus/sys/impl/dd/DataDictionaryOverrideMaintainableImpl.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/impl/dd/DataDictionaryOverrideMaintainableImpl.java
@@ -19,45 +19,68 @@
 package org.kuali.coeus.sys.impl.dd;
 
 
+import org.apache.commons.lang3.StringUtils;
 import org.kuali.coeus.sys.framework.dd.DataDictionaryOverride;
 import org.kuali.coeus.sys.framework.service.KcServiceLocator;
 import org.kuali.kra.infrastructure.Constants;
 import org.kuali.rice.coreservice.framework.parameter.ParameterService;
-import org.kuali.rice.kns.datadictionary.exporter.DataDictionaryMap;
 import org.kuali.rice.kns.maintenance.KualiMaintainableImpl;
 import org.kuali.rice.krad.datadictionary.DataDictionary;
 import org.kuali.rice.krad.service.DataDictionaryService;
+import org.kuali.rice.krad.service.SequenceAccessorService;
+import org.kuali.rice.krad.util.KRADConstants;
 
 import java.util.Collection;
 
 public class DataDictionaryOverrideMaintainableImpl extends KualiMaintainableImpl {
 
     private static final String DATA_DICTIONARY_RUNTIME_OVERRIDE_PARM = "DATA_DICTIONARY_RUNTIME_OVERRIDE";
+    private static final String SEQ_DD_OVERRIDE_ID = "SEQ_DD_OVERRIDE_ID";
+
     private transient ParameterService parameterService;
+    private transient SequenceAccessorService sequenceAccessorService;
 
     @Override
     public void saveBusinessObject() {
         final DataDictionaryOverride override = (DataDictionaryOverride) getBusinessObject();
-        if (override.isActive() && getParameterService().getParameterValueAsBoolean(Constants.KC_SYS, Constants.KC_ALL_PARAMETER_DETAIL_TYPE_CODE, DATA_DICTIONARY_RUNTIME_OVERRIDE_PARM)) {
-            final DataDictionary modifiedDataDictionary = DataDictionaryOverrideUtils.createNewWithOverride(getDataDictionaryService().getDataDictionary(), override.getAttachmentContent());
-            final DataDictionaryMap previousMap = (DataDictionaryMap) getDataDictionaryService().getDataDictionaryMap();
-            //in case there are multiple instances of the DataDictionaryService, get them all and set the new DataDictionary instance
-            final Collection<DataDictionaryService> ddServices = KcServiceLocator.getServicesOfType(DataDictionaryService.class);
-            previousMap.clear();
 
-            ddServices.forEach(ddService -> {
-                ddService.setDataDictionary(modifiedDataDictionary);
-                if (ddService instanceof org.kuali.rice.kns.service.DataDictionaryService) {
-                    ((org.kuali.rice.kns.service.DataDictionaryService) ddService).setDataDictionaryMap(previousMap);
-                }
-            });
+        if (!KRADConstants.MAINTENANCE_DELETE_ACTION.equals(getMaintenanceAction())) {
+            if (StringUtils.isBlank(override.getId())) {
+                override.setId(getSequenceAccessorService().getNextAvailableSequenceNumber(SEQ_DD_OVERRIDE_ID, DataDictionaryOverride.class).toString());
+            }
+
+            if (isRuntimeApplicationEnabled()) {
+                resetDataDictionary(override.isActive() ? DataDictionaryOverrideUtils.createNewWithOverride(getDataDictionaryService().getDataDictionary(), override.getId(), override.getAttachmentContent()) :
+                        DataDictionaryOverrideUtils.createNewRemovingOverride(getDataDictionaryService().getDataDictionary(), override.getId()));
+            }
+            super.saveBusinessObject();
         }
-        super.saveBusinessObject();
+    }
+
+    protected void resetDataDictionary(DataDictionary modifiedDataDictionary) {
+        final Collection<DataDictionaryService> ddServices = KcServiceLocator.getServicesOfType(DataDictionaryService.class);
+
+        ddServices.forEach(ddService -> {
+            ddService.setDataDictionary(modifiedDataDictionary);
+            if (ddService instanceof org.kuali.rice.kns.service.DataDictionaryService) {
+                ((org.kuali.rice.kns.service.DataDictionaryService) ddService).getDataDictionaryMap().clear();
+            }
+        });
     }
 
     @Override
-    protected org.kuali.rice.kns.service.DataDictionaryService getDataDictionaryService() {
-        return (org.kuali.rice.kns.service.DataDictionaryService) super.getDataDictionaryService();
+    public void deleteDataObject() {
+        final DataDictionaryOverride override = (DataDictionaryOverride) getBusinessObject();
+
+        if (isRuntimeApplicationEnabled()) {
+            resetDataDictionary(DataDictionaryOverrideUtils.createNewRemovingOverride(getDataDictionaryService().getDataDictionary(), override.getId()));
+        }
+
+        super.deleteDataObject();
+    }
+
+    protected boolean isRuntimeApplicationEnabled() {
+        return getParameterService().getParameterValueAsBoolean(Constants.KC_SYS, Constants.KC_ALL_PARAMETER_DETAIL_TYPE_CODE, DATA_DICTIONARY_RUNTIME_OVERRIDE_PARM);
     }
 
     public ParameterService getParameterService() {
@@ -70,5 +93,16 @@ public class DataDictionaryOverrideMaintainableImpl extends KualiMaintainableImp
 
     public void setParameterService(ParameterService parameterService) {
         this.parameterService = parameterService;
+    }
+
+    public SequenceAccessorService getSequenceAccessorService() {
+        if (sequenceAccessorService == null) {
+            sequenceAccessorService = KcServiceLocator.getService(SequenceAccessorService.class);
+        }
+        return sequenceAccessorService;
+    }
+
+    public void setSequenceAccessorService(SequenceAccessorService sequenceAccessorService) {
+        this.sequenceAccessorService = sequenceAccessorService;
     }
 }
--- coeus-impl/src/main/java/org/kuali/coeus/sys/impl/dd/DataDictionaryOverrideUtils.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/impl/dd/DataDictionaryOverrideUtils.java
@@ -18,46 +18,117 @@
  */
 package org.kuali.coeus.sys.impl.dd;
 
-import org.kuali.coeus.sys.framework.dd.DataDictionaryOverride;
+
+import org.kuali.coeus.sys.framework.util.CollectionUtils;
 import org.kuali.rice.krad.datadictionary.DataDictionary;
 import org.kuali.rice.krad.datadictionary.DataDictionaryException;
 import org.springframework.core.io.Resource;
 import org.springframework.security.util.InMemoryResource;
 
-
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import static org.kuali.coeus.sys.framework.util.CollectionUtils.*;
 
-public class DataDictionaryOverrideUtils {
 
+public final class DataDictionaryOverrideUtils {
 
     private DataDictionaryOverrideUtils() {
         throw new UnsupportedOperationException("do not call");
     }
 
-    public static DataDictionary createNewWithOverride(DataDictionary old, byte[] override) throws DataDictionaryException {
-        final DataDictionary modifiedDataDictionary = new DataDictionary();
-        final Resource newOverride = new InMemoryResource(override);
+    /**
+     * This method creates a new DataDictionary instance based on an old instance conditionally adding/replacing an in-memory data dictionary override file.
+     * The internal state of the source data dictionary is copied to the target so that updates to the target data dictionary do not impact the source.
+     *
+     * If the overrideId already exists in the source data dictionary, then the override file (byte[]) will replace the file with the same id in the same load order.
+     * If the overrideId does not exist, then the override file (byte[]) will be added to the end of the load order as a new override.
+     */
+    public static DataDictionary createNewWithOverride(DataDictionary old, String overrideId, byte[] override) throws DataDictionaryException {
+        final DataDictionary modifiedDataDictionary = copyDataDictionaryWithOverride(old);
 
-        final Map<String, List<Resource>> previousFiles = old.getModuleDictionaryFiles();
-        modifiedDataDictionary.setModuleDictionaryFiles(previousFiles);
+        addOrReplaceOverrideResource(modifiedDataDictionary, overrideId, new InMemoryResource(override, overrideId));
 
-        final List<String> previousLoadOrder = old.getModuleLoadOrder();
-        if (!previousLoadOrder.contains(DataDictionaryOverrideConstants.OVERRIDE)) {
-            previousLoadOrder.add(DataDictionaryOverrideConstants.OVERRIDE);
-        }
+        modifiedDataDictionary.parseDataDictionaryConfigurationFiles();
+        modifiedDataDictionary.validateDD();
 
-        modifiedDataDictionary.setModuleLoadOrder(previousLoadOrder);
-
-        modifiedDataDictionary.addModuleDictionaryFile(DataDictionaryOverrideConstants.OVERRIDE, newOverride);
-        try {
-            modifiedDataDictionary.parseDataDictionaryConfigurationFiles();
-            modifiedDataDictionary.validateDD();
-        } catch (RuntimeException e) {
-            previousFiles.get(DataDictionaryOverrideConstants.OVERRIDE).remove(newOverride);
-            throw e;
-        }
         return modifiedDataDictionary;
+    }
+
+    /**
+     * This method creates a new DataDictionary instance based on an old instance conditionally removing an in-memory data dictionary override file.
+     * The internal state of the source data dictionary is copied to the target so that updates to the target data dictionary do not impact the source.
+     *
+     * If the overrideId already exists in the source data dictionary, then the override file (byte[]) removed.
+     * If the overrideId does not exist, then nothing is removed.
+     */
+    public static DataDictionary createNewRemovingOverride(DataDictionary old, String overrideId) throws DataDictionaryException {
+        final DataDictionary modifiedDataDictionary = copyDataDictionaryWithOverride(old);
+
+        removeOverrideResource(modifiedDataDictionary, overrideId);
+
+        modifiedDataDictionary.parseDataDictionaryConfigurationFiles();
+        modifiedDataDictionary.validateDD();
+
+        return modifiedDataDictionary;
+    }
+
+    private static DataDictionary copyDataDictionaryWithOverride(DataDictionary old) {
+        final DataDictionary copiedDataDictionary = new DataDictionary();
+        copiedDataDictionary.setModuleDictionaryFiles(copyModuleDictionaryFilesWithOverride(old.getModuleDictionaryFiles()));
+        copiedDataDictionary.setModuleLoadOrder(copyModuleLoadOrderWithOverride(old.getModuleLoadOrder()));
+
+        return copiedDataDictionary;
+    }
+
+    private static List<String> copyModuleLoadOrderWithOverride(List<String> loadOrder) {
+        final List<String> copiedLoadOrder = new ArrayList<>(loadOrder);
+        if (!copiedLoadOrder.contains(DataDictionaryOverrideConstants.OVERRIDE)) {
+            copiedLoadOrder.add(DataDictionaryOverrideConstants.OVERRIDE);
+        }
+        return copiedLoadOrder;
+    }
+
+    private static Map<String, List<Resource>> copyModuleDictionaryFilesWithOverride(Map<String, List<Resource>> moduleDictionaryFiles) {
+        final Map<String, List<Resource>> copiedResources = copyModuleDictionaryFiles(moduleDictionaryFiles);
+        copiedResources.putIfAbsent(DataDictionaryOverrideConstants.OVERRIDE, new ArrayList<>());
+        return copiedResources;
+    }
+
+    private static Map<String, List<Resource>> copyModuleDictionaryFiles(Map<String, List<Resource>> moduleDictionaryFiles) {
+        return moduleDictionaryFiles.entrySet().stream()
+                .map(entry -> CollectionUtils.<String, List<Resource>>entry(entry.getKey(), new ArrayList<>(entry.getValue())))
+                .collect(entriesToMap());
+    }
+
+    private static void addOrReplaceOverrideResource(DataDictionary dataDictionary, String overrideId, Resource resource) {
+        final List<Resource> overrideFiles = dataDictionary.getModuleDictionaryFiles().get(DataDictionaryOverrideConstants.OVERRIDE);
+        int previousOverrideIndex = -1;
+        for (int i = 0; i < overrideFiles.size(); i++) {
+            final Resource r = overrideFiles.get(i);
+            if (r instanceof InMemoryResource && overrideId.equals(r.getDescription())) {
+                previousOverrideIndex = i;
+                break;
+            }
+        }
+
+        if (previousOverrideIndex != -1) {
+            dataDictionary.getModuleDictionaryFiles().get(DataDictionaryOverrideConstants.OVERRIDE).set(previousOverrideIndex, resource);
+        } else {
+            dataDictionary.getModuleDictionaryFiles().get(DataDictionaryOverrideConstants.OVERRIDE).add(resource);
+        }
+    }
+
+    private static void removeOverrideResource(DataDictionary dataDictionary, String overrideId) {
+        final List<Resource> overrideFiles = dataDictionary.getModuleDictionaryFiles().get(DataDictionaryOverrideConstants.OVERRIDE);
+        for (Iterator<Resource> i = overrideFiles.iterator(); i.hasNext();) {
+            final Resource r = i.next();
+            if (r instanceof InMemoryResource && overrideId.equals(r.getDescription())) {
+                i.remove();
+                break;
+            }
+        }
     }
 }
