c19a24166dc6651733a7576805df10a989afe549
mukadder <mukadder@bu.edu>
2016-11-29 12:02:00 -0500

    BUKC - BU changes for FandADistributions not sticking to awardFandADistributions

--- coeus-impl/src/main/java/org/kuali/kra/timeandmoney/service/impl/TimeAndMoneyServiceImpl.java
+++ coeus-impl/src/main/java/org/kuali/kra/timeandmoney/service/impl/TimeAndMoneyServiceImpl.java
@@ -1,18 +1,18 @@
 /*
  * Kuali Coeus, a comprehensive research administration system for higher education.
- * 
+ *
  * Copyright 2005-2016 Kuali, Inc.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -313,6 +313,11 @@ public class TimeAndMoneyServiceImpl implements TimeAndMoneyService {
     */
     private boolean mustSetFandADistributions(List<AwardDirectFandADistribution> awardFandADistributions, List<AwardDirectFandADistribution> tAndMFandADistributions) {
         boolean needToSave = false;
+        //BU changes
+		if (awardFandADistributions.size() != tAndMFandADistributions.size()) {
+			needToSave = true;
+			return needToSave;
+		}
         for (AwardDirectFandADistribution awardDistribution: awardFandADistributions) {
             boolean found = false;
             for (AwardDirectFandADistribution tAndMDistribution: tAndMFandADistributions) {
@@ -324,6 +329,7 @@ public class TimeAndMoneyServiceImpl implements TimeAndMoneyService {
                 needToSave = true;
             }
         }
+        //End BU changes
         return needToSave;
     }
 
